51368fa02750ce5cccca736ce889e91033c89d8d
Amit Kohli <amit.kohli@t-mobile.com>
2009-03-17 11:33:23 -0700

    removed warnings

--- core/java/com/tmobile/widget/PlutoTabWidget.java
+++ core/java/com/tmobile/widget/PlutoTabWidget.java
@@ -2,7 +2,6 @@ package com.tmobile.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TabWidget;
