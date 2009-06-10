161c6505160ee4424208bd3387e2eedf0f039e6b
Chris Cogar <chriscogar@t-mobile.com>
2009-06-10 11:40:07 -0400

    updated widget, added graphic-setting methods

--- core/java/com/tmobile/widget/ListItemIconTwinLabelSecondaryText.java
+++ core/java/com/tmobile/widget/ListItemIconTwinLabelSecondaryText.java
@@ -1,15 +1,16 @@
 package com.tmobile.widget;
 
+import com.android.internal.R;
+
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.android.internal.R;
 
 public class ListItemIconTwinLabelSecondaryText extends LinearLayout {
 
@@ -95,26 +96,61 @@ public class ListItemIconTwinLabelSecondaryText extends LinearLayout {
 		return mImage1.getDrawable();
 	}
 
-	public void setIcon1(Drawable image1) {
-		mImage1.setImageDrawable(image1);
-	}
+    public void setIcon1(Drawable image1) {
+        mImage1.setImageDrawable(image1);
+    }
+    
+    public void setIcon1(Bitmap image1) {
+        mImage1.setImageBitmap(image1);
+    }
+    
+    public void setIcon1ImageResource(int resId) {
+        mImage1.setImageResource(resId);
+    }
+
+    public void setIcon1Visibility(int flag) {
+        mImage1.setVisibility(flag);
+    }
 
 	public Drawable getIcon2() {
 		return mImage2.getDrawable();
 	}
 
-	public void setIcon2(Drawable image2) {
-		mImage2.setImageDrawable(image2);
-	}
-	
+    public void setIcon2(Drawable image2) {
+        mImage2.setImageDrawable(image2);
+    }
+
+    public void setIcon2(Bitmap image2) {
+        mImage2.setImageBitmap(image2);
+    }
+    
+    public void setIcon2ImageResource(int resId) {
+        mImage2.setImageResource(resId);
+    }
+
+    public void setIcon2Visibility(int flag) {
+        mImage2.setVisibility(flag);
+    }
 	
 	public Drawable getIcon3() {
 		return mImage3.getDrawable();
 	}
 
-	public void setIcon3(Drawable image3) {
-		mImage3.setImageDrawable(image3);
-	}
+    public void setIcon3(Drawable image3) {
+        mImage3.setImageDrawable(image3);
+    }
+
+    public void setIcon3(Bitmap image3) {
+        mImage3.setImageBitmap(image3);
+    }
+    
+    public void setIcon3ImageResource(int resId) {
+        mImage3.setImageResource(resId);
+    }
+
+    public void setIcon3Visibility(int flag) {
+        mImage3.setVisibility(flag);
+    }
 
 	public Drawable getIcon4() {
 		return mImage4.getDrawable();
@@ -123,5 +159,17 @@ public class ListItemIconTwinLabelSecondaryText extends LinearLayout {
 	public void setIcon4(Drawable image4) {
 		mImage4.setImageDrawable(image4);
 	}
+
+    public void setIcon4(Bitmap image4) {
+        mImage4.setImageBitmap(image4);
+    }
+    
+    public void setIcon4ImageResource(int resId) {
+        mImage4.setImageResource(resId);
+    }
+
+    public void setIcon4Visibility(int flag) {
+        mImage4.setVisibility(flag);
+    }
 	
 }
