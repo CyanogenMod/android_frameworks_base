f47acd5d16f3ee51ea7e96d5781e9450f662ad41
Agarwal, Shirish Kumar <shirisha@qualcomm.com>
2009-08-15 21:15:46 +0530

    Wifi: Moved setting the Wiif State to ENABLING/DISABLING in the if-else condition.
    Earlier it was before the if condition because of that the
    previous wifi statewas overwritten. previous wifi state is required to
    disable the wifi only ifit was enabled earlier.
    
    CRs-Fixed: 191491

--- services/java/com/android/server/WifiService.java
+++ services/java/com/android/server/WifiService.java
@@ -491,10 +491,10 @@ public class WifiService extends IWifiManager.Stub {
         if (enable && isAirplaneModeOn()) {
             return false;
         }
-        setWifiEnabledState(enable ? WIFI_STATE_ENABLING : WIFI_STATE_DISABLING, uid);
 
 
         if (enable) {
+            setWifiEnabledState(WIFI_STATE_ENABLING, uid);
             if (!WifiNative.loadDriver()) {
                 Log.e(TAG, "Failed to load Wi-Fi driver.");
                 setWifiEnabledState(WIFI_STATE_UNKNOWN, uid);
@@ -509,6 +509,7 @@ public class WifiService extends IWifiManager.Stub {
             registerForBroadcasts();
             mWifiStateTracker.startEventLoop();
         } else if (mWifiState == WIFI_STATE_ENABLED) {
+            setWifiEnabledState(WIFI_STATE_DISABLING, uid);
 
             mContext.unregisterReceiver(mReceiver);
            // Remove notification (it will no-op if it isn't visible)
