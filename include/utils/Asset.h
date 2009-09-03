9b2d107d9b3d6c474d465c59d2a0886c1e1b3058
Steve Kondik <shade@chemlab.org>
2009-09-03 19:48:51 -0400

    Raise UNCOMPRESS_DATA_MAX.

--- include/utils/Asset.h
+++ include/utils/Asset.h
@@ -63,11 +63,7 @@ public:
 
     enum {
         /* data larger than this does not get uncompressed into a buffer */
-#ifdef HAVE_ANDROID_OS
-        UNCOMPRESS_DATA_MAX = 1 * 1024 * 1024
-#else
-        UNCOMPRESS_DATA_MAX = 2 * 1024 * 1024
-#endif
+        UNCOMPRESS_DATA_MAX = 3 * 1024 * 1024
     };
 
     /*
