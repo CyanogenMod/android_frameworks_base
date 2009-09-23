d99adc2257ce46ea98504c944b97371dffdcb767
Adrian Salido <c_asalid@quicinc.com>
2009-09-23 13:57:57 -0700

    frameworks/base: fix resuming of video playback when paused on app resume
    For some videos there is a delay in calculating video size, so the video is
    started once this is received. However it should only be started if requested,
    added check for flag used to request start of playback.
    
    CRs-Fixed: 211488

--- core/java/android/widget/VideoView.java
+++ core/java/android/widget/VideoView.java
@@ -411,7 +411,7 @@ public class VideoView extends SurfaceView implements MediaPlayerControl {
                     mMediaPlayer.seekTo(mSeekWhenPrepared);
                     mSeekWhenPrepared = 0;
                 }
-                if (!mIsPlaybackCompleted) {
+                if (mStartWhenPrepared && !mIsPlaybackCompleted) {
                     start();
                 } 
                 if (mMediaController != null) {
