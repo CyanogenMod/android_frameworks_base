ac2c7ec0b9b8390aa5069544eb1ddbbd0b1e07b2
Omprakash Dhyade c_odhyad@quicinc.com <c_odhyad@quicinc.com>
2009-12-03 12:12:02 -0800

    frameworks/base: Add setFd api to Overlay
    setFd() is for client to pass its own buffer id
    for Overlay HW engine to directly access.
    
    Change-Id: I4e727cdb7d34d24b84bde41892694969ad2d4841

--- include/ui/Overlay.h
+++ include/ui/Overlay.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,6 +92,7 @@ public:
 
     /* set the buffer attributes */
     status_t setParameter(int param, int value);
+    status_t setFd(int fd);
 
     /* returns the address of a given buffer if supported, NULL otherwise. */
     void* getBufferAddress(overlay_buffer_t buffer);
