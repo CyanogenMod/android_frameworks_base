75af894961294a7450e1752d7c42463f0999afdb
Srinivasan Kannan <srinkann@quicinc.com>
2009-12-03 15:53:03 -0800

    Camera: Adding Raw Snapshot support
    Support for capturing raw sensor data is enabled

--- libs/camera/CameraParameters.cpp
+++ libs/camera/CameraParameters.cpp
@@ -130,6 +130,8 @@ const char CameraParameters::PIXEL_FORMAT_YUV420SP[] = "yuv420sp";
 const char CameraParameters::PIXEL_FORMAT_YUV422I[] = "yuv422i-yuyv";
 const char CameraParameters::PIXEL_FORMAT_RGB565[] = "rgb565";
 const char CameraParameters::PIXEL_FORMAT_JPEG[] = "jpeg";
+const char CameraParameters::PIXEL_FORMAT_RAW[] = "raw";
+
 
 // Values for focus mode settings.
 const char CameraParameters::FOCUS_MODE_AUTO[] = "auto";
