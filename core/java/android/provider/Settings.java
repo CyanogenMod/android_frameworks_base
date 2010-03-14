644a6fd76d50696dab9b71fab3e401d3735b4615
Steve Kondik <shade@chemlab.org>
2010-03-14 03:35:44 -0400

    Add system setting for trackball wake (metalhead8816)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1679,6 +1679,12 @@ public final class Settings {
         public static final String UNLOCK_SOUND = "unlock_sound";
 
         /**
+         * Whether to wake the screen with the trackball. The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String TRACKBALL_WAKE_SCREEN = "trackball_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          * @hide
