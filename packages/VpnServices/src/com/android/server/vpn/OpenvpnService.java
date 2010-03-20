75b2a25dee28ff6fcd1ff63dac24df36627db0f8
Steve Kondik <shade@chemlab.org>
2010-03-20 03:07:46 -0400

    Set script-security to 2 for OpenVPN.

--- packages/VpnServices/src/com/android/server/vpn/OpenvpnService.java
+++ packages/VpnServices/src/com/android/server/vpn/OpenvpnService.java
@@ -106,6 +106,8 @@ class OpenvpnService extends VpnService<OpenvpnProfile> {
         }
         args.add("--up");
         args.add("/system/xbin/openvpn-up.sh");
+        args.add("--script-security");
+        args.add("2");
 
         DaemonProxy mtpd = getDaemons().startDaemon(MTPD);
         mtpd.sendCommand(args.toArray(new String[args.size()]));
