2e0d4ae4a65699171b3049147f95f423c8f8785c
Sri Lakshmi Punuru <c_spunur@qualcomm.com>
2010-03-24 15:22:39 +0530

    WindowManagerService: Remove window with no app token when not able to dispatch events
    When there is an ANR on a dialog that is not created from an activity, as the app token
    i.e. history record does not exist for this window, window manager service is not able
    to handle the key events properly leading to run in an infinite loop. This appears to be
    a UI freeze in users perspective. Thus removing the window when windowManager is not able
    to dispatch events to a window with no app token.
    
    CRs-Fixed: 229780

--- services/java/com/android/server/WindowManagerService.java
+++ services/java/com/android/server/WindowManagerService.java
@@ -4598,6 +4598,10 @@ public class WindowManagerService extends IWindowManager.Stub
                         //dump();
                         if (targetWin != null) {
                             at = targetWin.getAppToken();
+                            if (at == null) {
+                               removeWindowInnerLocked(targetWin.mSession, targetWin);
+                               return null;
+                            }
                         } else if (targetApp != null) {
                             at = targetApp.appToken;
                         }
