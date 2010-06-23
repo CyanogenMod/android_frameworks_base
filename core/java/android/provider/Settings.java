ed9202b61e77b75c80ad44dbf15731e9aba1236e
ctso <ctsoyars@gmail.com>
2010-06-23 21:45:52 +0000

    Add option for unlocking the phone via trackball/menu
    Conflicts:
    
    	core/java/android/provider/Settings.java
    
    Change-Id: Ieff16aa5f0f7abcb88470fd297706cd381eb4074

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1685,6 +1685,18 @@ public final class Settings {
         public static final String TRACKBALL_WAKE_SCREEN = "trackball_wake_screen";
 
         /**
+         * Whether to unlock the screen with the trackball.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String TRACKBALL_UNLOCK_SCREEN = "trackball_unlock_screen";
+
+        /**
+         * Whether to unlock the menu key.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String MENU_UNLOCK_SCREEN = "menu_unlock_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          * @hide
