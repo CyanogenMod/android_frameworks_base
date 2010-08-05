af5f700ef9034a238f95ab8d5e58292969ed1316
Pedlar <pedlar88@gmail.com>
2010-08-04 21:46:29 -0400

    re merging

--- libs/surfaceflinger/LayerBase.cpp
+++ libs/surfaceflinger/LayerBase.cpp
@@ -39,6 +39,7 @@
 #define RENDER_EFFECT_AMBER 4
 #define RENDER_EFFECT_SALMON 5
 #define RENDER_EFFECT_FUSCIA 6
+#define RENDER_EFFECT_N1_CALIBRATED 7
 
 namespace android {
 
@@ -466,6 +467,9 @@ void LayerBase::drawWithOpenGL(const Region& clip, const Texture& texture) const
                 break;
             case RENDER_EFFECT_FUSCIA:
                 glColor4x(alpha, 0, alpha*0.5, alpha);
+                break;
+            case RENDER_EFFECT_N1_CALIBRATED:
+                glColor4x(alpha, alpha*0.98, alpha*0.82, alpha);
                 break;
         }
         glEnable(GL_BLEND);
