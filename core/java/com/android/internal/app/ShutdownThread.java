7da356427fb057898bed7368f44f8bc80a728535
Mathias Jeppsson <mathias.jeppsson@sonyericsson.com>
2010-08-17 13:33:09 +0200

    Crash when powering off.
    Fixing a race condition. If "confirm" is true and the shutdown
    dialog is shown it may take some time before the user presses the
    ok button. During this time a new call to shutdown may be made with
    confirm=false. When this happens the shutdown thread is started by
    the second call, and later when the users presses the ok button. This
    results in ShutdownThread.beginShutdownSequence() being called
    twice. I.e., the beginShutdownSequence lacks the proper protection
    for this error case.
    
    Change-Id: Ib9b01c5a43f4dc23de09057f3fc1507000317faf

--- core/java/com/android/internal/app/ShutdownThread.java
+++ core/java/com/android/internal/app/ShutdownThread.java
@@ -162,6 +162,10 @@ public final class ShutdownThread extends Thread {
 
     private static void beginShutdownSequence(Context context) {
         synchronized (sIsStartedGuard) {
+            if (sIsStarted) {
+                Log.d(TAG, "Shutdown sequence already running, returning.");
+                return;
+            }
             sIsStarted = true;
         }
 
