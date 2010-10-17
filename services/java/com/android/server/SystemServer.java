ac06111695e71f704d263f9c42af0b28faf3a43f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-10-17 13:59:25 +0100

    Add RingerSwitch support
    This patch adds support for the Geeksphone ONE's MUTE hard key.
    I don't know of any other device with such a key, so this will
    defaults to disabled. It's enabled by setting the "ro.config.ringerswitch"
    property to 1
    
    Change-Id: Ica18f3f3ee872b4b911ddd79b95ba569cb13f4e8

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -102,6 +102,7 @@ class ServerThread extends Thread {
         UiModeManagerService uiMode = null;
         RecognitionManagerService recognition = null;
         ThrottleService throttle = null;
+        RingerSwitchObserver ringer = null;
 
         // Critical services...
         try {
@@ -372,6 +373,16 @@ class ServerThread extends Thread {
             }
 
             try {
+                if (SystemProperties.get("ro.config.ringerswitch").equals("1")) {
+                    Slog.i(TAG, "RingerSwitch Observer");
+                    // Listen for hard ringer switch changes
+                    ringer = new RingerSwitchObserver(context);
+                }
+            } catch (Throwable e) {
+                Slog.e(TAG, "Failure starting RingerSwitchObserver", e);
+            }
+
+            try {
                 Slog.i(TAG, "Dock Observer");
                 // Listen for dock station changes
                 dock = new DockObserver(context, power);
@@ -422,6 +433,7 @@ class ServerThread extends Thread {
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting DiskStats Service", e);
             }
+
         }
 
         // make sure the ADB_ENABLED setting value matches the secure property value
