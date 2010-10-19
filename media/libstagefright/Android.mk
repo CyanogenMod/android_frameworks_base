402b66da42d06a675c8f3f4a29e926b67c1e7a79
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2010-10-19 21:45:37 +0100

    libstagefright: Simplify platform filter, add new platform
    Reduce the number of checks to 1, and add the msm7625 variant to the check
    
    Change-Id: I9dbe0ac125bdb06dbc4468732953d498a0865370

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -100,13 +100,7 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_SHARED_LIBRARIES += libdl
 endif
 
-ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-        LOCAL_CFLAGS += -DUSE_QCOM_OMX_FIX
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
-        LOCAL_CFLAGS += -DUSE_QCOM_OMX_FIX
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+ifneq ($(filter qsd8k msm7k msm7625 msm7x30, $(TARGET_BOARD_PLATFORM)),)
         LOCAL_CFLAGS += -DUSE_QCOM_OMX_FIX
 endif
 
