bc097e94dfd15ecaac4d057d30c68d9c77a54744
Manuel Auer <auer.manuel@gmail.com>
2010-10-27 08:27:03 +0200

    Changing string for apn from empty to  when no string is provided by SIM Card. This fixes the roaming problem with some virtual network providers, since some of them have an empty string (check with getprop gsm.sim.operator.alpha). For sure no provider will write empty in their SIM cards. Data roaming can be disabled after that. Roaming in foreign countrys will still work since it checks the MCC (Mobile Country Code). Thanks to my friend Sewi for helping me finding this. :)
    Change-Id: I26ddb849963701acc8a2b1f36fdd18705d46550d

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1286,7 +1286,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
      * @return true for roaming state set
      */
     private boolean isRoamingBetweenOperators(boolean gsmRoaming, ServiceState s) {
-        String spn = SystemProperties.get(TelephonyProperties.PROPERTY_ICC_OPERATOR_ALPHA, "empty");
+        String spn = SystemProperties.get(TelephonyProperties.PROPERTY_ICC_OPERATOR_ALPHA, "");
 
         String onsl = s.getOperatorAlphaLong();
         String onss = s.getOperatorAlphaShort();
