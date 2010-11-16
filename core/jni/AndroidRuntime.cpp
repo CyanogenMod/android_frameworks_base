8be436fd4431f694a5787f278048b22425ae20e0
Nan YE <nan.yeah@gmail.com>
2010-11-16 23:24:56 +0800

    Fixed: cannot read out correct heap size from dalvik.vm.heapsize
    Change-Id: Ia9b61fcbcd2e6c69d11699ed5e6c935760650eed

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -658,7 +658,7 @@ int AndroidRuntime::startVm(JavaVM** pJavaVM, JNIEnv** pEnv)
     strcpy(heapsizeOptsBuf, "-Xmx");
     property_get("persist.sys.vm.heapsize", propBuf, "");
     if (strcmp(propBuf, "") == 0) {
-        property_get("dalvim.vm.heapsize", propBuf, "16m");
+        property_get("dalvik.vm.heapsize", propBuf, "16m");
     }
     strcpy(heapsizeOptsBuf+4, propBuf);
     LOGI("Heap size: %s", heapsizeOptsBuf);
