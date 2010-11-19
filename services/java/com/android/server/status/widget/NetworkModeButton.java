99cd4681672d8520a4f50d6644f856beb65184ed
nadlabak <pavel@doshaska.net>
2010-11-19 12:43:48 -0800

    Fix 2g/3g toggle on UMTS networks (the same fix has been already applied to Settings app power widget)
    Change-Id: I255359ba4a6ba3d384140c4c9bd5f73e780b9037

--- services/java/com/android/server/status/widget/NetworkModeButton.java
+++ services/java/com/android/server/status/widget/NetworkModeButton.java
@@ -44,6 +44,7 @@ public class NetworkModeButton extends PowerButton{
         switch(networkMode) {
             case Phone.NT_MODE_WCDMA_PREF:
             case Phone.NT_MODE_WCDMA_ONLY:
+            case Phone.NT_MODE_GSM_UMTS:
                 return PowerButton.STATE_ENABLED;
             case Phone.NT_MODE_GSM_ONLY:
                 return PowerButton.STATE_DISABLED;
@@ -98,6 +99,7 @@ public class NetworkModeButton extends PowerButton{
         Intent intent = new Intent(MODIFY_NETWORK_MODE);
         switch (networkMode ) {
         case Phone.NT_MODE_WCDMA_PREF:
+        case Phone.NT_MODE_GSM_UMTS:
             intent.putExtra(NETWORK_MODE, Phone.NT_MODE_GSM_ONLY);
             currentInternalState = PowerButton.STATE_TURNING_OFF;
             intendedNetworkMode=Phone.NT_MODE_GSM_ONLY;
