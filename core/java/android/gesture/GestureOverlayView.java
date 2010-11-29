a814ad7ab5d915224f54e5cd2c89b755414547a8
Michael Webster <miketwebster@gmail.com>
2010-11-28 20:03:12 -0500

    Gestures fix
    - added 'none' to xml attributes for 'orientation' -
    - Updated GestureOverlayView to allow an orientation of 'none.
    
    This allows the lockscreen to recognize gestures in any direction,
    and of any complexity.

--- core/java/android/gesture/GestureOverlayView.java
+++ core/java/android/gesture/GestureOverlayView.java
@@ -58,6 +58,7 @@ public class GestureOverlayView extends FrameLayout {
 
     public static final int ORIENTATION_HORIZONTAL = 0;
     public static final int ORIENTATION_VERTICAL = 1;
+    public static final int ORIENTATION_NONE = 2;
 
     private static final int FADE_ANIMATION_RATE = 16;
     private static final boolean GESTURE_RENDERING_ANTIALIAS = true;
@@ -645,7 +646,7 @@ public class GestureOverlayView extends FrameLayout {
                         angle = 180 - angle;
                     }
 
-                    if (box.squareness > mGestureStrokeSquarenessTreshold ||
+                    if (mOrientation == ORIENTATION_NONE || box.squareness > mGestureStrokeSquarenessTreshold ||
                             (mOrientation == ORIENTATION_VERTICAL ?
                                     angle < mGestureStrokeAngleThreshold :
                                     angle > mGestureStrokeAngleThreshold)) {
