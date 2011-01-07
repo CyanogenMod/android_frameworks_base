e97f0c525ce91971cc4859e7980343a395c0822d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-01-07 19:31:51 +0000

    Add overscroll effects configuration
    I has my bouncy again!
    Includes the fixes done in CM6, and Robert
    Burns' weight configuration
    
    Change-Id: Ib0a90b47b80ac8b213fd137340419a31dc5fbc1a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2279,6 +2279,16 @@ public final class Settings {
         /** @hide */
         public static final String ELECTRON_BEAM_ANIMATION_OFF = "electron_beam_animation_off";
 
+        /** @hide */
+        public static final String OVERSCROLL_EFFECT = "overscroll_effect";
+
+        /**
+         * Sets the overscroller weight (edge bounce effect on lists)
+         * @hide
+         */
+        public static final String OVERSCROLL_WEIGHT = "overscroll_weight";
+
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
