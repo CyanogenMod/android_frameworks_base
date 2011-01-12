7506b729b8feb5291f31db6b2a8b34abf30745e6
Sven Dawitz <sven@dawitz.de>
2011-01-12 15:26:12 +0100

    Rotate 180 degree setting
    cm61 had a similar setting, which i could not port
    since WindowOrientationListener.java changed a lot.
    This patch will reflect the original idea and is
    working pretty well.
    
    Change-Id: I8deaf6654f186dfa1d877409418983bfe9e6daba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1724,25 +1724,33 @@ public final class Settings {
          * disabled by the application.
          */
         public static final String ACCELEROMETER_ROTATION = "accelerometer_rotation";
-        
+
+         /**
+         * Control weather 180 degree rotation should be included
+         * if ACCELEROMETER_ROTATION is enabled. If 0 no 180 degree rotation will be
+         * executed, if 1 the 180 degree rotation is executed when ACCELEROMETER_ROTATION is true.
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATE_180 = "accelerometer_rotate_180";
+
         /**
          * Specifies the number of recent apps to show (8, 12, 16)
          * @hide
          */
         public static final String RECENT_APPS_NUMBER = "recent_apps_number";
-        
+
         /**
          * Specifies the number of recent apps to show (8, 12, 16)
          * @hide
          */
         public static final String RECENT_APPS_SHOW_TITLE = "recent_apps_show_title";
-        
+
         /**
          * Specifies whether or not to use a custom app instead of the recent applications dialog
          * @hide
          */
         public static final String USE_CUSTOM_APP = "use_custom_app";
-        
+
         /**
          * Stores the uri of the custom application to use
          * @hide
@@ -1877,7 +1885,7 @@ public final class Settings {
          * @hide
          */
         public static final String HAPTIC_UP_ARRAY = "haptic_up_array";
-        
+
         /**
          * Value for long presses - same format as _DOWN_ARRAY
          * @hide
@@ -2212,7 +2220,7 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_ALWAYS_MUSIC_CONTROLS = "lockscreen_always_music_controls";
-        
+
         /**
          * Whether to listen for gestures on the lockscreen
          * @hide
@@ -2346,6 +2354,7 @@ public final class Settings {
             TIME_12_24,
             DATE_FORMAT,
             ACCELEROMETER_ROTATION,
+            ACCELEROMETER_ROTATE_180,
             DTMF_TONE_WHEN_DIALING,
             DTMF_TONE_TYPE_WHEN_DIALING,
             EMERGENCY_TONE,
@@ -4104,7 +4113,7 @@ public final class Settings {
          * @hide
          */
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_on_longpress_back";
-        
+
         /**
          * Whether to disable the lockscreen unlock tab
          * @hide
