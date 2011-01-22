830b9bbb9f90a34205e4fc00796a9233cc4ab456
repo sync <martin@longhome.co.uk>
2011-01-22 18:16:03 +0000

    Added notification profile support.
    This adds finer-grained support for notification tones, and groups settings into profiles,
    which can be easily switched. API support is also added, allowing configuration, and profile
    switching from another program (e.g. Tasker, Locale).
    
    A profile represents a particular set of settings, and could be for example "Silent",
    "Office", or "Night". Each profile contains the settings for a definable set of application
    groups.
    
    An application group represents a group of applications to which the settings should apply.
    For example "SMS" might contain "Messaging" and "Handcent".
    
    The applications in an application group are globally defined. The notification settings for
    an application group are per-profile.
    
    For each profile/application group you can define behaviour for lights/vibrate and sound.
    Either you can suppress, override, or leave unchanged (ie system, or app default).
    
    Example:
    
    Work : SMS vibrate. Ringer vibrate. Email no alert. Gmail vibrate.
    Night: SMS silent. Ringer on. Email silent. Gmail silent.
    
    There is still a lot of work to be done here. A lot of code tidy up, javadocs, standards etc.
    Might be suitable for nightlies. I have only added notification support - I plan to either
    add AudioManager support or update the Phone application, depending on which is most
    appropriate.
    
    This has been on dumb Nokias for years, I'm surprised a single smartphone doesn't have it yet.
    
    Change-Id: I9662651dd597392dfe915f8cd81d66064f00bb76

---
+++ core/java/android/app/NotificationGroup.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NotificationGroup implements Parcelable {
+
+    private String mName;
+
+    private Set<String> mPackages = new HashSet<String>();
+
+    public static final Parcelable.Creator<NotificationGroup> CREATOR = new Parcelable.Creator<NotificationGroup>() {
+        public NotificationGroup createFromParcel(Parcel in) {
+            return new NotificationGroup(in);
+        }
+
+        @Override
+        public NotificationGroup[] newArray(int size) {
+            return new NotificationGroup[size];
+        }
+    };
+
+    public NotificationGroup(String name) {
+        this.mName = name;
+    }
+
+    private NotificationGroup(Parcel in) {
+        readFromParcel(in);
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void addPackage(String pkg) {
+        mPackages.add(pkg);
+    }
+
+    public String[] getPackages() {
+        return mPackages.toArray(new String[mPackages.size()]);
+    }
+
+    public void removePackage(String pkg) {
+        mPackages.remove(pkg);
+    }
+
+    public boolean hasPackage(String pkg) {
+        boolean result = mPackages.contains(pkg);
+        Log.i("PROFILE", "Group: " + mName + " containing : " + pkg + " : " + result);
+        return result;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+        dest.writeStringArray(getPackages());
+    }
+
+    public void readFromParcel(Parcel in) {
+        mName = in.readString();
+        mPackages.addAll(Arrays.asList(in.readStringArray()));
+    }
+
+    public String getXmlString() {
+        StringBuilder builder = new StringBuilder();
+        getXmlString(builder);
+        return builder.toString();
+    }
+
+    public void getXmlString(StringBuilder builder) {
+        builder.append("<notificationGroup name=\"" + getName() + "\">\n");
+        for (String pkg : mPackages) {
+            builder.append("<package>" + pkg + "</package>\n");
+        }
+        builder.append("</notificationGroup>\n");
+    }
+
+    public static NotificationGroup fromXml(XmlPullParser xpp) throws XmlPullParserException,
+            IOException {
+        NotificationGroup notificationGroup = new NotificationGroup(xpp.getAttributeValue(null,
+                "name"));
+        int event = xpp.next();
+        while (event != XmlPullParser.END_TAG || !xpp.getName().equals("notificationGroup")) {
+            if (event == XmlPullParser.START_TAG) {
+                String name = xpp.getName();
+                if (name.equals("package")) {
+                    String pkg = xpp.nextText();
+                    notificationGroup.addPackage(pkg);
+                }
+            }
+            event = xpp.next();
+        }
+        return notificationGroup;
+    }
+}
