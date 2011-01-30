b00dcebfd7db952f41a9ab3be9c7fbea0dc0187a
Steve Kondik <shade@chemlab.org>
2011-01-30 17:42:58 -0500

    Don't bail if we can't set WAL mode on an SQLite database
    Change-Id: Iabfe6830ea10df50f655b84ba3b24507f3f1ed4e

--- core/jni/android_database_SQLiteDatabase.cpp
+++ core/jni/android_database_SQLiteDatabase.cpp
@@ -126,19 +126,17 @@ static void dbopen(JNIEnv* env, jobject object, jstring pathString, jint flags)
     // Configure databases to run in WAL mode.
     err = sqlite3_exec(handle,"PRAGMA journal_mode = WAL;",
                        NULL, NULL,&zErrMsg);
-    if (SQLITE_OK != err) {
+    if (SQLITE_OK == err) {
+        // Set autocheckpoint = 100 pages
+        err = sqlite3_wal_autocheckpoint(handle,
+                                         100);
+        if (SQLITE_OK != err) {
+            LOGE("sqlite3_exec to set WAL autocheckpoint failed\n");
+            throw_sqlite3_exception(env, handle);
+            goto done;
+        }
+    } else {
        LOGE("sqlite3_exec to set journal_mode = WAL failed\n");
-       throw_sqlite3_exception(env, handle);
-       goto done;
-    }
-
-    // Set autocheckpoint = 100 pages
-    err = sqlite3_wal_autocheckpoint(handle,
-                                     100);
-    if (SQLITE_OK != err) {
-       LOGE("sqlite3_exec to set WAL autocheckpoint failed\n");
-       throw_sqlite3_exception(env, handle);
-       goto done;
     }
 
     // The soft heap limit prevents the page cache allocations from growing
