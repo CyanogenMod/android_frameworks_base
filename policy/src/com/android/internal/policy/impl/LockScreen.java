f819475da83bc1416f758ff55f22fcfd6a70c7d2
Sven Dawitz <sven@dawitz.de>
2011-02-16 11:56:20 +0100

    Lockscreen fix: AM/PM string always shown - ignoring 24h format
    Change-Id: Iddaf13dd08f858e819e1da3fe8635fdba01fc6d8

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -129,6 +129,10 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
     private String mDateFormatString;
     private boolean mEnableMenuKeyInLockScreen;
 
+    // time format from system settings - contains 12 or 24
+    private int mTime12_24 = (Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.TIME_12_24, 12));
+
     private boolean mTrackballUnlockScreen = (Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.TRACKBALL_UNLOCK_SCREEN, 0) == 1);
 
@@ -295,6 +299,8 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
 
         mTime = (TextView) findViewById(R.id.timeDisplay);
         mAmPm = (TextView) findViewById(R.id.am_pm);
+        if(mTime12_24 == 24)
+            mAmPm.setVisibility(View.GONE);
         mDate = (TextView) findViewById(R.id.date);
         mStatus1 = (TextView) findViewById(R.id.status1);
         mStatus2 = (TextView) findViewById(R.id.status2);
@@ -1284,6 +1290,9 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
         mAlbumArt.setVisibility(visibility);
         mStatus1.setVisibility(visibility);
         mStatus2.setVisibility(visibility);
+
         mNowPlayingScreen=(visibility == View.VISIBLE);
+        if(mTime12_24 == 24)
+            mAmPm.setVisibility(View.GONE);
     }
 }
