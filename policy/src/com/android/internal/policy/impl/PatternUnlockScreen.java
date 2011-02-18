d06fa3658a2821b8bf130e64f002b733fc615921
Sven Dawitz <sven@dawitz.de>
2011-02-18 21:54:48 +0100

    Fixed issue 2846: Pattern lockscreen displays %d%% instead of bat level
    Change-Id: Ia5886718336457733645ca816cf1b3a4aa407bd4

--- policy/src/com/android/internal/policy/impl/PatternUnlockScreen.java
+++ policy/src/com/android/internal/policy/impl/PatternUnlockScreen.java
@@ -312,7 +312,7 @@ class PatternUnlockScreen extends LinearLayoutWithDefaultTouchRecepient
                   mStatus1.setText(getContext().getString(R.string.lockscreen_plugged_in, mBatteryLevel));
               }
             } else {
-                mStatus1.setText(getContext().getString(R.string.lockscreen_low_battery));
+                mStatus1.setText(getContext().getString(R.string.lockscreen_low_battery, mBatteryLevel));
             }
             mStatus1.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_lock_idle_charging, 0, 0, 0);
 
