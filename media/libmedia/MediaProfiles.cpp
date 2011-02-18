a253734423f1c998349f90a106acc613365e1844
Ravi Kumar Alamanda <ralama@codeaurora.org>
2011-02-18 16:25:40 +0530

    frameworks/base: Add support for QCELP/EVRC non-tunnel encoding in SF
    Add support for QCELP and EVRC formats non-tunnel mode encoding
    in StageFright using hardware accelerated encoders.
    
    Patchset 1: Add QCOM_HARDWARE ifdefs.
    Patchset 2: Fix ifdef derp.
    Patchset 3: Fix endif derp, change omx-core include to vendor/qcom.
    
    Conflicts:
    
    	media/libstagefright/Android.mk
    	media/libstagefright/OMXCodec.cpp
    
    Change-Id: I179030d71a9598ddddcf754e963fbed1e1357024
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- media/libmedia/MediaProfiles.cpp
+++ media/libmedia/MediaProfiles.cpp
@@ -817,6 +817,9 @@ MediaProfiles::createDefaultCamcorderProfiles(MediaProfiles *profiles)
 MediaProfiles::createDefaultAudioEncoders(MediaProfiles *profiles)
 {
     profiles->mAudioEncoders.add(createDefaultAmrNBEncoderCap());
+#ifdef QCOM_HARDWARE
+    profiles->mAudioEncoders.add(createDefaultAacEncoderCap());
+#endif
 }
 
 /*static*/ void
@@ -850,6 +853,15 @@ MediaProfiles::createDefaultAmrNBEncoderCap()
     return new MediaProfiles::AudioEncoderCap(
         AUDIO_ENCODER_AMR_NB, 5525, 12200, 8000, 8000, 1, 1);
 }
+
+#ifdef QCOM_HARDWARE
+/*static*/ MediaProfiles::AudioEncoderCap*
+MediaProfiles::createDefaultAacEncoderCap()
+{
+    return new MediaProfiles::AudioEncoderCap(
+        AUDIO_ENCODER_AAC, 64000, 156000, 8000, 48000, 1, 2);
+}
+#endif
 
 /*static*/ void
 MediaProfiles::createDefaultImageEncodingQualityLevels(MediaProfiles *profiles)
