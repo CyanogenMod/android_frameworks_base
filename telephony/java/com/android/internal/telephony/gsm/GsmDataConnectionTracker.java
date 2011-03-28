a799afd72ffc689de57a50e3e9c2c8d092d9ccec
Kostya Dmitriev <kostya@dmitriev.in.ua>
2011-03-28 15:27:59 +0300

    Fix for what seems to be a typo responsible for some cases of issue 2207 (http://code.google.com/p/android/issues/detail?id=2207)
    Change-Id: I61503990035718fef98e61f020fc7799e848424c

--- telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
@@ -440,7 +440,7 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
         boolean desiredPowerState = mGsmPhone.mSST.getDesiredPowerState();
 
         if ((state == State.IDLE || state == State.SCANNING)
-                && (gprsState == ServiceState.STATE_IN_SERVICE || noAutoAttach)
+                && (gprsState == ServiceState.STATE_IN_SERVICE || !noAutoAttach)
                 && mGsmPhone.mSIMRecords.getRecordsLoaded()
                 && (mGsmPhone.mSST.isConcurrentVoiceAndData() ||
                         phone.getState() == Phone.State.IDLE )
