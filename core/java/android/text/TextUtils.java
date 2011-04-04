CyanogenMod/android_frameworks_base

commit 43e45eb94e1aa0d1c4dbd10e99df8e366f637261

-Adds RTL (Right-To-Left) support for all RTL languages -Paragraphs containing RTL text are consistently aligned to the right and text is measured correctly when reshaped -Supports reshaping (connecting) of standard Arabic letters, including Farsi, Kurdish, Turkish and Urdu -Supports reshaping of supplement Arabic letters used in languages such as Farsi and Urdu -Mirroring of symetric symbols such as '(' and ')' when surrouded by RTL text -Tested by several Arabic native speakers
By Eyad Aboulouz.

Change-Id: Ifd3e24cde0c68c97245e97cf41b71c3701078e48

core/java/android/text/TextUtils.java
@@ -43,10 +43,14 @@ import android.text.style.TextAppearanceSpan;
 import android.text.style.TypefaceSpan;
 import android.text.style.URLSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.util.Printer;
 
 import com.android.internal.util.ArrayUtils;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.regex.Pattern;
 import java.util.Iterator;
 
@@ -1679,6 +1683,117 @@ public class TextUtils {
             }
         }
         return false;
+    }
+
+
+    public static boolean isRTLCharacter(char c) {
+        //range of RTL characters per unicode specification
+        return (c >= 0x0590 && c <= 0x05FF) ||
+               (c >= 0xFB1D && c <= 0xFB4F) ||
+               (c >= 0x0600 && c <= 0x07BF) ||
+               (c >= 0xFB50 && c <= 0xFDFF) ||
+               (c >= 0xFE70 && c <= 0xFEFF)    ;
+    }
+
+    /**
+     * function to check if text range has RTL characters.
+     */
+    public static boolean hasRTLCharacters(final char[] text, int start, int end) {
+        if (text == null)
+            return false;
+
+        //go through all characters
+        for (int i=start; i<end; i++) {
+            if (isRTLCharacter(text[i]))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * function to check if text range has RTL characters.
+     */
+    public static boolean hasRTLCharacters(CharSequence text, int start, int end) {
+        if (text == null)
+            return false;
+
+        //go through all characters
+        for (int i=start; i<end; i++) {
+            if (isRTLCharacter(text.charAt(i)))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * function to process bidi on the given text
+     * @param src
+     * @return String
+     */
+    public static String processBidi (final String src) {
+        return (TextUtils.processBidi(src, 0, src.length()));
+    }
+
+    /**
+     * function to process bidi the given text
+     * @param src
+     * @return char[]
+     */
+    public static char[] processBidi (final char[] src) {
+
+        return (TextUtils.processBidi(src, 0, src.length));
+    }
+
+    /**
+     * function to process bidi on the given text
+     * @param src
+     * @param begin
+     * @param end
+     * @return String
+     */
+    public static String processBidi (final String src, int start, int end) {
+
+        char[] chars2 = TextUtils.processBidi(src.toCharArray(), start, end);
+
+        return (new String(chars2));
+    }
+
+    /**
+     * @author: Eyad Aboulouz
+     * function to process bidi on the given text
+     * @param src
+     * @param start
+     * @param end
+     * @return char[]
+     */
+    public static char[] processBidi (final char[] src, int start, int end) {
+
+        if (src == null)
+            return (src);
+
+        //if there are no right-to-left characters
+        if (!hasRTLCharacters(src,start,end))
+            return (src);
+
+        try {
+            char[] outputTxt = new char[end-start];
+            char[] ret = src.clone();
+
+            int outputSize = AndroidBidi.reorderAndReshapeBidiText(ret, outputTxt, start, end-start);
+
+            if (outputSize != (end-start))
+                throw new Exception ("Error Processing Bidi Reordering And Reshaping");
+
+            System.arraycopy(outputTxt, 0, ret, start, end-start);
+
+            return (ret);
+
+        } catch (Exception e) {
+
+            return (src);
+        }
     }
 
     private static Object sLock = new Object();

core/java/android/text/AndroidBidi.java
@@ -44,5 +44,27 @@ package android.text;
         return result;
     }
 
+    /**
+     * @author: Eyad Aboulouz
+     * Bidi text reordering and reshaping by by calling native reorderReshapeBidiText function
+     * @param chs
+     * @param reshapedChs
+     * @param off
+     * @param len
+     * @return int
+     */
+    public static int reorderAndReshapeBidiText(char[] chs, char[] outputChs, int off, int len) {
+
+        if (chs == null)
+            throw new NullPointerException();
+
+        if (off < 0 || len < 0 || off + len > chs.length)
+            throw new IndexOutOfBoundsException();
+
+        return reorderReshapeBidiText(chs, outputChs, off, len);
+    }
+
     private native static int runBidi(int dir, char[] chs, byte[] chInfo, int n, boolean haveInfo);
+
+    private native static int reorderReshapeBidiText(char[] chs, char[] outputChs, int off, int len);
 }

core/java/android/text/Layout.java
@@ -366,7 +366,7 @@ public abstract class Layout {
                     Assert.assertTrue(dir == DIR_LEFT_TO_RIGHT);
                     Assert.assertNotNull(c);
                 }
-                c.drawText(buf, start, end, x, lbaseline, paint,false);
+                c.drawText(buf, start, end, x, lbaseline, paint);
             } else {
                 drawText(c, buf, start, end, dir, directions,
                     x, ltop, lbaseline, lbottom, paint, mWorkPaint,

core/java/android/text/SpannableStringBuilder.java
@@ -1042,14 +1042,14 @@ implements CharSequence, GetChars, Spannable, Editable, Appendable,
         checkRange("drawText", start, end);
 
         if (end <= mGapStart) {
-            c.drawText(mText, start, end - start, x, y, p,true);
+            c.drawText(mText, start, end - start, x, y, p);
         } else if (start >= mGapStart) {
-            c.drawText(mText, start + mGapLength, end - start, x, y, p,true);
+            c.drawText(mText, start + mGapLength, end - start, x, y, p);
         } else {
             char[] buf = TextUtils.obtain(end - start);
 
             getChars(start, end, buf, 0);
-            c.drawText(buf, 0, end - start, x, y, p,true);
+            c.drawText(buf, 0, end - start, x, y, p);
             TextUtils.recycle(buf);
         }
     }

core/java/android/text/StaticLayout.java
@@ -267,12 +267,13 @@ extends Layout
                 // Do mirroring for right-to-left segments
 
                 for (int i = 0; i < n; i++) {
-                    if (chdirs[i] == Character.DIRECTIONALITY_RIGHT_TO_LEFT) {
+                    if (chdirs[i] == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
+                        chdirs[i] == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC) {
                         int j;
 
                         for (j = i; j < n; j++) {
-                            if (chdirs[j] !=
-                                Character.DIRECTIONALITY_RIGHT_TO_LEFT)
+                            if (chdirs[j] != Character.DIRECTIONALITY_RIGHT_TO_LEFT &&
+                                chdirs[j] != Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC)
                                 break;
                         }
 
@@ -650,7 +651,8 @@ extends Layout
             // Heuristic - LTR unless paragraph contains any RTL chars
             dir = DIR_LEFT_TO_RIGHT;
             for (int j = 0; j < n; j++) {
-                if (chInfo[j] == Character.DIRECTIONALITY_RIGHT_TO_LEFT) {
+                if (chInfo[j] == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
+                    chInfo[j] == Character.DIRECTIONALITY_RIGHT_TO_LEFT_ARABIC) {
                     dir = DIR_RIGHT_TO_LEFT;
                     break;
                 }

core/java/android/text/Styled.java
@@ -69,25 +69,30 @@ public class Styled
         paint.baselineShift = 0;
         workPaint.set(paint);
 
-		if (spans.length > 0) {
-			for (int i = 0; i < spans.length; i++) {
-				CharacterStyle span = spans[i];
+        if (spans.length > 0) {
+            for (int i = 0; i < spans.length; i++) {
+                CharacterStyle span = spans[i];
 
-				if (span instanceof ReplacementSpan) {
-					replacement = (ReplacementSpan)span;
-				}
-				else {
-					span.updateDrawState(workPaint);
-				}
-			}
-		}
+                if (span instanceof ReplacementSpan) {
+                    replacement = (ReplacementSpan)span;
+                }
+                else {
+                    span.updateDrawState(workPaint);
+                }
+            }
+        }
 
         if (replacement == null) {
             CharSequence tmp;
             int tmpstart, tmpend;
 
             if (runIsRtl) {
-                tmp = TextUtils.getReverse(text, start, end);
+                // if the text has RTL characters then it will be reversed when mirrored:
+                if (TextUtils.hasRTLCharacters(text,start,end))
+                    tmp = text.subSequence(start,end);
+                else // otherwise we need to reverse it here:
+                    tmp = TextUtils.getReverse(text, start, end);
+
                 tmpstart = 0;
                 // XXX: assumes getReverse doesn't change the length of the text
                 tmpend = end - start;
@@ -129,7 +134,7 @@ public class Styled
                     }
 
                     canvas.drawText(tmp, tmpstart, tmpend,
-                                    x - ret, y + workPaint.baselineShift, workPaint,false);
+                                    x - ret, y + workPaint.baselineShift, workPaint);
                 } else {
                     if (needWidth) {
                         if (!haveWidth) {
@@ -139,7 +144,7 @@ public class Styled
                     }
 
                     canvas.drawText(tmp, tmpstart, tmpend,
-                                    x, y + workPaint.baselineShift, workPaint,false);
+                                    x, y + workPaint.baselineShift, workPaint);
                 }
             } else {
                 if (needWidth && !haveWidth) {
@@ -190,19 +195,19 @@ public class Styled
         MetricAffectingSpan[] spans =
             text.getSpans(start, end, MetricAffectingSpan.class);
 
-		ReplacementSpan replacement = null;
+        ReplacementSpan replacement = null;
         workPaint.set(paint);
-		
-		for (int i = 0; i < spans.length; i++) {
-			MetricAffectingSpan span = spans[i];
-			if (span instanceof ReplacementSpan) {
-				replacement = (ReplacementSpan)span;
-			}
-			else {
-				span.updateMeasureState(workPaint);
-			}
-		}
-	
+
+        for (int i = 0; i < spans.length; i++) {
+            MetricAffectingSpan span = spans[i];
+            if (span instanceof ReplacementSpan) {
+                replacement = (ReplacementSpan)span;
+            }
+            else {
+                span.updateMeasureState(workPaint);
+            }
+        }
+
         int result;
         if (replacement == null) {
             workPaint.getFontMetricsInt(fmi);
@@ -253,7 +258,12 @@ public class Styled
             float ret = 0;
 
             if (runIsRtl) {
-                CharSequence tmp = TextUtils.getReverse(text, start, end);
+                CharSequence tmp;
+                // if the text has RTL characters then it will be reversed when mirrored:
+                if (TextUtils.hasRTLCharacters(text,start,end))
+                    tmp = text.subSequence(start,end);
+                else // otherwise we need to reverse it here:
+                    tmp = TextUtils.getReverse(text, start, end);
                 // XXX: this assumes getReverse doesn't tweak the length of
                 // the text
                 int tmpend = end - start;
@@ -263,13 +273,13 @@ public class Styled
 
                 if (canvas != null)
                     canvas.drawText(tmp, 0, tmpend,
-                                    x - ret, y, paint,false);
+                                    x - ret, y, paint);
             } else {
                 if (needWidth)
                     ret = paint.measureText(text, start, end);
 
                 if (canvas != null)
-                    canvas.drawText(text, start, end, x, y, paint,false);
+                    canvas.drawText(text, start, end, x, y, paint);
             }
 
             if (fmi != null) {
@@ -278,7 +288,7 @@ public class Styled
 
             return ret * dir;   // Layout.DIR_RIGHT_TO_LEFT == -1
         }
-        
+
         float ox = x;
         int minAscent = 0, maxDescent = 0, minTop = 0, maxBottom = 0;
 

core/java/android/widget/TextView.java
@@ -2822,7 +2822,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
 
         public void drawText(Canvas c, int start, int end,
                              float x, float y, Paint p) {
-            c.drawText(mChars, start + mStart, end - start, x, y, p,false);
+            c.drawText(mChars, start + mStart, end - start, x, y, p);
         }
 
         public float measureText(int start, int end, Paint p) {

core/jni/android_text_AndroidBidi.cpp
@@ -22,10 +22,13 @@
 #include "utils/misc.h"
 #include "utils/Log.h"
 #include "unicode/ubidi.h"
+#include "unicode/uchar.h"
+#include "unicode/ushape.h"
+
 
 namespace android {
     
-static jint runBidi(JNIEnv* env, jobject obj, jint dir, jcharArray chsArray, 
+static jint runBidi(JNIEnv* env, jclass c, jint dir, jcharArray chsArray,
                     jbyteArray infoArray, int n, jboolean haveInfo)
 {
     // Parameters are checked on java side
@@ -56,9 +59,66 @@ static jint runBidi(JNIEnv* env, jobject obj, jint dir, jcharArray chsArray,
     return result;
 }
 
+/*
+Native Bidi text reordering and Arabic shaping
+by: Eyad Aboulouz
+*/
+static jint reorderReshapeBidiText (JNIEnv* env, jclass c, jcharArray srcArray, jcharArray destArray, jint offset, jint n) {
+
+    bool hasErrors = false;
+    jint outputSize = 0;
+    UChar *intermediate = new UChar[n];
+    UChar *output = new UChar[n];
+    UErrorCode status = U_ZERO_ERROR;
+
+    UBiDi *para = ubidi_openSized(n, 0, &status);
+
+    ubidi_setReorderingMode(para, UBIDI_REORDER_INVERSE_LIKE_DIRECT);
+
+    jchar* src = env->GetCharArrayElements(srcArray, NULL);
+
+    if (src != NULL && para != NULL && U_SUCCESS(status)) {
+
+        ubidi_setPara(para, src+offset, n, UBIDI_DEFAULT_RTL, NULL, &status);
+
+        if (U_SUCCESS(status)) {
+
+            ubidi_writeReordered(para, intermediate, n, UBIDI_DO_MIRRORING | UBIDI_REMOVE_BIDI_CONTROLS, &status);
+
+            if (U_SUCCESS(status)) {
+
+                outputSize = u_shapeArabic(intermediate, n, output, n, U_SHAPE_TEXT_DIRECTION_VISUAL_LTR | U_SHAPE_LETTERS_SHAPE | U_SHAPE_LENGTH_FIXED_SPACES_AT_END, &status);
+
+                if (U_SUCCESS(status))
+                    env->SetCharArrayRegion(destArray, 0, outputSize, output);
+                else
+                    hasErrors = true;
+            } else
+                hasErrors = true;
+        } else
+            hasErrors = true;
+    } else
+        hasErrors = true;
+
+    delete [] intermediate;
+    delete [] output;
+
+    if (para != NULL)
+        ubidi_close(para);
+
+    env->ReleaseCharArrayElements(srcArray, src, JNI_ABORT);
+
+    if (hasErrors)
+        jniThrowException(env, "java/lang/RuntimeException", NULL);
+
+    return outputSize;
+}
+
 static JNINativeMethod gMethods[] = {
         { "runBidi", "(I[C[BIZ)I",
-        (void*) runBidi }
+        (void*) runBidi },
+        { "reorderReshapeBidiText", "([C[CII)I",
+        (void*) reorderReshapeBidiText }
 };
 
 int register_android_text_AndroidBidi(JNIEnv* env)

commit f75c9f110f0bc2cba401a8b95c9c4377c6c057e9

RTL: Fix handling of special unicode marker 0xFEFF
This fixes a problem introduced by commit 43e45eb94e1
which (wrongly) handled the 0xFEFF mark as RTL.

Change-Id: Icf8fce91e6df8c4fa8e01eb893174a9fdb8c0766

core/java/android/text/TextUtils.java
@@ -1692,7 +1692,7 @@ public class TextUtils {
                (c >= 0xFB1D && c <= 0xFB4F) ||
                (c >= 0x0600 && c <= 0x07BF) ||
                (c >= 0xFB50 && c <= 0xFDFF) ||
-               (c >= 0xFE70 && c <= 0xFEFF)    ;
+               (c >= 0xFE70 && c <= 0xFEFE)    ;
     }
 
     /**