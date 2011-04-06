95530d468cc6fd143302867b1118bf078617dfc4
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-06 23:55:28 +0100

    libcameraservice: Add support for LGE FFC
    Change-Id: Iec98bcf9b0a26c934955b27edf7252849b1cf42e

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1315,8 +1315,12 @@ void CameraService::Client::copyFrameAndPostCopiedFrame(
     client->dataCallback(CAMERA_MSG_PREVIEW_FRAME, frame);
 }
 
+#define FFC_VENDOR_HTC 0x1
+#define FFC_VENDOR_LGE 0x2
+int mFrontCameraType = 0;
+
 int CameraService::Client::getOrientation(int degrees, bool mirror) {
-    if (!mirror) {
+    if (!mirror || mFrontCameraType == FFC_VENDOR_LGE) {
         if (degrees == 0) return 0;
         else if (degrees == 90) return HAL_TRANSFORM_ROT_90;
         else if (degrees == 180) return HAL_TRANSFORM_ROT_180;
@@ -1435,8 +1439,13 @@ static const CameraInfo sCameraInfo[] = {
 
 static int getNumberOfCameras() {
     if (access(HTC_SWITCH_CAMERA_FILE_PATH, W_OK) == 0) {
+        mFrontCameraType = FFC_VENDOR_HTC;
         return 2;
     }
+#ifdef BOARD_HAS_LGE_FFC
+    mFrontCameraType = FFC_VENDOR_LGE;
+    return 2;
+#endif
     /* FIXME: Support non-HTC front camera */
     return 1;
 }
@@ -1469,7 +1478,17 @@ extern "C" sp<CameraHardwareInterface> HAL_openCameraHardware(int cameraId)
 {
     LOGV("openCameraHardware: call createInstance");
     if (getNumberOfCameras() == 2) {
-        htcCameraSwitch(cameraId);
+        if (mFrontCameraType == FFC_VENDOR_HTC) {
+            htcCameraSwitch(cameraId);
+        } else if (mFrontCameraType == FFC_VENDOR_LGE) {
+            sp<CameraHardwareInterface> hardware = openCameraHardware(cameraId);
+            if (hardware != NULL) {
+                CameraParameters params(hardware->getParameters());
+                params.set("camera-sensor", cameraId);
+                hardware->setParameters(params);
+            }
+            return hardware;
+        }
 #ifdef BOARD_USE_REVERSE_FFC
         if (cameraId == 1) {
             /* Change default parameters for the front camera */
