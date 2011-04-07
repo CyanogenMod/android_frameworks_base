7ee5906ab03c4319251aeb9890559759007f4a08
Eyad Aboulouz <eyad.aboulouz@gmail.com>
2011-04-07 04:33:39 -0400

    RTL bug fixes:
    1) LTR text followed by RTL could become accidently reversed if the LTR text
    contains a nuetral character, such as a symbol
    Examples: http://www.test.com RTL -> com.test.www://http RTL
    
    2) Fixes an issue where (LTR) RTL -> RTL (LTR)  (i.e. the left-to-right is positioned
    incorrectly)
    
    3) Fixes an issue where RTL 2001 -> RTL 1002  (i.e. digits reversed)
    
    Change-Id: I5baa3cd3afdf19b681f83dbde90fd132ab1d15dd

--- core/java/android/text/StaticLayout.java
+++ core/java/android/text/StaticLayout.java
@@ -740,8 +740,7 @@ extends Layout
             // add condition if the separator is a space
             else if (isSpace && prev != SOR &&
                             (   next == Character.DIRECTIONALITY_EUROPEAN_NUMBER
-                             || next == Character.DIRECTIONALITY_ARABIC_NUMBER
-                             || next == Character.DIRECTIONALITY_OTHER_NEUTRALS) ) {
+                             || next == Character.DIRECTIONALITY_ARABIC_NUMBER  ) ) {
                 chInfo[j] = SOR;
                 for (int k=j+1; k < n; ++k) {
                     if (chInfo[k] == Character.DIRECTIONALITY_LEFT_TO_RIGHT) {
@@ -827,8 +826,6 @@ extends Layout
             } else if (d == Character.DIRECTIONALITY_EUROPEAN_NUMBER ||
                        d == Character.DIRECTIONALITY_ARABIC_NUMBER) {
                 cur = Character.DIRECTIONALITY_LEFT_TO_RIGHT;
-            } else if (d == Character.DIRECTIONALITY_OTHER_NEUTRALS) {
-               chInfo[j] = cur = SOR;
             } else {
                 byte dd = SOR;
                 int k;
@@ -837,8 +834,7 @@ extends Layout
                     dd = chInfo[k];
 
                     if (dd == Character.DIRECTIONALITY_LEFT_TO_RIGHT ||
-                        dd == Character.DIRECTIONALITY_RIGHT_TO_LEFT ||
-                        dd == Character.DIRECTIONALITY_OTHER_NEUTRALS) {
+                        dd == Character.DIRECTIONALITY_RIGHT_TO_LEFT) {
                         break;
                     }
                     if (dd == Character.DIRECTIONALITY_EUROPEAN_NUMBER ||
