8543d04f0e1ebed586ce65d8e2c52e190d5cee2e
Chris Soyars <ctso@ctso.me>
2011-04-21 02:29:57 -0400

    Add a @hide to TextUtils.getMirror
    Change-Id: I32b817ce88930ec9435e97e37c458a155ae4036d

--- core/java/android/text/TextUtils.java
+++ core/java/android/text/TextUtils.java
@@ -534,6 +534,7 @@ public class TextUtils {
         private int mEnd;
     }
 
+    /** @hide */
     public static CharSequence getMirror(CharSequence source,
                                           int start, int end) {
         return new Mirrorer(source, start, end);
