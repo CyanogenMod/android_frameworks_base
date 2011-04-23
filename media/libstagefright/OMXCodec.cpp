297468b0a9aec9caa54f7773679a0d588d799488
Steve Kondik <shade@chemlab.org>
2011-04-23 17:14:26 -0400

    Revert "ignore sf error"
    This reverts commit 892f7572970a1dab445ee8674e91511cc0771516.
    
    TOTAL DERPFEST WTF.

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1132,8 +1132,6 @@ status_t OMXCodec::getVideoProfileLevel(
     CODEC_LOGV("Default profile: %ld, level %ld",
             defaultProfileLevel.mProfile, defaultProfileLevel.mLevel);
 
-    return OK;
-
     // Are the default profile and level overwriten?
     int32_t profile, level;
     if (!meta->findInt32(kKeyVideoProfile, &profile)) {
