9c3c6e965ccf2fa5ee2541d92f5d7327684f236e
Prabhakaran Mc <prabhakaranmc@codeaurora.org>
2011-04-26 11:58:00 +0530

    frameworks/base: bluetooth: AVRCP 1.3 feature changes
    Bluetooth module will listen metadata/event change intents
    from Music app and post data to Bluez stack.
    
    Conflicts:
    
    	core/java/android/server/BluetoothA2dpService.java
    	core/jni/android_server_BluetoothEventLoop.cpp
    
    Gingerbread change Icbfb1712dde98c9677f57c869bcfffed2bb42a14
    
    Change-Id: Id8cbec68302b53befbec4273013668a58c755369

--- core/jni/android_server_BluetoothEventLoop.cpp
+++ core/jni/android_server_BluetoothEventLoop.cpp
@@ -288,6 +288,13 @@ static jboolean setUpEventLoop(native_data_t *nat) {
             LOG_AND_FREE_DBUS_ERROR(&err);
             return JNI_FALSE;
         }
+        dbus_bus_add_match(nat->conn,
+                "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".Control'",
+                &err);
+        if (dbus_error_is_set(&err)) {
+            LOG_AND_FREE_DBUS_ERROR(&err);
+            return JNI_FALSE;
+        }
 
         dbus_bus_add_match(nat->conn,
                 "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".HealthDevice'",
@@ -450,6 +457,12 @@ static void tearDownEventLoop(native_data_t *nat) {
 
         dbus_bus_remove_match(nat->conn,
                 "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".AudioSink'",
+                &err);
+        if (dbus_error_is_set(&err)) {
+            LOG_AND_FREE_DBUS_ERROR(&err);
+        }
+        dbus_bus_remove_match(nat->conn,
+                "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".Control'",
                 &err);
         if (dbus_error_is_set(&err)) {
             LOG_AND_FREE_DBUS_ERROR(&err);
