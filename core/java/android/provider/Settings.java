c6d71b4be20c5b3823eb0b9f4c4af780d3a775e9
Danesh <daneshm90@gmail.com>
2011-04-28 13:30:22 -0400

    Lockscreen feedback and indicator toggle
    Change-Id: Id4628081a06d4c929a8d8890bc1b50be00473435

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2543,6 +2543,13 @@ public final class Settings {
          */
         public static final String EXPANDED_HIDE_SCROLLBAR = "expanded_hide_scrollbar";
 
+	/**
+         * Hide indicator in status bar widget
+         *
+         * @hide
+         */
+        public static final String EXPANDED_HIDE_INDICATOR = "expanded_hide_indicator";
+
         /**
          * Notification Indicator Color
          *
