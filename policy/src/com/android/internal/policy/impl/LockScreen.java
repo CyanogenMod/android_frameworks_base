cb9722e776d0b7b14cc4788d93962701f082efb6
Danesh <daneshm90@gmail.com>
2011-05-02 20:10:40 -0400

    Show custom application icon in lockscreen
    - Fixed tabs/spaces as per suggestion
    
    Change-Id: I289cef4ac0ed1e3709fefd702bf983549cbfcc87

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -22,7 +22,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.RotarySelector;
 import com.android.internal.widget.SlidingTab;
 import com.android.internal.widget.SlidingTab.OnTriggerListener;
-
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -337,7 +337,13 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
         if (mSelector2 != null) {
             mSelector2.setHoldAfterTrigger(true, false);
             mSelector2.setLeftHintText(R.string.lockscreen_phone_label);
-            mSelector2.setRightHintText(R.string.lockscreen_messaging_label);
+            try {
+                    String packageName = mCustomAppActivity.split("component=")[1].split("/")[0];
+                    CharSequence label = getContext().getPackageManager().getApplicationLabel(getContext().getPackageManager().getApplicationInfo(packageName, getContext().getPackageManager().GET_META_DATA));
+                    mSelector2.setRightHintText(label);
+            }catch (java.lang.Throwable t) {
+                    mSelector2.setRightHintText(R.string.lockscreen_messaging_label);
+            }
         }
         mEmergencyCallText = (TextView) findViewById(R.id.emergencyCallText);
         mEmergencyCallButton = (Button) findViewById(R.id.emergencyCallButton);
@@ -412,13 +418,32 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
         mAudioManager = (AudioManager) getContext().getSystemService(Context.AUDIO_SERVICE);
         mSilentMode = isSilentMode();
 
-
         //Rotary setup
         if(!mRotaryUnlockDown){
             mRotarySelector.setLeftHandleResource(R.drawable.ic_jog_dial_unlock);
-            mRotarySelector.setMidHandleResource((mCustomIconStyle == 1) ? R.drawable.ic_jog_dial_custom : R.drawable.ic_jog_dial_messaging);
+            try{
+                    String packageName = mCustomAppActivity.split("component=")[1].split("/")[0];
+                    if (mCustomIconStyle == 2) {
+                        mRotarySelector.setMidHandleResource(R.drawable.ic_jog_dial_custom);
+                    }else{
+                        Drawable icon = getContext().getPackageManager().getApplicationIcon(packageName);
+                        mRotarySelector.setMidHandleResource(icon);
+                    }
+            }catch (java.lang.Throwable t) {
+                    mRotarySelector.setMidHandleResource(R.drawable.ic_jog_dial_custom);
+            }
         }else{
-            mRotarySelector.setLeftHandleResource((mCustomIconStyle == 1) ? R.drawable.ic_jog_dial_custom : R.drawable.ic_jog_dial_messaging);
+            try{
+                    String packageName = mCustomAppActivity.split("component=")[1].split("/")[0];
+                    if (mCustomIconStyle == 2) {
+                        mRotarySelector.setLeftHandleResource(R.drawable.ic_jog_dial_custom);
+                    }else{
+                        Drawable icon = getContext().getPackageManager().getApplicationIcon(packageName);
+                        mRotarySelector.setLeftHandleResource(icon);
+                    }
+            }catch (java.lang.Throwable t) {
+                    mRotarySelector.setLeftHandleResource(R.drawable.ic_jog_dial_custom);
+            }
             mRotarySelector.setMidHandleResource(R.drawable.ic_jog_dial_unlock);
         }
         mRotarySelector.enableCustomAppDimple(mCustomAppToggle);
@@ -448,12 +473,17 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
             mSelector2.setLeftTabResources(R.drawable.ic_jog_dial_answer,
                     R.drawable.jog_tab_target_green, R.drawable.jog_tab_bar_left_generic,
                     R.drawable.jog_tab_left_generic);
-
-            mSelector2.setRightTabResources((mCustomIconStyle == 1) ? R.drawable.ic_jog_dial_custom
-                         : R.drawable.ic_jog_dial_messaging,
-                    R.drawable.jog_tab_target_green, R.drawable.jog_tab_bar_right_generic,
-                    R.drawable.jog_tab_right_generic);
-
+            try{
+                    String packageName = mCustomAppActivity.split("component=")[1].split("/")[0];
+                    if (mCustomIconStyle == 2) {
+                        mSelector2.setRightTabResources(R.drawable.ic_jog_dial_custom, R.drawable.jog_tab_target_green, R.drawable.jog_tab_bar_right_generic, R.drawable.jog_tab_right_generic);
+                    }else{
+                        Drawable icon = getContext().getPackageManager().getApplicationIcon(packageName);
+                        mSelector2.setRightTabResources(icon, R.drawable.jog_tab_target_green, R.drawable.jog_tab_bar_right_generic, R.drawable.jog_tab_right_generic);
+                    }
+            }catch (java.lang.Throwable t) {
+                    mSelector2.setRightTabResources(R.drawable.ic_jog_dial_custom, R.drawable.jog_tab_target_green, R.drawable.jog_tab_bar_right_generic, R.drawable.jog_tab_right_generic);
+            }
             mSelector2.setOnTriggerListener(new OnTriggerListener() {
                 public void onTrigger(View v, int whichHandle) {
                     if (whichHandle == SlidingTab.OnTriggerListener.LEFT_HANDLE) {
