f0aa982c3b514062c81c59cce6d9d1c7f1625449
Quarx2k <agent00791@gmail.com>
2011-05-13 14:40:43 +0700

    Wide screen camera profile for 16/9e devices (480x854)
    from CyanogenDefy team
    
    Change-Id: I39b7d3f95495e9e8ecb48533478e24bd2f660da9

--- include/media/MediaProfiles.h
+++ include/media/MediaProfiles.h
@@ -26,7 +26,8 @@ namespace android {
 enum camcorder_quality {
     CAMCORDER_QUALITY_LOW  = 0,
     CAMCORDER_QUALITY_HIGH = 1,
-    CAMCORDER_QUALITY_HD = 2
+    CAMCORDER_QUALITY_HD = 2,
+    CAMCORDER_QUALITY_WIDE = 3
 };
 
 enum video_decoder {
