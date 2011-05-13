557bab0daa13f584501f1ebeaae661b9448730bb
Dalingrin <dalingrin@gmail.com>
2011-05-13 02:22:54 -0500

    Add option to force wake lock while USB connected
    Allows the use of a system property to force the
    "Stay Awake" option in Settings->Applications->Debugging
    
    Change-Id: I52e9a326ee94aaee3b5b71f0b9568dd4e9fe2468

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -52,6 +52,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.WorkSource;
 import android.provider.Settings.SettingNotFoundException;
 import android.provider.Settings;
@@ -485,8 +486,13 @@ class PowerManagerService extends IPowerManager.Stub
 
             synchronized (mLocks) {
                 // STAY_ON_WHILE_PLUGGED_IN, default to when plugged into AC
-                mStayOnConditions = getInt(STAY_ON_WHILE_PLUGGED_IN,
+                if (SystemProperties.getBoolean("ro.powermanager.awake_on_usb", false)) {
+                    mStayOnConditions = BatteryManager.BATTERY_PLUGGED_AC |
+                        BatteryManager.BATTERY_PLUGGED_USB;
+                } else {
+                    mStayOnConditions = getInt(STAY_ON_WHILE_PLUGGED_IN,
                         BatteryManager.BATTERY_PLUGGED_AC);
+                }
                 updateWakeLockLocked();
 
                 // SCREEN_OFF_TIMEOUT, default to 15 seconds
