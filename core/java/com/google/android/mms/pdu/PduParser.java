3f8122d8981a92a27c44e300a3229e85610d3c01
Jordan Patterson <jordanp@gmail.com>
2011-05-16 08:29:09 -0600

    Merge fix for PduParser.java - Part 1
    It looks like there was a merge issue in PduParser.java that duplicated
    some code when the gingerbread code was merged.  The change in
    gingerbread essentially put the entire PduPart.P_CONTENT_DISPOSITION in
    an if statement, and set it to positive for all devices.
    
    This fixes MMS problems on the Nexus One, and possibly other phones.
    The second part reverts the config.xml change on supersonic, as it
    should now work with the default code.
    
    Change-Id: I47b0f17327f906055e1d358226b5db0ddfb2241f

--- core/java/com/google/android/mms/pdu/PduParser.java
+++ core/java/com/google/android/mms/pdu/PduParser.java
@@ -1559,41 +1559,6 @@ public class PduParser {
                          * Attachment = <Octet 129>
                          * Inline = <Octet 130>
                          */
-                        int len = -1;
-                        boolean validDispositionLength = true;
-                        pduDataStream.mark(1);
-                        
-                        try {
-                            len = parseValueLength(pduDataStream);
-                        } catch (RuntimeException e) { 
-                            // tolerate invalid content-disposition length
-                            len = 31;
-                            validDispositionLength = false;
-                            pduDataStream.reset();
-                        }
-                        
-                        pduDataStream.mark(1);
-                        int thisStartPos = pduDataStream.available();
-                        int thisEndPos = 0;
-                        int value = pduDataStream.read();
-                        
-                        if (validDispositionLength) {
-                            if (value == PduPart.P_DISPOSITION_FROM_DATA ) {
-                                part.setContentDisposition(PduPart.DISPOSITION_FROM_DATA);
-                            } else if (value == PduPart.P_DISPOSITION_ATTACHMENT) {
-                                part.setContentDisposition(PduPart.DISPOSITION_ATTACHMENT);
-                            } else if (value == PduPart.P_DISPOSITION_INLINE) {
-                                part.setContentDisposition(PduPart.DISPOSITION_INLINE);
-                            } else {
-                                pduDataStream.reset();
-                                /* Token-text */
-                                part.setContentDisposition(parseWapString(pduDataStream, TYPE_TEXT_STRING));
-                            }
-                        } else {
-                            pduDataStream.reset();
-                            /* Token-text */
-                            part.setContentDisposition(parseWapString(pduDataStream, TYPE_TEXT_STRING));
-                        }
 
                         /*
                          * some carrier mmsc servers do not support content_disposition
@@ -1603,12 +1568,25 @@ public class PduParser {
                                 .android.internal.R.bool.config_mms_content_disposition_support);
 
                         if (contentDisposition) {
-                            len = parseValueLength(pduDataStream);
-                            pduDataStream.mark(1);
-                            thisStartPos = pduDataStream.available();
-                            thisEndPos = 0;
-                            value = pduDataStream.read();
+                          int len = -1;
+                          boolean validDispositionLength = true;
+                          pduDataStream.mark(1);
 
+                          try {
+                            len = parseValueLength(pduDataStream);
+                          } catch (RuntimeException e) {
+                            // tolerate invalid content-disposition length
+                            len = 31;
+                            validDispositionLength = false;
+                            pduDataStream.reset();
+                          }
+
+                          pduDataStream.mark(1);
+                          int thisStartPos = pduDataStream.available();
+                          int thisEndPos = 0;
+                          int value = pduDataStream.read();
+
+                          if (validDispositionLength) {
                             if (value == PduPart.P_DISPOSITION_FROM_DATA ) {
                                 part.setContentDisposition(PduPart.DISPOSITION_FROM_DATA);
                             } else if (value == PduPart.P_DISPOSITION_ATTACHMENT) {
@@ -1621,11 +1599,16 @@ public class PduParser {
                                 part.setContentDisposition(parseWapString(pduDataStream
                                         , TYPE_TEXT_STRING));
                             }
-                        }
+                          } else {
+                              pduDataStream.reset();
+                              /* Token-text */
+                              part.setContentDisposition(parseWapString(pduDataStream, TYPE_TEXT_STRING));
+                          }
 
-                        /* get filename parameter and skip other parameters */
-                        thisEndPos = pduDataStream.available();
-                        if (thisStartPos - thisEndPos < len) {
+
+                          /* get filename parameter and skip other parameters */
+                          thisEndPos = pduDataStream.available();
+                          if (thisStartPos - thisEndPos < len) {
                             value = pduDataStream.read();
                             if (value == PduPart.P_FILENAME) { //filename is text-string
                                 part.setFilename(parseWapString(pduDataStream
@@ -1639,11 +1622,11 @@ public class PduParser {
                                 byte[] temp = new byte[last];
                                 pduDataStream.read(temp, 0, last);
                             }
+                          }
+
+                          tempPos = pduDataStream.available();
+                          lastLen = length - (startPos - tempPos);
                         }
-
-                        tempPos = pduDataStream.available();
-                        lastLen = length - (startPos - tempPos);
-
                         break;
                     default:
                         if (LOCAL_LOGV) {
