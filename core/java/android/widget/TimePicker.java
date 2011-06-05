46f6323a5b9e2035bab9647dd8f18cfca6997186
Jiri Tyr <jiri.tyr@gmail.com>
2011-06-05 20:13:03 +0200

    More ergonomic TimePicker and DatePicker
    Currently, when the text in the TimePicker or in the DatePicker dialog
    is needed to change, it is necessary to either click several times on
    the spinner buttons or manually rewrite the values with
    software/hardware keyboard. The manual rewrite is very slow as it is
    necessary to delete the text before new one can be entered. This change
    implements more ergonomic behavior of the TimePicker and the DatePicker
    which allow to enter time or date much faster. If the editable field is
    focused, all text is automatically selected in order to be ready for
    rewrite. If it is not the last field in the row and if the maximum
    length of text is reached, the next field is automatically selected.
    
    In the case of TimePicker:
    If the Hour field is focused, all text is selected and ready for
    rewrite. Once two characters are entered, the Minute field is focused.
    When the Minute filed is focused, all text is selected and ready for
    rewrite.
    
    In the case of DatePicker (US locale):
    If the Month field is focused, all text is selected and ready for
    rewrite. Once three characters are entered, the Day field is focused.
    When the Day field is focused, all text is selected and ready for
    rewrite. Once two characters are entered, the Year field is focused.
    When the Year field is focused, all text is selected and ready for
    rewrite. The order of the fields vary according to user's locale.
    
    Change-Id: Ibe5d678c80f2803d698340aa0c8f795cdc7a7e03

--- core/java/android/widget/TimePicker.java
+++ core/java/android/widget/TimePicker.java
@@ -24,6 +24,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnTextChangedListener;
 
 import com.android.internal.R;
 
@@ -126,6 +127,13 @@ public class TimePicker extends FrameLayout {
                 onTimeChanged();
             }
         });
+        mHourPicker.setOnTextChangeListener(new OnTextChangedListener() {
+			public void onTextChanged(String text) {
+				if (text.length() == 2) {
+					mMinutePicker.requestTextFocus();
+				}
+			}
+		});
 
         // digits of minute
         mMinutePicker = (NumberPicker) findViewById(R.id.minute);
