7e8984d9ce6e3cf6651d582684fec41a3c8aeb70
Vairavan Srinivasan <vairav@codeaurora.org>
2011-06-06 13:42:54 -0700

    frameworks/base: Fix for the race in Wallpaperservice
    Wallpaperservice has a race among message handler of
    DO_DETACH and service's onDestroy. In certain cases,
    the engine kept track in mActiveEngines is removed by
    message handler of DO_DETACH and service's onDestroy
    doesn't get a entry in mActivieEngines and isn't able
    to invoke detach. This keeps the broadcast receiver,
    mReceiver active and is unregistered by the framework
    and a corresponding leak is reported.
    
      Later, the message handler of DO_DETACH continues
    and invokes detach on mEngine, which attempts to
    unregister mReceiver and framework throws an exception
    "Receiver not registered: android.service.wallpaper.
    WallpaperService$Engine$1@40874580" and causes the
    framework to reboot.
    
      In case of system_server, WindownManagerPolicy and
    android.server.ServerThread contest for access of
    shared members, mActiveEngines and mEgine. Fix is to
    protect the critical section via synchronized block.
    
    Change-Id: Ice79cb8344fb54bcc6e9f76f4b143daac2608d7b

--- core/java/android/service/wallpaper/WallpaperService.java
+++ core/java/android/service/wallpaper/WallpaperService.java
@@ -899,6 +899,7 @@ public abstract class WallpaperService extends Service {
         }
         
         void detach() {
+           synchronized (mLock) {
             if (mDestroyed) {
                 return;
             }
@@ -941,6 +942,7 @@ public abstract class WallpaperService extends Service {
                     mInputChannel = null;
                 }
             }
+           }
         }
     }
     
@@ -1027,13 +1029,17 @@ public abstract class WallpaperService extends Service {
                     }
                     Engine engine = onCreateEngine();
                     mEngine = engine;
-                    mActiveEngines.add(engine);
+                    synchronized (mActiveEngines) {
+                        mActiveEngines.add(engine);
+                    }
                     engine.attach(this);
                     return;
                 }
                 case DO_DETACH: {
-                    mActiveEngines.remove(mEngine);
-                    mEngine.detach();
+                        mEngine.detach();
+                    synchronized (mActiveEngines) {
+                        mActiveEngines.remove(mEngine);
+                    }
                     return;
                 }
                 case DO_SET_DESIRED_SIZE: {
@@ -1115,10 +1121,13 @@ public abstract class WallpaperService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        for (int i=0; i<mActiveEngines.size(); i++) {
-            mActiveEngines.get(i).detach();
+        synchronized (mActiveEngines) {
+           for (int i=0; i<mActiveEngines.size(); i++) {
+                Engine engine = mActiveEngines.get(i);
+                engine.detach();
+           }
+           mActiveEngines.clear();
         }
-        mActiveEngines.clear();
     }
 
     /**
