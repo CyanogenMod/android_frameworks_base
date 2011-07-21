215870761db3ecae48fa3cbb0a72eebebc13f281
Xoror <cuongbui@gmail.com>
2011-07-21 19:04:12 +0200

    code formatting BT GPS support added. Implements this change request: http://code.google.com/p/android/issues/detail?id=9057 more information see: http://iloki.nl
    Change-Id: I83dd49eb914c24cd182c428d1f7afb90900cf6b1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2570,6 +2570,12 @@ public final class Settings {
         public static final String MENU_UNLOCK_SCREEN = "menu_unlock_screen";
 
         /**
+         * External BT GPS device
+         * @hide
+         */
+        public static final String EXTERNAL_GPS_BT_DEVICE = "0";
+
+        /**
          * Whether to enable quiet hours.
          * @hide
          */
