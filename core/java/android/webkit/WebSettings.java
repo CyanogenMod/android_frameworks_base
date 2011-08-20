5bd805e9b576caaa7f2f2f90d51b67d95ede9421
Martin Brabham <optedoblivion@gmail.com>
2011-08-19 18:14:27 -0700

    WebSettings: Add ro.cm.version to default user agent.
    Change-Id: Ic14a1626ab7bec26297e2041bdcfb26ca7b3a440

--- core/java/android/webkit/WebSettings.java
+++ core/java/android/webkit/WebSettings.java
@@ -22,6 +22,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemProperties;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 
@@ -509,6 +510,9 @@ public class WebSettings {
             buffer.append(" Build/");
             buffer.append(id);
         }
+        final String cmversion = SystemProperties.get("ro.cm.version");
+        if (cmversion != null && cmversion.length() > 0)
+            buffer.append("; CyanogenMod-" + cmversion.replaceAll("(.+?)-.*","$1"));
         String mobile = mContext.getResources().getText(
             com.android.internal.R.string.web_user_agent_target_content).toString();
         final String base = mContext.getResources().getText(
