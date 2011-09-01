5eb9e6f1a2c0385664c3346e9fea4e0ef719bd08
Naomi Luis <nluis@codeaurora.org>
2011-08-31 17:07:35 -0700

    libui: Always allocate buffers from gralloc
    All graphic buffers should be allocated from the gralloc. Gralloc
    supports allocating buffers even for software-only implementations.
    
    Change-Id: Iad4c72f0c0aeabb00a8f53a46dcf72c5436c0935
    CRs-fixed: 297748

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -91,11 +91,7 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h, PixelFormat forma
     // we have a h/w allocator and h/w buffer is requested
     status_t err; 
     
-    if (usage & GRALLOC_USAGE_HW_MASK) {
-        err = mAllocDev->alloc(mAllocDev, w, h, format, usage, handle, stride);
-    } else {
-        err = sw_gralloc_handle_t::alloc(w, h, format, usage, handle, stride);
-    }
+    err = mAllocDev->alloc(mAllocDev, w, h, format, usage, handle, stride);
 
     LOGW_IF(err, "alloc(%u, %u, %d, %08x, ...) failed %d (%s)",
             w, h, format, usage, err, strerror(-err));
