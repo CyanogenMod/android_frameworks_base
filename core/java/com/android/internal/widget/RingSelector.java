d596f6260ba302506b5100d1403a008e85568afd
Danesh Mondegarian <daneshm90@gmail.com>
2011-09-26 11:33:53 -0400

    RingLockScreen : Custom App Hover Backlight
    Highlights the custom app icons to indicate
    which one is currently active.
    
    Change-Id: I9ba5c0e3cb23f1ace00e4d5b3ea77c16dbdcd9b6

--- core/java/com/android/internal/widget/RingSelector.java
+++ core/java/com/android/internal/widget/RingSelector.java
@@ -87,7 +87,7 @@ public class RingSelector extends ViewGroup {
      * Either {@link #HORIZONTAL} or {@link #VERTICAL}.
      */
     private int mOrientation;
-
+    private int mRingHover = -1; //Keeps track of last hovered custom app
     private Ring mLeftRing;
     private Ring mRightRing;
     private Ring mMiddleRing;
@@ -987,8 +987,36 @@ public class RingSelector extends ViewGroup {
         int deltaY = (int) y - ring.getTop() - (ring.getHeight() / 2);
         ring.offsetLeftAndRight(deltaX);
         ring.offsetTopAndBottom(deltaY);
+        setHoverBackLight(x,y);
         invalidate();
     }
+
+    private void setHoverBackLight(float x, float y) {
+        if (!mUseMiddleRing)
+            return;
+        boolean ringsTouched = false;
+        int q;
+        for (q = 0; q < 4; q++) {
+            if (!mSecRings[q].isHidden() && mSecRings[q].contains((int) x,(int) y)) {
+                ringsTouched = true;
+                break;
+            }
+        }
+        if (ringsTouched) {
+            if (mRingHover == -1) {
+                mRingHover = q;
+                mSecRings[q].setRingBackgroundResource(R.drawable.jog_ring_ring_pressed_green);
+            } else if (mRingHover != q) {
+                mSecRings[mRingHover].setRingBackgroundResource(R.drawable.jog_ring_ring_normal);
+                mRingHover = q;
+                mSecRings[q].setRingBackgroundResource(R.drawable.jog_ring_ring_pressed_green);
+            }
+        } else if (mRingHover != -1 && !mSecRings[mRingHover].isHidden()) {
+            mSecRings[mRingHover].setRingBackgroundResource(R.drawable.jog_ring_ring_normal);
+            mRingHover = -1;
+        }
+    }
+
 
     /**
      * Sets the left ring icon to a given resource.
