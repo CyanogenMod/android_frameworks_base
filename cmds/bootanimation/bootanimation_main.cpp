3a41423f883f3d43df06e448c37249c716959440
nebkat <nebkat@gmail.com>
2011-09-27 11:18:28 +0100

    Preference to disable bootanimation for faster boot. (1/2 Framework) Implemented in ICS
    On galaxys2 it went from 13s to 5s when enabled.
    
    Change-Id: I89bdbc11a939544b9fb3cfc48d49892da473ef92

--- cmds/bootanimation/bootanimation_main.cpp
+++ cmds/bootanimation/bootanimation_main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char** argv)
 #endif
 
     char value[PROPERTY_VALUE_MAX];
-    property_get("debug.sf.nobootanimation", value, "0");
+    property_get("persist.sys.nobootanimation", value, "0");
     int noBootAnimation = atoi(value);
     ALOGI_IF(noBootAnimation,  "boot animation disabled");
     if (!noBootAnimation) {
