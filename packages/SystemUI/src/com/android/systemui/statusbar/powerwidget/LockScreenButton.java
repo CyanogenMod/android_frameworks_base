bf65dcc8f983d2e919871dd188b5be91eb21c9af
Scott Brady <sbradymobile@gmail.com>
2011-10-07 15:03:41 -0700

    Fix for issue 2978: Lock screen notification power widget
    Change LockScreenButton to use the system setting for
    disabling the lockscreen.
    
    Change-Id: Ic7091df4967d8157263b4ac087a05d8968616e02

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/LockScreenButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/LockScreenButton.java
@@ -2,30 +2,20 @@ package com.android.systemui.statusbar.powerwidget;
 
 import com.android.systemui.R;
 
-import android.app.Activity;
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.Gravity;
-import android.widget.Toast;
+import android.provider.Settings;
 
 public class LockScreenButton extends PowerButton {
 
     private static Boolean LOCK_SCREEN_STATE = null;
 
-    private KeyguardLock mLock = null;
-
     public LockScreenButton() { mType = BUTTON_LOCKSCREEN; }
 
     @Override
     protected void updateState() {
-        getState();
-        if (LOCK_SCREEN_STATE == null) {
-            mIcon = R.drawable.stat_lock_screen_off;
-            mState = STATE_INTERMEDIATE;
-        } else if (LOCK_SCREEN_STATE) {
+        getState(mView.getContext());
+        if (LOCK_SCREEN_STATE) {
             mIcon = R.drawable.stat_lock_screen_on;
             mState = STATE_ENABLED;
         } else {
@@ -37,26 +27,9 @@ public class LockScreenButton extends PowerButton {
     @Override
     protected void toggleState() {
         Context context = mView.getContext();
-        getState();
-        if(LOCK_SCREEN_STATE == null) {
-            Toast msg = Toast.makeText(context, "Not yet initialized", Toast.LENGTH_LONG);
-            msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
-            msg.show();
-        } else {
-            getLock(context);
-            if (mLock != null) {
-                if (LOCK_SCREEN_STATE) {
-                  mLock.disableKeyguard();
-                  LOCK_SCREEN_STATE = false;
-                } else {
-                  mLock.reenableKeyguard();
-                  LOCK_SCREEN_STATE = true;
-                }
-            }
-        }
-
-        // we're handling this, so just update our buttons now
-        // this is UGLY, do it better later >.>
+        getState(context);
+        Settings.System.putInt(context.getContentResolver(),
+                    Settings.System.LOCKSCREEN_DISABLED, LOCK_SCREEN_STATE ? 1 : 0);
         update();
     }
 
@@ -69,19 +42,9 @@ public class LockScreenButton extends PowerButton {
         return true;
     }
 
-    private KeyguardLock getLock(Context context) {
-        if (mLock == null) {
-            KeyguardManager keyguardManager = (KeyguardManager)context.
-                    getSystemService(Activity.KEYGUARD_SERVICE);
-            mLock = keyguardManager.newKeyguardLock(Context.KEYGUARD_SERVICE);
-        }
-        return mLock;
-    }
-
-    private static boolean getState() {
-        if (LOCK_SCREEN_STATE == null) {
-            LOCK_SCREEN_STATE = true;
-        }
+    private static boolean getState(Context context) {
+        LOCK_SCREEN_STATE = (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.LOCKSCREEN_DISABLED, 0) == 0);
         return LOCK_SCREEN_STATE;
     }
 }
