562b2ded6fd5ebb9aa4ea42aa08a42895b62f4b4
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-10-19 17:47:56 +0200

    stagefright: TI 720P Codecs support
    Only enabled for jordan, we could add later BoardConfig variables
    
    Note: Droid 2 and Droid X also use these codecs
    
    Change-Id: I13a01acf600caf50d954f8c0f833a220d314bf56

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1047,17 +1047,37 @@ void AwesomePlayer::setAudioSource(sp<MediaSource> source) {
 
 status_t AwesomePlayer::initAudioDecoder() {
     sp<MetaData> meta = mAudioTrack->getFormat();
-
+    const char *componentName;
     const char *mime;
     CHECK(meta->findCString(kKeyMIMEType, &mime));
 
     if (!strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_RAW)) {
         mAudioSource = mAudioTrack;
     } else {
+#ifdef USE_TI720P_DECODER
+        if (!strcasecmp(mime, MEDIA_MIMETYPE_AUDIO_AAC) &&
+            (!strcmp(mComponentName, "OMX.TI.720P.Decoder") ||
+             !strcmp(mComponentName, "OMX.TI.Video.Decoder")) ) {
+
+            componentName = "OMX.TI.AAC.decode";
+
+            mAudioSource = OMXCodec::Create(
+                    mClient.interface(), mAudioTrack->getFormat(),
+                    false,
+                    mAudioTrack, componentName);
+        } else {
+
+            mAudioSource = OMXCodec::Create(
+                    mClient.interface(), mAudioTrack->getFormat(),
+                    false,
+                    mAudioTrack);
+        }
+#else
         mAudioSource = OMXCodec::Create(
                 mClient.interface(), mAudioTrack->getFormat(),
                 false, // createEncoder
                 mAudioTrack);
+#endif
     }
 
     if (mAudioSource != NULL) {
