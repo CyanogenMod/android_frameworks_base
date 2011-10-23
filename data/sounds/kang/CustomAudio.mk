1a2e3ee56d1b8ba2c49e7404b2d5a3c2132cb4b8
Ladios Jonquil <ladios.jonquil@gmail.com>
2011-10-23 22:53:10 +0800

    sounds: Allow kangers to include git-ignored audio files
    The audio directories will be created under
    frameworks/base/data/sounds/kang/ when you lunch or make. Just put
    your ringtones in any of those, and they will be included in your
    build. No further setup or makefile coding is required. And most
    importantly, they are transparent to git.
    
    Change-Id: Iddebabb6189299ee341334afd1ebe30bc3e01fc7

--- data/sounds/kang/CustomAudio.mk
+++ data/sounds/kang/CustomAudio.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2011 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,11 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-$(call inherit-product, frameworks/base/data/sounds/OriginalAudio.mk)
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage2.mk)
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage3.mk)
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage4.mk)
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage5.mk)
-$(call inherit-product, frameworks/base/data/sounds/kang/CustomAudio.mk)
+LOCAL_PATH:= frameworks/base/data/sounds/kang
+include $(CLEAR_VARS)
+
+audio_types := $(shell sed -e 's/^\#.*//' $(LOCAL_PATH)/.gitignore)
+$(shell mkdir -p $(addprefix $(LOCAL_PATH)/,$(audio_types)))
+audio_files := $(foreach type,$(audio_types),$(addprefix $(type),$(notdir $(wildcard $(LOCAL_PATH)/$(type)*))))
+audio_pairs := $(foreach file,$(audio_files),$(LOCAL_PATH)/$(file):system/media/audio/$(file))
+PRODUCT_COPY_FILES += $(audio_pairs)
