5d07df7cc373b4e12fd99aeb54cdef65e6fc0db1
Renaud Lepage <root@cybikbase.com>
2011-10-25 22:26:37 -0400

    Implementing Sunfire-related fixes - New RIL (fine-tuning to be done) - RIL Object creation modification - Fixing a bug involving the switch from GSM to CDMA (and back) - Fixed a CDMA bug seemingly specific to the TegraWorld bi-mode radio implementation
    Change-Id: Ic1aa7aae15b6e45a9968171bb1e8c216697791a8

--- telephony/java/com/android/internal/telephony/PhoneProxy.java
+++ telephony/java/com/android/internal/telephony/PhoneProxy.java
@@ -89,6 +89,8 @@ public class PhoneProxy extends Handler implements Phone {
             if(mOutgoingPhone.equals("GSM")) {
                 logd("Make a new CDMAPhone and destroy the old GSMPhone.");
 
+                // Since we already know we're disposing of this
+                CallManager.getInstance().unregisterPhone(mActivePhone);
                 ((GSMPhone)mActivePhone).dispose();
                 Phone oldPhone = mActivePhone;
 
@@ -100,10 +102,16 @@ public class PhoneProxy extends Handler implements Phone {
 
                 mActivePhone = PhoneFactory.getCdmaPhone();
                 ((GSMPhone)oldPhone).removeReferences();
+                
+                // out with the old...
                 oldPhone = null;
+
+                // ...and in with the new!
+                CallManager.getInstance().registerPhoneAsDefault(mActivePhone);
             } else {
                 logd("Make a new GSMPhone and destroy the old CDMAPhone.");
 
+                CallManager.getInstance().unregisterPhone(mActivePhone);
                 ((CDMAPhone)mActivePhone).dispose();
                 //mActivePhone = null;
                 Phone oldPhone = mActivePhone;
@@ -116,7 +124,9 @@ public class PhoneProxy extends Handler implements Phone {
 
                 mActivePhone = PhoneFactory.getGsmPhone();
                 ((CDMAPhone)oldPhone).removeReferences();
+                 
                 oldPhone = null;
+                CallManager.getInstance().registerPhoneAsDefault(mActivePhone);
             }
 
             if (mResetModemOnRadioTechnologyChange) {
