ac25f3ad69c4e5005579ea944d2d0068f4f8679b
Will DeBerry <hillbillyhacker86@gmail.com>
2011-10-29 12:48:29 -0400

    LockScreen Alignment: Right (Part 2/2)
    Add alignments to the Pattern UnlockScreen as well.
    
    Enable LandScape Alignment for everything except Right Alignment on default LockScreen as this causes overlapping issues.
    
    Change-Id: I9962172de7219d480f4925b20cd0884f7d0c4461

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -104,8 +104,10 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
     private TextView mDate;
     private TextView mTime;
     private TextView mAmPm;
-    private TextView mStatus1;
-    private TextView mStatus2;
+    private LinearLayout mStatusBox;
+    private TextView mStatusCharging;
+    private TextView mStatusAlarm;
+    private TextView mStatusCalendar;
     private TextView mScreenLocked;
     private TextView mEmergencyCallText;
     private Button mEmergencyCallButton;
@@ -136,7 +138,7 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
     private int mBatteryLevel = 100;
 
     private String mNextAlarm = null;
-    private Drawable mAlarmIcon = null;
+    private String mNextCalendar = null;
     private String mCharging = null;
     private Drawable mChargingIcon = null;
 
@@ -357,8 +359,11 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
         mTime = (TextView) findViewById(R.id.timeDisplay);
         mAmPm = (TextView) findViewById(R.id.am_pm);
         mDate = (TextView) findViewById(R.id.date);
-        mStatus1 = (TextView) findViewById(R.id.status1);
-        mStatus2 = (TextView) findViewById(R.id.status2);
+
+        mStatusBox = (LinearLayout) findViewById(R.id.status_box);
+        mStatusCharging = (TextView) findViewById(R.id.status_charging);
+        mStatusAlarm = (TextView) findViewById(R.id.status_alarm);
+        mStatusCalendar = (TextView) findViewById(R.id.status_calendar);
 
         mCustomMsg = (TextView) findViewById(R.id.customMsg);
 
@@ -673,31 +678,72 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
         }
 
         resetStatusInfo(updateMonitor);
-        centerWidgets();
+        switch (mWidgetLayout) {
+            case 2:
+                centerWidgets();
+                break;
+            case 3:
+                rightWidgets();
+                break;
+        }
     }
 
     private void centerWidgets() {
-        if (mWidgetLayout == 2) {
-            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
-            mCarrier.getLayoutParams();
+        RelativeLayout.LayoutParams layoutParams;
+        layoutParams = (RelativeLayout.LayoutParams) mCarrier.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        mCarrier.setLayoutParams(layoutParams);
+        mCarrier.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        mStatusBox.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        centerWidget(mClock);
+        centerWidget(mDate);
+        centerWidget(mStatusCharging);
+        centerWidget(mStatusAlarm);
+        centerWidget(mStatusCalendar);
+    }
+
+    private void centerWidget(View view) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        if (params instanceof RelativeLayout.LayoutParams) {
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.CENTER_HORIZONTAL, 1);
+        } else if (params instanceof LinearLayout.LayoutParams) {
+            LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) params;
+            p.gravity = Gravity.CENTER_HORIZONTAL;
+            p.leftMargin = 0;
+            p.rightMargin = 0;
+        }
+        view.setLayoutParams(params);
+    }
+
+    private void rightWidgets() {
+        if (mCreationOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            RelativeLayout.LayoutParams layoutParams;
+            layoutParams = (RelativeLayout.LayoutParams) mCarrier.getLayoutParams();
             layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
             mCarrier.setLayoutParams(layoutParams);
-            mCarrier.setGravity(Gravity.CENTER_HORIZONTAL);
-            layoutParams = (RelativeLayout.LayoutParams)mDate.getLayoutParams();
-            layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, 1);
-            mDate.setLayoutParams(layoutParams);
-            layoutParams = (RelativeLayout.LayoutParams)mStatus1.getLayoutParams();
-            layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, 1);
-            layoutParams.leftMargin = 0;
-            mStatus1.setLayoutParams(layoutParams);
-            layoutParams = (RelativeLayout.LayoutParams)mStatus2.getLayoutParams();
-            layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, 1);
-            layoutParams.leftMargin = 0;
-            mStatus2.setLayoutParams(layoutParams);
-            layoutParams = (RelativeLayout.LayoutParams)mClock.getLayoutParams();
-            layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, 1);
-            mClock.setLayoutParams(layoutParams);
+            mCarrier.setGravity(Gravity.LEFT);
+
+            mStatusBox.setGravity(Gravity.LEFT);
+
+            rightWidget(mClock);
+            rightWidget(mDate);
+            rightWidget(mStatusCharging);
+            rightWidget(mStatusAlarm);
+            rightWidget(mStatusCalendar);
         }
+    }
+
+    private void rightWidget(View view) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        if (params instanceof RelativeLayout.LayoutParams) {
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 1);
+        } else if (params instanceof LinearLayout.LayoutParams) {
+            LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) params;
+            p.gravity = Gravity.RIGHT;
+        }
+        view.setLayoutParams(params);
     }
 
     static void setBackground(Context bcontext, ViewGroup layout){
@@ -759,6 +805,7 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
 
         refreshBatteryStringAndIcon();
         refreshAlarmDisplay();
+        refreshCalendarDisplay();
         refreshMusicStatus();
         refreshPlayingTitle();
 
@@ -917,6 +964,8 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
 
     private void refreshAlarmDisplay() {
         mNextAlarm = mLockPatternUtils.getNextAlarm();
+        updateStatusLines();
+    }
 
         if (mNextAlarm != null) {
             mAlarmIcon = getContext().getResources().getDrawable(R.drawable.ic_lock_idle_alarm);
@@ -1043,33 +1092,32 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
     }
 
     private void updateStatusLines() {
-        if (!mStatus.showStatusLines()
-                || (mCharging == null && mNextAlarm == null) || mLensePortrait || mWidgetLayout == 1) {
-            mStatus1.setVisibility(View.INVISIBLE);
-            mStatus2.setVisibility(View.INVISIBLE);
-        } else if (mCharging != null && mNextAlarm == null) {
-            // charging only
-            mStatus1.setVisibility(View.VISIBLE);
-            mStatus2.setVisibility(View.INVISIBLE);
+        if (!mStatus.showStatusLines() || mLensePortrait || mWidgetLayout == 1) {
+            mStatusBox.setVisibility(INVISIBLE);
+        } else {
+            mStatusBox.setVisibility(VISIBLE);
 
-            mStatus1.setText(mCharging);
-            mStatus1.setCompoundDrawablesWithIntrinsicBounds(mChargingIcon, null, null, null);
-        } else if (mNextAlarm != null && mCharging == null) {
-            // next alarm only
-            mStatus1.setVisibility(View.VISIBLE);
-            mStatus2.setVisibility(View.INVISIBLE);
+            if (mCharging != null) {
+                mStatusCharging.setText(mCharging);
+                mStatusCharging.setCompoundDrawablesWithIntrinsicBounds(mChargingIcon, null, null, null);
+                mStatusCharging.setVisibility(VISIBLE);
+            } else {
+                mStatusCharging.setVisibility(GONE);
+            }
 
-            mStatus1.setText(mNextAlarm);
-            mStatus1.setCompoundDrawablesWithIntrinsicBounds(mAlarmIcon, null, null, null);
-        } else if (mCharging != null && mNextAlarm != null) {
-            // both charging and next alarm
-            mStatus1.setVisibility(View.VISIBLE);
-            mStatus2.setVisibility(View.VISIBLE);
+            if (mNextAlarm != null) {
+                mStatusAlarm.setText(mNextAlarm);
+                mStatusAlarm.setVisibility(VISIBLE);
+            } else {
+                mStatusAlarm.setVisibility(GONE);
+            }
 
-            mStatus1.setText(mCharging);
-            mStatus1.setCompoundDrawablesWithIntrinsicBounds(mChargingIcon, null, null, null);
-            mStatus2.setText(mNextAlarm);
-            mStatus2.setCompoundDrawablesWithIntrinsicBounds(mAlarmIcon, null, null, null);
+            if (mNextCalendar != null) {
+                mStatusCalendar.setText(mNextCalendar);
+                mStatusCalendar.setVisibility(VISIBLE);
+            } else {
+                mStatusCalendar.setVisibility(GONE);
+            }
         }
     }
 
