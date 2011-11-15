fe500dbd0af95112b9714ae17ae2ac9aae0d7a68
Gary King <gking@nvidia.com>
2011-11-15 21:16:17 +0000

    [opengl] implement work around for tegra errata 657451
    copy the tegra errata work around compile flags from bionic, to
    pick up the correct __get_tls implementation

--- opengl/libagl/Android.mk
+++ opengl/libagl/Android.mk
@@ -38,6 +38,10 @@ ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
     LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
 endif
 
+ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+    LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
+endif
+
 # we need to access the private Bionic header <bionic_tls.h>
 # on ARM platforms, we need to mirror the ARCH_ARM_HAVE_TLS_REGISTER
 # behavior from the bionic Android.mk file
