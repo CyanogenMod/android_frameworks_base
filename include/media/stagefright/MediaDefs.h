a6ec7958eddb7b93454e8158817e59760c7f0f6e
vivek mehta <mvivek@codeaurora.org>
2011-11-15 16:19:40 -0800

    framework/base: Change to enable mm-parser.
    - mm-parser related enums are added in framework
    - adding ExtendedExtractor module to plug-in mm-parser with framework
    - adding MIME type that are supported by mm-parser
    
    Patchset 1: Add QCOM_HARDWARE ifdefs.
    
    Change-Id: I06b330275605a84668c6690570805892eb2b17d0
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- include/media/stagefright/MediaDefs.h
+++ include/media/stagefright/MediaDefs.h
@@ -55,6 +55,20 @@ extern const char *MEDIA_MIMETYPE_CONTAINER_WVM;
 
 extern const char *MEDIA_MIMETYPE_TEXT_3GPP;
 
+#ifdef QCOM_HARDWARE
+extern const char *MEDIA_MIMETYPE_AUDIO_EVRC;
+
+extern const char *MEDIA_MIMETYPE_VIDEO_WMV;
+extern const char *MEDIA_MIMETYPE_AUDIO_WMA;
+extern const char *MEDIA_MIMETYPE_CONTAINER_ASF;
+extern const char *MEDIA_MIMETYPE_VIDEO_DIVX;
+extern const char *MEDIA_MIMETYPE_AUDIO_AC3;
+extern const char *MEDIA_MIMETYPE_CONTAINER_AAC;
+extern const char *MEDIA_MIMETYPE_CONTAINER_QCP;
+extern const char *MEDIA_MIMETYPE_VIDEO_DIVX311;
+extern const char *MEDIA_MIMETYPE_VIDEO_DIVX4;
+extern const char *MEDIA_MIMETYPE_CONTAINER_MPEG2;
+#endif
 }  // namespace android
 
 #endif  // MEDIA_DEFS_H_
