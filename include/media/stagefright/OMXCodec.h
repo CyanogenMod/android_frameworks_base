beb1d4b2503562d65f452e1c57307220b058ae14
vivek mehta <mvivek@codeaurora.org>
2011-11-15 19:34:08 -0800

    frameworks/base: Enabling mmparser for AVI, ASF, QCP, AAC formats
    - Add support in MediaScanner for AVI, DIVX
    - Add new mime definitions for AVI, DIVX, WMA, WMV, AAC, QCP, AC3, DIVX311
    - Add Extended Extractor, register new sniffers
    - Enable DIVX, VC1, WMA, QCELP components in OMXCodec
    
    Patchset 1: Add QCOM_HARDWARE ifdefs.
    Patchset 2: Fix AVIExtractor include
    Patchset 3: Whitespace.
    
    Conflicts:
    
    	include/media/stagefright/OMXCodec.h
    	media/libstagefright/DataSource.cpp
    	media/libstagefright/Android.mk
    	media/libstagefright/OMXCodec.cpp
    	media/libstagefright/StagefrightMediaScanner.cpp
    
    Change-Id: Ie92c8a95f513bc9b915a46144adb603d03d1fa18
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
- *
+ * Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*--------------------------------------------------------------------------
+Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+--------------------------------------------------------------------------*/
 
 #ifndef OMX_CODEC_H_
 
@@ -117,6 +120,9 @@ private:
     };
 
     enum {
+#ifdef QCOM_HARDWARE
+        kPortIndexBoth   = -1,
+#endif
         kPortIndexInput  = 0,
         kPortIndexOutput = 1
     };
@@ -146,6 +152,8 @@ private:
         kOutputBuffersAreUnreadable           = 8192,
 #ifdef QCOM_HARDWARE
         kStoreMetaDataInInputVideoBuffers     = 16384,
+        kRequiresGlobalFlush                  = 0x20000000, // 2^29
+        kRequiresWMAProComponent              = 0x40000000, //2^30
 #endif
     };
 
@@ -245,7 +253,13 @@ private:
 
     void setAMRFormat(bool isWAMR, int32_t bitRate);
     status_t setAACFormat(int32_t numChannels, int32_t sampleRate, int32_t bitRate);
+#ifdef QCOM_HARDWARE
+    void setEVRCFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
+#endif
     void setG711Format(int32_t numChannels);
+#ifdef QCOM_HARDWARE
+    void setQCELPFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
+#endif
 
     status_t setVideoPortFormatType(
             OMX_U32 portIndex,
@@ -361,6 +375,10 @@ private:
 
     OMXCodec(const OMXCodec &);
     OMXCodec &operator=(const OMXCodec &);
+#ifdef QCOM_HARDWARE
+    status_t setWMAFormat(const sp<MetaData> &inputFormat);
+    void setAC3Format(int32_t numChannels, int32_t sampleRate);
+#endif
 };
 
 struct CodecCapabilities {
