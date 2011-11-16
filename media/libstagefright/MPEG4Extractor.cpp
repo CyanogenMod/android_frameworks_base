01d2c9fe0c03a9ead1b1f3538955e2aba5959e73
Shalaj Jain <shalajj@codeaurora.org>
2011-11-15 20:25:25 -0800

    libstagefright: Adding support for k3g1, skm3 and mp41 major brands
    This change adds support for ftyp's k3g1, skm3 and mp41 major brands in
    the mp4 parser
    
    Change-Id: I816f80ec1ab288df15a41116bb0b2fedd3b2f692
    CRs-Fixed: 257978

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -2309,7 +2309,8 @@ static bool LegacySniffMPEG4(
         || !memcmp(header, "ftypM4A ", 8) || !memcmp(header, "ftypf4v ", 8)
         || !memcmp(header, "ftypkddi", 8) || !memcmp(header, "ftypM4VP", 8)
         || !memcmp(header, "ftypMSNV", 8) || !memcmp(header, "ftypavc1", 8)
-        || !memcmp(header, "ftypmmp4", 8)) {
+        || !memcmp(header, "ftypmmp4", 8) || !memcmp(header, "ftypk3g1", 8)
+        || !memcmp(header, "ftypmp41", 8) || !memcmp(header, "ftypskm3", 8)) {
         *mimeType = MEDIA_MIMETYPE_CONTAINER_MPEG4;
         *confidence = 0.6;
 
