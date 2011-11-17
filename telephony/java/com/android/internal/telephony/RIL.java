57b1db5f9a9696195acb1f5ac19a70258a897839
Ricardo Cerqueira <github@cerqueira.org>
2011-11-17 11:16:23 +0000

    RIL: Support signal strength format from v3 vendor RILs
    Enable by setting property ro.telephony.ril.v3=1

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -3232,10 +3232,17 @@ public class RIL extends BaseCommands implements CommandsInterface {
         int numInts = 12;
         int response[];
 
+        int oldRil = SystemProperties.getInt("ro.telephony.ril.v3",
+                0);
+
         /* TODO: Add SignalStrength class to match RIL_SignalStrength */
         response = new int[numInts];
         for (int i = 0 ; i < numInts ; i++) {
-            response[i] = p.readInt();
+            if (oldRil!=0 && i>6 && i<12) {
+                response[i] = -1;
+            } else {
+                response[i] = p.readInt();
+            }
         }
 
         return response;
