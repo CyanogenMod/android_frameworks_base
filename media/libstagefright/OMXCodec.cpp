2c07fa6ef39c7098ede56a057688f4cd5c180d70
Karthik Gunda <gunda@codeaurora.org>
2011-11-20 12:51:33 +0530

    libstagefright: Enable hardware decoder for mp3
    Enable hardware decoder for mp3 audio format
    
    Patchset 1: Add QCOM_HARDWARE ifdefs.
    
    Change-Id: I8c4fcc14dbfa5748db5ba9e93b1dc16565abee5f
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -176,6 +176,9 @@ static sp<MediaSource> InstantiateSoftwareEncoder(
 
 static const CodecInfo kDecoderInfo[] = {
     { MEDIA_MIMETYPE_IMAGE_JPEG, "OMX.TI.JPEG.decode" },
+#ifdef QCOM_HARDWARE
+    { MEDIA_MIMETYPE_AUDIO_MPEG, "OMX.qcom.audio.decoder.mp3" },
+#endif
 //    { MEDIA_MIMETYPE_AUDIO_MPEG, "OMX.TI.MP3.decode" },
     { MEDIA_MIMETYPE_AUDIO_MPEG, "OMX.google.mp3.decoder" },
     { MEDIA_MIMETYPE_AUDIO_MPEG_LAYER_II, "OMX.Nvidia.mp2.decoder" },
