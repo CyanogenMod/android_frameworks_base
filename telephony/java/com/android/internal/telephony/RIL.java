421d92c44df4e05792d6e1a40382e810711c720a
Emilio López <turl@tuxfamily.org>
2011-11-20 22:12:28 +0000

    RIL: handle the lack of version gracefully on older RILs
    Change-Id: I8227c55968f502112c5b6ac334eee505c29924c2

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -3027,8 +3027,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
     protected Object
     responseDataCallList(Parcel p) {
         ArrayList<DataCallState> response;
-
-        int ver = p.readInt();
+        int oldRil = SystemProperties.getInt("ro.telephony.ril.v3", 0);
+        int ver = (oldRil!=0 ? 3 : p.readInt());
         int num = p.readInt();
         riljLog("responseDataCallList ver=" + ver + " num=" + num);
 
@@ -3042,7 +3042,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
     protected Object
     responseSetupDataCall(Parcel p) {
-        int ver = p.readInt();
+        int oldRil = SystemProperties.getInt("ro.telephony.ril.v3", 0);
+        int ver = (oldRil!=0 ? 3 : p.readInt());
         int num = p.readInt();
         if (RILJ_LOGV) riljLog("responseSetupDataCall ver=" + ver + " num=" + num);
 
