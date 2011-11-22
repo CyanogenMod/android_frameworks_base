4ad30deefbea1b442707a46f13d76b1be4cfcc75
Ankit Premrajka <ankitp@codeaurora.org>
2011-11-21 19:25:02 -0800

    Add customized parameters for qcom camera features.
    Squashed commit of the following submissions to CAF:
    
    camera: add custom camera parameters to hardware java file.
    
    Change-Id: Ic2b363df6d25a0c3a49aed30e717d10874f533ae
    
    Camera: Add support for wavelet denoise
    
    Change-Id: I97be72615fe5909987ac430034bbdc16f2d20509
    
    Camera : Add support for histogram in frameworks
    
    Change-Id: Ibf751c3b2d488f4be0afaaa3e994372bafeffbe7
    
    Camera: Add ZSL support in framework.
    
    Change-Id: I4e863020d7d407d9c3936beb83dc7abf385c4654
    
    Camera: Add touch AF/AEC support in ICS framework
    
    In, ICS the old touch AF/AEC functions getTouchIndexAf(),
    setTouchIndexAf() etc  are no longer valid. Added new functions  to get
    the center of touched area.
    
    Change-Id: I37d00040f895cd8a4de2e2a438971aa2d9c1a045
    
    Camera: Add support for Red eye reduction
    
    Change-Id: Iada280ac33aee8b1114dc40d8d79e0e47ad5c2fd
    
    Conflicts:
    
    	api/current.txt
    
    Camera: Enable streaming texture and YV12 feature
    
    Change-Id: I814c92b469e0f25baef0ce80338761d1e6443085
    
    Camera: Add frameworks support for high frame rate
    
    Change-Id: I9722e9b7e61433651ab5f76f76208853eb1fd849
    
    Camera: Simplify ASD menu and best mode menu.
    
    Change-Id: Ia7a1eca9268eec021a6e2c3f7ce2a932e8819d50
    
    Camera : Macro definition change. PIXEL_FORMAT_YV12 to "yuv420p"
    
    Why: CTS testcase is expecting "yuv420p" as the format string for YV12 mode.
    
    CRs-Fixed: 328252
    (cherry picked from commit 0a263f67b75497e4b95b9de8c370ed9496ab59ef)
    
    Change-Id: I66f418a52775a969d81bcb332bbe500370e3ab7c
    
    Camera: Changes for face detection
    
    Change-Id: I7b2172e8690d6e23762e42f5888fecbfda15ba61
    
    Camera: Add support for burst count
    
    Change-Id: I4488a5eac1eebb0b5c8e2cc7cbbe7be0c7301be7
    
    Camera: enable HDR feature
    
    (cherry picked from commit e40770bbdadd925fc1e07a08249a15981b961570)
    
    Change-Id: Ie9ca3066e8e53710bf292d047a3724aefc029854
    
    Camera : Add API to find fullsize liveshot support
    
    Change-Id: I3c2fc3578decbe5cf87fded5362f317df0b40b5e
    (cherry picked from commit 65d80f04892776eec1c19c844eacb8c454b4bd1a)
    
    Camera: Set default camera area string
    
    The default invalid value for KEY_FOCUS_AREA and
    KEY_METERING_AREA is changed to "(0,0,0,0,0)"
    CRs-fixed: 334934
    
    Change-Id: I354802c8611ee5c00a591b96b1a1ca04926969cb
    
    Camera: Change the scenemode names in framework
    
    To comply with ICS API Specification,
    SCENE_MODE_AUTO changed to SCENE_MODE_ASD
    SCENE_MODE_OFF changed to SCENE_MODE_AUTO
    
    Change-Id: I8e744f10c728f469923b09f69d6d64bc5c7c6850
    
    Camera : Framework changes for camcorder UI menu
    
    Change-Id: Iff9e9021da44b67f81a24c828b960113ee249248
    
    Camera : Remove unwanted video resolution in Framework
    
    - FWVGA, WQVGA resolution UI options are removed for camcorder.
    
    Change-Id: If621140966b01dbc8c30a4d161e6771528b8adda
    
    Camera : Enable FWVGA and WQVGA resolutions in camcorder media profiles
    
    Change-Id: Iad9cc33f7b6d8eb7de21df372c016edeef0b99ba
    
    Unlock camera AWB lock only when white-balance is changed.
    
    bug:5633546
    Change-Id: I90bb4b5b445daedf38a64e2f8fb980fc0f0aaa81
    
    Change-Id: Ie3cc15132489502e64321776d49d96846b5705d6

--- media/jni/android_media_MediaProfiles.cpp
+++ media/jni/android_media_MediaProfiles.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-//#define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 #define LOG_TAG "MediaProfilesJNI"
 #include <utils/Log.h>
 
@@ -36,12 +36,15 @@ MediaProfiles *sProfiles = NULL;
 static void
 android_media_MediaProfiles_native_init(JNIEnv *env)
 {
-    LOGV("native_init");
+    LOGE("native_init");
     Mutex::Autolock lock(sLock);
+    LOGE("After native_init lock");
 
     if (sProfiles == NULL) {
+        LOGE("Guru : native_init profiles");
         sProfiles = MediaProfiles::getInstance();
     }
+    LOGE("retun sProfiles");
 }
 
 static jint
@@ -172,7 +175,7 @@ static bool isCamcorderQualityKnown(int quality)
 static jobject
 android_media_MediaProfiles_native_get_camcorder_profile(JNIEnv *env, jobject thiz, jint id, jint quality)
 {
-    LOGV("native_get_camcorder_profile: %d %d", id, quality);
+    LOGE("native_get_camcorder_profile: %d %d", id, quality);
     if (!isCamcorderQualityKnown(quality)) {
         jniThrowException(env, "java/lang/RuntimeException", "Unknown camcorder profile quality");
         return NULL;
@@ -221,7 +224,7 @@ android_media_MediaProfiles_native_get_camcorder_profile(JNIEnv *env, jobject th
 static jboolean
 android_media_MediaProfiles_native_has_camcorder_profile(JNIEnv *env, jobject thiz, jint id, jint quality)
 {
-    LOGV("native_has_camcorder_profile: %d %d", id, quality);
+    LOGE("native_has_camcorder_profile: %d %d", id, quality);
     if (!isCamcorderQualityKnown(quality)) {
         return false;
     }
