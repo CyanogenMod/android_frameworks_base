68cbf843aed8fd956b2bd60858331a2f236ec575
Luis GarciaCalderon <luisgc@codeaurora.org>
2011-11-23 19:05:43 -0800

    libstagefright: Add NV12 Tile to RGB color conversion
    - Adds support of NV12 Tile to RGB565 color conversion
    using the qcom color convertor library.
    
    Patchset 1: Add QCOM_HARDWARE ifdefs
    
    Change-Id: I8f6fa92651b666376af514b46a5027f56492dd43

--- include/media/stagefright/ColorConverter.h
+++ include/media/stagefright/ColorConverter.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
