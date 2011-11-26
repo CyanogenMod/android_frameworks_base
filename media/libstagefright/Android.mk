e8d80d3c28f129894a4a71d8c74897254a2ca548
Ramakant Singh <ramaka@codeaurora.org>
2011-11-26 18:37:19 +0530

    frameworks/base: libstagefright: use new gralloc header
    Conflicts:
    
    	media/libstagefright/Android.mk
    
    Change-Id: Ia4dcd07bb62ce767f8d714bc84df016adc1a0552
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -183,9 +183,10 @@ LOCAL_SHARED_LIBRARIES += \
 LOCAL_CFLAGS += -Wno-multichar
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-        LOCAL_C_INCLUDES += $(TOP)/hardware/msm7k/libgralloc-qsd8k
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libgralloc
         LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/omx/mm-core/omxcore/inc
         LOCAL_C_INCLUDES += $(TOP)/system/core/include
+        LOCAL_C_INCLUDES += $(TOP)/hardware/libhardware_legacy/include
         LOCAL_CFLAGS += -DQCOM_HARDWARE
 endif
 
