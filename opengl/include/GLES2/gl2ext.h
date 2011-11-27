c5f10b563313415d9369ea8d38ebdb8b0095af35
Hashcode <hashcode0f@gmail.com>
2011-11-27 03:48:56 -0800

    OpenGL hack to enable hwacceleration, from hashcode, cleaned up
    support for GL apis without GL_TEXTURE_EXTERNAL_OES
    
    note: personally i use a common global cflag, the board flag
          seems not enough :
    
    COMMON_GLOBAL_CFLAGS += -DBOARD_GL_OES_EGL_IMG_EXTERNAL_HACK
    
    Change-Id: Ib3d9882a802dccf0e063d135511417cd5c227ea9

--- opengl/include/GLES2/gl2ext.h
+++ opengl/include/GLES2/gl2ext.h
@@ -148,7 +148,11 @@ typedef void* GLeglImageOES;
 
 /* GL_OES_EGL_image_external */
 #ifndef GL_OES_EGL_image_external
+#ifdef BOARD_GL_OES_EGL_IMG_EXTERNAL_HACK
+#define GL_TEXTURE_EXTERNAL_OES                                 0x0DE1
+#else
 #define GL_TEXTURE_EXTERNAL_OES                                 0x8D65
+#endif
 #define GL_SAMPLER_EXTERNAL_OES                                 0x8D66
 #define GL_TEXTURE_BINDING_EXTERNAL_OES                         0x8D67
 #define GL_REQUIRED_TEXTURE_IMAGE_UNITS_OES                     0x8D68
--- opengl/libagl/state.cpp
+++ opengl/libagl/state.cpp
@@ -191,10 +191,11 @@ static void enable_disable(ogles_context_t* c, GLenum cap, int enabled)
         // these need to fall through into the rasterizer
         c->rasterizer.procs.enableDisable(c, cap, enabled);
         break;
+#ifndef BOARD_GL_OES_EGL_IMG_EXTERNAL_HACK
     case GL_TEXTURE_EXTERNAL_OES:
         c->rasterizer.procs.enableDisable(c, GL_TEXTURE_2D, enabled);
         break;
-
+#endif
     case GL_MULTISAMPLE:
     case GL_SAMPLE_ALPHA_TO_COVERAGE:
     case GL_SAMPLE_ALPHA_TO_ONE:
