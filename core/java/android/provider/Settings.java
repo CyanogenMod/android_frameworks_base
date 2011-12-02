73bb81fa9d6a5d7adfc06d87f8b115d4ab84ae46
KalimochoAz <calimochoazucarado@gmail.com>
2011-12-02 19:10:02 +0100

    Add framework definition to include Torch
    Change-Id: I21de929089b06e5022f90c1fab6228a543102a19

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1870,6 +1870,11 @@ public final class Settings {
         public static final String SIP_ASK_ME_EACH_TIME = "SIP_ASK_ME_EACH_TIME";
 
         /**
+         * Torch state (flashlight)
+         * @hide
+         */
+        public static final String TORCH_STATE = "torch_state";
+        /**
          * Pointer speed setting.
          * This is an integer value in a range between -7 and +7, so there are 15 possible values.
          *   -7 = slowest
@@ -1878,6 +1883,12 @@ public final class Settings {
          * @hide
          */
         public static final String POINTER_SPEED = "pointer_speed";
+
+        /**
+        * Notification Power Widget - Custom Torch Mode
+        * @hide
+        */
+        public static final String EXPANDED_FLASH_MODE = "expanded_flash_mode";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
