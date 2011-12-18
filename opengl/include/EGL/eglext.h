78c1d400ae7b50efbf48da8198022f877de60bb8
Arne Coucheron <arco68@gmail.com>
2011-12-18 07:24:45 +0100

    opengl/libs/EGL: Bring back eglRenderBufferANDROID extention
    This change brings back the eglGetRenderBufferANDROID extension Define EGL_EGLEXT_PROTOTYPES in order to be able to use this extension.
    
        Revert "cleanup EGL a bit. remove unused extension."
    
        This reverts commit 9d75f1341524910b00d35dd4eeb426d7e7791f6e.
    
    Change-Id: I8b3805157cb1f72af507bc823d16910b738cec4c

--- opengl/include/EGL/eglext.h
+++ opengl/include/EGL/eglext.h
@@ -229,6 +229,20 @@ struct ANativeWindowBuffer;
 #define EGL_NATIVE_BUFFER_ANDROID               0x3140  /* eglCreateImageKHR target */
 #endif
 
+#ifdef QCOM_HARDWARE
+#ifndef EGL_EGLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES 1
+#endif
+
+#ifndef EGL_ANDROID_get_render_buffer
+#define EGL_ANDROID_get_render_buffer 1
+#ifdef EGL_EGLEXT_PROTOTYPES
+EGLAPI EGLClientBuffer EGLAPIENTRY eglGetRenderBufferANDROID(EGLDisplay dpy, EGLSurface draw);
+#endif
+typedef EGLClientBuffer (EGLAPIENTRYP PFNEGLGETRENDERBUFFERANDROIDPROC) (EGLDisplay dpy, EGLSurface draw);
+#endif
+#endif // QCOM_HARDWARE
+
 #ifndef EGL_ANDROID_recordable
 #define EGL_ANDROID_recordable 1
 #define EGL_RECORDABLE_ANDROID                  0x3142  /* EGLConfig attribute */
