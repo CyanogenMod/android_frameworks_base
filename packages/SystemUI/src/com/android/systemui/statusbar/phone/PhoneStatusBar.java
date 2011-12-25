6728e2172d6f0a0a207b6f232bfdc91a20f5b024
Danesh M <daneshm90@gmail.com>
2011-12-25 18:38:22 -0500

    This allows the ability to control brightness by sliding across the statusbar.
    Currently, off by default, until CMSettings is merged.
    
    Patchset 2 : Cleanup as per suggestions.
                 Removed duplicate key from Settings, after settings merge
                 Reduce mLinger threshold (Need feedback on whether its too sensitive for some)
    Patchset 3 : Minor cleanup
    
    Change-Id: I19ee7de07be7bfeabde3fb2ae3c60ebdbb4afd72

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -38,12 +38,14 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.IPowerManager;
 import android.os.RemoteException;
 import android.os.Handler;
 import android.os.Message;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Slog;
@@ -226,6 +228,7 @@ public class PhoneStatusBar extends StatusBar {
     boolean mAnimatingReveal = false;
     int mViewDelta;
     int[] mAbsPos = new int[2];
+    int mLinger = 0;
     Runnable mPostCollapseCleanup = null;
 
 
@@ -1499,6 +1502,7 @@ public class PhoneStatusBar extends StatusBar {
         final int hitSize = statusBarSize*2;
         final int y = (int)event.getRawY();
         if (action == MotionEvent.ACTION_DOWN) {
+            mLinger = 0;
             if (!mExpanded) {
                 mViewDelta = statusBarSize - y;
             } else {
@@ -1523,7 +1527,62 @@ public class PhoneStatusBar extends StatusBar {
             final int minY = statusBarSize + mCloseView.getHeight();
             if (action == MotionEvent.ACTION_MOVE) {
                 if (mAnimatingReveal && y < minY) {
-                    // nothing
+                    try {
+                        //Based on code from daryelv (https://github.com/daryelv)
+                        boolean brightnessControl = Settings.System.getInt(mStatusBarView.getContext().getContentResolver(),
+                                Settings.System.STATUS_BAR_BRIGHTNESS_TOGGLE) == 1;
+                        if (brightnessControl){
+                            mVelocityTracker.computeCurrentVelocity(1000);
+                            float yVel = mVelocityTracker.getYVelocity();
+                            if (yVel < 0) {
+                                yVel = -yVel;
+                            }
+                            if (yVel < 50.0f) {
+                                if (mLinger > 20) {
+                                    Context context = mStatusBarView.getContext();
+                                    boolean autoBrightness = false;
+                                    try {
+                                        autoBrightness = Settings.System.getInt(context.getContentResolver(),
+                                                Settings.System.SCREEN_BRIGHTNESS_MODE) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+                                    }catch (SettingNotFoundException e) {
+                                        autoBrightness = false;
+                                    }
+                                    if (!autoBrightness) {
+                                        float x = (float) event.getRawX();
+                                        float screenWidth = (float) context.getResources().getDisplayMetrics().widthPixels;
+                                        final int maxBrightness = (int) Math.round(android.os.Power.BRIGHTNESS_ON);
+                                        final int minBrightness = context.getResources().getInteger(
+                                                com.android.internal.R.integer.config_screenBrightnessDim);
+                                        int newBrightness = (int) Math.round(((x/screenWidth) * maxBrightness));
+                                        if (newBrightness < minBrightness) {
+                                            newBrightness = minBrightness;
+                                        } else if (newBrightness > maxBrightness) {
+                                            newBrightness = maxBrightness;
+                                        }
+                                        try {
+                                            IPowerManager power = IPowerManager.Stub.asInterface(ServiceManager.getService("power"));
+                                            if (power != null) {
+                                                power.setBacklightBrightness(newBrightness);
+                                                Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS,
+                                                        newBrightness);
+                                            }
+                                        } catch (RemoteException e) {
+                                            Slog.w(TAG, "Setting Brightness failed: " + e);
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    mLinger++;
+                                }
+                            }
+                            else
+                            {
+                                mLinger = 0;
+                            }
+                        }
+                    }catch (SettingNotFoundException e){
+                    }
                 } else  {
                     mAnimatingReveal = false;
                     updateExpandedViewPos(y + mViewDelta);
