3097d7d36e8459b6bc5f95cb4a4b0550e04cb740
Sushil Chauhan <sushilchauhan@codeaurora.org>
2012-01-04 14:42:07 -0800

    SurfaceTextureClient: Add null-pointer check.
    Change-Id: I3b69b92f59b4fd3fe28cc55816004b0dfe05549e
    CRs-Fixed: 328648

--- libs/gui/SurfaceTextureClient.cpp
+++ libs/gui/SurfaceTextureClient.cpp
@@ -188,6 +188,14 @@ int SurfaceTextureClient::cancelBuffer(android_native_buffer_t* buffer) {
 
 int SurfaceTextureClient::getSlotFromBufferLocked(
         android_native_buffer_t* buffer) const {
+
+#ifdef QCOM_HARDWARE
+    if (buffer == NULL) {
+        LOGE("getSlotFromBufferLocked: encountered NULL buffer");
+        return BAD_VALUE;
+    }
+#endif
+
     bool dumpedState = false;
     for (int i = 0; i < NUM_BUFFER_SLOTS; i++) {
         // XXX: Dump the slots whenever we hit a NULL entry while searching for
