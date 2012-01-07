a4b66e9d931573249020ee3106bf5286f5fa8c37
Michaël Burtin <bukington@gmail.com>
2012-01-07 10:33:12 +0100

    Add support for recent Huawei Qualcom RIL
    Override RIL class with some Huawei specific calls.
    Add a new CARDSTATE_NOT_READY state to Icc card status.
    Add Huawei specific EF path.
    Change the definition of NETWORK_LOCKED app state for Huawei RIL.
    
    Change-Id: I66e4d7a42a0a5ca416e5d9e114e41c184afe5bd7

--- telephony/java/com/android/internal/telephony/IccCardStatus.java
+++ telephony/java/com/android/internal/telephony/IccCardStatus.java
@@ -29,7 +29,8 @@ public class IccCardStatus {
     public enum CardState {
         CARDSTATE_ABSENT,
         CARDSTATE_PRESENT,
-        CARDSTATE_ERROR;
+        CARDSTATE_ERROR,
+        CARDSTATE_NOT_READY;
 
         boolean isCardPresent() {
             return this == CARDSTATE_PRESENT;
@@ -81,6 +82,9 @@ public class IccCardStatus {
             break;
         case 2:
             mCardState = CardState.CARDSTATE_ERROR;
+            break;
+        case 3:
+            mCardState = CardState.CARDSTATE_NOT_READY;
             break;
         default:
             throw new RuntimeException("Unrecognized RIL_CardState: " + state);
