5a63233c9ca4f7aa2e57c776fb275e5c3d78d2ff
James Peterson <james.d.peterson@gmail.com>
2012-01-08 10:37:16 -0600

    LCD Brightness - continued change to allow lcd dimming in both docked and undocked modes.  It does not make sence to not allow screen to dim in either instance. Better options are welcome.
    Change-Id: I501ec96fce64b9629a198ab713858eabee602600

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -2699,12 +2699,7 @@ public class PowerManagerService extends IPowerManager.Stub
         if (mLightSensorValue != value) {
             mLightSensorValue = value;
             if ((mPowerState & BATTERY_LOW_BIT) == 0) {
-                // use maximum light sensor value seen since screen went on for LCD to avoid flicker
-                // we only do this if we are docked, since lighting should be stable when
-                // stationary in a dock.
-                int lcdValue = getAutoBrightnessValue(
-                        (!mIsDocked ? value : mHighestLightSensorValue),
-                        mLastLcdValue,
+                int lcdValue = getAutoBrightnessValue(value, mLastLcdValue,
                         (mCustomLightEnabled ? mCustomLightLevels : mAutoBrightnessLevels),
                         (mCustomLightEnabled ? mCustomLcdValues : mLcdBacklightValues));
 
