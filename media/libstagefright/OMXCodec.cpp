bc1f7c8498538a442bd7b5c5843a516d88cd7344
Haynes Mathew George <hgeorge@codeaurora.org>
2012-01-12 15:12:40 -0800

    libstagefright: Fix to free buffers properly in ERROR state
    - In the ERROR state, OMXCodec::on_message tries to free the output
      buffers in the FBD sequence. This can fail if the OMX component hasn't
      been moved to idle state.
    - OMXNodeInstance does not check for the return value of OMX_FreeBuffer
      call and returns the buffer entry from the list of active buffers
    - This buffer ends up being unfreed which can adversely affect
      the next video playback
    
    (cherry picked from commit 6ef3096b6db1cee7fec57da3ae4c519cdaa28bbb)
    
    Change-Id: I9d80fd0f3c3e75da41697370f787df72316b32e4
    CRs-Fixed: 329874

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3370,6 +3370,13 @@ status_t OMXCodec::freeBuffer(OMX_U32 portIndex, size_t bufIndex) {
     if (err == OK) {
         buffers->removeAt(bufIndex);
     }
+#ifdef QCOM_HARDWARE
+    else {
+        LOGW("Warning, free Buffer failed, to be freed later"
+             " returning OK to prevent crash..");
+        return OK;
+    }
+#endif
 
     return err;
 }
@@ -4550,6 +4557,12 @@ status_t OMXCodec::stop() {
             if (state != OMX_StateExecuting) {
                 break;
             }
+#ifdef QCOM_HARDWARE
+            else {
+                CODEC_LOGV("Component is still in executing state, fall through and move component"
+                           " to idle");
+            }
+#endif
             // else fall through to the idling code
             isError = true;
         }
