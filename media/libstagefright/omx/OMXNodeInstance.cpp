aa1b2d03e61f526a2c4dc5445823598646f4bc69
Mohan Kumar Gubbihalli Lachma Naik <mgubbi@codeaurora.org>
2012-01-13 16:51:52 -0800

    libstagefright: Fix crash during 3D Video playback
    The crash was observed while handling extra data in FTB.
    This was due to improper assignment of buffer handle to
    pBuffer. This assignement is corrected in OMX component
    and hence CHECK_EQ for buffer handle in OMX nodeInstance
    is removed.
    
    Change-Id: Ib1eb206a201f2e23aec2137b64b93bcb0fc2d221
    CRs-Fixed: 328535

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -455,7 +455,9 @@ status_t OMXNodeInstance::useGraphicBuffer2_l(
         return UNKNOWN_ERROR;
     }
 
+#ifndef QCOM_HARDWARE
     CHECK_EQ(header->pBuffer, bufferHandle);
+#endif
     CHECK_EQ(header->pAppPrivate, bufferMeta);
 
     *buffer = header;
