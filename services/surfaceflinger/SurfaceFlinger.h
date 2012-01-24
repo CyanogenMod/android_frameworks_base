bb3c66eea8281c95421c0f1c9d90794fc84ed042
Won-Kyu Park <wkpark@gmail.com>
2012-01-24 22:35:33 +0900

    Support screenshot / renderScreenToTexture under OpenGL ES 1.0
    current implemented screenshot / renderScreenToTexture() of
    SurfaceFlinger use FrameBuffer Object(FBO) in the OpenGL ES 1.1/2.0
    extention
    
    directly use framebuffer device /dev/graphics/fb0 to emulate FBO.
    
    Tested under the old adreno130/msm7201 (unofficial CM9)
    
    Please see also http://review.cyanogenmod.com/11022
    
    Change-Id: I0eada5bfe9b389f491691b770f5662935d0fe8e4

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -341,6 +341,15 @@ private:
                     uint32_t reqWidth, uint32_t reqHeight,
                     uint32_t minLayerZ, uint32_t maxLayerZ);
 
+            status_t directCaptureScreenImplLocked(DisplayID dpy,
+                    sp<IMemoryHeap>* heap,
+                    uint32_t* width, uint32_t* height, PixelFormat* format,
+                    uint32_t reqWidth, uint32_t reqHeight,
+                    uint32_t minLayerZ, uint32_t maxLayerZ);
+
+            status_t directRenderScreenToTextureLocked(DisplayID dpy,
+                    GLuint* textureName, GLfloat* uOut, GLfloat* vOut);
+
             status_t turnElectronBeamOffImplLocked(int32_t mode);
             status_t turnElectronBeamOnImplLocked(int32_t mode);
             status_t electronBeamOffAnimationImplLocked();
