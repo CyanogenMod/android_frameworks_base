92d7f452ca990b4efaa5c3bec64f06e2fbb58f39
Tom Giordano <tomgiordano83@gmail.com>
2012-01-24 22:51:01 +1100

    framework: fix screenshot and rotation aimation for devices with abnormal hw rotation
    Change-Id: I42694931cb1a754aa6c7652c0a96304ef1f88492

--- services/java/com/android/server/wm/ScreenRotationAnimation.java
+++ services/java/com/android/server/wm/ScreenRotationAnimation.java
@@ -188,17 +188,28 @@ class ScreenRotationAnimation {
     public ScreenRotationAnimation(Context context, SurfaceSession session,
             boolean inTransaction, int originalWidth, int originalHeight, int originalRotation) {
         mContext = context;
-
-        // Screenshot does NOT include rotation!
-        if (originalRotation == Surface.ROTATION_90
+        int mSnapshotRotation;
+        // Allow for abnormal hardware orientation
+        mSnapshotRotation = (4 - android.os.SystemProperties.getInt("ro.sf.hwrotation",0) / 90) % 4;
+        if (mSnapshotRotation == Surface.ROTATION_0 || mSnapshotRotation == Surface.ROTATION_180) {
+            if (originalRotation == Surface.ROTATION_90
                 || originalRotation == Surface.ROTATION_270) {
-            mWidth = originalHeight;
-            mHeight = originalWidth;
+                mWidth = originalHeight;
+                mHeight = originalWidth;
+            } else {
+                mWidth = originalWidth;
+                mHeight = originalHeight;
+            }
         } else {
-            mWidth = originalWidth;
-            mHeight = originalHeight;
+            if (originalRotation == Surface.ROTATION_90
+                || originalRotation == Surface.ROTATION_270) {
+                mWidth = originalWidth;
+                mHeight = originalHeight;
+            } else {
+                mWidth = originalHeight;
+                mHeight = originalWidth;
+            }
         }
-
         mOriginalRotation = originalRotation;
         mOriginalWidth = originalWidth;
         mOriginalHeight = originalHeight;
