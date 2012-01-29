02b789bc571b09eadb8711420f646a903c3d3ad4
Daniel Bateman <jetison.24@gmail.com>
2012-01-29 14:03:29 -0600

    Fixes crash on boot when Disable Lockscreen is enabled in Tablet Tweaks.
    Change-Id: I54b1221bb7bdbad5cbbd43a49defe169c6a32a71
    
    Fixed indentation.
    
    Change-Id: I54b1221bb7bdbad5cbbd43a49defe169c6a32a71
    
    Sigh... fixed indentation again.
    
    Change-Id: I54b1221bb7bdbad5cbbd43a49defe169c6a32a71

--- services/java/com/android/server/WindowManagerService.java
+++ services/java/com/android/server/WindowManagerService.java
@@ -10078,7 +10078,14 @@ public class WindowManagerService extends IWindowManager.Stub
         if (mDisplayFrozen) {
             return;
         }
-
+        
+        /* We might have a null mFxSession here if the user has
+           the lock-screen disabled.*/
+        if (mFxSession == null) {
+            mFxSession = new SurfaceSession();
+        }
+        
+        
         mScreenFrozenLock.acquire();
 
         long now = SystemClock.uptimeMillis();
