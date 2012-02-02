10220eca785766b276d563e18717c1c3c2e385ea
Mike Kasick <mike@kasick.org>
2012-02-01 21:36:49 -0500

    Port wimax support changes from CM9.
    - Register WiMax service in ContextImpl (fixes 4G toggle).
    - Fix misdocumented EXTRA_4G_STATE in WimaxManagerConstants.
    - Share wimaxClassLoader between WimaxHelper and ConnectivityService.
    - Add ACCESS_4G_RADIO_DISABLE, ACCESS_4G_STATE, and CHANGE_4G_STATE
      permissions to SystemUI.
    
    Note that WimaxManagerConstants was previously misdocumented, confabulating
    the lookup key EXTRA_4G_STATE ("4g_state") with EXTRA_WIMAX_STATUS
    ("wimax_status"), which no longer exists.  This is also fixed.
    
    Change-Id: I4f5973e7bd2ff4c860036df0f38589b34ad72b49

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -67,6 +67,7 @@ import android.net.Proxy;
 import android.net.ProxyProperties;
 import android.net.RouteInfo;
 import android.net.wifi.WifiStateTracker;
+import android.net.wimax.WimaxHelper;
 import android.net.wimax.WimaxManagerConstants;
 import android.os.Binder;
 import android.os.FileUtils;
@@ -609,8 +610,6 @@ public class ConnectivityService extends IConnectivityManager.Stub {
         Class wimaxStateTrackerClass = null;
         Class wimaxServiceClass = null;
         Class wimaxManagerClass;
-        String wimaxJarLocation;
-        String wimaxLibLocation;
         String wimaxManagerClassName;
         String wimaxServiceClassName;
         String wimaxStateTrackerClassName;
@@ -622,10 +621,6 @@ public class ConnectivityService extends IConnectivityManager.Stub {
 
         if (isWimaxEnabled) {
             try {
-                wimaxJarLocation = context.getResources().getString(
-                        com.android.internal.R.string.config_wimaxServiceJarLocation);
-                wimaxLibLocation = context.getResources().getString(
-                        com.android.internal.R.string.config_wimaxNativeLibLocation);
                 wimaxManagerClassName = context.getResources().getString(
                         com.android.internal.R.string.config_wimaxManagerClassname);
                 wimaxServiceClassName = context.getResources().getString(
@@ -633,10 +628,7 @@ public class ConnectivityService extends IConnectivityManager.Stub {
                 wimaxStateTrackerClassName = context.getResources().getString(
                         com.android.internal.R.string.config_wimaxStateTrackerClassname);
 
-                if (DBG) log("wimaxJarLocation: " + wimaxJarLocation);
-                wimaxClassLoader =  new DexClassLoader(wimaxJarLocation,
-                        new ContextWrapper(context).getCacheDir().getAbsolutePath(),
-                        wimaxLibLocation, ClassLoader.getSystemClassLoader());
+                wimaxClassLoader = WimaxHelper.getWimaxClassLoader(context);
 
                 try {
                     wimaxManagerClass = wimaxClassLoader.loadClass(wimaxManagerClassName);
