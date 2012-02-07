311a3898bec6de2153354082ac85bad65b78bfe5
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-02-07 23:25:51 +0700

    Improve scrolling cache
    Scrolling cache helps make short scrolls/flings smooth but will
    cause stutter when long flings are made. This patch disables
    scrolling cache when long flings are made.
    
    This patch also fixes a related bug where scrolling cache will
    not be enabled properly when transitioning from flinging to scrolling.
    
    Change-Id: I1fdbaad92263c9f983a32900e74eba1f0367902b

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -3650,7 +3650,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                     // Keep the fling alive a little longer
                     postDelayed(this, FLYWHEEL_TIMEOUT);
                 } else {
-                    endFling();
+                    endFling(false); // Don't disable the scrolling cache right after it was enabled
                     mTouchMode = TOUCH_MODE_SCROLL;
                     reportScrollStateChange(OnScrollListener.SCROLL_STATE_TOUCH_SCROLL);
                 }
@@ -3658,12 +3658,18 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         };
 
         private static final int FLYWHEEL_TIMEOUT = 40; // milliseconds
+        private static final int FLING_NO_CACHE_THRESHOLD = 6500;
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
         }
 
         void start(int initialVelocity) {
+            if (Math.abs(initialVelocity) > FLING_NO_CACHE_THRESHOLD) {
+                // For long flings, scrolling cache causes stutter, so don't use it
+                clearScrollingCache();
+            }
+
             int initialY = initialVelocity < 0 ? Integer.MAX_VALUE : 0;
             mLastFlingY = initialY;
             mScroller.fling(0, initialY, 0, initialVelocity,
@@ -3733,13 +3739,18 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         void endFling() {
+            endFling(true);
+        }
+
+        void endFling(boolean clearCache) {
             mTouchMode = TOUCH_MODE_REST;
 
             removeCallbacks(this);
             removeCallbacks(mCheckFlywheel);
 
             reportScrollStateChange(OnScrollListener.SCROLL_STATE_IDLE);
-            clearScrollingCache();
+            if (clearCache)
+                clearScrollingCache();
             mScroller.abortAnimation();
 
             if (mFlingStrictSpan != null) {
