9828f89ff2e1d557ad36d3691d49be6d85d93ec5
Sang Tae Park <pastime1971@gmail.com>
2012-02-08 08:59:50 -0800

    Improve scrolling cache (ported from ICS)
    Scrolling cache helps make short scrolls/flings smooth but will
    cause stutter when long flings are made. This patch automatically
    disables scrolling cache when long flings are made.
    
    ported from ICS commit, http://review.cyanogenmod.com/#change,12537
    
    Change-Id: I120a5ac50b36dfcdab16ef77aed5ac40930a2dc5

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -481,6 +481,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
     private Runnable mClearScrollingCache;
     private int mMinimumVelocity;
     private int mMaximumVelocity;
+    private int mDecacheThreshold;
     
     final boolean[] mIsScrap = new boolean[1];
     
@@ -677,6 +678,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         mTouchSlop = configuration.getScaledTouchSlop();
         mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
         mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
+        mDecacheThreshold = mMaximumVelocity / 2;
         mOverscrollDistance = configuration.getScaledOverscrollDistance();
         mOverflingDistance = configuration.getScaledOverflingDistance();
 
@@ -2864,6 +2866,11 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         void start(int initialVelocity) {
+            if (Math.abs(initialVelocity) > mDecacheThreshold) {
+                // For long flings, scrolling cache causes stutter, so don't use it
+                clearScrollingCache();
+            }
+
             int initialY = initialVelocity < 0 ? Integer.MAX_VALUE : 0;
             mLastFlingY = initialY;
             mScroller.fling(0, initialY, 0, initialVelocity,
