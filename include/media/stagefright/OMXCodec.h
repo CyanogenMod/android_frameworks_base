19081676b525ed3184afd860ae72aa1eb4b2f324
Wong Yong Jie <yjwong92@gmail.com>
2012-02-19 23:20:05 +0800

    Temporary commit for working qcom OMX decoding
    Testing for H/W video decoding support.
    
    Added QCOM-specific bits to cmds-stagefright.
    
    We now have full 1080p hardware video acceleration!
    
    frameworks/base/nuplayer: Fixes to enable HTTP live streaming
    
    - Enable QCOM specifc color formats in ACodec
    - Do not skip lines without /n, in M3U Parser
    - Stop scanning sources until reset is handled in NuPlayer.
    
    Conflicts:
    
    	media/libstagefright/ACodec.cpp
    
    frameworks/base/NuPlayer: Handle Seek from NuPlayer
    
    - Seek was handled by the source by queuing discontinuity events
      individually for each stream.With this approach renderer will
      continue till discontinuity events are handled for each stream
    - With the new approach seek is handled by the player -
      - get the new seek time from source
      - flush the source TS segments in LiveDataSource, access units
        in TS Parser, decoders and renderer
      - notify current position to driver
      - LiveSession will not queue a discontinuity for seek.
    - Add a mutex lock for connect & disconnect ChromiumHTTPDataSource
    - Handle flush in ACodec - Idle to Executing & Loaded to Idle states
    
    Conflicts:
    
    	media/libmediaplayerservice/nuplayer/NuPlayer.cpp
    
    frameworks/base/nuplayer: Support for dynamic port reconfig in HTTP Live
    
    - BW switch is detected by the decoder by a port reconfig event
      instead of a discontinuity event
    - Handle port reconfig dyanamically in ACodec
      Steps are - Flush - Get all buffers back - Disable - Enable
    - Handle PID change in TS Parser - Set PID based on stream type
    
    libstagefright: return proper color format on target basis.
    
     Video preferred Color format is different for different targets.
    Add a method to choose the preferred format for each target.
    
    Conflicts:
    
    	media/libstagefright/ACodec.cpp
    
    httplive/ACodec: Enable smooth streaming
    
    - Use property to enable - hls.enable.smooth.streaming
    - Set component in smooth streaming mode by doing a setParameter
      with OMX extension for SS
    - Handle Port Reconfig Info event from component
    - Whenever resolution changes do update buffer geometry on
      native window
    
    Conflicts:
    
    	media/libstagefright/ACodec.cpp
    
    Conflicts:
    
    	media/libstagefright/OMXCodec.cpp
    
    Change-Id: Ia1c4f0d0ee9e3877eb4808a1524101bbd049434b

--- include/media/stagefright/OMXCodec.h
+++ include/media/stagefright/OMXCodec.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*--------------------------------------------------------------------------
+Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+--------------------------------------------------------------------------*/
 
 #ifndef OMX_CODEC_H_
 
@@ -56,6 +60,11 @@ struct OMXCodec : public MediaSource,
 
         // Secure decoding mode
         kUseSecureInputBuffers = 256,
+
+#ifdef QCOM_HARDWARE
+        kEnableThumbnailMode = 512,
+        kUseMinBufferCount = 32768,
+#endif
     };
     static sp<MediaSource> Create(
             const sp<IOMX> &omx,
@@ -109,10 +118,18 @@ private:
         EXECUTING_TO_IDLE,
         IDLE_TO_LOADED,
         RECONFIGURING,
+#ifdef QCOM_HARDWARE
+        PAUSING,
+        FLUSHING,
+        PAUSED,
+#endif
         ERROR
     };
 
     enum {
+#ifdef QCOM_HARDWARE
+        kPortIndexBoth   = -1,
+#endif
         kPortIndexInput  = 0,
         kPortIndexOutput = 1
     };
@@ -140,6 +157,11 @@ private:
         kAvoidMemcopyInputRecordingFrames     = 2048,
         kRequiresLargerEncoderOutputBuffer    = 4096,
         kOutputBuffersAreUnreadable           = 8192,
+#ifdef QCOM_HARDWARE
+        kStoreMetaDataInInputVideoBuffers     = 16384,
+        kRequiresGlobalFlush                  = 0x20000000, // 2^29
+        kRequiresWMAProComponent              = 0x40000000, //2^30
+#endif
     };
 
     enum BufferStatus {
@@ -193,6 +215,9 @@ private:
     ReadOptions::SeekMode mSeekMode;
     int64_t mTargetTimeUs;
     bool mOutputPortSettingsChangedPending;
+#ifdef QCOM_HARDWARE
+    bool mThumbnailMode;
+#endif
 
     MediaBuffer *mLeftOverBuffer;
 
@@ -212,9 +237,23 @@ private:
     List<size_t> mFilledBuffers;
     Condition mBufferFilled;
 
+#ifdef QCOM_HARDWARE
+    bool mIsMetaDataStoredInVideoBuffers;
+    bool mOnlySubmitOneBufferAtOneTime;
+    bool mInterlaceFormatDetected;
+    bool mSPSParsed;
+    bool bInvalidState;
+#endif
+
     // Used to record the decoding time for an output picture from
     // a video encoder.
     List<int64_t> mDecodingTimeList;
+#ifdef QCOM_HARDWARE
+    bool m3DVideoDetected;
+
+    //Used to indicate if the AAC container has ADIF format
+    int32_t mIsAacFormatAdif;
+#endif
 
     OMXCodec(const sp<IOMX> &omx, IOMX::node_id node,
              uint32_t quirks, uint32_t flags,
@@ -229,7 +268,13 @@ private:
 
     void setAMRFormat(bool isWAMR, int32_t bitRate);
     status_t setAACFormat(int32_t numChannels, int32_t sampleRate, int32_t bitRate);
+#ifdef QCOM_HARDWARE
+    void setEVRCFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
+    void setG711Format(int32_t numChannels, int32_t sampleRate);
+    void setQCELPFormat( int32_t sampleRate, int32_t numChannels, int32_t bitRate);
+#else
     void setG711Format(int32_t numChannels);
+#endif
 
     status_t setVideoPortFormatType(
             OMX_U32 portIndex,
@@ -243,6 +288,9 @@ private:
     status_t setupErrorCorrectionParameters();
     status_t setupH263EncoderParameters(const sp<MetaData>& meta);
     status_t setupMPEG4EncoderParameters(const sp<MetaData>& meta);
+#ifdef QCOM_HARDWARE
+    status_t setupMPEG2EncoderParameters(const sp<MetaData>& meta);
+#endif
     status_t setupAVCEncoderParameters(const sp<MetaData>& meta);
     status_t findTargetColorFormat(
             const sp<MetaData>& meta, OMX_COLOR_FORMATTYPE *colorFormat);
@@ -336,12 +384,31 @@ private:
 
     int64_t retrieveDecodingTimeUs(bool isCodecSpecific);
 
+#ifdef QCOM_HARDWARE
+    void parseFlags();
+#endif
     status_t parseAVCCodecSpecificData(
             const void *data, size_t size,
+#ifdef QCOM_HARDWARE
+            unsigned *profile, unsigned *level, const sp<MetaData> &meta);
+#else
             unsigned *profile, unsigned *level);
+#endif
+
+#ifdef QCOM_HARDWARE
+    status_t processSEIData();
+#endif
 
     OMXCodec(const OMXCodec &);
     OMXCodec &operator=(const OMXCodec &);
+#ifdef QCOM_HARDWARE
+    status_t setWMAFormat(const sp<MetaData> &inputFormat);
+    void setAC3Format(int32_t numChannels, int32_t sampleRate);
+
+    int64_t latenessUs;
+    uint32_t LC_level; // LOW_COMPLEXITY level
+    int32_t mInterlaceFrame;
+#endif
 };
 
 struct CodecCapabilities {
