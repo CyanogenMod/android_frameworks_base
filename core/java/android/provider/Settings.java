db1e13f2aeb434304758c5213143904f415ad1ca
nebkat <nebkat@teamhacksung.org>
2012-02-22 21:56:46 +0000

    Electron Beam Animation (1/2 Frameworks)
    Change-Id: Ia6e67983008eca505cead7cd4cb196627f5e2f77

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1529,6 +1529,18 @@ public final class Settings {
         public static final String LIGHT_FILTER_INTERVAL = "light_filter_interval";
 
         /**
+         * Whether to enable the electron beam animation when turning screen on
+         *
+         * @hide */
+        public static final String ELECTRON_BEAM_ANIMATION_ON = "electron_beam_animation_on";
+
+        /**
+         * Whether to enable the electron beam animation when turning screen off
+         *
+         * @hide */
+        public static final String ELECTRON_BEAM_ANIMATION_OFF = "electron_beam_animation_off";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          */
         public static final String SHOW_PROCESSES = "show_processes";
