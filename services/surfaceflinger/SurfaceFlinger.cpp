0662345c1d0ef64a59e4f83a0b9719896e4ca9e6
Chetan Kalyan <ckalyan@codeaurora.org>
2012-02-28 19:22:07 -0800

    SurfaceFlinger: Return error on invalid buffer creation
    When a buffer is re-allocated on reconfiguration, return NULL
    if there is an error in re-allocation.
    
    Also, remove warnings during compilation.
    
    Change-Id: Ie01bfcfbb469a106ef99bf1725376e2753206708
    CRs-Fixed: 337660

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -105,13 +105,13 @@ SurfaceFlinger::SurfaceFlinger()
         mDebugInTransaction(0),
         mLastTransactionTime(0),
         mBootFinished(false),
-        mConsoleSignals(0),
+#ifdef QCOM_HDMI_OUT
+        mHDMIOutput(EXT_DISPLAY_OFF),
+#endif
 #ifdef QCOM_HARDWARE
         mCanSkipComposition(false),
 #endif
-#ifdef QCOM_HDMI_OUT
-        mHDMIOutput(false),
-#endif
+        mConsoleSignals(0),
         mSecureFrameBuffer(0)
 {
     init();
@@ -2745,7 +2745,11 @@ sp<GraphicBuffer> GraphicBufferAlloc::createGraphicBuffer(uint32_t w, uint32_t h
         return 0;
     }
 #ifdef QCOM_HARDWARE
-    checkBuffer((native_handle_t *)graphicBuffer->handle, mSize, usage);
+    err = checkBuffer((native_handle_t *)graphicBuffer->handle, mSize, usage);
+    if (err) {
+        LOGE("%s: checkBuffer failed",__FUNCTION__);
+        return 0;
+    }
     Mutex::Autolock _l(mLock);
     if (-1 != mFreedIndex) {
         mBuffers.insertAt(graphicBuffer, mFreedIndex);
@@ -2760,7 +2764,7 @@ sp<GraphicBuffer> GraphicBufferAlloc::createGraphicBuffer(uint32_t w, uint32_t h
 #ifdef QCOM_HARDWARE
 void GraphicBufferAlloc::freeAllGraphicBuffersExcept(int bufIdx) {
     Mutex::Autolock _l(mLock);
-    if (0 <= bufIdx && bufIdx < mBuffers.size()) {
+    if (bufIdx >= 0 && bufIdx < (int)mBuffers.size()) {
         sp<GraphicBuffer> b(mBuffers[bufIdx]);
         mBuffers.clear();
         mBuffers.add(b);
@@ -2772,7 +2776,7 @@ void GraphicBufferAlloc::freeAllGraphicBuffersExcept(int bufIdx) {
 
 void GraphicBufferAlloc::freeGraphicBufferAtIndex(int bufIdx) {
      Mutex::Autolock _l(mLock);
-     if (0 <= bufIdx && bufIdx < mBuffers.size()) {
+     if (bufIdx >= 0 && bufIdx < (int)mBuffers.size()) {
         mBuffers.removeItemsAt(bufIdx);
         mFreedIndex = bufIdx;
      } else {
