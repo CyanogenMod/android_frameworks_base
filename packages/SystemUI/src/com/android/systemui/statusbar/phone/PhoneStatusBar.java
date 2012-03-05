185040a453f41a91846f4753e4bd59a6f78aae64
Danesh Mondegarian <daneshm90@gmail.com>
2012-03-05 15:53:35 -0500

    NavigationBar : Customization
    Allows user to customize navigation bar.
    
    Features :
    - Choose from 1-6 # of buttons
    - Re-arrange / assign buttons
    
    Patchset 1  : Initial commit
    Patchset 2  : Resize side drawables to avoid requestRelayout() [HACK]
    Patchset 3  : Added extras to intent to explicity change state
    Patchset 4  : New drawables (David Tonder)
                  Fix button animation
    Patchset 5  : Switch button layout to stock dimensions
                  Adjust padding according to new layout
    Patchset 6  : Change "Blank" to "Empty" where applicable
    Patchset 7  : Lowercase key names (Will require a wipe of the database entry)
                  Fix issue where during edit mode if screen turns off, layout gets distorted on restore
                  Add "save" extra to intent to clarify whether to save the layout or not
    Patchset 8  : Rebase for 4.0.4
    Patchset 9  : Fix padding / lowLights
    Patchset 10 : Minor fixes
    Patchset 11 : Fix issue with losing edit mode after theme changes
    	    : Fix whitespace issue
                : Fix low light gravity in landscape
                : Add regular menu button option (icons by DvTonder)
    	    : Localize resources
                : Fix padding issue
    Patchset 12 : Hide searchbutton like others on lockscreen
    Patchset 13 : Fix lowLight padding
    Patchset 14 : Code cleanup
    Patchset 15 : Minor edit
    
    Change-Id: If24a4413f99b8b4661ffd7ad9565c005fa22d7ba

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -68,7 +68,6 @@ import android.view.WindowManagerImpl;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RemoteViews;
 import android.widget.ScrollView;
@@ -532,9 +531,7 @@ public class PhoneStatusBar extends StatusBar {
 
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
-
-        mNavigationBarView.getRecentsButton().setOnClickListener(mRecentsClickListener);
-        mNavigationBarView.getRecentsButton().setOnTouchListener(mRecentsPanel);
+        mNavigationBarView.setListener(mRecentsClickListener,mRecentsPanel);
     }
 
     // For small-screen devices (read: phones) that lack hardware navigation buttons
@@ -2399,6 +2396,7 @@ public class PhoneStatusBar extends StatusBar {
         mNotificationData.clear();
 
         if (mNavigationBarView != null) {
+            mNavigationBarView.unregisterReceivers();
             WindowManagerImpl.getDefault().removeView(mNavigationBarView);
         }
         View newStatusBarView = makeStatusBarView();
