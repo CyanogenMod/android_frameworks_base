215dba73f6ce493bb385523b0d0926abd3099a12
Diogo Ferreira <defer@cyanogenmod.com>
2012-03-09 21:35:55 +0000

    SemcRil: Use the new simplified QualcommSimReadyRIL
    This patch changes SemcRIL to use the common functions in
    QualcommSimReadyRIL. It should not be used without them.
    
    Change-Id: Ibff11760b9eb86dc7741622ba934dc9b78e3e94c

--- telephony/java/com/android/internal/telephony/SemcRIL.java
+++ telephony/java/com/android/internal/telephony/SemcRIL.java
@@ -36,48 +36,11 @@ import java.util.*;
  *
  * {@hide}
  */
-public class SemcRIL extends RIL {
+public class SemcRIL extends QualcommNoSimReadyRIL {
     String mAid = "";
-    IccHandler mIccHandler;
-    HandlerThread mIccThread;
 
     public SemcRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
-        mIccHandler = null;
-    }
-
-    @Override
-    protected RadioState getRadioStateFromInt(int stateInt) {
-        RadioState state;
-
-        /* RIL_RadioState ril.h */
-        switch(stateInt) {
-            case 0: state = RadioState.RADIO_OFF; break;
-            case 1: state = RadioState.RADIO_UNAVAILABLE; break;
-            case 2: {
-                if (mIccHandler == null)
-                {
-                    mIccThread = new HandlerThread("IccHandler");
-                    mIccThread.start();
-                    mIccHandler = new IccHandler(this, mIccThread.getLooper());
-                }
-                mIccHandler.run();
-                state = mPhoneType == RILConstants.CDMA_PHONE ? RadioState.RUIM_NOT_READY : RadioState.SIM_NOT_READY;
-                break;
-            }
-            case 3: state = RadioState.SIM_LOCKED_OR_ABSENT; break;
-            case 4: state = RadioState.SIM_READY; break;
-            case 5: state = RadioState.RUIM_NOT_READY; break;
-            case 6: state = RadioState.RUIM_READY; break;
-            case 7: state = RadioState.RUIM_LOCKED_OR_ABSENT; break;
-            case 8: state = RadioState.NV_NOT_READY; break;
-            case 9: state = RadioState.NV_READY; break;
-
-            default:
-                throw new RuntimeException(
-                            "Unrecognized RIL_RadioState: " + stateInt);
-        }
-        return state;
     }
 
     @Override
@@ -339,116 +302,5 @@ public class SemcRIL extends RIL {
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
 
         send(rr);
-    }
-
-    class IccHandler extends Handler implements Runnable {
-        private static final int EVENT_RADIO_ON = 1;
-        private static final int EVENT_ICC_STATUS_CHANGED = 2;
-        private static final int EVENT_GET_ICC_STATUS_DONE = 3;
-        private static final int EVENT_RADIO_OFF_OR_UNAVAILABLE = 4;
-
-        private RIL mRil;
-        private boolean mRadioOn = false;
-
-        public IccHandler (RIL ril, Looper looper) {
-            super (looper);
-            mRil = ril;
-        }
-
-        public void handleMessage (Message paramMessage) {
-            switch (paramMessage.what) {
-                case EVENT_RADIO_ON:
-                    mRadioOn = true;
-                    Log.d(LOG_TAG, "Radio on -> Forcing sim status update");
-                    sendMessage(obtainMessage(EVENT_ICC_STATUS_CHANGED));
-                    break;
-                case EVENT_GET_ICC_STATUS_DONE:
-                    AsyncResult asyncResult = (AsyncResult) paramMessage.obj;
-                    if (asyncResult.exception != null) {
-                        Log.e (LOG_TAG, "IccCardStatusDone shouldn't return exceptions!", asyncResult.exception);
-                        break;
-                    }
-                    IccCardStatus status = (IccCardStatus) asyncResult.result;
-                    if (status.getNumApplications() == 0) {
-                        if (!mRil.getRadioState().isOn()) {
-                            break;
-                        }
-
-                        if (mPhoneType == RILConstants.CDMA_PHONE) {
-                            mRil.setRadioState(CommandsInterface.RadioState.RUIM_LOCKED_OR_ABSENT);
-                        } else {
-                            mRil.setRadioState(CommandsInterface.RadioState.SIM_LOCKED_OR_ABSENT);
-                        }
-                    } else {
-                        int appIndex = -1;
-                        if (mPhoneType == RILConstants.CDMA_PHONE) {
-                            appIndex = status.getCdmaSubscriptionAppIndex();
-                            Log.d(LOG_TAG, "This is a CDMA PHONE " + appIndex);
-                        } else {
-                            appIndex = status.getGsmUmtsSubscriptionAppIndex();
-                            Log.d(LOG_TAG, "This is a GSM PHONE " + appIndex);
-                        }
-
-                        IccCardApplication application = status.getApplication(appIndex);
-                        IccCardApplication.AppState app_state = application.app_state;
-                        IccCardApplication.AppType app_type = application.app_type;
-
-                        switch (app_state) {
-                            case APPSTATE_PIN:
-                            case APPSTATE_PUK:
-                                switch (app_type) {
-                                    case APPTYPE_SIM:
-                                    case APPTYPE_USIM:
-                                        mRil.setRadioState(CommandsInterface.RadioState.SIM_LOCKED_OR_ABSENT);
-                                        break;
-                                    case APPTYPE_RUIM:
-                                        mRil.setRadioState(CommandsInterface.RadioState.RUIM_LOCKED_OR_ABSENT);
-                                        break;
-                                    default:
-                                        Log.e(LOG_TAG, "Currently we don't handle SIMs of type: " + app_type);
-                                        return;
-                                }
-                                break;
-                            case APPSTATE_READY:
-                                switch (app_type) {
-                                    case APPTYPE_SIM:
-                                    case APPTYPE_USIM:
-                                        mRil.setRadioState(CommandsInterface.RadioState.SIM_READY);
-                                        break;
-                                    case APPTYPE_RUIM:
-                                        mRil.setRadioState(CommandsInterface.RadioState.RUIM_READY);
-                                        break;
-                                    default:
-                                        Log.e(LOG_TAG, "Currently we don't handle SIMs of type: " + app_type);
-                                        return;
-                                }
-                                break;
-                            default:
-                                return;
-                        }
-                    }
-                    break;
-                case EVENT_ICC_STATUS_CHANGED:
-                    if (mRadioOn) {
-                        Log.d(LOG_TAG, "Received EVENT_ICC_STATUS_CHANGED, calling getIccCardStatus");
-                         mRil.getIccCardStatus(obtainMessage(EVENT_GET_ICC_STATUS_DONE, paramMessage.obj));
-                    } else {
-                         Log.d(LOG_TAG, "Received EVENT_ICC_STATUS_CHANGED while radio is not ON. Ignoring");
-                    }
-                    break;
-                case EVENT_RADIO_OFF_OR_UNAVAILABLE:
-                    mRadioOn = false;
-                    // disposeCards(); // to be verified;
-                default:
-                    Log.e(LOG_TAG, " Unknown Event " + paramMessage.what);
-                    break;
-            }
-        }
-
-        public void run () {
-            mRil.registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, null);
-            Message msg = obtainMessage(EVENT_RADIO_ON);
-            mRil.getIccCardStatus(msg);
-        }
     }
 }
