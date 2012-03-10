78d976c61f0549ada94bddc3a32295df8cba02df
Ameer Ghouse <aghouse1@gmail.com>
2012-03-10 12:51:26 -0600

    fix mms for sprint devices. sprint sends content disposition without length value, which can cause a runtime exception.
    Change-Id: I29f1edc1391fe0eed4898063a9c5b361d71668b4

--- core/java/com/google/android/mms/pdu/PduParser.java
+++ core/java/com/google/android/mms/pdu/PduParser.java
@@ -1567,13 +1567,24 @@ public class PduParser {
                                 .android.internal.R.bool.config_mms_content_disposition_support);
 
                         if (contentDisposition) {
-                            int len = parseValueLength(pduDataStream);
+                            int len = -1;
+			    boolean validDispositionLength = true;
+			    pduDataStream.mark(1);
+			    
+			    try {
+				len = parseValueLength(pduDataStream);
+			    } catch (RuntimeException e){
+				len = 31;
+				validDispositionLength = false;
+				pduDataStream.reset();
+			    }
+				
                             pduDataStream.mark(1);
                             int thisStartPos = pduDataStream.available();
                             int thisEndPos = 0;
                             int value = pduDataStream.read();
 
-                            if (value == PduPart.P_DISPOSITION_FROM_DATA ) {
+                            if (validDispositionLength) {
                                 part.setContentDisposition(PduPart.DISPOSITION_FROM_DATA);
                             } else if (value == PduPart.P_DISPOSITION_ATTACHMENT) {
                                 part.setContentDisposition(PduPart.DISPOSITION_ATTACHMENT);
