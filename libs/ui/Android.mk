5f3ca4fc4b399db12140bb25c5c94132c8c47d46
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-13 12:22:47 -0400

    msm7x30: Camera HAL for HTC Sense devices (2/2)
    This is the overlay portion of the camera HAL for msm7x30 devices. This
    patch provides the overlay support for the specialized camera library
    wrapper HAL. It is used with http://review.cyanogenmod.com/#change,12793

--- libs/ui/Android.mk
+++ libs/ui/Android.mk
@@ -66,8 +66,12 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES := \
     external/skia/include/core
 
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    LOCAL_SRC_FILES+= Overlay.cpp
+endif
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-LOCAL_CFLAGS += -DQCOM_HARDWARE
+    LOCAL_CFLAGS += -DQCOM_HARDWARE
 endif
 
 LOCAL_MODULE:= libui
