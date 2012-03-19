b598b047876da739f3b98fa2bfa41ad02578469a
Prabhanjan Kandula <pkandula@codeaurora.org>
2012-03-19 19:38:12 +0530

    frameworks/base: surfaceflinger: Enable dither if needed
    SF is checking if dithering is required but not enabling it.
    Enable for layer drawing and  also for screenshot for 16bpp
    framebuffer only.
    
    CRs-fixed: 333721
    (cherry picked from commit 85c02ed97b4a1d0cc343a452959570b379928d59)
    
    Change-Id: I599fe5b9683db208c8b32a1d3d395d5203f78fd3

--- services/surfaceflinger/LayerScreenshot.cpp
+++ services/surfaceflinger/LayerScreenshot.cpp
@@ -124,6 +124,9 @@ void LayerScreenshot::onDraw(const Region& clip) const
         }
 
         glColor4f(0, 0, 0, alpha);
+        if(hw.getFormat() == PIXEL_FORMAT_RGB_565) {
+            glEnable(GL_DITHER);
+        }
 
         glDisable(GL_TEXTURE_EXTERNAL_OES);
         glEnable(GL_TEXTURE_2D);
@@ -148,6 +151,9 @@ void LayerScreenshot::onDraw(const Region& clip) const
         glDisable(GL_BLEND);
         glDisable(GL_TEXTURE_2D);
         glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+        if(hw.getFormat() == PIXEL_FORMAT_RGB_565) {
+            glDisable(GL_DITHER);
+        }
     }
 }
 
