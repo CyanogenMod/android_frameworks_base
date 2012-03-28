a19e5afae290da490540a8966e89ca8d5da0c9d6
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-03-28 23:17:01 +0200

    stagefright: more exynos4 enhancements
    Change-Id: I1f6421ed295804ffc0182b874a26aef755472b51

--- media/libstagefright/CameraSource.cpp
+++ media/libstagefright/CameraSource.cpp
@@ -100,7 +100,12 @@ static int32_t getColorFormat(const char* colorFormat) {
     }
 
     if (!strcmp(colorFormat, CameraParameters::PIXEL_FORMAT_YUV420SP)) {
+#ifdef EXYNOS4210_ENHANCEMENTS
+        static const int OMX_SEC_COLOR_FormatNV12LPhysicalAddress = 0x7F000002;
+        return OMX_SEC_COLOR_FormatNV12LPhysicalAddress;
+#else
         return OMX_COLOR_FormatYUV420SemiPlanar;
+#endif
     }
 
     if (!strcmp(colorFormat, CameraParameters::PIXEL_FORMAT_YUV422I)) {
