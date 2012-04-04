b6f6fc53e1c5e26e07ee8b84d35abc3e3b485a59
Ed Carrigan <cretin45@gmail.com>
2012-04-04 09:09:19 -0700

    Allow HTC cameras to incorrectly report wrong framerate.
    HTC Cameras report wrong framerate. 31 instead of 30. Don't return an error in this case.
    
    * Patchset 2: add HTC camera flag
    
    Change-Id: I05437dd3340d43db4a51e49990dbd6a4a107a6c5

--- media/libstagefright/CameraSource.cpp
+++ media/libstagefright/CameraSource.cpp
@@ -440,7 +440,14 @@ status_t CameraSource::checkFrameRate(
 
     // Check the actual video frame rate against the target/requested
     // video frame rate.
-    if (frameRate != -1 && (frameRateActual - frameRate) != 0) {
+    int32_t frameRateDiff = frameRateActual - frameRate;
+#ifdef POS_HTC_CAMERA
+    //HTC Cameras incorrectly report 31 fps instead of 30.
+    LOGV("CameraSource frameRateActual %d", frameRateActual);
+    frameRateDiff = frameRateDiff > 1 ? frameRateDiff : 0;
+#endif
+    LOGE("CameraSource frameRate %d", frameRate);
+    if (frameRate != -1 && (frameRateDiff) != 0) {
         LOGE("Failed to set preview frame rate to %d fps. The actual "
                 "frame rate is %d", frameRate, frameRateActual);
         return UNKNOWN_ERROR;
