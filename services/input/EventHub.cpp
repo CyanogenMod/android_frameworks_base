b7bd7a4f09d97b7931a8a1aeeea9c2ab25f5716f
Vidyakumar Athota <vathota@codeaurora.org>
2012-04-03 19:32:05 -0700

    EventHub: Modify the keyboard layout file name for headset button
    The keyboard layout file name was formed based on the name of
    tabla sound card on current machine. If it doesn't find such
    file, it is going to use Generic keyboard layout where the
    key mappings are different, even though the scan code is same.
    The sound card name differs across different machine types.
    So, generalising the keyboard layout file name will work across
    all machine types.
    
    CRs-fixed: 344297
    Change-Id: I416b6179f51ac98fe7dfae5268ffb58c243540a4

--- services/input/EventHub.cpp
+++ services/input/EventHub.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2005 The Android Open Source Project
+ * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,6 +31,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <memory.h>
@@ -976,6 +978,7 @@ static const int32_t GAMEPAD_KEYCODES[] = {
 
 status_t EventHub::openDeviceLocked(const char *devicePath) {
     char buffer[80];
+    const char *temp1 = "Button Jack";
 
     ALOGV("Opening device: %s", devicePath);
 
@@ -991,6 +994,10 @@ status_t EventHub::openDeviceLocked(const char *devicePath) {
     if(ioctl(fd, EVIOCGNAME(sizeof(buffer) - 1), &buffer) < 1) {
         //fprintf(stderr, "could not get device name for %s, %s\n", devicePath, strerror(errno));
     } else {
+        if((strstr(buffer, temp1)) != NULL) {
+            memset(buffer, 0, sizeof(buffer));
+            strlcpy(buffer, temp1, sizeof(buffer));
+        }
         buffer[sizeof(buffer) - 1] = '\0';
         identifier.name.setTo(buffer);
     }
