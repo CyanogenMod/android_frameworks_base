7a801a91e6cc29e10b7a39e461a45a6b8b60a445
Ricardo Cerqueira <github@cerqueira.org>
2012-04-05 16:25:34 +0100

    LGEQualcommUiccRIL: Fix LTE signal strength
    The RSRP sign is wrong, results in always-excellent network indication. Fix
    it.
    
    Change-Id: I3ad40eabca88babf41ad89903648e76f6d8c0830

--- telephony/java/com/android/internal/telephony/LGEQualcommUiccRIL.java
+++ telephony/java/com/android/internal/telephony/LGEQualcommUiccRIL.java
@@ -432,6 +432,9 @@ public class LGEQualcommUiccRIL extends LGEQualcommRIL implements CommandsInterf
                 response[i] = -1;
                 noLte = true;
             }
+            if (i == 8 && !noLte) {
+                response[i] *= -1;
+            }
         }
 
         return response;
