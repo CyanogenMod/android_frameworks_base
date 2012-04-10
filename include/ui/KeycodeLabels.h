72b694b254844855409b284055450d9662c6c964
Alexander Hofbauer <alex@derhofbauer.at>
2012-04-10 18:37:31 +0200

    Implement additional keycodes
    Adds keys for
    
    *) WLAN, Bluetooth, touchpad toggle,
    *) brightness down, up, auto and
    *) screenshot
    
    as found on docks of current asus transformer devices.
    
    Change-Id: I94a5a4e80e5de85db49a1044c7dcb9ef28db075c

--- include/ui/KeycodeLabels.h
+++ include/ui/KeycodeLabels.h
@@ -235,6 +235,13 @@ static const KeycodeLabel KEYCODES[] = {
     { "CALENDAR", 208 },
     { "MUSIC", 209 },
     { "CALCULATOR", 210 },
+    { "TOGGLE_WIFI", 211 },
+    { "TOGGLE_BT", 212 },
+    { "TOGGLE_TOUCHPAD", 213 },
+    { "BRIGHTNESS_DOWN", 214 },
+    { "BRIGHTNESS_UP", 215 },
+    { "BRIGHTNESS_AUTO", 216 },
+    { "SCREENSHOT", 217 },
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
