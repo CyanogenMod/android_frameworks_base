94b351b4f98f951bfa5723fe28c98b29f1cfcfb0
Prabhanjan Kandula <pkandula@codeaurora.org>
2012-05-01 16:10:02 +0530

    surfaceflinger: Fix top-bottom UI composition with S3D.
    -Fix the scissor computation for top and bottom frame in S3D.
    -Fix the artifacts seen in top-bottom case by clearing the
     color buffer with scissor applied.
    
    CRs-fixed: 346986
    (cherry picked from commit 241b5b6b41fa4e1b2eca3565fd27b454a6aff262)
    
    Change-Id: I8fb98007f67351009762c4494e260142b998ddb5

--- services/surfaceflinger/LayerBase.cpp
+++ services/surfaceflinger/LayerBase.cpp
@@ -546,6 +546,7 @@ void LayerBase::drawS3DUIWithOpenGL(const Region& clip) const
     int composeS3DFormat = mQCLayer->needsS3DCompose();
     int xoffset = fbWidth/2 ;
     int yoffset = fbHeight/2;
+    glClearColor(0,0,0,0);
     // Calculate the new vertices for S3D conversion
     switch (composeS3DFormat) {
         case QCBaseLayer::eS3D_SIDE_BY_SIDE:
@@ -561,6 +562,7 @@ void LayerBase::drawS3DUIWithOpenGL(const Region& clip) const
                 const Rect& r = *it++;
                 const GLint sy = fbHeight - (r.top + r.height());
                 glScissor(r.left/2, sy, r.width()/2, r.height());
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLE_FAN, 0, 4);//glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, pindices);
             }
 
@@ -571,6 +573,7 @@ void LayerBase::drawS3DUIWithOpenGL(const Region& clip) const
                 const Rect& r = *it++;
                 const GLint sy = fbHeight - (r.top + r.height());
                 glScissor(xoffset+r.left/2, sy, r.width()/2, r.height());
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLE_FAN, 0, 4);//glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, pindices);
             }
             break;
@@ -586,7 +589,8 @@ void LayerBase::drawS3DUIWithOpenGL(const Region& clip) const
             while (it != end) {
                 const Rect& r = *it++;
                 const GLint sy = fbHeight - (r.top + r.height());
-                glScissor(r.left, yoffset + sy/2, r.width(), r.height()/2);
+                glScissor(r.left, sy/2, r.width(), r.height()/2);
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLE_FAN, 0, 4);//glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, pindices);
             }
 
@@ -596,7 +600,8 @@ void LayerBase::drawS3DUIWithOpenGL(const Region& clip) const
             while (it != end) {
                 const Rect& r = *it++;
                 const GLint sy = fbHeight - (r.top + r.height());
-                glScissor(r.left, sy/2, r.width(), r.height()/2);
+                glScissor(r.left, yoffset+sy/2, r.width(), r.height()/2);
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLE_FAN, 0, 4);//glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, pindices);
             }
             break;
