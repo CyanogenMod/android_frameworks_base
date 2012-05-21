50f5f13ab51addee337d8bdf4d6fbf30eeab1cb8
Steve Kondik <shade@chemlab.org>
2012-05-21 15:48:21 -0700

    stagefright: Fix dyslexic ifdef
     * This is backwards. Ooops.
    
    Change-Id: I2adca96ff868942eca58295bd79d2993ab4999c7

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -549,12 +549,12 @@ status_t ACodec::allocateOutputBuffersFromNativeWindow() {
     err = native_window_set_buffers_geometry(
             mNativeWindow.get(),
 #ifdef QCOM_HARDWARE
-            def.format.video.nFrameWidth,
-            def.format.video.nFrameHeight,
-            format);
-#else
             def.format.video.nStride,
             def.format.video.nSliceHeight,
+            format);
+#else
+            def.format.video.nFrameWidth,
+            def.format.video.nFrameHeight,
             def.format.video.eColorFormat);
 #endif
 #endif
