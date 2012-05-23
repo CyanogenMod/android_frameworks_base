d2f99386aa4684bb557094f9d690b4f0b5c07c45
David Ferguson <ferguson.david@gmail.com>
2012-05-23 08:18:15 -0400

    telephony: when building apn list, use preferred apn if it can handle requested type
    GsmDataConnectionTracker::setupData() does not consult TelephonyProvider when it
    picks an APN for mms. It just uses apnContext.getNextWaitingApn() which picks the
    first one in the list. On AT&T it picks wap.cingular, which is wrong for LTE.
    
    Since the data connection is made properly, setupData() stops scanning and never
    tries the correct APN (pta). The result is sending of the mms just hangs.
    
    This change causes GsmDataConnectionTracker::buildWaitingApns() to first try the
    currently active APN if it can handle the type of APN being requested, then it
    tried the preferred APN and then the rest of the APN's, in that order.
    
    This fixes the MMS problem since the correct APN (pta) is chosen.
    
    Change-Id: I076b88d42f59241cf468d678ad0e628d0f2d9e8d
    
    Conflicts:
    
    	telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java

--- telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
@@ -1388,6 +1388,8 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
                 + ", reason:" + apnContext.getReason());
         }
         apnContext.setState(State.CONNECTED);
+        mActiveApn = apnContext.getApnSetting();
+
         // setState(State.CONNECTED);
         mPhone.notifyDataConnection(apnContext.getReason(), apnContext.getApnType());
         startNetStatPoll();
@@ -2425,13 +2427,37 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
                 mPreferredApn = null;
             }
         }
+
+        // If the currently active data connect can handle the requested type, try it first
+        if ((mActiveApn != null) && mActiveApn.canHandleType(requestedApnType)) {
+            if (DBG) log("buildWaitingApns: X added already active apnList=" + apnList);
+            apnList.add(mActiveApn);
+        }
+
         if (mAllApns != null) {
+            // Use the preferred APN if it can handle the type being requested
+            if (canSetPreferApn && mPreferredApn != null) {
+                if (DBG) {
+                    log("buildWaitingApns: Preferred APN:" + operator + ":"
+                        + mPreferredApn.numeric + ":" + mPreferredApn);
+                }
+                if ((mPreferredApn.numeric.equals(operator) && mPreferredApn.canHandleType(requestedApnType)) &&
+                    (mPreferredApn.bearer == 0 || mPreferredApn.bearer == networkType) && 
+                    !apnList.contains(mPreferredApn))
+                {
+                    apnList.add(mPreferredApn);
+                    if (DBG) log("buildWaitingApns: X added preferred apnList=" + apnList);
+                }
+            }
+
+            // Add all the rest of the apns that can handle the requested type
             for (ApnSetting apn : mAllApns) {
-                if (apn.canHandleType(requestedApnType)) {
-                    if (apn.bearer == 0 || apn.bearer == networkType) {
-                        if (DBG) log("apn info : " +apn.toString());
-                        apnList.add(apn);
-                    }
+                if ((apn.canHandleType(requestedApnType)) &&
+                    (apn.bearer == 0 || apn.bearer == networkType) &&
+                    !apnList.contains(apn))
+                {
+                    if (DBG) log("apn info : " +apn.toString());
+                    apnList.add(apn);
                 }
             }
         } else {
