1acfe4ddec2c17360984391b4f4720e388efff2d
Danesh M <daneshm90@gmail.com>
2012-05-27 12:41:54 -0400

    LockScreen Weather : Align City/Condition/Time text
    Align the text inside the view to the right in addition to aligning
    itself to its parent.
    
    Patchset 2 : Single line + marquee
    Patchset 3 : Single line/marquee weather condition
               : Add to setSelected array to allow marquee to work
    
    Change-Id: I52fd44cb5bfaf78c4946bf155c6ff771b5151e80

--- policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
+++ policy/src/com/android/internal/policy/impl/KeyguardStatusViewManager.java
@@ -238,7 +238,7 @@ class KeyguardStatusViewManager implements OnClickListener {
 
         // Required to get Marquee to work.
         final View scrollableViews[] = { mCarrierView, mDateView, mStatus1View, mOwnerInfoView,
-                mAlarmStatusView };
+                mAlarmStatusView, mWeatherCity, mWeatherCondition };
         for (View v : scrollableViews) {
             if (v != null) {
                 v.setSelected(true);
