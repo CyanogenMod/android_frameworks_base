372454db46b3465a4f69d7f6e40f54704e2bb671
Steve Kondik <shade@chemlab.org>
2012-05-28 07:19:59 -0700

    systemui: Add option to enable notification counts
     * The code is already there, this adds a runtime toggle for it.
    
    Change-Id: I093fae9b0e2e263c11a41f07c9669c18afda978d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2449,6 +2449,12 @@ public final class Settings {
         public static final String WEATHER_INVERT_LOWHIGH = "weather_invert_lowhigh";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
