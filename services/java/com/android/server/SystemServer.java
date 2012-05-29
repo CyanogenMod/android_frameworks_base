1bb8e42475cd2e03be4c27a0954c8eca528d8e83
Brian Floersch <gh123man@gmail.com>
2012-05-29 03:46:59 -0400

    HW Rotation lock for tablets (1/2)
    Enabled use of hardware switch for rotation lock
    on android tablets containing this feature
    such as the acer iconia a500
    
    Change-Id: Iaaecf3c6e8ae74ae9f5aefffa2c9b7d547ef0119

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -144,6 +144,7 @@ class ServerThread extends Thread {
         BluetoothService bluetooth = null;
         BluetoothA2dpService bluetoothA2dp = null;
         DockObserver dock = null;
+        RotationSwitchObserver rotateSwitch = null;
         UsbService usb = null;
         UiModeManagerService uiMode = null;
         RecognitionManagerService recognition = null;
@@ -281,6 +282,9 @@ class ServerThread extends Thread {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service", e);
         }
+
+        boolean hasRotationLock = context.getResources().getBoolean(com.android
+                .internal.R.bool.config_hasRotationLockSwitch);
 
         DevicePolicyManagerService devicePolicy = null;
         StatusBarManagerService statusBar = null;
@@ -531,6 +535,16 @@ class ServerThread extends Thread {
             }
 
             try {
+                if (hasRotationLock) {
+                        Slog.i(TAG, "Rotation Switch Observer");
+                        // Listen for switch changes
+                        rotateSwitch = new RotationSwitchObserver(context);
+                }
+            } catch (Throwable e) {
+                reportWtf("starting RotationSwitchObserver", e);
+            }
+
+            try {
                 Slog.i(TAG, "Wired Accessory Observer");
                 // Listen for wired headset changes
                 new WiredAccessoryObserver(context);
@@ -696,6 +710,7 @@ class ServerThread extends Thread {
         final NetworkPolicyManagerService networkPolicyF = networkPolicy;
         final ConnectivityService connectivityF = connectivity;
         final DockObserver dockF = dock;
+        final RotationSwitchObserver rotateSwitchF = rotateSwitch;
         final UsbService usbF = usb;
         final ThrottleService throttleF = throttle;
         final UiModeManagerService uiModeF = uiMode;
@@ -748,6 +763,11 @@ class ServerThread extends Thread {
                     if (dockF != null) dockF.systemReady();
                 } catch (Throwable e) {
                     reportWtf("making Dock Service ready", e);
+                }
+                try {
+                    if (rotateSwitchF != null) rotateSwitchF.systemReady();
+                } catch (Throwable e) {
+                    reportWtf("making Rotation Switch Service ready", e);
                 }
                 try {
                     if (usbF != null) usbF.systemReady();
