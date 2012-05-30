c1ccade8fe528add8f9ea36b70a689c17f70d1ea
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-05-30 15:45:25 +0100

    QualcommSharedRIL: Fix restoring network mode preference on reboot.
    Change-Id Ib9e4bba6da705c127ce1f3aa3c242b9262541cee fixed this in the
    master RIL class, and broke the qcom-specific implementation. Fix
    it by rescoping the variable.
    
    Change-Id: I70784ee0f8e7b0b74cab8dafdc83610191ba245c

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -1862,7 +1862,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
         if (RILJ_LOGD) riljLog("setCurrentPreferredNetworkType: " + mSetPreferredNetworkType);
         setPreferredNetworkType(mSetPreferredNetworkType, null);
     }
-    private int mSetPreferredNetworkType;
+    protected int mSetPreferredNetworkType;
 
     /**
      * {@inheritDoc}
