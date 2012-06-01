55b65f36fd4ae11c3db3c94b54bd0fd3663a1f77
Danesh M <daneshm90@gmail.com>
2012-05-31 22:28:38 -0400

    Framework : Lockscreen background customization (1/2)
    Allow user to customize lockscreen background (port from cm7)
    
    Part 2 : http://review.cyanogenmod.com/#/c/16914/
    
    Change-Id: I3f904486b8c7bcfcb63f54937810f6ee9c1721ef

--- policy/src/com/android/internal/policy/impl/PatternUnlockScreen.java
+++ policy/src/com/android/internal/policy/impl/PatternUnlockScreen.java
@@ -24,6 +24,7 @@ import android.security.KeyStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.util.Log;
 import com.android.internal.R;
@@ -169,7 +170,7 @@ class PatternUnlockScreen extends LinearLayoutWithDefaultTouchRecepient
             Log.d(TAG, "landscape mode");
             inflater.inflate(R.layout.keyguard_screen_unlock_landscape, this, true);
         }
-
+        LockScreen.setBackground(context, (ViewGroup) findViewById(R.id.root));
         mKeyguardStatusViewManager = new KeyguardStatusViewManager(this, mUpdateMonitor,
                 mLockPatternUtils, mCallback, true);
 
