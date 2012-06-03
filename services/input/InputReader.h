db70e7a68c54813803729cb1f532146f2cd0cdad
Steve Kondik <shade@chemlab.org>
2012-06-03 07:27:20 -0700

    input: Add option to toggle pointer icon when using stylus
    * The visible pointer icon when hovering or drawing with the stylus is
      quite annoying when trying to actually draw with it. Turn it off by
      default and add an option to turn it on.
    
    Change-Id: Ib7f23e8b69589e3f875d150caf05065b64676e44

--- services/input/InputReader.h
+++ services/input/InputReader.h
@@ -75,6 +75,9 @@ struct InputReaderConfiguration {
         // The device name alias supplied by the may have changed for some devices.
         CHANGE_DEVICE_ALIAS = 1 << 5,
 
+        // Stylus icon option changed.
+        CHANGE_STYLUS_ICON_ENABLED = 1 << 6,
+
         // All devices must be reopened.
         CHANGE_MUST_REOPEN = 1 << 31,
     };
@@ -162,6 +165,9 @@ struct InputReaderConfiguration {
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
 
+    // True to show the pointer icon when a stylus is used.
+    bool stylusIconEnabled;
+
     // Ignore finger touches this long after the stylus has been used (including hover)
     nsecs_t stylusPalmRejectionTime;
 
@@ -182,6 +188,7 @@ struct InputReaderConfiguration {
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
             showTouches(false),
+            stylusIconEnabled(false),
             stylusPalmRejectionTime(50 * 10000000LL) // 50 ms
     { }
 
@@ -1609,6 +1616,8 @@ private:
     const VirtualKey* findVirtualKeyHit(int32_t x, int32_t y);
 
     void assignPointerIds();
+
+    void unfadePointer(PointerControllerInterface::Transition transition);
 
     bool rejectPalm(nsecs_t when);
 };
