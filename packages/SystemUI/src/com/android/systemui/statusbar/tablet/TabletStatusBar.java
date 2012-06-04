be4022234e1cc57628b6c14ffde8ab6a5d4cc3ce
James Sullins <jcsullins@gmail.com>
2012-06-04 00:56:16 -0500

    SystemUI: disable dock_battery_text if no dock_battery
    Change-Id: I984f3a161198ab5936520b4a6efc29f11baca25e

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -236,6 +236,8 @@ public class TabletStatusBar extends StatusBar implements
             mDockBatteryController.addIconView((ImageView)mNotificationPanel.findViewById(R.id.dock_battery));
             mDockBatteryController.addLabelView(
                     (TextView)mNotificationPanel.findViewById(R.id.dock_battery_text));
+        } else {
+            mNotificationPanel.findViewById(R.id.dock_battery_text).setVisibility(View.GONE);
         }
         // Bt
         mBluetoothController.addIconView(
