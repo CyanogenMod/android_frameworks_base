43033a71ee77b40dd66e64ccc1117e3d9cfa6afe
Evan McClain <aeroevan@gmail.com>
2012-06-08 13:51:30 -0400

    rs: Turn off Werror.
    It doesn't look like all of the aliasing violations were fixed.
    
    Change-Id: I18a3e7dbb778d17d9fba76353cbd0bacb671245d

--- libs/rs/Android.mk
+++ libs/rs/Android.mk
@@ -135,7 +135,7 @@ LOCAL_STATIC_LIBRARIES := libdex libft2
 LOCAL_C_INCLUDES += external/freetype/include external/zlib dalvik
 LOCAL_C_INCLUDES += frameworks/compile/libbcc/include
 
-LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS += -Wall -Wno-unused-parameter -Wno-unused-variable
 
 LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libRS
@@ -180,7 +180,7 @@ $(GEN): $(intermediates)/%.cpp : $(LOCAL_PATH)/%.cpp.rsg
 
 LOCAL_GENERATED_SOURCES += $(GEN)
 
-LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS += -Wall -Wno-unused-parameter -Wno-unused-variable
 LOCAL_CFLAGS += -DANDROID_RS_SERIALIZE
 LOCAL_CFLAGS += -fPIC
 
