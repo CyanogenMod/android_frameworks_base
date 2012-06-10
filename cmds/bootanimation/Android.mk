e6b54405aa70d7503a114d9c90ef7518abdd7133
Emilio López <turl@tuxfamily.org>
2012-06-10 17:39:10 +0000

    bootanimation: performance enhancements
    This patch implements two simple performance enhancements, which
    might benefit devices. They are completely opt-in by setting
    the following variables.
    
      * TARGET_BOOTANIMATION_PRELOAD: will preload the bootanimation
        zip to memory; this is handy on devices where the storage
        might be slow. By preloading it, we get quick access to the
        zip file, and the animation doesn't stutter.
    
      * TARGET_BOOTANIMATION_TEXTURE_CACHE: allows maintainers to
        force texture cache on/off.
    
    Also fixes the sign warnings related to int / size_t comparison
    
    Change-Id: Ie92d1b0aa90b43ba39c368a2a33657a60c3a64b5
    
    Conflicts:
    
    	cmds/bootanimation/BootAnimation.cpp

--- cmds/bootanimation/Android.mk
+++ cmds/bootanimation/Android.mk
@@ -21,6 +21,18 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES := \
 	$(call include-path-for, corecg graphics)
 
+ifeq ($(TARGET_BOOTANIMATION_PRELOAD),true)
+    LOCAL_CFLAGS += -DPRELOAD_BOOTANIMATION
+endif
+
+ifeq ($(TARGET_BOOTANIMATION_TEXTURE_CACHE),true)
+    LOCAL_CFLAGS += -DNO_TEXTURE_CACHE=0
+endif
+
+ifeq ($(TARGET_BOOTANIMATION_TEXTURE_CACHE),false)
+    LOCAL_CFLAGS += -DNO_TEXTURE_CACHE=1
+endif
+
 LOCAL_MODULE:= bootanimation
 
 
