f61a2008ebdcf1d5b0d35be9ea39e2f32fdd064c
Riaz Rahaman <riazr@codeaurora.org>
2012-06-14 11:45:43 +0200

    libstagefright: Software decoder is the first choice for thumbnail generation
    Set the flag to use software decoder only for the
    first call to extract video thumbnail.
    
    Change-Id: I09b3b3da5bfe57f21b0fecfbcc5aff7337bb91a2
    CRs-Fixed: 323190

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -394,15 +394,15 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
         }
         else {
             frame = extractVideoFrameWithCodecFlags(
+                &mClient, trackMeta, source, OMXCodec::kSoftwareCodecsOnly,
+                timeUs, option);
+        }
+    }
 #else
     VideoFrame *frame =
         extractVideoFrameWithCodecFlags(
-#endif
                 &mClient, trackMeta, source, OMXCodec::kPreferSoftwareCodecs,
                 timeUs, option);
-#ifdef QCOM_HARDWARE
-        }
-    }
 #endif
 
     if (frame == NULL) {
