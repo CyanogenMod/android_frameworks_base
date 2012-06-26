f69dbd6aed99cab2ca3cfb44ca3222ad65ece8a5
Jiju Kinattingal <jijuk@codeaurora.org>
2012-06-26 12:43:49 +0530

    Telephony: Try setup data call when the conditions are met in applyNewState
    The data connection failed is not broadcast for the enable APN type
    requests when the data is disabled from the settings menu.
    Changes to try intiate the data call if the conditions are met whenever
    updating the enabled/met flags of the ApnContext to make sure that the
    proper data connection state is broadcast.
    
    CRs-Fixed: 371400
    (cherry picked from commit 5f6ef98113e1f5fd26720c0c666e30318940e6f9)
    
    Change-Id: I27e4dee95883116c7fd853368662ea104195a5e5

--- telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java
@@ -1824,13 +1824,16 @@ public final class GsmDataConnectionTracker extends DataConnectionTracker {
                     apnContext.getDependencyMet() +"))");
         }
         if (apnContext.isReady()) {
-            if (enabled && met) return;
-            if (!enabled) {
-                apnContext.setReason(Phone.REASON_DATA_DISABLED);
+            if (enabled && met) {
+                trySetup = true;
             } else {
-                apnContext.setReason(Phone.REASON_DATA_DEPENDENCY_UNMET);
+                if (!enabled) {
+                    apnContext.setReason(Phone.REASON_DATA_DISABLED);
+                } else {
+                    apnContext.setReason(Phone.REASON_DATA_DEPENDENCY_UNMET);
+                }
+                cleanup = true;
             }
-            cleanup = true;
         } else {
             if (enabled && met) {
                 if (apnContext.isEnabled()) {
