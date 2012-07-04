96bbc92520d4214458d6dd3fdc37f5b82e0b3982
Volker Braun <vbraun.name@gmail.com>
2012-07-04 22:29:13 +0100

    Disable touch while using the stylus (Galaxy Note)
    Android 4.0 cannot form multitouch events from multiple input
    devices. So it is not possible to report one touchpoint from the
    stylus position and, at the same time, another touchpoint from a
    finger touch. Instead, when a new input device starts up the currently
    active input is cancelled. This is highly undesirable while writing
    with the pen.
    
    The easiest solution is to ignore non-stylus touch events while the
    stylus is within range (hovering) of the touchscreen. For example,
    N-trig digitizers implement this in hardware. But wacom digitizers do
    report pen data simultaneously with touch data. This patch disables
    (non-stylus) touch input within 50ms of stylus data. On my Galaxy
    Note this is necessary to make stylus input usable.
    
    Change-Id: I9f458d85d8bf83d6cf03b467d46ce066fd55b7e0

--- services/input/InputReader.h
+++ services/input/InputReader.h
@@ -152,6 +152,9 @@ struct InputReaderConfiguration {
     // True to show the pointer icon when a stylus is used.
     bool stylusIconEnabled;
 
+    // Ignore finger touches this long after the stylus has been used (including hover)
+    nsecs_t stylusPalmRejectionTime;
+
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
             pointerVelocityControlParameters(1.0f, 500.0f, 3000.0f, 3.0f),
@@ -169,7 +172,9 @@ struct InputReaderConfiguration {
             pointerGestureMovementSpeedRatio(0.8f),
             pointerGestureZoomSpeedRatio(0.3f),
             showTouches(false),
-            stylusIconEnabled(false) { }
+            stylusIconEnabled(false),
+            stylusPalmRejectionTime(50 * 10000000LL) // 50 ms
+    { }
 
     bool getDisplayInfo(int32_t displayId, bool external,
             int32_t* width, int32_t* height, int32_t* orientation) const;
@@ -1480,6 +1485,9 @@ private:
     VelocityControl mWheelXVelocityControl;
     VelocityControl mWheelYVelocityControl;
 
+    // The time the stylus event was processed by any TouchInputMapper
+    static nsecs_t mLastStylusTime;
+
     void sync(nsecs_t when);
 
     bool consumeRawTouches(nsecs_t when, uint32_t policyFlags);
@@ -1534,6 +1542,8 @@ private:
     void assignPointerIds();
 
     void unfadePointer(PointerControllerInterface::Transition transition);
+
+    bool rejectPalm(nsecs_t when);
 };
 
 
