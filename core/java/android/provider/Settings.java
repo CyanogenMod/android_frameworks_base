f63c1b674e0971980f3c1f263f7e0c307a412bb2
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-07-05 18:45:28 +0200

    implement noise suppression for phone calls (1/2)
    Change-Id: Ie67f15fd154dd0e64b6f3a007c938bf162e25e77

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2777,6 +2777,13 @@ public final class Settings {
         public static final String TTY_MODE = "tty_mode";
 
         /**
+         * Whether noise suppression is enabled. The value is
+         * boolean (1 or 0).
+         * @hide
+         */
+        public static final String NOISE_SUPPRESSION = "noise_suppression";
+
+        /**
          * Whether the sounds effects (key clicks, lid open ...) are enabled. The value is
          * boolean (1 or 0).
          */
@@ -3486,6 +3493,7 @@ public final class Settings {
             DTMF_TONE_TYPE_WHEN_DIALING,
             HEARING_AID,
             TTY_MODE,
+            NOISE_SUPPRESSION,
             SOUND_EFFECTS_ENABLED,
             HAPTIC_FEEDBACK_ENABLED,
             POWER_SOUNDS_ENABLED,       // moved to global
