e45cf7d232490f44aecf8f2447220a8b5ace4c10
Ethan Chen <intervigil@gmail.com>
2012-07-12 01:35:36 -0700

    bootanimation: fix usage of LOGW
    Change-Id: I1d59df9f44f414d80058a532644cbbb40e22c701

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -312,7 +312,7 @@ status_t BootAnimation::readyToRun() {
 
             free(crappyBuffer);
         } else {
-            LOGW("Unable to allocate memory to preload the animation");
+            ALOGW("Unable to allocate memory to preload the animation");
         }
         fclose(fd);
     }
