6b61b5f4b19f63cefe98614b1fbb3c17f9233e8b
Ryan Welton <rwelton24@gmail.com>
2012-07-13 14:00:00 +0100

    Extended Power Menu - Reboot
    Ported the reboot menu from CM9 to CM10, the original patch was not
    clean and require a bit of re-work.
    
    Change-Id: I70cf515f097ea94aedaa7469b0cb18c797af4b57
    
    Extended Power Menu: Initialize reboot reason to the first reason
    Change-Id: Id94863d56c45ff10d3ded7f62e3f2ef7b8cd0e69
    
    Extended Power Menu - Reboot
    Change-Id: I8c3a773ba943dc42e8cf3feab32c462d25d3bb06

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -392,6 +392,7 @@ public interface WindowManagerPolicy {
         public void switchKeyboardLayout(int deviceId, int direction);
 
         public void shutdown();
+        public void reboot();
         public void rebootSafeMode();
     }
 
--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2011 David van Tonder
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -198,8 +199,27 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         onAirplaneModeChanged();
 
         mItems = new ArrayList<Action>();
+        
+        // first: reboot
+        mItems.add(
+            new SinglePressAction(
+                        com.android.internal.R.drawable.ic_lock_reboot, 
+                        R.string.global_action_reboot) {
+                
+                    public void onPress() {
+                        mWindowManagerFuncs.reboot();
+                    }
 
-        // first: power off
+                    public boolean showDuringKeyguard() {
+                        return true;
+                    }
+
+                    public boolean showBeforeProvisioning() {
+                        return true;
+                    }
+                });
+        
+        // second: power off
         mItems.add(
             new SinglePressAction(
                     com.android.internal.R.drawable.ic_lock_power_off,
@@ -223,6 +243,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     return true;
                 }
             });
+
 
         // next: airplane mode
         mItems.add(mAirplaneModeOn);
