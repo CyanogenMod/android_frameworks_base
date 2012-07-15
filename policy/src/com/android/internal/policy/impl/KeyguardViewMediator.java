b1598536437745e14ccbc3c6fcced3c904d66e61
DvTonder <david.vantonder@gmail.com>
2012-07-15 10:28:15 -0400

    Framework: Port CM9 features to CM10 (WIP)
    This commit includes:
     - Power menu Reboot (not fully functional yet)
     - Power menu screenshot
     - Profiles
     - Lock screen Calendar
     - Lock screen Weather
     - Notification light customization
     - Battery light customization
     - and a few more things to support Settings
    
    Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb

--- policy/src/com/android/internal/policy/impl/KeyguardViewMediator.java
+++ policy/src/com/android/internal/policy/impl/KeyguardViewMediator.java
@@ -25,6 +25,8 @@ import com.android.internal.widget.LockPatternUtils;
 import android.app.ActivityManagerNative;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Profile;
+import android.app.ProfileManager;
 import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -252,6 +254,8 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
     private int mUnlockSoundId;
     private int mLockSoundStreamId;
 
+    private ProfileManager mProfileManager;
+
     /**
      * The volume applied to the lock/unlock sounds.
      */
@@ -288,6 +292,8 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
                 PowerManager.PARTIAL_WAKE_LOCK,
                 "keyguardWakeAndHandOff");
         mWakeAndHandOff.setReferenceCounted(false);
+
+        mProfileManager = (ProfileManager) context.getSystemService(Context.PROFILE_SERVICE);
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(DELAYED_KEYGUARD_ACTION);
@@ -364,11 +370,21 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
             mScreenOn = false;
             if (DEBUG) Log.d(TAG, "onScreenTurnedOff(" + why + ")");
 
-            // Lock immediately based on setting if secure (user has a pin/pattern/password).
-            // This also "locks" the device when not secure to provide easy access to the
-            // camera while preventing unwanted input.
-            final boolean lockImmediately =
-                mLockPatternUtils.getPowerButtonInstantlyLocks() || !mLockPatternUtils.isSecure();
+            // Prepare for handling Lock/Slide lock delay and timeout
+            boolean lockImmediately = false;
+            final ContentResolver cr = mContext.getContentResolver();
+            boolean separateSlideLockTimeoutEnabled = Settings.System.getInt(cr,
+                    Settings.System.SCREEN_LOCK_SLIDE_DELAY_TOGGLE, 0) == 1;
+            if (mLockPatternUtils.isSecure()) {
+                // Lock immediately based on setting if secure (user has a pin/pattern/password)
+                // This is retained as-is to ensue AOSP security integrity is maintained
+                lockImmediately = mLockPatternUtils.getPowerButtonInstantlyLocks();
+            } else {
+                // Unless a separate slide lock timeout is enabled, this "locks" the device when
+                // not secure to provide easy access to the camera while preventing unwanted input
+                lockImmediately = separateSlideLockTimeoutEnabled ? false
+                        : mLockPatternUtils.getPowerButtonInstantlyLocks();
+            }
 
             if (mExitSecureCallback != null) {
                 if (DEBUG) Log.d(TAG, "pending exit secure callback cancelled");
@@ -387,7 +403,6 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
                 // to enable it a little bit later (i.e, give the user a chance
                 // to turn the screen back on within a certain window without
                 // having to unlock the screen)
-                final ContentResolver cr = mContext.getContentResolver();
 
                 // From DisplaySettings
                 long displayTimeout = Settings.System.getInt(cr, SCREEN_OFF_TIMEOUT,
@@ -398,9 +413,24 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
                         Settings.Secure.LOCK_SCREEN_LOCK_AFTER_TIMEOUT,
                         KEYGUARD_LOCK_AFTER_DELAY_DEFAULT);
 
+                // From CyanogenMod specific Settings
+                int slideLockTimeoutDelay = (why == WindowManagerPolicy.OFF_BECAUSE_OF_TIMEOUT ? Settings.System
+                        .getInt(cr, Settings.System.SCREEN_LOCK_SLIDE_TIMEOUT_DELAY,
+                                KEYGUARD_LOCK_AFTER_DELAY_DEFAULT) : Settings.System.getInt(cr,
+                        Settings.System.SCREEN_LOCK_SLIDE_SCREENOFF_DELAY, 0));
+
                 // From DevicePolicyAdmin
                 final long policyTimeout = mLockPatternUtils.getDevicePolicyManager()
                         .getMaximumTimeToLock(null);
+
+                if (DEBUG) Log.d(TAG, "Security lock screen timeout delay is " + lockAfterTimeout
+                        + " ms; slide lock screen timeout delay is "
+                        + slideLockTimeoutDelay
+                        + " ms; Separate slide lock delay settings considered: "
+                        + separateSlideLockTimeoutEnabled
+                        + "; Policy timeout is "
+                        + policyTimeout
+                        + " ms");
 
                 long timeout;
                 if (policyTimeout > 0) {
@@ -408,7 +438,8 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
                     displayTimeout = Math.max(displayTimeout, 0); // ignore negative values
                     timeout = Math.min(policyTimeout - displayTimeout, lockAfterTimeout);
                 } else {
-                    timeout = lockAfterTimeout;
+                    // Not sure lockAfterTimeout is needed any more but keeping it for AOSP compatibility
+                    timeout = separateSlideLockTimeoutEnabled ? slideLockTimeoutDelay : lockAfterTimeout;
                 }
 
                 if (timeout <= 0) {
@@ -635,6 +666,13 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
 
         if (mLockPatternUtils.isLockScreenDisabled() && !lockedOrMissing) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because lockscreen is off");
+            return;
+        }
+
+        // if the current profile has disabled us, don't show
+        if (!lockedOrMissing
+                && mProfileManager.getActiveProfile().getScreenLockMode() == Profile.LockMode.DISABLE) {
+            if (DEBUG) Log.d(TAG, "doKeyguard: not showing because of profile override");
             return;
         }
 
--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -5163,6 +5163,12 @@ public class WindowManagerService extends IWindowManager.Stub
         ShutdownThread.rebootSafeMode(mContext, true);
     }
 
+    // Called by window manager policy.  Not exposed externally.
+    @Override
+    public void reboot() {
+        ShutdownThread.reboot(mContext, "null", true);
+    }
+
     public void setInputFilter(InputFilter filter) {
         mInputManager.setInputFilter(filter);
     }
