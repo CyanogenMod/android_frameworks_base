8ae5af8af74b2508ffaaec2a8936413a036f0f67
Danesh M <daneshm90@gmail.com>
2012-07-15 22:18:41 -0400

    Framework : Lockscreen - Customizable shortcuts
    Allow users to choose lockscreen shortcuts / icons :
    	Phone - 1-5 targets
    	Tablets - 1-8 targets
    
    CM10 port
    ---------
    Patchset 1-8: Initial port
    Patchset 9  : Revert to CM9 drawables
    Patchset 10 : Rebase
    
    Change-Id: I9b163efeef8f47b1d21f7f61552ebe424049cfeb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2209,6 +2209,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+         * Stores values for custom lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
+
+        /**
          * URI for the low battery sound file.
          * @hide
          */
