2fb410bfc590e3c10c32a850e8602a78d1b3d5d2
Danesh M <daneshm90@gmail.com>
2012-07-15 22:18:41 -0400

    Framework : Lockscreen - Customizable shortcuts
    Forward port lockscreen shortcuts
    
    Change-Id: Ic3b935df5c0da35f31373d96943e442dc39cf11b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2614,6 +2614,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+         * Stores values for custom lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
