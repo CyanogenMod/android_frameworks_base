00ac3264174c9921fab3de47d26e5c650a7fb0a5
Danesh M <daneshm90@gmail.com>
2012-07-15 22:18:41 -0400

    Framework : Lockscreen - Customizable shortcuts
    Patchset 1 : Initial port
    Patchset 2 : Disable magneticTargets
    Patchset 3 : Disable firstOffset
    
    ---------------- Squashed Commits ------------------
    
    Allow users to choose lockscreen shortcuts / icons :
    	Phone - 1-5 targets
    	Tablets - 1-8 targets
    
    CM10 port
    ---------
    Patchset 1-8: Initial port
    Patchset 9  : Revert to CM9 drawables
    Patchset 10 : Rebase
    
    Change-Id: I9b163efeef8f47b1d21f7f61552ebe424049cfeb
    
    Fix all upscaled lockscreen drawables and add a few
    
    Several of the original CM9 drawables were upscaled
    from a lower quality source. I rescaled some from the
    originals and recreated the missing ones in vector
    format plus added some new ones.
    
    Change-Id: I112c35cf087daf0679a12bdad89258121dedeb0c
    
    Lockscreen : Update camera availability check
    Make the default availability of the target dependant on the
    availability of a camera rather than the presense of a predefined resource.
    
    Also update .Camera to .CameraLauncher since that is what we default too.
    
    Change-Id: Ib3f06cc76fb7f7df20ec632051d5dbe754a9c69e
    
    LockscreenShortcuts : Fix minor bugs
    
    Fixes :
    
          - Inability to have contacts/direct dials..etc on lockscreen (http://goo.gl/BKEJs)
          - Multiple activities of same type on stack.
    
    Change-Id: I6c2c3e1c87239267601d78eb38778d0388f4a41c
    
    ----------------------------------------------------
    
    Change-Id: I9b163efeef8f47b1d21f7f61552ebe424049cfeb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2464,6 +2464,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+         * Stores values for custom lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
