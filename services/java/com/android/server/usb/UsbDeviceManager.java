3a5ae11a156c17e70a203b8af8fd3337f77824ea
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-20 07:18:05 +0200

    ADB Over Network, integration of the adb-host mode (already present)
    The feature can be used/tested without the Settings part :
    
      setprop service.adb.tcp.port 5555
    
    Note: This ADB setting is not persistent (for security purpose) and
    require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
    Note : ADB_PORT setting key was added in the commit a6c11c9d
    
    Change-Id: I5c61a53948349c785356cb5aae165110d75e3074
    Signed-off-by: Tanguy Pruvot <tanguy.pruvot@gmail.com>

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -348,6 +348,10 @@ public class UsbDeviceManager {
                         Settings.Secure.getUriFor(Settings.Secure.ADB_ENABLED),
                                 false, new AdbSettingsObserver());
 
+                mContentResolver.registerContentObserver(
+                        Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
+                                false, new AdbSettingsObserver());
+
                 // Watch for USB configuration changes
                 mUEventObserver.startObserving(USB_STATE_MATCH);
                 mUEventObserver.startObserving(ACCESSORY_START_MATCH);
