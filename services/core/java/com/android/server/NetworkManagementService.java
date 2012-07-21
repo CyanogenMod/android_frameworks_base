df6d01c1ffac34949b5f90f4560f9b5c03d5ab08
Michaël Burtin <bukington@gmail.com>
2012-07-21 01:59:23 -0700

    frameworks/base: support devices with SoftAp that don't require firmware reload
    Partial forward port from ICS.
    Original Change-Id: If2db6e8ce675c3214ce645231c7a354f41ea0cc6
    
    Change-Id: Iacb2498ec0f567a0a7c4e0dbf9127b70f09223bd

--- services/core/java/com/android/server/NetworkManagementService.java
+++ services/core/java/com/android/server/NetworkManagementService.java
@@ -40,6 +40,7 @@ import static com.android.server.NetworkManagementService.NetdResponseCode.TtyLi
 import static com.android.server.NetworkManagementSocketTagger.PROP_QTAGUID_ENABLED;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.net.INetworkManagementEventObserver;
 import android.net.InterfaceConfiguration;
@@ -1403,7 +1404,10 @@ public class NetworkManagementService extends INetworkManagementService.Stub
             WifiConfiguration wifiConfig, String wlanIface) {
         mContext.enforceCallingOrSelfPermission(CONNECTIVITY_INTERNAL, TAG);
         try {
-            wifiFirmwareReload(wlanIface, "AP");
+            if (mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_wifiApFirmwareReload)) {
+                wifiFirmwareReload(wlanIface, "AP");
+            }
             if (wifiConfig == null) {
                 mConnector.execute("softap", "set", wlanIface);
             } else {
