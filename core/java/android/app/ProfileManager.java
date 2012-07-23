8f54187fe0ccf421013b1d3cd7d404d397cb6ac6
DvTonder <david.vantonder@gmail.com>
2012-07-23 17:16:04 -0400

    Framework: Add the ability to enable/disable use of Profiles (Part 2/2) WIP!
    This will remove the Profiles item from the Power menu when the user has
    selected to disable the use of System Profiles.
    
    TODO: Better handling of returning a default profile when requested
    
    Patchset 1-4: Initial commit
    Patchset 5  : Additional error checking to prevent profile changes
    Patchset 6  : Remove WIP
    Patchset 7  : Workaround for NPE when no profile returned as our
                  null checking in profiles is not the best
                : Add WIP
    Patchset 8  : Rebase
    
    Change-Id: I3c29b1bbd8f72c5432a1906a6b412d9412279c9f

--- core/java/android/app/ProfileManager.java
+++ core/java/android/app/ProfileManager.java
@@ -18,13 +18,16 @@ package android.app;
 
 import java.util.UUID;
 
+import android.R;
 import android.content.Context;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.ParcelUuid;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.util.Log;
+
 
 /**
  * @hide
@@ -36,6 +39,8 @@ public class ProfileManager {
     private Context mContext;
 
     private static final String TAG = "ProfileManager";
+
+    private static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
 
     /** @hide */
     static public IProfileManager getService() {
@@ -54,28 +59,48 @@ public class ProfileManager {
 
     @Deprecated
     public void setActiveProfile(String profileName) {
-        try {
-            getService().setActiveProfileByName(profileName);
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                getService().setActiveProfileByName(profileName);
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
         }
     }
 
     public void setActiveProfile(UUID profileUuid) {
-        try {
-            getService().setActiveProfile(new ParcelUuid(profileUuid));
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                getService().setActiveProfile(new ParcelUuid(profileUuid));
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
         }
     }
 
     public Profile getActiveProfile() {
-        try {
-            return getService().getActiveProfile();
-        } catch (RemoteException e) {
-            Log.e(TAG, e.getLocalizedMessage(), e);
-        }
-        return null;
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                SYSTEM_PROFILES_ENABLED, 1) == 1) {
+            // Profiles are enabled, return active profile
+            try {
+                return getService().getActiveProfile();
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
+            return null;
+        } else {
+            // Profiles are not enabled, return the default profile
+            try {
+                return getService().getProfileByName(R.string.profileNameDefault);
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getLocalizedMessage(), e);
+            }
+            return null;
+      }
     }
 
     /** @hide */
