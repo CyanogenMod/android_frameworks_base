e072d5ca767f372dc187470cd7e91a47cfcfc30c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-25 13:42:41 +0200

    Option to hide Adb notification icon (framework)
    Change-Id: Ie5325b3c6cde64be8d36f6afc4e747addb5ce8df

--- services/java/com/android/server/usb/LegacyUsbDeviceManager.java
+++ services/java/com/android/server/usb/LegacyUsbDeviceManager.java
@@ -312,6 +312,14 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
                     Settings.Secure.getUriFor(Settings.Secure.ADB_ENABLED),
                     false, new AdbSettingsObserver());
 
+                mContentResolver.registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.ADB_NOTIFY),
+                    false, new ContentObserver(null) {
+                        public void onChange(boolean selfChange) {
+                            updateAdbNotification();
+                        }
+                    });
+
                 // Watch for USB configuration changes
                 if (mLegacy) {
                     mUEventObserver.startObserving(USB_LEGACY_MATCH);
@@ -591,7 +599,10 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
             if (mNotificationManager == null) return;
             final int id = com.android.internal.R.string.adb_active_notification_title;
             if (mAdbEnabled && mConnected) {
-                if ("0".equals(SystemProperties.get("persist.adb.notify"))) return;
+                if ("0".equals(SystemProperties.get("persist.adb.notify"))
+                 || Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.ADB_NOTIFY, 1) == 0)
+                    return;
 
                 if (!mAdbNotificationShown) {
                     Resources r = mContext.getResources();
--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -347,10 +347,17 @@ public class UsbDeviceManager {
                 mContentResolver.registerContentObserver(
                         Settings.Secure.getUriFor(Settings.Secure.ADB_ENABLED),
                                 false, new AdbSettingsObserver());
-
                 mContentResolver.registerContentObserver(
                         Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
                                 false, new AdbSettingsObserver());
+                mContentResolver.registerContentObserver(
+                        Settings.Secure.getUriFor(Settings.Secure.ADB_NOTIFY),
+                                false, new ContentObserver(null) {
+                            public void onChange(boolean selfChange) {
+                                updateAdbNotification();
+                            }
+                        }
+                );
 
                 // Watch for USB configuration changes
                 mUEventObserver.startObserving(USB_STATE_MATCH);
@@ -679,7 +686,10 @@ public class UsbDeviceManager {
             if (mNotificationManager == null) return;
             final int id = com.android.internal.R.string.adb_active_notification_title;
             if (mAdbEnabled && mConnected) {
-                if ("0".equals(SystemProperties.get("persist.adb.notify"))) return;
+                if ("0".equals(SystemProperties.get("persist.adb.notify"))
+                 || Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.ADB_NOTIFY, 1) == 0)
+                    return;
 
                 if (!mAdbNotificationShown) {
                     Resources r = mContext.getResources();
