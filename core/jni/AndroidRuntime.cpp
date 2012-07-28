c5bb1bbd0a59640ec394ee729b57de34295a4e99
David Ng <dave@codeaurora.org>
2012-07-27 17:17:14 -0700

    ActivityTrigger:  New class to invoke when activity starts/resumes
    Change-Id: I46fa419e7b97953ba5581ee03127f0e3f93e48f0
    (cherry picked from commit 71a57d30370584488a246a904750e7bdad5ba9a0)
    (cherry picked from commit 5d2abff8e4d399e6bd320ea11bf672ad3b3be980)
    (cherry picked from commit ffd89bcf976985acf0392c0dd667f8b5241d77ac)
    
    frameworks/base: update perflock path name in Activity Trigger
    
    Update the perflock path name and add
    backup path for backward compatibility
    
    Change-Id: I3d2c6f6fe1b3c63ec02827d06d5af667266db8d5
    (cherry picked from commit 4db57e5021c4091352f40f6e4399b0b929f63151)
    (cherry picked from commit b9fc7b99697a1b2f891dc642ff27c8a4194ca79d)
    (cherry picked from commit 5910b639f0dea078049e31718e00bede2eb2ec00)
    
    frameworks/base: update library path name in Activity Trigger
    
    Change path from /system/lib to /vendor/lib
    since it is no longer needed.
    
    Change-Id: I5fa7a367b21d4128c92ab7190944c176a1f4dae6
    (cherry picked from commit 7abea046455c1c8fc99126ae6cf9a6d4823ca50f)
    (cherry picked from commit 5aadc3f081bef5b26b13e0bb4955354e3be0bcd7)
    (cherry picked from commit 133db12892589a21865a900c3709073ed86eaf8a)

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -180,6 +180,7 @@ extern int register_android_animation_PropertyValuesHolder(JNIEnv *env);
 extern int register_com_android_internal_content_NativeLibraryHelper(JNIEnv *env);
 extern int register_com_android_internal_net_NetworkStatsFactory(JNIEnv *env);
 extern int register_android_content_res_PackageRedirectionMap(JNIEnv* env);
+extern int register_com_android_internal_app_ActivityTrigger(JNIEnv *env);
 
 static AndroidRuntime* gCurRuntime = NULL;
 
@@ -1222,6 +1223,9 @@ static const RegJNIRec gRegJNI[] = {
     REG_JNI(register_com_android_internal_content_NativeLibraryHelper),
     REG_JNI(register_com_android_internal_net_NetworkStatsFactory),
     REG_JNI(register_android_content_res_PackageRedirectionMap),
+#ifdef QCOM_HARDWARE
+    REG_JNI(register_com_android_internal_app_ActivityTrigger),
+#endif
 };
 
 /*
