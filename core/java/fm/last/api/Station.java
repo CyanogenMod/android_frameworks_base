42b06b8a0c11d52a5fdc62e28c1298b8637ade6b
Trey Briggs <treybriggs@gmail.com>
2012-08-04 16:15:30 -0500

    Bluetooth: AVRCP 1.3 features changes (2/2)
    Bluetooth module will listen metadata/event change intents
    from Music app and post data to Bluez stack.
    
    Merged the following from ICS:
    Change-Id: 37151cf5162404e27c985a1b6a874cfd9a51d657
    Change-Id: f3d9f182f0eacc3ead491f7d3f571f5ed93f7c20
    Change-Id: 432fdab5fbbfb6687b571d1e06d6b51e944ed4d5
    Change-Id: b4d51e3bc1b52f6c1ff1d006aae6cff261c739e9
    Change-Id: f1c1549254c17be681066544efbbcf7ad1e25913
    
           modified:   core/java/android/server/BluetoothA2dpService.java
           new file:   core/java/fm/last/api/Station.java
           modified:   core/jni/android_server_BluetoothA2dpService.cpp
           modified:   core/jni/android_server_BluetoothEventLoop.cpp
           new file:   core/res/res/values/avrcp_data.xml
           modified:   core/res/res/values/public.xml
    
    Signed-off-by: Trey Briggs <treybriggs@gmail.com>

---
+++ core/java/fm/last/api/Station.java
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   Copyright 2005-2009 Last.fm Ltd.                                      *
+ *   Portions contributed by Casey Link, Lukasz Wisniewski,                *
+ *   Mike Jennings, and Michael Novak Jr.                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+package fm.last.api;
+
+import java.io.Serializable;
+
+/**
+ * @author jennings Date: Oct 23, 2008
+ */
+public class Station implements Serializable {
+
+	private static final long serialVersionUID = 1499806102972292532L;
+	private String name;
+	private String type;
+	private String url;
+	private String supportsDiscovery;
+
+	public Station(String name, String type, String url, String supportsDiscovery) {
+		this.name = name;
+		this.type = type;
+		this.url = url;
+		this.supportsDiscovery = supportsDiscovery;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getSupportsDiscovery() {
+		return supportsDiscovery;
+	}
+}
--- core/jni/android_server_BluetoothEventLoop.cpp
+++ core/jni/android_server_BluetoothEventLoop.cpp
@@ -287,6 +287,13 @@ static jboolean setUpEventLoop(native_data_t *nat) {
             LOG_AND_FREE_DBUS_ERROR(&err);
             return JNI_FALSE;
         }
+        dbus_bus_add_match(nat->conn,
+                "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".Control'",
+                &err);
+        if (dbus_error_is_set(&err)) {
+            LOG_AND_FREE_DBUS_ERROR(&err);
+            return JNI_FALSE;
+        }
 
         dbus_bus_add_match(nat->conn,
                 "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".HealthDevice'",
@@ -449,6 +456,12 @@ static void tearDownEventLoop(native_data_t *nat) {
 
         dbus_bus_remove_match(nat->conn,
                 "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".AudioSink'",
+                &err);
+        if (dbus_error_is_set(&err)) {
+            LOG_AND_FREE_DBUS_ERROR(&err);
+        }
+        dbus_bus_remove_match(nat->conn,
+                "type='signal',interface='"BLUEZ_DBUS_BASE_IFC".Control'",
                 &err);
         if (dbus_error_is_set(&err)) {
             LOG_AND_FREE_DBUS_ERROR(&err);
