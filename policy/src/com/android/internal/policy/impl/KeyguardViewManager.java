45c49e2f6975e4d077db38796ae99dfe2e8539e8
Kelly <mushroomhead52e@gmail.com>
2012-08-10 14:00:23 -0400

    Add lockscreen rotation as an optional rotation setting (1/2)
    This will allow the user to select wether or not to use the
    rotation settings for the lockscreen.  This is dependent
    upon the rotation setting being checked.
    
    Change-Id: Ic1bd1e570a9119e12f71cda94ce57eca5095a659

--- policy/src/com/android/internal/policy/impl/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/KeyguardViewManager.java
@@ -109,7 +109,7 @@ public class KeyguardViewManager implements KeyguardWindowController {
         Resources res = mContext.getResources();
         boolean enableScreenRotation =
                 SystemProperties.getBoolean("lockscreen.rot_override",false)
-                || (res.getBoolean(R.bool.config_enableLockScreenRotation) && (Settings.System.getInt(mContext.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 1) != 0));
+                || ((Settings.System.getInt(mContext.getContentResolver(), Settings.System.LOCKSCREEN_ROTATION, 1) != 0) && (Settings.System.getInt(mContext.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 1) != 0));
         if (mKeyguardHost == null) {
             if (DEBUG) Log.d(TAG, "keyguard host is null, creating it...");
 
