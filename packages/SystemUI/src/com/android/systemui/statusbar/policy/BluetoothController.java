be2af3a9dd1ae9b9c45ad0c0760026c4c9fd1db5
rob43 <robert4three@gmail.com>
2012-08-10 23:26:29 -0500

    Framework: Tablet powerwidget improvements
    Change-Id: I671edccef30de061625ebad425612c00e4268823

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BluetoothController.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * This code has been modified. Portions copyright (C) 2012 ParanoidAndroid Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
