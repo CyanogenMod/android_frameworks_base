bcc8f733796f3fcd1c2180336960d747a6995f0c
Pierre-Antoine LaFayette <plafayet@codeaurora.org>
2012-08-16 15:55:32 -0400

    [webkit] Runtime enablement of WebGL
    Hook up debug setting to enable/disable WebGL.
    
    (cherry-picked from commit 69ee15e5b0bc1e0b2d41d80f9f5247b826c62ceb)
    
    Change-Id: Ib07427378450a858d6b6308e23b5e1aaac002dd3

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -126,6 +126,7 @@ public class WebSettingsClassic extends WebSettings {
     private boolean         mEnableSmoothTransition = false;
     private boolean         mForceUserScalable = false;
     private boolean         mPasswordEchoEnabled = true;
+    private boolean         mWebGLEnabled = true;
 
     // AutoFill Profile data
     public static class AutoFillProfile {
@@ -1257,7 +1258,7 @@ public class WebSettingsClassic extends WebSettings {
     @Override
     public synchronized void setAppCachePath(String path) {
         // We test for a valid path and for repeated setting on the native
-        // side, but we can avoid syncing in some simple cases. 
+        // side, but we can avoid syncing in some simple cases.
         if (mAppCachePath == null && path != null && !path.isEmpty()) {
             mAppCachePath = path;
             postSync();
@@ -1634,6 +1635,25 @@ public class WebSettingsClassic extends WebSettings {
     }
 
     /**
+     * @hide
+     */
+    public synchronized boolean isWebGLAvailable() {
+        return nativeIsWebGLAvailable();
+    }
+
+    /**
+     * Sets whether WebGL is enabled.
+     * @param flag Set to true to enable WebGL.
+     * @hide
+     */
+    public synchronized void setWebGLEnabled(boolean flag) {
+        if (mWebGLEnabled != flag) {
+            mWebGLEnabled = flag;
+            postSync();
+        }
+    }
+
+    /**
      * Sets whether viewport metatag can disable zooming.
      * @param flag Whether or not to forceably enable user scalable.
      */
@@ -1745,4 +1765,5 @@ public class WebSettingsClassic extends WebSettings {
 
     // Synchronize the native and java settings.
     private native void nativeSync(int nativeFrame);
+    private native boolean nativeIsWebGLAvailable();
 }
