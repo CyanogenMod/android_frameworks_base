ac187b6f7b4b89d32595565434595747231fda9d
Pierre-Antoine LaFayette <plafayet@codeaurora.org>
2012-08-16 15:55:32 -0400

    [webkit] Runtime enablement of WebGL
    Hook up debug setting to enable/disable WebGL.
    
    Change-Id: I46790e61471a76eb5a8d8ff552022e12c3782204

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -125,6 +125,7 @@ public class WebSettingsClassic extends WebSettings {
     private boolean         mEnableSmoothTransition = false;
     private boolean         mForceUserScalable = false;
     private boolean         mPasswordEchoEnabled = true;
+    private boolean         mWebGLEnabled = true;
 
     // AutoFill Profile data
     public static class AutoFillProfile {
@@ -1249,7 +1250,7 @@ public class WebSettingsClassic extends WebSettings {
     @Override
     public synchronized void setAppCachePath(String path) {
         // We test for a valid path and for repeated setting on the native
-        // side, but we can avoid syncing in some simple cases. 
+        // side, but we can avoid syncing in some simple cases.
         if (mAppCachePath == null && path != null && !path.isEmpty()) {
             mAppCachePath = path;
             postSync();
@@ -1626,6 +1627,24 @@ public class WebSettingsClassic extends WebSettings {
     }
 
     /**
+     * @hide
+     */
+    public synchronized boolean isWebGLAvailable() {
+        return nativeIsWebGLAvailable();
+    }
+
+    /**
+     * Sets whether WebGL is enabled.
+     * @param flag Set to true to enable WebGL.
+     */
+    public synchronized void setWebGLEnabled(boolean flag) {
+        if (mWebGLEnabled != flag) {
+            mWebGLEnabled = flag;
+            postSync();
+        }
+    }
+
+    /**
      * Sets whether viewport metatag can disable zooming.
      * @param flag Whether or not to forceably enable user scalable.
      */
@@ -1737,4 +1756,5 @@ public class WebSettingsClassic extends WebSettings {
 
     // Synchronize the native and java settings.
     private native void nativeSync(int nativeFrame);
+    private native boolean nativeIsWebGLAvailable();
 }
