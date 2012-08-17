8293e02eab31560e26032a2ee06c51e04b3ae798
Rajulu Ponnada <rajulu@codeaurora.org>
2012-08-16 17:15:19 -0700

    frameworks/base: fix for garbage screen updates in certain testcases
    fxing blackbox popup issue when using twitter signup
    
    Change-Id: I7449e31c1e93580076b2a23c39cd5a3aefab006b
    (cherry picked from commit 9de660f617554ef4c2a988876dd8fc86e9328666)

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -654,7 +654,8 @@ bool OpenGLRenderer::createFboLayer(Layer* layer, Rect& bounds, sp<Snapshot> sna
     glBindFramebuffer(GL_FRAMEBUFFER, layer->getFbo());
 #ifdef QCOM_HARDWARE
     TILERENDERING_START(layer->getFbo(), clip.left, clip.top,
-                        clip.right, clip.bottom, mWidth, mHeight);
+                        clip.right, clip.bottom,
+                        bounds.getWidth(), bounds.getHeight());
 #endif
     layer->bindTexture();
 
