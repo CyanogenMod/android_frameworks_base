84e96346b753566562346be97114bf33c6f6cbd9
Phil Tunstall <ptunstall@gmail.com>
2012-08-27 22:22:18 +0100

    Framework: Volume panel cleanup (1/2)
    - Hide notifications volume slider when linked with ringtone volume.
    - Default to expandable panel style. This was made default on the settings side
    in commit Ibd25bd1f058d49ab0560ed073c3b45782d165cbb
    - Use a ContentObserver to monitor system settings.
    - Moved static final variables out of Settings.java.
    - Cleanup of a couple of untidy bits from initial jellybean merge.
    
    Change-Id: I738da63a4d59ad5ee52b1aa9e0e4f4c9c93a7576

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1583,20 +1583,14 @@ public final class Settings {
 
 
         /**
-         * Volume Overlay Mode, This is behaviour of the volume overlay panel
-         * Defaults to 0 - which is simple
+         * Volume Overlay Mode. This is the style of the volume overlay panel.
+         *      0 - Single
+         *      1 - Expandable (default)
+         *      2 - Expanded
+         *      3 - None
          * @hide
          */
         public static final String MODE_VOLUME_OVERLAY = "mode_volume_overlay";
-
-        /** @hide */
-        public static final int VOLUME_OVERLAY_SINGLE = 0;
-        /** @hide */
-        public static final int VOLUME_OVERLAY_EXPANDABLE = 1;
-        /** @hide */
-        public static final int VOLUME_OVERLAY_EXPANDED = 2;
-        /** @hide */
-        public static final int VOLUME_OVERLAY_NONE = 3;
 
         /**
          * Ringer mode. This is used internally, changing this value will not
