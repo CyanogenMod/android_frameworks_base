a9452a949dd3dca5e6317a9a108914e87b398194
Aaron Echols <atechols@gmail.com>
2012-08-29 15:46:31 -0700

    Add resource to disable the CM IME selector method (2/2)
    The CM IME selector is enabled along side the Android version on tablet devices,
    creating duplicate IME selectors. This gives an easy option to disable the CM
    IME selector as a resource value for tablet or similar devices.
    
    Thanks to Mike Kasick for the much needed help. :)
    
    Change-Id: I18b60209933350fb624f20307077664ac026bfea

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -1438,8 +1438,14 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
             mCurMethodId = null;
             unbindCurrentMethodLocked(true);
         }
-        mShowOngoingImeSwitcherForPhones = Settings.System.getInt(mContext.getContentResolver(),
-               Settings.System.STATUS_BAR_IME_SWITCHER, 1) == 1;
+        // code to disable the CM Phone IME switcher with config_show_cmIMESwitcher set = false
+        try {
+            mShowOngoingImeSwitcherForPhones = Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.STATUS_BAR_IME_SWITCHER) == 1;
+        } catch (SettingNotFoundException e) {
+            mShowOngoingImeSwitcherForPhones = mRes.getBoolean(
+            com.android.internal.R.bool.config_show_cmIMESwitcher);
+        }
     }
 
     /* package */ void setInputMethodLocked(String id, int subtypeId) {
