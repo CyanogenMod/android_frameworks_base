6e933bbfb2d384c2624ab0b5f67bd314d292ec5a
Mike Kasick <mike@kasick.org>
2012-08-30 13:42:27 -0400

    RILs: Eliminate repeated overrides of responseOperatorInfos.
    - Add mQANElements field, default to 4 in RIL, use in responseOperatorInfos.
      This may be set to 5 in subclassed RIL constructors.
    
    - Remove repeated overrides of responseOperatorInfos by subclassed RILs, as
      there's no logic change, just a disagreement as to whether the
      QUERY_AVAILABLE_NETWORKS response is an array with 4 or 5 per-network
      elements.
    
    - Size OperatorInfo ArrayList appropriately in SemcRIL and SonyQualcommRIL.
    
    Change-Id: I1254a670d8611f76aad13a6f197c67012c7d4c47

--- telephony/java/com/android/internal/telephony/SemcRIL.java
+++ telephony/java/com/android/internal/telephony/SemcRIL.java
@@ -51,6 +51,7 @@ public class SemcRIL extends RIL implements CommandsInterface {
 
     public SemcRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
+        mQANElements = 5;
     }
 
     @Override
@@ -214,31 +215,6 @@ public class SemcRIL extends RIL implements CommandsInterface {
                               + " aid: " + aid);
 
         send(rr);
-    }
-
-    @Override
-    protected Object
-    responseOperatorInfos(Parcel p) {
-        String strings[] = (String [])responseStrings(p);
-        ArrayList<OperatorInfo> ret;
-
-        if (strings.length % 5 != 0) {
-            throw new RuntimeException(
-                "RIL_REQUEST_QUERY_AVAILABLE_NETWORKS: invalid response. Got "
-                + strings.length + " strings, expected multible of 5");
-        }
-
-        ret = new ArrayList<OperatorInfo>(strings.length / 4);
-
-        for (int i = 0 ; i < strings.length ; i += 5) {
-            ret.add (
-                new OperatorInfo(
-                    strings[i+0],
-                    strings[i+1],
-                    strings[i+2],
-                    strings[i+3]));
-        }
-        return ret;
     }
 
     @Override
