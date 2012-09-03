CyanogenMod/android_frameworks_base

commit 0ff962809a9c7cec71eb480231d96116a102ec34

framework: Allow specifying max wallpaper width
For some devices with very high-res screen (1920x1200), the wallpaper
width suggested by the launcher may be too large for the GPU and so
we need to limit it. (2048 for the above mentioned screen)

We don't need to limit the height since that's the height of the
screen.

Change-Id: I188ac585370fb705cfa50d5ad84786e6f2cbd016

services/java/com/android/server/WallpaperManagerService.java
@@ -529,6 +529,12 @@ class WallpaperManagerService extends IWallpaperManager.Stub {
             throw new IllegalArgumentException("width and height must be > 0");
         }
 
+        int maxWidth = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_wallpaperMaxWidth);
+        if (width > maxWidth) {
+            width = maxWidth;
+        }
+
         synchronized (mLock) {
             if (width != wallpaper.width || height != wallpaper.height) {
                 wallpaper.width = width;

core/res/res/values/config.xml
@@ -952,4 +952,9 @@
     <string-array translatable="false" name="config_accessoryDockNames">
         <item>"dock"</item>
     </string-array>
+
+    <!-- Maximum wallpaper width for devices with very high-res screen (1920x1200)
+         the wallpaper width suggestion by the launcher may be too large for
+         the GPU to handle. -->
+    <integer name="config_wallpaperMaxWidth">-1</integer>
 </resources>

core/res/res/values/public.xml
@@ -3685,6 +3685,7 @@
   <java-symbol type="integer" name="config_deviceHardwareKeys" />
   <java-symbol type="bool" name="config_hasRemovableLid" />
   <java-symbol type="bool" name="config_samsung_stk" />
+  <java-symbol type="integer" name="config_wallpaperMaxWidth" />
 
   <!-- Notification and battery light -->
   <java-symbol type="bool" name="config_intrusiveBatteryLed" />

commit 9cbfaf211810f22b1b44403b4cab40d6411dafc2

Services: Fix wallpaper scrolling
Wallpaper it's not scrolling with the maxwidth set to -1. Bug
introduced by commit 0ff962809a9c7cec71eb480231d96116a102ec34.

Change-Id: I8bf98bb2e8f7e07ad763ae5326d8dd6cf7cc5cb5

services/java/com/android/server/WallpaperManagerService.java
@@ -531,7 +531,7 @@ class WallpaperManagerService extends IWallpaperManager.Stub {
 
         int maxWidth = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_wallpaperMaxWidth);
-        if (width > maxWidth) {
+        if (maxWidth > 0 && width > maxWidth) {
             width = maxWidth;
         }
 