f16c9b2a1999a587f8b0845bf9643dd57226a357
Takashi Matsuoka <mentwar@gmail.com>
2012-09-08 22:04:55 +1000

    Set mUSIM and mSetPreferredNetworkType.
    This is required to enable "GSM/WCDMA/LTE auto" mode on evita, and to make the device load that setting after reboot.
    
    Change-Id: Iec563669f50724e7726b0c0e23f62910442cac76

--- telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
+++ telephony/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -107,7 +107,17 @@ public class HTCQualcommRIL extends QualcommSharedRIL implements CommandsInterfa
             Log.d(LOG_TAG, "This is a GSM PHONE " + appIndex);
         }
 
-        mAid = status.getApplication(appIndex).aid;
+        if (numApplications > 0) {
+            IccCardApplication application = status.getApplication(appIndex);
+            mAid = application.aid;
+            mUSIM = application.app_type
+                      == IccCardApplication.AppType.APPTYPE_USIM;
+            mSetPreferredNetworkType = mPreferredNetworkType;
+
+            if (TextUtils.isEmpty(mAid))
+               mAid = "";
+            Log.d(LOG_TAG, "mAid " + mAid);
+        }
 
         return status;
     }
