76b1691caede365ad6fa99f2cac1ad19152862aa
Daniel Bateman <jetison.24@gmail.com>
2012-09-08 20:40:13 -0500

    Lockscreen: Add clock alignment (1/2)
    Screenshots:
    - Left: https://dl.dropbox.com/u/4806199/Screenshot_2012-09-08-20-24-30.png
    - Center: https://dl.dropbox.com/u/4806199/Screenshot_2012-09-08-20-24-15.png
    - Right: https://dl.dropbox.com/u/4806199/Screenshot_2012-09-08-20-24-23.png
    
    * Portrait only!
    
    Patch-set 2:
        * Fixed boot loop when using security settings
        * Support for sw600dp
    
    Change-Id: I41aeea50e95b0655c2bc90b56ee506e7baa470e2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2678,6 +2678,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ALWAYS_SHOW_BATTERY = "lockscreen_always_show_battery";
 
         /**
+         * Where to align the clock on the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_CLOCK_ALIGN = "lockscreen_clock_align";
+
+        /**
          * Show the pending notification counts as overlays on the status bar
          * @hide
          */
