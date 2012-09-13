47aa3b7e587accdf7c8b276202452cc7cddc10a9
Andrew Dodd <atd7@cornell.edu>
2012-09-12 21:57:51 -0400

    wifi: Allow Settings to retrieve country code
    This makes getCountryCode behave like getFrequencyBand
    
    This allows Settings to get/set country code like
    it can for frequency band.
    
    add aditional fixes to original implementation
    and changes for 4.4
    
    Change-Id: Id9f00d3a678642c6c142fd1511af820d729507a7

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -276,6 +276,9 @@ public class WifiStateMachine extends StateMachine {
     /* Tracks current frequency mode */
     private AtomicInteger mFrequencyBand = new AtomicInteger(WifiManager.WIFI_FREQUENCY_BAND_AUTO);
 
+    /* Tracks current country code */
+    private String mCountryCode = "GB";
+
     /* Tracks if we are filtering Multicast v4 packets. Default is to filter. */
     private AtomicBoolean mFilteringMulticastV4Packets = new AtomicBoolean(true);
 
@@ -1482,6 +1485,15 @@ public class WifiStateMachine extends StateMachine {
      * @param persist {@code true} if the setting should be remembered.
      */
     public void setCountryCode(String countryCode, boolean persist) {
+
+        String countryCodeUser = Settings.Global.getString(mContext.getContentResolver(),
+                Settings.Global.WIFI_COUNTRY_CODE_USER);
+        if (countryCodeUser != null && countryCodeUser != countryCode) {
+            persist = true;
+            countryCode = countryCodeUser;
+            mCountryCode = countryCode;
+        }
+
         if (persist) {
             mPersistedCountryCode = countryCode;
             Settings.Global.putString(mContext.getContentResolver(),
@@ -1504,6 +1516,13 @@ public class WifiStateMachine extends StateMachine {
         List<WifiChannel> result = (List<WifiChannel>) resultMsg.obj;
         resultMsg.recycle();
         return result;
+    }
+
+    /**
+     * Returns the operational country code
+     */
+    public String getCountryCode() {
+        return mCountryCode;
     }
 
     /**
@@ -1737,6 +1756,7 @@ public class WifiStateMachine extends StateMachine {
                 Settings.Global.WIFI_COUNTRY_CODE);
         if (countryCode != null && !countryCode.isEmpty()) {
             setCountryCode(countryCode, false);
+            mCountryCode = countryCode;
         } else {
             // On wifi-only devices, some drivers don't find hidden SSIDs unless DRIVER COUNTRY
             // is called. Pinging the wifi driver without country code resolves this issue.
