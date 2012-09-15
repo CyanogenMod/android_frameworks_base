134e3883c463db69a1733a04dd3ef6b6b0f00dcb
D4rKn3sSyS <jedga_95@outlook.com>
2012-09-15 15:00:48 -0500

    "Clear all" button on recent apps
        Ported from ParanoidAndroid by zigackly:
        https://github.com/ParanoidAndroid/android_frameworks_base/commit/f8c44e6ef667a3ebe9b658f148fa78a8d4f9a8dd
    
        Add button to tablet layout ported from Codename Android by zigackly:
        https://github.com/CNA/android_frameworks_base/commit/8d259fe3588c261baa5a17c361255cf28cc82bfe
    
        Patch set 2: Make the layout consistent, with the button at the top right.
    
        Patch set 3: Remove the redundant use of removeTask()
    
        Patch set 4: Top right layout does not work in tablet mode: make it top left for tablets only.
    
        Patch set 5: Different implementation (No need to keep track of views) - Danesh M
    
    Change-Id: Iceb86eef41160f1c17e51463b37fd97e27acfb2e

--- packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.database.DataSetObserver;
 import android.graphics.Canvas;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.FloatMath;
@@ -56,6 +57,7 @@ public class RecentsHorizontalScrollView extends HorizontalScrollView
     private RecentsScrollViewPerformanceHelper mPerformanceHelper;
     private HashSet<View> mRecycledViews;
     private int mNumItemsInOneScreenful;
+    private Handler mHandler;
 
     public RecentsHorizontalScrollView(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
@@ -64,6 +66,7 @@ public class RecentsHorizontalScrollView extends HorizontalScrollView
         mSwipeHelper = new SwipeHelper(SwipeHelper.Y, this, densityScale, pagingTouchSlop);
         mPerformanceHelper = RecentsScrollViewPerformanceHelper.create(context, attrs, this, false);
         mRecycledViews = new HashSet<View>();
+        mHandler = new Handler();
     }
 
     public void setMinSwipeAlpha(float minAlpha) {
@@ -167,6 +170,35 @@ public class RecentsHorizontalScrollView extends HorizontalScrollView
     @Override
     public void removeViewInLayout(final View view) {
         dismissChild(view);
+    }
+
+    @Override
+    public void removeAllViewsInLayout() {
+        smoothScrollTo(0, 0);
+        Thread clearAll = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int count = mLinearLayout.getChildCount();
+                View[] refView = new View[count];
+                for (int i = 0; i < count; i++) {
+                    refView[i] = mLinearLayout.getChildAt(i);
+                }
+                for (int i = 0; i < count; i++) {
+                    final View child = refView[i];
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            dismissChild(child);
+                        }
+                    });
+                    try {
+                        Thread.sleep(150);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+        clearAll.start();
     }
 
     public boolean onInterceptTouchEvent(MotionEvent ev) {
