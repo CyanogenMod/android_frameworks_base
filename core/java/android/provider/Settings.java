4aad10d22424667b2e8a9072ba7c4d3f583aa74f
Steve Kondik <shade@chemlab.org>
2012-09-26 00:31:30 -0700

    Add @hide to MOCK_SMS stuff
    Change-Id: I3f65cb43052c24f8e4c42a80773fb785c90c038a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3446,6 +3446,7 @@ public final class Settings {
         /**
          * Setting to allow the use of {@link SMSDispatcher#MockSmsReceiver} to simulate
          * the reception of SMS for testing purposes during application development.
+         * @hide
          */
         public static final String ALLOW_MOCK_SMS = "mock_sms";
 
