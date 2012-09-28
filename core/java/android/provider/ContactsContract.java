9f6c74b4fa66dedf4526d450c3302bf0f548e726
Chad Wagner <chad.wagner@gmail.com>
2012-09-28 17:59:41 -0400

    Allow SIP calls to be added as a contact from call log (2/2)
    Change-Id: I9a154b88e40e250c67fcced77ca0a9626619b667

--- core/java/android/provider/ContactsContract.java
+++ core/java/android/provider/ContactsContract.java
@@ -6042,6 +6042,12 @@ public final class ContactsContract {
              * <P>Type: boolean</P>
              */
             public static final String IM_ISPRIMARY = "im_isprimary";
+
+            /**
+             * The extra field for the contact sip address.
+             * <P>Type: String</P>
+             */
+            public static final String SIP_ADDRESS = "sip_address";
         }
     }
 }
