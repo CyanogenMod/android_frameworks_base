fbaa9d635a7ff5e571a534ca6265216118212474
zigackly <zigackerly@gmail.com>
2012-10-08 10:24:24 +0100

    Expanded desktop capability (2/2)
    	Ported from Codename android
    	Abiltiy to hide navigation bar from Team EOS
    
    	Screenshot:  https://www.dropbox.com/s/izz57ux01c6hve2/Screenshot_2012-10-14-12-08-24.png
    
    Change-Id: I2dd395c7f27789311b09cd429359915287f97d2d

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -673,6 +673,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HARDWARE_KEY_REBINDING), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.EXPANDED_DESKTOP_STATE), false, this);
             if (SEPARATE_TIMEOUT_FOR_SCREEN_SAVER) {
                 resolver.registerContentObserver(Settings.Secure.getUriFor(
                         "screensaver_timeout"), false, this);
@@ -2795,6 +2797,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // decided that it can't be hidden (because of the screen aspect ratio),
         // then take that into account.
         navVisible |= !mCanHideNavigationBar;
+        navVisible &= (Settings.System.getInt(mContext.getContentResolver(), Settings.System.EXPANDED_DESKTOP_STATE, 0) == 0);
 
         if (mNavigationBar != null) {
             // Force the navigation bar to its appropriate place and
@@ -3357,7 +3360,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // and mTopIsFullscreen is that that mTopIsFullscreen is set only if the window
                 // has the FLAG_FULLSCREEN set.  Not sure if there is another way that to be the
                 // case though.
-                if (topIsFullscreen) {
+                if (topIsFullscreen || Settings.System.getInt(mContext.getContentResolver(), Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1) {
                     if (DEBUG_LAYOUT) Log.v(TAG, "** HIDING status bar");
                     if (mStatusBar.hideLw(true)) {
                         changes |= FINISH_LAYOUT_REDO_LAYOUT;
