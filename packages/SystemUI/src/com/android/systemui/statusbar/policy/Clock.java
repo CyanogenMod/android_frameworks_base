ec7cfc74bf950e763584a170b90d9b1290d86616
Sven Dawitz <sven@dawitz.de>
2012-10-16 14:25:57 +0200

    SystemUI: Forward port Statusbar Clock and Date actions
    Includes:
    
    Add onClick action to notification clock and date
    
    In expanded StatusBar
    - clicking clock opens alarms
    - clicking date opens calendar - today selected
    
    http://www.youtube.com/watch?v=MLom5e3nQoA
    
    PS3: added turn-blue-on-click
    PS4++: nitpicking cleanups
    
    Change-Id: I850732dcca0e65af59ce009aaf9f7ba18bcaf915
    
    Add onLongClick action to notication clock and date
    
    - As discussed with ciwrl on google+, LongClick opens DateTimeSettings
    - Moved on-touch color change to superior xml solution, as suggested by
      Andrew Boren <preludedrew@gmail.com> here: http://review.evervolv.com/#/c/3676/"
    
    Change-Id: I1fae71e347ffcb786227ba0b2651a66f2df35e27
    
    Add SET_ALARM permission for SystemUI
    
    If one or more third-party alarm clock app is installed, clicking clock should open
    an app chooser for the alarm clock. However, permission for com.android.alarm.permission.SET_ALARM
    is required to make this behaviour work properly.
    
    See http://forum.xda-developers.com/showpost.php?p=33155463&postcount=2749 for symptom if this
    permission is not assigned.
    
    Change-Id: Iaf8cb9aba6e53da021da5373493b1419096e91fa

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -16,43 +16,39 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.database.ContentObserver;
-import android.graphics.Canvas;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.os.Handler;
+import android.provider.AlarmClock;
 import android.provider.Settings;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.format.DateFormat;
 import android.text.style.CharacterStyle;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
 import android.util.AttributeSet;
-import android.util.Slog;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
+
+import com.android.internal.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-import com.android.internal.R;
-
 /**
  * This widget display an analogic clock with two hands for hours and
  * minutes.
  */
-public class Clock extends TextView {
+public class Clock extends TextView implements OnClickListener, OnLongClickListener {
     private boolean mAttached;
     private Calendar mCalendar;
     private String mClockFormatString;
@@ -101,6 +97,10 @@ public class Clock extends TextView {
         mHandler = new Handler();
         SettingsObserver settingsObserver = new SettingsObserver(mHandler);
         settingsObserver.observe();
+        if(isClickable()){
+            setOnClickListener(this);
+            setOnLongClickListener(this);
+        }
         updateSettings();
     }
 
@@ -232,7 +232,7 @@ public class Clock extends TextView {
                 return formatted;
             }
         }
- 
+
         return result;
 
     }
@@ -259,6 +259,40 @@ public class Clock extends TextView {
             setVisibility(View.VISIBLE);
         else
             setVisibility(View.GONE);
+    }
+
+    private void collapseStartActivity(Intent what) {
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapsePanels();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start activity
+        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(what);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        collapseStartActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.settings.DATE_SETTINGS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        collapseStartActivity(intent);
+
+        // consume event
+        return true;
     }
 }
 
