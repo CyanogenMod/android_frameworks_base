4d8092dbb492854cb507fa3c9bd3c71ca6cfe2f7
Sven Dawitz <sven@dawitz.de>
2012-10-16 14:25:57 +0200

    Add onClick action to notification clock and date
    In expanded StatusBar
    - clicking clock opens alarms
    - clicking date opens calendar - today selected
    
    http://www.youtube.com/watch?v=MLom5e3nQoA
    
    PS3: added turn-blue-on-click
    PS4: nitpicking cleanups
    
    Change-Id: I850732dcca0e65af59ce009aaf9f7ba18bcaf915

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -16,15 +16,23 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.provider.CalendarContract;
 import android.text.TextUtils.TruncateAt;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.util.Slog;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewParent;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -33,7 +41,7 @@ import com.android.systemui.R;
 
 import java.util.Date;
 
-public final class DateView extends LinearLayout {
+public final class DateView extends LinearLayout implements OnClickListener, OnTouchListener {
     private static final String TAG = "DateView";
 
     private TextView mDoW;
@@ -42,6 +50,7 @@ public final class DateView extends LinearLayout {
     private boolean mAttachedToWindow;
     private boolean mWindowVisible;
     private boolean mUpdating;
+    private int mDefaultColor;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -75,6 +84,10 @@ public final class DateView extends LinearLayout {
         mDate.setEllipsize(TruncateAt.END);
         mDate.setTextAppearance(context, R.style.TextAppearance_StatusBar_Expanded_Date);
         mDate.setIncludeFontPadding(false);
+        setOnClickListener(this);
+        setOnTouchListener(this);
+
+        mDefaultColor = mDate.getCurrentTextColor();
 
         // Extract how DoW and Date are distributed in the layout
         // The format is distributed as %1$s\n%2$s or %2$s\n%1$s but always in
@@ -103,7 +116,7 @@ public final class DateView extends LinearLayout {
         mAttachedToWindow = true;
         setUpdates();
     }
-    
+
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
@@ -179,5 +192,49 @@ public final class DateView extends LinearLayout {
                 mContext.unregisterReceiver(mIntentReceiver);
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        mDate.setTextColor(mDefaultColor);
+        mDoW.setTextColor(mDefaultColor);
+
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapse();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start calendar - today is selected
+        long nowMillis = System.currentTimeMillis();
+
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder, nowMillis);
+        Intent intent = new Intent(Intent.ACTION_VIEW)
+                .setData(builder.build());
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(intent);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int a = event.getAction();
+        if (a == MotionEvent.ACTION_DOWN) {
+            int cTouch = getResources().getColor(com.android.internal.R.color.holo_blue_light);
+            mDate.setTextColor(cTouch);
+            mDoW.setTextColor(cTouch);
+        } else if (a == MotionEvent.ACTION_CANCEL || a == MotionEvent.ACTION_UP) {
+            mDate.setTextColor(mDefaultColor);
+            mDoW.setTextColor(mDefaultColor);
+        }
+        // never consume touch event, so onClick is propperly processed
+        return false;
     }
 }
