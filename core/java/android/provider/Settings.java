ac141f426474f094b9940ce25fcdf69264bd2e42
João Ventura <joaojonesventura@gmail.com>
2012-11-08 23:29:40 +0000

    Mms auto-retrieval (1/2): framework
    - Settings: Add auto-retrieval and auto-retrieval-on-roaming strings
    - GsmDataConnectionTracker: Gets settings and auto-connects APN_TYPE_MMS
    
    Patch set 2   : Fix NPE when the preference was not yet set to a defaulted
                    value
                  : Add @hide's
                  : Set proper defaults
    
    Change-Id: I9c4ea47cd39cd017fba3d5b0649d0e84aaf78b4e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1317,6 +1317,23 @@ public final class Settings {
          */
         public static final String WIFI_STATIC_DNS2 = "wifi_static_dns2";
 
+        /**
+         * Allows automatic retrieval of mms contents
+         * <p>Type: INT</p>
+         * 0 -- false
+         * 1 -- true
+         * @hide
+         */
+        public static final String MMS_AUTO_RETRIEVAL = "mms_auto_retrieval";
+
+        /**
+         * Allows automatic retrieval of mms contents during roaming
+         * <p>Type: INT</p>
+         * 0 -- false
+         * 1 -- true
+         * @hide
+         */
+        public static final String MMS_AUTO_RETRIEVAL_ON_ROAMING = "mms_auto_on_roaming";
 
         /**
          * Determines whether remote devices may discover and/or connect to
@@ -2918,6 +2935,8 @@ public final class Settings {
             WIFI_STATIC_NETMASK,
             WIFI_STATIC_DNS1,
             WIFI_STATIC_DNS2,
+            MMS_AUTO_RETRIEVAL,
+            MMS_AUTO_RETRIEVAL_ON_ROAMING,
             BLUETOOTH_DISCOVERABILITY,
             BLUETOOTH_DISCOVERABILITY_TIMEOUT,
             DIM_SCREEN,
