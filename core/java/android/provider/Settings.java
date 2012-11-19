ef44d827bd6a789b62741c32e57c29ea80289976
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-18 22:34:58 -0500

    SystemUi: Navigation bar Quick launch shortcuts (2 of 2)
    This allows up to 3 targets on the Navigation bar ring
    or Home buttons with custom Quick launch shortcut actions.
    
    Patchset 1 - 21 - Initial commits and edits
    Patchset 22 - Update commit message
    
    Change-Id: Ic5bc6021d55b9c266527463bb916283feb00ad06

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3018,6 +3018,17 @@ public final class Settings {
         public static final String NOTIFICATION_CONVERT_SOUND_TO_VIBRATION = "convert_sound_to_vibration";
 
         /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
