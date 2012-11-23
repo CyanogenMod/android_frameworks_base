b2569394aba085175d4598bd69e3167ec3dfa24d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-23 11:05:03 +0000

    Silence continuous warnings about lack of kernel tracing support
    Yes, we get it. Tracing is disabled.
    
    Change-Id: I09227f9be36190551331e99d4cd827e527ad6b6d

--- core/java/android/os/Trace.java
+++ core/java/android/os/Trace.java
@@ -100,7 +100,7 @@ public final class Trace {
     private static long cacheEnabledTags() {
         long tags = nativeGetEnabledTags();
         if (tags == TRACE_FLAGS_NOT_READY) {
-            Log.w(TAG, "Unexpected value from nativeGetEnabledTags: " + tags);
+            //Log.w(TAG, "Unexpected value from nativeGetEnabledTags: " + tags);
             // keep going
         }
         sEnabledTags = tags;
