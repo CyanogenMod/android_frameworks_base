a65aab81cb853d9217786ece1acd8475fe2ce62e
Mårten Kongstad <marten.kongstad@sonymobile.com>
2012-11-23 10:59:00 +0100

    Runtime resource overlay, iteration 2, test cases
    Add automated test cases for runtime resource overlay, iteration 2.
    
    The tests cases are a mixture of python to run 'adb shell' commands and
    regular instrumentation tests. Python is also used to reboot the Android
    device between tests, to setup different overlay scenarios for
    framework-res.apk.
    
    Run core/tests/overlaytests/testrunner.py --help for more information on
    how to execute the test suite.
    
    Change-Id: I692aa1a7ad073efd116b24f9ec7f197dfd65dfef

--- core/tests/overlaytests/OverlayTest/Android.mk
+++ core/tests/overlaytests/OverlayTest/Android.mk
@@ -5,6 +5,8 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_PACKAGE_NAME := OverlayTest
 
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 include $(BUILD_PACKAGE)
