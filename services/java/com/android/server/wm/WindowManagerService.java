2e87b1b06ad366fdb68fa56eac77075b13ceeec2
Steve Kondik <shade@chemlab.org>
2012-11-23 23:22:43 -0500

    input: Add workaround for broken hardware keyboard support
    * Add workaround var for devices with broken kernels
    
    Change-Id: Ib6ade70f399067469ba5aece48c6ec4608125d75

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -576,6 +576,8 @@ public class WindowManagerService extends IWindowManager.Stub
     final DisplayManagerService mDisplayManagerService;
     final DisplayManager mDisplayManager;
 
+    private boolean mForceDisableHardwareKeyboard = false;
+
     // Who is holding the screen on.
     Session mHoldingScreenOn;
     PowerManager.WakeLock mHoldingScreenWakeLock;
@@ -835,6 +837,9 @@ public class WindowManagerService extends IWindowManager.Stub
         mInputManager = inputManager;
         mFxSession = new SurfaceSession();
         mAnimator = new WindowAnimator(this);
+
+        mForceDisableHardwareKeyboard = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceDisableHardwareKeyboard);
 
         initPolicy(uiHandler);
 
@@ -6966,7 +6971,10 @@ public class WindowManagerService extends IWindowManager.Stub
             }
 
             // Determine whether a hard keyboard is available and enabled.
-            boolean hardKeyboardAvailable = config.keyboard != Configuration.KEYBOARD_NOKEYS;
+            boolean hardKeyboardAvailable = false;
+            if (!mForceDisableHardwareKeyboard) {
+                mHardKeyboardAvailable = config.keyboard != Configuration.KEYBOARD_NOKEYS;
+            }
             if (hardKeyboardAvailable != mHardKeyboardAvailable) {
                 mHardKeyboardAvailable = hardKeyboardAvailable;
                 mHardKeyboardEnabled = hardKeyboardAvailable;
