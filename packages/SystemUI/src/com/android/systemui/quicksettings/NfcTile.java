791b9a15a0b8717ef17bc086024d135d5893fe02
Danesh Mondegarian <daneshm90@gmail.com>
2012-11-25 02:08:40 +0100

    QuickSettings : Port from cm-10.2 (WIP)
    Port quicksettings/ribbon from cm-10.2
    
    Change-Id: I6710a62a82ed274eb9e8c86cd2a1faf657ae53f1

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/NfcTile.java
@@ -0,0 +1,103 @@
+package com.android.systemui.quicksettings;
+
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+
+
+public class NfcTile extends QuickSettingsTile {
+
+    private static NfcAdapter mNfcAdapter;
+    private static final int NFC_ADAPTER_UNKNOWN = -100;
+
+    public NfcTile(Context context, 
+            QuickSettingsController qsc) {
+        super(context, qsc);
+
+        mOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleState();
+                updateResources();
+            }
+        };
+
+        mOnLongClick = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent("android.settings.NFC_SETTINGS");
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startSettingsActivity(intent);
+                return true;
+            }
+        };
+
+        qsc.registerAction(NfcAdapter.ACTION_ADAPTER_STATE_CHANGED, this);
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile(getNfcState());
+        super.onPostCreate();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        updateResources();
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile(getNfcState());
+        updateQuickSettings();
+    }
+
+    protected void toggleState() {
+        int state = getNfcState();
+        switch (state) {
+            case NfcAdapter.STATE_TURNING_ON:
+            case NfcAdapter.STATE_ON:
+                mNfcAdapter.disable();
+                break;
+            case NfcAdapter.STATE_TURNING_OFF:
+            case NfcAdapter.STATE_OFF:
+                mNfcAdapter.enable();
+                break;
+        }
+    }
+
+    private synchronized void updateTile(int state) {
+
+        switch (state) {
+        case NfcAdapter.STATE_TURNING_ON:
+        case NfcAdapter.STATE_ON:
+            mDrawable = R.drawable.ic_qs_nfc_on;
+            mLabel = mContext.getString(R.string.quick_settings_nfc);
+            break;
+        case NfcAdapter.STATE_TURNING_OFF:
+        case NfcAdapter.STATE_OFF:
+        default:
+            mDrawable = R.drawable.ic_qs_nfc_off;
+            mLabel = mContext.getString(R.string.quick_settings_nfc_off);
+            break;
+        }
+    }
+
+    private int getNfcState() {
+        if (mNfcAdapter == null) {
+            try {
+                mNfcAdapter = NfcAdapter.getNfcAdapter(mContext);
+            } catch (UnsupportedOperationException e) {
+                return NFC_ADAPTER_UNKNOWN;
+            }
+        }
+        return mNfcAdapter.getAdapterState();
+    }
+}
---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -0,0 +1,137 @@
+package com.android.systemui.quicksettings;
+
+import android.app.ActivityManagerNative;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+import com.android.systemui.statusbar.phone.PhoneStatusBar;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsTileView;
+
+public class QuickSettingsTile implements OnClickListener {
+
+    protected final Context mContext;
+    protected QuickSettingsContainerView mContainer;
+    protected QuickSettingsTileView mTile;
+    protected OnClickListener mOnClick;
+    protected OnLongClickListener mOnLongClick;
+    protected final int mTileLayout;
+    protected int mDrawable;
+    protected String mLabel;
+    protected PhoneStatusBar mStatusbarService;
+    protected QuickSettingsController mQsc;
+    protected SharedPreferences mPrefs;
+
+    public QuickSettingsTile(Context context, QuickSettingsController qsc) {
+        this(context, qsc, R.layout.quick_settings_tile_basic);
+    }
+
+    public QuickSettingsTile(Context context, QuickSettingsController qsc, int layout) {
+        mContext = context;
+        mDrawable = R.drawable.ic_notifications;
+        mLabel = mContext.getString(R.string.quick_settings_label_enabled);
+        mStatusbarService = qsc.mStatusBarService;
+        mQsc = qsc;
+        mTileLayout = layout;
+        mPrefs = mContext.getSharedPreferences("quicksettings", Context.MODE_PRIVATE);
+    }
+
+    public void setupQuickSettingsTile(LayoutInflater inflater,
+            QuickSettingsContainerView container) {
+        mTile = (QuickSettingsTileView) inflater.inflate(
+                R.layout.quick_settings_tile, container, false);
+        mTile.setContent(mTileLayout, inflater);
+        mContainer = container;
+        mContainer.addView(mTile);
+        onPostCreate();
+        updateQuickSettings();
+        mTile.setOnClickListener(this);
+        mTile.setOnLongClickListener(mOnLongClick);
+    }
+
+    public void setLabelVisibility(boolean visible) {
+        TextView tv = (TextView) mTile.findViewById(R.id.text);
+        if (tv != null) {
+            tv.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+        View sepPadding = mTile.findViewById(R.id.separator_padding);
+        if (sepPadding != null) {
+            sepPadding.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    void onPostCreate() {}
+
+    public void onDestroy() {}
+
+    public void onReceive(Context context, Intent intent) {}
+
+    public void onChangeUri(ContentResolver resolver, Uri uri) {}
+
+    public void updateResources() {
+        if(mTile != null) {
+            updateQuickSettings();
+        }
+    }
+
+    void updateQuickSettings() {
+        TextView tv = (TextView) mTile.findViewById(R.id.text);
+        if (tv != null) {
+            tv.setText(mLabel);
+        }
+        ImageView image = (ImageView) mTile.findViewById(R.id.image);
+        if (image != null) {
+            image.setImageResource(mDrawable);
+        }
+    }
+
+    void startSettingsActivity(String action) {
+        Intent intent = new Intent(action);
+        startSettingsActivity(intent);
+    }
+
+    void startSettingsActivity(Intent intent) {
+        startSettingsActivity(intent, true);
+    }
+
+    private void startSettingsActivity(Intent intent, boolean onlyProvisioned) {
+        if (onlyProvisioned && !mStatusbarService.isDeviceProvisioned()) return;
+        try {
+            // Dismiss the lock screen when Settings starts.
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (RemoteException e) {
+        }
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
+        mStatusbarService.animateCollapsePanels();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mOnClick != null) {
+            mOnClick.onClick(v);
+        }
+
+        ContentResolver resolver = mContext.getContentResolver();
+        boolean shouldCollapse = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_COLLAPSE_PANEL, 0, UserHandle.USER_CURRENT) == 1;
+        if (shouldCollapse) {
+            mQsc.mBar.collapseAllPanels(true);
+        }
+    }
+}
