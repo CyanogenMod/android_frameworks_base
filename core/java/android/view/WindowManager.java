a1f1361e6e43473099c9034d01f798d5e00f9a28
Danesh M <daneshm90@gmail.com>
2012-11-29 22:26:21 -0500

    Camera : Powerkey shutter (1/2)
    Allows user to take use power key to take pictures/videos
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a
    (cherry picked from commit 15661444ae1faea831218f0c936b756de2f0698b)

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1135,6 +1135,12 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_WAS_NOT_FULLSCREEN = 0x00002000;
 
         /**
+         * Window flag: Overrides default power key behavior
+         * @{hide}
+         */
+        public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
+
+        /**
          * Window flag: adding additional blur layer and set this as masking layer
          * {@hide}
          */
