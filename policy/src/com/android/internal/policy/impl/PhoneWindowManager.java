95229ce70e4cc200296a510fb702fb7fd8b20491
Danesh M <daneshm90@gmail.com>
2012-11-29 22:26:21 -0500

    Camera : Powerkey shutter (1/2)
    Allows user to take use power key to take pictures/videos
    
    Original author: DaneshM (devatwork)
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3676,6 +3676,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                if ((mTopFullscreenOpaqueWindowState.getAttrs().flags
+                        & WindowManager.LayoutParams.PREVENT_POWER_KEY) != 0){
+                    return result;
+                }
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
                     if (isScreenOn && !mPowerKeyTriggered
