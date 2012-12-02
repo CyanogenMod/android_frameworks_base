61ca6a6d3b7725adf4881601e708727441d38fdc
DvTonder <david.vantonder@gmail.com>
2012-12-02 08:48:36 -0500

    Framework: Forward port Battery light support and fixes
    Change-Id: Ic81d3ffb7548c7b7e34ffecd2f9db781f13b705d

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -713,21 +713,11 @@ public final class BatteryService extends Binder {
     private final class Led {
         private final LightsService.Light mBatteryLight;
 
-        private final int mBatteryLowARGB;
-        private final int mBatteryMediumARGB;
-        private final int mBatteryFullARGB;
         private final int mBatteryLedOn;
         private final int mBatteryLedOff;
 
         public Led(Context context, LightsService lights) {
             mBatteryLight = lights.getLight(LightsService.LIGHT_ID_BATTERY);
-
-            mBatteryLowARGB = mContext.getResources().getInteger(
-                    com.android.internal.R.integer.config_notificationsBatteryLowARGB);
-            mBatteryMediumARGB = mContext.getResources().getInteger(
-                    com.android.internal.R.integer.config_notificationsBatteryMediumARGB);
-            mBatteryFullARGB = mContext.getResources().getInteger(
-                    com.android.internal.R.integer.config_notificationsBatteryFullARGB);
 
             // Does the Device support changing battery LED colors?
             mMultiColorLed = context.getResources().getBoolean(
@@ -781,7 +771,7 @@ public final class BatteryService extends Binder {
                     mBatteryLight.setColor(mBatteryMediumARGB);
                 }
             } else {
-                //No lights if not charging and not low
+                // No lights if not charging and not low
                 mBatteryLight.turnOff();
             }
         }
