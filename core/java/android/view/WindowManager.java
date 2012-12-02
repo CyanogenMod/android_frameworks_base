2848dfef7537ef59a9c0d927fcd3573599d5aefe
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-12-02 21:11:11 +0000

    Underp public API... again
    Change-Id: Ibb8d74cec9de3c2227451d33f3fe4ce82a731514

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -915,7 +915,7 @@ public interface WindowManager extends ViewManager {
 
 
         /** Window flag: Overrides default power key behavior
-            @{hide} */
+            {@hide} */
         public static final int PREVENT_POWER_KEY = 0x80000000;
 
         /**
