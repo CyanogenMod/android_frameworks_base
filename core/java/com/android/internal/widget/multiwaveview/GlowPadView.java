8d037a2ded2edafc2ecafad2a8d799cb3f82f629
Danesh Mondegarian <daneshm90@gmail.com>
2012-12-12 00:08:14 -0500

    GlowPadView : Re-enable magneticTargets
    Re-enable magneticTargets and add logic to make it
    compatible with lockscreen shortcuts.
    
    Change-Id: I523141f03566804888fde08a116f73ea62ca523e

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -951,6 +951,11 @@ public class GlowPadView extends View {
                         if (angleMatches && (dist2(tx, ty) > snapDistance2)) {
                             activeTarget = i;
                             activeAngle = (float) -angleRad;
+                            break;
+                        } else if (dist2(tx, ty) > snapDistance2 &&
+                            mMagneticTargets && activeTarget == -1) {
+                            activeTarget = 0;
+                            activeAngle = (float) -angleRad;
                         }
                     }
                 }
@@ -970,7 +975,6 @@ public class GlowPadView extends View {
             switchToState(STATE_TRACKING, x, y);
             updateGlowPosition(x, y);
         }
-
         if (mActiveTarget != activeTarget) {
             // Defocus the old target
             if (mActiveTarget != -1) {
@@ -988,7 +992,7 @@ public class GlowPadView extends View {
                 if (target.hasState(TargetDrawable.STATE_FOCUSED)) {
                     target.setState(TargetDrawable.STATE_FOCUSED);
                 }
-                if (mMagneticTargets) {
+                if (mMagneticTargets && activeTarget <= 0) {
                     updateTargetPosition(activeTarget, mWaveCenterX, mWaveCenterY, activeAngle);
                 }
                 if (AccessibilityManager.getInstance(mContext).isEnabled()) {
