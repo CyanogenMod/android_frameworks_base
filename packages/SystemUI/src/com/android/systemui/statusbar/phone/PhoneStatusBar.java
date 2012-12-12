e2a2d16bd5fea2023f8c77b70514fc8b94c252dc
DvTonder <david.vantonder@gmail.com>
2012-12-11 21:59:59 -0500

    SystemUI: Tweak number of notifications detection method
    Change-Id: Ibd46fa5039e1d3062dcdb8f3c4dbd9ec1bdc73b0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2617,8 +2617,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
 
         int N = mNotificationData.size();
-        int thisUsersNotifications = 0;
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             Entry ent = mNotificationData.get(N-i-1);
             if(ent != null
                     && ent.notification != null
@@ -2628,14 +2627,12 @@ public class PhoneStatusBar extends BaseStatusBar {
                     case com.android.internal.R.drawable.stat_sys_adb:
                         continue;
                 }
-                thisUsersNotifications++;
+                // We have at least one notification, we cannot skip
+                return false;
             }
         }
-        if(thisUsersNotifications == 0) {
-            return true;
-        }
-
-        return false;
+        // No notifications for current user, lets skip to Settings panel
+        return true;
     }
 
     private static class FastColorDrawable extends Drawable {
