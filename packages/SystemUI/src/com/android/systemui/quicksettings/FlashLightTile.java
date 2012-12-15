83734e97e5b0eecf5a26b3def166cb4397085512
Danesh Mondegarian <daneshm90@gmail.com>
2012-12-14 20:31:46 -0500

    QuickSettings : Add torch tile
    Patchset 2 : New torch drawables (Björn Lundén)
    
    Change-Id: I0831266d503195ee249a85d0d728bdae321e56cd

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/FlashLightTile.java
@@ -0,0 +1,66 @@
+package com.android.systemui.quicksettings;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+
+public class FlashLightTile extends QuickSettingsTile {
+
+    public FlashLightTile(Context context, LayoutInflater inflater,
+            QuickSettingsContainerView container,
+            QuickSettingsController qsc, Handler handler) {
+        super(context, inflater, container, qsc);
+        TorchObserver observer = new TorchObserver(handler);
+        observer.startObserving();
+        updateTileState();
+
+        onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT");
+                mContext.sendBroadcast(i);
+            }
+        };
+    }
+
+    private void updateTileState() {
+        boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.TORCH_STATE, 0) == 1;
+        if(enabled) {
+            mDrawable = R.drawable.ic_qs_flashlight_on;
+        } else {
+            mDrawable = R.drawable.ic_qs_flashlight_off;
+        }
+    }
+
+    private class TorchObserver extends ContentObserver {
+        public TorchObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateTileState();
+            updateQuickSettings();
+        }
+
+        public void startObserving() {
+            final ContentResolver cr = mContext.getContentResolver();
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.TORCH_STATE), false, this);
+        }
+    }
+
+}
