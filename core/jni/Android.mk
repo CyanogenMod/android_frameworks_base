56940543ade0cb22d6e7447527ca0a4b7d69cf31
Nagender Telkar <ntelkar@codeaurora.org>
2012-12-17 10:03:31 -0800

    build: Update Android_runtime module for all targets
    Enable com_android_internal_app_ActivityTrigger for all targets
    
    Change-Id: Ia7c7f9014a5710b0ae8a72c0c2c5443d08a4c1f5
    (cherry picked from commit f0a3a21bffeff91124e29f4f810ae7b887bf3260)
    (cherry picked from commit 0d89b4e01396c399903911d6e694b6ea29c75311)

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -149,11 +149,9 @@ LOCAL_SRC_FILES:= \
 	android_content_res_ObbScanner.cpp \
 	android_content_res_Configuration.cpp \
 	android_animation_PropertyValuesHolder.cpp \
-	com_android_internal_net_NetworkStatsFactory.cpp
-        ifeq ($(call is-vendor-board-platform,QCOM),true)
-        LOCAL_SRC_FILES += com_android_internal_app_ActivityTrigger.cpp
-        LOCAL_SRC_FILES += org_codeaurora_Performance.cpp
-        endif
+	com_android_internal_net_NetworkStatsFactory.cpp \
+        com_android_internal_app_ActivityTrigger.cpp \
+        org_codeaurora_Performance.cpp
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
