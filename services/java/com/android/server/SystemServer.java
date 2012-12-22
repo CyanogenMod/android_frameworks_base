3c5c44878699ec5af4edafc878634a62b5e97200
Alexander Ivanov <caivano@codeaurora.org>
2012-12-21 19:24:55 -0800

    server: start cne service
    Connectivity Engine (CnE) provides advanced network selection per socket,
    reduced signalling traffic, and increased power savings.
    The service implements the interface defined by CNEManager which Android
    applications may use.
    
    Change-Id: I9ffd1f5d60f5e9e185ab482a35b3b07f29a4124c
    (cherry picked from commit 3a4e3ba1bc67c460ec3c2fd4ef169ae25e3e69b1)
    (cherry picked from commit 1bb0036652318ac8bbecf3e6c810bbc779387c61)
    (cherry picked from commit 3c8af4a2546d9b71d113072d680107cb56651e71)
    (cherry picked from commit 861a4b062196020bf24a5b3731c717b5b95a7d77)
    (cherry picked from commit e3dba9799892f13c5bc2376b81cf5dceae17492c)
    
    Add Connectivity Engine (CnE) WQE support
    
    Enhanced connectivity features viz. Wifi Quality Estimation.
    Supports dual network mode - wifi & mobile simultaneously.
    Use QcConnectivityService when CnE is enabled for all CnE features.
    
    Change-Id: I14b02e21515bfc1b5fd2fc4f28e5190fa80f83f8
    
    server: Initialize ConnectivityService on low memory device
    
    Skip initialization of QcConnectivityService and
    use ConnectivityService.java for a low memory device
    
    CRs-Fixed: 531430
    
    Change-Id: I2111ec32f6e40ca79b7fc91ec4cef97ac27f3b3a
    
    Update CNE feature values.
    
    Start QcConnectivityServices for the new
    application traffic profiling feature too.
    
    Change-Id: I52ae76420ebd098d43fe0663f41ce622a407b90e
    
    connectivity: allow inheritance to subclass
    
    set modifier to protected in order to allow
    subclass to use the same data and avoid redefinition.
    
    Change-Id: I2dc688d3e433775cb073ec6faae3981807212cc9
    
    Change visibility of methods
    
    Change the visibility of some methods so that
    they are accessible to the child class.
    
    Change-Id: Ie1a45978b42742cac0aaedba29fb2375882c0ae4
    CRs-Fixed: 611347
    
    Conflicts:
    	services/java/com/android/server/SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
  * This code has been modified.  Portions copyright (C) 2010, T-Mobile USA, Inc.
+ * Copyright (c) 2012, 2013, 2014. The Linux Foundation. All rights reserved.
+ * Not a Contribution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,9 +34,13 @@ import android.database.Cursor;
 import android.media.AudioService;
 import android.net.wifi.p2p.WifiP2pService;
 import android.os.Environment;
+import android.net.INetworkPolicyManager;
+import android.net.INetworkStatsService;
+import android.os.IBinder;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.INetworkManagementService;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.StrictMode;
@@ -52,6 +58,7 @@ import android.view.WindowManager;
 import com.android.internal.R;
 import com.android.internal.os.BinderInternal;
 import com.android.internal.os.SamplingProfilerIntegration;
+import com.android.internal.util.MemInfoReader;
 import com.android.server.accessibility.AccessibilityManagerService;
 import com.android.server.accounts.AccountManagerService;
 import com.android.server.am.ActivityManagerService;
@@ -86,6 +93,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import dalvik.system.PathClassLoader;
+import java.lang.reflect.Constructor;
 
 class ServerThread {
     private static final String TAG = "SystemServer";
@@ -161,6 +171,7 @@ class ServerThread {
         NetworkStatsService networkStats = null;
         NetworkPolicyManagerService networkPolicy = null;
         ConnectivityService connectivity = null;
+        Object qcCon = null;
         WifiP2pService wifiP2p = null;
         WifiService wifi = null;
         NsdService serviceDiscovery= null;
@@ -541,19 +552,48 @@ class ServerThread {
                     reportWtf("starting Wi-Fi Service", e);
                 }
 
-                try {
-                    Slog.i(TAG, "Connectivity Service");
-                    connectivity = new ConnectivityService(
-                            context, networkManagement, networkStats, networkPolicy);
-                    ServiceManager.addService(Context.CONNECTIVITY_SERVICE, connectivity);
-                    networkStats.bindConnectivityManager(connectivity);
-                    networkPolicy.bindConnectivityManager(connectivity);
+               try {
+                   int enableCne = 1;
+                   if (!deviceHasSufficientMemory()) {
+                       enableCne = SystemProperties.getInt("persist.cne.override.memlimit", 0);
+                   }
+                   int cneFeature = (enableCne == 1) ?
+                       SystemProperties.getInt("persist.cne.feature", 0) : 0;
 
-                    wifiP2p.connectivityServiceReady();
-                    wifi.checkAndStartWifi();
-                } catch (Throwable e) {
-                    reportWtf("starting Connectivity Service", e);
-                }
+                   try {
+                       if ( cneFeature > 0 && cneFeature < 10 ) {
+                           Slog.i(TAG, "QcConnectivity Service");
+                           PathClassLoader qcsClassLoader =
+                               new PathClassLoader("/system/framework/services-ext.jar",
+                                       ClassLoader.getSystemClassLoader());
+                           Class qcsClass =
+                               qcsClassLoader.loadClass("com.android.server.QcConnectivityService");
+                           Constructor qcsConstructor = qcsClass.getConstructor
+                               (new Class[] {Context.class, INetworkManagementService.class,
+                                INetworkStatsService.class, INetworkPolicyManager.class});
+                           qcCon = qcsConstructor.newInstance(
+                                   context, networkManagement, networkStats, networkPolicy);
+                           connectivity = (ConnectivityService) qcCon;
+                       } else {
+                           Slog.i(TAG, "Connectivity Service");
+                           connectivity = new ConnectivityService( context, networkManagement,
+                                   networkStats, networkPolicy);
+                       }
+                   } catch (Throwable e) {
+                       connectivity = new ConnectivityService( context, networkManagement,
+                               networkStats, networkPolicy);
+                   }
+
+                   if (connectivity != null) {
+                       ServiceManager.addService(Context.CONNECTIVITY_SERVICE, connectivity);
+                       networkStats.bindConnectivityManager(connectivity);
+                       networkPolicy.bindConnectivityManager(connectivity);
+                       wifi.checkAndStartWifi();
+                       wifiP2p.connectivityServiceReady();
+                   }
+               } catch (Throwable e) {
+                   reportWtf("starting Connectivity Service", e);
+               }
 
                 try {
                     Slog.i(TAG, "Network Service Discovery Service");
@@ -819,7 +859,7 @@ class ServerThread {
                 }
             }
 
-            if (!disableNonCoreServices && 
+            if (!disableNonCoreServices &&
                 context.getResources().getBoolean(R.bool.config_dreamsSupported)) {
                 try {
                     Slog.i(TAG, "Dreams Service");
@@ -1236,6 +1276,17 @@ class ServerThread {
                     "com.android.systemui.SystemUIService"));
         //Slog.d(TAG, "Starting service: " + intent);
         context.startServiceAsUser(intent, UserHandle.OWNER);
+    }
+
+    private static final boolean deviceHasSufficientMemory() {
+        final long MEMORY_SIZE_MIN = 512 * 1024 * 1024;
+
+        MemInfoReader minfo = new MemInfoReader();
+        minfo.readMemInfo();
+        if (minfo.getTotalSize() <= MEMORY_SIZE_MIN) {
+            return false;
+        }
+        return true;
     }
 }
 
