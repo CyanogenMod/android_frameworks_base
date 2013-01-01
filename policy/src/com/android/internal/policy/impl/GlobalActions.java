a76937e7ea9e5f9e4b917c85a4966ebe08bf6ae7
Danesh Mondegarian <daneshm90@gmail.com>
2013-01-01 18:07:11 -0500

    Screenshot : Pass current user
    Needed to complete the snapshot procedure
    
    Change-Id: I40f63e261e213136cd739c06da6f005173427ae0

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -636,7 +636,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 @Override
                 public void onServiceDisconnected(ComponentName name) {}
             };
-            if (mContext.bindService(intent, conn, Context.BIND_AUTO_CREATE)) {
+            if (mContext.bindService(intent, conn, Context.BIND_AUTO_CREATE, UserHandle.USER_CURRENT)) {
                 mScreenshotConnection = conn;
                 mHandler.postDelayed(mScreenshotTimeout, 10000);
             }
