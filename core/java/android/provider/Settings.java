4705f043ef5bebf8dd7d7da16a84f0f36aefa7e8
kristof.petho <kristof.petho@gmail.com>
2013-01-06 09:56:54 +0100

    [WIP] Stylus gestures 1/2
    Ported from CM10.
    
    Requires /Stylus gestures 2/2/
    
    Change-Id: I95d2f5923320f351b83fc03cfe18335986095eba

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2901,12 +2901,70 @@ public final class Settings {
          * Action for long-pressing menu button on lock screen
          * @hide
          */
+
         public static final String LOCKSCREEN_LONG_MENU_ACTION = "lockscreen_long_menu_action";
+
+        /**
+         * Enable Stylus Gestures
+         *
+         * @hide
+         */
+
+        public static final String ENABLE_STYLUS_GESTURES = "enable_stylus_gestures";
+
+        /**
+         * Left Swipe Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_LEFT_SWIPE = "gestures_left_swipe";
+
+        /**
+         * Right Swipe Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_RIGHT_SWIPE = "gestures_right_swipe";
+
+        /**
+         * Up Swipe Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_UP_SWIPE = "gestures_up_swipe";
+
+        /**
+         * down Swipe Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_DOWN_SWIPE = "gestures_down_swipe";
+
+        /**
+         * Long press Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_LONG_PRESS = "gestures_long_press";
+
+        /**
+         * double tap Action
+         *
+         * @hide
+         */
+
+        public static final String GESTURES_DOUBLE_TAP = "gestures_double_tap";
 
          /**
           * Always show the battery status on the lockscreen
           * @hide
           */
+
         public static final String LOCKSCREEN_ALWAYS_SHOW_BATTERY = "lockscreen_always_show_battery";
 
         /**
