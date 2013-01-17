320ef60391fe2a7ac141bee44b04517abcb09c7e
Manu Gautam <mgautam@codeaurora.org>
2013-01-17 12:13:16 +0530

    UsbManager: Increase delay for USB DISCONNECT event broadcast
    When a function (e.g. MTP/RNDIS/ADB) is enabled USB re-enumeration
    happens which results in USB disconnect followed by USB connect.
    UsbDeviceManager delays the USB disconnect event notification
    so that clients wouldn't treat it as if USB cable is really removed.
    
    Increase this delay to 5 seconds, as on some HOST (mainly WinXP) it
    has been observed that re-enumeration takes longer time.
    Also, for actual cable disconnect make sure that DISCONNECT is
    broadcast immediately.
    
    CRs-Fixed: 438139
    Change-Id: I589e5014e7583b24c6e16f91013f7d67c55a69c0

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -96,7 +96,9 @@ public class UsbDeviceManager {
     // Delay for debouncing USB disconnects.
     // We often get rapid connect/disconnect events when enabling USB functions,
     // which need debouncing.
-    private static final int UPDATE_DELAY = 1000;
+    private static final int UPDATE_DELAY = 5000;
+    // Function enable is in progress
+    private boolean mSoftSwitch;
 
     private static final String BOOT_MODE_PROPERTY = "ro.bootmode";
 
@@ -441,6 +443,7 @@ public class UsbDeviceManager {
                 connected = 1;
                 configured = 0;
             } else if ("CONFIGURED".equals(state)) {
+                mSoftSwitch = false;
                 connected = 1;
                 configured = 1;
             } else {
@@ -452,7 +455,7 @@ public class UsbDeviceManager {
             msg.arg1 = connected;
             msg.arg2 = configured;
             // debounce disconnects to avoid problems bringing up USB tethering
-            sendMessageDelayed(msg, (connected == 0) ? UPDATE_DELAY : 0);
+            sendMessageDelayed(msg, ((connected == 0) && mSoftSwitch) ? UPDATE_DELAY : 0);
         }
 
         private boolean waitForState(String state) {
@@ -500,6 +503,7 @@ public class UsbDeviceManager {
                     functions = removeFunction(functions, UsbManager.USB_FUNCTION_ADB);
                 }
                 if (!mDefaultFunctions.equals(functions)) {
+                    mSoftSwitch = true;
                     if (!setUsbConfig("none")) {
                         Slog.e(TAG, "Failed to disable USB");
                         // revert to previous configuration if we fail
@@ -532,6 +536,7 @@ public class UsbDeviceManager {
                     functions = removeFunction(functions, UsbManager.USB_FUNCTION_ADB);
                 }
                 if (!mCurrentFunctions.equals(functions)) {
+                    mSoftSwitch = true;
                     if (!setUsbConfig("none")) {
                         Slog.e(TAG, "Failed to disable USB");
                         // revert to previous configuration if we fail
