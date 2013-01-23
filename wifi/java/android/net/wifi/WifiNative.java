056ce7dab71a2157ba2c928d86347ae65f0716d8
Jorge Ruesga <jorge@ruesga.com>
2013-01-23 01:48:37 +0100

    Wi-Fi: Pass default country code to driver
    This patch reverts partially the change http://review.cyanogenmod.org/#/c/28918/8, and
    passing the default country code ("GB") to the driver instead of null.
    
    Change-Id: I7d7115155a0a52261931ffaddaf35f316d095800
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -360,10 +360,6 @@ public class WifiNative {
     }
 
     public boolean setCountryCode(String countryCode) {
-        if (countryCode == null) {
-            // Ping the driver
-            return doBooleanCommand("DRIVER COUNTRY");
-        }
         return doBooleanCommand("DRIVER COUNTRY " + countryCode);
     }
 
