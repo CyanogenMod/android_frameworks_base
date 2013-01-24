dbb1f9b5a2184c23265d77bec41ac94e0bee2824
Jorge Ruesga <jorge@ruesga.com>
2013-01-24 02:34:49 +0100

    DeviceKeyHandle: The device should consume only known keys
    When the device receive the key, only should consume it if the device
    know about the key. Otherwise, the key must be handle by the active app.
    Also make mDeviceKeyHandler private (is not useful outside this class)
    
    Change-Id: I4b9ea57b802e8c8c88c8b93a63d510f5952b0700
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -259,7 +259,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 KeyEvent.KEYCODE_CALCULATOR, Intent.CATEGORY_APP_CALCULATOR);
     }
 
-    DeviceKeyHandler mDeviceKeyHandler;
+    private DeviceKeyHandler mDeviceKeyHandler;
 
     /**
      * Lock protecting internal state.  Must not call out into window
@@ -1222,9 +1222,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 Constructor<?> constructor = klass.getConstructor(Context.class);
                 mDeviceKeyHandler = (DeviceKeyHandler) constructor.newInstance(
                         mContext);
-                Slog.d(TAG, "Device key handler loaded");
+                if(DEBUG) Slog.d(TAG, "Device key handler loaded");
             } catch (Exception e) {
-                Slog.d(TAG, "Could not instantiate device key handler "
+                Slog.w(TAG, "Could not instantiate device key handler "
                         + deviceKeyHandlerClass + " from class "
                         + deviceKeyHandlerLib, e);
             }
@@ -2527,11 +2527,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return -1;
         }
 
+        // Specific device key handling
         if (mDeviceKeyHandler != null) {
             try {
-                return mDeviceKeyHandler.handleKeyEvent(event);
+                // The device only should consume known keys.
+                if (mDeviceKeyHandler.handleKeyEvent(event)) {
+                    return -1;
+                }
             } catch (Exception e) {
-                Slog.d(TAG, "Could not dispatch event to device key handler", e);
+                Slog.w(TAG, "Could not dispatch event to device key handler", e);
             }
         }
 
