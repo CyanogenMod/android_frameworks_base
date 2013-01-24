e721132bfffb039d56b6db03ac5ad9729f809b32
Sangkyu Lee <sk82.lee@lge.com>
2013-01-24 12:08:57 +0900

    Remove duplicate code in Editor.java
    Eliminate duplicates by calling resumeBlink()/suspendBlink().
    
    Change-Id: Ia19ae1bf1a5c981700aa20be67ed9d557e2bfc56
    Signed-off-by: Sangkyu Lee <sk82.lee@lge.com>

--- core/java/android/widget/Editor.java
+++ core/java/android/widget/Editor.java
@@ -1009,14 +1009,9 @@ public class Editor {
 
     void onWindowFocusChanged(boolean hasWindowFocus) {
         if (hasWindowFocus) {
-            if (mBlink != null) {
-                mBlink.uncancel();
-                makeBlink();
-            }
+            resumeBlink();
         } else {
-            if (mBlink != null) {
-                mBlink.cancel();
-            }
+            suspendBlink();
             if (mInputContentType != null) {
                 mInputContentType.enterDown = false;
             }
