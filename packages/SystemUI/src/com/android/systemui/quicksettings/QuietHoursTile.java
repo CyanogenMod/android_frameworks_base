eeb11165b6e5443be802b475017cb7aca9d6f213
Danesh Mondegarian <daneshm90@gmail.com>
2013-02-02 00:01:50 -0500

    QuietHours Tile (1/2)
    Allows user to toggle state of quiet hours
    
    Part 2 : http://review.cyanogenmod.org/#/c/31302/
    
    Patchset 2 : New drawables courtesy of Björn Lundén
             3 : Make multiuser compatible
    
    Change-Id: I10f7b7befbf7405e47247d90bfee0f30c3ecd7c9

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
@@ -0,0 +1,60 @@
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+
+public class QuietHoursTile extends QuickSettingsTile {
+
+    private boolean mEnabled;
+
+    public QuietHoursTile(Context context, LayoutInflater inflater,
+            QuickSettingsContainerView container, QuickSettingsController qsc) {
+        super(context, inflater, container, qsc);
+        updateTileState();
+        mOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_ENABLED,
+                        mEnabled ? 0 : 1, UserHandle.USER_CURRENT);
+            }
+        };
+        mOnLongClick = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.android.settings", "com.android.settings.Settings$QuietHoursSettingsActivity");
+                startSettingsActivity(intent);
+                return true;
+            }
+        };
+        qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_ENABLED), this);
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateTileState();
+        updateQuickSettings();
+    }
+
+    private void updateTileState() {
+        mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        mLabel = mContext.getString(R.string.quick_settings_quiethours);
+        if (mEnabled) {
+            mDrawable = R.drawable.ic_qs_quiet_hours_on;
+        } else {
+            mDrawable = R.drawable.ic_qs_quiet_hours_off;
+        }
+    }
+
+}
