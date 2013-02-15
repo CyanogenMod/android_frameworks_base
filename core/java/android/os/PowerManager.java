640a0d3c41775d4eeffc1a451eedd541249e625a
Danny Baumann <dannybaumann@web.de>
2013-02-15 09:24:06 +0100

    Add method to PowerManager that allows querying the lowest possible brightness.
    Used by the auto-brightness adjustment to adjust the sliders.
    
    Change-Id: Idb58fd86dee1a053fe858291be6c9b3169ac4247

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -335,6 +335,19 @@ public final class PowerManager {
     }
 
     /**
+     * Gets the minimum screen brightness.
+     * This is the lowest possible screen brightness; the screen will
+     * never become dimmer than that.
+     * @hide
+     */
+    public int getMinimumAbsoluteScreenBrightness() {
+        int minSetting = getMinimumScreenBrightnessSetting();
+        int dimSetting = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_screenBrightnessDim);
+        return Math.min(minSetting, dimSetting);
+    }
+
+    /**
      * Returns true if the screen auto-brightness adjustment setting should
      * be available in the UI.  This setting is experimental and disabled by default.
      * @hide
