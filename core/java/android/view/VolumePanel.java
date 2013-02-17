774e3f96164fdcd3e16bac13b73143faf04a809a
StevenHarperUK <stevenharperuk@gmail.com>
2013-02-17 13:22:42 +0000

    Feature : Volume adjust sound preference 1/2
    This patch allows the users to choose if they want the audible tone when adjusting the volume
    
    The new preference is in
    Settings | Sound
        Volume adjust sound
            Play sound when adjusting volume with hard keys
    The Checkbox is defaulted to ON (1 in defaults.xml)
    
    Look for 2/2 for the Settings, Sound Panel Change
    
    Change-Id: I705c23065706317760ba60efc1a71f3ba6082bbd

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -825,6 +825,12 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
 
     protected void onPlaySound(int streamType, int flags) {
 
+        // If preference is no sound - just exit here
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                 Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 0) {
+             return;
+         }
+
         if (hasMessages(MSG_STOP_SOUNDS)) {
             removeMessages(MSG_STOP_SOUNDS);
             // Force stop right now
