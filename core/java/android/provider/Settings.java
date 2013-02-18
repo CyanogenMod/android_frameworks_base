0247d4f4e4390ef62fe0ccb5103e9e622c17aa0e
DvTonder <david.vantonder@gmail.com>
2013-02-18 09:32:02 -0500

    Framework: Add Advanced reboot (2 of 2)
    This commit responds to a setting in Development settings for including
    options in the power menu for rebooting into recovery or bootloader. It
    is defauled to off.
    
    When enabled, the Advanced reboot options will only be available once the
    device is unlocked.
    
    Change-Id: I675f31de1f82c344c7923e953fd4a6e26dd036da

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4859,6 +4859,12 @@ public final class Settings {
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or bootloader
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4900,7 +4906,8 @@ public final class Settings {
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
             LOCK_SCREEN_OWNER_INFO,
-            LOCK_SCREEN_OWNER_INFO_ENABLED
+            LOCK_SCREEN_OWNER_INFO_ENABLED,
+            ADVANCED_REBOOT
         };
 
         /**
