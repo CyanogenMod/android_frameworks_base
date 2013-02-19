6bfba7a929b22391205e8929bbc4573075feafc5
sbrissen <sbrissen@hotmail.com>
2013-02-19 11:09:42 -0500

    Workaround until gps can be fixed, force gps to stay in standalone mode when ro.agps.allow=false
    tested and working on t0ltecdma devices
    
    Change-Id: I917de8cf3c59865a423dafa605ee4f10a5838bb6

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -45,6 +45,7 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.WorkSource;
 import android.provider.Settings;
@@ -951,9 +952,10 @@ public class GpsLocationProvider implements LocationProviderInterface {
             mLastFixTime = 0;
             mStarted = true;
             mPositionMode = GPS_POSITION_MODE_STANDALONE;
+            boolean mAGPSAllow = SystemProperties.getBoolean("ro.agps.allow",true);
 
              if (Settings.Global.getInt(mContext.getContentResolver(),
-                    Settings.Global.ASSISTED_GPS_ENABLED, 1) != 0) {
+                    Settings.Global.ASSISTED_GPS_ENABLED, 1) != 0 && mAGPSAllow) {
                 if (hasCapability(GPS_CAPABILITY_MSB)) {
                     mPositionMode = GPS_POSITION_MODE_MS_BASED;
                 }
