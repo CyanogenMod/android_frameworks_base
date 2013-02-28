d2d1faf00ab033c2c09c38af6a98686f44bc2d84
DvTonder <david.vantonder@gmail.com>
2013-02-27 22:15:56 -0500

    QuickSettings: Add LTE Tile (1 of 2)
    Thank you to Bjorn Lunden for the drawables
    
    Change-Id: Ia25d098020586e06d38589f84d3d1a581e7cbd9a

--- core/java/com/android/internal/util/cm/QSUtils.java
+++ core/java/com/android/internal/util/cm/QSUtils.java
@@ -9,6 +9,9 @@ import android.hardware.display.WifiDisplayStatus;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
+
+import com.android.internal.telephony.PhoneConstants;
 
 public class QSUtils {
         public static boolean deviceSupportsUsbTether(Context ctx) {
@@ -36,5 +39,10 @@ public class QSUtils {
 
         public static boolean deviceSupportsNfc(Context ctx) {
             return NfcAdapter.getDefaultAdapter(ctx) != null;
+        }
+
+        public static boolean deviceSupportsLte(Context ctx) {
+            final TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+            return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) || tm.getLteOnGsmMode() != 0;
         }
 }
