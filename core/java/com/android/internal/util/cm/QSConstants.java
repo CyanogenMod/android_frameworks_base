d2b7a0e0b0c5a6fb350214858a9f2468fd3b9e60
stofstik <verstegen.daan@gmail.com>
2013-03-10 15:16:48 +0100

    Added Volume Tile to Quicksettings
    Drawable by blunden
    
    Change-Id: I04451d8b4669ae0d4f798010a4c61e5e82990b76

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -27,6 +27,7 @@ public class QSConstants {
         public static final String TILE_NFC = "toggleNfc";
         public static final String TILE_USBTETHER = "toggleUsbTether";
         public static final String TILE_QUIETHOURS = "toggleQuietHours";
+        public static final String TILE_VOLUME = "toggleVolume";
 
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
