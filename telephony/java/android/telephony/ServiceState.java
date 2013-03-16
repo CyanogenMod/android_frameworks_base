23e043aca9ae7be5057b537f7590e1523c65b7ff
codeworkx <codeworkx@cyanogenmod.org>
2013-03-16 00:35:47 +0000

    ServiceState: add support for DCHSPAP
    Change-Id: Ib0a328652c14580718400b00146b770481be5458

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -100,6 +100,8 @@ public class ServiceState implements Parcelable {
     public static final int RIL_RADIO_TECHNOLOGY_LTE = 14;
     /** @hide */
     public static final int RIL_RADIO_TECHNOLOGY_HSPAP = 15;
+    /** @hide */
+    public static final int RIL_RADIO_TECHNOLOGY_DCHSPAP = 30;
     /**
      * GSM radio technology only supports voice. It does not support data.
      * @hide
@@ -453,6 +455,7 @@ public class ServiceState implements Parcelable {
                 rtString = "LTE";
                 break;
             case RIL_RADIO_TECHNOLOGY_HSPAP:
+            case RIL_RADIO_TECHNOLOGY_DCHSPAP:
                 rtString = "HSPAP";
                 break;
             case RIL_RADIO_TECHNOLOGY_GSM:
@@ -686,6 +689,7 @@ public class ServiceState implements Parcelable {
         case ServiceState.RIL_RADIO_TECHNOLOGY_LTE:
             return TelephonyManager.NETWORK_TYPE_LTE;
         case ServiceState.RIL_RADIO_TECHNOLOGY_HSPAP:
+        case ServiceState.RIL_RADIO_TECHNOLOGY_DCHSPAP:
             return TelephonyManager.NETWORK_TYPE_HSPAP;
         default:
             return TelephonyManager.NETWORK_TYPE_UNKNOWN;
@@ -717,6 +721,7 @@ public class ServiceState implements Parcelable {
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPA
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_LTE
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPAP
+                || radioTechnology == RIL_RADIO_TECHNOLOGY_DCHSPAP
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_GSM;
     }
 
