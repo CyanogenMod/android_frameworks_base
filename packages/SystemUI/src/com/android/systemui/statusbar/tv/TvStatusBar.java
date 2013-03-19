9db52cd1ea44e38d9dfc664da98cb8aa192174a0
Jens Doll <jens.doll@gmail.com>
2013-03-19 23:55:33 +0100

    Added support for multiple navigation key bars
    System UI was build with the assumption that there is only one
    navigation bar at a time, and that this bar is managed by
    Phone UI, Tablet UI or TV UI.
    
    This commit adds simple call back facilities at BaseStatusBar
    for both, navigation key hints and menu key visibility change
    events. These call backs are wired up in Phone UI, Tablet UI
    and the TV UI stub.
    
    This allows us to have more than on navigation key bar
    in the UI and define those navigation bars directly
    in BaseStatusBar, where it is available for all UIs.
    
    Patch Set 2:
    * CyanogenMod Code Style
    * Added documentation.
    * One interface for all
    * Added setDisabledFlags() to the list
    
    Change-Id: Id4de440e21c2f5c78b5e185030f9c5e0f7e6bef7

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -58,6 +58,7 @@ public class TvStatusBar extends BaseStatusBar {
 
     @Override
     public void disable(int state) {
+        propagateDisabledFlags(state);
     }
 
     @Override
@@ -74,6 +75,7 @@ public class TvStatusBar extends BaseStatusBar {
 
     @Override
     public void topAppWindowChanged(boolean visible) {
+        propagateMenuVisibility(visible);
     }
 
     @Override
@@ -90,6 +92,7 @@ public class TvStatusBar extends BaseStatusBar {
 
     @Override // CommandQueue
     public void setNavigationIconHints(int hints) {
+        propagateNavigationIconHints(hints);
     }
 
     @Override
