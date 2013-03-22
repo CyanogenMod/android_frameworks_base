dbd9e0a09dcb6afec413eaf52103cbb85bbce361
Jens Doll <jens.doll@gmail.com>
2013-03-22 17:11:10 +0100

    Add pie control optimizations for Phone UI.
    This commit brings some pie control tweaks to the Phone UI. These
    tweaks make the pie controls react to several conditions that are
    are special to the Phone UI. That means that this piece of code
    is more deeply embedded within the SystemUI code.
    
    This mostly adds the foloowing behaviour, that is Phone UI
    dependent:
    * Disable BOTTOM trigger when notification area is expanded
    * Disable LEFT and RIGHT trigger when a keyguard is active
    * Enable search light feature
    
    Patch Set 3:
    * CyanogenMod Code Style.
    * setDisabledFlags now belongs to navigation bar callbacks
    
    Patch Set 5:
    * Removed duplicated .observe()
    
    Patch Set 10:
    * Fixed unintended trigger activation
    
    Change-Id: I551745b69791adca75190c54282be19d85db75a9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -27,6 +27,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.PieController.Position;
 
 public class PhoneStatusBarView extends PanelBar {
     private static final String TAG = "PhoneStatusBarView";
@@ -164,6 +165,12 @@ public class PhoneStatusBarView extends PanelBar {
         mBar.makeExpandedInvisibleSoon();
         mFadingPanel = null;
         mLastFullyOpenedPanel = null;
+
+        // show up you pie controls
+        mBar.updatePieTriggerMask(Position.LEFT.FLAG
+                | Position.TOP.FLAG
+                | Position.RIGHT.FLAG
+                | Position.TOP.FLAG);
     }
 
     @Override
@@ -172,6 +179,14 @@ public class PhoneStatusBarView extends PanelBar {
         if (openPanel != mLastFullyOpenedPanel) {
             openPanel.sendAccessibilityEvent(AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
         }
+
+        // back off you pie controls!
+        if (mShouldFade) {
+            mBar.updatePieTriggerMask(Position.LEFT.FLAG
+                    | Position.RIGHT.FLAG
+                    | Position.TOP.FLAG);
+        }
+
         mFadingPanel = openPanel;
         mLastFullyOpenedPanel = openPanel;
         mShouldFade = true; // now you own the fade, mister
