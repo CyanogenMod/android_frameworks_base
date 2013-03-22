9f94d8defa183a987749a1528c69de3893928ee2
Bruno Randolf <br1@einfach.org>
2013-03-22 17:12:17 +0000

    Wifi: Get the list of supported channels
    Get the list of supported channels/frequencies from wpa_supplicant and provide
    them thru WifiManager.getSupportedChannels();
    
    One channel is represented thru the class WifiChannel which includes channel
    number, frequency in MHz and a flag if IBSS mode is allowed on this channel.
    
    Like isIbssSupported() we can only expect a result after WIFI_STATE_ENABLED has
    been reached.
    
    Change-Id: I9fc34642b9a0047f6871b180a3a7a69d47b7e2ff

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -151,6 +151,8 @@ public class WifiStateMachine extends StateMachine {
 
     private PowerManager.WakeLock mSuspendWakeLock;
 
+    private List<WifiChannel> mSupportedChannels;
+
     /**
      * Interval in milliseconds between polling for RSSI
      * and linkspeed information
@@ -372,6 +374,9 @@ public class WifiStateMachine extends StateMachine {
 
     /* Is IBSS mode supported by the driver? */
     public static final int CMD_GET_IBSS_SUPPORTED        = BASE + 134;
+
+    /* Get supported channels */
+    public static final int CMD_GET_SUPPORTED_CHANNELS    = BASE + 135;
 
     private static final int CONNECT_MODE   = 1;
     private static final int SCAN_ONLY_MODE = 2;
@@ -1097,6 +1102,13 @@ public class WifiStateMachine extends StateMachine {
     public int syncIsIbssSupported(AsyncChannel channel) {
         Message resultMsg = channel.sendMessageSynchronously(CMD_GET_IBSS_SUPPORTED);
         int result = resultMsg.arg1;
+        resultMsg.recycle();
+        return result;
+    }
+
+    public List<WifiChannel> syncGetSupportedChannels(AsyncChannel channel) {
+        Message resultMsg = channel.sendMessageSynchronously(CMD_GET_SUPPORTED_CHANNELS);
+        List<WifiChannel> result = (List<WifiChannel>) resultMsg.obj;
         resultMsg.recycle();
         return result;
     }
@@ -1950,6 +1962,9 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, FAILURE);
                     break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, (List<WifiChannel>) null);
+                    break;
                 case CMD_GET_CONFIGURED_NETWORKS:
                     replyToMessage(message, message.what, (List<WifiConfiguration>) null);
                     break;
@@ -2419,6 +2434,7 @@ public class WifiStateMachine extends StateMachine {
                     initializeWpsDetails();
 
                     mIbssSupported = mWifiNative.getModeCapability("IBSS");
+                    mSupportedChannels = mWifiNative.getSupportedChannels();
 
                     sendSupplicantConnectionChangedBroadcast(true);
                     transitionTo(mDriverStartedState);
@@ -2452,6 +2468,7 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_START_PACKET_FILTERING:
                 case CMD_STOP_PACKET_FILTERING:
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -2599,6 +2616,7 @@ public class WifiStateMachine extends StateMachine {
                     }
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
+                case CMD_GET_SUPPORTED_CHANNELS:
                     deferMessage(message);
                     break;
                 default:
@@ -2947,6 +2965,9 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, mIbssSupported ? 1 : 0);
+                    break;
+                case CMD_GET_SUPPORTED_CHANNELS:
+                    replyToMessage(message, message.what, mSupportedChannels);
                     break;
                 default:
                     return NOT_HANDLED;
