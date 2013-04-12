52bb1cb07e681c052c91179a2fa648fb747cf949
Eugene E. Kashpureff Jr <eugene@kashpureff.org>
2013-04-12 05:47:54 +0000

    UserManager: add support for bovid users
    The method isUserAGoat has historically assumed (naively) that the user
    is not bovid in nature. This patch adds support for dynamic goat entity
    detection.
    
    Credit to Chris Ennis for authorship and Nathan Grebowiec for testing
    and troubleshooting. Thanks to Evan Charlton and Danny Baumann for proof
    reading and style suggestions.
    
    Change-Id: I7adcf325a2af87afb48d3dc1532b225f5775bb11

--- core/java/android/os/UserManager.java
+++ core/java/android/os/UserManager.java
@@ -23,6 +23,7 @@ import android.content.pm.UserInfo;
 import android.graphics.Bitmap;
 import android.content.res.Resources;
 import android.util.Log;
+import android.provider.Settings;
 
 import java.util.List;
 
@@ -74,13 +75,20 @@ public class UserManager {
         }
     }
 
-   /**
+    /**
      * Used to determine whether the user making this call is subject to
      * teleportations.
      * @return whether the user making this call is a goat 
      */
     public boolean isUserAGoat() {
-        return false;
+        try {
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.CMSTATS_ANONYMOUS_OPT_IN) == 0) {
+                return true;
+            }
+        } catch(android.provider.Settings.SettingNotFoundException ex) {
+            return false;
+        }
     }
  
     /**
