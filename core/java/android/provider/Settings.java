4effd20262b417e462ae0810a71e574ab3a7b5e7
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-04-21 22:59:43 +0700

    Option to use volume keys to control media volume anytime (2/2)
    See Settings part for description
    
    Change-Id: I78d26b0bd41f5b58514ae9c68208a8c950396823

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3509,6 +3509,13 @@ public final class Settings {
         public static final String NONE_IS_SILENT = "none_is_silent";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         *
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
