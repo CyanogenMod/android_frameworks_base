f2287ebb646bad08e5651420900a056fb8e0a5b7
Steve Kondik <shade@chemlab.org>
2013-05-04 03:07:51 -0700

    Add support for sending raw commands
     * Certain camera drivers need magic commands to select special modes
       such as ZSL or HDR. Add support for sending raw commands from
       applications.
    
    Change-Id: I53a803456751982e407afe1738349b0aa981d3dc
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -1123,6 +1123,17 @@ static void android_hardware_Camera_sendVendorCommand(JNIEnv *env, jobject thiz,
     }
 }
 
+static void android_hardware_Camera_sendRawCommand(JNIEnv *env, jobject thiz, jint arg1, jint arg2, jint arg3)
+{
+    ALOGV("sendRawCommand %d, %d, %d", arg1, arg2, arg3);
+    sp<Camera> camera = get_native_camera(env, thiz, NULL);
+    if (camera == 0) return;
+
+    if (camera->sendCommand(arg1, arg2, arg3) != NO_ERROR) {
+        jniThrowRuntimeException(env, "send raw command failed");
+    }
+}
+
 //-------------------------------------------------
 
 static const JNINativeMethod camMethods[] = {
@@ -1225,6 +1236,9 @@ static const JNINativeMethod camMethods[] = {
   { "_sendVendorCommand",
     "(III)V",
     (void *)android_hardware_Camera_sendVendorCommand },
+  { "sendRawCommand",
+    "(III)V",
+    (void *)android_hardware_Camera_sendRawCommand},
 };
 
 struct field {
