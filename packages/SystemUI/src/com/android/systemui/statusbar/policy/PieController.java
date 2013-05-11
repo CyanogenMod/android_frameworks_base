a66f8caeb9a20aa3df90a4a32f9b095068b2449d
Lars Greiss <kufikugel@googlemail.com>
2013-05-11 21:50:34 +0200

    Framworks: disable observer and receiver if PIE is disabled
    on high end devices that is maybe not needed. But there
    is in general no need to enable the obeserver/receiver
    when PIE is disabled (or expanded mode is off)
    This change destroy and disables the PIE in most parts
    when it is disabled. + a small doubel register
    correction
    
    greetings SlimRoms
    
    PatchSet 2:
    - remove PieLayout observer completly - thanks Jens
    
    PatchSet 3:
    - remove unnecessary call to setupSnapPoints()
    - reuse already created snap points
    
    PatchSet 4:
    - small corrections on setupSnapPoints() and call
    - remove not needed second call of PieController attachTo()
    - disable TelephonyManager listener
    
    PatchSet 5:
    - fix up detach logic
    
    PatchSet 7:
    - cleanup
    
    PatchSet 8:
    - Effective Java rule #41
    - fixed some pie controller leak
    
    Change-Id: I8ef8797e7d4f8ee73d31a8296f5747415567bbda

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -105,7 +105,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
     private IWindowManager mWm;
     private int mBatteryLevel;
     private int mBatteryStatus;
-    private boolean mHasTelephony;
+    private TelephonyManager mTelephonyManager;
     private ServiceState mServiceState;
 
     // all pie slices that are managed by the controller
@@ -321,8 +321,10 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         mWm = IWindowManager.Stub.asInterface(ServiceManager.getService("window"));
 
-        final PackageManager pm = mContext.getPackageManager();
-        mHasTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+        if (mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            mTelephonyManager =
+                    (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        }
 
         final Resources res = mContext.getResources();
         Tracker.sDistance = res.getDimensionPixelSize(R.dimen.pie_trigger_distance);
@@ -331,11 +333,27 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         mBackAltIcon = res.getDrawable(R.drawable.ic_sysbar_back_ime);
     }
 
-    public void attachTo(BaseStatusBar statusBar) {
+    public void detachContainer() {
+        if (mPieContainer == null) {
+            return;
+        }
+
+        if (mTelephonyManager != null) {
+            mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+        }
+
+        mContext.unregisterReceiver(mBroadcastReceiver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+
+        mPieContainer.clearSlices();
+        mPieContainer = null;
+    }
+
+    public void attachStatusBar(BaseStatusBar statusBar) {
         mStatusBar = statusBar;
     }
 
-    public void attachTo(PieLayout container) {
+    public void attachContainer(PieLayout container) {
         mPieContainer = container;
         mPieContainer.clearSlices();
 
@@ -366,17 +384,13 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         // start listening for changes
         mSettingsObserver.observe();
 
-        mContext.registerReceiver(mBroadcastReceiver,
-                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         mContext.registerReceiver(mBroadcastReceiver, filter);
 
-        if (mHasTelephony) {
-            TelephonyManager telephonyManager =
-                    (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-            telephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
+        if (mTelephonyManager != null) {
+            mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
         }
     }
 
@@ -654,7 +668,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
     }
 
     public String getOperatorState() {
-        if (!mHasTelephony) {
+        if (mTelephonyManager == null) {
             return null;
         }
         if (mServiceState == null || mServiceState.getState() == ServiceState.STATE_OUT_OF_SERVICE) {
