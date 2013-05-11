6c0bfce4de14fe1b8eebc61fa406996690d75faf
Lars Greiss <kufikugel@googlemail.com>
2013-05-11 21:50:34 +0200

    Framworks: disable observer and receiver if PIE is disabled
    on high end devices that is maybe not needed. But there
    is in general no need to enable the obeserver/receiver
    when PIE is disabled (or expanded mode is off)
    This change destroy and disables the PIE in most parts
    when it is disabled. + a small doubel register
    correction
    
    greetings SlimRoms
    
    PatchSet 2:
    - remove PieLayout observer completly - thanks Jens
    
    PatchSet 3:
    - remove unnecessary call to setupSnapPoints()
    - reuse already created snap points
    
    PatchSet 4:
    - small corrections on setupSnapPoints() and call
    - remove not needed second call of PieController attachTo()
    - disable TelephonyManager listener
    
    PatchSet 5:
    - tired Lars changed the detach logic like Danny suggested :p
    
    Change-Id: I8ef8797e7d4f8ee73d31a8296f5747415567bbda

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -106,6 +106,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
     private int mBatteryLevel;
     private int mBatteryStatus;
     private boolean mHasTelephony;
+    private TelephonyManager mTelephonyManager;
     private ServiceState mServiceState;
 
     // all pie slices that are managed by the controller
@@ -331,6 +332,23 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         mBackAltIcon = res.getDrawable(R.drawable.ic_sysbar_back_ime);
     }
 
+    public void detachContainer() {
+        if (mPieContainer == null) {
+            return;
+        }
+
+        if (mTelephonyManager != null) {
+            mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+            mTelephonyManager = null;
+        }
+
+        mContext.unregisterReceiver(mBroadcastReceiver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
+
+        mPieContainer.clearSlices();
+        mPieContainer = null;
+    }
+
     public void attachTo(BaseStatusBar statusBar) {
         mStatusBar = statusBar;
     }
@@ -366,17 +384,15 @@ public class PieController implements BaseStatusBar.NavigationBarCallback,
         // start listening for changes
         mSettingsObserver.observe();
 
-        mContext.registerReceiver(mBroadcastReceiver,
-                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         mContext.registerReceiver(mBroadcastReceiver, filter);
 
         if (mHasTelephony) {
-            TelephonyManager telephonyManager =
+            mTelephonyManager =
                     (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-            telephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
+            mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
         }
     }
 
