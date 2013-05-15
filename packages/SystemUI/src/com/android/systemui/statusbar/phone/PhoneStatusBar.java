e155cb4dc471c494ba011708cbddc4d2cc802295
Danesh Mondegarian <daneshm90@gmail.com>
2013-05-14 22:09:27 -0400

    NavigationBar : Only inflate once
    Since the navbar now has updateResources(), there is no need to reinflate
    it upon a theme change.
    
    Change-Id: Ie4753a767a1cb45eb459ee692d16067f83cc7d23

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -485,7 +485,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
             if (DEBUG) Slog.v(TAG, "hasNavigationBar=" + showNav);
-            if (showNav && !mRecreating) {
+            if (mNavigationBarView == null && showNav && !mRecreating) {
                 mNavigationBarView =
                     (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
 
