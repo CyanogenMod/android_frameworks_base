9e1aef7ef82a4f3357b96599b3d10259ba458a91
Jens Doll <jens.doll@gmail.com>
2013-05-20 15:44:08 +0200

    Pie Controls: Fix sound effects.
    After the pie service commits, sound effects were broken for
    HOME and BACK keys.
    
    This commit fixes this, by moving the play back of the sound
    effect from the injection directly to the onClick().
    
    Change-Id: Id59848ccddac20cef80500f6e72d76afa97ae093

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -159,7 +159,6 @@ public class PieController implements BaseStatusBar.NavigationBarCallback, PieVi
                 case MSG_INJECT_KEY_DOWN:
                     inputManager.injectInputEvent((KeyEvent) m.obj,
                             InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
-                    mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
                     break;
                 case MSG_INJECT_KEY_UP:
                     inputManager.injectInputEvent((KeyEvent) m.obj,
@@ -577,12 +576,14 @@ public class PieController implements BaseStatusBar.NavigationBarCallback, PieVi
         long when = SystemClock.uptimeMillis();
         ButtonInfo bi = (ButtonInfo) item.tag;
 
+        // play sound effect directly, since detaching the container will prevent to play the sound
+        // at a later time.
+        mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
         if (bi.keyCode != 0) {
             injectKeyDelayed(bi.keyCode, when);
         } else {
             // provide the same haptic feedback as if a virtual key is pressed
             mPieContainer.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
-            mPieContainer.playSoundEffect(SoundEffectConstants.CLICK);
             if (bi == NavigationButtons.RECENT) {
                 if (mStatusBar != null) {
                     mStatusBar.toggleRecentApps();
