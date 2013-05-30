a71907310cb8289bd00f95325f6638d15fff6a83
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-05-30 21:32:55 +0530

    Telephony: Add TD-SCDMA network modes
    1)TD-SCDMA Signal Strength support.
    2)Add TD-SCDMA network type support in MobileDataStateTracker class
    to identify TD-SCDMA as UMTS variant and use the relevant
    kernel data stack tcp buffer sizes defined.
    3) Add TD-SCDMA to the list of supported network modes.
    
    Change-Id: I158821574484bfaf47d1660406e591e2afb3be5e
    CRs-Fixed: 403258, 479310
    (cherry picked from commit 2ba1b7292c6b458fb72587c1f29a5c00b70717cd)
    (cherry picked from commit d58b065e7e3d84461a3c981e7a7d4673f3567ef5)
    (cherry picked from commit 7844d69d3d323b3969766309c2ad5bc80411fbae)

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -146,6 +146,11 @@ public class ServiceState implements Parcelable {
      * @hide
      */
     public static final int RIL_RADIO_TECHNOLOGY_GSM = 16;
+    /**
+     * TD-SCDMA
+     * @hide
+     */
+    public static final int RIL_RADIO_TECHNOLOGY_TD_SCDMA = 17;
 
     /**
      * Available registration states for GSM, UMTS and CDMA.
@@ -533,6 +538,9 @@ public class ServiceState implements Parcelable {
             case RIL_RADIO_TECHNOLOGY_GSM:
                 rtString = "GSM";
                 break;
+            case RIL_RADIO_TECHNOLOGY_TD_SCDMA:
+                rtString = "TD-SCDMA";
+                break;
             default:
                 rtString = "Unexpected";
                 Rlog.w(LOG_TAG, "Unexpected radioTechnology=" + rt);
@@ -799,6 +807,8 @@ public class ServiceState implements Parcelable {
             return TelephonyManager.NETWORK_TYPE_HSPAP;
         case ServiceState.RIL_RADIO_TECHNOLOGY_GSM:
             return TelephonyManager.NETWORK_TYPE_GSM;
+        case ServiceState.RIL_RADIO_TECHNOLOGY_TD_SCDMA:
+            return TelephonyManager.NETWORK_TYPE_TD_SCDMA;
         default:
             return TelephonyManager.NETWORK_TYPE_UNKNOWN;
         }
@@ -848,7 +858,8 @@ public class ServiceState implements Parcelable {
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPA
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_LTE
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPAP
-                || radioTechnology == RIL_RADIO_TECHNOLOGY_GSM;
+                || radioTechnology == RIL_RADIO_TECHNOLOGY_GSM
+                || radioTechnology == RIL_RADIO_TECHNOLOGY_TD_SCDMA;
     }
 
     /** @hide */
