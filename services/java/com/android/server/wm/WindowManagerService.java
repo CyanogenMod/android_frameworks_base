f3123e33851892861c9c9e454c37aa87e25cc357
Ethan Chen <intervigil@gmail.com>
2013-05-30 22:12:18 -0700

    Detect whether overflow button should be shown based on menu key state
    Some devices may have hardware keys but not contain a hardware menu key.
    Hardware keys are also able to remapped. The overflow button detection logic
    currently does not take that into account. This change allows detects
    whether any key is capable of emitting the MENU_ACTION keystroke in
    order to determine whether the overflow button should be shown or not.
    
    Change-Id: I8699bd0242560314f8eadc54b187c69c992cf875

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10192,6 +10192,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean hasMenuKeyEnabled() {
+        return mPolicy.hasMenuKeyEnabled();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
--- tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
+++ tools/layoutlib/bridge/src/android/view/IWindowManagerImpl.java
@@ -498,4 +498,9 @@ public class IWindowManagerImpl implements IWindowManager {
     @Override
     public void setTouchExplorationEnabled(boolean enabled) {
     }
+
+    @Override
+    public boolean hasMenuKeyEnabled() {
+        return true;
+    }
 }
