3d9a74923ab91ca018af3a9131b446d958fc5b88
DvTonder <david.vantonder@gmail.com>
2013-06-01 13:45:02 -0400

    Framework: Add support to enable/disable Electron Beam (1/2)
    Parts were brought over from CM9 but not all of it.  This completes
    the port.
    
    Change-Id: I2ab24581ca775ec7af3eecf7e1dcac72c91e3124

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -799,8 +799,9 @@ final class DisplayPowerController {
                         } else if (mPowerState.prepareElectronBeam(
                                 mElectronBeamFadesConfig ?
                                         ElectronBeam.MODE_FADE :
-                                                ElectronBeam.MODE_COOL_DOWN)
-                                && mPowerState.isScreenOn()) {
+                                        ElectronBeam.MODE_COOL_DOWN)
+                                && mPowerState.isScreenOn()
+                                && useScreenOffAnimation()) {
                             mElectronBeamOffAnimator.start();
                         } else {
                             mElectronBeamOffAnimator.end();
@@ -1495,4 +1496,9 @@ final class DisplayPowerController {
             updatePowerState();
         }
     };
+
+    private boolean useScreenOffAnimation() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.SCREEN_OFF_ANIMATION, 1) == 1;
+    }
 }
