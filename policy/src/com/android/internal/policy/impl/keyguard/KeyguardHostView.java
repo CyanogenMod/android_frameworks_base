fa583e172247cc4c863f53bf92ef71c65fe96ff1
Danesh Mondegarian <daneshm90@gmail.com>
2013-06-03 00:38:47 -0400

    Lockscreen : Add custom background overlay
    Adds an overlay on top of custom images/colors to better match
    stock appearance.
    
    https://jira.cyanogenmod.org/browse/CYAN-1280
    
    Change-Id: I1210d208de912e7f6a1941284f1fe1ff0776ab48

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -38,8 +38,11 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Looper;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -325,9 +328,10 @@ public class KeyguardHostView extends KeyguardViewBase {
             return;
         }
 
+        Drawable back = null;
         if (!background.isEmpty()) {
             try {
-                setBackgroundColor(Integer.parseInt(background));
+                back = new ColorDrawable(Integer.parseInt(background));
             } catch(NumberFormatException e) {
                 Log.e(TAG, "Invalid background color " + background);
             }
@@ -336,10 +340,14 @@ public class KeyguardHostView extends KeyguardViewBase {
                 Context settingsContext = getContext().createPackageContext("com.android.settings", 0);
                 String wallpaperFile = settingsContext.getFilesDir() + "/lockwallpaper";
                 Bitmap backgroundBitmap = BitmapFactory.decodeFile(wallpaperFile);
-                setBackgroundDrawable(new BitmapDrawable(backgroundBitmap));
+                back = new BitmapDrawable(getContext().getResources(), backgroundBitmap);
             } catch (NameNotFoundException e) {
-            // Do nothing here
+                // Do nothing here
             }
+        }
+        if (back != null) {
+            back.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.MULTIPLY);
+            setBackground(back);
         }
     }
 
