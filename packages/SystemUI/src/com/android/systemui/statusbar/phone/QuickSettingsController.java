00d1553b204c3ef5aa1b2c423d1c8ed6903cbc55
Danny Baumann <dannybaumann@web.de>
2013-06-21 08:57:11 +0200

    Use sticky broadcast to convey torch state (1/2).
    It doesn't really make sense to store the (non-persistent) state of the
    torch in a (persistent) setting. As the torch is sending a broadcast
    anyway, make that broadcast sticky and use it to convey the state.
    
    Change-Id: If845bef27cc79990794aa748dce18cfea3bac1f5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -390,7 +390,7 @@ public class QuickSettingsController {
         }
     }
 
-    public void registerAction(Object action, QuickSettingsTile tile) {
+    public void registerAction(String action, QuickSettingsTile tile) {
         registerInMap(action, tile, mReceiverMap);
     }
 
