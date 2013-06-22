ff41d1dfbf467b72dec53906bbe4f7fa291a30d8
Timo Wendt <timo@tjwendt.de>
2013-06-22 23:04:00 +0200

    Irda: Add Irda System Service for Samsung devices
    Add Irda System Service that is compatible with the Samsung devices
    .
    This is not a perfect solution yet, but it does work and should therefore be a starting point.
    I do not know how to make this code exclusive to the S4, maybe someone can help me out with that.
    
    PatchSet 2: Now the Irda service is not started by default. It can be enabled via overlay.
    
    PatchSet 3: Rebase
    
    PatchSet 4: It now uses a HAL module to communicate with sysfs. However it currently
    fails when loading the module
    
    PatchSet 5: Corrected a typo in a comment
    
    PatchSet 6: Cleanup
    
    PatchSet 7: Renamed native method to send_ircode_native
    
    Patchset 8: Renamed commit message to indicate general applicability to Samsung devices
    including n80xx.  Need to determine how to handle HTC and Sony...  Also added logging
    when module load fails or device opening fails.
    
    Change-Id: Id4da55cfbd297fa46c50aa8a6af9cd4d056086cd

---
+++ services/java/com/android/server/IrdaManagerService.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.hardware.IIrdaManager;
+import android.os.Looper;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+
+public class IrdaManagerService extends IIrdaManager.Stub {
+    private static final String TAG = "IrdaManagerService";
+    private IrdaWorkerHandler mHandler;
+    private Looper mServiceLooper;
+    private Context mContext;
+    private int mNativePointer;
+    public IrdaManagerService(Context context) {
+        super();
+        mContext = context;
+        mNativePointer = init_native();
+        HandlerThread mWorker = new HandlerThread("IrdaServiceWorker");
+        mWorker.start();
+        mServiceLooper = mWorker.getLooper();
+        mHandler = new IrdaWorkerHandler(mServiceLooper);
+    }
+
+    public void write_irsend(String irCode) {
+        if (irCode != null) {
+            Message msg = mHandler.obtainMessage(IrdaWorkerHandler.MESSAGE_SET, irCode);
+            msg.sendToTarget();
+        }
+    }
+
+    private class IrdaWorkerHandler extends Handler {
+        private static final int MESSAGE_SET = 0;
+
+        public IrdaWorkerHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MESSAGE_SET) {
+                String irCode = (String) msg.obj;
+                byte[] buffer = irCode.getBytes();
+                send_ircode_native(mNativePointer, buffer);
+            }
+        }
+    }
+
+    private static native int init_native();
+    private static native void finalize_native(int ptr);
+    private static native void send_ircode_native(int ptr, byte[] buffer);
+}
