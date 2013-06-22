4f5ab403f6454920c7e998170cf2d59b1f3e48f1
Timo Wendt <timo@tjwendt.de>
2013-06-22 23:04:00 +0200

    Irda: Add Irda System Service for I9505
    Add Irda System Service that is compatible with the Galaxy S4.
    This is not a perfect solution yet, but it does work and should therefore be a starting point.
    I do not know how to make this code exclusive to the S4, maybe someone can help me out with that.
    
    PatchSet 2: Now the Irda service is not started by default. It can be enabled via overlay.
    
    PatchSet 3: Rebase
    
    PatchSet 4: It now uses a HAL module to communicate with sysfs. However it currently
    fails when loading the module
    
    PatchSet 5: Corrected a typo in a comment
    
    PatchSet 6: Cleanup
    
    Change-Id: Id4da55cfbd297fa46c50aa8a6af9cd4d056086cd

---
+++ core/java/android/hardware/IrdaManager.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware;
+
+import android.os.RemoteException;
+
+/**
+ * This class provides access to the system irda services.
+ *
+ * <p>You do not
+ * instantiate this class directly; instead, retrieve it through
+ * {@link android.content.Context#getSystemService
+ * Context.getSystemService(Context.IRDA_SERVICE)}.
+ */
+public class IrdaManager
+{
+    private final IIrdaManager mService;
+
+    /**
+     * package private on purpose
+     */
+    public IrdaManager(IIrdaManager service) {
+        mService = service;
+    }
+
+    /**
+     * Send IR code via the emitter
+     *
+     * The format of the code can be converted from the pronto format
+     * as follows
+     *
+     * Example pronto code:
+     * 0000 006D 0000 0022 00AC 00AB 0015 0041 0015 0041 0015 0041 0015
+     * 0016 0015 0016 0015 0016 0015 0016 0015 0016 0015 0041 0015 0041
+     * 0015 0041 0015 0016 0015 0016 0015 0016 0015 0016 0015 0016 0015
+     * 0016 0015 0016 0015 0016 0015 0041 0015 0041 0015 0016 0015 0016
+     * 0015 0041 0015 0041 0015 0041 0015 0041 0015 0016 0015 0016 0015
+     * 0041 0015 0041 0015 0016 0015 0689
+     *
+     * 1. word 0000: not needed
+     * 2. word 006D: this has to be converted to the frequency as follows:
+     *    first vonvert it to decimal 109
+     *    now caculate the frequency 1000000/(109 * .241246) = 38000 rounded
+     * 3. word 0000: not needed
+     * 4. word 0022: not needed
+     * all the remaining numbers have to be converted to decimal
+     *
+     * now build a comma separated string without spaces with the frequency
+     * and the remaining numbers as follows:
+     *
+     * 38000,172,171,21,65,21,65,21,65,21,22,21,22,21,22,21,22,21,22,21,65,
+     * 21,65,21,65,21,22,21,22,21,22,21,22,21,22,21,22,21,22,21,22,21,65,21,
+     * 65,21,22,21,22,21,65,21,65,21,65,21,65,21,22,21,22,21,65,21,65,21,22,
+     * 21,1673
+     *
+     */
+    public void write_irsend(String code) {
+        try {
+            mService.write_irsend(code);
+        } catch (RemoteException ex) {
+        }
+    }
+}
