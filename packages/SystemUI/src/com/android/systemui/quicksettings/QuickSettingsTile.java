256b642429633450184c51b224b67dbe3989fa58
Nebojsa Cvetkovic <nebkat@teamhacksung.org>
2013-06-23 21:08:02 +0200

    QuickSettings: Camera Tile
    Change-Id: Id0beb3dd559536bb5571ddc87cb0bcb4945b1a77

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -73,8 +73,10 @@ public class QuickSettingsTile implements OnClickListener {
 
     void updateQuickSettings(){
         TextView tv = (TextView) mTile.findViewById(R.id.tile_textview);
-        tv.setCompoundDrawablesWithIntrinsicBounds(0, mDrawable, 0, 0);
-        tv.setText(mLabel);
+        if (tv != null) {
+            tv.setCompoundDrawablesWithIntrinsicBounds(0, mDrawable, 0, 0);
+            tv.setText(mLabel);
+        }
     }
 
     void startSettingsActivity(String action){
@@ -100,7 +102,7 @@ public class QuickSettingsTile implements OnClickListener {
 
     @Override
     public final void onClick(View v) {
-        mOnClick.onClick(v);
+        if (mOnClick != null) mOnClick.onClick(v);
         ContentResolver resolver = mContext.getContentResolver();
         boolean shouldCollapse = Settings.System.getInt(resolver, Settings.System.QS_COLLAPSE_PANEL, 0) == 1;
         if (shouldCollapse) {
