16dad200b3a3beb5775a3832add158490af616dc
Srinu Jella <sjella@codeaurora.org>
2013-07-08 15:12:29 +0530

    Bluetooth: SAP: Add framework changes to support SAP
    Bluetooth SIM access server (SAP) profile APIs added to the
    framework APIs. As SAP server supports only disconnection
    of SAP profile level connection this API is implemeted
    along with other APIs.
    
    It registers bluetooth state changed callback with the
    adapter service,once there is a change in the bluetooth
    state,it will bind/unbind the SAP service which is
    implemented in BluetoothExt.APK.
    
    SAP service, and other SAP Application layer is implemented
    in BluetoothExt.APK.
    
    Change-Id: I7215f503244c5ee277e83d60d6960f18559acfb9
    
    Conflicts:
    	core/java/android/bluetooth/BluetoothDevice.java
    (cherry picked from commit 86d14a60283c2315bea8a34f24dd0c22139a136b)
    (cherry picked from commit 718d372656fa7a456247c4909baac6e7104049f8)

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1189,6 +1189,9 @@ public final class BluetoothAdapter {
         } else if (profile == BluetoothProfile.PAN) {
             BluetoothPan pan = new BluetoothPan(context, listener);
             return true;
+        } else if (profile == BluetoothProfile.SAP) {
+            BluetoothSap sap = new BluetoothSap(context, listener);
+            return true;
         } else if (profile == BluetoothProfile.HEALTH) {
             BluetoothHealth health = new BluetoothHealth(context, listener);
             return true;
@@ -1230,6 +1233,10 @@ public final class BluetoothAdapter {
             case BluetoothProfile.PAN:
                 BluetoothPan pan = (BluetoothPan)proxy;
                 pan.close();
+                break;
+            case BluetoothProfile.SAP:
+                BluetoothSap sap = (BluetoothSap)proxy;
+                sap.close();
                 break;
             case BluetoothProfile.HEALTH:
                 BluetoothHealth health = (BluetoothHealth)proxy;
