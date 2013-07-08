cef6d25a40db2afb26908242f8b03c9ee05665a3
Srinu Jella <sjella@codeaurora.org>
2013-07-08 15:12:29 +0530

    Bluetooth: SAP: Add framework changes to support SAP
    Bluetooth SIM access server (SAP) profile APIs added to the
    framework APIs. As SAP server supports only disconnection
    of SAP profile level connection this API is implemeted
    along with other APIs.
    
    It registers bluetooth state changed callback with the
    adapter service,once there is a change in the bluetooth
    state,it will bind/unbind the SAP service which is
    implemented in BluetoothExt.APK.
    
    SAP service, and other SAP Application layer is implemented
    in BluetoothExt.APK.
    
    Change-Id: I7215f503244c5ee277e83d60d6960f18559acfb9
    
    Conflicts:
    	core/java/android/bluetooth/BluetoothDevice.java
    (cherry picked from commit 86d14a60283c2315bea8a34f24dd0c22139a136b)
    (cherry picked from commit 718d372656fa7a456247c4909baac6e7104049f8)

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -104,6 +104,12 @@ public interface BluetoothProfile {
     public static final int MAP = 9;
 
     /**
+     * SAP
+     * @hide
+     */
+    public static final int SAP = 20;
+
+    /**
      * Default priority for devices that we try to auto-connect to and
      * and allow incoming connections for the profile
      * @hide
