a94735d5fe00de1622214ddb50aacbafc97b102e
Nitin Shivpure <nshivpur@codeaurora.org>
2013-07-08 11:10:34 +0530

    Bluetooth: Disable bluetooth only if BT Sevice is connected
    Bluetooth service is imidiately disconnected from the adapter
    after SSR OR if any bluetooth services is crashed OR framework
    is crashed. As Bluetooth  service is not connected OR Bluetooth
    is not enabled, So BT disable should be ignored & bluetooth
    persist setting should not be changed.
    
    CRs-Fixed: 499116
    
    Change-Id: I19de939346d014cdf918e11711f2d6066e55d054

--- services/java/com/android/server/BluetoothManagerService.java
+++ services/java/com/android/server/BluetoothManagerService.java
@@ -123,6 +123,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
     private HandlerThread mThread;
     private final BluetoothHandler mHandler;
     private int mErrorRecoveryRetryCounter;
+    private boolean mIsBluetoothServiceConnected = false;
 
     private void registerForAirplaneMode(IntentFilter filter) {
         final ContentResolver resolver = mContext.getContentResolver();
@@ -425,6 +426,12 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             (!checkIfCallerIsForegroundUser())) {
             Log.w(TAG,"disable(): not allowed for non-active and non system user");
             return false;
+        }
+
+        if (!mIsBluetoothServiceConnected || mState != BluetoothAdapter.STATE_ON)
+        {
+            Log.d(TAG, "Disable(): Service is not Connected Or Bluetooth is not enabled");
+            return true;
         }
 
         if (DBG) {
@@ -784,6 +791,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                 {
                     if (DBG) Log.d(TAG,"MESSAGE_BLUETOOTH_SERVICE_CONNECTED: " + msg.arg1);
 
+                    mIsBluetoothServiceConnected = true;
+
                     IBinder service = (IBinder) msg.obj;
                     synchronized(mConnection) {
                         if (msg.arg1 == SERVICE_IBLUETOOTHGATT) {
@@ -872,6 +881,9 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                 case MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED:
                 {
                     Log.e(TAG, "MESSAGE_BLUETOOTH_SERVICE_DISCONNECTED: " + msg.arg1);
+
+                    mIsBluetoothServiceConnected = false;
+
                     synchronized(mConnection) {
                         if (msg.arg1 == SERVICE_IBLUETOOTH) {
                             // if service is unbinded already, do nothing and return
@@ -887,7 +899,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     }
 
                     if (mEnable) {
-                        mEnable = false;
+                        if (!isBluetoothPersistedStateOn())
+                            mEnable = false;
                         // Send a Bluetooth Restart message
                         Message restartMsg = mHandler.obtainMessage(
                             MESSAGE_RESTART_BLUETOOTH_SERVICE);
