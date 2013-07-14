9f1b3d88533f8e4be875f4f31494418aa6bd0355
ghepeu <ghepeu@gmail.com>
2013-07-14 21:01:49 +0200

    Add support for Samsung extended AGPS
    The AGPS implementation in the GPS chipset used in some Samsung devices
    (i9100, i9300) can make use of the Psc field. Adapt the relevant
    functions hiding the changes incompatible with other devices under the
    AGPS_USE_PSC #define.
    
    Credit to Qaweck from xda-developers forum for finding the meaning
    of the field.
    
    Change-Id: Ie4691c79ca379a1f5c0a87500c1b06b56ae7ac0d

--- services/core/jni/com_android_server_location_GpsLocationProvider.cpp
+++ services/core/jni/com_android_server_location_GpsLocationProvider.cpp
@@ -641,7 +641,7 @@ static jint android_location_GpsLocationProvider_read_sv_status(JNIEnv* env, job
 }
 
 static void android_location_GpsLocationProvider_agps_set_reference_location_cellid(
-        JNIEnv* /* env */, jobject /* obj */, jint type, jint mcc, jint mnc, jint lac, jint cid)
+        JNIEnv* /* env */, jobject /* obj */, jint type, jint mcc, jint mnc, jint lac, jint cid, jint psc)
 {
     AGpsRefLocation location;
 
@@ -657,6 +657,11 @@ static void android_location_GpsLocationProvider_agps_set_reference_location_cel
             location.u.cellID.mcc = mcc;
             location.u.cellID.mnc = mnc;
             location.u.cellID.lac = lac;
+#ifdef AGPS_USE_PSC
+            location.u.cellID.psc = psc;
+#else
+            (void *)psc;
+#endif
             location.u.cellID.cid = cid;
             break;
         default:
@@ -1477,7 +1482,7 @@ static JNINativeMethod sMethods[] = {
             "(ILjava/lang/String;)V",
             (void*)android_location_GpsLocationProvider_agps_set_id},
     {"native_agps_set_ref_location_cellid",
-            "(IIIII)V",
+            "(IIIIII)V",
             (void*)android_location_GpsLocationProvider_agps_set_reference_location_cellid},
     {"native_set_agps_server",
             "(ILjava/lang/String;I)V",
