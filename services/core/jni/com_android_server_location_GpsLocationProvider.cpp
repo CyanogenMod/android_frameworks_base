c515f5fa191a7d60c3731108923e98b05be6ae0b
ghepeu <ghepeu@gmail.com>
2013-07-14 21:01:49 +0200

    Add support for Samsung extended AGPS
    The AGPS implementation in the GPS chipset used in some Samsung devices
    (i9100, i9300) can make use of the Psc field. Adapt the relevant
    functions hiding the changes incompatible with other devices under the
    AGPS_USE_PSC #define.
    
    Credit to Qaweck from xda-developers forum for finding the meaning
    of the field.
    
    Change-Id: Ie4691c79ca379a1f5c0a87500c1b06b56ae7ac0d

--- services/core/jni/com_android_server_location_GpsLocationProvider.cpp
+++ services/core/jni/com_android_server_location_GpsLocationProvider.cpp
@@ -628,7 +628,7 @@ static jint android_location_GpsLocationProvider_read_sv_status(JNIEnv* env, job
 }
 
 static void android_location_GpsLocationProvider_agps_set_reference_location_cellid(JNIEnv* env,
-        jobject obj, jint type, jint mcc, jint mnc, jint lac, jint cid)
+        jobject obj, jint type, jint mcc, jint mnc, jint lac, jint psc, jint cid)
 {
     AGpsRefLocation location;
 
@@ -641,9 +641,13 @@ static void android_location_GpsLocationProvider_agps_set_reference_location_cel
         case AGPS_REF_LOCATION_TYPE_GSM_CELLID:
         case AGPS_REF_LOCATION_TYPE_UMTS_CELLID:
             location.type = type;
+            location.u.cellID.type = type;
             location.u.cellID.mcc = mcc;
             location.u.cellID.mnc = mnc;
             location.u.cellID.lac = lac;
+#ifdef AGPS_USE_PSC
+            location.u.cellID.psc = psc;
+#endif
             location.u.cellID.cid = cid;
             break;
         default:
@@ -1463,7 +1467,7 @@ static JNINativeMethod sMethods[] = {
             "(ILjava/lang/String;)V",
             (void*)android_location_GpsLocationProvider_agps_set_id},
     {"native_agps_set_ref_location_cellid",
-            "(IIIII)V",
+            "(IIIIII)V",
             (void*)android_location_GpsLocationProvider_agps_set_reference_location_cellid},
     {"native_set_agps_server",
             "(ILjava/lang/String;I)V",
