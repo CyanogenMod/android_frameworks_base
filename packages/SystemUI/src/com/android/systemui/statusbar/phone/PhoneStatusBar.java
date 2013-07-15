44e61e439df32a63471086307ab97ef6f9d0237d
Danny Baumann <dannybaumann@web.de>
2013-07-15 13:31:32 +0200

    Make notification-shade-collapse-after-dismiss behaviour configurable.
    Change-Id: I1be0be2753b479e6d2e4fc8e17d971d03ce01bb7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2598,7 +2598,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                 }
 
                 if (snapshot.isEmpty()) {
-                    animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE);
+                    maybeCollapseAfterNotificationRemoval(true);
                     return;
                 }
 
