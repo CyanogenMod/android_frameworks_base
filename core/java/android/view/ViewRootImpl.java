ac191ba45b8be8a69b779318919bcd1050c946bf
Michael Bestas <mikeioannina@gmail.com>
2013-07-23 02:06:13 +0300

    Add support for zeus touchpads
    Original implementation by farenheith.
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I26c7d3f4ff6f0043a45d9ebdc3fa7cc29f06ef9d

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -95,7 +95,7 @@ import java.util.HashSet;
  */
 @SuppressWarnings({"EmptyCatchBlock", "PointlessBooleanExpression"})
 public final class ViewRootImpl implements ViewParent,
-        View.AttachInfo.Callbacks, HardwareRenderer.HardwareDrawCallbacks {
+        View.AttachInfo.Callbacks, HardwareRenderer.HardwareDrawCallbacks, ViewRoot {
     private static final String TAG = "ViewRootImpl";
     private static final boolean DBG = false;
     private static final boolean LOCAL_LOGV = false;
@@ -5317,6 +5317,12 @@ public final class ViewRootImpl implements ViewParent,
                 mSource.resetAccessibilityStateChanged();
                 mSource = null;
             }
+        }
+    }
+
+    public void setProcessPositionEvents(boolean b) {
+        if (mView != null) {
+            mView.setProcessGenericMotionAsPointer(b);
         }
     }
 }
