a3fc865d9b7dbbb86e0a6cd55caa5d58664cbb76
pulser <pulser@villainrom.co.uk>
2013-07-22 08:35:31 +0100

    Separate Change Encryption Password Entry (2 of 2)
    This permits the use of a different (hopefully longer and more complex!) encryption password,
    while using a simpler (and easier to type) lockscreen password.
    
    Requires Settings change to give UI for this
    
    Change-Id: Ib54532f92f4f3beb8713fd73e466ae46b7e15f7d

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -634,9 +634,7 @@ public class LockPatternUtils {
             KeyStore keyStore = KeyStore.getInstance();
             if (password != null) {
                 if (userHandle == UserHandle.USER_OWNER) {
-                    // Update the encryption password.
-                    updateEncryptionPassword(password);
-
+                    // Encryption password is not updated here, allowing different passwords
                     // Update the keystore password
                     keyStore.password(password);
                 }
@@ -721,6 +719,29 @@ public class LockPatternUtils {
             Log.e(TAG, "Unable to save lock password " + re);
         }
     }
+
+     /**
+     * Save a device encryption password.  Does not do any checking on complexity.
+     * @param password The password to save
+     */
+    public void saveEncryptionPassword(String password) {
+        this.saveEncryptionPassword(password, getCurrentOrCallingUserId());
+    }
+
+     /**
+     * Save a device encryption password.  Does not do any checking on complexity.
+     * @param password The password to save
+     * @param userHandle The userId of the user to change the password for
+     */
+    public void saveEncryptionPassword(String password, int userHandle) {
+        if (password != null) {
+            if (userHandle == UserHandle.USER_OWNER) {
+                // Update the encryption password.
+                updateEncryptionPassword(password);
+            }
+        }
+    }
+
 
     /**
      * Retrieves the quality mode we're in.
