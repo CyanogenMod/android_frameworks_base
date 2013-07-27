1b08fa5057a2a819b6548753a0bf716c2ba8ac43
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-07-27 18:50:11 +0300

    Call clipboard change listeners outside the setPrimaryClip transaction
    If a clipboard change listener has been registered by application X and
    application Y calls setPrimaryClip, the app ops security check for
    the listener would raise an exception due to the calling uid (app Y) and
    listener uid (app X) not matching.
    
    To avoid this, detach the setPrimaryClip caller before calling the
    listeners, so that the security check is done through the privileged
    system uid.
    
    Should fix AOSP bug #58043:
    https://code.google.com/p/android/issues/detail?id=58043
    
    Change-Id: I2d0bf3b618560c2d7617c48593f839f2b6a86708

--- services/java/com/android/server/ClipboardService.java
+++ services/java/com/android/server/ClipboardService.java
@@ -162,6 +162,9 @@ public class ClipboardService extends IClipboard.Stub {
             clearActiveOwnersLocked();
             PerUserClipboard clipboard = getClipboard();
             clipboard.primaryClip = clip;
+
+            // Check and call listeners as the service itself
+            long token = Binder.clearCallingIdentity();
             final int n = clipboard.primaryClipListeners.beginBroadcast();
             for (int i = 0; i < n; i++) {
                 try {
@@ -179,6 +182,7 @@ public class ClipboardService extends IClipboard.Stub {
                 }
             }
             clipboard.primaryClipListeners.finishBroadcast();
+            Binder.restoreCallingIdentity(token);
         }
     }
     
