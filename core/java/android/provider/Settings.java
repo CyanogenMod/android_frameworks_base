9b829a9dc966a972382173f7de0fae1b070f49c3
Elliott Hughes <enh@google.com>
2013-08-02 09:07:54 -0700

    When wifi wants ASCII lowercasing, it needs to ask for it.
    http://elliotth.blogspot.com/2012/01/beware-convenience-methods.html
    
    Bug: https://code.google.com/p/android/issues/detail?id=58359
    Change-Id: Ibf25d0bbc76015cf8353ec01ab2b743cbc2bde67
    
    Conflicts:
    
    	wifi/java/android/net/wifi/WifiStateMachine.java
    
    If frameworks wants ASCII casing, it should explicity ask for it.
    
    http://elliotth.blogspot.com/2012/01/beware-convenience-methods.html
    
    Bug: https://code.google.com/p/android/issues/detail?id=58359
    Change-Id: Iaab02e718a7be7bda22e626dca05d79bfd2a8fc4
    
    If graphics wants ASCII lowercasing, it needs to ask for it.
    
    http://elliotth.blogspot.com/2012/01/beware-convenience-methods.html
    
    Bug: https://code.google.com/p/android/issues/detail?id=58359
    Change-Id: I13c106985302335dbb15bb9176d35ec6b4546d4e
    
    If media wants ASCII lowercasing, it needs to ask for it.
    
    http://elliotth.blogspot.com/2012/01/beware-convenience-methods.html
    
    Use toLowerCase(Locale.ROOT) if you want ASCII for machine consumption,
    and use toLowerCase(Locale.getDefault()) if you want the user's locale's
    casing rules for human consumption.
    
    Bug: https://code.google.com/p/android/issues/detail?id=58359
    Change-Id: Id9005a17e34217a81bef3b40031b9e2e6272f45d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -58,6 +58,7 @@ import com.android.internal.widget.ILockSettings;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 
 /**
  * The Settings provider contains global system-level device preferences.
@@ -6401,7 +6402,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothHeadsetPriorityKey(String address) {
-            return BLUETOOTH_HEADSET_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_HEADSET_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
@@ -6409,7 +6410,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothA2dpSinkPriorityKey(String address) {
-            return BLUETOOTH_A2DP_SINK_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_A2DP_SINK_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
@@ -6417,7 +6418,7 @@ public final class Settings {
          * @hide
          */
         public static final String getBluetoothInputDevicePriorityKey(String address) {
-            return BLUETOOTH_INPUT_DEVICE_PRIORITY_PREFIX + address.toUpperCase();
+            return BLUETOOTH_INPUT_DEVICE_PRIORITY_PREFIX + address.toUpperCase(Locale.ROOT);
         }
 
         /**
