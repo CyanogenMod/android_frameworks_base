58ebcdc57ae037c46d6839744aebca505da0e736
Skrilax_CZ <skrilax@gmail.com>
2013-08-03 20:45:58 +0200

    Add the possiblity of using secondary storage for user 0
    Since Android 4.2 it is not possible to use external SD card to use for
    application data (such as downloaded content for games). This patch adds
    the possibility to use external SD card as a storage for user ID 0.
    Requires the path to be defined by SECONDARY_STORAGE and used property
    "persist.sys.env.use_sec_storage" to check if it's to be used. The property
    is to be set in "Advanced Settings" under device tree (this should be
    available to user ID 0 only!)
    
    Change-Id: I73d4711eac53118c0234110f06070fc42a975c44

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -1274,7 +1274,9 @@ class MountService extends IMountService.Stub
         }
 
         final UserEnvironment userEnv = new UserEnvironment(user.getIdentifier());
-        final File path = userEnv.getExternalStorageDirectory();
+        // Use default storage directory (remains /storage/emulated/0 even if switched
+        // external sdcard for user 0
+        final File path = userEnv.getDefaultExternalStorageDirectory();
         final StorageVolume volume = StorageVolume.fromTemplate(mEmulatedTemplate, path, user);
         volume.setStorageId(0);
         addVolumeLocked(volume);
