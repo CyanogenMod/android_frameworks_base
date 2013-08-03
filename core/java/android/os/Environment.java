57e7ef1ac05b7109269ad368f78b6d37e545c198
Skrilax_CZ <skrilax@gmail.com>
2013-08-03 20:45:58 +0200

    Add the possiblity of using secondary storage for user 0
    Since Android 4.2 it is not possible to use external SD card to use for
    application data (such as downloaded content for games). This patch adds
    the possibility to use external SD card as a storage for user ID 0.
    Requires the path to be defined by SECONDARY_STORAGE and used property
    "persist.sys.env_use_sec_storage" to check if it's to be used. The property
    is to be set in "Advanced Settings" under device tree (this should be
    available to user ID 0 only!)
    
    Change-Id: I3288de270bb4fe03171a8cf9d53db111bf58b8b7

--- core/java/android/os/Environment.java
+++ core/java/android/os/Environment.java
@@ -36,6 +36,7 @@ public class Environment {
     private static final String ENV_EMULATED_STORAGE_SOURCE = "EMULATED_STORAGE_SOURCE";
     private static final String ENV_EMULATED_STORAGE_TARGET = "EMULATED_STORAGE_TARGET";
     private static final String ENV_MEDIA_STORAGE = "MEDIA_STORAGE";
+    private static final String ENV_SECONDARY_STORAGE = "SECONDARY_STORAGE";
 
     /** {@hide} */
     public static String DIRECTORY_ANDROID = "Android";
@@ -44,6 +45,10 @@ public class Environment {
             = getDirectory("ANDROID_ROOT", "/system");
 
     private static final String SYSTEM_PROPERTY_EFS_ENABLED = "persist.security.efs.enabled";
+
+    private static final String SYSTEM_PROPERTY_SECONDARY_STORAGE = "persist.sys.env_use_sec_storage";
+
+    private static boolean sUseSecondaryStorage;
 
     private static UserEnvironment sCurrentUser;
 
@@ -71,6 +76,8 @@ public class Environment {
     }
 
     static {
+        // No hotswap - read it just once
+        sUseSecondaryStorage = SystemProperties.getBoolean(SYSTEM_PROPERTY_SECONDARY_STORAGE, false);
         initForCurrentUser();
     }
 
@@ -89,6 +96,7 @@ public class Environment {
         // TODO: generalize further to create package-specific environment
 
         private final File mExternalStorage;
+        private final File mDefaultExternalStorage;
         private final File mExternalStorageAndroidData;
         private final File mExternalStorageAndroidMedia;
         private final File mExternalStorageAndroidObb;
@@ -99,6 +107,7 @@ public class Environment {
             String rawExternalStorage = System.getenv(ENV_EXTERNAL_STORAGE);
             String rawEmulatedStorageTarget = System.getenv(ENV_EMULATED_STORAGE_TARGET);
             String rawMediaStorage = System.getenv(ENV_MEDIA_STORAGE);
+            String rawSecondaryStorage = System.getenv(ENV_SECONDARY_STORAGE);
             if (TextUtils.isEmpty(rawMediaStorage)) {
                 rawMediaStorage = "/data/media";
             }
@@ -108,10 +117,22 @@ public class Environment {
                 // userId burned into them.
                 final String rawUserId = Integer.toString(userId);
                 final File emulatedBase = new File(rawEmulatedStorageTarget);
+
+                // Check if we are supposed to use external sdcard
+                if (userId == 0 && sUseSecondaryStorage && !TextUtils.isEmpty(rawSecondaryStorage)) {
+                    // /storage/sdcard1
+                    mExternalStorage = new File(rawSecondaryStorage);
+                    // /storage/emulated/0
+                    mDefaultExternalStorage = buildPath(emulatedBase, rawUserId);
+                } else {
+                    // /storage/emulated/0
+                    mExternalStorage = buildPath(emulatedBase, rawUserId);
+                    mDefaultExternalStorage = mExternalStorage;
+                }
+
+                // Media storage is also with userId burned into them
                 final File mediaBase = new File(rawMediaStorage);
 
-                // /storage/emulated/0
-                mExternalStorage = buildPath(emulatedBase, rawUserId);
                 // /data/media/0
                 mMediaStorage = buildPath(mediaBase, rawUserId);
 
@@ -124,6 +145,7 @@ public class Environment {
 
                 // /storage/sdcard0
                 mExternalStorage = new File(rawExternalStorage);
+                mDefaultExternalStorage = mExternalStorage;
                 // /data/media
                 mMediaStorage = new File(rawMediaStorage);
             }
@@ -135,6 +157,11 @@ public class Environment {
 
         public File getExternalStorageDirectory() {
             return mExternalStorage;
+        }
+
+        /** {@hide} */
+        public File getDefaultExternalStorageDirectory() {
+            return mDefaultExternalStorage;
         }
 
         public File getExternalStorageObbDirectory() {
