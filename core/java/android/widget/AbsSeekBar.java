cd82ac8cad17e079faa070879bf835deccea8669
Danny Baumann <dannybaumann@web.de>
2013-08-05 10:32:19 +0200

    Allow adjusting progress on touch events.
    Change-Id: Ide0a7ae057a62f3631a32eeaa63f3e04dec5bf54

--- core/java/android/widget/AbsSeekBar.java
+++ core/java/android/widget/AbsSeekBar.java
@@ -448,7 +448,14 @@ public abstract class AbsSeekBar extends ProgressBar {
         final int max = getMax();
         progress += scale * max;
         
-        setProgress((int) progress, true);
+        setProgress(updateTouchProgress(getProgress(), (int) progress), true);
+    }
+
+    /**
+     * @hide
+     */
+    protected int updateTouchProgress(int lastProgress, int newProgress) {
+        return newProgress;
     }
 
     /**
