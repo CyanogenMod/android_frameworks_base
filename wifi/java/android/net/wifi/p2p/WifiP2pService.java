09670ae844975caeee0683d8fc47ed967dde0ec6
Deepthi Gowri <deepthi@codeaurora.org>
2013-08-06 13:22:42 +0530

    P2P: Set wifi preferred channels when the supplicant is started.
    Set wifi preferred channels when the supplicant is started, if
    there are any safe channels from the previous LTE event received
    from RIL. This is to ensure that p2p GO is always started on one
    safe channels.
    
    Change-Id: Ib0c36dbc4dfdec3721adb2f72d604158eeff9a87
    CRs-Fixed: 523207

--- wifi/java/android/net/wifi/p2p/WifiP2pService.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pService.java
@@ -890,6 +890,9 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
             if (DBG) logd(getName() + message.toString());
             switch (message.what) {
                 case WifiMonitor.SUP_CONNECTION_EVENT:
+                   if (startSafeChannel!=0)
+                       mWifiNative.setPreferredChannel(
+                                        startSafeChannel, endSafeChannel);
                     if (DBG) logd("P2p socket connection successful");
                     mIsWifiP2pEnabled = true;
                     transitionTo(mInactiveState);
