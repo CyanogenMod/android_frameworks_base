b9dbda67cf431626913e6d3eb86959de0c9e7c1e
Sandeep Kunta <skunta@codeaurora.org>
2013-08-10 20:55:38 +0530

    Telephony: Add support for NETWORK_TYPE_GSM
    Add support for NETWORK_TYPE_GSM in TelephonyManager and
    corresponding mapping in ServicState.java so that when
    application queries getVoiceNetworkType() and currently
    camped rat is GSM, they get network mode as GSM instead
    of network mode unknown.
    
    Change-Id: I5e971c625532696776b9555ad8cfd85eba4fc423
    CRs-Fixed: 526435
    (cherry picked from commit 3e01fcec13725955f0ca3fb20730ff21f4edc128)
    (cherry picked from commit c70f5687965908cd585e52901cf6cd88bd405140)
    (cherry picked from commit 8e3c2300729ecb3cd3e6b640b0174f49e2189f0c)

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -620,6 +620,8 @@ public class TelephonyManager {
     public static final int NETWORK_TYPE_EHRPD = 14;
     /** Current network is HSPA+ */
     public static final int NETWORK_TYPE_HSPAP = 15;
+    /** Current network is GSM {@hide} */
+    public static final int NETWORK_TYPE_GSM = 16;
 
     /**
      * @return the NETWORK_TYPE_xxxx for current data connection.
@@ -722,6 +724,7 @@ public class TelephonyManager {
     public static int getNetworkClass(int networkType) {
         switch (networkType) {
             case NETWORK_TYPE_GPRS:
+            case NETWORK_TYPE_GSM:
             case NETWORK_TYPE_EDGE:
             case NETWORK_TYPE_CDMA:
             case NETWORK_TYPE_1xRTT:
@@ -788,6 +791,8 @@ public class TelephonyManager {
                 return "iDEN";
             case NETWORK_TYPE_HSPAP:
                 return "HSPA+";
+            case NETWORK_TYPE_GSM:
+                return "GSM";
             default:
                 return "UNKNOWN";
         }
