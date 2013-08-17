6eb07cadbb5c82633c64667488ed2b08f960f1f6
Kiril Mikos <kiril.mik.os@gmail.com>
2013-08-17 19:09:54 +0900

    Allow showing statusbar on top of fullscreen window.
    Swipe from top of the screen to show statusbar.
    Statusbar will automatically disappears after 2 sec.
    
    PS7: Also show statusbar when user moved finger far enough.
    PS8: Compatible with Expanded desktop.
    PS12:
      Fixed button in statusbar.
      Don't hide statusbar while expanded.
      Hide statusbar after collapsed.
      Disabled by default.
      Some small fixes.
    PS19: Works through PieService.
    PS20: Fixed class and variables naming.
    
    Change-Id: I59bd022be6eb065ec6b9361213cc4fddc065b925

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1756,7 +1756,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
         // don't allow expanding via e.g. service call while status bar is hidden
         // due to expanded desktop
-        if (getExpandedDesktopMode() == 2) {
+        if (getExpandedDesktopMode() == Settings.System.EXPANDED_DESKTOP_STATUS_BAR_HIDDEN
+                && !mExpandedVisible) {
             return;
         }
 
@@ -1835,7 +1836,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
         // don't allow expanding via e.g. service call while status bar is hidden
         // due to expanded desktop
-        if (getExpandedDesktopMode() == 2) {
+        if (getExpandedDesktopMode() == Settings.System.EXPANDED_DESKTOP_STATUS_BAR_HIDDEN
+                && !mExpandedVisible) {
             return;
         }
 
