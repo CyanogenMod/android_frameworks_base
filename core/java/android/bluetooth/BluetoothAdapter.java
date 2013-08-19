cbbe81cb826fa6d783dd42c847f452dbade7e688
Gaurav Asati <gasati@codeaurora.org>
2013-08-19 19:03:51 +0530

    Bluetooth: Support  HFP Client role on Bluedroid.
    Implementation changes to support HFP Client role
    on Bluedroid stack
    
    Change-Id: Ie37e95d3b0e3f785af818fb495c835e420792f4d
    (cherry picked from commit 072fad9a9a17c2a31660e7ac6cd6eba82d63fe50)
    (cherry picked from commit 559508238c2fe1f8f10049549b8a29053399407c)

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2013 The Linux Foundation. All rights reserved
+ * Not a Contribution.
  * Copyright (C) 2009 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -1198,6 +1200,9 @@ public final class BluetoothAdapter {
         } else if (profile == BluetoothProfile.MAP) {
             BluetoothMap map = new BluetoothMap(context, listener);
             return true;
+        } else if (profile == BluetoothProfile.HANDSFREE_CLIENT) {
+            BluetoothHandsfreeClient hfpclient = new BluetoothHandsfreeClient(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -1253,6 +1258,10 @@ public final class BluetoothAdapter {
             case BluetoothProfile.MAP:
                 BluetoothMap map = (BluetoothMap)proxy;
                 map.close();
+                break;
+            case BluetoothProfile.HANDSFREE_CLIENT:
+                BluetoothHandsfreeClient hfpclient = (BluetoothHandsfreeClient)proxy;
+                hfpclient.close();
                 break;
         }
     }
