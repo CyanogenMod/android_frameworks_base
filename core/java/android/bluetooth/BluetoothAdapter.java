cb903dc6e386923b1220808cad9e8a838b9e612f
Gaurav Asati <gasati@codeaurora.org>
2013-08-19 19:03:51 +0530

    Bluetooth: Support  HFP Client role on Bluedroid.
    Implementation changes to support HFP Client role
    on Bluedroid stack
    
    Change-Id: Ie37e95d3b0e3f785af818fb495c835e420792f4d
    (cherry picked from commit 072fad9a9a17c2a31660e7ac6cd6eba82d63fe50)
    (cherry picked from commit 559508238c2fe1f8f10049549b8a29053399407c)

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2013 The Linux Foundation. All rights reserved
+ * Not a Contribution.
  * Copyright (C) 2009 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -1206,6 +1208,9 @@ public final class BluetoothAdapter {
         } else if (profile == BluetoothProfile.MAP) {
             BluetoothMap map = new BluetoothMap(context, listener);
             return true;
+        } else if (profile == BluetoothProfile.HANDSFREE_CLIENT) {
+            BluetoothHandsfreeClient hfpclient = new BluetoothHandsfreeClient(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -1261,6 +1266,10 @@ public final class BluetoothAdapter {
             case BluetoothProfile.MAP:
                 BluetoothMap map = (BluetoothMap)proxy;
                 map.close();
+                break;
+            case BluetoothProfile.HANDSFREE_CLIENT:
+                BluetoothHandsfreeClient hfpclient = (BluetoothHandsfreeClient)proxy;
+                hfpclient.close();
                 break;
         }
     }
--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2013 The Linux Foundation. All rights reserved
+ * Not a Contribution.
  * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -108,6 +110,12 @@ public interface BluetoothProfile {
      * @hide
      */
     public static final int SAP = 20;
+
+     /**
+     * Handsfree Client - HFP HF Role
+     * @hide
+     */
+    public static final int HANDSFREE_CLIENT = 10;
 
     /**
      * Default priority for devices that we try to auto-connect to and
