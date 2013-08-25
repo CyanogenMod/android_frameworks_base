3990d275fc2697678dd8daed3f5dce65b410cfb1
Danesh Mondegarian <daneshm90@gmail.com>
2013-08-25 16:25:21 -0700

    QuickSettings : Fix update issue if linked
    Currently ribbon does not reflect updates to QS if they're linked.
    This patchset addresses that.
    
    Change-Id: I3787c30892c13dbcb7e5c48ff99cd2220b638302

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -121,6 +121,7 @@ public class QuickSettingsController {
     public PhoneStatusBar mStatusBarService;
     private final String mSettingsString;
     private boolean mHideLiveTiles;
+    private boolean mHideLiveTileLabels;
 
     private InputMethodTile mIMETile;
 
@@ -340,6 +341,11 @@ public class QuickSettingsController {
         loadTiles();
         setupBroadcastReceiver();
         setupContentObserver();
+        if (mHideLiveTileLabels) {
+            for (QuickSettingsTile t : mQuickSettingsTiles) {
+                t.setLabelVisibility(false);
+            }
+        }
     }
 
     void setupContentObserver() {
@@ -435,9 +441,9 @@ public class QuickSettingsController {
         }
     }
 
-    public void setTileTitleVisibility(boolean visible) {
-        for (QuickSettingsTile t : mQuickSettingsTiles) {
-            t.setLabelVisibility(visible);
+    public void hideLiveTileLabels(boolean hide) {
+        if (mHideLiveTileLabels != hide) {
+            mHideLiveTileLabels = hide;
         }
     }
 
