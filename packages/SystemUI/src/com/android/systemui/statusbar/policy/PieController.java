ae6a756881bf5de0425382e004ed6b5bed5d5ce4
Kiril Mikos <kiril.mik.os@gmail.com>
2013-08-27 21:33:30 +0900

    SystemUI: Fix wrong behavior of recents pie.
    Call preloadRecentTasksList when user moves over
    recents pie.
    
    Change-Id: Ic62343cb6ae9c5ab8bb90bf6eca4fb2d13493cac

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java
@@ -306,6 +306,9 @@ public class PieController implements BaseStatusBar.NavigationBarCallback, PieVi
 
     public void attachStatusBar(BaseStatusBar statusBar) {
         mStatusBar = statusBar;
+        if (mPieContainer != null) {
+            mPieContainer.attachStatusBar(statusBar);
+        }
     }
 
     private void setupContainer() {
@@ -313,6 +316,7 @@ public class PieController implements BaseStatusBar.NavigationBarCallback, PieVi
             mPieContainer = new PieView(mContext);
             mPieContainer.setOnSnapListener(this);
             mPieContainer.setOnExitListener(this);
+            mPieContainer.attachStatusBar(mStatusBar);
 
             if (mTelephonyManager != null) {
                 mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
