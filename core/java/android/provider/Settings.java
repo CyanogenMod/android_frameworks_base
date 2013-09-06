46f6e6a45ac4c93b88a85798b7645ac6cec5a62c
Danesh M <daneshm90@gmail.com>
2013-09-05 19:23:15 -0700

    Privacy Guard : Reimplement backed by AppOps
    Re-implements privacy guard to use appops.
    
    Change-Id: I2b52e8cf0d6e126fb156f83df885b5551503cd9d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5188,6 +5188,12 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Whether newly installed apps should run with privacy guard by default
+         * @hide
+         */
+         public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -5234,7 +5240,8 @@ public final class Settings {
             DIALPAD_AUTOCOMPLETE,
             LOCK_SCREEN_OWNER_INFO,
             LOCK_SCREEN_OWNER_INFO_ENABLED,
-            ADVANCED_REBOOT
+            ADVANCED_REBOOT,
+            PRIVACY_GUARD_DEFAULT
         };
 
         /**
