8c628809576788c26f6f2b258ee71761c9bc9169
Alex Yakavenka <ayakav@codeaurora.org>
2013-09-07 02:09:49 -0400

    Add onDataRatChanged trigger to DcTracker
    It is required to reload apns when data rat switches from
    gsm to cdma
    
    Change-Id: Ifdb04d0c8be325a226dbc707a3fa8c8c697089fa
    CRs-Fixed: 530277

--- telephony/java/com/android/internal/telephony/DctConstants.java
+++ telephony/java/com/android/internal/telephony/DctConstants.java
@@ -94,6 +94,7 @@ public class DctConstants {
     public static final int EVENT_DISCONNECT_DC_RETRYING = BASE + 34;
     public static final int EVENT_DATA_SETUP_COMPLETE_ERROR = BASE + 35;
     public static final int CMD_SET_ENABLE_FAIL_FAST_MOBILE_DATA = BASE + 36;
+    public static final int EVENT_DATA_RAT_CHANGED = BASE + 39;
 
     /***** Constants *****/
 
