0943143ae5400213e08dabb3383f78bb47fef83c
Aleksander Ciepiela <alekciepiela@gmail.com>
2013-09-10 19:53:54 +0200

    SystemUI/Added ADB over WiFi QuickSettings Tile
    Part 1. It also needs changes to Settings app which is a separate commit
    
    Change-Id: I340846a171571a7b61d2c68f2a6875794be5d5b2

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/WiFiADBTile.java
@@ -0,0 +1,87 @@
+
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.NetworkUtils;
+import android.net.Uri;
+import android.net.wifi.IWifiManager;
+import android.net.wifi.WifiInfo;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+
+public class WiFiADBTile extends QuickSettingsTile {
+    private static final String TAG = "WiFiAdbTile";
+
+    public WiFiADBTile(Context context, QuickSettingsController qsc) {
+        super(context, qsc);
+
+        mOnClick = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.Secure.putInt(mContext.getContentResolver(), Settings.Secure.ADB_PORT,
+                        !getEnabled() ? 5555 : -1);
+            }
+        };
+
+        mOnLongClick = new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                startSettingsActivity(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+                return true;
+            }
+        };
+
+        qsc.registerObservedContent(Settings.Secure.getUriFor(Settings.Secure.ADB_PORT), this);
+        updateResources();
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        super.updateResources();
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateResources();
+    }
+
+    private synchronized void updateTile() {
+        if (getEnabled()) {
+            WifiInfo wifiInfo = null;
+            IWifiManager wifiManager = IWifiManager.Stub.asInterface(ServiceManager
+                    .getService(Context.WIFI_SERVICE));
+            try {
+                wifiInfo = wifiManager.getConnectionInfo();
+            } catch (RemoteException e) {
+                Log.e(TAG, "wifiManager, getConnectionInfo()", e);
+            }
+            if (wifiInfo != null) {
+                // if adb over wifi is enabled and wifiInfo is not null set the
+                // label to "hostAddress:port" and drawable to the enabled icon
+                String hostAddress = NetworkUtils.intToInetAddress(wifiInfo.getIpAddress())
+                        .getHostAddress();
+                mLabel = hostAddress + ":5555";
+                mDrawable = R.drawable.ic_qs_wifi_adb_on;
+                return;
+            }
+        }
+        // Otherwise set the disabled label and icon
+        mLabel = mContext.getString(R.string.quick_settings_wifiadb_disabled_label);
+        mDrawable = R.drawable.ic_qs_wifi_adb_off;
+    }
+
+    private boolean getEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(), Settings.Secure.ADB_PORT, 0) > 0;
+    }
+
+}
