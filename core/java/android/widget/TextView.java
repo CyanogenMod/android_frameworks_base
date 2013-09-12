877e645c6caab41ad7906d2c584b433d376921f0
Paul Quei <paulquei@gmail.com>
2013-09-12 22:30:39 +0800

    Fix "Email" App data lost when screen rotated.
    Bug Description:
    1.Open "Email" App contained in AOSP.
    2.Setup your email account if not.
    3.Click "Compose" button on the left bottom.
    4.In "To", enter more than 3 email addresses, like "test1@gmail.com,test2@gmail.com,test3@gmail.com,test4@gmail.com,test5@gmail".
    5.Click "Subject" or "Compose email", and the "To" data will be shrank.It will be like "test1@gmail.com test2@gmail.com +3". The "+3" means there are 3 more addresses shrank.
    6.Rotate screen, either from vertical to horizontal or from horizontal to vertical.
    7.The "To" data is lost, it will be "test1@gmail.com test2@gmail +1".When you click to expand, there are only 3 addresses left.
    Why and how to solve:
    Data lost when screen rotated because RecipientEditTextView doesn't save data on orientation changed.
    So, create a static class save the data when needed and restore later on.
    
    Change-Id: I0c709cbb59515338aa903c611139aee727785670
    Signed-off-by: Paul Quei <paulquei@gmail.com>

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -8875,7 +8875,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
         boolean frozenWithFocus;
         CharSequence error;
 
-        SavedState(Parcelable superState) {
+        public SavedState(Parcelable superState) {
             super(superState);
         }
 
@@ -8918,7 +8918,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
             }
         };
 
-        private SavedState(Parcel in) {
+        public SavedState(Parcel in) {
             super(in);
             selStart = in.readInt();
             selEnd = in.readInt();
