e5b3253d07c1109866b9273e34d0fda4dadcdea0
Omprakash Dhyade <odhyade@codeaurora.org>
2013-09-11 20:03:54 -0700

    Perf: Add cpu boost for overscroller fling.
    Use perflock api's to add cpu boost for
    overscroller fling. 2 Cores are made online
    with minumum 1.1GHz nominal cpu freq for the
    duration of the fling.
    Cpu boost will be controlled by
    config_enableCpuBoostForOverScrollerFling.
    Default value of config is false and to turn
    on use device specific overlay files.
    
    Change-Id: If0a545e126d157d7e2fdaecc1108d41469172cd6

--- core/java/android/widget/OverScroller.java
+++ core/java/android/widget/OverScroller.java
@@ -23,6 +23,7 @@ import android.util.Log;
 import android.view.ViewConfiguration;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
+import org.codeaurora.Performance;
 
 /**
  * This class encapsulates scrolling with the ability to overshoot the bounds
@@ -601,6 +602,15 @@ public class OverScroller {
         private static final int CUBIC = 1;
         private static final int BALLISTIC = 2;
 
+        /*
+         * Perf boost related variables
+         * Enabled/Disabled using config_enableCpuBoostForOverScrollerFling
+         * true value turns it on, by default will be turned off
+         */
+        private Performance mPerf = null;
+        private boolean mIsPerfLockAcquired = false;
+        private boolean mIsPerfBoostEnabled = false;
+
         static {
             float x_min = 0.0f;
             float y_min = 0.0f;
@@ -645,6 +655,9 @@ public class OverScroller {
                     * 39.37f // inch/meter
                     * ppi
                     * 0.84f; // look and feel tuning
+
+            mIsPerfBoostEnabled = context.getResources().getBoolean(
+                   com.android.internal.R.bool.config_enableCpuBoostForOverScrollerFling);
         }
 
         void updateScroll(float q) {
@@ -692,6 +705,11 @@ public class OverScroller {
         }
 
         void finish() {
+            if (mIsPerfLockAcquired && mPerf != null) {
+                mPerf.perfLockRelease();
+                mIsPerfLockAcquired = false;
+            }
+
             mCurrentPosition = mFinal;
             // Not reset since WebView relies on this value for fast fling.
             // TODO: restore when WebView uses the fast fling implemented in this class.
@@ -762,6 +780,14 @@ public class OverScroller {
             if (velocity != 0) {
                 mDuration = mSplineDuration = getSplineFlingDuration(velocity);
                 totalDistance = getSplineFlingDistance(velocity);
+                if (mPerf == null && mIsPerfBoostEnabled) {
+                    mPerf = new Performance();
+                }
+
+                if (mPerf != null) {
+                    mIsPerfLockAcquired = true;
+                    mPerf.perfLockAcquire(mDuration, mPerf.CPUS_ON_2, 0x20B, 0x30B);
+                }
             }
 
             mSplineDistance = (int) (totalDistance * Math.signum(velocity));
