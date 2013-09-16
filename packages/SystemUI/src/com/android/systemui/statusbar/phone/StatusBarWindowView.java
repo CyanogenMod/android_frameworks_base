b291f2f86cb4f81a108a1b3a9ea67435b55e03c9
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-09-16 12:51:01 +0300

    SystemUI: do not collapse panels if keyevent was canceled.
    Change-Id: I9af82e9fa2305142521075938dfd05521adeb9f4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -85,7 +85,7 @@ public class StatusBarWindowView extends FrameLayout
         boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         switch (event.getKeyCode()) {
         case KeyEvent.KEYCODE_BACK:
-            if (!down) {
+            if (!down && !event.isCanceled()) {
                 mService.animateCollapsePanels();
             }
             return true;
