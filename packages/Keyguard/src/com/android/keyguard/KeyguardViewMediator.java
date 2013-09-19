bd65daf5b368240fcbf16a8eee23209fbee4c10e
kaiyiz <kaiyiz@codeaurora.org>
2013-09-19 16:03:30 +0800

    Keyguard: PIN lock input views flash when SIM status ready
    When airplane mode is turned off and DSDS mode is on, cat service
    will request RIL to update the SIM card status. When the broadcast
    of SIM ready status is received, keyguardview will be reset. So the
    PIN lock input view will flash and bring bad user experience.
    
    As there is nothing to update by resetting keyguardview while SIM
    is ready. So dismiss the ready status of SIM card.
    
    CRs-Fixed: 537172
    
    Change-Id: I510e8f1e2f08cb946fe47f02b015ae5bf89d5025

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -433,7 +433,7 @@ public class KeyguardViewMediator {
                     break;
                 case READY:
                     synchronized (this) {
-                        if (isShowing()) {
+                        if (isShowing() && !isSecure()) {
                             resetStateLocked(null);
                         }
                     }
