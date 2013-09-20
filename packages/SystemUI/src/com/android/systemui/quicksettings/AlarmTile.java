c9b3d4318917e39895e98e2edb94d1d71e13ff2f
Danny Baumann <dannybaumann@web.de>
2013-09-20 08:22:03 +0200

    Use generic alarm intent.
    Change-Id: Ibb911c1e34ec95cdd52867b8a68cce25f09dbc10

--- packages/SystemUI/src/com/android/systemui/quicksettings/AlarmTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/AlarmTile.java
@@ -1,12 +1,12 @@
 package com.android.systemui.quicksettings;
 
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.provider.AlarmClock;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -25,11 +25,7 @@ public class AlarmTile extends QuickSettingsTile {
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setComponent(new ComponentName(
-                        "com.android.deskclock",
-                        "com.android.deskclock.AlarmClock"));
-                startSettingsActivity(intent);
+                startSettingsActivity(new Intent(AlarmClock.ACTION_SET_ALARM));
             }
         };
 
