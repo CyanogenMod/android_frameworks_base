d41a61f865dc69cb17d1a2b3841d84cdeb60388e
Alok Kediya <kediya@codeaurora.org>
2013-09-25 14:30:36 +0530

    Camera: Add support for QC camera features.
    Added APIs and keys for all QC features. This
    will enable application to get/set paramters.
    (cherrypicked from commit d0c5a0ee7e9af41320fed00ff324c6d0f22eb697)
    Change-Id: I68c23aaf7267645efeb4d4743b390a048292b7fd
    (cherry picked from commit ad2fd19b7ed6a610cab4bd3ae261178bcafa66af)
    (cherry picked from commit cfcebdf9c402bb5f1976f07ba5dc1e15f5811fc2)
    (cherry picked from commit 4ef4a5e937b2ec7355c5893c80dbc103d163364d)

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -90,24 +90,25 @@ public class CamcorderProfile
      */
     public static final int QUALITY_QVGA = 7;
 
-    /**
-     * {@hide}
+    /** @hide
+     * Quality level corresponding to the FWVGA resolution.
      */
     public static final int QUALITY_FWVGA = 8;
-    /**
-     * {@hide}
+    /** @hide
+     * Quality level corresponding to the WVGA resolution.
      */
     public static final int QUALITY_WVGA = 9;
 
-    /**
-     * {@hide}
+    /** @hide
+     * Quality level corresponding to the VGA resolution.
      */
     public static final int QUALITY_VGA = 10;
 
-    /**
-     * {@hide}
+    /** @hide
+     * Quality level corresponding to the WQVGA resolution.
      */
     public static final int QUALITY_WQVGA = 11;
+
 
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
