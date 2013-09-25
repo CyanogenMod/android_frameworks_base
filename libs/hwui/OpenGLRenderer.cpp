b7236d63a153eb789408c4edd055037b0fdb4252
Rama Vaddula <rvaddula@codeaurora.org>
2013-09-25 15:55:25 -0700

    Remove opaque check in preparing dirty region
    Since preserve swap is enabled, we need to clear the color buffer
    when the scissor rect is prepared for a new process. This prevents
    garbage being present from the previous process in the color buffer.
    
    CRs-Fixed: 549755
    
    Change-Id: Icd12ae388077b8c9ed329c37314e896500078543

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -181,6 +181,16 @@ void OpenGLRenderer::discardFramebuffer(float left, float top, float right, floa
 }
 
 void OpenGLRenderer::clear(float left, float top, float right, float bottom, bool opaque) {
+#ifdef QCOM_BSP_LEGACY
+    mRenderState.scissor().setEnabled(true);
+    mRenderState.scissor().set(left, getViewportHeight() - bottom, right - left, bottom - top);
+    glClear(GL_COLOR_BUFFER_BIT);
+    mDirty = true;
+    if (opaque) {
+        mRenderState.scissor().reset();
+        return;
+    }
+#else
     if (!opaque) {
         mRenderState.scissor().setEnabled(true);
         mRenderState.scissor().set(left, getViewportHeight() - bottom, right - left, bottom - top);
@@ -190,6 +200,7 @@ void OpenGLRenderer::clear(float left, float top, float right, float bottom, boo
     }
 
     mRenderState.scissor().reset();
+#endif
 }
 
 void OpenGLRenderer::startTilingCurrentClip(bool opaque, bool expand) {
