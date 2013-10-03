b957ac6af6eff790f5ccd40796d4ed42ce0abd0a
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-10-03 19:35:39 +0530

    Telephony(MSIM): Add support for MultiSim
    Make changes to add support for MultiSim.
      -Add TelephonyManager and TelePhonyRegistry for MultiSim
      -Add ITelephony and IPhoneSubinfo aidl files for MultiSim
      -Extend ShutdownThread to close all radio's for MultiSim
      -Add Constants and Utils used for MultiSim
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings.
    
    Change Default subscription property name.
     - Change default subscription property name from
       persist.default.subscription to persist.radio.default.sub so
       that it can be set by radio group access processes.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide database flags as per subscription for both Mobile
       data and Data roaming options.
     - Add function to provide DDS susbcriber id.
    
    Set priority when send sms
     - Add the priority support in 3gpp2 text message
    
    Add support for emergency calls
     -Populate ecclist by reading ril.ecclist corresponds to
      each subscription.
     -If dialed number is present in consolidated ecclist then
      mark as emergency number.
    
    Define invalid subscription constant
     -Define a new constant for invalid subscription id.
    
    Third Party Intent Handling
     -Remove firing of below intents from TelephonyRegistry when
      Multisim is enabled, as they will be fired from MSimTelephonyReistry
      ACTION_SERVICE_STATE_CHANGED
      ACTION_SIGNAL_STRENGTH_CHANGED
      ACTION_ANY_DATA_CONNECTION_STATE_CHANGED
      ACTION_DATA_CONNECTION_FAILED
     -Remove below intent from both TelephonyRegistry & MSimTelephonyReistry
      as it will be fired in MSimDefaultNotifier.
       ACTION_PHONE_STATE_CHANGED
    
    Change-Id: I6ee5d2a22e44fe7d0d68a41af0481ddbbbe21c3f
    (cherry picked from commit 1014515bac8729ecc20493f763e00a3953c738e0)
    (cherry picked from commit 021c08f412bc6117f9b139376371f3344d90abfb)

--- Android.mk
+++ Android.mk
@@ -282,9 +282,12 @@ LOCAL_SRC_FILES += \
 	media/java/android/media/IRingtonePlayer.aidl \
 	telephony/java/com/android/internal/telephony/IPhoneStateListener.aidl \
 	telephony/java/com/android/internal/telephony/IPhoneSubInfo.aidl \
+	telephony/java/com/android/internal/telephony/msim/IPhoneSubInfoMSim.aidl \
 	telephony/java/com/android/internal/telephony/ITelephony.aidl \
+	telephony/java/com/android/internal/telephony/msim/ITelephonyMSim.aidl \
 	telephony/java/com/android/internal/telephony/ISms.aidl \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
+	telephony/java/com/android/internal/telephony/ITelephonyRegistryMSim.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
 	wifi/java/android/net/wifi/IWifiManager.aidl \
 	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl \
@@ -420,7 +423,9 @@ aidl_files := \
 	frameworks/base/location/java/com/android/internal/location/ProviderRequest.aidl \
 	frameworks/base/telephony/java/android/telephony/ServiceState.aidl \
 	frameworks/base/telephony/java/com/android/internal/telephony/IPhoneSubInfo.aidl \
+	frameworks/base/telephony/java/com/android/internal/telephony/msim/IPhoneSubInfoMSim.aidl \
 	frameworks/base/telephony/java/com/android/internal/telephony/ITelephony.aidl \
+    frameworks/base/telephony/java/com/android/internal/telephony/msim/ITelephonyMSim.aidl \
 	frameworks/base/wifi/java/android/net/wifi/BatchedScanSettings.aidl \
 	frameworks/base/wifi/java/android/net/wifi/BatchedScanResult.aidl \
 
