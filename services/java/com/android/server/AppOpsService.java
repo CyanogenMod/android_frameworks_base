0adf3a7269f862b03020b9ea98af49f3c814e9ac
Sam Mortimer <sam@mortimer.me.uk>
2013-10-02 22:06:42 -0700

    AppOps: Add per Op allowed and ignored counters
    Change-Id: Ifb3d25e17dbab082b816a0a655b4796a83af336b

--- services/java/com/android/server/AppOpsService.java
+++ services/java/com/android/server/AppOpsService.java
@@ -136,6 +136,8 @@ public class AppOpsService extends IAppOpsService.Stub {
         public int nesting;
         public int askNesting;
         public PermissionDialogResult dialogResult;
+        public int allowedCount;
+        public int ignoredCount;
 
         public Op(int _uid, String _packageName, int _op, boolean strict) {
             uid = _uid;
@@ -334,7 +336,8 @@ public class AppOpsService extends IAppOpsService.Stub {
             for (int j=0; j<pkgOps.size(); j++) {
                 Op curOp = pkgOps.valueAt(j);
                 resOps.add(new AppOpsManager.OpEntry(curOp.op, curOp.mode, curOp.time,
-                        curOp.rejectTime, curOp.duration));
+                        curOp.rejectTime, curOp.duration,
+                        curOp.allowedCount, curOp.ignoredCount));
             }
         } else {
             for (int j=0; j<ops.length; j++) {
@@ -344,7 +347,8 @@ public class AppOpsService extends IAppOpsService.Stub {
                         resOps = new ArrayList<AppOpsManager.OpEntry>();
                     }
                     resOps.add(new AppOpsManager.OpEntry(curOp.op, curOp.mode, curOp.time,
-                            curOp.rejectTime, curOp.duration));
+                            curOp.rejectTime, curOp.duration,
+                            curOp.allowedCount, curOp.ignoredCount));
                 }
             }
         }
@@ -612,11 +616,13 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (DEBUG) Log.d(TAG, "recordOperation: reject #" + mode + " for code "
                         + switchCode + " (" + code + ") uid " + uid + " package " + packageName);
                 op.rejectTime = System.currentTimeMillis();
+                op.ignoredCount++;
             } else if (mode == AppOpsManager.MODE_ALLOWED) {
                 if (DEBUG) Log.d(TAG, "recordOperation: allowing code " + code + " uid " + uid
                         + " package " + packageName);
                 op.time = System.currentTimeMillis();
                 op.rejectTime = 0;
+                op.allowedCount++;
             }
         }
     }
@@ -1177,6 +1183,14 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (dur != null) {
                     op.duration = Integer.parseInt(dur);
                 }
+                String allowed = parser.getAttributeValue(null, "ac");
+                if (allowed != null) {
+                    op.allowedCount = Integer.parseInt(allowed);
+                }
+                String ignored = parser.getAttributeValue(null, "ic");
+                if (ignored != null) {
+                    op.ignoredCount = Integer.parseInt(ignored);
+                }
                 HashMap<String, Ops> pkgOps = mUidOps.get(uid);
                 if (pkgOps == null) {
                     pkgOps = new HashMap<String, Ops>();
@@ -1248,6 +1262,14 @@ public class AppOpsService extends IAppOpsService.Stub {
                             int dur = op.getDuration();
                             if (dur != 0) {
                                 out.attribute(null, "d", Integer.toString(dur));
+                            }
+                            int allowed = op.getAllowedCount();
+                            if (allowed != 0) {
+                                out.attribute(null, "ac", Integer.toString(allowed));
+                            }
+                            int ignored = op.getIgnoredCount();
+                            if (ignored != 0) {
+                                out.attribute(null, "ic", Integer.toString(ignored));
                             }
                             out.endTag(null, "op");
                         }
@@ -1385,6 +1407,28 @@ public class AppOpsService extends IAppOpsService.Stub {
             int switchOp = AppOpsManager.opToSwitch(op);
             setMode(switchOp, uid, packageName, state
                     ? AppOpsManager.MODE_IGNORED : AppOpsManager.MODE_ALLOWED);
+        }
+    }
+
+    @Override
+    public void resetCounters() {
+        mContext.enforcePermission(android.Manifest.permission.UPDATE_APP_OPS_STATS,
+                Binder.getCallingPid(), Binder.getCallingUid(), null);
+        synchronized (this) {
+            for (int i=0; i<mUidOps.size(); i++) {
+                HashMap<String, Ops> packages = mUidOps.valueAt(i);
+                for (Map.Entry<String, Ops> ent : packages.entrySet()) {
+                    String packageName = ent.getKey();
+                    Ops pkgOps = ent.getValue();
+                    for (int j=0; j<pkgOps.size(); j++) {
+                        Op curOp = pkgOps.valueAt(j);
+                        curOp.allowedCount = 0;
+                        curOp.ignoredCount = 0;
+                    }
+                }
+            }
+            // ensure the counter reset persists
+            scheduleWriteNowLocked();
         }
     }
 }
