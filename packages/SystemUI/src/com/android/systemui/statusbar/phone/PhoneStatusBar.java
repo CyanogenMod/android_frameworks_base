17fdd8c247cde5bb0af389e9f4ba01caff768da4
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-05 23:13:52 +0200

    QS: Make it possible to show smaller icons (1/2)
    * Remove text and show more icons on one row
    
    Change-Id: Ie247aba1ae5fbdb8f5298e30fb2f04a985e73532

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3091,6 +3091,14 @@ public class PhoneStatusBar extends BaseStatusBar {
                     cleanupRibbon();
                     inflateRibbon();
                     mRibbonView.setVisibility(View.VISIBLE);
+             
+            } else if (uri != null && uri.equals(Settings.System.getUriFor(
+                    Settings.System.QUICK_SETTINGS_SHOW_LABELS))) {
+                if (mSettingsContainer != null) {
+                    mQS.setupQuickSettings();
+                    mSettingsContainer.updateResources();
+                }
+            
             } else if (mSettingsContainer != null) {
                 mQS.setupQuickSettings();
                 if (mQuickAccessLayoutLinked && mRibbonQS != null) {
@@ -3140,6 +3148,11 @@ public class PhoneStatusBar extends BaseStatusBar {
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_RIBBON_TILES),
                     false, this, UserHandle.USER_ALL);
+
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUICK_SETTINGS_SHOW_LABELS),
+                    false, this, UserHandle.USER_ALL);
+
         }
     }
 }
