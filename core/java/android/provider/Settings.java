1e78b1e9564cb128c3ad6e99ac3bde3c186a2b93
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-05 23:13:52 +0200

    QS: Make it possible to show smaller icons (1/2)
    * Remove text and show more icons on one row
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Change-Id: Ie247aba1ae5fbdb8f5298e30fb2f04a985e73532

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2736,6 +2736,14 @@ public final class Settings {
          */
         public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
 
+
+        /**
+         * Quick Settings show text under icon
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_SHOW_LABELS = "qs_show_text";
+
         /**
          * Quick Settings Panel Dynamic Tiles
          *
