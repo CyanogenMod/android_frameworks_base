c80d34f015c7db1c2aa98112dbc626d14b017071
Susheel nyamala <snyamala@codeaurora.org>
2013-10-07 21:43:15 +0530

    Property constants declaration for OMH
    Added RIL request declaration to query OMH profile
    Added event to be broadcasted on OMH profile read complete
    
    Conflicts:
    	telephony/java/com/android/internal/telephony/RILConstants.java
    
    Change-Id: Idfeb2f3f1a3aa0403a7ff0ab7c69fc945d9e7083
    Conflicts:
    telephony/java/com/android/internal/telephony/DctConstants.java

--- telephony/java/com/android/internal/telephony/DctConstants.java
+++ telephony/java/com/android/internal/telephony/DctConstants.java
@@ -1,5 +1,8 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ *
+ * Not a Contribution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,6 +103,7 @@ public class DctConstants {
     public static final int CMD_NET_STAT_POLL = BASE + 40;
     public static final int EVENT_DATA_RAT_CHANGED = BASE + 41;
     public static final int CMD_CLEAR_PROVISIONING_SPINNER = BASE + 42;
+    public static final int EVENT_MODEM_DATA_PROFILE_READY = BASE + 43;
 
     /***** Constants *****/
 
