c819e5c6f21f99392ba0e39411076975b7982cd1
Surajit Podder <spodder@codeaurora.org>
2013-10-08 00:29:16 +0530

    video: Add support for extended media listener
    Added changes to handle additional message types for
    dash playback.
    
    Change-Id: Id07828866de8cae44137f26c1e74417aaa45704f

--- media/jni/Android.mk
+++ media/jni/Android.mk
@@ -11,6 +11,7 @@ LOCAL_SRC_FILES:= \
     android_media_MediaHTTPConnection.cpp \
     android_media_MediaMuxer.cpp \
     android_media_MediaPlayer.cpp \
+    android_media_ExtMediaPlayer.cpp \
     android_media_MediaRecorder.cpp \
     android_media_MediaScanner.cpp \
     android_media_MediaMetadataRetriever.cpp \
