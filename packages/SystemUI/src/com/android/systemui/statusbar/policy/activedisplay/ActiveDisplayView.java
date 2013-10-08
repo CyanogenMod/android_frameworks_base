b5ec5e435838a56a9b8e3b806663038da2aa6f0c
0xD34D <clark@scheffsblend.com>
2013-10-07 18:24:33 -0700

    Active display: Don't show active display if call state is not idle.
    This will prevent active display from displaying when a call is
    coming in and let the call screen show up instead of active display.
    
    Change-Id: I2cb79e097ae24dc6ad6c8a33be88eb19f7d8d235

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -921,7 +921,7 @@ public class ActiveDisplayView extends FrameLayout {
      * @return True if it should be used, false otherwise.
      */
     private boolean isValidNotification(StatusBarNotification sbn) {
-        return ((sbn.isClearable() || mShowAllNotifications)
+        return (!isOnCall() && (sbn.isClearable() || mShowAllNotifications)
                 && !(mHideLowPriorityNotifications && sbn.getNotification().priority < HIDE_NOTIFICATIONS_BELOW_SCORE));
     }
 
