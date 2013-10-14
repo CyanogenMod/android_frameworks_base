0002d3845964a421b8eb1b3cc7a6eb695c5901f4
Jorge Ruesga <jorge@ruesga.com>
2013-10-14 02:24:55 +0200

    Frameworks: QS performance profiles tile
    Patchset 2: Fix not null comparation
    Patchset 3: xxhdpi drawables
    
    Change-Id: Ib32d3734b63f88a3473e0ab10d8edf30dae9ceb6
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -43,6 +43,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.dreams.DreamService;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Log;
@@ -104,6 +105,28 @@ class ServerThread extends Thread {
                 Settings.Secure.ADB_PORT, 0);
             // setting this will control whether ADB runs on TCP/IP or USB
             SystemProperties.set("service.adb.tcp.port", Integer.toString(adbPort));
+        }
+    }
+
+    private class PerformanceProfileObserver extends ContentObserver {
+        private final String mPropName;
+        private final String mPropDef;
+
+        public PerformanceProfileObserver(Context ctx) {
+            super(null);
+            mPropName =
+                    ctx.getString(com.android.internal.R.string.config_perf_profile_prop);
+            mPropDef =
+                    ctx.getString(com.android.internal.R.string.config_perf_profile_default_entry);
+        }
+        @Override
+        public void onChange(boolean selfChange) {
+            String perfProfile = Settings.System.getString(mContentResolver,
+                Settings.System.PERFORMANCE_PROFILE);
+            if (perfProfile == null) {
+                perfProfile = mPropDef;
+            }
+            SystemProperties.set(mPropName, perfProfile);
         }
     }
 
@@ -862,6 +885,12 @@ class ServerThread extends Thread {
         mContentResolver.registerContentObserver(
             Settings.Secure.getUriFor(Settings.Secure.ADB_PORT),
             false, new AdbPortObserver());
+        if (!TextUtils.isEmpty(context.getString(
+                com.android.internal.R.string.config_perf_profile_prop))) {
+            mContentResolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.PERFORMANCE_PROFILE),
+                    false, new PerformanceProfileObserver(context));
+        }
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
