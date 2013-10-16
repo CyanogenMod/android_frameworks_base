73cfd17f786d254044cfc4e474263b4317a4c9fb
jppeniche <jp.penixe@gmail.com>
2013-10-16 10:39:31 +0200

    Active-Display: Make GlowPadView center the current notification icon for us
    ps2: make scaling both smarter and stupider SIMULTANEOUSLY

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -147,6 +147,8 @@ public class GlowPadView extends View {
     private TargetDrawable mOuterRing;
     private Vibrator mVibrator;
 
+	private TargetDrawable mCenterDrawable; 
+	
     private int mFeedbackCount = 3;
     private int mVibrationDuration = 0;
     private int mGrabbedState;
@@ -281,6 +283,8 @@ public class GlowPadView extends View {
         TypedValue handle = a.peekValue(R.styleable.GlowPadView_handleDrawable);
         mHandleDrawable = new TargetDrawable(res, handle != null ? handle.resourceId : 0);
         mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
+		mCenterDrawable = new TargetDrawable(res, 0);
+        mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE); 
         mOuterRing = new TargetDrawable(res,
                 getResourceId(a, R.styleable.GlowPadView_outerRingDrawable));
 
@@ -430,6 +434,8 @@ public class GlowPadView extends View {
                 startBackgroundAnimation(0, 0.0f);
                 mHandleDrawable.setState(TargetDrawable.STATE_INACTIVE);
                 mHandleDrawable.setAlpha(1.0f);
+				mCenterDrawable.setState(TargetDrawable.STATE_INACTIVE);
+                mCenterDrawable.setAlpha(1.0f); 
                 mPaintText.setAlpha(255);
                 break;
 
@@ -440,6 +446,7 @@ public class GlowPadView extends View {
             case STATE_FIRST_TOUCH:
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+				mCenterDrawable.setAlpha(0.0f); 
                 deactivateTargets();
                 showTargets(true);
                 startBackgroundAnimation(INITIAL_SHOW_HANDLE_DURATION, 1.0f);
@@ -452,6 +459,7 @@ public class GlowPadView extends View {
             case STATE_TRACKING:
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+				mCenterDrawable.setAlpha(0.0f); 
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 1.0f, null);
                 break;
 
@@ -459,6 +467,7 @@ public class GlowPadView extends View {
                 // TODO: Add transition states (see list_selector_background_transition.xml)
                 mPaintText.setAlpha(0);
                 mHandleDrawable.setAlpha(0.0f);
+				mCenterDrawable.setAlpha(0.0f); 
                 showGlow(REVEAL_GLOW_DURATION , REVEAL_GLOW_DELAY, 0.0f, null);
                 break;
 
@@ -1231,6 +1240,8 @@ public class GlowPadView extends View {
 
         mHandleDrawable.setPositionX(newWaveCenterX);
         mHandleDrawable.setPositionY(newWaveCenterY);
+		mCenterDrawable.setPositionX(newWaveCenterX);
+        mCenterDrawable.setPositionY(newWaveCenterY); 
 
         updateTargetPositions(newWaveCenterX, newWaveCenterY);
         updatePointCloudPosition(newWaveCenterX, newWaveCenterY);
@@ -1298,6 +1309,7 @@ public class GlowPadView extends View {
                 target.draw(canvas);
             }
         }
+		mCenterDrawable.draw(canvas); 
         mHandleDrawable.draw(canvas);
 
         if (!TextUtils.isEmpty(mHandleText) && mPaintText.getAlpha() != 0) {
@@ -1323,7 +1335,13 @@ public class GlowPadView extends View {
                 text, new TextPaint(mPaintText), mMaxTextArcLength,
                 TextUtils.TruncateAt.END).toString();
     }
-
+ 
+	public void setCenterDrawable(TargetDrawable d) {
+		mCenterDrawable = new TargetDrawable(d);
+		mCenterDrawable.setPositionX(mWaveCenterX);
+		mCenterDrawable.setPositionY(mWaveCenterY);
+	}
+ 
     public void setDrawOuterRing(boolean drawOuterRing) {
         mDrawOuterRing = drawOuterRing;
     }
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -116,7 +116,6 @@ public class ActiveDisplayView extends FrameLayout {
     private GlowPadView mGlowPadView;
     private View mRemoteView;
     private View mClock;
-    private ImageView mCurrentNotificationIcon;
     private FrameLayout mRemoteViewLayout;
     private FrameLayout mContents;
     private ObjectAnimator mAnim;
@@ -219,7 +218,6 @@ public class ActiveDisplayView extends FrameLayout {
         }
 
         public void onReleased(final View v, final int handle) {
-            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 1f).start();
             doTransition(mOverflowNotifications, 1.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 0f).start();
@@ -233,7 +231,6 @@ public class ActiveDisplayView extends FrameLayout {
             // prevent the ActiveDisplayView from turning off while user is interacting with it
             cancelTimeoutTimer();
             restoreBrightness();
-            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 0f).start();
             doTransition(mOverflowNotifications, 0.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 1f).start();
@@ -449,7 +446,7 @@ public class ActiveDisplayView extends FrameLayout {
 
         mRemoteViewLayout = (FrameLayout) contents.findViewById(R.id.remote_content_parent);
         mClock = contents.findViewById(R.id.clock_view);
-        mCurrentNotificationIcon = (ImageView) contents.findViewById(R.id.current_notification_icon);
+  
 
         mOverflowNotifications = (LinearLayout) contents.findViewById(R.id.keyguard_other_notifications);
         mOverflowNotifications.setOnTouchListener(mOverflowTouchListener);
@@ -967,7 +964,10 @@ public class ActiveDisplayView extends FrameLayout {
         } catch (Resources.NotFoundException nfe) {
             mNotificationDrawable = mContext.getResources().getDrawable(R.drawable.ic_ad_unknown_icon);
         }
-        mCurrentNotificationIcon.setImageDrawable(mNotificationDrawable);
+		 TargetDrawable centerDrawable = new TargetDrawable(getResources(),createCenterDrawable(mNotificationDrawable));
+            centerDrawable.setScaleX(0.9f);
+            centerDrawable.setScaleY(0.9f);
+            mGlowPadView.setCenterDrawable(centerDrawable); 
         setHandleText(sbn);
         mNotification = sbn;
         mGlowPadView.post(new Runnable() {
@@ -1033,6 +1033,13 @@ public class ActiveDisplayView extends FrameLayout {
         return stateListDrawable;
     }
 
+	 private Drawable createCenterDrawable(Drawable handle) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(TargetDrawable.STATE_INACTIVE, handle);
+        
+        return stateListDrawable;
+    }
+ 
     private SensorEventListener mSensorListener = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent event) {
