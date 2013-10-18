1f7e4adc599c8ed3528cc0bee5d792a3cea88687
Mahesh A Saptasagar <msapta@codeaurora.org>
2013-10-18 14:34:40 +0530

    Revert "p2p: Fix for discovery failure until AP is connected in WFD client"
    This reverts commit 738434a7df1cf7d0aada02e7905bdb4a51b5d3d6.
    
    Change-Id: I4c1f061aa26077134029527dfc3e55112fed5038
    CRs-Fixed: 532138

--- wifi/java/android/net/wifi/p2p/WifiP2pService.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pService.java
@@ -975,11 +975,15 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                     boolean blocked = (message.arg1 == ENABLED ? true : false);
                     if (mDiscoveryBlocked == blocked) break;
                     mDiscoveryBlocked = blocked;
-                    if (blocked) {
-                        if (mDiscoveryStarted) {
-                            mWifiNative.p2pStopFind();
-                        }
+                    if (blocked && mDiscoveryStarted) {
+                        mWifiNative.p2pStopFind();
                         mDiscoveryPostponed = true;
+                    }
+                    if (!blocked && mDiscoveryPostponed) {
+                        mDiscoveryPostponed = false;
+                        mWifiNative.p2pFind(DISCOVER_TIMEOUT_S);
+                    }
+                    if (blocked) {
                         try {
                             StateMachine m = (StateMachine)message.obj;
                             m.sendMessage(message.arg2);
@@ -987,17 +991,12 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                             loge("unable to send BLOCK_DISCOVERY response: " + e);
                         }
                     }
-                    if (!blocked && mDiscoveryPostponed) {
-                        mDiscoveryPostponed = false;
-                        mWifiNative.p2pFind(DISCOVER_TIMEOUT_S);
-                    }
                     break;
                 case WifiP2pManager.DISCOVER_PEERS:
                     if (mDiscoveryBlocked) {
-                    /* do not send discovery failure to apps.
-                       since discovery is postponed and not failed */
-                       loge("P2P_FIND is deffered");
-                       break;
+                        replyToMessage(message, WifiP2pManager.DISCOVER_PEERS_FAILED,
+                                WifiP2pManager.BUSY);
+                        break;
                     }
                     // do not send service discovery request while normal find operation.
                     clearSupplicantServiceRequest();
