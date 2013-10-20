170a6daba1b00bc9e6217b4a01fe48c40758960f
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-10-20 18:50:10 +0300

    Fix removal of dock battery views in PhoneStatusBar
    removeView is not recursive. Find and remove the dock views from their
    direct parent. Fixes inconsistent paddings/margins with the wi-fi
    indicator on tablets.
    
    Change-Id: I56745059aaf84b1e52b6a4fd8806481beae0daff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -641,12 +641,10 @@ public class PhoneStatusBar extends BaseStatusBar {
             mDockBatteryController.addStateChangedCallback(callback);
         } else {
             // Remove dock battery icons if device doesn't hava dock battery support
-            View v = mStatusBarView.findViewById(R.id.dock_battery);
-            if (v != null) mStatusBarView.removeView(v);
-            v = mStatusBarView.findViewById(R.id.dock_battery_text);
-            if (v != null) mStatusBarView.removeView(v);
-            v = mStatusBarView.findViewById(R.id.circle_dock_battery);
-            if (v != null) mStatusBarView.removeView(v);
+            ViewGroup cluster = (ViewGroup) mStatusBarView.findViewById(R.id.signal_battery_cluster);
+            cluster.removeView(cluster.findViewById(R.id.dock_battery));
+            cluster.removeView(cluster.findViewById(R.id.dock_battery_text));
+            cluster.removeView(cluster.findViewById(R.id.circle_dock_battery));
             mCircleDockBattery = null;
         }
 
