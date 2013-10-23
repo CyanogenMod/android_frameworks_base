c6557ebd2a636b0f38a13dab893e99b34ef175f0
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-23 02:15:56 +0100

    PieInputFilter: Fix the check for the input device type
    input sources are composites with all the supported capabilities,
    not direct representations of the hardware.
    A touchscreen with gesture or stylus capabilities will be a
    TOUCHSCREEN|GESTURE, for example, and fail this check. Fix it.
    
    Change-Id: Ib3a5b27be9f461245c6d010e1a7f454e9284437f

--- services/java/com/android/server/pie/PieInputFilter.java
+++ services/java/com/android/server/pie/PieInputFilter.java
@@ -226,7 +226,8 @@ public class PieInputFilter implements IInputFilter {
             Trace.traceBegin(Trace.TRACE_TAG_INPUT, "filterInputEvent");
         }
         try {
-            if (event.getSource() != InputDevice.SOURCE_TOUCHSCREEN
+            if (((event.getSource() & InputDevice.SOURCE_TOUCHSCREEN)
+                 != InputDevice.SOURCE_TOUCHSCREEN)
                     || !(event instanceof MotionEvent)) {
                 sendInputEvent(event, policyFlags);
                 return;
