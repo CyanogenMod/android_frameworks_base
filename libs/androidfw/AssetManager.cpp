495e2b32f628e686f4c8474aa7b0b12575d579fa
Elliott Hughes <enh@google.com>
2013-10-29 13:12:55 -0700

    Fix a variety of small publicly-reported bugs.
    Possible NULL dereference in cmds/bootanimation/BootAnimation.cpp.
    https://code.google.com/p/android/issues/detail?id=61556
    
    Missing fclose in core/jni/android_os_Debug.cpp.
    https://code.google.com/p/android/issues/detail?id=61546
    
    Bad loop guards in core/jni/android_util_Process.cpp.
    https://code.google.com/p/android/issues/detail?id=61557
    
    Assignment to wrong variable in libs/androidfw/AssetManager.cpp.
    https://code.google.com/p/android/issues/detail?id=61560
    
    Missing delete[]s in libs/androidfw/ObbFile.cpp.
    https://code.google.com/p/android/issues/detail?id=61549
    
    Leaks on error in tools/aapt/Images.cpp.
    https://code.google.com/p/android/issues/detail?id=61552
    
    Two missing fclose calls in tools/aapt/Resource.cpp.
    https://code.google.com/p/android/issues/detail?id=61553
    
    Missing fclose in tools/aidl/aidl.cpp.
    https://code.google.com/p/android/issues/detail?id=61554
    
    Change-Id: I5820f3824e72d07a9acb776cf0af3e7443f5694a

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -434,7 +434,7 @@ void AssetManager::setLocaleLocked(const char* locale)
         delete[] mLocale;
     }
     mLocale = strdupNew(locale);
-    
+
     updateResourceParamsLocked();
 }
 
@@ -900,7 +900,7 @@ Asset* AssetManager::openInLocaleVendorLocked(const char* fileName, AccessMode m
             /* look at the filesystem on disk */
             String8 path(createPathNameLocked(ap, locale, vendor));
             path.appendPath(fileName);
-    
+
             String8 excludeName(path);
             excludeName.append(kExcludeExtension);
             if (::getFileType(excludeName.string()) != kFileTypeNonexistent) {
@@ -908,28 +908,28 @@ Asset* AssetManager::openInLocaleVendorLocked(const char* fileName, AccessMode m
                 //printf("+++ excluding '%s'\n", (const char*) excludeName);
                 return kExcludedAsset;
             }
-    
+
             pAsset = openAssetFromFileLocked(path, mode);
-    
+
             if (pAsset == NULL) {
                 /* try again, this time with ".gz" */
                 path.append(".gz");
                 pAsset = openAssetFromFileLocked(path, mode);
             }
-    
+
             if (pAsset != NULL)
                 pAsset->setAssetSource(path);
         } else {
             /* find in cache */
             String8 path(createPathNameLocked(ap, locale, vendor));
             path.appendPath(fileName);
-    
+
             AssetDir::FileInfo tmpInfo;
             bool found = false;
-    
+
             String8 excludeName(path);
             excludeName.append(kExcludeExtension);
-    
+
             if (mCache.indexOf(excludeName) != NAME_NOT_FOUND) {
                 /* go no farther */
                 //printf("+++ Excluding '%s'\n", (const char*) excludeName);
@@ -1751,7 +1751,7 @@ bool AssetManager::fncScanAndMergeDirLocked(
 
     // XXX This is broken -- the filename cache needs to hold the base
     // asset path separately from its filename.
-    
+
     partialPath = createPathNameLocked(ap, locale, vendor);
     if (dirName[0] != '\0') {
         partialPath.appendPath(dirName);
