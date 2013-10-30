a2303b8f55b72474f7b20fa21ae7b6dcfd3fa905
Harry Yang <harryy@codeaurora.org>
2013-10-29 22:48:30 -0700

    frameworks/base: Fix race conditions on resume/suspend using power key
    The wakelock PowerManagerService.Display sometimes cannot be released
    after two quick power key pressings like a double click during phone
    sleep (the target will resume then supposedly suspend shortly).
    
    The second key event disturbs display power state on resume, as a
    result display resources won't get released on suspend, which is fixed
    in this change.
    
    CRs-Fixed: 566059
    Change-Id: I633fd377ab235f203ebfb649a74449a2385740c4

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -816,6 +816,7 @@ final class DisplayPowerController {
                     if (!mElectronBeamOffAnimator.isStarted()) {
                         if (mPowerState.getElectronBeamLevel() == 0.0f) {
                             setScreenOn(false);
+                            unblockScreenOn();
                         } else if (mPowerState.prepareElectronBeam(
                                 //mElectronBeamFadesConfig ?
 				mElectronBeamMode == 0 ?
