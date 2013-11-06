664587a98f9d9ef53fa6fc7303740f40f491940e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-06 16:23:13 +0000

    SystemUI: Reinflate on theme change
    Reflect the updated assets immediately once the config_changed
    notification happens
    
    Change-Id: I480a719fbdd45875277871af39ce65849918ec0a

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationData.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationData.java
@@ -131,6 +131,10 @@ public class NotificationData {
         return e;
     }
 
+    public void clear() {
+        mEntries.clear();
+    }
+
     /**
      * Return whether there are any visible items (i.e. items without an error).
      */
