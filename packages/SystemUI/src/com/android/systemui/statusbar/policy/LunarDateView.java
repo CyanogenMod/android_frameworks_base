26014e87ad9a42d3f79da089ab2935b305732c38
martincz <martincz@mokeetek.com>
2013-11-09 02:42:04 +0800

    SystemUI: lunar date in statusbar
    Change-Id: If4936b8ff7687a92ce081e217346d3f6b832d15d
    
    Conflicts:
    	core/mokee/res/values-zh-rCN/donottranslate-cldr.xml
    	core/mokee/res/values-zh-rTW/donottranslate-cldr.xml
    	core/mokee/res/values/donottranslate-cldr.xml
    	core/res/res/values/mk_symbols.xml
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/LunarDateView.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright (C) 2013 The MoKee OpenSource Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.policy;
+
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.mokee.util.Lunar;
+import android.mokee.util.MoKeeUtils;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.text.format.DateFormat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.ViewParent;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.systemui.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LunarDateView extends TextView implements OnClickListener, OnLongClickListener {
+    private static final String TAG = "LunarDateView";
+
+    private RelativeLayout mParent;
+
+    private boolean mAttachedToWindow;
+    private boolean mWindowVisible;
+    private boolean mUpdating;
+
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (Intent.ACTION_TIME_TICK.equals(action)
+                    || Intent.ACTION_TIME_CHANGED.equals(action)
+                    || Intent.ACTION_TIMEZONE_CHANGED.equals(action)
+                    || Intent.ACTION_LOCALE_CHANGED.equals(action)) {
+                updateClock();
+            }
+        }
+    };
+
+    public LunarDateView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOnClickListener(this);
+        setOnLongClickListener(this);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mAttachedToWindow = true;
+        setUpdates();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mAttachedToWindow = false;
+        if (mParent != null) {
+            mParent.setOnClickListener(null);
+            mParent.setOnLongClickListener(null);
+            mParent = null;
+        }
+        setUpdates();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mParent == null) {
+            mParent = (RelativeLayout) getParent();
+            mParent.setOnClickListener(this);
+            mParent.setOnLongClickListener(this);
+        }
+
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        mWindowVisible = visibility == VISIBLE;
+        setUpdates();
+    }
+
+    @Override
+    protected void onVisibilityChanged(View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        setUpdates();
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        // makes the large background bitmap not force us to full width
+        return 0;
+    }
+
+    protected void updateClock() {
+        Resources res = mContext.getResources();
+        if (MoKeeUtils.isChineseLanguage()) {
+            CharSequence mDateFormatString = res.getText(com.mokee.internal.R.string.abbrev_wday_month_day_year);
+            setText(buildLunarDate(DateFormat.format(mDateFormatString, new Date()).toString()));
+            setVisibility(View.VISIBLE);
+            return;
+        }
+        setText("");
+        setVisibility(View.GONE);
+    }
+
+    private boolean isVisible() {
+        View v = this;
+        while (true) {
+            if (v.getVisibility() != VISIBLE) {
+                return false;
+            }
+            final ViewParent parent = v.getParent();
+            if (parent instanceof View) {
+                v = (View)parent;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    private void setUpdates() {
+        if (MoKeeUtils.isChineseLanguage()) {
+            setVisibility(View.VISIBLE);
+        } else {
+            setVisibility(View.GONE);
+        }
+        boolean update = mAttachedToWindow && mWindowVisible && isVisible();
+        if (update != mUpdating) {
+            mUpdating = update;
+            if (update) {
+                // Register for Intent broadcasts for the clock and battery
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(Intent.ACTION_TIME_TICK);
+                filter.addAction(Intent.ACTION_TIME_CHANGED);
+                filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+                filter.addAction(Intent.ACTION_LOCALE_CHANGED);
+                mContext.registerReceiver(mIntentReceiver, filter, null, null);
+                updateClock();
+            } else {
+                mContext.unregisterReceiver(mIntentReceiver);
+            }
+        }
+    }
+
+    private void collapseStartActivity(Intent what) {
+        // collapse status bar
+        StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(
+                Context.STATUS_BAR_SERVICE);
+        statusBarManager.collapsePanels();
+
+        // dismiss keyguard in case it was active and no passcode set
+        try {
+            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+        } catch (Exception ex) {
+            // no action needed here
+        }
+
+        // start activity
+        what.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(what);
+    }
+
+    @Override
+    public void onClick(View v) {
+        long nowMillis = System.currentTimeMillis();
+
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder, nowMillis);
+        Intent intent = new Intent(Intent.ACTION_VIEW)
+                .setData(builder.build());
+        collapseStartActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Intent intent = new Intent("android.settings.DATE_SETTINGS");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        collapseStartActivity(intent);
+
+        // consume event
+        return true;
+    }
+
+    /**
+    * Get lunar date
+    */
+    private String buildLunarDate(String date){
+        List<String> list = new ArrayList<String>();
+        Calendar cal = Calendar.getInstance();    
+        Pattern p = Pattern.compile("[\\u4e00-\\u9fa5]+|\\d+");
+        Matcher m = p.matcher(date);
+        while(m.find()){
+            list.add(m.group());
+        }
+        cal.set(Integer.parseInt(list.get(0)), Integer.parseInt(list.get(2)) - 1,Integer.parseInt(list.get(4)));
+        Lunar lunar = new Lunar(cal);
+        String lunarStr = lunar.toString();
+        return lunarStr.substring(2, lunarStr.length());
+    }
+}
