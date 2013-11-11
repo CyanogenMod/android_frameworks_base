45576f98871d121cb246317a709e5b1a76d45f6c
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-11-11 21:23:37 +0200

    SystemUI: fix RecentPanel position in landscape mode.
    In landscape mdoe we forcing ltr layout direction. This cause
    call to resolvePadding too late and discards paddings
    computed by fitSystemWindows. To workaround this we can catch
    onRtlPropertiesChanged and force executuin of fitSystemWindows
    one more time. To fix scroll issue after padding will be setted
    we calling scrollTo with correct position.
    
    Change-Id: I0ac10ac3554142b15114ce7588b61debda27235a

--- packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.database.DataSetObserver;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.FloatMath;
@@ -371,5 +372,21 @@ public class RecentsHorizontalScrollView extends HorizontalScrollView
 
     public void setCallback(RecentsCallback callback) {
         mCallback = callback;
+    }
+
+    @Override
+    public void onRtlPropertiesChanged(int layoutDirection) {
+        super.onRtlPropertiesChanged(layoutDirection);
+
+        requestFitSystemWindows();
+    }
+
+    @Override
+    protected boolean fitSystemWindows(Rect insets) {
+        final boolean res = super.fitSystemWindows(insets);
+
+        scrollTo(- mRight + mPaddingRight, 0);
+
+        return res;
     }
 }
