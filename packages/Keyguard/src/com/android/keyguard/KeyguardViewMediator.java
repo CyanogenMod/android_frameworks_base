7e68cd80c273a815c298bcfe55ae4765ab2eb269
Dan Pasanen <dan.pasanen@gmail.com>
2013-11-13 09:01:28 -0600

    KeyguardViewMediator: Play lock screen sound sooner
    When the screen is locked, there is a long pause before the lock
    sound actually plays. This makes that sound happen much sooner.
    
    Change-Id: I62547a183d68b93a33651c4078b5788d324055a9

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -1221,6 +1221,12 @@ public class KeyguardViewMediator {
                 if (DEBUG) Log.d(TAG, "handleShow");
             }
 
+            new Thread(new Runnable() {
+                public void run() {
+                    playSounds(true);
+                }
+            }).start();
+
             mKeyguardViewManager.show(options);
             mShowing = true;
             mKeyguardDonePending = false;
@@ -1231,9 +1237,6 @@ public class KeyguardViewMediator {
                 ActivityManagerNative.getDefault().closeSystemDialogs("lock");
             } catch (RemoteException e) {
             }
-
-            // Do this at the end to not slow down display of the keyguard.
-            playSounds(true);
 
             mShowKeyguardWakeLock.release();
         }
