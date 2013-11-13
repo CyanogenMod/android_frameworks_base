ce3133769dd22cfbe8371ff36e0383fceb5233a1
Steve Kondik <shade@chemlab.org>
2013-11-13 03:57:08 -0800

    camera: Better fix for face detection score issue
     * Do this at the lower layer so that third-party apps don't have
       issus with the nonstandard score.
    
    Change-Id: Ic012b092d2f47dbaab8cbfe2bd215b955800cc11

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -384,7 +384,11 @@ void JNICameraContext::postMetadata(JNIEnv *env, int32_t msgType, camera_frame_m
         env->SetIntField(rect, fields.rect_right, metadata->faces[i].rect[2]);
         env->SetIntField(rect, fields.rect_bottom, metadata->faces[i].rect[3]);
         env->SetObjectField(face, fields.face_rect, rect);
+#ifdef FIX_FACE_DETECTION_SCORE
+        env->SetIntField(face, fields.face_score, (metadata->faces[i].score ? 0 : 100));
+#else
         env->SetIntField(face, fields.face_score, metadata->faces[i].score);
+#endif
 
 #ifdef QCOM_HARDWARE
         jobject point1 = env->NewObject(mPointClass, fields.point_constructor);
