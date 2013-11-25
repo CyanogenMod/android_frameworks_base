e48034b63b137f6901cf417409953dabf60c93e4
Tom Giordano <tomgiordano83@gmail.com>
2013-11-24 23:33:54 -0800

    framework: fix screenshot and rotation animation for devices with abnormal hw rotation
    port foreward from cm10.1:
    https://github.com/CyanogenMod/android_frameworks_base/commit/ac555b9ca9a76d13a26fc9ea40980fe57258761b#diff-d6f7bba268a742c91252cc9e64e2381a
    
    Change-Id: I14b6e2403b9be345a1a591138f8a3a12ca8e4f89
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	services/java/com/android/server/wm/WindowManagerService.java

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -297,6 +297,8 @@ public class WindowManagerService extends IWindowManager.Stub
 
     private final boolean mHeadless;
 
+    private final int mSfHwRotation;
+
     private BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             mUiContext = null;
@@ -816,6 +818,9 @@ public class WindowManagerService extends IWindowManager.Stub
         } finally {
             SurfaceControl.closeTransaction();
         }
+
+        // Load hardware rotation from prop
+        mSfHwRotation = android.os.SystemProperties.getInt("ro.sf.hwrotation",0) / 90;
 
         ThemeUtils.registerThemeChangeReceiver(mContext, mThemeChangeReceiver);
     }
@@ -5687,6 +5692,9 @@ public class WindowManagerService extends IWindowManager.Stub
 
                 // The screenshot API does not apply the current screen rotation.
                 rot = getDefaultDisplayContentLocked().getDisplay().getRotation();
+                // Allow for abnormal hardware orientation
+                rot = (rot + mSfHwRotation) % 4;
+
                 int fw = frame.width();
                 int fh = frame.height();
 
