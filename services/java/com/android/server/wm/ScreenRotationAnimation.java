02160d188c474002a46104714a756e0981d38732
zevektor <clio2k6@gmail.com>
2013-11-27 13:38:59 +0100

    Revert "Fixed rotation crashes"
    This reverts commit 00637844169b373bf07bf14033142b3cb581c4cd.

--- services/java/com/android/server/wm/ScreenRotationAnimation.java
+++ services/java/com/android/server/wm/ScreenRotationAnimation.java
@@ -218,16 +218,15 @@ class ScreenRotationAnimation {
         mOriginalRotation = originalRotation;
         mOriginalWidth = originalWidth;
         mOriginalHeight = originalHeight;
-	
+
         if (!inTransaction) {
             if (WindowManagerService.SHOW_LIGHT_TRANSACTIONS) Slog.i(WindowManagerService.TAG,
                     ">>> OPEN TRANSACTION ScreenRotationAnimation");
             SurfaceControl.openTransaction();
         }
-	
-        try {/*
+
+        try {
             try {
-            	
                 if (WindowManagerService.DEBUG_SURFACE_TRACE) {
                     mSurfaceControl = new SurfaceTrace(session, "ScreenshotSurface",
                             mWidth, mHeight,
@@ -251,15 +250,14 @@ class ScreenRotationAnimation {
             } catch (SurfaceControl.OutOfResourcesException e) {
                 Slog.w(TAG, "Unable to allocate freeze surface", e);
             }
-		*/
+
             if (WindowManagerService.SHOW_TRANSACTIONS ||
                     WindowManagerService.SHOW_SURFACE_ALLOC) Slog.i(WindowManagerService.TAG,
                             "  FREEZE " + mSurfaceControl + ": CREATE");
-		//VK
-            //setRotationInTransaction(originalRotation);
+
+            setRotationInTransaction(originalRotation);
         } finally {
             if (!inTransaction) {
-            	//VK
                 SurfaceControl.closeTransaction();
                 if (WindowManagerService.SHOW_LIGHT_TRANSACTIONS) Slog.i(WindowManagerService.TAG,
                         "<<< CLOSE TRANSACTION ScreenRotationAnimation");
