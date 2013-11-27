bd5124d8154fe421eb71dcbdc3624bce08fe28f2
PrimeDirective <activethrasher00@gmail.com>
2013-11-27 04:14:21 -0500

    memoize activity manager graphics acceleration check
    also account for devices without hardware rendering support
    
    Change-Id: Ib400d85a261d9c5d0fd5770d6cda5f61832cba6b

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -51,6 +51,7 @@ import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.Slog;
+import android.view.HardwareRenderer;
 
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
@@ -284,9 +285,15 @@ public class ActivityManager {
     /** @hide Process is being cached for later use and is empty. */
     public static final int PROCESS_STATE_CACHED_EMPTY = 13;
 
+    private static boolean NOPE;
+
     /*package*/ ActivityManager(Context context, Handler handler) {
         mContext = context;
         mHandler = handler;
+
+        NOPE = (isLowRamDeviceStatic() ||
+                !HardwareRenderer.isAvailable()      ||
+                Resources.getSystem().getBoolean(com.android.internal.R.bool.config_avoidGfxAccel));
     }
 
     /**
@@ -458,8 +465,10 @@ public class ActivityManager {
      * @hide
      */
     static public boolean isHighEndGfx() {
-        return !isLowRamDeviceStatic() &&
-                !Resources.getSystem().getBoolean(com.android.internal.R.bool.config_avoidGfxAccel);
+        if (NOPE) {
+			return false;
+        }
+        return true;
     }
 
     /**
