e3ce7f5a38de8218810e0de5ae403f2f2a818aa2
Mahesh A Saptasagar <msapta@codeaurora.org>
2013-11-28 23:08:36 +0530

    wlan: Error handling on PNO failure due to active P2P session.
    If set PNO fails due to concurrent active p2p sessions,
    handle the error condition in framework and issue a
    regular scan for every framework background scan interval
    until set PNO is either success or disabled.
    
    Change-Id: Ia4e8d9aef9ed24e534cb57cea7c8ba7a7ffc509e
    CRs-Fixed: 568458

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -3894,7 +3894,11 @@ public class WifiStateMachine extends StateMachine {
                  * cleared
                  */
                 if (!mScanResultIsPending) {
-                    mWifiNative.enableBackgroundScan(true);
+                    if (!mWifiNative.enableBackgroundScan(true)) {
+                        setScanAlarm(true);
+                    } else {
+                        setScanAlarm(false);
+                    }
                 }
             } else {
                 setScanAlarm(true);
@@ -3948,8 +3952,11 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_ENABLE_BACKGROUND_SCAN:
                     mEnableBackgroundScan = (message.arg1 == 1);
                     if (mEnableBackgroundScan) {
-                        mWifiNative.enableBackgroundScan(true);
-                        setScanAlarm(false);
+                        if (!mWifiNative.enableBackgroundScan(true)) {
+                            setScanAlarm(true);
+                        } else {
+                            setScanAlarm(false);
+                        }
                     } else {
                         mWifiNative.enableBackgroundScan(false);
                         setScanAlarm(true);
@@ -3975,7 +3982,11 @@ public class WifiStateMachine extends StateMachine {
                 case WifiMonitor.SCAN_RESULTS_EVENT:
                     /* Re-enable background scan when a pending scan result is received */
                     if (mEnableBackgroundScan && mScanResultIsPending) {
-                        mWifiNative.enableBackgroundScan(true);
+                        if (!mWifiNative.enableBackgroundScan(true)) {
+                            setScanAlarm(true);
+                        } else {
+                            setScanAlarm(false);
+                        }
                     }
                     /* Handled in parent state */
                     ret = NOT_HANDLED;
