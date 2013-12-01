d8342de102c961e98184d02739c66cfa0c197206
maxwen <max.weninger@gmail.com>
2013-12-01 16:47:13 +0100

    [1/3] base: allow disabling non intrusive incall ui
    Change-Id: Ibe9c65f1fe024a859d98d96fd86199607a1060de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3433,6 +3433,20 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+         * Volume key controls ringtone or media sound stream
+         *
+         * @hide
+         */
+        public static final String VOLUME_KEYS_DEFAULT = "volume_keys_default";
+
+        /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
