de2481d85f4b6a0adff28fda08d46ebbad939ac6
iKillCypher <xzain69@gmail.com>
2013-12-01 21:19:15 +0800

    Port Forward Quick Unlock
    Change-Id: Ic64c47bb35ee4da13b0332d853725a6ef60ac8d4

--- packages/Keyguard/src/com/android/keyguard/KeyguardPasswordView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPasswordView.java
@@ -44,6 +44,7 @@ public class KeyguardPasswordView extends KeyguardAbsKeyInputView
         implements KeyguardSecurityView, OnEditorActionListener, TextWatcher {
 
     private final boolean mShowImeAtScreenOn;
+    private boolean mQuickUnlock;
 
     InputMethodManager mImm;
 
@@ -107,6 +108,9 @@ public class KeyguardPasswordView extends KeyguardAbsKeyInputView
             }
         });
 
+	mQuickUnlock = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
+
         mPasswordEntry.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
@@ -117,6 +121,22 @@ public class KeyguardPasswordView extends KeyguardAbsKeyInputView
             public void afterTextChanged(Editable s) {
                 if (mCallback != null) {
                     mCallback.userActivity(0);
+                }
+		
+		if (mQuickUnlock) {
+                    String entry = mPasswordEntry.getText().toString();
+                    if (entry.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT) {
+                        if (mLockPatternUtils.checkPassword(entry)) {
+                            mCallback.keyguardDone(true);
+                            mCallback.reportSuccessfulUnlockAttempt();
+                        } else {
+                            mCallback.reportFailedUnlockAttempt();
+                            if (0 == (mUpdateMonitor.getFailedAttempts() % LockPatternUtils.FAILED_ATTEMPTS_BEFORE_TIMEOUT)) {
+                                long deadline = mLockPatternUtils.setLockoutAttemptDeadline();
+                                handleAttemptLockout(deadline);
+                            }
+                        }
+                    }
                 }
             }
         });
