5fd0bf670d1e0bf45c8bbf1c7b405be5e32789c7
danidicky <dicky1982@hotmail.com>
2013-12-02 15:59:04 +0100

    add support to DC-HSPA+ on status bar

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/TelephonyIcons.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/TelephonyIcons.java
@@ -147,7 +147,19 @@ class TelephonyIcons {
               R.drawable.stat_sys_data_fully_connected_hp,
               R.drawable.stat_sys_data_fully_connected_hp }
     };
-
+    
+	// DC-HSPAP
+			static final int[][] DATA_DC = {
+			{ R.drawable.stat_sys_data_connected_dc,
+			R.drawable.stat_sys_data_connected_dc,
+			R.drawable.stat_sys_data_connected_dc,
+			R.drawable.stat_sys_data_connected_dc },
+			{ R.drawable.stat_sys_data_fully_connected_dc,
+			R.drawable.stat_sys_data_fully_connected_dc,
+			R.drawable.stat_sys_data_fully_connected_dc,
+			R.drawable.stat_sys_data_fully_connected_dc }
+			};
+    
     static final int[] QS_DATA_HP = {
                 R.drawable.ic_qs_signal_hp,
                 R.drawable.ic_qs_signal_full_hp
