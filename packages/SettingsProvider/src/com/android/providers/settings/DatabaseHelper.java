618af0223346c121485bb31ddf3ebede909c0f48
Ethan Chen <intervigil@gmail.com>
2013-12-03 03:44:46 +0000

    Revert "Turn off carrier provisioning by default"
    This reverts commit 78511f621406bf8da153b6fec2da424fe27e6b11.
    
    Change-Id: Idb612d82fb0391b73c7c5f2263f79599791fc533
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2307,9 +2307,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     R.integer.def_low_battery_sound_timeout);
 
             // --- New global settings start here
-            loadIntegerSetting(stmt, Settings.Global.TETHER_DUN_REQUIRED,
-                    R.integer.def_tether_dun_required);
-
         } finally {
             if (stmt != null) stmt.close();
         }
