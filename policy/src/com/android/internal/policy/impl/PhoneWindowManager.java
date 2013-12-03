3a8ecfe2600170d8409c7db966c560ee0a2bb1f4
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-12-03 19:16:41 +0200

    Fix Immersive confirmation in expanded mode.
    The confirmation should come only once.
    
    Change-Id: I5914d655bd5f9315ac4cc6fc7d70cc4ee45bb7b2

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4538,8 +4538,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
-                    mImmersiveModeConfirmation.onPowerKeyDown(isScreenOn, event.getDownTime(),
-                            isImmersiveMode(mLastSystemUiFlags));
+                    if (mExpandedDesktopStyle == 0) {
+                        mImmersiveModeConfirmation.onPowerKeyDown(isScreenOn, event.getDownTime(),
+                                isImmersiveMode(mLastSystemUiFlags));
+                    }
                     if (isScreenOn && !mPowerKeyTriggered
                             && (event.getFlags() & KeyEvent.FLAG_FALLBACK) == 0) {
                         mPowerKeyTriggered = true;
