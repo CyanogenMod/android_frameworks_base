3aa7ff69fdaa424c3c3b170036cecfafa8394012
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-12-06 17:09:14 +0200

    WindowManagerService: fix application transitions in expanded mode.
    Change-Id: If7d3fc3ab566dedc3a5b38cd0a6dc3185f494e20
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/view/WindowManagerPolicy.java

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1235,4 +1235,11 @@ public interface WindowManagerPolicy {
      * @hide
      */
     public int getCurrentNavigationBarSize();
+
+    /**
+     * Check if immersive mode hides navigation bar
+     *
+     * @return True if navbar can be hidden by immersive mode
+     */
+    public boolean isImmersiveMode(int vis);
 }
