19c9e7df472646330143e9750bcb951b91b89a5c
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-12-06 17:09:14 +0200

    WindowManagerService: fix application transitions in expanded mode.
    Change-Id: If7d3fc3ab566dedc3a5b38cd0a6dc3185f494e20

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3859,11 +3859,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return vis;
     }
 
-    private boolean expandedDesktopHidesNavigationBar() {
+    public boolean expandedDesktopHidesNavigationBar() {
         return mExpandedDesktopStyle != 0;
     }
 
-    private boolean expandedDesktopHidesStatusBar() {
+    public boolean expandedDesktopHidesStatusBar() {
         return mExpandedDesktopStyle == 2;
     }
 
@@ -5834,7 +5834,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private boolean isImmersiveMode(int vis) {
+    public boolean isImmersiveMode(int vis) {
         final int flags = View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         return mNavigationBar != null
                 && (vis & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) != 0
