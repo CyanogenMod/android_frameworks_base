1121facde1ed0e0511a275078ed286e0e2e2bba9
Danny Baumann <dannybaumann@web.de>
2013-12-11 10:13:48 +0100

    Remove 'force overflow button' logic.
    It's force-enabled now anyway.
    
    Change-Id: Ib4489456d40650316d45865fedfae9c3c960b81e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3327,14 +3327,6 @@ public final class Settings {
          public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
         /**
-         * Control the display of the action overflow button within app UI.
-         * 0 = use system default
-         * 1 = force on
-         * @hide
-         */
-        public static final String UI_FORCE_OVERFLOW_BUTTON = "ui_force_overflow_button";
-
-        /**
          * Performance profile
          * @see config_perf_profile_prop in frameworks/base/core/res/res/values/config.xml
          * @hide
