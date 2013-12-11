1ed5f7a9f6124c0e91461dd2901386eacec12c02
Oskar Grönqvist <oskar.gronqvist@sonymobile.com>
2013-12-11 14:14:33 +0100

    Set Settings.Global.WIFI_SCAN_ALWAYS_AVAILABLE default value
    To be able to customize the "WiFi scan always available"
    setting we need to set a default value.
    
    Change-Id: I3673f2613ab9b6a947aed7e29d4cc876140c2180

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2360,6 +2360,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.Global.LOW_BATTERY_SOUND_TIMEOUT,
                     R.integer.def_low_battery_sound_timeout);
 
+            loadIntegerSetting(stmt, Settings.Global.WIFI_SCAN_ALWAYS_AVAILABLE,
+                    R.integer.def_wifi_scan_always_available);
+
             // --- New global settings start here
             loadIntegerSetting(stmt, Settings.Global.TETHER_DUN_REQUIRED,
                     R.integer.def_tether_dun_required);
