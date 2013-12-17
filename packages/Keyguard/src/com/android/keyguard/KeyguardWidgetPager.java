c8d3d77cda8251840ffa7a83e6cca19cbee86eaf
Lars Greiss <kufikugel@googlemail.com>
2013-12-17 20:00:56 +0100

    Keyguard: disable statusbar clock for dashclock, lockclock and desclock
    afaik this was an original commit from Danesh and we extended it later.
    Whatever thanks to him in this case :)
    
    Change-Id: I67a370b0a76f938fa3e022e8c467f2d6ebca87c0

--- packages/Keyguard/src/com/android/keyguard/KeyguardWidgetPager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardWidgetPager.java
@@ -54,6 +54,12 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
     protected static float OVERSCROLL_MAX_ROTATION = 30;
     private static final boolean PERFORM_OVERSCROLL_ROTATION = true;
 
+    private static final String[] CLOCK_WIDGET_PACKAGES = new String[] {
+        "net.nurik.roman.dashclock",
+        "com.cyanogenmod.lockclock",
+        "com.android.deskclock"
+    };
+
     private static final int FLAG_HAS_LOCAL_HOUR = 0x1;
     private static final int FLAG_HAS_LOCAL_MINUTE = 0x2;
 
@@ -143,6 +149,16 @@ public class KeyguardWidgetPager extends PagedView implements PagedView.PageSwit
             ViewGroup vg = (ViewGroup) newPage;
             if (vg.getChildAt(0) instanceof KeyguardStatusView) {
                 showingClock = true;
+            } else if (vg.getChildAt(0) instanceof AppWidgetHostView) {
+                AppWidgetProviderInfo info =
+                        ((AppWidgetHostView) vg.getChildAt(0)).getAppWidgetInfo();
+                String widgetPackage = info.provider.getPackageName();
+                for (String packageName : CLOCK_WIDGET_PACKAGES) {
+                    if (packageName.equals(widgetPackage)) {
+                        showingClock = true;
+                        break;
+                    }
+                }
             }
         }
 
