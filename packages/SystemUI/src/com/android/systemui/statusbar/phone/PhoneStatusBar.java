17cae8356aa5d174bdfa1783aa421db3e267b36d
Lars Greiss <kufikugel@googlemail.com>
2013-12-17 19:00:48 +0100

    fb: add ticker entries only if we are not recreating statusbar
    if both happens at the same time it will result in an systemUI crash.
    Funny that since 2 years where this method exist no one discovered it.
    Due to the new theme lightsensor mode we have way more switches so
    maybe thats why I got it now
    
    whatever we had already a check but the tick method can be called from
    outside as well so just check if we are in the recreation or not and return
    if we are in recreation process. Old check is now redundant and can be
    removed
    
    Change-Id: I0feeaaa16d14fc631124d155dc18ba346f788796

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1197,7 +1197,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                 notification.getNotification().fullScreenIntent.send();
             } catch (PendingIntent.CanceledException e) {
             }
-        } else if (!mRecreating) {
+        } else {
             // usual case: status bar visible & not immersive
 
             // show the ticker if there isn't already a heads up
@@ -2513,6 +2513,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
     @Override
     protected void tick(IBinder key, StatusBarNotification n, boolean firstTime) {
+        // we are in the process of recreating the statusbar
+        // so nothing to do right now
+        if (mRecreating) {
+            return;
+        }
         // no ticking in lights-out mode
         if (!areLightsOn()) return;
 
