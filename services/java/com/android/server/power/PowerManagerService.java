9cf2a41749b0f9e732a14c62ac60e9bcf9e48a27
Kun Liang <kunliang@codeaurora.org>
2013-12-17 16:11:07 +0800

    QuickBoot: don't wake up screen when usb connected
    Don't wake up screen in QuickBoot mode when usb connected.
    
    Change-Id: If65936a0edbbc3b36a0768294e5c3bd7d741018a

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -1487,6 +1487,10 @@ public final class PowerManagerService extends IPowerManager.Stub
             return false;
         }
 
+       if (SystemProperties.getInt("sys.quickboot.enable", 0) == 1) {
+            return false;
+        }
+
         // Don't wake when undocked from wireless charger.
         // See WirelessChargerDetector for justification.
         if (wasPowered && !mIsPowered
