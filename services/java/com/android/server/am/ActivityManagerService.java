d2172cc1d18aa718b700a546b4dcd6454b501266
Samjith Sadasivan <samjit@codeaurora.org>
2013-12-18 19:08:29 +0530

    Revert "Fix for Concurrency issue reported in  525529"
    This reverts commit 7eb1fcb76989f11aaa9ca0a78e0361239ff5317d.
    
    Change-Id: I37815b93bf2b25caff171888dc13788479a9c8c4
    CRs-Fixed: 585504

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -1227,11 +1227,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     mHandler.sendMessageDelayed(nmsg, ActiveServices.SERVICE_TIMEOUT);
                     return;
                 }
-                //synchronising to avoid proc.executingServices set
-                //getting updated while being iterated in serviceTimeout()
-                synchronized(ActivityManagerService.this){
-                    mServices.serviceTimeout((ProcessRecord)msg.obj);
-                }
+                mServices.serviceTimeout((ProcessRecord)msg.obj);
             } break;
             case UPDATE_TIME_ZONE: {
                 synchronized (ActivityManagerService.this) {
