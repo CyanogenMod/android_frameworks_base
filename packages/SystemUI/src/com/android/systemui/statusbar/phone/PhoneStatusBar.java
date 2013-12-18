efef2c443230749154558fda7defa6d763137157
cordell <cordellsmail@gmail.com>
2013-12-18 17:42:07 -0600

    spelling error "status bra"
    Change-Id: I0914af77cbd957024300f4ad621a1dd380edb13d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -363,7 +363,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     };
 
-    // status bra brightness control
+    // status bar brightness control
     Runnable mLongPressBrightnessChange = new Runnable() {
         @Override
         public void run() {
