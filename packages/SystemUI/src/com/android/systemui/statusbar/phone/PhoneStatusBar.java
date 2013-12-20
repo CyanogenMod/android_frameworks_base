65e57dd4da58039bc90460f29eda1e459f030215
Michael Bestas <mikeioannina@gmail.com>
2013-12-20 04:53:41 +0200

    Squashed fixes for Statusbar Clock and Date actions
    Align the status bar date to the bottom
    
    As in AOSP. Also occupy the whole height area so that the date can be
    easily clicked.
    
    Change-Id: I66e5b7a754f27571f987657cc7ccd2ef3bef3f9e
    
    Remove redundant click action from date & time container layout
    
    Unlike AOSP, CyanogenMod provides separate actions for both the clock
    and date & time views in the expanded notification area. AOSP has a
    single action for both views.
    
    This commit removes the redundant AOSP click action completely. In
    addition, the clock and date views are modified to occupy the entire
    height of the layout to increase the clickable area.
    
    Change-Id: I78f4353c9ae4646f14124a38ef94e44ab920a067

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -622,10 +622,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHasFlipSettings = res.getBoolean(R.bool.config_hasFlipSettingsPanel);
 
         mDateTimeView = mNotificationPanelHeader.findViewById(R.id.datetime);
-        if (mDateTimeView != null) {
-            mDateTimeView.setOnClickListener(mClockClickListener);
-            mDateTimeView.setEnabled(true);
-        }
 
         mSettingsButton = (ImageView) mStatusBarWindow.findViewById(R.id.settings_button);
         if (mSettingsButton != null) {
@@ -2777,13 +2773,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                 startActivityDismissingKeyguard(
                         new Intent(android.provider.Settings.ACTION_SETTINGS), true);
             }
-        }
-    };
-
-    private View.OnClickListener mClockClickListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            startActivityDismissingKeyguard(
-                    new Intent(Intent.ACTION_QUICK_CLOCK), true); // have fun, everyone
         }
     };
 
