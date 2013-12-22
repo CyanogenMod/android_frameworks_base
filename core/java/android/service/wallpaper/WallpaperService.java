1f6e809e9d3f0b82067cbb748f7721c6a7903791
nuclearmistake <nuclearmistake@gmail.com>
2013-12-22 11:24:53 -0500

    Revert "frameworks/base: Fix for the race in Wallpaperservice"
    debugging a ui deadlock... so this smells a bit funny.
    
    This reverts commit 2166fd09e16e8bb9f82580e995b5dde533e6857e.

--- core/java/android/service/wallpaper/WallpaperService.java
+++ core/java/android/service/wallpaper/WallpaperService.java
@@ -899,7 +899,6 @@ public abstract class WallpaperService extends Service {
         }
         
         void detach() {
-           synchronized (mLock) {
             if (mDestroyed) {
                 return;
             }
@@ -942,7 +941,6 @@ public abstract class WallpaperService extends Service {
                     mInputChannel = null;
                 }
             }
-           }
         }
     }
     
@@ -1029,17 +1027,13 @@ public abstract class WallpaperService extends Service {
                     }
                     Engine engine = onCreateEngine();
                     mEngine = engine;
-                    synchronized (mActiveEngines) {
-                        mActiveEngines.add(engine);
-                    }
+                    mActiveEngines.add(engine);
                     engine.attach(this);
                     return;
                 }
                 case DO_DETACH: {
-                        mEngine.detach();
-                    synchronized (mActiveEngines) {
-                        mActiveEngines.remove(mEngine);
-                    }
+                    mActiveEngines.remove(mEngine);
+                    mEngine.detach();
                     return;
                 }
                 case DO_SET_DESIRED_SIZE: {
@@ -1121,13 +1115,10 @@ public abstract class WallpaperService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        synchronized (mActiveEngines) {
-           for (int i=0; i<mActiveEngines.size(); i++) {
-                Engine engine = mActiveEngines.get(i);
-                engine.detach();
-           }
-           mActiveEngines.clear();
+        for (int i=0; i<mActiveEngines.size(); i++) {
+            mActiveEngines.get(i).detach();
         }
+        mActiveEngines.clear();
     }
 
     /**
