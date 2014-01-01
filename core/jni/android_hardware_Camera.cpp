150bf578bab5d05b143936ca1d5f165eb8a45054
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-01 06:17:55 +0000

    Limit QC camera features to post-ICS HALs
    ICS blobs don't support longshot, and have a different API/method enum
    for histograms
    
    Change-Id: I6704b9302344ae2ae6386312b68de669bb5c11f2

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -388,7 +388,7 @@ void JNICameraContext::setCallbackMode(JNIEnv *env, bool installed, bool manualM
 static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jboolean enable)
 {
     ALOGV("setLongshot");
-#ifdef QCOM_HARDWARE
+#if defined(QCOM_HARDWARE) && !defined(ICS_CAMERA_BLOB)
     JNICameraContext* context;
     status_t rc;
     sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -409,7 +409,7 @@ static void android_hardware_Camera_setLongshot(JNIEnv *env, jobject thiz, jbool
 static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  {
    ALOGV("sendHistogramData" );
-#ifdef QCOM_HARDWARE
+#if defined(QCOM_HARDWARE) && !defined(ICS_CAMERA_BLOB)
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
@@ -425,7 +425,7 @@ static void android_hardware_Camera_sendHistogramData(JNIEnv *env, jobject thiz)
  static void android_hardware_Camera_setHistogramMode(JNIEnv *env, jobject thiz, jboolean mode)
  {
    ALOGV("setHistogramMode: mode:%d", (int)mode);
-#ifdef QCOM_HARDWARE
+#if defined(QCOM_HARDWARE) && !defined(ICS_CAMERA_BLOB)
    JNICameraContext* context;
    status_t rc;
    sp<Camera> camera = get_native_camera(env, thiz, &context);
