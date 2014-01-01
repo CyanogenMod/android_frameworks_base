4956af8fd1cd04e7aaaa73c54cfd2b5dc467c9dc
Sam Mortimer <sam@mortimer.me.uk>
2013-12-31 16:22:05 -0800

    [2/2] Framework: instant led test
    Adds support a new notification extra boolean
    EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
    notification light picker to force lights on
    when the screen is on.
    
    Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -561,6 +561,13 @@ public class Notification implements Parcelable
     public static final String EXTRA_AS_HEADS_UP = "headsup";
 
     /**
+     * Used by light picker in Settings to force
+     * notification lights on when screen is on
+     * @hide
+     */
+    public static final String EXTRA_FORCE_SHOW_LIGHTS = "android.forceShowLights";
+
+    /**
      * Value for {@link #EXTRA_AS_HEADS_UP}.
      * @hide
      */
