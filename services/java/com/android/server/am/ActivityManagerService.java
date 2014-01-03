7d72ca47204ad670cac6cc344d8a31f6564d52e5
Lorenzo M <djmesias@yahoo.es>
2014-01-04 00:56:06 +0100

    privacyguard: Notification dimissable
    Change-Id: I4c7090511d1a44a0408afa19421bb9284d15f291

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -1704,7 +1704,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     Notification notification = new Notification();
                     notification.icon = com.android.internal.R.drawable.stat_notify_privacy_guard;
                     notification.when = 0;
-                    notification.flags = Notification.FLAG_ONGOING_EVENT;
+                    notification.flags = Notification.FLAG_AUTO_CANCEL;
                     notification.priority = Notification.PRIORITY_LOW;
                     notification.defaults = 0;
                     notification.sound = null;
