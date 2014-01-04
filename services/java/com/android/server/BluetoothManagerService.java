01fcf5da537d5a8f5b98a69518a62ea139044e0d
Sam Mortimer <sam@mortimer.me.uk>
2014-01-03 18:38:48 -0800

    [2/2] Framework AppOps: Forward port ops from cm-10.2
        Squash of:
    
        Author: Sam Mortimer <sam@mortimer.me.uk>
        Date:   Fri Sep 27 21:30:34 2013 -0700
        [2/2] AppOps: Add alarm wakeup op
        When alarm wakeups are denied/ignored,
        the alarm types are remapped as follows:
        RTC_WAKEUP -> RTC
        ELAPSED_REALTIME_WAKEUP -> ELAPSED_REALTIME
        Change-Id: I3a5023e1f6260004319290e776bdb2daf73e9fdf
    
        Author: Kun Liang <kunliang@codeaurora.org>
        Date:   Fri Aug 30 23:56:59 2013 -0700
        AppOps: Add data connect control into AppOps
        Check user permission before enabling/disabling mobile data.
        Change-Id: I6e1895b130788dfccbc0a8523dadf1559b698988
    
        Author: Shashank Mittal <mittals@codeaurora.org>
        Date:   Thu Sep 5 17:24:44 2013 -0700
        AppOps: Add Bluetooth enable control into AppOps
        Check user permission before enabling bluetooth.
        Change-Id: Iefa4ea3319b33b2639beaf38d261d83649bbcf85
    
        Author: Shashank Mittal <mittals@codeaurora.org>
        Date:   Tue Sep 3 19:23:10 2013 -0700
        wifi: Confirm user permission before toggling wifi.
        Check user permissions before enabling/disabling wifi.
        Change-Id: I1ddae6e47f42b6d3fc831c2c135ece75cf9e935d
    
        Author: Qaweck <qaweck@gmail.com>
        Date:   Dec 6, 2013 06:21
        Fix bluetooth and data enable with ProfileManager
        Handle the case of system uid in AppOpsManager
        Change-Id: I5f9c3ba2e2ed0b1c825b995bf2724066ae796bb4
    
    Change-Id: I1d403d4325f105cd91f815f790cb01dd9b9f5516

--- services/java/com/android/server/BluetoothManagerService.java
+++ services/java/com/android/server/BluetoothManagerService.java
@@ -32,6 +32,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.app.AppOpsManager;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
@@ -389,7 +390,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         return true;
 
     }
-    public boolean enable() {
+    public boolean enable(String callingPackage) {
         if ((Binder.getCallingUid() != Process.SYSTEM_UID) &&
             (!checkIfCallerIsForegroundUser())) {
             Log.w(TAG,"enable(): not allowed for non-active and non system user");
@@ -401,6 +402,13 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
         if (DBG) {
             Log.d(TAG,"enable():  mBluetooth =" + mBluetooth +
                     " mBinding = " + mBinding);
+        }
+
+        AppOpsManager appOps = (AppOpsManager)mContext.getSystemService(Context.APP_OPS_SERVICE);
+        int callingUid = Binder.getCallingUid();
+        if (appOps.noteOp(AppOpsManager.OP_BLUETOOTH_CHANGE, callingUid, callingPackage) !=
+                AppOpsManager.MODE_ALLOWED) {
+            return false;
         }
 
         synchronized(mReceiver) {
