651795eedff3ed8b38036f87a0b8303a36b561ae
Lars Greiss <kufikugel@googlemail.com>
2014-01-04 17:21:00 +0100

    fb: fix reconstruct statusbar illegal argument exception
    Finally I got a logcat what is happening in rare cases. If the reconstruct
    happens during a notification ticker was send it returns a negative
    width value for the static layout and systemUI crashes.
    
    E/AndroidRuntime(  970): java.lang.IllegalArgumentException: Layout: -23 < 0
    E/AndroidRuntime(  970):        at android.text.Layout.<init>(Layout.java:138)
    E/AndroidRuntime(  970):        at android.text.StaticLayout.<init>(StaticLayout.java:104)
    E/AndroidRuntime(  970):        at android.text.StaticLayout.<init>(StaticLayout.java:90)
    E/AndroidRuntime(  970):        at android.text.StaticLayout.<init>(StaticLayout.java:68)
    E/AndroidRuntime(  970):        at android.text.StaticLayout.<init>(StaticLayout.java:48)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.phone.Ticker$Segment.getLayout(Ticker.java:72)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.phone.Ticker$Segment.getText(Ticker.java:91)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.phone.Ticker.addEntry(Ticker.java:226)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.phone.PhoneStatusBar.tick(PhoneStatusBar.java:2862)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.phone.PhoneStatusBar.addNotification(PhoneStatusBar.java:1432)
    E/AndroidRuntime(  970):        at com.android.systemui.statusbar.CommandQueue$H.handleMessage(CommandQueue.java:322)
    
    to avoid this check before a new StaticLayout is constructed if the width is < 0 and if this
    is the case abort the operation. This is in this case a legit way due that the notification
    gets reconstructed during this process later so or so
    
    Change-Id: I6ed68bbb9226101d3573f3d76deb793ddae69627

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -78,7 +78,11 @@ public abstract class Ticker {
         public StaticLayout getLayout(CharSequence substr) {
             int w = mTextSwitcher.getWidth() - mTextSwitcher.getPaddingLeft()
                     - mTextSwitcher.getPaddingRight();
-            return new StaticLayout(substr, mPaint, w, Alignment.ALIGN_NORMAL, 1, 0, true);
+            if (w < 0) {
+                return null;
+            } else {
+                return new StaticLayout(substr, mPaint, w, Alignment.ALIGN_NORMAL, 1, 0, true);
+            }
         }
 
         public CharSequence rtrim(CharSequence substr, int start, int end) {
@@ -98,6 +102,9 @@ public abstract class Ticker {
             }
             CharSequence substr = this.text.subSequence(this.current, this.text.length());
             StaticLayout l = getLayout(substr);
+            if (l == null) {
+                return null;
+            }
             int lineCount = l.getLineCount();
             if (lineCount > 0) {
                 int start = l.getLineStart(0);
@@ -124,6 +131,9 @@ public abstract class Ticker {
 
             CharSequence substr = this.text.subSequence(index, this.text.length());
             StaticLayout l = getLayout(substr);
+            if (l == null) {
+                return null;
+            }
             final int lineCount = l.getLineCount();
             int i;
             for (i=0; i<lineCount; i++) {
