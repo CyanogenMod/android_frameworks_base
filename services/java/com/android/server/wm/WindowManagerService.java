b2f2f8b6c6f1bf7c9e705508615075244a4f5f64
jungheang.lee <jungheang.lee@lge.com>
2014-01-08 16:11:47 +0900

    Appears black screen fix for a while
    This issue is side-effect of added google patch on KK MR1.
    
    Fix a couple of issues with mOpeningApps.
    https://android.googlesource.com/platform/frameworks/base/+/a8ee262c04749db4f32f73d2b9e7f58989c4638b%5E%21/#F0
    
    [Reproduce path]
    Settings -> Location -> Location OFF -> Location ON -> "Location consent Dialog" Popup-> "Agree btn" press
    -> Black screen appears for a while
    
    Change-Id: I14efdce87e8d78d38f9aae48568f69937e4fe1fa

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -4344,6 +4344,10 @@ public class WindowManagerService extends IWindowManager.Stub
             // If we are preparing an app transition, then delay changing
             // the visibility of this token until we execute that transition.
             if (okToDisplay() && mAppTransition.isTransitionSet()) {
+                // Already in requested state, don't do anything more.
+                if (wtoken.hiddenRequested != visible) {
+                    return;
+                }
                 wtoken.hiddenRequested = !visible;
 
                 if (!wtoken.startingDisplayed) {
