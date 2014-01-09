0870422ef884aabf564769cb1c64e9cbab0076ac
Schubi <schubi@erlangen.ccc.de>
2014-01-09 16:27:41 +0100

    Lockscreen notifications: Show all notifications when waking device on incoming notifications
    Change-Id: I6e03673a197ba172caf6d00118897038c37a9790

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -176,6 +176,7 @@ public class NotificationViewManager {
                         config.forceExpandedView) && config.wakeOnNotification && screenOffAndNotCovered
                         && !ongoingAndReposted) {
                 wakeDevice();
+                mHostView.showAllNotifications();
             }
         }
         @Override
