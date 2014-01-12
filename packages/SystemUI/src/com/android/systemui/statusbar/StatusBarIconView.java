d1cc6d35a67bd8ce7cbcd24e95d1f112409dfa21
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-01-13 01:19:03 +0200

    Don't show notification count for a notification with just one item
    For example, the Play Store sets the number attribute to 1 for a single
    app update. No point in showing a counter for that.
    
    Change-Id: I818622419f4f043fa8f04339ab24744aca7c1584

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -138,7 +138,7 @@ public class StatusBarIconView extends AnimatedImageView {
         }
 
         if (!numberEquals || force) {
-            if (icon.number > 0 && mShowNotificationCount) {
+            if (icon.number > 1 && mShowNotificationCount) {
                 if (mNumberBackground == null) {
                     mNumberBackground = getContext().getResources().getDrawable(
                             R.drawable.ic_notification_overlay);
