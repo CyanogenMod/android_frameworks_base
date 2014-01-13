38504230d26ba5da84cfb8dd317c3334d36dcea2
burstlam <burstlam@gmail.com>
2014-01-14 07:25:32 +0800

    Merge Derp

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -190,7 +190,7 @@ public class NotificationViewManager {
             boolean ongoingAndReposted = sbn.isOngoing() && mHostView.containsNotification(sbn);
             if (mHostView.addNotification(sbn, (screenOffAndNotCovered || mIsScreenOn) && !ongoingAndReposted,
                         config.forceExpandedView) && config.wakeOnNotification && screenOffAndNotCovered
-                        && !ongoingAndReposted && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM) {
+                        && !ongoingAndReposted && !QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
                 wakeDevice();
                 mHostView.showAllNotifications();
             }
