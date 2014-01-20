538be3a5b6fc41722195ad300b10a17bffc7acf5
jumoog <kilianvonpflugk@gmail.com>
2014-01-20 08:28:39 -0500

    Fix battery icon
    Change-Id: Ieb475591b229be70530e28462a4b1660484fc4ea

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3144,6 +3144,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarContainer.addView(mStatusBarWindow);
 
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
+
+        updateSettings();
+
         mRecreating = false;
     }
 
