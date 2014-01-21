2380b76f0ae019c0e1b0a14ef0a6f636a7ace516
Alexander Martinz <eviscerationls@gmail.com>
2014-01-21 22:20:37 +0100

    Revert "[1/2] Feature: custom notification drawer background"
    This reverts commit d94d0516dd6bb89311e5592c23f05d801b7ebe52.
    
    Change-Id: I62da1f0d580c5ec92c2f3c74fbb1d41c3cc520c1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3741,30 +3741,6 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
-        /**
-         * Sets the portrait background of notification drawer
-         * @hide
-         */
-        public static final String NOTIFICATION_BACKGROUND = "notification_background";
-
-        /**
-         * Sets the landscape background of notification drawer
-         * @hide
-         */
-        public static final String NOTIFICATION_BACKGROUND_LANDSCAPE = "notification_background_landscape";
-
-        /**
-         * Sets the alpha (transparency) of notification wallpaper
-         * @hide
-         */
-        public static final String NOTIF_WALLPAPER_ALPHA = "notif_wallpaper_alpha";
-
-        /**
-         * Sets the alpha (transparency) of notifications
-         * @hide
-         */
-        public static final String NOTIF_ALPHA = "notif_alpha";
-
          /**
          * Allows blurring the lockscreen background
          * @hide
