b9d3e1def72b55947dd64458116b4a7cf70881df
xplodwild <xplod@ouverta.fr>
2014-01-30 13:07:25 +0100

    [1/4] base: Audio Themes Support
    Change-Id: Id80dc7f0bcbd39b5305d6ea0fb8bd9fa516f3634
    
    Conflicts:
    	media/java/android/media/AudioService.java
    	media/java/android/media/MediaActionSound.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4734,6 +4734,12 @@ public final class Settings {
          */
         public static final String GESTURE_TYPE_ONE = "gesture_type_one";
 
+        /**
+         * Defines the custom path to use for UI sound effects (null for default)
+         * @hide
+         */
+        public static final String CUSTOM_SOUND_EFFECTS_PATH = "custom_sound_effects_path";
+
          /**
          * Action for long-pressing back button on lock screen
          * @hide
