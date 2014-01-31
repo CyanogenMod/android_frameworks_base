f5e27962c2cd06a561108da775c800f637a69ea3
Pramod Sivaraman <spramod@codeaurora.org>
2014-01-31 14:26:19 +0530

    Bluetooth:Settings:Handled ACTION_STATE_CHANGED intent at boot time
    BluetoothAdapter.ACTION_STATE_CHANGED intent is added to foreground
    activity to handle BT sync issue with  Power widget at the boot time
    
    Change-Id: Ib26cb4630c711587da789dc920aa66ded97a9a3b
    CRs-Fixed: 576879

--- services/java/com/android/server/BluetoothManagerService.java
+++ services/java/com/android/server/BluetoothManagerService.java
@@ -1283,6 +1283,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             intent.putExtra(BluetoothAdapter.EXTRA_PREVIOUS_STATE, prevState);
             intent.putExtra(BluetoothAdapter.EXTRA_STATE, newState);
             intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
+            intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
             if (DBG) Log.d(TAG,"Bluetooth State Change Intent: " + prevState + " -> " + newState);
             mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
                     BLUETOOTH_PERM);
