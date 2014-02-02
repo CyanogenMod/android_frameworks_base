d77efc75201460100734b7f9b587f0d5a91d7252
David96 <xd_e-mail@gmx.de>
2014-02-02 18:41:35 +0100

    Lockscreen Notifications: try to fix occasionally SystemUI FC on boot
    Change-Id: I3ede8f31cfdd70eb0cf1285e6c9c8002d95caa69

--- packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
@@ -577,8 +577,15 @@ public class NotificationHostView extends FrameLayout {
     }
 
     private void setButtonDrawable() {
-        IStatusBarService statusBar = IStatusBarService.Stub.asInterface(
+        IStatusBarService statusBar = null;
+        try {
+            statusBar = IStatusBarService.Stub.asInterface(
                 ServiceManager.getService(Context.STATUS_BAR_SERVICE));
+        } catch (Exception ex) {
+            Log.w(TAG, "Failed to get statusbar service!");
+            return;
+        }
+
         if (statusBar != null) {
             try {
                 if (mNotifications.size() == 0) {
@@ -588,7 +595,9 @@ public class NotificationHostView extends FrameLayout {
                 } else {
                     statusBar.setButtonDrawable(0, 1);
                 }
-            } catch (RemoteException ex) {}
+            } catch (Exception ex) {
+                Log.e(TAG, "Failed to set button drawable!");
+            }
         }
     }
 
