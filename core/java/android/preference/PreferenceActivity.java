ae1125eab1bb61b6be7357eb1cf4c9825bff98af
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-02-02 16:35:08 +0200

    Fix PreferenceActivity's isMultiPane on single-pane screens
    isMultiPane returns true incorrectly on single-pane screens when under a
    preference header.
    
    Change-Id: Ic17e939c257ea4c8db02e69bb961432e9deb0d39

--- core/java/android/preference/PreferenceActivity.java
+++ core/java/android/preference/PreferenceActivity.java
@@ -699,7 +699,7 @@ public abstract class PreferenceActivity extends ListActivity implements
      * and a preference fragment.
      */
     public boolean isMultiPane() {
-        return hasHeaders() && mPrefsContainer.getVisibility() == View.VISIBLE;
+        return hasHeaders() && !mSinglePane;
     }
 
     /**
