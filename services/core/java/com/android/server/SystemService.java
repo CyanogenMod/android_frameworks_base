f5ba35864afbfd99cfb6c7f38886857f0b462e4f
Sam Mortimer <sam@mortimer.me.uk>
2014-02-01 21:26:25 -0800

    Restore ProfileGroup notification override functionality
    Also clean up a little.
    
    JIRA: CYAN-3164
    Change-Id: I9b71a225996315f517b59d2d6f4074175bbf2d64

--- services/core/java/com/android/server/SystemService.java
+++ services/core/java/com/android/server/SystemService.java
@@ -80,7 +80,7 @@ public abstract class SystemService {
      */
     public static final int PHASE_BOOT_COMPLETED = 1000;
 
-    private final Context mContext;
+    protected final Context mContext;
 
     /**
      * Initializes the system service.
