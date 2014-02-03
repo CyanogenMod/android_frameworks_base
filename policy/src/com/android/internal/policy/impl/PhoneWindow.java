6c3ef750038ed985846f4cbf97cfb93e3eaf8baf
burstlam <burstlam@gmail.com>
2014-02-04 02:35:51 +0800

    Halo : fix floating activity

--- policy/src/com/android/internal/policy/impl/PhoneWindow.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindow.java
@@ -2461,8 +2461,17 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
                 return false;
             }
             final Callback cb = getCallback();
-            return cb != null && !isDestroyed() && mFeatureId < 0 ? cb.dispatchTouchEvent(ev)
-                    : super.dispatchTouchEvent(ev);
+            if (cb != null && !isDestroyed() && mFeatureId < 0) {
+                if (cb instanceof android.app.Activity && mIsFloatingWindow) {
+                    android.app.Activity act = (android.app.Activity)cb;
+                    if (shouldCloseOnTouch(act, ev)) {
+                        act.finishFloating();
+                        return true;
+                    }
+                }
+                return cb.dispatchTouchEvent(ev);
+            }
+            return super.dispatchTouchEvent(ev);
         }
 
         @Override
