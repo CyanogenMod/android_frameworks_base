f0ab5dccd4d9eb1bab79944ec0ec8e68a595d8c1
Eiichi Ohnishi <eiichi.ohnishi@sonymobile.com>
2014-02-04 19:43:56 +0900

    Fix can't change airplane mode from GlobalActions.
    A PhoneStateListener is registered in the constructor of
    GlobalActions which, on rare occasions, leads to a callback being
    received immediately. In these cases because mHasTelephony has not
    been initialized, airplane mode is not refreshed in the callback
    which disables the option to set airplane mode from GlobalActions.
    
    To fix this issue mHasTelephony has to be set before registering
    the listener.
    
    Change-Id: If117a879d3244414b369a1711bf46526b7a5ae24

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -150,13 +150,14 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         ThemeUtils.registerThemeChangeReceiver(context, mThemeChangeReceiver);
 
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        mHasTelephony = cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+
         // get notified of phone state changes
         TelephonyManager telephonyManager =
                 (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         telephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SERVICE_STATE);
-        ConnectivityManager cm = (ConnectivityManager)
-                context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        mHasTelephony = cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
         mContext.getContentResolver().registerContentObserver(
                 Settings.Global.getUriFor(Settings.Global.AIRPLANE_MODE_ON), true,
                 mAirplaneModeObserver);
