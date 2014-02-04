afa9edfbdd656efd7a46b406dbe9b2be49273ef3
Lars Greiss <kufikugel@googlemail.com>
2014-02-04 04:33:46 +0100

    fb: fix on tablets notification drawer height on custom image background
    The measurment bases always on the height of the complete view. Due that we set
    an image as background (does not make a difference if we do it like now with an image
    view or set the background resource for the complete view) the image is calculated into
    the measured height as well which leads into the problem the the panel completly opens
    to the bottom.
    
    To prevent that measure only the real content and decide based of this value how much the
    rubberbands should go into action. This will be only done on devices which set a minimum
    height for the drawer which indicates that we need to measure the content more exactly.
    
    Change-Id: I9772abc8a48d5e46b5bb6e6239c0b4826115ac6a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -42,6 +42,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.systemui.R;
@@ -86,6 +88,11 @@ public class PanelView extends FrameLayout {
     private float mBrakingSpeedPx = 150; // XXX Resource
 
     private View mHandleView;
+    private LinearLayout mNotificationPanelContent;
+    private QuickSettingsScrollView mQsPanelContent;
+    private HorizontalScrollView mNotifiShorcuts;
+    private boolean mIsDynamicHeight = false;
+    private int mContainerMinimumHeight = -1;
     private float mPeekHeight;
     private float mTouchOffset;
     private float mExpandedFraction = 0;
@@ -418,6 +425,9 @@ public class PanelView extends FrameLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         mHandleView = findViewById(R.id.handle);
+        mNotificationPanelContent = (LinearLayout) findViewById(R.id.notification_panel_content);
+        mQsPanelContent = (QuickSettingsScrollView) findViewById(R.id.quicksettings_panel_content);
+        mNotifiShorcuts = (HorizontalScrollView) findViewById(R.id.custom_notification_scrollview);
 
         loadDimens();
 
@@ -594,16 +604,29 @@ public class PanelView extends FrameLayout {
         return mHandleView;
     }
 
+    public void setIsDynamicHeight(boolean isDynamicHeight) {
+        mIsDynamicHeight = isDynamicHeight;
+    }
+
+    public void setContainerMinimumHeight(int minHeight) {
+        mContainerMinimumHeight = minHeight;
+    }
+
     // Rubberbands the panel to hold its contents.
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         if (DEBUG) logf("onMeasure(%d, %d) -> (%d, %d)",
-                widthMeasureSpec, heightMeasureSpec, getMeasuredWidth(), getMeasuredHeight());
+                widthMeasureSpec, heightMeasureSpec, getMeasuredWidth(),
+                mIsDynamicHeight ? getContentHeight() : getMeasuredHeight());
 
         // Did one of our children change size?
-        int newHeight = getMeasuredHeight();
+        int newHeight = mIsDynamicHeight ? getContentHeight() : getMeasuredHeight();
+        if (mIsDynamicHeight && mContainerMinimumHeight > 0
+                && mContainerMinimumHeight > newHeight) {
+            newHeight = mContainerMinimumHeight;
+        }
         if (newHeight != mFullHeight) {
             mFullHeight = newHeight;
             // If the user isn't actively poking us, let's rubberband to the content
@@ -617,6 +640,24 @@ public class PanelView extends FrameLayout {
         setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
     }
 
+    private int getContentHeight() {
+        int height = 0;
+        if (mHandleView != null) {
+            height += mHandleView.getMeasuredHeight();
+        }
+        if (mNotificationPanelContent != null) {
+            height += mNotificationPanelContent.getMeasuredHeight();
+        }
+        if (mQsPanelContent != null) {
+            height += mQsPanelContent.getMeasuredHeight();
+        }
+        if (mNotifiShorcuts != null) {
+            height += mNotifiShorcuts.getMeasuredHeight();
+        }
+        // Our window might have a dropshadow
+        height += getPaddingBottom();
+        return height;
+    }
 
     public void setExpandedHeight(float height) {
         if (DEBUG) logf("setExpandedHeight(%.1f)", height);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3312,9 +3312,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void updateExpandedViewPos(int thingy) {
         if (SPEW) Log.v(TAG, "updateExpandedViewPos");
 
+        final int minHeight = (int)(mNotificationPanelMinHeightFrac * mCurrentDisplaySize.y);
         // on larger devices, the notification panel is propped open a bit
-        mNotificationPanel.setMinimumHeight(
-                (int)(mNotificationPanelMinHeightFrac * mCurrentDisplaySize.y));
+        mNotificationPanel.setIsDynamicHeight(minHeight > 0);
+        mNotificationPanel.setContainerMinimumHeight(minHeight);
 
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mNotificationPanel.getLayoutParams();
         lp.gravity = mNotificationPanelGravity;
@@ -3326,6 +3327,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             lp.gravity = mSettingsPanelGravity;
             lp.setMarginEnd(mNotificationPanelMarginPx);
             mSettingsPanel.setLayoutParams(lp);
+            mSettingsPanel.setIsDynamicHeight(minHeight > 0);
         }
 
         if (ENABLE_HEADS_UP && mHeadsUpNotificationView != null) {
