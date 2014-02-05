461f12bd4d38297d5a0ca5966092ac1a5aedf232
Langes <langes72@gmail.com>
2014-02-05 23:11:17 +0200

    QS: give animation love to network mode and wifi tiles
    Change-Id: I59dcede96ce5895e3ce38c29f36c9f08b11e3599

--- packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/MobileNetworkTypeTile.java
@@ -88,6 +88,9 @@ public class MobileNetworkTypeTile extends QuickSettingsTile implements NetworkS
                 }
                 mMode = NETWORK_MODE_UNKNOWN;
                 mContext.sendBroadcast(intent);
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
 
--- packages/SystemUI/src/com/android/systemui/quicksettings/WiFiTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/WiFiTile.java
@@ -23,6 +23,9 @@ public class WiFiTile extends NetworkTile {
             public void onClick(View v) {
                 WifiManager wfm = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
                 wfm.setWifiEnabled(!wfm.isWifiEnabled());
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
             }
         };
         mOnLongClick = new OnLongClickListener() {
