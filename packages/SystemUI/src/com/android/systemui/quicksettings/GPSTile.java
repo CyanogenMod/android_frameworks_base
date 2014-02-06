b9ab123746060ca65beb125ec964c593de5b3da6
Kiril Mikos <kiril.mik.os@gmail.com>
2014-02-06 22:12:00 +0900

    SystemUI: Adjust GPS tile behaviour.
    If user installed GMS than confirmation dialog will be shown.
    Notification panel should be collapsed in this case.
    
    Change-Id: I6383d7825a5cf4ffcc9c39bdd8084d6adfb60dbb

--- packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/GPSTile.java
@@ -1,18 +1,18 @@
 package com.android.systemui.quicksettings;
 
+import android.app.ActivityManagerNative;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.location.LocationManager;
+import android.content.pm.PackageManager;
+import android.os.RemoteException;
 import android.provider.Settings;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
-import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.policy.LocationController;
 import com.android.systemui.statusbar.policy.LocationController.LocationSettingsChangeCallback;
 
@@ -22,18 +22,39 @@ public class GPSTile extends QuickSettingsTile implements LocationSettingsChange
     ContentResolver mContentResolver;
     private LocationController mLocationController;
     private boolean mLocationEnabled;
+    private boolean mGmsInstalled;
 
     public GPSTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
 
         mContentResolver = mContext.getContentResolver();
+        try {
+            mGmsInstalled =
+                    mContext.getPackageManager().getPackageInfo("com.google.android.gms", 0)
+                            != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            mGmsInstalled = false;
+        }
         mLocationController = new LocationController(mContext);
         mLocationController.addSettingsChangedCallback(this);
 
         mOnClick = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mLocationController.setLocationEnabled(!mLocationEnabled);
+                final boolean newLocationEnabledState = !mLocationEnabled;
+                if (mLocationController.setLocationEnabled(newLocationEnabledState)
+                        && newLocationEnabledState && mGmsInstalled) {
+                    // If we've successfully switched from location off to on, close the
+                    // notifications tray to show the network location provider consent dialog.
+                    Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+                    mContext.sendBroadcast(closeDialog);
+
+                    try {
+                        ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+                    } catch (RemoteException e) {
+                        // Do nothing
+                    }
+                }
             }
         };
 
