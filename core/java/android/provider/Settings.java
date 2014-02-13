8702c2ec7f79d1e81fdd6d2596db9a8d104a4dcb
Evisceration <eviscerationls@gmail.com>
2014-02-13 18:35:47 +0100

    [1/2] base: Add Haptic Feedback to quicktiles
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2827,6 +2827,13 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
 
         /**
+         * Let Quick Settings Panel Tiles vibrate on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_tiles_vibrate";
+
+        /**
          * Allows to show the background activity back the lockscreen
          * @hide
          */
--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -13,6 +13,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,6 +46,8 @@ public class QuickSettingsTile implements OnClickListener {
 
     private Handler mHandler = new Handler();
 
+    protected Vibrator mVibrator;
+
     public QuickSettingsTile(Context context, QuickSettingsController qsc) {
         this(context, qsc, R.layout.quick_settings_tile_basic);
     }
@@ -57,6 +60,7 @@ public class QuickSettingsTile implements OnClickListener {
         mQsc = qsc;
         mTileLayout = layout;
         mPrefs = mContext.getSharedPreferences("quicksettings", Context.MODE_PRIVATE);
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public void setupQuickSettingsTile(LayoutInflater inflater,
@@ -109,6 +113,18 @@ public class QuickSettingsTile implements OnClickListener {
         View image = mTile.findViewById(R.id.image);
         if (image != null && image instanceof ImageView) {
             ((ImageView) image).setImageResource(mDrawable);
+        }
+    }
+
+    public boolean isVibrationEnabled() {
+        return (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUICK_SETTINGS_TILES_VIBRATE, 1) == 1);
+    }
+
+    public void vibrateTile(int duration) {
+        if (!isVibrationEnabled()) { return; }
+        if (mVibrator != null) {
+            if (mVibrator.hasVibrator()) { mVibrator.vibrate(duration); }
         }
     }
 
@@ -177,11 +193,13 @@ public class QuickSettingsTile implements OnClickListener {
             mOnClick.onClick(v);
         }
 
-        ContentResolver resolver = mContext.getContentResolver();
+        final ContentResolver resolver = mContext.getContentResolver();
         boolean shouldCollapse = Settings.System.getIntForUser(resolver,
                 Settings.System.QS_COLLAPSE_PANEL, 0, UserHandle.USER_CURRENT) == 1;
         if (shouldCollapse) {
             mQsc.mBar.collapseAllPanels(true);
         }
+
+        vibrateTile(30);
     }
 }
