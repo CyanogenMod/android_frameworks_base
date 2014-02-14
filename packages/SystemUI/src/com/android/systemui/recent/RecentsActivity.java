98ca0787d77093cc19a048ae7abc58d11051fda5
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-02-14 06:11:44 +0200

    Show live wallpapers in the recents view
    Change-Id: Iefbf4f292b6a9bfdb516f154cc2976db2ba86cde

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -120,12 +120,7 @@ public class RecentsActivity extends Activity {
 
     @Override
     public void onStart() {
-        // Hide wallpaper if it's not a static image
-        if (forceOpaqueBackground(this)) {
-            updateWallpaperVisibility(false);
-        } else {
-            updateWallpaperVisibility(true);
-        }
+        updateWallpaperVisibility(true);
         mShowing = true;
         if (mRecentsPanel != null) {
             // Call and refresh the recent tasks list in case we didn't preload tasks
