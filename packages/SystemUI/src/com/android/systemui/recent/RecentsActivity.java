d629ac4b393a01738d4b949b7e56167b89ab14ad
Veeti Paananen <veeti.paananen@rojekti.fi>
2014-02-14 06:11:44 +0200

    Show live wallpapers in the recents view on high-end devices
    Change-Id: Iefbf4f292b6a9bfdb516f154cc2976db2ba86cde

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -114,12 +114,13 @@ public class RecentsActivity extends Activity {
     }
 
     public static boolean forceOpaqueBackground(Context context) {
-        return WallpaperManager.getInstance(context).getWallpaperInfo() != null;
+        return WallpaperManager.getInstance(context).getWallpaperInfo() != null
+                && !ActivityManager.isHighEndGfx();
     }
 
     @Override
     public void onStart() {
-        // Hide wallpaper if it's not a static image
+        // Hide wallpaper if it's not a static image and device is low-end
         if (forceOpaqueBackground(this)) {
             updateWallpaperVisibility(false);
         } else {
