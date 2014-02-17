3e1d6de72d5f54ba981bb3f45fac34b9d0be4bd2
Loris Chiocca <loris@chiocca.ch>
2014-02-17 13:25:25 +0100

    base: apply custom timeout upon volume dialog instantiation
    The custom timeout is correctly applied at runtime, but after a reboot
    the VolumePanel will revert to the default timeout, since the custom
    value is not read from the config during instantiation
    
    Change-Id: Ifa389011247adeee70ad5f3ff0b0072dd3dff02a

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -364,6 +364,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         final int chosenStyle = Settings.System.getInt(context.getContentResolver(),
                 Settings.System.MODE_VOLUME_OVERLAY, VOLUME_OVERLAY_EXPANDABLE);
         changeOverlayStyle(chosenStyle);
+        mCustomTimeoutDelay = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.VOLUME_PANEL_TIMEOUT, TIMEOUT_DELAY);
 
         context.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.VOLUME_LINK_NOTIFICATION), false,
