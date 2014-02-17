f6c0afa01dc204df79849cd8ffc3251d420cb514
Evisceration <eviscerationls@gmail.com>
2014-02-17 11:19:55 +0100

    Screenrecord: save screenrecord in "Screenrecords" directory
    previously it was saved in "Screenshots"
    also refactor source a bit, remove warnings
    
    Change-Id: Idf9b024bbbf1816aa91e5e455525750a761ddb73

--- packages/SystemUI/src/com/android/systemui/omni/screenrecord/GlobalScreenrecord.java
+++ packages/SystemUI/src/com/android/systemui/omni/screenrecord/GlobalScreenrecord.java
@@ -34,12 +34,9 @@ package com.android.systemui.omni.screenrecord;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.media.MediaActionSound;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
@@ -67,13 +64,12 @@ class GlobalScreenrecord {
     private static final int MSG_TASK_ENDED = 1;
     private static final int MSG_TASK_ERROR = 2;
 
-    private static final String TMP_PATH = "/sdcard/__tmp_screenrecord.mp4";
+    private static final String TMP_PATH = Environment.getExternalStorageDirectory()
+            + File.separator + "__tmp_screenrecord.mp4";
 
     private Context mContext;
     private Handler mHandler;
     private NotificationManager mNotificationManager;
-
-    private MediaActionSound mCameraSound;
 
     private CaptureThread mCaptureThread;
 
@@ -118,7 +114,7 @@ class GlobalScreenrecord {
                 Log.e(TAG, "Error while starting the screenrecord process", e);
             }
         }
-    };
+    }
 
 
     /**
@@ -214,9 +210,11 @@ class GlobalScreenrecord {
         mHandler.postDelayed(new Runnable() { public void run() {
             mCaptureThread = null;
 
-            String fileName = "SCR_" + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".mp4";
-            File pictures = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            File screenshots = new File(pictures, "Screenshots");
+            final String fileName = "SCR_"
+                    + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) + ".mp4";
+            final File pictures = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+            final File screenshots = new File(pictures, "Screenrecords");
 
             if (!screenshots.exists()) {
                 if (!screenshots.mkdir()) {
@@ -225,17 +223,19 @@ class GlobalScreenrecord {
                 }
             }
 
-            File input = new File(TMP_PATH);
+            final File input = new File(TMP_PATH);
             final File output = new File(screenshots, fileName);
 
             Log.d(TAG, "Copying file to " + output.getAbsolutePath());
 
             try {
                 copyFileUsingStream(input, output);
-                input.delete();
+                if (!input.delete()) {
+                    Log.i(TAG, "Couldn't delete temporary screenrecord");
+                }
             } catch (IOException e) {
                 Log.e(TAG, "Unable to copy output file", e);
-                Message msg = Message.obtain(mHandler, MSG_TASK_ERROR);
+                final Message msg = Message.obtain(mHandler, MSG_TASK_ERROR);
                 mHandler.sendMessage(msg);
             }
 
@@ -263,8 +263,12 @@ class GlobalScreenrecord {
                 os.write(buffer, 0, length);
             }
         } finally {
-            is.close();
-            os.close();
+            if (is != null) {
+                is.close();
+            }
+            if (os != null) {
+                os.close();
+            }
         }
     }
 }
