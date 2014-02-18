7109cfcd712cee644b48d8e6acab79bc3329ef7f
Jorge Ruesga <jorge@ruesga.com>
2014-02-19 00:54:42 +0100

    core: fix volume panel more button transparency
    Change-Id: I9f728256efbaf138611822079ea7737cd345bb19
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -1177,7 +1177,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     public void onClick(View v) {
         if (mTranslucentDialog && mShouldRunDropTranslucentAnimation) {
             startRemoveTranslucentAnimation();
-        } else if (v == mMoreButton) {
+        }
+        if (v == mMoreButton) {
             expand();
         } else if (v instanceof ImageView) {
             Intent volumeSettings = new Intent(android.provider.Settings.ACTION_SOUND_SETTINGS);
@@ -1193,8 +1194,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         if (!mTranslucentDialog || mRunningDropTranslucentAnimation) return;
 
         mPanel.getBackground().setAlpha(TRANSLUCENT_START_LEVEL);
-        mMoreButton.setAlpha(0.0f);
-        mDivider.setAlpha(0.0f);
+        mMoreButton.setAlpha(TRANSLUCENT_START_LEVEL);
+        mDivider.setAlpha(TRANSLUCENT_START_LEVEL);
         mShouldRunDropTranslucentAnimation = true;
     }
 
