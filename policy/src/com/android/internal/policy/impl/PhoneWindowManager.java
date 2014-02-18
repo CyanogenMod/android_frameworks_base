1900bac69caa62c07aae0e7cd7abf8085fa423cf
Pakkapon Phongthawee <pure.gif@gmail.com>
2014-02-18 21:53:25 +0700

    Expanded Desktop : Fix Left handed navbar during landscape mode
    from http://review.cyanogenmod.org/#/c/57454
    when you enable expanded desktop and left handed navbar.
    you need to swipe from right edge to show navbar.
    but this patch will change to swipe from left edge to show navbar.
    Video avalible at http://youtu.be/rUy14LQj9iE
    
    Patchset2 : change tab to whitespace
    Patchset3 : remove \r after @override in PhoneWindowManager.java
    Patchset4 : Update commit message
    Patchset5 : Address code formating comments
    Patchset6 : Whitespace
    
    Change-Id: I65a15c6f5e662e429a818484de6b71e81c7ae713

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1263,7 +1263,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     }
                     @Override
                     public void onSwipeFromRight() {
-                        if (mNavigationBar != null && !mNavigationBarOnBottom) {
+                        if (mNavigationBar != null && !mNavigationBarOnBottom &&
+                                !mNavigationBarLeftInLandscape) {
+                            requestTransientBars(mNavigationBar);
+                        }
+                    }
+                    @Override
+                    public void onSwipeFromLeft() {
+                        if (mNavigationBar != null && !mNavigationBarOnBottom &&
+                                mNavigationBarLeftInLandscape) {
                             requestTransientBars(mNavigationBar);
                         }
                     }
