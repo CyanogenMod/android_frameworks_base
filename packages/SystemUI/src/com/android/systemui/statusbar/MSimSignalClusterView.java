a4d15957d1373af61d3e41d33b4d528181e7419f
dhacker29 <dhackerdvm@gmail.com>
2014-02-18 23:22:18 -0600

    Untangle the statusbar for dual sim
    This is just enough to get the statusbar to not forceclose.
    
    TODO in future patch set:
      Implement signal text for dual sim.
      Implement hidden view for dual sim.
    
    Change-Id: Ieeb95e3a0b1653b0e80527e1a388075654097b89

--- packages/SystemUI/src/com/android/systemui/statusbar/MSimSignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/MSimSignalClusterView.java
@@ -45,6 +45,11 @@ public class MSimSignalClusterView
 
     MSimNetworkController mMSimNC;
 
+    public static final int STYLE_NORMAL = 0;
+    public static final int STYLE_TEXT = 1;
+    public static final int STYLE_HIDDEN = 2;
+
+    private int mSignalClusterStyle = STYLE_NORMAL;
     private boolean mWifiVisible = false;
     private int mWifiStrengthId = 0, mWifiActivityId = 0;
     private boolean mMobileVisible = false;
@@ -236,7 +241,10 @@ public class MSimSignalClusterView
                 mSpacer.setVisibility(View.GONE);
             }
         }
+    }
 
+    public void setStyle(int style) {
+        mSignalClusterStyle = style;
     }
 }
 
