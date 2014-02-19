226714ab96be4a3c40a40633f80956954f4dd9a6
dhacker29 <dhackerdvm@gmail.com>
2014-02-18 23:22:18 -0600

    WIP! Untangle the statusbar for dual sim
    This is just enough to get the statusbar to not forceclose.
    TODO:
      Implement signal text for dual sim.
      Implement hidden view for dual sim.
    
    Change-Id: Ieeb95e3a0b1653b0e80527e1a388075654097b89

--- packages/SystemUI/src/com/android/systemui/quicksettings/AirplaneModeTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/AirplaneModeTile.java
@@ -10,6 +10,7 @@ import android.view.View.OnLongClickListener;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.policy.MSimNetworkController;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NetworkController.NetworkSignalChangedCallback;
 
