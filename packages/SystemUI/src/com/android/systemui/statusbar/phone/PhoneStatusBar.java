ae41fbc6946fe98bfc3cfd6143fac847c85a1216
Steve Kondik <shade@chemlab.org>
2014-02-20 11:49:59 -0800

    systemui: Fix crash on theme change
     * Avoid NPE on theme change on devices without a navigation bar.
    
    Change-Id: I29321e7747e7aa87f4539913b5a071f6e7d6a72d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3077,7 +3077,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         makeStatusBarView();
         repositionNavigationBar();
-        mNavigationBarView.updateResources();
+        if (mNavigationBarView != null) {
+            mNavigationBarView.updateResources();
+        }
 
         // recreate StatusBarIconViews.
         for (int i = 0; i < nIcons; i++) {
