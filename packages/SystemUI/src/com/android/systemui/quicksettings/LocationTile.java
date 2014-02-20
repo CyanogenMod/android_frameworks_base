fc19401e2bb6a34ffd11adcd96e903c3fb1a1763
Mahdi-Rom <mahdi.rom@gmail.com>
2014-02-20 12:47:43 +0000

    Location Tile: add swipe actions to switch mode

--- packages/SystemUI/src/com/android/systemui/quicksettings/LocationTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/LocationTile.java
@@ -66,6 +66,22 @@ public class LocationTile extends QuickSettingsTile implements LocationSettingsC
     }
 
     @Override
+    public void onFlingRight() {
+        if (mLocationEnabled) {
+            mLocationController.switchLocationMode(mLocationMode);
+        super.onFlingRight();
+        }
+    }
+
+    @Override
+    public void onFlingLeft() {
+        if (mLocationEnabled) {
+            mLocationController.switchLocationMode(mLocationMode);
+        super.onFlingLeft();
+        }
+    }
+
+    @Override
     void onPostCreate() {
         updateTile();
         super.onPostCreate();
