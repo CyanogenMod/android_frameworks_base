31f3a5fa7f52e90cf541ad3fb4040992df563c42
Eiichi,Ohnishi <eiichi.ohnishi@sonymobile.com>
2014-02-27 19:36:53 +0900

    Fix progress of VolumePanel never refresh
    On some occasions when disconnecting a headset it is possible for
    the volume slider to enter a state where it is no longer updates.
    
    The cause was that ProgressBar enters a state where a call to
    doRefreshProgress no longer updates the progress.
    
    The solution was to make sure mRefreshIsPosted is set to false
    when mRefreshProgressRunnable is removed.
    
    Change-Id: I1929e4916a582d09a7ec4911eb4f0052ad7a51ba

--- core/java/android/widget/ProgressBar.java
+++ core/java/android/widget/ProgressBar.java
@@ -1179,9 +1179,7 @@ public class ProgressBar extends View {
         }
         if (mRefreshProgressRunnable != null) {
             removeCallbacks(mRefreshProgressRunnable);
-        }
-        if (mRefreshProgressRunnable != null && mRefreshIsPosted) {
-            removeCallbacks(mRefreshProgressRunnable);
+            mRefreshIsPosted = false;
         }
         if (mAccessibilityEventSender != null) {
             removeCallbacks(mAccessibilityEventSender);
