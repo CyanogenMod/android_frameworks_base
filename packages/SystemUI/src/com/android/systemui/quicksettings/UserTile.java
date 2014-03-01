CyanogenMod/android_frameworks_base

commit 67735c4efc458534906b2fb4683300c6e84c809b

UserTile : Check if profile has a photo
In the event that the profile doesn't have a photo, don't default to the
user's photo, instead show a default avatar.

Change-Id: I80de324ae1e151693cf3716bf87d39a16096ca12

packages/SystemUI/src/com/android/systemui/quicksettings/UserTile.java
@@ -1,5 +1,9 @@
 package com.android.systemui.quicksettings;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.Intent;
@@ -7,8 +11,10 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.RemoteException;
 import android.os.UserHandle;
@@ -128,17 +134,19 @@ public class UserTile extends QuickSettingsTile {
 
                 String name = null;
                 Drawable avatar = null;
+                String id = null;
                 // If it's a single-user device, get the profile name, since the nickname is not
                 // usually valid
                 if (um.getUsers().size() <= 1) {
                     // Try and read the display name from the local profile
                     final Cursor cursor = context.getContentResolver().query(
-                            Profile.CONTENT_RAW_CONTACTS_URI, new String[] {Phone._ID, Phone.DISPLAY_NAME},
-                            RawContacts.DELETED + "=0", null, null);
+                            Profile.CONTENT_URI, new String[] {Phone._ID, Phone.DISPLAY_NAME},
+                            null, null, null);
                     if (cursor != null) {
                         try {
                             if (cursor.moveToFirst()) {
                                 name = cursor.getString(cursor.getColumnIndex(Phone.DISPLAY_NAME));
+                                id = cursor.getString(cursor.getColumnIndex(Phone._ID));
                             }
                         } finally {
                             cursor.close();
@@ -149,11 +157,24 @@ public class UserTile extends QuickSettingsTile {
                             avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
                             name = mContext.getResources().getString(com.android.internal.R.string.owner_name);
                         } else {
-                            Bitmap rawAvatar = um.getUserIcon(userId);
-                            if (rawAvatar != null) {
+                            Bitmap rawAvatar = null;
+                            InputStream is = null;
+                            try {
+                                Uri.Builder uriBuilder = ContactsContract.RawContacts.CONTENT_URI.buildUpon();
+                                uriBuilder.appendPath(id);
+                                uriBuilder.appendPath(Contacts.Photo.DISPLAY_PHOTO);
+                                is = mContext.getContentResolver().openInputStream(uriBuilder.build());
+                                rawAvatar = BitmapFactory.decodeStream(is);
                                 avatar = new BitmapDrawable(mContext.getResources(), rawAvatar);
-                            } else {
+                            } catch (FileNotFoundException e) {
                                 avatar = mContext.getResources().getDrawable(R.drawable.ic_qs_default_user);
+                            } finally {
+                                if (is != null) {
+                                    try {
+                                        is.close();
+                                    } catch (IOException e) {
+                                    }
+                                }
                             }
                         }
                     }

commit 6d32c0eee6b6c094ed0a7d4af811616f1d1e6050

SystemUI: Fix displaying of user photo.
If owner profile does not have a photo but any associated profile have it
then photo will not be displayed. This case was broken by 67735c4efc458534906b2fb4683300c6e84c809b.

Change-Id: Ied7b1f8a5998c18bffcfd8bd603b4aa35d705c80

packages/SystemUI/src/com/android/systemui/quicksettings/UserTile.java
@@ -140,13 +140,13 @@ public class UserTile extends QuickSettingsTile {
                 if (um.getUsers().size() <= 1) {
                     // Try and read the display name from the local profile
                     final Cursor cursor = context.getContentResolver().query(
-                            Profile.CONTENT_URI, new String[] {Phone._ID, Phone.DISPLAY_NAME},
+                            Profile.CONTENT_URI, new String[] {Phone.PHOTO_FILE_ID, Phone.DISPLAY_NAME},
                             null, null, null);
                     if (cursor != null) {
                         try {
                             if (cursor.moveToFirst()) {
                                 name = cursor.getString(cursor.getColumnIndex(Phone.DISPLAY_NAME));
-                                id = cursor.getString(cursor.getColumnIndex(Phone._ID));
+                                id = cursor.getString(cursor.getColumnIndex(Phone.PHOTO_FILE_ID));
                             }
                         } finally {
                             cursor.close();
@@ -160,9 +160,8 @@ public class UserTile extends QuickSettingsTile {
                             Bitmap rawAvatar = null;
                             InputStream is = null;
                             try {
-                                Uri.Builder uriBuilder = ContactsContract.RawContacts.CONTENT_URI.buildUpon();
+                                Uri.Builder uriBuilder = ContactsContract.DisplayPhoto.CONTENT_URI.buildUpon();
                                 uriBuilder.appendPath(id);
-                                uriBuilder.appendPath(Contacts.Photo.DISPLAY_PHOTO);
                                 is = mContext.getContentResolver().openInputStream(uriBuilder.build());
                                 rawAvatar = BitmapFactory.decodeStream(is);
                                 avatar = new BitmapDrawable(mContext.getResources(), rawAvatar);