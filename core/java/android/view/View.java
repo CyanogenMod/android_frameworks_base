56befa463e314512ff638cd5783397060dada0d9
burstlam <burstlam@gmail.com>
2014-03-05 02:15:06 +0800

    Merge Derp

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -2464,6 +2464,13 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
     public static final int SYSTEM_UI_FLAG_IMMERSIVE_STICKY = 0x00001000;
 
     /**
+     * @hide
+     *
+     * Flag to force showing the navigation bar even in expanded desktop mode.
+     */
+    public static final int SYSTEM_UI_FLAG_SHOW_NAVIGATION_IN_EXPANDED_DESKTOP = 0x00008000;
+
+    /**
      * @deprecated Use {@link #SYSTEM_UI_FLAG_LOW_PROFILE} instead.
      */
     public static final int STATUS_BAR_HIDDEN = SYSTEM_UI_FLAG_LOW_PROFILE;
