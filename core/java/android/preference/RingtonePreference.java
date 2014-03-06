1edbe45d28ae4ed4a221c1fcbfb1f16ef2b99852
Karthik Reddy Katta <a_katta@codeaurora.org>
2014-03-06 16:27:57 +0530

    Ringtone: Add support for selection of phone ringtone for SIM-2.
    -Issue: "The process android.process.media has stopped" pop up is
     displayed when ringtone for SIM-2 is selected.
    -Rootcause: There is no type and URI defined for SIM-2 ringtone
     in the RingtoneManager. This is leading to SQl exception when
     executing a selection query.
    -Fix: Add URIs for the SIM-2 and SIM-3 ringtones. Define
     interfaces to return them to app side based on subscription ID.
    
    CRs-Fixed: 612945
    Change-Id: If68c2d008dd86d2b55d3fb7f5667bbadc238d0b3

--- core/java/android/preference/RingtonePreference.java
+++ core/java/android/preference/RingtonePreference.java
@@ -49,6 +49,7 @@ public class RingtonePreference extends Preference implements
     private boolean mShowSilent;
     
     private int mRequestCode;
+    private int mSubscriptionID = 0; /* Sub-1 by default */
 
     public RingtonePreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -94,6 +95,28 @@ public class RingtonePreference extends Preference implements
      */
     public void setRingtoneType(int type) {
         mRingtoneType = type;
+    }
+
+    /**
+     * Returns the subscription ID.
+     *
+     * @return The current subscription ID.
+     * @see #setSubId(int)
+     * @hide
+     */
+    public int getSubId() {
+        return mSubscriptionID;
+    }
+
+    /**
+     * Sets the subscription ID.
+     *
+     * @param subId subscription ID.
+     * @see #getSubId(int)
+     * @hide
+     */
+    public void setSubId(int subId) {
+        mSubscriptionID = subId;
     }
 
     /**
@@ -162,8 +185,13 @@ public class RingtonePreference extends Preference implements
         
         ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, mShowDefault);
         if (mShowDefault) {
-            ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_DEFAULT_URI,
+            if (getRingtoneType() == RingtoneManager.TYPE_RINGTONE) {
+                ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_DEFAULT_URI,
+                    RingtoneManager.getDefaultRingtoneUriBySubId(getSubId()));
+            } else {
+                ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_DEFAULT_URI,
                     RingtoneManager.getDefaultUri(getRingtoneType()));
+            }
         }
 
         ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, mShowSilent);
