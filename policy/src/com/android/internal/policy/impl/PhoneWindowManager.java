c225e2667450b828207be97f2373b24c3179ed27
Griffin Millender <griffinn.millender@gmail.com>
2014-03-08 12:10:47 -0300

    Frameworks: Ability to Enable/Disable Smart Wake covers
    Change-Id: I83d96696fefedc19fc3bce7265a4fcf2d4c894bc

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -334,7 +334,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mDeskDockEnablesAccelerometer;
     int mLidKeyboardAccessibility;
     int mLidNavigationAccessibility;
-    boolean mLidControlsSleep;
+    boolean mLidControlsSleep = false;
     int mLongPressOnPowerBehavior = -1;
     boolean mScreenOnEarly = false;
     boolean mScreenOnFully = false;
@@ -4382,9 +4382,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         updateRotation(true);
 
         if (lidOpen) {
-            mPowerManager.wakeUp(SystemClock.uptimeMillis());
-        } else if (!mLidControlsSleep) {
-            mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
+            if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_LID_WAKE, 0, UserHandle.USER_CURRENT) == 1) {
+                mPowerManager.wakeUp(SystemClock.uptimeMillis());
+            } else if (!mLidControlsSleep) {
+                mPowerManager.userActivity(SystemClock.uptimeMillis(), false);
+            }
         }
     }
 
@@ -5849,6 +5852,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
+        }
+
+        if (mLidState == LID_CLOSED && Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.LOCKSCREEN_LID_WAKE, 0, UserHandle.USER_CURRENT) == 1) {
+            mPowerManager.goToSleep(SystemClock.uptimeMillis());
         }
     }
 
