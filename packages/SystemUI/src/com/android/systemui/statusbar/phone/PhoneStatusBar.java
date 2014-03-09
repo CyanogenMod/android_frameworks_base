49c078ad9909b0aaad6c8165a9e8328f286a23e0
blackhand1001 <blackhand1001@gmail.com>
2014-03-09 19:49:02 -0400

    SystemUI: Set Status bar opaque during drawer pulldown on tablets only
    The status bar only needs to be set as opaque when the notification
    drawer is pulled down on 600dp and 720dp devices. On phones, this
    is unnecessary as the top header of the drawer covers the status
    bar on the phone UI. This change makes a huge difference in
    reponsiveness on devices that run the phone UI as there is no
    longer a delay before the drawer appears when interacting with it
    on an activity with transclucent window decor.
    
    Patch Set 2: Fix formatting
    
    Change-Id: I932bb92a5245f0323d3317776efbdc9151db449d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2444,7 +2444,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void checkBarModes() {
         if (mDemoMode) return;
         int sbMode = mStatusBarMode;
-        if (panelsEnabled() && (mInteractingWindows & StatusBarManager.WINDOW_STATUS_BAR) != 0) {
+        if (panelsEnabled() && !mHasFlipSettings && (mInteractingWindows & StatusBarManager.WINDOW_STATUS_BAR) != 0) {
             // if panels are expandable, force the status bar opaque on any interaction
             sbMode = MODE_OPAQUE;
         }
