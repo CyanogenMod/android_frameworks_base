5b501acb12949db0058730b77e6dd7b31f101930
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-03-10 13:57:00 +0530

    Lockscreen Blur: Disable if music is playing
    Some apps don't employ full screen album art.
    In that case album art doesn't override wallpaper & content behind screen is clearly visible.
    This fixes that issue.

--- services/java/com/android/server/wm/WindowAnimator.java
+++ services/java/com/android/server/wm/WindowAnimator.java
@@ -12,6 +12,7 @@ import static com.android.server.wm.WindowManagerService.LayoutFields.SET_ORIENT
 import static com.android.server.wm.WindowManagerService.LayoutFields.SET_WALLPAPER_ACTION_PENDING;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.os.Debug;
 import android.os.SystemClock;
 import android.provider.Settings;
@@ -76,6 +77,8 @@ public class WindowAnimator {
     static final int KEYGUARD_ANIMATING_OUT = 3;
     int mForceHiding = KEYGUARD_NOT_SHOWN;
 
+    private AudioManager mAudioManager;
+
     private String forceHidingToString() {
         switch (mForceHiding) {
             case KEYGUARD_NOT_SHOWN:    return "KEYGUARD_NOT_SHOWN";
@@ -90,6 +93,8 @@ public class WindowAnimator {
         mService = service;
         mContext = service.mContext;
         mPolicy = service.mPolicy;
+
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 
         mAnimationRunnable = new Runnable() {
             @Override
@@ -245,7 +250,8 @@ public class WindowAnimator {
                         mService.mFocusMayChange = true;
                     }
                     if (win.isReadyForDisplay()) {
-                        if (Settings.System.getInt(mContext.getContentResolver(),
+                        if (mAudioManager.isMusicActive() || Settings.System.getInt(
+                                mContext.getContentResolver(),
                                 Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 0) {
                             if (nowAnimating) {
                                 if (winAnimator.mAnimationIsEntrance) {
