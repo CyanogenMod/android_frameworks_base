a3e73bff780d7b463fda3b189eff5382a823c32a
Lars Greiss <kufikugel@googlemail.com>
2014-03-12 12:19:41 -0300

    Frameworks: derp
    well it does not effect us due that we proper preload etc. But if it is not
    a cancel repload event should reset the mIsPreloaded flag
    
    sorry
    
    Change-Id: Icc4494e712689282d5b4c06fa7031e7fb659cb6a

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -272,6 +272,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
     public void cancelPreloadingRecentTasksList() {
         if (DEBUG) Log.d(TAG, "cancel preloading recents");
         if (mRecentPanelView != null) {
+            mIsPreloaded = false;
             mRecentPanelView.setCancelledByUser(true);
         }
     }
