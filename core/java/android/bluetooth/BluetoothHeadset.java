8742205bd1b466ba8d76b69b9d26e7c8e8e61359
Sumit Bajpai <sbajpai@codeaurora.org>
2014-03-13 19:30:52 +0530

    BT: HF: Control verbose logging at runtime.
    Verbose logging in BluetoothHeadset can be
    controlled from adb shell.
    
    Change-Id: If63974402a4fc91065c8007a224388af12f49cf7
    CRs-Fixed: 601899

--- core/java/android/bluetooth/BluetoothHeadset.java
+++ core/java/android/bluetooth/BluetoothHeadset.java
@@ -47,7 +47,7 @@ import java.util.List;
 public final class BluetoothHeadset implements BluetoothProfile {
     private static final String TAG = "BluetoothHeadset";
     private static final boolean DBG = true;
-    private static final boolean VDBG = false;
+    private static final boolean VDBG = true;
 
     /**
      * Intent used to broadcast the change in connection state of the Headset
