025ee2b806f3295d9d4aeb6cd3320c9669507c08
Jorge Ruesga <jorge@ruesga.com>
2014-03-14 21:27:34 +0100

    FaceUnlock: Check if faceunlock is disabled
    JIRA: CYAN-3622
    Issue: https://jira.cyanogenmod.org/browse/CYAN-3622
    
    Change-Id: Iaf656f1aecb72610a46183294de25151e8df4b05
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -26,6 +26,7 @@ import android.appwidget.AppWidgetManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -929,7 +930,11 @@ public class LockPatternUtils {
         // Check that it's installed
         PackageManager pm = mContext.getPackageManager();
         try {
-            pm.getPackageInfo("com.android.facelock", PackageManager.GET_ACTIVITIES);
+            PackageInfo pi = pm.getPackageInfo("com.android.facelock",
+                    PackageManager.GET_ACTIVITIES);
+            if (!pi.applicationInfo.enabled) {
+                return false;
+            }
         } catch (PackageManager.NameNotFoundException e) {
             return false;
         }
