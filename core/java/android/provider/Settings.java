fbc5c0fcc7d9efcfcb6af1b23950a771eeff263b
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-03-14 14:16:40 -0400

    [1/4] Dialer lookup
    Squashed commit of the following:
    
    commit 780211109f80d07b11d7a0261544b1a0fed10ea0
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Mar 7 17:11:02 2014 -0500
    
        Add people lookup settings keys
    
        Change-Id: I2ff9bb7684d04f4f9b640939c7db269873aa3308
    
    commit 0e5ed5ac782c2ee253179a03a2c5842ad3de6831
    Author: Xiao-Long Chen <chenxiaolong@cxl.epac.to>
    Date:   Fri Jan 31 11:43:10 2014 -0500
    
        Frameworks: Add settings keys for forward and reverse phone number lookup
    
        Change-Id: I1c4c7825b4361b7a52dd419f83afd7b9fcbe1494
    
    Change-Id: I68e4214e813695021b64c29ab1d831a944a6275a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2766,6 +2766,48 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
 
         /**
+         * Enable looking up of phone numbers of nearby places
+         *
+         * @hide
+         */
+        public static final String ENABLE_FORWARD_LOOKUP = "enable_forward_lookup";
+
+        /**
+         * Enable looking up of phone numbers of people
+         *
+         * @hide
+         */
+        public static final String ENABLE_PEOPLE_LOOKUP = "enable_people_lookup";
+
+        /**
+         * Enable looking up of information of phone numbers not in the contacts
+         *
+         * @hide
+         */
+        public static final String ENABLE_REVERSE_LOOKUP = "enable_reverse_lookup";
+
+        /**
+         * The forward lookup provider
+         *
+         * @hide
+         */
+        public static final String FORWARD_LOOKUP_PROVIDER = "forward_lookup_provider";
+
+        /**
+         * The people lookup provider
+         *
+         * @hide
+         */
+        public static final String PEOPLE_LOOKUP_PROVIDER = "people_lookup_provider";
+
+        /**
+         * The reverse lookup provider
+         *
+         * @hide
+         */
+        public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
+
+        /**
          * Quick Settings Panel Dynamic Tiles
          *
          * @hide
