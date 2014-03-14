6e77e310c0baa61880a185dc5f08fbb5c5810227
Jubakuba <Jubakuba@gmail.com>
2014-03-14 07:22:08 -0600

    Framework: Notify User Interaction on Unlock
    If user is unlocking or opening an app...
    	Notify so the screen timeout is
    	reset.
    
    PS2: Dependency
    
    Change-Id: Id605aa1ee01adfb1a1f043eaac1627eb524442d8

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -130,6 +130,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                 }
             } else {
                 if (target == mTargetOffset) {
+                    mCallback.userActivity(0);
                     mCallback.dismiss(false);
                 } else {
                     int realTarget = target - mTargetOffset - 1;
@@ -137,9 +138,11 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                             ? mStoredTargets[realTarget] : null;
 
                     if (GlowPadView.EMPTY_TARGET.equals(targetUri)) {
+                        mCallback.userActivity(0);
                         mCallback.dismiss(false);
                     } else {
                         SlimActions.processAction(mContext, targetUri, false);
+                        mCallback.userActivity(0);
                     }
                 }
             }
