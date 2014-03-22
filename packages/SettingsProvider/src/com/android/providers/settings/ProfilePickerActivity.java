bc268a90276d4f91d49a2d004f078aa27c712a94
Jorge Ruesga <jorge@ruesga.com>
2014-03-22 11:58:18 +0100

    base: Add theme support to Ringtone and Profile pickers activities
    Patchset 3: Add support for theme RingtonePreference view. The dialog is
                automatically adjusted to the caller activity base theme.
    Patchset 4: Don't break the android api
    
    For Ringtones:
    
    http://review.cyanogenmod.org/#/c/61393/
    
    For Profiles:
    
    Usage:
    
    Intent intent = new Intent(ProfileManager.ACTION_PROFILE_PICKER);
    intent.putExtra(ProfileManager.EXTRA_PROFILE_DIALOG_THEME, R.style.CustomDialogTheme); <<< Add your custom dialog theme here
    startActivityForResult(intent, 0);
    
    Style Usage:
    
    Define an style that inherits from an android base theme
    
    <style name="CustomDialogThemeDark" parent="@*android:style/Theme.Holo.Dialog.Alert">
    
    or
    
    <style name="CustomDialogThemeLight" parent="@*android:style/Theme.Holo.Light.Dialog.Alert">
    
    or define your own dialog style
    
    Change-Id: Icadbcf2c617c51177466989acdab9435066a47a0
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SettingsProvider/src/com/android/providers/settings/ProfilePickerActivity.java
+++ packages/SettingsProvider/src/com/android/providers/settings/ProfilePickerActivity.java
@@ -22,6 +22,9 @@ import android.app.ProfileManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.content.res.Resources.Theme;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -81,6 +84,21 @@ public final class ProfilePickerActivity extends AlertActivity implements
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
+
+        // Set custom theme
+        int themeExtra = intent.getIntExtra(ProfileManager.EXTRA_PROFILE_DIALOG_THEME, 0);
+        if (themeExtra != 0) {
+            try {
+                Resources resources = getPackageManager().getResourcesForApplication(
+                        getCallingPackage());
+                Theme theme = resources.newTheme();
+                theme.applyStyle(themeExtra, true);
+                getTheme().setTo(theme);
+
+            } catch (NameNotFoundException e) {
+                // Resource not available. Fall-through default theme
+            }
+        }
 
         if (savedInstanceState != null) {
             mClickedPos = savedInstanceState.getInt(SAVE_CLICKED_POS, -1);
