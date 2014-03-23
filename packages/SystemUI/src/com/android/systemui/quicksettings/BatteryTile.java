7be89534c81380d5de715660c4167a4fd152bad7
burstlam <burstlam@gmail.com>
2014-03-23 23:11:56 +0800

    Revert "Base: Change battery back tile onclick action"
    This reverts commit bc8a324440c0c001974fb0360415505cc3d373d7.

--- packages/SystemUI/src/com/android/systemui/quicksettings/BatteryTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/BatteryTile.java
@@ -59,10 +59,7 @@ public class BatteryTile extends QuickSettingsTile implements BatteryStateChange
         mOnClick = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.setClassName("com.android.settings", "com.android.settings.BatteryInfo");
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startSettingsActivity(intent);
+                startSettingsActivity(Intent.ACTION_POWER_USAGE_SUMMARY);
             }
         };
 
