d5653ebdb616a56015fe22fba11462a34aaa76a5
Roman Birg <roman@cyngn.com>
2014-03-26 16:17:50 -0700

    add VoiceWakeup Settings value
    Change-Id: I798d1ed9b2bda0fe8a36b8fa2bd17d74d2a09cab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4329,6 +4329,20 @@ public final class Settings {
         public static final String FLIP_ACTION_KEY = "flip_action";
 
         /**
+         * Whether to enable voice wakeup.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String VOICE_WAKEUP = "voice_wakeup";
+
+        /**
+         * An intent (a flattened Uri String) to launch when user voice launch
+         * action is detected. An empty or null string will launch the default
+         * voice search activity.
+         * @hide
+         */
+        public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
