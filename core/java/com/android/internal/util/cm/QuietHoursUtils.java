6f1e8acc86f12cec93dfe24fe32fbe3b63a7c09f
DvTonder <david.vantonder@gmail.com>
2014-03-26 20:55:12 -0500

    WIP: Quiet Hours: Add 4 state (off, timed inactive, timed active and forced on) support
    Change-Id: I2486d2dd40d0db9fceab74d53fba233bac508de3

--- core/java/com/android/internal/util/cm/QuietHoursUtils.java
+++ core/java/com/android/internal/util/cm/QuietHoursUtils.java
@@ -22,27 +22,78 @@ import android.provider.Settings;
 import java.util.Calendar;
 
 public class QuietHoursUtils {
+
+    public static String ACTION_QUIET_HOURS = "com.cyanogenmod.util.quiethours_intent_action";
+    public static final long DAY_IN_MILLIS = 24L * 60L * 60L * 1000L;
+    public static final long MINUTES_IN_MILLIS = 60L * 1000L;
+
     public static boolean inQuietHours(Context context, String option) {
+        if (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.QUIET_HOURS_FORCED, 0) != 0) {
+            // If Quiet hours is forced return immediately
+            return true;
+        }
+
+        // Check if we are in timed Quiet hours mode
         boolean quietHoursEnabled = Settings.System.getInt(context.getContentResolver(),
                 Settings.System.QUIET_HOURS_ENABLED, 0) != 0;
-        int quietHoursStart = Settings.System.getInt(context.getContentResolver(),
-                Settings.System.QUIET_HOURS_START, 0);
-        int quietHoursEnd = Settings.System.getInt(context.getContentResolver(),
-                Settings.System.QUIET_HOURS_END, 0);
         boolean quietHoursOption = Settings.System.getInt(context.getContentResolver(),
                 option, 0) != 0;
-        if (quietHoursEnabled && quietHoursOption && (quietHoursStart != quietHoursEnd)) {
-                // Get the date in "quiet hours" format.
-                Calendar calendar = Calendar.getInstance();
-                int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60
-                                           + calendar.get(Calendar.MINUTE);
-                if (quietHoursEnd < quietHoursStart) {
-                    // Starts at night, ends in the morning.
-                    return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
-                } else {
-                    return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
-                }
+
+        if (quietHoursEnabled && quietHoursOption) {
+            int quietHoursStart = Settings.System.getInt(context.getContentResolver(),
+                    Settings.System.QUIET_HOURS_START, 0);
+            int quietHoursEnd = Settings.System.getInt(context.getContentResolver(),
+                    Settings.System.QUIET_HOURS_END, 0);
+            return isQuietHoursActive(quietHoursStart, quietHoursEnd);
+        }
+
+        return false;
+    }
+
+    public static boolean isQuietHoursActive(int quietHoursStart, int quietHoursEnd) {
+        if (quietHoursStart != quietHoursEnd) {
+            // Get the date in "quiet hours" format.
+            Calendar cal = Calendar.getInstance();
+            int minutes = cal.get(Calendar.HOUR_OF_DAY) * 60 + cal.get(Calendar.MINUTE);
+
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning.
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                // Starts in the morning, ends at night.
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
         }
         return false;
     }
+
+    public static long getNextTrigger(int quietHoursStart, int quietHoursEnd) {
+        Calendar cal = Calendar.getInstance();
+        long offset = 0;
+
+        if (quietHoursStart != quietHoursEnd) {
+            // Get the date in "quiet hours" format.
+            int minutes = cal.get(Calendar.HOUR_OF_DAY) * 60 + cal.get(Calendar.MINUTE);
+
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning.
+                if (minutes > quietHoursStart) {
+                    // End is the next day
+                    offset = ((quietHoursEnd - minutes) * MINUTES_IN_MILLIS) + DAY_IN_MILLIS;
+                } else {
+                    offset = (quietHoursStart - minutes) * MINUTES_IN_MILLIS;
+                }
+            } else {
+                // Starts in the morning, ends at night.
+                if ((minutes > quietHoursStart) && (minutes < quietHoursEnd)) {
+                    offset = (quietHoursEnd - minutes) * MINUTES_IN_MILLIS;
+                } else {
+                    offset = (quietHoursStart - minutes) * MINUTES_IN_MILLIS;
+                }
+            }
+        }
+        return cal.getTimeInMillis() + offset;
+    }
+
 }
--- packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
@@ -1,21 +1,32 @@
 package com.android.systemui.quicksettings;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.android.internal.util.cm.QuietHoursUtils;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
 
 public class QuietHoursTile extends QuickSettingsTile {
 
+    private static int ALARM_ID = 1010101;
+
     private boolean mEnabled;
+    private boolean mForced;
+    private int mStart;
+    private int mEnd;
+    private QuietHoursReceiver mReceiver;
 
     public QuietHoursTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
@@ -23,38 +34,100 @@ public class QuietHoursTile extends QuickSettingsTile {
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_ENABLED,
-                        mEnabled ? 0 : 1, UserHandle.USER_CURRENT);
+                toggleState();
             }
         };
         mOnLongClick = new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.setClassName("com.android.settings", "com.android.settings.Settings$QuietHoursSettingsActivity");
+                intent.setClassName("com.android.settings",
+                        "com.android.settings.Settings$QuietHoursSettingsActivity");
                 startSettingsActivity(intent);
                 return true;
             }
         };
         qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_ENABLED), this);
+        qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_FORCED), this);
+        qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_START), this);
+        qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_END), this);
     }
 
     @Override
     void onPostCreate() {
+        if (mReceiver == null) {
+            mReceiver = new QuietHoursReceiver();
+            mContext.registerReceiver(mReceiver,
+                    new IntentFilter(QuietHoursUtils.ACTION_QUIET_HOURS));
+        }
+
         updateTile();
         super.onPostCreate();
     }
 
+
+    @Override
+    public void onDestroy() {
+        if (mReceiver != null) {
+            // Get rid of the alarm
+            AlarmManager am = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+            am.cancel(getQuietHoursAlarmIntent());
+
+            // Remove the receiver
+            mContext.unregisterReceiver(mReceiver);
+        }
+        super.onDestroy();
+    }
+
     @Override
     public void updateResources() {
+        mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        mForced = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_FORCED, 0, UserHandle.USER_CURRENT) == 1;
+        mStart = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_START, 0, UserHandle.USER_CURRENT);
+        mEnd = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_END, 0, UserHandle.USER_CURRENT);
+
         updateTile();
         super.updateResources();
     }
 
+    private void toggleState() {
+        if (!mEnabled && !mForced) {
+            mEnabled = true;
+        } else if (mEnabled && !mForced) {
+            mForced = true;
+        } else if (mEnabled && mForced) {
+            mEnabled = false;
+            mForced = false;
+        }
+
+        // Store the setting
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED,
+                mEnabled ? 1 : 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_FORCED,
+                mForced ? 1 : 0, UserHandle.USER_CURRENT);
+    }
+
     private synchronized void updateTile() {
-        mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (mEnabled) {
+        if (mEnabled && !mForced) {
+            // Do a generic query to see if we are in the Quiet hours period
+            if (QuietHoursUtils.isQuietHoursActive(mStart, mEnd)) {
+                mDrawable = R.drawable.ic_qs_quiet_hours_on_timed;
+            } else {
+                mDrawable = R.drawable.ic_qs_quiet_hours_off_timed;
+            }
+            mLabel = mContext.getString(R.string.quick_settings_quiethours);
+
+            // Set the alarm for the next trigger time
+            AlarmManager am = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
+            am.set(AlarmManager.RTC, QuietHoursUtils.getNextTrigger(mStart, mEnd),
+                    getQuietHoursAlarmIntent());
+        } else if (mForced) {
             mDrawable = R.drawable.ic_qs_quiet_hours_on;
             mLabel = mContext.getString(R.string.quick_settings_quiethours);
         } else {
@@ -68,4 +141,18 @@ public class QuietHoursTile extends QuickSettingsTile {
         updateResources();
     }
 
+    private class QuietHoursReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateResources();
+        }
+    }
+
+    private PendingIntent getQuietHoursAlarmIntent() {
+        Intent i = new Intent();
+        i.setAction(QuietHoursUtils.ACTION_QUIET_HOURS);
+        PendingIntent pi = PendingIntent.getBroadcast(mContext, ALARM_ID, i,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        return pi;
+    }
 }
