05b90c65fb08e1c7a48dd2e31da38ffd8eaf7cf7
DvTonder <david.vantonder@gmail.com>
2014-03-26 20:55:12 -0500

    Quiet Hours: Add 4 state (off, timed inactive, timed active and forced on) support
    Change-Id: I2486d2dd40d0db9fceab74d53fba233bac508de3

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
@@ -9,6 +9,7 @@ import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.android.internal.util.cm.QuietHoursUtils;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
@@ -16,6 +17,7 @@ import com.android.systemui.statusbar.phone.QuickSettingsController;
 public class QuietHoursTile extends QuickSettingsTile {
 
     private boolean mEnabled;
+    private boolean mForced;
 
     public QuietHoursTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
@@ -23,8 +25,7 @@ public class QuietHoursTile extends QuickSettingsTile {
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_ENABLED,
-                        mEnabled ? 0 : 1, UserHandle.USER_CURRENT);
+                toggleState();
             }
         };
         mOnLongClick = new View.OnLongClickListener() {
@@ -37,6 +38,7 @@ public class QuietHoursTile extends QuickSettingsTile {
             }
         };
         qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_ENABLED), this);
+        qsc.registerObservedContent(Settings.System.getUriFor(Settings.System.QUIET_HOURS_FORCED), this);
     }
 
     @Override
@@ -51,10 +53,37 @@ public class QuietHoursTile extends QuickSettingsTile {
         super.updateResources();
     }
 
+    private void toggleState() {
+        if (!mEnabled && !mForced) {
+            mEnabled = true;
+        } else if (mEnabled && !mForced) {
+            mForced = true;
+        } else if (mEnabled && mForced) {
+            mEnabled = false;
+            mForced = false;
+        }
+
+        // Store the setting
+        Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_ENABLED,
+                mEnabled ? 1 : 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_FORCED,
+                mForced ? 1 : 0, UserHandle.USER_CURRENT);
+    }
+
     private synchronized void updateTile() {
         mEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (mEnabled) {
+        mForced = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_FORCED, 0, UserHandle.USER_CURRENT) == 1;
+        if (mEnabled && !mForced) {
+            // Do a generic query to see if we are in the Quiet hours period
+            if (QuietHoursUtils.inQuietHours(mContext, null)) {
+                mDrawable = R.drawable.ic_qs_quiet_hours_on_timed;
+            } else {
+                mDrawable = R.drawable.ic_qs_quiet_hours_off_timed;
+            }
+            mLabel = mContext.getString(R.string.quick_settings_quiethours);
+        } else if (mForced) {
             mDrawable = R.drawable.ic_qs_quiet_hours_on;
             mLabel = mContext.getString(R.string.quick_settings_quiethours);
         } else {
