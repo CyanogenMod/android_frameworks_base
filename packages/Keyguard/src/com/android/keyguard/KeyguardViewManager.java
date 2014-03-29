4471b3bc18cbaa8f85ed7632a94a7c1f3bbeac5c
lion0738 <lion0738@naver.com>
2014-03-30 01:47:28 +0900

    Fix Lockscreen Blur Rotation
    reset mLastRotation value on every recreate.
    
    Change-Id: Id09ab65d8668ffb1f146e00d6ee2ec8ec09012fb

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -236,6 +236,7 @@ public class KeyguardViewManager {
         if (DEBUG) Log.d(TAG, "show(); mKeyguardView==" + mKeyguardView);
 
         boolean enableScreenRotation = shouldEnableScreenRotation();
+        mLastRotation = 0;
 
         maybeCreateKeyguardLocked(enableScreenRotation, false, options);
         maybeEnableScreenRotation(enableScreenRotation);
