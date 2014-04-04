632fcc33d2a574e5a718efb1e2176b18ccad939a
Juffin Alex Varghese <jalex@codeaurora.org>
2014-04-04 19:26:15 +0530

    Bluetooth-OBEX: Fine tune OBEX Tx MTU for better performace/power
    This will change improve Tx throughput and better power numbers
    during operation.
    
    CRs-Fixed: 625250
    Change-Id: Ife61b1e48c633c3ea2f7bba7afce21c7c16ddd85

--- obex/javax/obex/ObexHelper.java
+++ obex/javax/obex/ObexHelper.java
@@ -76,7 +76,7 @@ public final class ObexHelper {
      * Temporary workaround to be able to push files to Windows 7.
      * TODO: Should be removed as soon as Microsoft updates their driver.
      */
-    public static final int MAX_CLIENT_PACKET_SIZE = 0xFC00;
+    public static final int MAX_CLIENT_PACKET_SIZE = 0xEC88;
     public static final int A2DP_OBEX_MAX_CLIENT_PACKET_SIZE = 0x2000;
 
     public static final int OBEX_OPCODE_CONNECT = 0x80;
