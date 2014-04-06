d7917ebd947599e2d1be7b4d41d80c2dd8af0c04
burstlam <burstlam@gmail.com>
2014-04-06 11:53:54 +0800

    Fix Halo/Floating Window bugs with previous app

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -1333,7 +1333,7 @@ final class ActivityStack {
                         break;
                     }
                 }
-            } else if (prevTask != topTask()) {
+            } else if (prevTask != topTask() && !prev.floatingWindow) {
                 // This task is going away but it was supposed to return to the home task.
                 // Now the task above it has to return to the home task instead.
                 final int taskNdx = mTaskHistory.indexOf(prevTask) + 1;
