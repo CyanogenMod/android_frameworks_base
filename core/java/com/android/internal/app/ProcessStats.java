6676e9fd0c1d747663d725f1b199da381cee1b8c
Yograj Verma <yverma@codeaurora.org>
2014-04-07 16:53:32 +0530

    IndexOutOfBoundsException observed in ProcessStats
    Sometime, IndexOutOfBoundsException is observed in ProcessStats
    while allocating longs array. This could be due to synchronization.
    This fix will return if array index is >= size.
    
    Change-Id: Id34892c6d990004912efdf68593a14ae991e3562
    CRs-Fixed: 638977

--- core/java/com/android/internal/app/ProcessStats.java
+++ core/java/com/android/internal/app/ProcessStats.java
@@ -2351,6 +2351,9 @@ public final class ProcessStats implements Parcelable {
                 mDurationsTable = mStats.mAddLongTable;
                 mDurationsTableSize = mStats.mAddLongTableSize;
             }
+            if (((off>>OFFSET_ARRAY_SHIFT)&OFFSET_ARRAY_MASK) >= mStats.mLongs.size()) {
+                return;
+            }
             long[] longs = mStats.mLongs.get((off>>OFFSET_ARRAY_SHIFT)&OFFSET_ARRAY_MASK);
             if (DEBUG) Slog.d(TAG, "Duration of " + mName + " state " + state + " inc by " + dur
                     + " from " + longs[(off>>OFFSET_INDEX_SHIFT)&OFFSET_INDEX_MASK]);
@@ -2716,6 +2719,9 @@ public final class ProcessStats implements Parcelable {
                 off = mStats.addLongData(~idx, state, PSS_COUNT);
                 mPssTable = mStats.mAddLongTable;
                 mPssTableSize = mStats.mAddLongTableSize;
+            }
+            if (((off>>OFFSET_ARRAY_SHIFT)&OFFSET_ARRAY_MASK) >= mStats.mLongs.size()) {
+                return;
             }
             long[] longs = mStats.mLongs.get((off>>OFFSET_ARRAY_SHIFT)&OFFSET_ARRAY_MASK);
             idx = (off>>OFFSET_INDEX_SHIFT)&OFFSET_INDEX_MASK;
