a6909f153d4b2f95fc712d74f437fed2251788e0
Clark Scheff <clark@cyngn.com>
2014-04-08 17:17:26 -0700

    SysUI: Don't recreate controllers.
    The various controllers, such as mLocationController, get recreated
    every time the status bar is recreated due to a theme change.  We
    should only need to create these if they are null, otherwise use
    the controller as is.  Also, each of these controlers registers
    themselves as a broadcast receiver and are never unregistered before
    recreating them.
    
    Change-Id: I6215aae5a82b7ced8f0f25cf592a804d38825260

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -196,7 +196,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     DockBatteryController mDockBatteryController;
     LocationController mLocationController;
     NetworkController mNetworkController;
-    RotationLockController mRotationLockController;
     MSimNetworkController mMSimNetworkController;
 
     int mNaturalBarHeight = -1;
@@ -834,12 +833,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         setAreThereNotifications();
 
         // Other icons
-        mLocationController = new LocationController(mContext); // will post a notification
-        mBatteryController = new BatteryController(mContext);
-        mDockBatteryController = new DockBatteryController(mContext);
-        mBluetoothController = new BluetoothController(mContext);
-        mRotationLockController = new RotationLockController(mContext);
-
         mBatteryView = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
         mDockBatteryView = (DockBatteryMeterView) mStatusBarView.findViewById(R.id.dock_battery);
 
