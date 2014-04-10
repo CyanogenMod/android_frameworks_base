b69557a7fde7967caff678762a6836f3af31faef
Haiyang Liu <haiyangl@codeaurora.org>
2014-04-10 13:52:05 +0800

    StatusBar: Do not show both voice and data icons on sub2
    Data and voice share the same network for sub2. Do not
    show both voice and data icons on sub two as required.
    
    Add a config config_showVoiceAndDataForSub to control
    whether show two bars for specified sub.
    
    Change-Id: Id461faa5f0e248fde0c2b332975a1a8e62b8cd6f
    CRs-Fixed: 646296

--- packages/SystemUI/src/com/android/systemui/statusbar/MSimSignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/MSimSignalClusterView.java
@@ -49,6 +49,7 @@ public class MSimSignalClusterView
     private final int STATUS_BAR_STYLE_DATA_VOICE = 3;
 
     private int mStyle = 0;
+    private int[] mShowTwoBars;
 
     MSimNetworkController mMSimNC;
 
@@ -161,6 +162,8 @@ public class MSimSignalClusterView
         }
 
         mStyle = context.getResources().getInteger(R.integer.status_bar_style);
+        mShowTwoBars = context.getResources().getIntArray(
+                R.array.config_showVoiceAndDataForSub);
     }
 
     public void setNetworkController(MSimNetworkController nc) {
@@ -442,11 +445,18 @@ public class MSimSignalClusterView
     }
 
     private boolean showBothDataAndVoice(int sub) {
-        return mStyle == STATUS_BAR_STYLE_DATA_VOICE
-            &&((mMobileTypeId[sub] == R.drawable.stat_sys_data_connected_3g)
+        if (mStyle != STATUS_BAR_STYLE_DATA_VOICE) {
+            return false;
+        }
+
+        if (mShowTwoBars[sub] == 0) {
+            return false;
+        }
+
+        return (mMobileTypeId[sub] == R.drawable.stat_sys_data_connected_3g)
                 || (mMobileTypeId[sub] == R.drawable.stat_sys_data_connected_4g)
                 || (mMobileTypeId[sub] == R.drawable.stat_sys_data_fully_connected_3g)
-                || (mMobileTypeId[sub] == R.drawable.stat_sys_data_fully_connected_4g));
+                || (mMobileTypeId[sub] == R.drawable.stat_sys_data_fully_connected_4g);
     }
 
     private boolean showBoth3gAnd1x() {
