4633baa1e3bb611e4640419f5256b166b0abdbc1
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2014-04-12 15:26:29 -0400

    Add OpenCnam paid account settings keys
    Change-Id: I895b5f2b2a8a8de568e5aa1cea5d66ba490985fc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3115,6 +3115,20 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
 
         /**
+         * The OpenCNAM paid account ID
+         *
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_ACCOUNT_SID = "dialer_opencnam_account_sid";
+
+        /**
+         * The OpenCNAM authentication token
+         *
+         * @hide
+         */
+        public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
+
+        /**
          * Quick Settings Panel Dynamic Tiles
          *
          * @hide
