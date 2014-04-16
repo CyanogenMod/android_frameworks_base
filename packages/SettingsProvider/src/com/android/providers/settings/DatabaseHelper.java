cc13e50917807b7a7d66279189f773489dbf0c0f
Ethan Chen <intervigil@gmail.com>
2014-04-16 16:01:05 -0700

    quickboot: Disable quickboot by default
    * We probably don't want to enable this by default on devices that have
      it because it is confusing to users attempting to shut down their
      phones.
    
    Change-Id: I80eb946f93509cb3b32f6c790611597921b9c54a

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2023,19 +2023,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     private void loadQuickBootSetting(SQLiteDatabase db) {
-        boolean qbEnabled = true;
-        final PackageManager pm = mContext.getPackageManager();
-        try {
-            pm.getPackageInfo("com.qapp.quickboot", PackageManager.GET_META_DATA);
-        } catch (NameNotFoundException e) {
-            qbEnabled = false;
-        }
         db.beginTransaction();
         SQLiteStatement stmt = null;
         try {
             stmt = db.compileStatement("INSERT OR REPLACE INTO global(name,value)"
                     + " VALUES(?,?);");
-            loadSetting(stmt, Settings.Global.ENABLE_QUICKBOOT, qbEnabled ? 1 : 0);
+            loadSetting(stmt, Settings.Global.ENABLE_QUICKBOOT, 0);
             db.setTransactionSuccessful();
         } finally {
             db.endTransaction();
