2f6717a230f1feeb927959df41d3601f6f7a361b
D4rKn3sSyS <jesdga95@gmail.com>
2014-04-16 21:09:05 +0200

    Floating Windows : multitasking support
    basic floating window are taken from AOSPA (HALO) by @D4rKn3sSyS
    
    Drawables courtesy of @Alex Cruz thanks to him :)
    
    with heavy change to get this work for multitasking
    
    this feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by @zst123)
    5. Automatically switch focus from other app (taken from multiwindow by @xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    this is still under testing, to make fully works
    so if there any bug because of this, DO NOT REPORT INTO OMNIROM JIRA
    
    to make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    
    Change-Id: Iad00c239de7d66e8bb45205ce626d4f08417b7cd

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -2802,7 +2802,12 @@ public final class ActivityThread {
                 r.stopped = false;
                 r.state = null;
             } catch (Exception e) {
-                // Unable to resume activity
+                /*if (!mInstrumentation.onException(r.activity, e)) {
+                    throw new RuntimeException(
+                        "Unable to resume activity "
+                        + r.intent.getComponent().toShortString()
+                        + ": " + e.toString(), e);
+                }*/
             }
         }
         return r;
--- core/java/android/view/IWindowManager.aidl
+++ core/java/android/view/IWindowManager.aidl
@@ -343,4 +343,10 @@ interface IWindowManager
      * @hide
      */
     int getSystemUIVisibility();
+
+    /** FLOAT VIEW **/
+    Rect getAppFullscreenViewRect();
+    Rect getAppMinimumViewRect();
+    Rect getFloatViewRect();
+    void notifyFloatActivityTouched(IBinder token, boolean force);
 }
