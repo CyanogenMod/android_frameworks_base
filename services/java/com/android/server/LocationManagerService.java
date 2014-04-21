85d77f021198119a3828023757cd017f19e7bf32
Kilian von Pflugk <kilianvonpflugk@gmail.com>
2014-04-21 14:17:44 +0200

    Revert "Fix for crash in LocationManagerService"
    This reverts commit 4f16bed84604c9d83449fed97b74a92d3e1b7255.
    
    Change-Id: Id09140550837dc96af0a8727c55d9d11db3762be

--- services/java/com/android/server/LocationManagerService.java
+++ services/java/com/android/server/LocationManagerService.java
@@ -2127,19 +2127,7 @@ public class LocationManagerService extends ILocationManager.Stub {
 
     private Location screenLocationLocked(Location location, String provider) {
 
-        if (isMockProvider(LocationManager.NETWORK_PROVIDER)) {
-            return location;
-        }
-
-        LocationProviderProxy providerProxy =
-                (LocationProviderProxy)mProvidersByName.get(LocationManager.NETWORK_PROVIDER);
-        if (mComboNlpPackageName == null || providerProxy == null ||
-            false == provider.equals(LocationManager.NETWORK_PROVIDER)) {
-            return location;
-        }
-
-        String connectedNlpPackage = providerProxy.getConnectedPackageName();
-        if (connectedNlpPackage == null || !connectedNlpPackage.equals(mComboNlpPackageName)) {
+        if (false == provider.equals(LocationManager.NETWORK_PROVIDER)) {
             return location;
         }
 
