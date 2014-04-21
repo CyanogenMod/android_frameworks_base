40adb8ddec1057f24d9274f584c5973e150710a1
Evisceration <eviscerationls@gmail.com>
2014-04-21 15:57:51 +0200

    StatusHeader: night begins two hours earlier, at least its dark outside
    Change-Id: Ic044b26f7820828c11c5a3baa2ad22269c8dac02

--- packages/SystemUI/src/com/android/systemui/omni/StatusHeaderMachine.java
+++ packages/SystemUI/src/com/android/systemui/omni/StatusHeaderMachine.java
@@ -39,8 +39,8 @@ public class StatusHeaderMachine {
     private static final String TAG = "StatusHeaderMachine";
 
     // Daily calendar periods
-    // Night starts at 18:00
-    private static final int TIME_NIGHT = 22;
+    // Night starts at 20:00
+    private static final int TIME_NIGHT = 20;
     private static final int DRAWABLE_NIGHT = R.drawable.notifhead_night;
     // Morning starts at 07:00
     private static final int TIME_MORNING = 7;
