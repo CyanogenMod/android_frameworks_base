2d074334f08fe0ea48439515ca3a0b68204fd185
Brint E. Kriebel <bekit@cyngn.com>
2014-04-21 13:50:15 -0700

    Revert "[1/2] Speed up booting on multicore devices."
    This reverts commit f800c5b4e43f1d5f3d6f7d44754adbba1a264d8d.
    
    Conflicts:
    	services/java/com/android/server/pm/PackageManagerService.java
    
    Change-Id: I917040a1ea0cd072ced28417edb7ad3cbbf91714

--- services/java/com/android/server/pm/Installer.java
+++ services/java/com/android/server/pm/Installer.java
@@ -19,15 +19,11 @@ package com.android.server.pm;
 import android.content.pm.PackageStats;
 import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Slog;
-import android.util.SparseArray;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 
 public final class Installer {
     private static final String TAG = "Installer";
@@ -43,16 +39,6 @@ public final class Installer {
     byte buf[] = new byte[1024];
 
     int buflen = 0;
-
-    final SparseArray<String> mResponses = new SparseArray<String>();
-    final ArrayList<Integer> mPendingRequests = new ArrayList<Integer>();
-    int mLastTransactionId = 0;
-    final Object mTransactionIdLock = new Object();
-    int id;
-
-    public Installer() {
-        mPollerThread.start();
-    }
 
     private boolean connect() {
         if (mSocket != null) {
@@ -96,24 +82,12 @@ public final class Installer {
         mSocket = null;
         mIn = null;
         mOut = null;
-
-        synchronized (mPendingRequests) {
-            mPendingRequests.clear();
-            checkPoller();
-        }
-        synchronized (mResponses) {
-            mResponses.notifyAll();
-        }
     }
 
-    private synchronized boolean readBytes(byte buffer[], int len) {
+    private boolean readBytes(byte buffer[], int len) {
         int off = 0, count;
-        try {
-            if (len < 0 || mIn.available() < len)
-                return false;
-        } catch (IOException e) {
+        if (len < 0)
             return false;
-        }
         while (off != len) {
             try {
                 count = mIn.read(buffer, off, len - off);
@@ -139,12 +113,9 @@ public final class Installer {
     private boolean readReply() {
         int len;
         buflen = 0;
-        if (!readBytes(buf, 6))
+        if (!readBytes(buf, 2))
             return false;
-        id = (((int) buf[0]) & 0xff) | ((((int) buf[1]) & 0xff) << 8)
-                | ((((int) buf[2]) & 0xff) << 16) | ((((int) buf[3]) & 0xff) << 24);
-
-        len = (((int) buf[4]) & 0xff) | ((((int) buf[5]) & 0xff) << 8);
+        len = (((int) buf[0]) & 0xff) | ((((int) buf[1]) & 0xff) << 8);
         if ((len < 1) || (len > 1024)) {
             Slog.e(TAG, "invalid reply length (" + len + ")");
             disconnect();
@@ -156,24 +127,11 @@ public final class Installer {
         return true;
     }
 
-    private synchronized boolean writeCommand(String _cmd, int id) {
+    private boolean writeCommand(String _cmd) {
         byte[] cmd = _cmd.getBytes();
         int len = cmd.length;
         if ((len < 1) || (len > 1024))
             return false;
-        buf[0] = (byte) (id & 0xff);
-        buf[1] = (byte) ((id >> 8) & 0xff);
-        buf[2] = (byte) ((id >> 16) & 0xff);
-        buf[3] = (byte) ((id >> 24) & 0xff);
-
-        try {
-            mOut.write(buf, 0, 4);
-        } catch (IOException e) {
-            Slog.e(TAG, "write error");
-            disconnect();
-            return false;
-        }
-
         buf[0] = (byte) (len & 0xff);
         buf[1] = (byte) ((len >> 8) & 0xff);
         try {
@@ -187,128 +145,39 @@ public final class Installer {
         return true;
     }
 
-
-    private String transaction(String cmd) {
+    private synchronized String transaction(String cmd) {
         if (!connect()) {
             Slog.e(TAG, "connection failed");
             return "-1";
         }
 
-        int transactionId;
-        synchronized (mTransactionIdLock) {
-            transactionId = mLastTransactionId++;
-            if (mLastTransactionId < 0) {
-                mLastTransactionId = 0;
+        if (!writeCommand(cmd)) {
+            /*
+             * If installd died and restarted in the background (unlikely but
+             * possible) we'll fail on the next write (this one). Try to
+             * reconnect and write the command one more time before giving up.
+             */
+            Slog.e(TAG, "write command failed? reconnect!");
+            if (!connect() || !writeCommand(cmd)) {
+                return "-1";
             }
-        }
-
-        try {
-            synchronized (mResponses) {
-                long startWaitTime = System.currentTimeMillis();
-                while(mResponses.get(transactionId) == null) {
-                    synchronized (mPendingRequests) {
-                        if (!mPendingRequests.contains(transactionId)) {
-                            if (!writeCommand(cmd, transactionId)) {
-                                /*
-                                 * If installd died and restarted in the background (unlikely but
-                                 * possible) we'll fail on the next write (this one). Try to
-                                 * reconnect and write the command one more time before giving up.
-                                 */
-                                Slog.e(TAG, "write command failed? reconnect!");
-                                if (!connect() || !writeCommand(cmd, transactionId)) {
-                                    return "-1";
-                                }
-                            }
-                            if (LOCAL_DEBUG) {
-                                Slog.i(TAG, "send ["+transactionId+"]: '" + cmd + "'");
-                            }
-                            mPendingRequests.add(transactionId);
-                            checkPoller();
-                        }
-                    }
-                    final long timeToWait = startWaitTime - System.currentTimeMillis() + 100000;
-                    if (timeToWait > 0) {
-                        mResponses.wait(100000);
-                    } else {
-                        Slog.e(TAG, "timeout wating for response");
-                        break;
-                    }
-                }
-            }
-        } catch (InterruptedException e) {
-            return "-1";
-        }
-
-        synchronized (mPendingRequests) {
-            mPendingRequests.remove(new Integer(transactionId));
-        }
-        checkPoller();
-
-        String s;
-        synchronized (mResponses) {
-            s = mResponses.get(transactionId);
-            mResponses.remove(transactionId);
         }
         if (LOCAL_DEBUG) {
-            Slog.i(TAG, "recv: ["+transactionId+"]'" + s + "'");
+            Slog.i(TAG, "send: '" + cmd + "'");
         }
-        return s == null ? "-1" : s;
-    }
-
-    private void checkPoller() {
-        synchronized (mPendingRequests) {
+        if (readReply()) {
+            String s = new String(buf, 0, buflen);
             if (LOCAL_DEBUG) {
-                Slog.v(TAG, "checkPoller handler = " + mHandler +
-                        " empty=" + mPendingRequests.isEmpty());
+                Slog.i(TAG, "recv: '" + s + "'");
             }
-            if (mHandler != null) {
-                if (mPendingRequests.isEmpty()) {
-                    needPolling = false;
-                    mHandler.removeCallbacks(mPollRunnable);
-                } else {
-                    needPolling = true;
-                    mHandler.post(mPollRunnable);
-                }
+            return s;
+        } else {
+            if (LOCAL_DEBUG) {
+                Slog.i(TAG, "fail");
             }
+            return "-1";
         }
     }
-
-    Handler mHandler;
-    Thread mPollerThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            Looper.prepare();
-            mHandler = new Handler();
-            checkPoller();
-            Looper.loop();
-        }
-    });
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-        mHandler.getLooper().quit();
-    }
-
-    boolean needPolling = false;
-    Runnable mPollRunnable = new Runnable() {
-        @Override
-        public void run() {
-            synchronized (mResponses) {
-                while (readReply()) {
-                    String s = new String(buf, 0, buflen);
-                    if (LOCAL_DEBUG) {
-                        Slog.v(TAG, "put: id = " + id + " s = " + s);
-                    }
-                    mResponses.put(id, s);
-                    mResponses.notifyAll();
-                }
-            }
-            if (needPolling) {
-                mHandler.postDelayed(this, 10);
-            }
-        }
-    };
 
     private int execute(String cmd) {
         String res = transaction(cmd);
