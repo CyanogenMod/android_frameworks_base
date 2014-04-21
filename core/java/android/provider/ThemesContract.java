25977783c56c242c7299d28f8e9e6137e67a112c
Andy Mast <andy@cyngn.com>
2014-04-21 11:54:53 -0700

    Style previews [1/3]
    Change-Id: Ifed0ef7829eb61492a39395e8140b99f01c5e6a3
    (cherry picked from commit 5ddfe621501fc33717f64022a7bf8cf04dbe3247)

--- core/java/android/provider/ThemesContract.java
+++ core/java/android/provider/ThemesContract.java
@@ -69,6 +69,12 @@ public class ThemesContract {
         public static final String LOCKSCREEN_URI = "lockscreen_uri";
 
         /**
+         * URI to an image that shows the style (aka skin) with theme applied
+         * <P>Type: TEXT</P>
+         */
+        public static final String STYLE_URI = "style_uri";
+
+        /**
          * TODO: Figure structure for actual animation instead of static
          * URI to an image of the boot_anim.
          * <P>Type: TEXT</P>
