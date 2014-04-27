8b3f0bd68b6681712b88d2e294c6f5426a2f7a00
burstlam <burstlam@gmail.com>
2014-04-27 22:41:08 +0800

    Revert "gps: fixed singleshot session that doesn't stop"
    This reverts commit 1774ba15a52506e4198f973e7ec0f563af2c022c.

--- services/java/com/android/server/LocationManagerService.java
+++ services/java/com/android/server/LocationManagerService.java
@@ -1339,11 +1339,6 @@ public class LocationManagerService extends ILocationManager.Stub {
                 if (removeReceiver && receiverRecords.size() == 0) {
                     removeUpdatesLocked(mReceiver);
                 }
-
-                // and also stop the provider if it has no more update records
-                if (globalRecords.isEmpty()) {
-                    applyRequirementsLocked(this.mProvider);
-                }
             }
         }
 
