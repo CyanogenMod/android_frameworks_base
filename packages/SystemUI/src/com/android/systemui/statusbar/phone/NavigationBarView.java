2de960878fb32b68bd02f78582c77f4d58b4b9e8
Lars Greiss <kufikugel@googlemail.com>
2014-04-27 17:30:32 +0200

    Frameworks: IME cursor controll be consistent
    If user has enabled volume button IME cursor control do not show
    navbar IME cursor control. One is enough and can be asumed to be the
    default one the user is using. So be consistent and just have one enabled.
    
    Change-Id: I1981579ef3ced52ba94bd84d4f4675cea379562e
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -111,6 +111,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
 
     private boolean mShowMenu;
     private boolean mIMENavigation;
+    private boolean mIMECursorDisabled;
     private int mDisabledFlags = 0;
     private int mNavigationIconHints = 0;
 
@@ -277,6 +278,10 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
         mButtonsConfig = ButtonsHelper.getNavBarConfigWithDescription(
                 mContext, "shortcut_action_values", "shortcut_action_entries");
         mButtonIdList = new ArrayList<Integer>();
+
+        mIMECursorDisabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0,
+                UserHandle.USER_CURRENT) > 0;
     }
 
     private void watchForDevicePolicyChanges() {
@@ -921,7 +926,8 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     }
 
     private void handleIMENavigation(boolean show, boolean force) {
-        if (!force && mIMENavigation == show) {
+        if (!force && mIMENavigation == show
+                || mIMECursorDisabled) {
             return;
         }
         View leftIMENavigationKeyView = getLeftIMENavigationButton();
@@ -1021,6 +1027,10 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
 
     public void setNavigationBarCanMove(boolean navigationBarCanMove) {
         mNavigationBarCanMove = navigationBarCanMove;
+    }
+
+    public void setNavigationBarDisableIMECursor(boolean disabled) {
+        mIMECursorDisabled = disabled;
     }
 
     public void reorient() {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -558,6 +558,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ENABLE_ACTIVE_DISPLAY),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -648,6 +651,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mNavigationBarView.setNavigationBarCanMove(mNavigationBarCanMove);
                 }
                 prepareNavigationBarView();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL))) {
+                if (mNavigationBarView != null) {
+                    mNavigationBarView.setNavigationBarDisableIMECursor(
+                            Settings.System.getIntForUser(mContext.getContentResolver(),
+                                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0,
+                                    UserHandle.USER_CURRENT) > 0);
+                }
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_SHORTCUTS_CONFIG))
                 || uri.equals(Settings.System.getUriFor(
