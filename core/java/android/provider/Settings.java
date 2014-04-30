f59d0693be9dba14aad1c15567ebe01a5b57dcf7
Hamster Tian <th0928@126.com>
2014-04-30 16:08:06 +0200

    [1/3]frameworks/base: add "Accept all files" option for incoming files via Bluetooth
    This patch defines a key to store the option value in Settings Provider db
    
    Change-Id: I993d7ec77b49f608a5a17e5a8516e52fc5d82946

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1758,6 +1758,13 @@ public final class Settings {
             "bluetooth_discoverability";
 
         /**
+         * If all file types can be accepted over Bluetooth OBEX.
+         * @hide
+         */
+        public static final String BLUETOOTH_ACCEPT_ALL_FILES =
+            "bluetooth_accept_all_files";
+
+        /**
          * Bluetooth discoverability timeout.  If this value is nonzero, then
          * Bluetooth becomes discoverable for a certain number of seconds,
          * after which is becomes simply connectable.  The value is in seconds.
@@ -3581,6 +3588,7 @@ public final class Settings {
             MMS_AUTO_RETRIEVAL_ON_ROAMING,
             BLUETOOTH_DISCOVERABILITY,
             BLUETOOTH_DISCOVERABILITY_TIMEOUT,
+            BLUETOOTH_ACCEPT_ALL_FILES,
             DIM_SCREEN,
             SCREEN_OFF_TIMEOUT,
             SCREEN_BRIGHTNESS,
