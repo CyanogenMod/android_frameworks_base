86348683c782f088b786197adf1ce68691581fa4
Andy Mast <andy@cyngn.com>
2014-05-01 10:09:35 -0700

    Themes: Support launcher based icon packs [1/2]
    Before this patch, icon packs written for Trebuchet/Nova/Apex could not be used
    with the new theme engine as the formats were different. Particularly the icon package IDs are 7F,
    the same ID as as regular apps. Furthermore the location of resources in these icon packs was different
    from what the theme engine expects. This patch adds support for these icon packs by overriding
    the pkg id.
    
    AssetManager now can override package ids without requiring aapt to recompile the APK.
    Note: XML references will still use the original id compiled with AAPT, so it is only useful for resources
    which do not use xml drawables (such as icon packs).
    
    PackageInfo now has a new member identify "legacy" (3rd party icon packs) written
    for launcher's like Trebuchet, Nova, and Apex.
    
    Change-Id: Icd12c097e156c9626df0772abeb50e00f7b61f64

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -435,7 +435,8 @@ public class ResourcesManager {
 
         if (piTheme == null || piTheme.applicationInfo == null ||
                     piTarget == null || piTarget.applicationInfo == null ||
-                    piAndroid == null || piAndroid.applicationInfo == null) {
+                    piAndroid == null || piAndroid.applicationInfo == null ||
+                    piTheme.mOverlayTargets == null) {
             return false;
         }
 
@@ -498,7 +499,16 @@ public class ResourcesManager {
             String iconDir = ThemeUtils.getIconPackDir(iconPkg); //ThemeUtils.getResDir(piTarget.packageName, piTheme);
             String resTablePath = iconDir + "/resources.arsc";
             String resApkPath = iconDir + "/resources.apk";
-            int cookie = assets.addIconPath(themeIconPath, resTablePath, resApkPath, prefixPath);
+
+            // Legacy Icon packs have everything in their APK
+            if (piIcon.isLegacyIconPackApk) {
+                prefixPath = "";
+                resApkPath = "";
+                resTablePath = "";
+            }
+
+            int cookie = assets.addIconPath(themeIconPath, resTablePath, resApkPath, prefixPath,
+                    Resources.THEME_ICON_PKG_ID);
             if (cookie != 0) {
                 assets.setIconPackCookie(cookie);
                 assets.setIconPackageName(iconPkg);
