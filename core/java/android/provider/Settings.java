6386bad10c83bc85c248b4c5e5c3821156057a99
Roman Birg <roman@cyngn.com>
2014-05-02 16:05:39 -0700

    SystemUI: add equalizer tile
    PS6: Add @Override to onPanelHidden
    PS7: Rename onPanelShownVisible to onPanelShown and cleanup
    PS8: Fix show and hide piping to QuickSettingsController,
    previously only tablet (and not flip) quick settings
    were having the calls go down to the calls.
    PS9: Revert accidental change
    PS10: Fix bad value // add missing perimssion
    PS11: add copyright header
    
    Change-Id: Id7d073699764eacbe1d769a18c249eb80feb2a88
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2861,6 +2861,13 @@ public final class Settings {
          *
          * @hide
          */
+        public static final String QS_EQUALIZER = "qs_dynamic_equalizer";
+
+        /**
+         * Quick Settings Panel Dynamic Tiles
+         *
+         * @hide
+         */
         public static final String QS_DYNAMIC_USBTETHER = "qs_dyanmic_usbtether";
 
         /**
