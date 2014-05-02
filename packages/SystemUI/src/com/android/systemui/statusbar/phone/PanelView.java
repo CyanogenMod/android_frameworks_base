2d3a4dcea11a2abf55016477ed06423415ec537a
Roman Birg <roman@cyngn.com>
2014-05-02 16:05:39 -0700

    SystemUI: add equalizer tile
    PS6: Add @Override to onPanelHidden
    PS7: Rename onPanelShownVisible to onPanelShown and cleanup
    PS8: Fix show and hide piping to QuickSettingsController,
    previously only tablet (and not flip) quick settings
    were having the calls go down to the calls.
    PS9: Revert accidental change
    PS10: Fix bad value // add missing perimssion
    PS11: add copyright header
    PS12: remove old code, cleanup
    PS13: cleanup PanelView PhoneStatusBar reference
    duplication, make quick settings get new calls
    with SettingsPanelView (for tablets)
    PS14: fix tablet NPE
    PS15: code cleanup
    
    Change-Id: Id7d073699764eacbe1d769a18c249eb80feb2a88
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -82,6 +82,9 @@ public class PanelView extends FrameLayout {
     private ObjectAnimator mPeekAnimator;
     private FlingTracker mVelocityTracker;
 
+    protected PhoneStatusBar mStatusBar;
+    private boolean mQuickSettingsVisible;
+
     /**
      * A very simple low-pass velocity filter for motion events; not nearly as sophisticated as
      * VelocityTracker but optimized for the kinds of gestures we expect to see in status bar
@@ -592,6 +595,14 @@ public class PanelView extends FrameLayout {
         if (!(mRubberbandingEnabled && (mTracking || mRubberbanding)) && h > fh) h = fh;
 
         mExpandedHeight = h;
+        if (isCollapsing()) {
+            onQuickSettingsHidden();
+        } else if (isFullyCollapsed()) {
+            // don't hide until dragged all the way up
+            onQuickSettingsHidden();
+        } else {
+            onQuickSettingsVisible();
+        }
 
         if (DEBUG) logf("setExpansion: height=%.1f fh=%.1f tracking=%s rubber=%s", h, fh, mTracking?"T":"f", mRubberbanding?"T":"f");
 
@@ -601,6 +612,20 @@ public class PanelView extends FrameLayout {
 //        setLayoutParams(lp);
 
         mExpandedFraction = Math.min(1f, (fh == 0) ? 0 : h / fh);
+    }
+
+    protected void onQuickSettingsHidden() {
+        if (mQuickSettingsVisible) {
+            mQuickSettingsVisible = false;
+            mStatusBar.onQuickSettingsHidden();
+        }
+    }
+
+    protected void onQuickSettingsVisible() {
+        if (!mQuickSettingsVisible) {
+            mQuickSettingsVisible = true;
+            mStatusBar.onQuickSettingsVisible();
+        }
     }
 
     private float getFullHeight() {
@@ -650,6 +675,10 @@ public class PanelView extends FrameLayout {
 
     public void setBar(PanelBar panelBar) {
         mBar = panelBar;
+    }
+
+    public void setStatusBar(PhoneStatusBar phoneStatusBar) {
+        mStatusBar = phoneStatusBar;
     }
 
     public void collapse() {
