05a9f5074ad1aae1f66bda3e242f15de54901af4
Ethan Chen <intervigil@gmail.com>
2014-05-02 18:50:31 +0000

    Revert "Set some GC parameters only for dalvik"
    This reverts commit de4539603e165f8df0d23a21b6a285213d7f712e.
    
    Change-Id: I2fd71b8b8b6e72cdbe929bfc2afe275cb46e6c29

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4200,28 +4200,23 @@ public final class ActivityThread {
 
         Log.d(TAG, "handleBindApplication:" + data.processName );
 
-        String runtime = VMRuntime.getRuntime().vmLibrary();
-
         String str  = SystemProperties.get("dalvik.vm.heaptargetutilization", "" );
         if( !str.equals("") ){
             float heapUtil = Float.valueOf(str.trim()).floatValue();
             VMRuntime.getRuntime().setTargetHeapUtilization(heapUtil);
             Log.d(TAG, "setTargetHeapUtilization:" + heapUtil );
         }
-        // ART currently doesn't support these methods
-        if (runtime.equals("libdvm.so")) {
-            String heapMinFree = SystemProperties.get("dalvik.vm.heapminfree", "" );
-            int minfree =  parseMemOption(heapMinFree);
-            if( minfree > 0){
-                VMRuntime.getRuntime().setTargetHeapMinFree(minfree);
-                Log.d(TAG, "setTargetHeapMinFree:" + minfree );
-            }
-            String heapConcurrentStart  = SystemProperties.get("dalvik.vm.heapconcurrentstart", "" );
-            int concurr_start =  parseMemOption(heapConcurrentStart);
-            if( concurr_start > 0){
-                VMRuntime.getRuntime().setTargetHeapConcurrentStart(concurr_start);
-                Log.d(TAG, "setTargetHeapConcurrentStart:" + concurr_start );
-            }
+        String heapMinFree = SystemProperties.get("dalvik.vm.heapminfree", "" );
+        int minfree =  parseMemOption(heapMinFree);
+        if( minfree > 0){
+            VMRuntime.getRuntime().setTargetHeapMinFree(minfree);
+            Log.d(TAG, "setTargetHeapMinFree:" + minfree );
+        }
+        String heapConcurrentStart  = SystemProperties.get("dalvik.vm.heapconcurrentstart", "" );
+        int concurr_start =  parseMemOption(heapConcurrentStart);
+        if( concurr_start > 0){
+            VMRuntime.getRuntime().setTargetHeapConcurrentStart(concurr_start);
+            Log.d(TAG, "setTargetHeapConcurrentStart:" + concurr_start );
         }
 
         ////
