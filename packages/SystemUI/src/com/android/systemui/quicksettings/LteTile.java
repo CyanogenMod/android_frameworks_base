e1fe751b28d311b1e25adeaf8f434ff4ec2f030d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-02 20:47:34 +0100

    QS: Add TD-SCDMA support to the LTE toggle tile
    Recognize TD-SDCDMA LTE modes as valid LTE to light up the tile.
    
    While I'm at it, remove the network checks from the actual toggle
    action, since TelephonyManager now ignores the boolean argument
    and always toggles, making the checks pointless.
    
    Change-Id: I8e0020def1f091a6fef9dce4d9700478a826e9a3

--- packages/SystemUI/src/com/android/systemui/quicksettings/LteTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/LteTile.java
@@ -74,6 +74,8 @@ public class LteTile extends QuickSettingsTile {
             case Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA:
             case Phone.NT_MODE_LTE_ONLY:
             case Phone.NT_MODE_LTE_WCDMA:
+            case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
+            case Phone.NT_MODE_TD_SCDMA_WCDMA_LTE:
                 mDrawable = R.drawable.ic_qs_lte_on;
                 mLabel = mContext.getString(R.string.quick_settings_lte);
                 break;
@@ -87,20 +89,7 @@ public class LteTile extends QuickSettingsTile {
     private void toggleLteState() {
         TelephonyManager tm = (TelephonyManager)
             mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        int network = getCurrentPreferredNetworkMode(mContext);
-        switch(network) {
-            case Phone.NT_MODE_GLOBAL:
-            case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
-            case Phone.NT_MODE_LTE_GSM_WCDMA:
-            case Phone.NT_MODE_LTE_CMDA_EVDO_GSM_WCDMA:
-            case Phone.NT_MODE_LTE_ONLY:
-            case Phone.NT_MODE_LTE_WCDMA:
-                tm.toggleLTE(false);
-                break;
-            default:
-                tm.toggleLTE(true);
-                break;
-        }
+        tm.toggleLTE(true);
     }
 
     private static int getCurrentPreferredNetworkMode(Context context) {
