018c5845a2df483e400b9b076d8701ae16216f14
burstlam <burstlam@gmail.com>
2014-05-04 02:50:44 +0800

    Revert "PEEK: configurable pickup timeout (1/2)"
    This reverts commit 547dd36b76661426e598546ebe2e792a41f82f9f.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5662,13 +5662,6 @@ public final class Settings {
         public static final String PEEK_STATE = "peek_state";
 
         /**
-         * Whether to not showing active display when there is annoying notifications.
-         * Set the timeout of peek when pikcing up the device
-         * @hide
-         */
-        public static final String PEEK_PICKUP_TIMEOUT = "peek_pickup_timeout";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
