a063564001305e81a1dcc74b84dcb563b539a25e
Abhinav1997 <abhinav.jhanwar.august2@gmail.com>
2014-05-04 19:19:55 +0000

    Removed p2p_supplicant service because we use wpa_supplicant

--- wifi/java/android/net/wifi/p2p/WifiP2pService.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pService.java
@@ -315,7 +315,7 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
         mContext = context;
 
         //STOPSHIP: get this from native side
-        mInterface = SystemProperties.get("wifi.p2pinterface", "p2p0");
+        mInterface = SystemProperties.get("wifi.p2pinterface", "wlan0");
         mNetworkInfo = new NetworkInfo(ConnectivityManager.TYPE_WIFI_P2P, 0, NETWORKTYPE, "");
 
         mP2pSupported = mContext.getPackageManager().hasSystemFeature(
