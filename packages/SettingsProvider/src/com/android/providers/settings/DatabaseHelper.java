41a6e93eae7406c1e280328043d9a7a3294031df
Steve Kondik <shade@chemlab.org>
2014-05-03 22:42:43 -0700

    settingsprovider: Set default value of animator duration scale
     * In case some crazy person wants to overlay it.
    
    Change-Id: I8072a794a7530012e99e1d3fa42f8757e7fad853

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -73,7 +73,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 117;
+    private static final int DATABASE_VERSION = 118;
 
     private Context mContext;
     private int mUserHandle;
@@ -1874,6 +1874,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             upgradeVersion = 117;
         }
 
+        if (upgradeVersion < 118) {
+            // On CM11 we've added new defaults on these for certain devices,
+            // and added a default for animator duration. Load them if the user
+            // hasn't set them.
+            db.beginTransaction();
+            SQLiteStatement stmt = null;
+            try {
+                stmt = db.compileStatement("INSERT OR IGNORE INTO " +
+                        TABLE_SYSTEM +"(name,value) VALUES(?,?);");
+                loadDefaultAnimationSettings(stmt);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
+            upgradeVersion = 118;
+        }
+
         // *** Remember to update DATABASE_VERSION above!
 
         if (upgradeVersion != currentVersion) {
@@ -2385,6 +2402,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 R.fraction.def_window_animation_scale, 1);
         loadFractionSetting(stmt, Settings.System.TRANSITION_ANIMATION_SCALE,
                 R.fraction.def_window_transition_scale, 1);
+        loadFractionSetting(stmt, Settings.System.ANIMATOR_DURATION_SCALE,
+                R.fraction.def_animator_duration_scale, 1);
     }
 
     private void loadDefaultHapticSettings(SQLiteStatement stmt) {
