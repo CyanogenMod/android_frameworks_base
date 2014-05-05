605b5f57bdf93e70b9e341a926b90e0ae5f79f74
D4rKn3sSyS <jesdga95@gmail.com>
2014-05-05 17:24:57 -0300

    Recents: Launch as Floating App
    Change-Id: Id087a3a0b38ffa637ad9d97647eff6a02d241006
    
    Conflicts:
    	packages/SystemUI/res/values/cr_strings.xml

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -1097,6 +1097,16 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                     } else {
                         throw new IllegalStateException("Oops, no tag on view " + selectedView);
                     }
+                } else if (item.getItemId() == R.id.recent_launch_floating) {
+                    ViewHolder viewHolder = (ViewHolder) selectedView.getTag();
+                    if (viewHolder != null) {
+                        final TaskDescription ad = viewHolder.taskDescription;
+                        Intent intent = ad.intent;
+                        intent.addFlags(Intent.FLAG_FLOATING_WINDOW
+                                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        dismissAndGoBack();
+                        getContext().startActivity(intent);
+                    }
                 } else {
                     return false;
                 }
