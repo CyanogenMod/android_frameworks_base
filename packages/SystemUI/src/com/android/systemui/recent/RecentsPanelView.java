ffcf349128330518242d90504fcc23a9efbcc4a9
Yamil Ghazi <yghazi@gmail.com>
2014-05-07 22:39:42 -0300

    Fix for launch app in floating mode from recents
    Change-Id: I44ff5f56e66c09065eae15d00d59248999536521

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -1101,10 +1101,10 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                     ViewHolder viewHolder = (ViewHolder) selectedView.getTag();
                     if (viewHolder != null) {
                         final TaskDescription ad = viewHolder.taskDescription;
+                        dismissAndGoBack();
                         Intent intent = ad.intent;
                         intent.addFlags(Intent.FLAG_FLOATING_WINDOW
                                 | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        dismissAndGoBack();
                         getContext().startActivity(intent);
                     }
                 } else {
