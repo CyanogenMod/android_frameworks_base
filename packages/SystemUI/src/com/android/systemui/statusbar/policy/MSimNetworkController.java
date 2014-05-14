cb6462dc8f93a6ad82e93516a4fc7ed822998004
Kun Liang <kunliang@codeaurora.org>
2014-05-14 13:51:42 +0800

    AppOps: fix no wifi icon when wifi enabled under dsds mode
    There should be wifi icon on statusbar if wifi enabled and
    security feature enabled. Need the same wifi indicator on
    both ssss and dsds mode.
    
    Change-Id: I2d120c6873272946ae787cc45158e9d04f6741c1

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
@@ -237,7 +237,7 @@ public class MSimNetworkController extends NetworkController {
     public void refreshSignalCluster(MSimSignalCluster cluster, int subscription) {
         cluster.setWifiIndicators(
                 // only show wifi in the cluster if connected or if wifi-only
-                mWifiEnabled && (mWifiConnected || !mHasMobileDataFeature),
+                mWifiEnabled && (mWifiConnected || !mHasMobileDataFeature || mAppOpsStrictEnabled),
                 mWifiIconId,
                 mWifiActivityIconId,
                 mContentDescriptionWifi);
