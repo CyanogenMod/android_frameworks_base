32b1f955b4dfb30a0b00d933a21910125bb4d026
Clark Scheff <clark@cyngn.com>
2014-05-16 11:24:00 -0700

    CM11 Themes: Don't set sys.refresh_theme on icon update
    Change-Id: I5ed49ad2aa93974c54434e092946c4be7699131d

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -12282,7 +12282,6 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     @Override
     public void updateIconMapping(String pkgName) {
-        SystemProperties.set("sys.refresh_theme", "1");
         mContext.enforceCallingOrSelfPermission(
                 android.Manifest.permission.CHANGE_CONFIGURATION,
                 "could not update icon mapping because caller does not have change config permission");
