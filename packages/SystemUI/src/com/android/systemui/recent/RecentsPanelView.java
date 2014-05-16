8d6a7f0f4dea62f803ef012abf6eba2d043adbc8
Oliver Reiche <oliver.reiche@gmail.com>
2014-05-16 06:58:46 -0300

    Keep app when opening floating window from recents panel
    Start activity asynchronously by appending it to system's
    message queue to let RecentsPanelView cleanly execute
    dismissAndGoBack before the floating window gets started.
    
    Change-Id: Ie58ddc81b4ea897b4243404a58fae47583e49e1a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -1239,10 +1239,15 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                     if (viewHolder != null) {
                         final TaskDescription ad = viewHolder.taskDescription;
                         dismissAndGoBack();
-                        Intent intent = ad.intent;
-                        intent.setFlags(Intent.FLAG_FLOATING_WINDOW
-                                | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        getContext().startActivity(intent);
+                        selectedView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = ad.intent;
+                                intent.setFlags(Intent.FLAG_FLOATING_WINDOW
+                                        | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                mContext.startActivity(intent);
+                            }
+                        });
                     }
                 } else if (item.getItemId() == R.id.recent_add_split_view) {
                     // Either start a new activity in split view, or move the current task
