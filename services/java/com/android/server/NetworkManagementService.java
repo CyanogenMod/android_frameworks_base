14518a0882a4fd0cdfb46b92a0bba2e08df15f5e
Harout Hedeshian <harouth@codeaurora.org>
2014-05-16 13:50:24 -0600

    NetworkManagementService: Change exception type thrown on IPv6
    Changed the exception type thrown from IllegalStateException to
    RemoteException whenever the IPv6 tethering rules fail to apply. The
    IllegalStateException is not caught by the caller causing the JVM
    to crash. By throwing RemoteException, the caller will catch and
    print and error message instead of crashing.
    
    CRs-Fixed: 662171
    Change-Id: I92fadd43479a330f09bb20f699a4b9852123ab68

--- services/java/com/android/server/NetworkManagementService.java
+++ services/java/com/android/server/NetworkManagementService.java
@@ -578,7 +578,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub
     }
 
     @Override
-    public void addUpstreamV6Interface(String iface) throws IllegalStateException {
+    public void addUpstreamV6Interface(String iface) throws RemoteException {
         mContext.enforceCallingOrSelfPermission(
                 android.Manifest.permission.ACCESS_NETWORK_STATE, "NetworkManagementService");
 
@@ -588,12 +588,12 @@ public class NetworkManagementService extends INetworkManagementService.Stub
             cmd.appendArg(iface);
             mConnector.execute(cmd);
         } catch (NativeDaemonConnectorException e) {
-            throw new IllegalStateException("Cannot add upstream interface");
+            throw new RemoteException("Cannot add upstream interface");
         }
     }
 
     @Override
-    public void removeUpstreamV6Interface(String iface) throws IllegalStateException {
+    public void removeUpstreamV6Interface(String iface) throws RemoteException {
         mContext.enforceCallingOrSelfPermission(
                 android.Manifest.permission.ACCESS_NETWORK_STATE, "NetworkManagementService");
 
@@ -604,7 +604,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub
             cmd.appendArg(iface);
             mConnector.execute(cmd);
         } catch (NativeDaemonConnectorException e) {
-            throw new IllegalStateException("Cannot remove upstream interface");
+            throw new RemoteException("Cannot remove upstream interface");
         }
     }
 
