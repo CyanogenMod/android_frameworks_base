511f4d9ed6ae64a5a5ee301c3cdb43085c49aab3
sssemil <suleymanovemil8@gmail.com>
2014-05-18 22:16:26 +0500

    framework: Add compass tile (requires http://review.cyanogenmod.org/#/c/64244/)
    Apply this http://review.cyanogenmod.org/#/c/64244/
    
    * Russian translation
    * Slovak translation
    
    Thanks to Jacob Müller for creating base
    
    Change-Id: I9240be46ac6968d0ea8452cac574dbc742bafd9e

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/CompassTile.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.quicksettings;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.View;
+import android.util.Log;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+
+
+public class CompassTile extends QuickSettingsTile implements SensorEventListener {
+    private boolean mActive = false;
+    private float currentDegree = -90f;
+    private int dgr;
+
+    private SensorManager mSensorManager;
+
+    public CompassTile(Context context, QuickSettingsController qsc) {
+        super(context, qsc);
+
+        // Initialize SensorManager
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+        mOnClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleState();
+                updateResources();
+            }
+        };
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile();
+        super.onPostCreate();
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        super.updateResources();
+    }
+
+    private synchronized void updateTile() {
+        if (mActive) {
+            mDrawable = R.drawable.ic_qs_compass_on;
+            mSensorManager.registerListener(this, mSensorManager
+                    .getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+        } else {
+            mSensorManager.unregisterListener(this);
+            mDrawable = R.drawable.ic_qs_compass_off;
+            mLabel = mContext.getString(R.string.quick_settings_compass);
+            mDegree = 0;
+        }
+    }
+
+    protected void toggleState() {
+        mActive = !mActive;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float degree = Math.round(event.values[0]);
+        mLabel = Float.toString(degree) + "°";
+        mDegree = -degree;
+        super.updateResources();
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing here
+    }
+}
