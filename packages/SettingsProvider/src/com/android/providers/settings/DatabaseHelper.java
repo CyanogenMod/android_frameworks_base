2500e80c9876872989599b70537f9d92283aa2cb
Steve Kondik <shade@chemlab.org>
2014-05-18 15:44:54 -0700

    settings: Enable the status bar double-tap to sleep gesture
    Change-Id: Ibe4f1d0c864f261211d7ded418abfe45633e1392
    
    Conflicts:
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2147,6 +2147,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 loadStringSetting(stmt, Settings.System.LOCKSCREEN_TARGETS,
                         R.string.def_lockscreen_targets_no_telephony);
             }
+
+            loadIntegerSetting(stmt, Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
+                    R.integer.def_double_tap_sleep_gesture);
+
         } finally {
             if (stmt != null) stmt.close();
         }
