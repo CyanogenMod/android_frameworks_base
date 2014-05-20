9567ddaafffb3cf6ba7cfc745958a3414261190f
Steve Kondik <shade@chemlab.org>
2014-05-20 14:27:49 -0700

    settings: Add support for default ribbon config
    Change-Id: Ib4567431ce136813466ec87bba0581caafe0b352

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2088,6 +2088,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    private void loadRibbonSetting(SQLiteStatement stmt) {
+        String tiles = mContext.getResources().getString(R.string.def_quick_settings_ribbon_tiles);
+        if (!TextUtils.isEmpty(tiles)) {
+            loadSetting(stmt, Settings.System.QS_QUICK_ACCESS, "1");
+            loadSetting(stmt, Settings.System.QS_QUICK_ACCESS_LINKED, "0");
+            loadSetting(stmt, Settings.System.QUICK_SETTINGS_RIBBON_TILES, tiles);
+        }
+    }
+
     private void loadSettings(SQLiteDatabase db) {
         loadSystemSettings(db);
         loadSecureSettings(db);
@@ -2168,6 +2177,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
                     R.integer.def_double_tap_sleep_gesture);
+
+            loadRibbonSetting(stmt);
 
         } finally {
             if (stmt != null) stmt.close();
