4ccbcde2ccffab2bd04b862b68732a6d1bdd62c0
ThatKawaiiGuy <ivanthegreat15@gmail.com>
2014-05-24 18:26:34 -0400

    forgot that

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3297,13 +3297,6 @@ public final class Settings {
         */
         public static final String COMBINED_BAR_AUTO_HIDE = "combined_bar_auto_hide";
         
-      	/**
-         * Animate-flip Quick Settings Panel Tiles on click
-         *
-         * @hide
-         */
-        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
-
         /**
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
