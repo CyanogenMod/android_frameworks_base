3919f7589ef4d6a58a8000c00701bcdb830f7612
dankoman <dankoman30@gmail.com>
2014-05-24 15:39:33 -0400

    FB:  Add transparency to Glowpad background [2/3]
    Adds the option to make glowpad view background transparent on incoming call so the caller's entire picture can be seen.
    
    PS3: Fix File Permissions
    
    Change-Id: I0c6d883b86f05055fbf5865ea98f6cb144da7b27

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2488,6 +2488,12 @@ public final class Settings {
         public static final String DTMF_TONE_TYPE_WHEN_DIALING = "dtmf_tone_type";
 
         /**
+         * Whether incall glowpad background is transparent or not.  The value is
+         * boolean (1 or 0).
+        */
+        public static final String INCALL_GLOWPAD_TRANSPARENCY = "incall_glowpad_transparency";
+
+        /**
          * Whether the hearing aid is enabled. The value is
          * boolean (1 or 0).
          * @hide
