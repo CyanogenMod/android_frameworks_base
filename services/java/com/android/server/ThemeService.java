406def65c6a2c453fd7b4415c71da0d97ead999f
Clark Scheff <clark@cyngn.com>
2014-05-28 02:40:35 -0700

    CM11 Themes: Don't kill setup wizards
    Change-Id: Ib68dbf08b5b65f66e73f2f22882932ced0851093

--- services/java/com/android/server/ThemeService.java
+++ services/java/com/android/server/ThemeService.java
@@ -77,6 +77,9 @@ import java.util.List;
 public class ThemeService extends IThemeService.Stub {
     private static final String TAG = ThemeService.class.getName();
 
+    private static final String GOOGLE_SETUPWIZARD_PACKAGE = "com.google.android.setupwizard";
+    private static final String CM_SETUPWIZARD_PACKAGE = "com.cyanogenmod.account";
+
     private HandlerThread mWorker;
     private ThemeWorkerHandler mHandler;
     private Context mContext;
@@ -597,7 +600,8 @@ public class ThemeService extends IThemeService.Stub {
 
         List<ResolveInfo> infos = pm.queryIntentActivities(homeIntent, 0);
         for(ResolveInfo info : infos) {
-            if (info.activityInfo != null && info.activityInfo.applicationInfo != null) {
+            if (info.activityInfo != null && info.activityInfo.applicationInfo != null &&
+                    !isSetupActivity(info)) {
                 String pkgToStop = info.activityInfo.applicationInfo.packageName;
                 Log.d(TAG, "Force stopping " +  pkgToStop + " for theme change");
                 try {
@@ -607,6 +611,11 @@ public class ThemeService extends IThemeService.Stub {
                 }
             }
         }
+    }
+
+    private boolean isSetupActivity(ResolveInfo info) {
+        return GOOGLE_SETUPWIZARD_PACKAGE.equals(info.activityInfo.packageName) ||
+               CM_SETUPWIZARD_PACKAGE.equals(info.activityInfo.packageName);
     }
 
     private void postProgress(String pkgName) {
