800cd0efcd22f9072640513d5164a03b75f11497
Evisceration <eviscerationls@gmail.com>
2014-06-01 20:01:12 +0200

    Bring DownloadManager API inline with AOSPA
    Change-Id: I9468e59617e2e6fd01a622c1c499ee52a096d291

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -983,6 +983,54 @@ public class DownloadManager {
     }
 
     /**
+     * Set the download status to STATUS_PAUSED_BY_MANUAL when user pause download.
+     *
+     * @param id the ID of the download to be paused
+     * @return the number of downloads actually updated
+     * @hide
+     */
+    public int pauseDownload(long... ids) {
+        if (ids == null || ids.length == 0) {
+            // called with nothing to pause!
+            throw new IllegalArgumentException("input param 'ids' can't be null");
+        }
+        ContentValues values = new ContentValues();
+        values.put(Downloads.Impl.COLUMN_STATUS, Downloads.Impl.STATUS_PAUSED_BY_MANUAL);
+        // if only one id is passed in, then include it in the uri itself.
+        // this will eliminate a full database scan in the download service.
+        if (ids.length == 1) {
+            return mResolver.update(ContentUris.withAppendedId(mBaseUri, ids[0]), values,
+                                    null, null);
+        }
+        return mResolver.update(mBaseUri, values, getWhereClauseForIds(ids),
+                                getWhereArgsForIds(ids));
+    }
+
+    /**
+     * Set the download status to STATUS_RUNNING when user resume a paused download.
+     *
+     * @param id the ID of the download to be resumed
+     * @return the number of downloads actually updated
+     * @hide
+     */
+    public int resumeDownload(long... ids) {
+        if (ids == null || ids.length == 0) {
+            // called with nothing to resume!
+            throw new IllegalArgumentException("input param 'ids' can't be null");
+        }
+        ContentValues values = new ContentValues();
+        values.put(Downloads.Impl.COLUMN_STATUS, Downloads.Impl.STATUS_RUNNING);
+        // if only one id is passed in, then include it in the uri itself.
+        // this will eliminate a full database scan in the download service.
+        if (ids.length == 1) {
+            return mResolver.update(ContentUris.withAppendedId(mBaseUri, ids[0]), values,
+                                    null, null);
+        }
+        return mResolver.update(mBaseUri, values, getWhereClauseForIds(ids),
+                                getWhereArgsForIds(ids));
+    }
+
+    /**
      * Cancel downloads and remove them from the download manager.  Each download will be stopped if
      * it was running, and it will no longer be accessible through the download manager.
      * If there is a downloaded file, partial or complete, it is deleted.
@@ -1130,34 +1178,6 @@ public class DownloadManager {
         values.put(Downloads.Impl.COLUMN_STATUS, Downloads.Impl.STATUS_PENDING);
         values.put(Downloads.Impl.COLUMN_FAILED_CONNECTIONS, 0);
         mResolver.update(mBaseUri, values, getWhereClauseForIds(ids), getWhereArgsForIds(ids));
-    }
-
-    /**
-     * Pause the given running download by manual.
-     *
-     * @param id the ID of the download to be paused
-     * @return the number of downloads actually updated
-     * @hide
-     */
-    public int pauseDownload(long id) {
-        ContentValues values = new ContentValues();
-        values.put(Downloads.Impl.COLUMN_STATUS, Downloads.Impl.STATUS_PAUSED_BY_MANUAL);
-
-        return mResolver.update(ContentUris.withAppendedId(mBaseUri, id), values, null, null);
-    }
-
-    /**
-     * Resume the given paused download by manual.
-     *
-     * @param id the ID of the download to be resumed
-     * @return the number of downloads actually updated
-     * @hide
-     */
-    public int resumeDownload(long id) {
-       ContentValues values = new ContentValues();
-       values.put(Downloads.Impl.COLUMN_STATUS, Downloads.Impl.STATUS_RUNNING);
-
-       return mResolver.update(ContentUris.withAppendedId(mBaseUri, id), values, null, null);
     }
 
     /**
@@ -1436,6 +1456,9 @@ public class DownloadManager {
 
                 case Downloads.Impl.STATUS_FILE_ALREADY_EXISTS_ERROR:
                     return ERROR_FILE_ALREADY_EXISTS;
+
+                case Downloads.Impl.STATUS_BLOCKED:
+                    return ERROR_BLOCKED;
 
                 default:
                     return ERROR_UNKNOWN;
