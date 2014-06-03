VanirAOSP/frameworks_base

Don't read settings constant on every touch event for double tap to sleep
Change-Id: I5108d24b7006e518926ce02d1a5eae8da406df8f

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -258,6 +258,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private TilesChangedObserver mTilesChangedObserver;
     private SettingsObserver mSettingsObserver;
     boolean mSearchPanelAllowed = true;
+    boolean mDoubleTapToSleep;
 
     // Ribbon settings
     private boolean mHasQuickAccessSettings;
@@ -397,6 +398,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.ENABLE_NAVIGATION_BAR), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ENABLE_NAVIGATION_RING), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE), false, this);
             updateSettings();
         }
 
@@ -3251,6 +3254,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             updateNavigationBarState();
         }
 
+        mDoubleTapToSleep = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1;
+
         orientationDependentImmersive = Settings.System.getIntForUser(resolver,
                 Settings.System.IMMERSIVE_ORIENTATION, 0,
                 UserHandle.USER_CURRENT);
@@ -3258,6 +3264,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         immersive = Settings.System.getIntForUser(resolver,
                 Settings.System.GLOBAL_IMMERSIVE_MODE_STATE, 0,
                 UserHandle.USER_CURRENT) == 1;
+    }
+
+    protected boolean isDoubleTapEnabled() {
+        return mDoubleTapToSleep;
     }
 
     private void updateRotationState() {

CyanogenMod/android_frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -283,6 +283,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     QuickSettingsContainerView mSettingsContainer;
     int mSettingsPanelGravity;
 
+    boolean mDoubleTapToSleep;
+
     // Ribbon settings
     private boolean mHasQuickAccessSettings;
     private boolean mQuickAccessLayoutLinked = true;
@@ -608,6 +610,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QUICK_SETTINGS_RIBBON_TILES), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -805,6 +810,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mFlipInterval = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.REMINDER_ALERT_INTERVAL, 1500, UserHandle.USER_CURRENT);
 
+            mDoubleTapToSleep = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0, UserHandle.USER_CURRENT) == 1;
+
             boolean reminderHolder = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
             if (reminderHolder != mReminderEnabled) {
@@ -822,6 +830,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 enableOrDisableReminder();
             }
         }
+    }
+
+    protected boolean isDoubleTapEnabled() {
+        return mDoubleTapToSleep;
     }
 
     private void updateActiveDisplayViewState() {