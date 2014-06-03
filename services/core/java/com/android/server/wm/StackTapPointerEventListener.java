bbb04b99166bcebb6a4591e566352b86d9551064
Uday Kiran jandhyala <ukiran@codeaurora.org>
2014-06-03 16:49:29 +0530

    Added validity check for Pointer Index
    Checking if the pointer index is valid before querying for X & Y
    coordinates. This is done to avoid IllegalArgumentException.
    
    Change-Id: Ic7f3022d33c313a0bfe8b55c9bc89019b33d23b7

--- services/core/java/com/android/server/wm/StackTapPointerEventListener.java
+++ services/core/java/com/android/server/wm/StackTapPointerEventListener.java
@@ -56,6 +56,11 @@ public class StackTapPointerEventListener implements PointerEventListener {
             case MotionEvent.ACTION_MOVE:
                 if (mPointerId >= 0) {
                     int index = motionEvent.findPointerIndex(mPointerId);
+                    if (index < 0) {
+                        mPointerId = -1;
+                        break;
+                    }
+
                     if ((motionEvent.getEventTime() - motionEvent.getDownTime()) > TAP_TIMEOUT_MSEC
                             || (motionEvent.getX(index) - mDownX) > mMotionSlop
                             || (motionEvent.getY(index) - mDownY) > mMotionSlop) {
