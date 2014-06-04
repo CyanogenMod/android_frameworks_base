72c5f5dc0901a947641ef1181d9b7c37bc574270
Abhishek Arpure <aarpure@codeaurora.org>
2014-06-04 19:35:10 +0530

    Add reboot reason to detect device shutdown
    This change will detect silent device shutdown
    
    Change-Id: Ia3c13e372b02dd4d3ec4a99d1c1f7c4787977ac3

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -262,6 +262,7 @@ public final class BatteryService extends SystemService {
                 @Override
                 public void run() {
                     if (ActivityManagerNative.isSystemReady()) {
+                        Slog.e(TAG, "silent_reboot shutdownIfNoPowerLocked");
                         Intent intent = new Intent(Intent.ACTION_REQUEST_SHUTDOWN);
                         intent.putExtra(Intent.EXTRA_KEY_CONFIRM, false);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -281,6 +282,7 @@ public final class BatteryService extends SystemService {
                 @Override
                 public void run() {
                     if (ActivityManagerNative.isSystemReady()) {
+                        Slog.e(TAG, "silent_reboot shutdownIfOverTempLocked");
                         Intent intent = new Intent(Intent.ACTION_REQUEST_SHUTDOWN);
                         intent.putExtra(Intent.EXTRA_KEY_CONFIRM, false);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
