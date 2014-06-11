4c803734d3a7c9a44fa9acfa89da473c1689aa58
zweif <gurkensindgruen@gmail.com>
2014-06-11 17:27:24 +0200

    Framework: fix disable screen-off animation
    This line got lost in "Merge tag 'android-4.4.3_r1' into HEAD" and it's not possible to disable screen-off animation.
    
    Change-Id: I050647afb30fae4547db561a2d3e212213e12873

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -829,7 +829,8 @@ final class DisplayPowerController {
                             mElectronBeamFadesConfig() ?
                                     ElectronBeam.MODE_FADE :
                                             ElectronBeam.MODE_COOL_DOWN)
-                            && mPowerState.isScreenOn()) {
+                            && mPowerState.isScreenOn()
+                            && useScreenOffAnimation()) {
                         mElectronBeamOffAnimator.start();
                     } else {
                         mElectronBeamOffAnimator.end();
