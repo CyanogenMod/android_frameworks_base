a89329fc319c991a19f96796e92cfd68258fa13e
Michael Bestas <mikeioannina@gmail.com>
2014-06-11 21:23:27 +0300

    Remove lockscreen slide delay option (2/2)
    Change-Id: Ibf93519637e0c08bad5d2224c3f76c99b0af60f7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3095,27 +3095,6 @@ public final class Settings {
         public static final int STATUS_BAR_COLLAPSE_IF_NO_CLEARABLE = 2;
 
         /**
-         * Whether to use a separate delay for "slide to unlock" and security
-         * lock
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_DELAY_TOGGLE = "screen_lock_slide_delay_toggle";
-
-        /**
-         * How many ms to delay before enabling the "slide to unlock" screen
-         * lock when the screen goes off due to timeout
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_TIMEOUT_DELAY = "screen_lock_slide_timeout_delay";
-
-        /**
-         * How many ms to delay before enabling the "slide to unlock" screen
-         * lock when the screen is turned off by the user
-         * @hide
-         */
-        public static final String SCREEN_LOCK_SLIDE_SCREENOFF_DELAY = "screen_lock_slide_screenoff_delay";
-
-        /**
          * Whether to use the custom quick unlock screen control
          * @hide
          */
