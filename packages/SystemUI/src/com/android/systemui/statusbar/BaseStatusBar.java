639c8452fa89b8f2599ae6f8d974aacf027eae54
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-06-12 19:47:25 +0530

    Floating window: Fix possible NPE
    Avoid possible NPE if app provides no interface such as pushbullet's
    test notification & other apps which provide one.
    
    Change-Id: I35c13959296a56e44f21f1aee0a736e7f786aa57
    
    Conflicts:
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -751,6 +751,12 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private void launchFloating(PendingIntent pIntent) {
+        if (pIntent == null) {
+            String text = mContext.getResources().getString(R.string.status_bar_floating_no_interface);
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(mContext, text, duration).show();
+            return;
+        }
         Intent overlay = new Intent();
         overlay.addFlags(Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         try {
