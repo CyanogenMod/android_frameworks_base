52cfeeff868517ebf0597e2b709854ddeb66f340
Clark Scheff <clark@cyngn.com>
2014-06-18 16:39:16 -0700

    Use systemui's applied theme for notifications.
    Notifications contain RemoteViews which are inflated using the
    application's context for which this notification belongs to.  This
    can look out of place if SystemUI is using a different theme than the
    rest of the system.
    
    This patch will use SystemUIs theme when inflating the RemoteViews,
    giving us a more consistent look in the notification drawer.
    
    Change-Id: I9514ce7fcc4858bad3d3c4190f55c1f5a1441d7c

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -32,6 +32,7 @@ import android.content.pm.IPackageDataObserver;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
+import android.content.res.ThemeConfig;
 import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.net.Uri;
@@ -763,10 +764,15 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         View contentViewLocal = null;
         View bigContentViewLocal = null;
+        final ThemeConfig themeConfig = mContext.getResources().getConfiguration().themeConfig;
+        String themePackageName = themeConfig != null ?
+                themeConfig.getOverlayPkgNameForApp(mContext.getPackageName()) : null;
         try {
-            contentViewLocal = contentView.apply(mContext, adaptive, mOnClickHandler);
+            contentViewLocal = contentView.apply(mContext, adaptive, mOnClickHandler,
+                    themePackageName);
             if (bigContentView != null) {
-                bigContentViewLocal = bigContentView.apply(mContext, adaptive, mOnClickHandler);
+                bigContentViewLocal = bigContentView.apply(mContext, adaptive, mOnClickHandler,
+                        themePackageName);
             }
         }
         catch (RuntimeException e) {
