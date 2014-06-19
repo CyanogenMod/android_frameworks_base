068a919b0cbd4d7476859f48f575723bc9bb149b
Clark Scheff <clark@cyngn.com>
2014-06-19 15:55:40 -0700

    Themes: Theme immersive mode cling.
    Change-Id: I5521b93f69e859c40cfd62eb4a367562200074e9

--- policy/src/com/android/internal/policy/impl/ImmersiveModeConfirmation.java
+++ policy/src/com/android/internal/policy/impl/ImmersiveModeConfirmation.java
@@ -23,6 +23,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ThemeUtils;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
@@ -67,6 +68,7 @@ public class ImmersiveModeConfirmation {
     private String mPromptPackage;
     private WindowManager mWindowManager;
     private boolean mStatusBarHidden;
+    private Context mUiContext;
 
     public ImmersiveModeConfirmation(Context context) {
         mContext = context;
@@ -74,6 +76,12 @@ public class ImmersiveModeConfirmation {
         mShowDelayMs = getNavBarExitDuration() * 3;
         mWindowManager = (WindowManager)
                 mContext.getSystemService(Context.WINDOW_SERVICE);
+        ThemeUtils.registerThemeChangeReceiver(context, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mUiContext = null;
+            }
+        });
     }
 
     private long getNavBarExitDuration() {
@@ -139,6 +147,13 @@ public class ImmersiveModeConfirmation {
             mWindowManager.removeView(mClingWindow);
             mClingWindow = null;
         }
+    }
+
+    private Context getUiContext() {
+        if (mUiContext == null) {
+            mUiContext = ThemeUtils.createUiContext(mContext);
+        }
+        return mUiContext != null ? mUiContext : mContext;
     }
 
     public WindowManager.LayoutParams getClingWindowLayoutParams() {
@@ -218,10 +233,10 @@ public class ImmersiveModeConfirmation {
             // create the confirmation cling
             if (mStatusBarHidden) {
                 mClingLayout = (ViewGroup)
-                        View.inflate(getContext(), R.layout.immersive_mode_cling, null);
+                        View.inflate(getUiContext(), R.layout.immersive_mode_cling, null);
             } else {
                 mClingLayout = (ViewGroup)
-                        View.inflate(getContext(), R.layout.immersive_mode_cling_bottom, null);
+                        View.inflate(getUiContext(), R.layout.immersive_mode_cling_bottom, null);
             }
 
             final Button ok = (Button) mClingLayout.findViewById(R.id.ok);
