68a8f56abd9a7533a9a1545f77b03bca12c7c659
Adnan <adnan@cyngn.com>
2014-06-20 10:40:02 -0700

    SystemUI: Expose heads up.
        - TODO: Add do not disturb list in settings
    
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>
    Change-Id: I276cb1c80d392b7dbac28e5f553207415b558481

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -2043,6 +2043,11 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     }
 
     @Override
+    public boolean isImeShowing() {
+        return mInputShown;
+    }
+
+    @Override
     public void showInputMethodPickerFromClient(IInputMethodClient client) {
         if (!calledFromValidUser()) {
             return;
