fd81abe658aab55ca9b2330490ba2f911fa0d0c4
burstlam <burstlam@gmail.com>
2014-06-21 02:26:18 +0800

    SlimRecents : add blacklist support to floating window menu

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentPanelView.java
@@ -20,6 +20,7 @@ package com.android.systemui.slimrecent;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
+import android.app.INotificationManager;
 import android.app.TaskStackBuilder;
 import android.app.admin.DevicePolicyManager;
 import android.content.ActivityNotFoundException;
@@ -37,6 +38,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Process;
+import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
@@ -48,6 +50,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.android.cards.internal.Card;
 import com.android.cards.internal.CardArrayAdapter;
@@ -103,6 +106,7 @@ public class RecentPanelView {
     private final ImageView mEmptyRecentView;
 
     private final RecentController mController;
+    private INotificationManager mNotificationManager;
 
     // Our array adapter holding all cards
     private CardArrayAdapter mCardArrayAdapter;
@@ -298,6 +302,12 @@ public class RecentPanelView {
         final PopupMenu popup = new PopupMenu(layoutContext, selectedView, Gravity.RIGHT);
         mPopup = popup;
 
+        // initialize if null
+        if (mNotificationManager == null) {
+            mNotificationManager = INotificationManager.Stub.asInterface(
+                    ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+        }
+
         // If recent panel is drawn on the right edge we allow the menu
         // if needed to draw over the left container edge.
         popup.setAllowLeftOverdraw(mMainGravity == Gravity.RIGHT);
@@ -346,13 +356,32 @@ public class RecentPanelView {
                 if (item.getItemId() == MENU_APP_DETAILS_ID) {
                     startApplicationDetailsActivity(td.packageName, null, null);
                 } else if (item.getItemId() == MENU_APP_FLOATING_ID) {
-                    Intent intent = new Intent(Intent.ACTION_MAIN);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            | Intent.FLAG_FLOATING_WINDOW);
-                    intent.setComponent(td.intent.getComponent());
-                    mContext.startActivity(intent);
-                    exit();
+                    String currentViewPackage = td.packageName;
+                    boolean allowed = true; // default on
+                    try {
+                        // preloaded apps are added to the blacklist array when is recreated, handled in the notification manager
+                        allowed = mNotificationManager.isPackageAllowedForFloatingMode(currentViewPackage);
+                    } catch (android.os.RemoteException ex) {
+                        // System is dead
+                    }
+                    if (!allowed) {
+                        exit();
+                        String text = mContext.getResources().getString(R.string.floating_mode_blacklisted_app);
+                        int duration = Toast.LENGTH_LONG;
+                        Toast.makeText(mContext, text, duration).show();
+                        return true;
+                    } else {
+                        exit();
+                    }
+                    selectedView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = td.intent;
+                            intent.setFlags(Intent.FLAG_FLOATING_WINDOW
+                                    | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivity(intent);
+                        }
+                    });
                 } else if (item.getItemId() == MENU_APP_STOP_ID) {
                     ActivityManager am = (ActivityManager)mContext.getSystemService(
                             Context.ACTIVITY_SERVICE);
