be9c1791f384dd89b9e5286f9bbd33732ca276d9
d34d <clark@cyngn.com>
2014-06-22 11:49:07 -0700

    Themes: Use INSTALL_LOCATION_INTERNAL_ONLY for legacy icon packs
    If an icon pack is currently applied and is installed using forward
    locking, the system will crash when the pack is updated.  Several
    applications can have the pack attached to their resources and this
    causes the system to crash because it cannot unmount the asec container
    to finish the post install process.  Installing these packs to internal
    storage without forward locking alleviates this issue.
    
    This is done with theme packs as well, including legacy themes.
    
    Change-Id: I155fc0bd671f3f9baaf2565d1c4d654a0a5348dc

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -1241,6 +1241,9 @@ public class PackageParser {
         // Only search the tree when the tag is directly below <manifest>
         final int searchDepth = parser.getDepth() + 1;
 
+        // Search for category and actions inside <intent-filter>
+        final int iconPackSearchDepth = parser.getDepth() + 4;
+
         final List<VerifierInfo> verifiers = new ArrayList<VerifierInfo>();
         boolean isTheme = false;
 
@@ -1272,9 +1275,35 @@ public class PackageParser {
                 isTheme = true;
                 installLocation = PackageInfo.INSTALL_LOCATION_INTERNAL_ONLY;
             }
+
+            if (parser.getDepth() == iconPackSearchDepth && isLegacyIconPack(parser)) {
+                isTheme = true;
+                installLocation = PackageInfo.INSTALL_LOCATION_INTERNAL_ONLY;
+            }
         }
 
         return new PackageLite(pkgName.intern(), versionCode, installLocation, verifiers, isTheme);
+    }
+
+    private static boolean isLegacyIconPack(XmlPullParser parser) {
+        boolean isAction = "action".equals(parser.getName());
+        boolean isCategory = "category".equals(parser.getName());
+        String[] items = isAction ? ThemeUtils.sSupportedActions :
+                (isCategory ? ThemeUtils.sSupportedCategories : null);
+
+        if (items != null) {
+            for (int i = 0; i < parser.getAttributeCount(); i++) {
+                if ("name".equals(parser.getAttributeName(i))) {
+                    final String value = parser.getAttributeValue(i);
+                    for (String item : items) {
+                        if (item.equals(value)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     /**
