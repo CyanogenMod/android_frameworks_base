5b9c276f964d7f5884702c1b51fded6ea967030a
Adnan <adnan@cyngn.com>
2014-06-23 17:28:33 -0700

    HeadsUp: Add heads up blacklist options. (1/2)
    Change-Id: If9e5eed658871400b3c21d9354262e0887bc592e

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -171,6 +171,9 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private RecentsComponent mRecents;
 
+    private ArrayList<String> mDndList;
+    private ArrayList<String> mBlacklist;
+
     private int mExpandedDesktopStyle = 0;
 
     public IStatusBarService getStatusBarService() {
@@ -200,6 +203,12 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         public void observe() {
             ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.HEADS_UP_CUSTOM_VALUES),
+                    false, this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.HEADS_UP_BLACKLIST_VALUES),
+                    false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -225,6 +234,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mExpandedDesktopStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.EXPANDED_DESKTOP_STYLE, 0, UserHandle.USER_CURRENT);
             }
+            final String dndString = Settings.System.getString(mContext.getContentResolver(),
+                    Settings.System.HEADS_UP_CUSTOM_VALUES);
+            final String blackString = Settings.System.getString(mContext.getContentResolver(),
+                    Settings.System.HEADS_UP_BLACKLIST_VALUES);
+            splitAndAddToArrayList(mDndList, dndString, "\\|");
+            splitAndAddToArrayList(mBlacklist, blackString, "\\|");
         }
     };
 
@@ -282,6 +297,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+
+        mDndList = new ArrayList<String>();
+        mBlacklist = new ArrayList<String>();
 
         mProvisioningObserver.onChange(false); // set up
         mContext.getContentResolver().registerContentObserver(
@@ -1182,6 +1200,11 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected boolean shouldInterrupt(StatusBarNotification sbn) {
         Notification notification = sbn.getNotification();
 
+        // check if notification from the package is blacklisted first
+        if (isPackageBlacklisted(sbn.getPackageName())) {
+            return false;
+        }
+
         // some predicates to make the boolean logic legible
         boolean isNoisy = (notification.defaults & Notification.DEFAULT_SOUND) != 0
                 || (notification.defaults & Notification.DEFAULT_VIBRATE) != 0
@@ -1238,22 +1261,23 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private boolean isPackageInDnd(String packageName) {
-        final String baseString = Settings.System.getString(mContext.getContentResolver(),
-                Settings.System.HEADS_UP_CUSTOM_VALUES);
+        return mDndList.contains(packageName);
+    }
 
+    private boolean isPackageBlacklisted(String packageName) {
+        return mBlacklist.contains(packageName);
+    }
+
+    private void splitAndAddToArrayList(ArrayList<String> arrayList,
+                                        String baseString, String separator) {
+        // clear first
+        arrayList.clear();
         if (baseString != null) {
-            final String[] array = TextUtils.split(baseString, "\\|");
+            final String[] array = TextUtils.split(baseString, separator);
             for (String item : array) {
-                if (TextUtils.isEmpty(item)) {
-                    continue;
-                }
-                if (TextUtils.equals(item, packageName)) {
-                    return true;
-                }
+                arrayList.add(item.trim());
             }
         }
-
-        return false;
     }
 
     // Q: What kinds of notifications should show during setup?
