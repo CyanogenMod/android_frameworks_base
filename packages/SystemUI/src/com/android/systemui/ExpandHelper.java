dae680df28c0cdc22f322e970c50ba093dd483d1
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-06-26 21:50:44 +0300

    SystemUI: Improve HeadsUp expanding performance.
    Change-Id: Ifd31caca6d2eb2432772b6a63f6b545f8288f449

--- packages/SystemUI/src/com/android/systemui/ExpandHelper.java
+++ packages/SystemUI/src/com/android/systemui/ExpandHelper.java
@@ -510,7 +510,6 @@ public class ExpandHelper implements Gefingerpoken, OnClickListener {
         }
         mExpanding = true;
         if (DEBUG) Log.d(TAG, "scale type " + expandType + " beginning on view: " + v);
-        mCallback.setUserLockedChild(v, true);
         setView(v);
         setGlow(GLOW_BASE);
         mScaler.setView(v);
@@ -522,11 +521,16 @@ public class ExpandHelper implements Gefingerpoken, OnClickListener {
             if (DEBUG) Log.d(TAG, "working on a non-expandable child");
             mNaturalHeight = mOldHeight;
         }
+        mCallback.setUserLockedChild(v, true);
         if (DEBUG) Log.d(TAG, "got mOldHeight: " + mOldHeight +
                     " mNaturalHeight: " + mNaturalHeight);
         if (v != null && v.getParent() != null) {
             v.getParent().requestDisallowInterceptTouchEvent(true);
         }
+    }
+
+    public float getNaturalHeight() {
+        return mNaturalHeight;
     }
 
     private void finishExpanding(boolean force) {
