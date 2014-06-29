7e8f3fc9d4c6b52d001cea895f13ad55cbb75ee2
Jacob Abrams <satur9nine@gmail.com>
2014-06-28 21:38:18 -0700

    Don't unnecessarily set LayoutParams on children
    Setting LayoutParams on children triggers requestLayout
    which can be expensive and result in significantly lower
    framerate for children layouts that don't need it. When
    requestLayout method is invoked the needToMeasure boolean
    will be true, this is what we'd like to avoid unless
    it is actually needed.
    
    BUG: https://code.google.com/p/android/issues/detail?id=72733
    
    Change-Id: Id5d8f3431b5f943b1279eae41ee43d32a99514fc
    Signed-off-by: Jacob Abrams <satur9nine@gmail.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -2387,7 +2387,9 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
                 lp = (LayoutParams) vlp;
             }
             lp.itemId = mAdapter.getItemId(position);
-            child.setLayoutParams(lp);
+            if (lp != vlp) {
+              child.setLayoutParams(lp);
+            }
         }
 
         if (AccessibilityManager.getInstance(mContext).isEnabled()) {
