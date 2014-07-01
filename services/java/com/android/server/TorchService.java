828d23845fbdfaa6c1cbd2551ffee35eeb43ed45
Roman Birg <roman@cyngn.com>
2014-07-01 15:21:19 -0700

    add framework torch service
    Change-Id: I0c7a055b33fdebe697f23d207b20408e06eb74a8
    Signed-off-by: Roman Birg <roman@cyngn.com>

---
+++ services/java/com/android/server/TorchService.java
@@ -0,0 +1,41 @@
+
+package com.android.server;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.ITorchService;
+import android.util.Log;
+
+public class TorchService extends ITorchService.Stub {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = TorchService.class.getSimpleName();
+
+    private final Context mContext;
+    private boolean mEatTorchLaunch = false;
+
+    public TorchService(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void onCameraOpened() {
+        if (DEBUG) Log.d(TAG, "onCameraOpened()");
+        if (mEatTorchLaunch) {
+            if (DEBUG) Log.d(TAG, "eating torch suppression");
+            mEatTorchLaunch = false;
+        } else {
+            if (DEBUG) Log.d(TAG, "killing torch");
+            Intent i = new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT");
+            i.putExtra("stop", true);
+            mContext.sendOrderedBroadcast(i, null);
+        }
+    }
+
+    @Override
+    public void onStartingTorch() {
+        if (DEBUG) Log.d(TAG, "onStartingTorch()");
+        mEatTorchLaunch = true;
+    }
+
+}
