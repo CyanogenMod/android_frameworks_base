75e7345fcb228f74a83b77b07ef8c34dbc3e6247
Andy Mast <andy@cyngn.com>
2014-07-08 14:56:15 -0700

    Themes: Allow theming navbar bg color
    Change-Id: I256afc30fc018ea752769029c5246bc7310c23a6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -386,6 +386,7 @@ public class NavigationBarView extends LinearLayout {
     public void updateResources(Resources res) {
         mThemedResources = res;
         getIcons(mThemedResources);
+        mBarTransitions.updateResources(res);
         for (int i = 0; i < mRotatedViews.length; i++) {
             ViewGroup container = (ViewGroup) mRotatedViews[i];
             if (container != null) {
