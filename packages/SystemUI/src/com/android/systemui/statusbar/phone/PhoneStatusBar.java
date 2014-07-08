a8712a0196219584e25ae94e02942447d3cdad49
Adnan <adnan@cyngn.com>
2014-07-07 18:14:49 -0700

    SystemUI: Clear disappearing children and remove HeadsUp on recreate.
        - Since some views animate in and out of visibility
        the call to removeAllViews on the mStatusBarContainer may still
        keep a reference to the view.
    
        - Also remove mHeadsUpNotificationView from the window manager if
        it's not null.
    
    Change-Id: I5ced26e0f36362e89779bd7c6dac4d8235e0c0e9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3263,7 +3263,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void recreateStatusBar() {
         mRecreating = true;
+
+        if (mHeadsUpNotificationView != null) {
+            removeHeadsUpView();
+            mHeadsUpNotificationView = null;
+        }
+
         mStatusBarContainer.removeAllViews();
+        mStatusBarContainer.clearDisappearingChildren();
 
         // extract icons from the soon-to-be recreated viewgroup.
         int nIcons = mStatusIcons.getChildCount();
