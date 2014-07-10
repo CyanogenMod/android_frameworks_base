CyanogenMod/android_frameworks_base

commit 8fff544ec349593cfa61ae2f5ec59419fff666b6

Themes: Update LoadedApk resources.
Instead of creating a new LoadedApk when resources are not up to
date, we use the existing LoadedApk but recreate mResources.

Change-Id: Ic1ad810863d03c2cedc751c9e84dd7c954daf3d7

core/java/android/app/ActivityThread.java
@@ -1656,8 +1656,7 @@ public final class ActivityThread {
                 ref = mResourcePackages.get(aInfo.packageName);
             }
             LoadedApk packageInfo = ref != null ? ref.get() : null;
-            if (packageInfo == null || (packageInfo.mResources != null
-                    && !packageInfo.mResources.getAssets().isUpToDate())) {
+            if (packageInfo == null) {
                 if (localLOGV) Slog.v(TAG, (includeCode ? "Loading code package "
                         : "Loading resource-only package ") + aInfo.packageName
                         + " (in " + (mBoundApplication != null
@@ -1674,6 +1673,11 @@ public final class ActivityThread {
                     mResourcePackages.put(aInfo.packageName,
                             new WeakReference<LoadedApk>(packageInfo));
                 }
+            }
+            if (packageInfo.mResources == null
+                    || !packageInfo.mResources.getAssets().isUpToDate()) {
+                packageInfo.mResources = null;
+                packageInfo.getResources(this);
             }
             return packageInfo;
         }

commit d48374a2c820541a6bd58b0603242b8005b70b97

Restore lazy loading of LoadedApk resources
This was broken in 8fff544ec349593cfa61ae2f5ec59419fff666b6. Since that change,
resources are always initialized in getPackageInfo(), even if they haven't been
loaded before (mResources == null).

It shouldn't be necessary in *any* case to preload the resources at this point.
Anything using the resources should call LoadedApk.getResources(), which will
initialize them just when needed. Setting mResources to null resets the cache
and will force a reload the next time the resources are requested.

Change-Id: I9bc9986297478e99752b1e2b9bd04dab3dcc9656

core/java/android/app/ActivityThread.java
@@ -1673,10 +1673,9 @@ public final class ActivityThread {
                             new WeakReference<LoadedApk>(packageInfo));
                 }
             }
-            if (packageInfo.mResources == null
-                    || !packageInfo.mResources.getAssets().isUpToDate()) {
+            if (packageInfo.mResources != null
+                    && !packageInfo.mResources.getAssets().isUpToDate()) {
                 packageInfo.mResources = null;
-                packageInfo.getResources(this);
             }
             return packageInfo;
         }