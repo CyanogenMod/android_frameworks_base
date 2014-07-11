ac95737a81e9d6bc09a7cfa52c0cb58daff48120
Evisceration <eviscerationls@gmail.com>
2014-07-12 00:31:25 +0200

    please don't die
    Change-Id: Id5d1ec1c1646fd72655aa290823fac174a79c76c

--- packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
@@ -428,6 +428,8 @@ public class NotificationHostView extends FrameLayout {
 
         RemoteViews rv = bigContentView ? sbn.getNotification().bigContentView : sbn.getNotification().contentView;
 
+        if (rv == null) return;
+
         final View remoteView = rv.apply(mContext, null);
         remoteView.setLayoutParams(new LayoutParams(mDynamicWidth ? LayoutParams.WRAP_CONTENT : LayoutParams.MATCH_PARENT,
                     LayoutParams.WRAP_CONTENT));
