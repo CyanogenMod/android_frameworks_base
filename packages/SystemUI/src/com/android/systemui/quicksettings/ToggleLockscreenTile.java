6f7cdff421d9ba6c549789da42d6c84c4cf2e0cd
Danesh M <daneshm90@gmail.com>
2014-07-10 19:05:33 -0700

    QuickSettings : Properly restore lockscreen tile state
    The keyguard lock relies on device policy manager service to be active,
    currently it fails because it tries to request before its up. Fix that
    by registering the request in a receiver
    
    Change-Id: I15e0d980ecf21c8316f4a771e93f4b520c556115

--- packages/SystemUI/src/com/android/systemui/quicksettings/ToggleLockscreenTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/ToggleLockscreenTile.java
@@ -1,104 +1,55 @@
 package com.android.systemui.quicksettings;
 
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
-import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 
-@SuppressWarnings("deprecation")
-public class ToggleLockscreenTile extends QuickSettingsTile
-        implements OnSharedPreferenceChangeListener {
+public class ToggleLockscreenTile extends QuickSettingsTile implements
+    LockscreenStateChanger.LockStateChangeListener {
 
-    private static final String KEY_DISABLED = "lockscreen_disabled";
-
-    private static KeyguardLock sLock = null;
-    private static int sLockTileCount = 0;
-    private static boolean sDisabledLockscreen = false;
+    private LockscreenStateChanger mLockscreenChanger;
 
     public ToggleLockscreenTile(Context context, QuickSettingsController qsc) {
         super(context, qsc);
 
+        mLockscreenChanger = LockscreenStateChanger.getInstance(context);
+        mLockscreenChanger.addListener(this);
         mOnClick = new OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                sDisabledLockscreen = !sDisabledLockscreen;
-                mPrefs.edit().putBoolean(KEY_DISABLED, sDisabledLockscreen).apply();
-                updateLockscreenState();
+                mLockscreenChanger.toggleState();
             }
         };
-
         mOnLongClick = new OnLongClickListener() {
-
             @Override
             public boolean onLongClick(View v) {
                 startSettingsActivity("android.settings.SECURITY_SETTINGS");
                 return true;
             }
         };
-    }
-
-    @Override
-    void onPostCreate() {
-        mPrefs.registerOnSharedPreferenceChangeListener(this);
-        if (sLockTileCount == 0) {
-            sDisabledLockscreen = mPrefs.getBoolean(KEY_DISABLED, false);
-            updateLockscreenState();
-        }
-        sLockTileCount++;
-        updateTile();
-        super.onPostCreate();
+        updateResources();
     }
 
     @Override
     public void onDestroy() {
-        mPrefs.unregisterOnSharedPreferenceChangeListener(this);
-        sLockTileCount--;
-        if (sLock != null && sLockTileCount < 1 && sDisabledLockscreen) {
-            sLock.reenableKeyguard();
-            sLock = null;
-        }
+        mLockscreenChanger.removeListener(this);
         super.onDestroy();
     }
 
     @Override
     public void updateResources() {
-        updateTile();
+        mLabel = mContext.getString(R.string.quick_settings_lockscreen);
+        mDrawable = mLockscreenChanger.isDisabled() ?
+                R.drawable.ic_qs_lock_screen_on : R.drawable.ic_qs_lock_screen_off;
         super.updateResources();
     }
 
-    private synchronized void updateTile() {
-        mLabel = mContext.getString(R.string.quick_settings_lockscreen);
-        mDrawable = sDisabledLockscreen ?
-                R.drawable.ic_qs_lock_screen_off : R.drawable.ic_qs_lock_screen_on;
-    }
-
-    private void updateLockscreenState() {
-        if (sLock == null) {
-            KeyguardManager keyguardManager = (KeyguardManager)
-                    mContext.getApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
-            sLock = keyguardManager.newKeyguardLock("LockscreenTile");
-        }
-        if (sDisabledLockscreen) {
-            sLock.disableKeyguard();
-        } else {
-            sLock.reenableKeyguard();
-        }
-    }
-
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (KEY_DISABLED.equals(key)) {
-            updateResources();
-        }
+    public void onLockStateChange(boolean enabled) {
+        updateResources();
     }
 }
