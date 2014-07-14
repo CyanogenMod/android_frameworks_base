5bdb607bd9f5418720ddd01b461d98618b604b2b
burstlam <burstlam@gmail.com>
2014-07-14 21:38:16 +0800

    Revert "Fix crash when clicking a file from recents or downloads"
    This reverts commit 61dfa05a7b7b8c44925d852cb2ab1de7d0ea34c2.

--- packages/DocumentsUI/src/com/android/documentsui/DocumentsActivity.java
+++ packages/DocumentsUI/src/com/android/documentsui/DocumentsActivity.java
@@ -1088,14 +1088,11 @@ public class DocumentsActivity extends Activity {
                 startActivity(view);
             } catch (ActivityNotFoundException ex2) {
                 File file = null;
-                int idx = doc.documentId.indexOf(":");
-                if (idx >= 0) {
-                    String id = doc.documentId.substring(0, idx);
-                    File volume = mIdToPath.get(id);
-                    if (volume != null) {
-                        String fileName = doc.documentId.substring(idx + 1);
-                        file = new File(volume, fileName);
-                    }
+                String id = doc.documentId.substring(0, doc.documentId.indexOf(":"));
+                File volume = mIdToPath.get(id);
+                if (volume != null) {
+                    String fileName = doc.documentId.substring(doc.documentId.indexOf(":") + 1);
+                    file = new File(volume, fileName);
                 }
                 if (file != null) {
                     view.setDataAndType(Uri.fromFile(file), doc.mimeType);
