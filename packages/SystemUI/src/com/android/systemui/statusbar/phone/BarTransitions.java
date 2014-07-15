38fb4b14b14d2b1d7e74e99890e513022c321d8c
Danny Baumann <dannybaumann@web.de>
2014-07-15 13:20:45 +0200

    Allow translucent navigation bar in landscape mode.
    Change-Id: I5388f3f8a34533624619714bacfba52f95fc6d85

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
@@ -66,6 +66,11 @@ public class BarTransitions {
         }
     }
 
+    protected void setGradientResourceId(int gradientResourceId) {
+        mBarBackground.setGradientResourceId(mView.getContext().getResources(),
+                gradientResourceId);
+    }
+
     public void updateResources(Resources res) {
         mBarBackground.updateResources(res);
     }
@@ -122,7 +127,7 @@ public class BarTransitions {
     }
 
     private static class BarBackgroundDrawable extends Drawable {
-        private final int mGradientResourceId;
+        private int mGradientResourceId;
         private final int mOpaqueColorResourceId;
         private final int mSemiTransparentColorResourceId;
         private final TimeInterpolator mInterpolator;
@@ -156,6 +161,11 @@ public class BarTransitions {
             mGradientResourceId = gradientResourceId;
             mOpaqueColorResourceId = opaqueColorResourceId;
             mSemiTransparentColorResourceId = semiTransparentColorResourceId;
+        }
+
+        public void setGradientResourceId(Resources res, int gradientResourceId) {
+            mGradient = res.getDrawable(gradientResourceId);
+            mGradientResourceId = gradientResourceId;
         }
 
         public void updateResources(Resources res)  {
