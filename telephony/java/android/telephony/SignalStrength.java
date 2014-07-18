34f1c683ebb7afaad309f830964d673f57ac1c41
dianlujitao <dianlujitao@gmail.com>
2014-07-18 09:24:38 +0800

    Fix cdma SignalStrength display
    Change-Id: I6dd5d95e52282b1686858134d89c09705bda435b

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -532,8 +532,8 @@ public class SignalStrength implements Parcelable {
                 /* We don't know cdma, use evdo */
                 level = evdoLevel;
             } else {
-                /* We know both, use the lowest level */
-                level = cdmaLevel < evdoLevel ? cdmaLevel : evdoLevel;
+                /* We know both, use the highest level */
+                level = cdmaLevel > evdoLevel ? cdmaLevel : evdoLevel;
             }
         }
         if (DBG) log("getLevel=" + level);
@@ -570,8 +570,8 @@ public class SignalStrength implements Parcelable {
                 /* We don't know cdma use, evdo */
                 asuLevel = evdoAsuLevel;
             } else {
-                /* We know both, use the lowest level */
-                asuLevel = cdmaAsuLevel < evdoAsuLevel ? cdmaAsuLevel : evdoAsuLevel;
+                /* We know both, use the highest level */
+                asuLevel = cdmaAsuLevel > evdoAsuLevel ? cdmaAsuLevel : evdoAsuLevel;
             }
         }
         if (DBG) log("getAsuLevel=" + asuLevel);
@@ -603,8 +603,7 @@ public class SignalStrength implements Parcelable {
             int cdmaDbm = getCdmaDbm();
             int evdoDbm = getEvdoDbm();
 
-            return (evdoDbm == -120) ? cdmaDbm : ((cdmaDbm == -120) ? evdoDbm
-                    : (cdmaDbm < evdoDbm ? cdmaDbm : evdoDbm));
+            return cdmaDbm;
         }
         if (DBG) log("getDbm=" + dBm);
         return dBm;
@@ -677,10 +676,10 @@ public class SignalStrength implements Parcelable {
         int levelDbm;
         int levelEcio;
 
-        if (cdmaDbm >= -75) levelDbm = SIGNAL_STRENGTH_GREAT;
-        else if (cdmaDbm >= -85) levelDbm = SIGNAL_STRENGTH_GOOD;
-        else if (cdmaDbm >= -95) levelDbm = SIGNAL_STRENGTH_MODERATE;
-        else if (cdmaDbm >= -100) levelDbm = SIGNAL_STRENGTH_POOR;
+        if (cdmaDbm >= -85) levelDbm = SIGNAL_STRENGTH_GREAT;
+        else if (cdmaDbm >= -95) levelDbm = SIGNAL_STRENGTH_GOOD;
+        else if (cdmaDbm >= -105) levelDbm = SIGNAL_STRENGTH_MODERATE;
+        else if (cdmaDbm >= -110) levelDbm = SIGNAL_STRENGTH_POOR;
         else levelDbm = SIGNAL_STRENGTH_NONE_OR_UNKNOWN;
 
         // Ec/Io are in dB*10
@@ -690,7 +689,7 @@ public class SignalStrength implements Parcelable {
         else if (cdmaEcio >= -150) levelEcio = SIGNAL_STRENGTH_POOR;
         else levelEcio = SIGNAL_STRENGTH_NONE_OR_UNKNOWN;
 
-        int level = (levelDbm < levelEcio) ? levelDbm : levelEcio;
+        int level = levelDbm;
         if (DBG) log("getCdmaLevel=" + level);
         return level;
     }
@@ -737,10 +736,10 @@ public class SignalStrength implements Parcelable {
         int levelEvdoDbm;
         int levelEvdoSnr;
 
-        if (evdoDbm >= -65) levelEvdoDbm = SIGNAL_STRENGTH_GREAT;
-        else if (evdoDbm >= -75) levelEvdoDbm = SIGNAL_STRENGTH_GOOD;
-        else if (evdoDbm >= -90) levelEvdoDbm = SIGNAL_STRENGTH_MODERATE;
-        else if (evdoDbm >= -105) levelEvdoDbm = SIGNAL_STRENGTH_POOR;
+        if (evdoDbm >= -85) levelEvdoDbm = SIGNAL_STRENGTH_GREAT;
+        else if (evdoDbm >= -95) levelEvdoDbm = SIGNAL_STRENGTH_GOOD;
+        else if (evdoDbm >= -105) levelEvdoDbm = SIGNAL_STRENGTH_MODERATE;
+        else if (evdoDbm >= -110) levelEvdoDbm = SIGNAL_STRENGTH_POOR;
         else levelEvdoDbm = SIGNAL_STRENGTH_NONE_OR_UNKNOWN;
 
         if (evdoSnr >= 7) levelEvdoSnr = SIGNAL_STRENGTH_GREAT;
@@ -749,7 +748,7 @@ public class SignalStrength implements Parcelable {
         else if (evdoSnr >= 1) levelEvdoSnr = SIGNAL_STRENGTH_POOR;
         else levelEvdoSnr = SIGNAL_STRENGTH_NONE_OR_UNKNOWN;
 
-        int level = (levelEvdoDbm < levelEvdoSnr) ? levelEvdoDbm : levelEvdoSnr;
+        int level = levelEvdoDbm;
         if (DBG) log("getEvdoLevel=" + level);
         return level;
     }
