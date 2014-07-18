f06f688b4a7649cc4decbde9e6ff70dcb6f284af
henry.uh_chen <henry.uh_chen@htc.com>
2014-07-18 15:11:18 +0800

    [View] Clear Hardware-Layers before system destroys HardwareRenderer​.
    Symptom: Sometimes HardwareLayer display is corrupted.
    
    Root Cause: In some cases, ViewRootImpl destroys HardwareRenderer​ without
    clearing HardwareLayers in the view hirarchy. If the system trims memory fully
    and kill the glContext at the same time, HardwareLayer may corrupt.
    
    Solution: Clear Hardware-Layers before system destroys HardwareRenderer​.
    
    Reproduce steps: Modify the cache's trim memory mechanism to make it easier to
    trim fully, HardwareLayers implemented in app will easily be corrupted.
    Modification of the cache is not required but make it easier to reproduce.
    
    Change-Id: If57c802cdacf8dc37cbbcc199e74f9482834f981

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -1568,6 +1568,9 @@ public final class ViewRootImpl implements ViewParent,
                     // Our surface is gone
                     if (mAttachInfo.mHardwareRenderer != null &&
                             mAttachInfo.mHardwareRenderer.isEnabled()) {
+                        // Destroy hardware layers before hardware renderer is destroyed
+                        mAttachInfo.mHardwareRenderer.destroyHardwareResources(mView);
+
                         mAttachInfo.mHardwareRenderer.destroy(true);
                     }
                 } else if (surfaceGenerationId != mSurface.getGenerationId() &&
