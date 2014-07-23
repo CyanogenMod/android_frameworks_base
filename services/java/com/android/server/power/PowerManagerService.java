dbf8f1b8047902b707310f01ac4814e553268a67
Michael Bestas <mikeioannina@gmail.com>
2014-07-23 12:14:34 +0000

    Revert "QuickBoot: don't wake up screen when usb connected"
    Duplicated code
    
    This reverts commit 9cf2a41749b0f9e732a14c62ac60e9bcf9e48a27.
    
    Change-Id: Ib2274d4147115275fb0d947ee5a78646cc1f901c

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -1487,10 +1487,6 @@ public final class PowerManagerService extends IPowerManager.Stub
             return false;
         }
 
-       if (SystemProperties.getInt("sys.quickboot.enable", 0) == 1) {
-            return false;
-        }
-
         // Don't wake when undocked from wireless charger.
         // See WirelessChargerDetector for justification.
         if (wasPowered && !mIsPowered
