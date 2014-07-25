9035415dfd33984294f2412934a7a12cb9fa6f61
Valter Strods <valters.strods@gmail.com>
2014-07-25 20:43:40 +0300

    SystemUI: Separate system bar BGs for themes
    This commit separates the navigation bar background colors from the
    status bar background colors which allows more modular theme-ability.
    Backwards compatibility for the old system bar values is still
    preserved, but the new values are to be prioritized.
    
    Thanks to Arz Bhatia for the suggestion.
    
    Change-Id: Ibe579d16ea35ca5bdcfe9f98143c3f91cd24900f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
@@ -55,10 +55,12 @@ public class BarTransitions {
 
     private int mMode;
 
-    public BarTransitions(View view, int gradientResourceId) {
+    public BarTransitions(View view, int gradientResourceId, int opaqueColorResourceId,
+            int semiTransparentColorResourceId) {
         mTag = "BarTransitions." + view.getClass().getSimpleName();
         mView = view;
-        mBarBackground = new BarBackgroundDrawable(mView.getContext(), gradientResourceId);
+        mBarBackground = new BarBackgroundDrawable(mView.getContext(), gradientResourceId,
+                opaqueColorResourceId, semiTransparentColorResourceId);
         if (HIGH_END) {
             mView.setBackground(mBarBackground);
         }
@@ -121,6 +123,8 @@ public class BarTransitions {
 
     private static class BarBackgroundDrawable extends Drawable {
         private final int mGradientResourceId;
+        private final int mOpaqueColorResourceId;
+        private final int mSemiTransparentColorResourceId;
         private final TimeInterpolator mInterpolator;
 
         private int mOpaque;
@@ -137,23 +141,26 @@ public class BarTransitions {
         private int mGradientAlphaStart;
         private int mColorStart;
 
-        public BarBackgroundDrawable(Context context, int gradientResourceId) {
+        public BarBackgroundDrawable(Context context, int gradientResourceId,
+                int opaqueColorResourceId, int semiTransparentColorResourceId) {
             final Resources res = context.getResources();
             if (DEBUG_COLORS) {
                 mOpaque = 0xff0000ff;
                 mSemiTransparent = 0x7f0000ff;
             } else {
-                mOpaque = res.getColor(R.color.system_bar_background_opaque);
-                mSemiTransparent = res.getColor(R.color.system_bar_background_semi_transparent);
+                mOpaque = res.getColor(opaqueColorResourceId);
+                mSemiTransparent = res.getColor(semiTransparentColorResourceId);
             }
             mGradient = res.getDrawable(gradientResourceId);
             mInterpolator = new LinearInterpolator();
             mGradientResourceId = gradientResourceId;
+            mOpaqueColorResourceId = opaqueColorResourceId;
+            mSemiTransparentColorResourceId = semiTransparentColorResourceId;
         }
 
         public void updateResources(Resources res)  {
-            mOpaque = res.getColor(R.color.system_bar_background_opaque);
-            mSemiTransparent = res.getColor(R.color.system_bar_background_semi_transparent);
+            mOpaque = res.getColor(mOpaqueColorResourceId);
+            mSemiTransparent = res.getColor(mSemiTransparentColorResourceId);
             // Retrieve the current bounds for mGradient so they can be set to
             // the new drawable being loaded, otherwise the bounds will be (0, 0, 0, 0)
             // and the gradient will not be drawn.
