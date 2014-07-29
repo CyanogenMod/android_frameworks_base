e27949864743aed3857944ce47257b37bde55c0a
Shashank Mittal <mittals@codeaurora.org>
2014-07-28 21:01:09 -0700

    StatusBar: Add support to show data and wifi activities.
    -- Show data activity icons.
    -- Show wifi activity icons.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
    
    Change-Id: Iee5038b007e91deeb2d3cb838ce3908818aefc91

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2011 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,9 +46,9 @@ public class SignalClusterView
 
     private boolean mVpnVisible = false;
     private boolean mWifiVisible = false;
-    private int mWifiStrengthId = 0;
+    private int mWifiStrengthId = 0, mWifiActivityId = 0;
     private boolean mMobileVisible = false;
-    private int mMobileStrengthId = 0, mMobileTypeId = 0;
+    private int mMobileStrengthId = 0, mMobileTypeId = 0, mMobileActivityId = 0;
     private int mNoSimIconId = 0;
     private boolean mIsAirplaneMode = false;
     private int mAirplaneIconId = 0;
@@ -55,6 +58,7 @@ public class SignalClusterView
 
     ViewGroup mWifiGroup, mMobileGroup;
     ImageView mVpn, mWifi, mMobile, mMobileType, mAirplane, mNoSimSlot;
+    ImageView mWifiActivity, mMobileActivity;
     View mWifiAirplaneSpacer;
     View mWifiSignalSpacer;
 
@@ -98,8 +102,10 @@ public class SignalClusterView
         mVpn            = (ImageView) findViewById(R.id.vpn);
         mWifiGroup      = (ViewGroup) findViewById(R.id.wifi_combo);
         mWifi           = (ImageView) findViewById(R.id.wifi_signal);
+        mWifiActivity   = (ImageView) findViewById(R.id.wifi_inout);
         mMobileGroup    = (ViewGroup) findViewById(R.id.mobile_combo);
         mMobile         = (ImageView) findViewById(R.id.mobile_signal);
+        mMobileActivity = (ImageView) findViewById(R.id.mobile_inout);
         mMobileType     = (ImageView) findViewById(R.id.mobile_type);
         mNoSimSlot      = (ImageView) findViewById(R.id.no_sim);
         mAirplane       = (ImageView) findViewById(R.id.airplane);
@@ -114,8 +120,10 @@ public class SignalClusterView
         mVpn            = null;
         mWifiGroup      = null;
         mWifi           = null;
+        mWifiActivity   = null;
         mMobileGroup    = null;
         mMobile         = null;
+        mMobileActivity = null;
         mMobileType     = null;
         mNoSimSlot      = null;
         mAirplane       = null;
@@ -136,20 +144,24 @@ public class SignalClusterView
     }
 
     @Override
-    public void setWifiIndicators(boolean visible, int strengthIcon, String contentDescription) {
+    public void setWifiIndicators(boolean visible, int strengthIcon, int activityIcon,
+            String contentDescription) {
         mWifiVisible = visible;
         mWifiStrengthId = strengthIcon;
+        mWifiActivityId = activityIcon;
         mWifiDescription = contentDescription;
 
         apply();
     }
 
     @Override
-    public void setMobileDataIndicators(boolean visible, int strengthIcon, int typeIcon,
-            String contentDescription, String typeContentDescription, boolean roaming,
+    public void setMobileDataIndicators(boolean visible, int strengthIcon,
+            int activityIcon, int typeIcon, String contentDescription,
+            String typeContentDescription, boolean roaming,
             boolean isTypeIconWide, int noSimIcon) {
         mMobileVisible = visible;
         mMobileStrengthId = strengthIcon;
+        mMobileActivityId = activityIcon;
         mMobileTypeId = typeIcon;
         mMobileDescription = contentDescription;
         mMobileTypeDescription = typeContentDescription;
@@ -187,8 +199,16 @@ public class SignalClusterView
             mWifi.setImageDrawable(null);
         }
 
+        if (mWifiActivity != null) {
+            mWifiActivity.setImageDrawable(null);
+        }
+
         if (mMobile != null) {
             mMobile.setImageDrawable(null);
+        }
+
+        if (mMobileActivity != null) {
+            mMobileActivity.setImageDrawable(null);
         }
 
         if (mMobileType != null) {
@@ -215,6 +235,7 @@ public class SignalClusterView
         if (DEBUG) Log.d(TAG, String.format("vpn: %s", mVpnVisible ? "VISIBLE" : "GONE"));
         if (mWifiVisible) {
             mWifi.setImageResource(mWifiStrengthId);
+            mWifiActivity.setImageResource(mWifiActivityId);
             mWifiGroup.setContentDescription(mWifiDescription);
             mWifiGroup.setVisibility(View.VISIBLE);
         } else {
@@ -222,12 +243,13 @@ public class SignalClusterView
         }
 
         if (DEBUG) Log.d(TAG,
-                String.format("wifi: %s sig=%d",
+                String.format("wifi: %s sig=%d act=%d",
                     (mWifiVisible ? "VISIBLE" : "GONE"),
-                    mWifiStrengthId));
+                    mWifiStrengthId, mWifiActivityId));
 
         if (mMobileVisible && !mIsAirplaneMode) {
             mMobile.setImageResource(mMobileStrengthId);
+            mMobileActivity.setImageResource(mMobileActivityId);
             mMobileType.setImageResource(mMobileTypeId);
             mMobileGroup.setContentDescription(mMobileTypeDescription + " " + mMobileDescription);
             mMobileGroup.setVisibility(View.VISIBLE);
@@ -258,9 +280,9 @@ public class SignalClusterView
         mMobile.setPaddingRelative(mIsMobileTypeIconWide ? mWideTypeIconStartPadding : 0, 0, 0, 0);
 
         if (DEBUG) Log.d(TAG,
-                String.format("mobile: %s sig=%d typ=%d",
+                String.format("mobile: %s sig=%d act=%d typ=%d",
                     (mMobileVisible ? "VISIBLE" : "GONE"),
-                    mMobileStrengthId, mMobileTypeId));
+                    mMobileStrengthId,  mMobileActivityId, mMobileTypeId));
 
         mMobileType.setVisibility((mRoaming || mMobileTypeId != 0) ? View.VISIBLE : View.GONE);
     }
