a7404e861ac1dac6279a39f7c814fbc86963d8a2
Mao Jinlong <c_jmao@codeaurora.org>
2014-07-29 15:42:13 +0800

    Alarm: Add one more RTC alarm type for poweroff alarm.
    Now we use type of RTC_WAKEUP for the poweroff alarm.
    Actually lots of other applications also use this type.
    But these applications never expect the device will
    power on after it has been shut down. So we need to
    add dedicated alarm type for the Deskclock or similar
    applications.
    
    Change-Id: Ib515d8a737ab73cdd809597ad8cde57030c0a0a1

--- core/java/android/app/AlarmManager.java
+++ core/java/android/app/AlarmManager.java
@@ -101,6 +101,13 @@ public class AlarmManager
     public static final int ELAPSED_REALTIME = 3;
 
     /**
+     * Alarm time in {@link System#currentTimeMillis System.currentTimeMillis()}
+     * (wall clock time in UTC), which will wake up the device when
+     * it goes off. And it will power on the devices when it shuts down.
+     */
+    public static final int RTC_POWEROFF_WAKEUP = 4;
+
+    /**
      * Broadcast Action: Sent after the value returned by
      * {@link #getNextAlarmClock()} has changed.
      *
