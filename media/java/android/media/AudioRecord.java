f363924905c667fcac3db96b9303c52942e217a8
Shashank Mittal <mittals@codeaurora.org>
2014-07-29 20:22:45 -0700

    AppOps: Check user permission before audio recording.
    Add permission check before audio recording.
    
    Change-Id: I55c8397df3169e031bba951cf180fa8c5f4c4ce0
    Conflicts:
    media/java/android/media/AudioRecord.java

--- media/java/android/media/AudioRecord.java
+++ media/java/android/media/AudioRecord.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2008 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,6 +24,8 @@ import java.nio.ByteBuffer;
 import java.util.Iterator;
 
 import android.os.Binder;
+import android.app.ActivityThread;
+import android.app.AppOpsManager;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -610,6 +615,10 @@ public class AudioRecord
         return mSessionId;
     }
 
+    private boolean isAudioRecordAllowed() {
+        String packageName = ActivityThread.currentPackageName();
+        return native_check_permission(packageName) == AppOpsManager.MODE_ALLOWED;
+    }
     //---------------------------------------------------------
     // Transport control methods
     //--------------------
@@ -619,6 +628,10 @@ public class AudioRecord
      */
     public void startRecording()
     throws IllegalStateException {
+        if (!isAudioRecordAllowed()) {
+            Log.e(TAG, "User permission denied!");
+            return;
+        }
         if (mState != STATE_INITIALIZED) {
             throw new IllegalStateException("startRecording() called on an "
                     + "uninitialized AudioRecord.");
@@ -642,6 +655,10 @@ public class AudioRecord
      */
     public void startRecording(MediaSyncEvent syncEvent)
     throws IllegalStateException {
+        if (!isAudioRecordAllowed()) {
+            Log.e(TAG, "User permission denied!");
+            return;
+        }
         if (mState != STATE_INITIALIZED) {
             throw new IllegalStateException("startRecording() called on an "
                     + "uninitialized AudioRecord.");
@@ -962,6 +979,8 @@ public class AudioRecord
 
     static private native final int native_get_min_buff_size(
             int sampleRateInHz, int channelCount, int audioFormat);
+
+    private native final int native_check_permission(String packageName);
 
 
     //---------------------------------------------------------
