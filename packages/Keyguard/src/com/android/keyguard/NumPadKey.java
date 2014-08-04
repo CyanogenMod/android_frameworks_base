1ef046f09658f632a461e31dd90b1e34e7fb3ea3
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2).
    Change-Id: Ibe8611eb0b8a5bd9275bc96682d2cbafaa37d8f5
    
    Add global scramble pin layout setting.
    
    Change-Id: I87e6baf041644cdde55fef34f4032c6950ac428f
    
    Keyguard: Fix bad merge from 12.0 on pin scramble.
    
    Change-Id: Ia9ee6bd43c45228ec7042ef8300ff9ca6f92f60f

--- packages/Keyguard/src/com/android/keyguard/NumPadKey.java
+++ packages/Keyguard/src/com/android/keyguard/NumPadKey.java
@@ -73,7 +73,6 @@ public class NumPadKey extends ViewGroup {
     public NumPadKey(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         setFocusable(true);
-
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NumPadKey);
 
         try {
@@ -88,7 +87,6 @@ public class NumPadKey extends ViewGroup {
         setAccessibilityDelegate(new ObscureSpeechDelegate(context));
 
         mEnableHaptics = new LockPatternUtils(context).isTactileFeedbackEnabled();
-
         mPM = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
@@ -98,7 +96,27 @@ public class NumPadKey extends ViewGroup {
         mDigitText.setText(Integer.toString(mDigit));
         mKlondikeText = (TextView) findViewById(R.id.klondike_text);
 
+        updateText();
+        setBackground(mContext.getDrawable(R.drawable.ripple_drawable));
+        setContentDescription(mDigitText.getText().toString());
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        // Reset the "announced headset" flag when detached.
+        ObscureSpeechDelegate.sAnnouncedHeadset = false;
+    }
+
+    public void setDigit(int digit) {
+        mDigit = digit;
+        updateText();
+    }
+
+    private void updateText() {
         if (mDigit >= 0) {
+            mDigitText.setText(Integer.toString(mDigit));
             if (sKlondike == null) {
                 sKlondike = getResources().getStringArray(R.array.lockscreen_num_pad_klondike);
             }
@@ -112,18 +130,8 @@ public class NumPadKey extends ViewGroup {
                 }
             }
         }
-
-        setBackground(mContext.getDrawable(R.drawable.ripple_drawable));
-        setContentDescription(mDigitText.getText().toString());
     }
 
-    @Override
-    public void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-
-        // Reset the "announced headset" flag when detached.
-        ObscureSpeechDelegate.sAnnouncedHeadset = false;
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
