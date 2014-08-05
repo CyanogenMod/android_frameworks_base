d8cd9872a143f9d1de9673009a9abf3ef72d4a24
Shareef Ali <shareefalis@gmail.com>
2014-08-05 00:04:47 -0400

    Telephony: fix tdsdma asu invalid value
    The magic value for invalid asu is "99", not "255". This error is causing the value validity checks to fail incorrectly, fixit.
    
    Change-Id: Ief816eb577ccf371b3c85df1fe9515eb0bfb3f06

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -970,7 +970,7 @@ public class SignalStrength implements Parcelable {
         final int tdScdmaDbm = getTdScdmaDbm();
         int tdScdmaAsuLevel;
 
-        if (tdScdmaDbm == INVALID) tdScdmaAsuLevel = 255;
+        if (tdScdmaDbm == INVALID) tdScdmaAsuLevel = 99;
         else tdScdmaAsuLevel = tdScdmaDbm + 120;
         if (DBG) log("TD-SCDMA Asu level: " + tdScdmaAsuLevel);
         return tdScdmaAsuLevel;
