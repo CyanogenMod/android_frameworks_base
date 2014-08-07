a2a06d10ff04ad7a1c96e98d432abedf5a1931e9
Joe Wooster <jeebobs@btinternet.com>
2014-08-07 13:39:39 +0100

    SystemUI: Always show translucent decor on recents panel
    Change-Id: I1c4915b84730f9bb90ee4ae4e7b29a69e2769f2f

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -178,8 +178,8 @@ public class RecentsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().addPrivateFlags(
-                WindowManager.LayoutParams.PRIVATE_FLAG_INHERIT_TRANSLUCENT_DECOR);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+                        | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         setContentView(R.layout.status_bar_recent_panel);
         mRecentsPanel = (RecentsPanelView) findViewById(R.id.recents_root);
         mRecentsPanel.setOnTouchListener(new TouchOutsideListener(mRecentsPanel));
