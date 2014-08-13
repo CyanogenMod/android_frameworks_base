a46a30e8df96ccf83209d4d7221247f5860ccab5
Roman Birg <roman@cyngn.com>
2014-08-13 15:47:21 -0700

    SystemUI: make long pressing recent switch to last app
    With this patch, long pressing the recents key on the navigation bar
    will attempt to switch to your last application in the activity stack
    before the active one.
    
    The user's current home launcher package is ignored as well as SystemUI (so that
    RecentsActivity does not influence the behavior).
    
    Change-Id: I38771e0fce16b55bb5186af47115b4e812cb60b0
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -33,6 +33,7 @@ import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
+import android.app.ActivityOptions;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.StatusBarManager;
@@ -42,6 +43,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.content.res.ThemeConfig;
 import android.content.res.Resources;
@@ -125,6 +127,7 @@ import com.android.systemui.statusbar.policy.OnSizeChangedListener;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         NetworkController.UpdateUIListener {
@@ -1092,6 +1095,50 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     };
 
+    private View.OnLongClickListener mRecentsLongPressListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            cancelPreloadingRecentTasksList();
+
+            final ActivityManager am =
+                    (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+            ActivityManager.RunningTaskInfo lastTask = getLastTask(am);
+
+            if (lastTask != null) {
+                if (DEBUG) Log.d(TAG, "switching to " + lastTask.topActivity.getPackageName());
+                final ActivityOptions opts = ActivityOptions.makeCustomAnimation(mContext,
+                        R.anim.last_app_in, R.anim.last_app_out);
+                am.moveTaskToFront(lastTask.id, ActivityManager.MOVE_TASK_NO_USER_ACTION,
+                        opts.toBundle());
+                return true;
+            }
+            return false;
+        }
+
+        private ActivityManager.RunningTaskInfo getLastTask(final ActivityManager am) {
+            final String defaultHomePackage = resolveCurrentLauncherPackage();
+            List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(5);
+
+            for (int i = 1; i < tasks.size(); i++) {
+                String packageName = tasks.get(i).topActivity.getPackageName();
+                if (!packageName.equals(defaultHomePackage)
+                        && !packageName.equals(mContext.getPackageName())) {
+                    return tasks.get(i);
+                }
+            }
+
+            return null;
+        }
+
+        private String resolveCurrentLauncherPackage() {
+            final Intent launcherIntent = new Intent(Intent.ACTION_MAIN)
+                    .addCategory(Intent.CATEGORY_HOME);
+            final PackageManager pm = mContext.getPackageManager();
+            final ResolveInfo launcherInfo = pm.resolveActivity(launcherIntent, 0);
+            return launcherInfo.activityInfo.packageName;
+        }
+    };
+
     private int mShowSearchHoldoff = 0;
     private Runnable mShowSearchPanel = new Runnable() {
         public void run() {
@@ -1133,7 +1180,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
         mNavigationBarView.setListeners(mRecentsClickListener,
-                mRecentsPreloadOnTouchListener, mHomeSearchActionListener);
+                mRecentsPreloadOnTouchListener, mRecentsLongPressListener,
+                mHomeSearchActionListener);
         updateSearchPanel();
     }
 
