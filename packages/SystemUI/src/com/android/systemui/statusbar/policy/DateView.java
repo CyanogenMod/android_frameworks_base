5633549e2527bf8ae1077621135370a99eeb74c0
kaiyiz <kaiyiz@codeaurora.org>
2014-08-16 15:54:57 +0800

    Regional: Customize features for regional package
    -Customize default Date&Time display format.
    -Customize mock and Dock on or off for carrier.
    -Customize accessibility on or off for carrier.
    -Customize default input method.
    -Customize default enable data roaming or not.
    -Customize default enable mobile data or not.
    -Customize default network.
    
    Change-Id: I3a74d0113d6b730c4d7935aaa2da307ebe500566

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateView.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ *
  * Copyright (C) 2008 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -105,11 +108,19 @@ public class DateView extends TextView {
         }
 
         mCurrentTime.setTime(System.currentTimeMillis());
-
-        final String text = mDateFormat.format(mCurrentTime);
+        final String text = getDateFormat();
         if (!text.equals(mLastText)) {
             setText(text);
             mLastText = text;
+        }
+    }
+
+    private String getDateFormat() {
+        if (getContext().getResources().getBoolean(
+                com.android.internal.R.bool.def_custom_dateformat)) {
+            return DateFormat.getDateFormat(getContext()).format(mCurrentTime);
+        } else {
+            return mDateFormat.format(mCurrentTime);
         }
     }
 }
