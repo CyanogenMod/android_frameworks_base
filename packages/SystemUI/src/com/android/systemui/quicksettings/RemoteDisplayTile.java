c890326f23a2a8f4c5da5cf8b200acadf5b650bb
Brint E. Kriebel <github@bekit.net>
2014-08-17 22:06:06 -0700

    Revert "QuickSettings : Register/unregister cast tile one time"
    This causes a CTS failure: android.security.cts.ListeningPortsTest
      testNoRemotelyAccessibleListeningUdp6Ports
    
    This reverts commit e037c1fe6dbdcd077719f7fc7b1902a887b56c23.
    
    Change-Id: Idc145b6fb17490664fb5614d44e1ae92c98df844

--- packages/SystemUI/src/com/android/systemui/quicksettings/RemoteDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/RemoteDisplayTile.java
@@ -82,17 +82,23 @@ public class RemoteDisplayTile extends QuickSettingsTile{
 
     @Override
     void onPostCreate() {
-        mMediaRouter.addCallback(MediaRouter.ROUTE_TYPE_REMOTE_DISPLAY,
-                mRemoteDisplayRouteCallback,
-                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
-        updateRemoteDisplays();
-        super.onPostCreate();
-    }
+        mTile.setOnPrepareListener(new QuickSettingsTileView.OnPrepareListener() {
+            @Override
+            public void onPrepare() {
+                mMediaRouter.addCallback(MediaRouter.ROUTE_TYPE_REMOTE_DISPLAY,
+                        mRemoteDisplayRouteCallback,
+                        MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+                updateRemoteDisplays();
+            }
+            @Override
+            public void onUnprepare() {
+                mMediaRouter.removeCallback(mRemoteDisplayRouteCallback);
+            }
+        });
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMediaRouter.removeCallback(mRemoteDisplayRouteCallback);
+        updateRemoteDisplays();
+
+        super.onPostCreate();
     }
 
     @Override
