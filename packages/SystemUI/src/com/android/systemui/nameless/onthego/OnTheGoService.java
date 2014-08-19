e3993de12d0e89966606e625b494c24038e6e144
PrimeDirective <activethrasher00@gmail.com>
2014-08-18 21:41:17 -0400

    OnTheGoService: Use correct visibility modifier
    private outer class objects cannot be directly accessed by private inner classes and
    require additional virtual accessor methods added during compile time.  This is
    inefficient.
    
    Change-Id: Iadef9463e64bcd587b291a4c18d8f2412b93dcd6

--- packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java
+++ packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java
@@ -74,7 +74,7 @@ public class OnTheGoService extends Service implements ShakeDetector.Listener {
     private static final int NOTIFICATION_ERROR   = 2;
 
     private final Handler mHandler       = new Handler();
-    private final Object  mRestartObject = new Object();
+    public final Object  mRestartObject = new Object();
 
     private FrameLayout         mOverlay;
     private Camera              mCamera;
