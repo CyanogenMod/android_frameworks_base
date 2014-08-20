a67f6989c6ee6e3cf64df69bf6cea38e186dc9db
Roman Birg <roman@cyngn.com>
2014-08-20 11:04:31 -0700

    policy: respect FLAG_TURN_SCREEN_ON flag with device flip covers
    If an application uses WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
    and the device supports covers and the cover is closed, then after the
    screen would turn on due to this flag, it would immediately be turned off
    because PhoneWindowManager thinks the cover is closed, so it puts the device to
    sleep.
    
    Respect this flag and do nothing if it is detected.
    
    Change-Id: I7eb36ce676730e47c551518e214b08561c401ea5
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6991,6 +6991,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mPowerManager.setKeyboardVisibility(isBuiltInKeyboardVisible());
 
         if (mLidState == LID_CLOSED && mLidControlsSleep) {
+            if (mFocusedWindow != null && (mFocusedWindow.getAttrs().flags
+                    & WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON) != 0) {
+                // if an application requests that the screen be turned on
+                // and there's a closed device cover, don't turn the screen off!
+                return;
+            }
+
             TelecomManager telephonyService = getTelecommService();
             if (!(telephonyService == null
                     || telephonyService.isRinging())) {
