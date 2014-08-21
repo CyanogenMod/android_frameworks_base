7d043804000b11a32579e1ec83001ef800fc601c
Sachin Sharma <sachsh@codeaurora.org>
2014-08-21 12:21:21 -0700

    Disable generation of tombstone on an ANR The commit: I5b78ce62b26f25d3dc14097e2988ba12c2d77826 (creating seperate files for ANRs traces) provided the following two debug capabilities: (1) renames traces.txt and (2) generates tombstones(for native traces) The second functionalatity is not needed any more since the traces.txt now includes the native traces as well. Generating tombstones for system server causes framework reboot and that should be avoided. This change will prevent dumping of tombstones during ANR.
    Change-Id: Ia6785261b8fe536d4aacc78e2f8be552e9335f0e

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5254,11 +5254,6 @@ public final class ActivityManagerService extends ActivityManagerNative
                 else
                     newTracesPath = tracesPath + "_" + app.processName;
                 traceRenameFile.renameTo(new File(newTracesPath));
-
-                Process.sendSignal(app.pid, 6);
-                SystemClock.sleep(1000);
-                Process.sendSignal(app.pid, 6);
-                SystemClock.sleep(1000);
             }
 
             // Bring up the infamous App Not Responding dialog
