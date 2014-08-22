ce37355c8e131bbbeced73d40983c2705b0263c8
Roman Birg <roman@cyngn.com>
2014-08-22 12:24:39 -0700

    SystemUI: enable equalizer for Wifi display
    While the EQ doesn't work while using the Chromecast, using Wireless
    display (miracast) still allows for control of the EQ since it's
    streaming from the device.
    
    Change-Id: I7322429b39438e66c013456b6f8a3242e333d93a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/quicksettings/EqualizerTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/EqualizerTile.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui.quicksettings;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Canvas;
@@ -26,6 +27,8 @@ import android.media.MediaRouter;
 import android.media.RemoteControlClient;
 import android.media.RemoteController;
 import android.media.audiofx.AudioEffect;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.view.View;
 
 import com.android.systemui.R;
@@ -42,6 +45,7 @@ public class EqualizerTile extends QuickSettingsTile {
     private AudioManager mAudioManager;
     private RemoteController mRemoteController;
     private QuickTileVisualizer mVisualizer;
+    private boolean mWifiDisplayActive;
 
     private RemoteController.OnClientUpdateListener mRCClientUpdateListener =
             new RemoteController.OnClientUpdateListener() {
@@ -141,6 +145,17 @@ public class EqualizerTile extends QuickSettingsTile {
                         AudioEffect.ACTION_DISPLAY_AUDIO_EFFECT_CONTROL_PANEL));
             }
         };
+        qsc.registerAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION, this);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(intent.getAction())) {
+            NetworkInfo networkInfo = (NetworkInfo) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+            mWifiDisplayActive = networkInfo.isConnected();
+            updateResources();
+        }
     }
 
     @Override
@@ -213,6 +228,9 @@ public class EqualizerTile extends QuickSettingsTile {
     private boolean isMusicPlaying() {
         switch (mCurrentPlayState) {
             case RemoteControlClient.PLAYSTATE_PLAYING:
+                if (mWifiDisplayActive) {
+                    return true;
+                }
                 // Check if Chromecast is active
                 MediaRouter mediaRouter = (MediaRouter)
                         mContext.getSystemService(Context.MEDIA_ROUTER_SERVICE);
