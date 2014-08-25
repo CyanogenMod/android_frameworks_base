854742c17e9ec7f94f2541ff41adf8534415d2b4
Roman Birg <roman@cyngn.com>
2014-08-25 15:35:40 -0700

    frameworks: allow last app action to be assigned to hardware keys
    Also fix multi-user handling for 'switch to last app' and 'kill
    foreground app' actions.
    
    Change-Id: I1fb586d19b1198e6888b0f11e0a2813acf0bcd5b
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Danny Baumann <dannybaumann@web.de>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -129,6 +129,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.cm.ActionUtils;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.BatteryMeterView;
@@ -4402,17 +4403,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
 
             if (hijackRecentsLongPress) {
-                final ActivityManager am =
-                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
-                ActivityManager.RunningTaskInfo lastTask = getLastTask(am);
-
-                if (lastTask != null) {
-                    if (DEBUG) Log.d(TAG, "switching to " + lastTask.topActivity.getPackageName());
-                    final ActivityOptions opts = ActivityOptions.makeCustomAnimation(mContext,
-                            R.anim.last_app_in, R.anim.last_app_out);
-                    am.moveTaskToFront(lastTask.id, ActivityManager.MOVE_TASK_NO_USER_ACTION,
-                            opts.toBundle());
-                }
+                ActionUtils.switchToLastApp(mContext, mCurrentUserId);
             }
         } catch (RemoteException e) {
             Log.d(TAG, "Unable to reach activity manager", e);
