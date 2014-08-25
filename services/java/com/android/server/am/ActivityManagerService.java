876704207fec6bbcd40b6ec9436eeb1cd7e77c03
louis_chang <louis_chang@htc.com>
2014-08-25 16:39:19 +0800

    [ActivityManager] Fix race condition in pss collection
    Symptom:
    There has a race condition that two threads are accessing
    the mPendingPssProcesses simultaneously. One of the thread
    is collecting the process pss by looping the mPendingPssProcesses.
    The other thread is requesting to collect pss of all processes,
    which clears mPendingPssProcesses and adding processes back.
    
    Solution:
    Avoid race condition by adding synchornized protection.
    
    Change-Id: Ifb090eda9c4a1b8e3fd980fe0171e9dd77773b46

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -1698,7 +1698,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                 break;
             }
             case REQUEST_ALL_PSS_MSG: {
-                requestPssAllProcsLocked(SystemClock.uptimeMillis(), true, false);
+                synchronized (ActivityManagerService.this) {
+                    requestPssAllProcsLocked(SystemClock.uptimeMillis(), true, false);
+                }
                 break;
             }
             case POST_PRIVACY_NOTIFICATION_MSG: {
