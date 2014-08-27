e580216f83c4330fc3e81103cfb98109e03e9bc1
Clark Scheff <clark@cyngn.com>
2014-08-27 14:39:56 -0700

    Themes: Fix native crash in libandroidfw
    If a missing attribute needs to be added in somewhere in the middle
    of the bag, a segfault would occur because we change curEntry to a
    value that is beyond the set's allocated space.  This patch stores
    curEntry in prevEntry before changing it's value and memmove uses
    prevEntry to index the set.
    
    Change-Id: Ie70f2ad465097c3acf76caa729999c424fb01ee2

--- libs/androidfw/ResourceTypes.cpp
+++ libs/androidfw/ResourceTypes.cpp
@@ -3780,6 +3780,7 @@ ssize_t ResTable::getBagLocked(uint32_t resID, const bag_entry** outBag,
                 if ((!isInside) || oldName != newName) {
                     // This is a new attribute...  figure out what to do with it.
                     // Need to alloc more memory...
+                    size_t prevEntry = curEntry;
                     curEntry = set->availAttrs;
                     set->availAttrs++;
                     const size_t newAvail = set->availAttrs;
@@ -3794,8 +3795,8 @@ ssize_t ResTable::getBagLocked(uint32_t resID, const bag_entry** outBag,
                                  set, entries, set->availAttrs));
                     if (isInside) {
                         // Going in the middle, need to make space.
-                        memmove(entries+curEntry+1, entries+curEntry,
-                                sizeof(bag_entry)*(set->numAttrs-curEntry));
+                        memmove(entries+prevEntry+1, entries+prevEntry,
+                                sizeof(bag_entry)*(set->numAttrs-prevEntry));
                     }
                     TABLE_NOISY(printf("#%d: Inserting new attribute: 0x%08x\n",
                                  curEntry, newName));
