553ff9fc650b2ddb22510a20b1a4abe23188688b
Clark Scheff <clark@cyngn.com>
2014-08-27 10:21:44 -0700

    Allow granting permissions based on signature in <allow-permission/>
    This patch allows us to either specify a sharedUserId or a package
    signature to use when granting the specific permission.
    
    Change-Id: I8aed78d40316e0e94ac1bfefc7c4a3016a2a9a6b

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -452,6 +452,9 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     final HashSet<String> mAllowances = new HashSet<String>();
 
+    final HashMap<Signature, HashSet<String>> mSignatureAllowances
+            = new HashMap<Signature, HashSet<String>>();
+
     // All available services, for your resolving pleasure.
     final ServiceIntentResolver mServices = new ServiceIntentResolver();
 
@@ -1844,14 +1847,35 @@ public class PackageManagerService extends IPackageManager.Stub {
                         continue;
                     }
                     String sharedUserId = parser.getAttributeValue(null, "sharedUserId");
-                    if (sharedUserId == null) {
+                    String signature = parser.getAttributeValue(null, "signature");
+                    if (signature == null && sharedUserId == null) {
                         Slog.w(TAG,
-                                "<allow-permission> without uid at "
+                                "<allow-permission> without uid or signature at "
                                         + parser.getPositionDescription());
                         XmlUtils.skipCurrentTag(parser);
                         continue;
                     }
-                    mAllowances.add(sharedUserId + ":" + perm);
+                    if (sharedUserId != null) mAllowances.add(sharedUserId + ":" + perm);
+                    if (signature != null) {
+                        Signature sig = null;
+                        try {
+                            sig = new Signature(signature);
+                        } catch (IllegalArgumentException e) {
+                            // sig will be null so we will log it below
+                        }
+                        if (sig != null) {
+                            HashSet<String> perms = mSignatureAllowances.get(sig);
+                            if (perms == null) {
+                                perms = new HashSet<String>();
+                                mSignatureAllowances.put(sig, perms);
+                            }
+                            perms.add(perm);
+                        } else {
+                            Slog.w(TAG,
+                                    "<allow-permission> with bad signature at "
+                                            + parser.getPositionDescription());
+                        }
+                    }
                     XmlUtils.skipCurrentTag(parser);
 
                 } else if ("library".equals(name)) {
@@ -2593,6 +2617,16 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
     }
 
+    private boolean isAllowedSignature(PackageParser.Package pkg, String permissionName) {
+        for (Signature pkgSig : pkg.mSignatures) {
+            HashSet<String> perms = mSignatureAllowances.get(pkgSig);
+            if (perms != null && perms.contains(permissionName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void grantPermission(String packageName, String permissionName) {
         mContext.enforceCallingOrSelfPermission(
                 android.Manifest.permission.GRANT_REVOKE_PERMISSIONS, null);
@@ -2605,8 +2639,6 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (bp == null) {
                 throw new IllegalArgumentException("Unknown permission: " + permissionName);
             }
-
-            checkGrantRevokePermissions(pkg, bp);
 
             final PackageSetting ps = (PackageSetting) pkg.mExtras;
             if (ps == null) {
@@ -6389,7 +6421,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                         && mAllowances.contains(pkg.mSharedUserId + ":" + perm));
         if (!allowed && (bp.protectionLevel
                 & PermissionInfo.PROTECTION_FLAG_SYSTEM) != 0) {
-            if (isSystemApp(pkg)) {
+            boolean allowedSig = isAllowedSignature(pkg, perm);
+            if (isSystemApp(pkg) || allowedSig) {
                 // For updated system applications, a system permission
                 // is granted only if it had been defined by the original application.
                 if (isUpdatedSystemApp(pkg)) {
@@ -6422,7 +6455,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
                     }
                 } else {
-                    allowed = isPrivilegedApp(pkg);
+                    allowed = isPrivilegedApp(pkg) || allowedSig;
                 }
             }
         }
