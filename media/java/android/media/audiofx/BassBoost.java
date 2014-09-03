c6673ab48da6fecb2257a1bed786f8c94a810fd6
maxwen <max.weninger@gmail.com>
2014-09-04 00:02:45 +0200

    base: audiofx: harden against offload effects
    Hack for now to allow using DSPManager for offload effects
    
    -offload effect equalizer has only 5 bands but DSPManager has hardcoded 6
    -offload effect bassboost doesnt support center freq
    
    Change-Id: I9ad661221654415b2be3ed18f8cf31453d6df637

--- media/java/android/media/audiofx/BassBoost.java
+++ media/java/android/media/audiofx/BassBoost.java
@@ -162,7 +162,11 @@ public class BassBoost extends AudioEffect {
      */
     public void setCenterFrequency(short freq)
     throws IllegalStateException, IllegalArgumentException, UnsupportedOperationException {
-        checkStatus(setParameter(PARAM_CENTER_FREQUENCY, freq));
+        try {
+            checkStatus(setParameter(PARAM_CENTER_FREQUENCY, freq));
+        } catch(IllegalArgumentException e) {
+            // ignore
+        }
     }
 
     /**
@@ -173,9 +177,13 @@ public class BassBoost extends AudioEffect {
      * @throws UnsupportedOperationException
      */
     public short getCenterFrequency() {
-        short[] value = new short[1];
-        checkStatus(getParameter(PARAM_CENTER_FREQUENCY, value));
-        return value[0];
+        try {
+            short[] value = new short[1];
+            checkStatus(getParameter(PARAM_CENTER_FREQUENCY, value));
+            return value[0];
+        } catch(IllegalArgumentException e) {
+            return 55;
+        }
     }
 
     /**
