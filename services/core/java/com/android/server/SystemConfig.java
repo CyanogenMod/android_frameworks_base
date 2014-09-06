6bbbba9aa865a4abf43a3ce2dd29524461cc4002
Clark Scheff <clark@cyngn.com>
2014-09-06 15:00:02 -0700

    Allow granting permissions based on signature in <allow-permission/>
    This patch allows us to either specify a sharedUserId or a package
    signature to use when granting the specific permission.
    
    Change-Id: I8aed78d40316e0e94ac1bfefc7c4a3016a2a9a6b

--- services/core/java/com/android/server/SystemConfig.java
+++ services/core/java/com/android/server/SystemConfig.java
@@ -18,6 +18,7 @@ package com.android.server;
 
 import android.app.ActivityManager;
 import android.content.pm.FeatureInfo;
+import android.content.pm.Signature;
 import android.os.*;
 import android.os.Process;
 import android.util.ArrayMap;
@@ -88,6 +89,9 @@ public class SystemConfig {
     // These are the app package names that should not allow IME switching.
     final ArraySet<String> mFixedImeApps = new ArraySet<>();
 
+    final HashMap<Signature, HashSet<String>> mSignatureAllowances
+            = new HashMap<Signature, HashSet<String>>();
+
     public static SystemConfig getInstance() {
         synchronized (SystemConfig.class) {
             if (sInstance == null) {
@@ -123,6 +127,10 @@ public class SystemConfig {
 
     public ArraySet<String> getFixedImeApps() {
         return mFixedImeApps;
+    }
+
+    public HashMap<Signature, HashSet<String>> getSignatureAllowances() {
+        return mSignatureAllowances;
     }
 
     SystemConfig() {
@@ -269,6 +277,43 @@ public class SystemConfig {
                         mSystemPermissions.put(uid, perms);
                     }
                     perms.add(perm);
+                    XmlUtils.skipCurrentTag(parser);
+
+                } else if ("allow-permission".equals(name)) {
+                    String perm = parser.getAttributeValue(null, "name");
+                    if (perm == null) {
+                        Slog.w(TAG,
+                                "<allow-permission> without name at "
+                                        + parser.getPositionDescription());
+                        XmlUtils.skipCurrentTag(parser);
+                        continue;
+                    }
+                    String signature = parser.getAttributeValue(null, "signature");
+                    if (signature == null) {
+                        Slog.w(TAG,
+                                "<allow-permission> without signature at "
+                                        + parser.getPositionDescription());
+                        XmlUtils.skipCurrentTag(parser);
+                        continue;
+                    }
+                    Signature sig = null;
+                    try {
+                        sig = new Signature(signature);
+                    } catch (IllegalArgumentException e) {
+                        // sig will be null so we will log it below
+                    }
+                    if (sig != null) {
+                        HashSet<String> perms = mSignatureAllowances.get(sig);
+                        if (perms == null) {
+                            perms = new HashSet<String>();
+                            mSignatureAllowances.put(sig, perms);
+                        }
+                        perms.add(perm);
+                    } else {
+                        Slog.w(TAG,
+                                "<allow-permission> with bad signature at "
+                                        + parser.getPositionDescription());
+                    }
                     XmlUtils.skipCurrentTag(parser);
 
                 } else if ("library".equals(name) && !onlyFeatures) {
--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -412,6 +412,7 @@ public class PackageManagerService extends IPackageManager.Stub {
     final int[] mGlobalGids;
     final SparseArray<ArraySet<String>> mSystemPermissions;
     final ArrayMap<String, FeatureInfo> mAvailableFeatures;
+    final ArrayMap<Signature, HashSet<String>> mSignatureAllowances;
 
     // If mac_permissions.xml was found for seinfo labeling.
     boolean mFoundPolicyFile;
@@ -1388,6 +1389,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         mGlobalGids = systemConfig.getGlobalGids();
         mSystemPermissions = systemConfig.getSystemPermissions();
         mAvailableFeatures = systemConfig.getAvailableFeatures();
+        mSignatureAllowances = systemConfig.getSignatureAllowances();
 
         synchronized (mInstallLock) {
         // writer
@@ -2692,6 +2694,16 @@ public class PackageManagerService extends IPackageManager.Stub {
                         + ". It is required by the application");
             }
         }
+    }
+
+    private boolean isAllowedSignature(PackageParser.Package pkg, String permissionName) {
+        for (Signature pkgSig : pkg.mSignatures) {
+            HashSet<String> perms = mSignatureAllowances.get(pkgSig);
+            if (perms != null && perms.contains(permissionName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -7274,7 +7286,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                         == PackageManager.SIGNATURE_MATCH);
         if (!allowed && (bp.protectionLevel
                 & PermissionInfo.PROTECTION_FLAG_SYSTEM) != 0) {
-            if (isSystemApp(pkg)) {
+            boolean allowedSig = isAllowedSignature(pkg, perm);
+            if (isSystemApp(pkg) || allowedSig) {
                 // For updated system applications, a system permission
                 // is granted only if it had been defined by the original application.
                 if (isUpdatedSystemApp(pkg)) {
@@ -7309,7 +7322,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
                     }
                 } else {
-                    allowed = isPrivilegedApp(pkg);
+                    allowed = isPrivilegedApp(pkg) || allowedSig;
                 }
             }
         }
