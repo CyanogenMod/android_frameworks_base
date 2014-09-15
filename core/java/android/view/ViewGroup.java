2057c4a4de4059671702a45d16fcfa0e56be5757
kaiyiz <kaiyiz@codeaurora.org>
2014-09-15 11:19:53 +0800

    frameworks/base: Reginal requirement for RTL
    Improve resolution of RTL
    
    Allow system apps support RTL with a low target sdk version.
    Fix RTL bugs for TextView, GridView, etc.
    
    Change-Id: I1ff4127f103f65f333195803bc7088e561719f6f

--- core/java/android/view/ViewGroup.java
+++ core/java/android/view/ViewGroup.java
@@ -18,6 +18,7 @@ package android.view;
 
 import android.animation.LayoutTransition;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
@@ -6635,8 +6636,10 @@ public abstract class ViewGroup extends View implements ViewParent, ViewManager 
             }
 
             final boolean hasRtlSupport = c.getApplicationInfo().hasRtlSupport();
+            final boolean isSystemApp = (c.getApplicationInfo().flags &
+                    ApplicationInfo.FLAG_SYSTEM) != 0;
             final int targetSdkVersion = c.getApplicationInfo().targetSdkVersion;
-            if (targetSdkVersion < JELLY_BEAN_MR1 || !hasRtlSupport) {
+            if (targetSdkVersion < JELLY_BEAN_MR1 && !isSystemApp || !hasRtlSupport) {
                 mMarginFlags |= RTL_COMPATIBILITY_MODE_MASK;
             }
 
