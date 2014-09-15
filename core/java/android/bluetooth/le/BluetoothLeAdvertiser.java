e2bc9b148e38f3764ac76a48b2235ae66d4947fc
Natiq Ahmed <mnatiq@codeaurora.org>
2014-09-15 19:23:04 +0530

    Bluetooth: Fix for hash map table entry for Advertisement
    Addition of callback to the hash map is been decided based on the
    success of multi advertiser callback i.e advertiser callback will be added
    iff the advertisment is succesfull.
    
    Conflicts:
    	core/java/android/bluetooth/le/BluetoothLeAdvertiser.java
    
    Change-Id: I90e810fdee944c1a82729bdf7fcd9ed26f5eb094
    CRs-fixed: 721903

--- core/java/android/bluetooth/le/BluetoothLeAdvertiser.java
+++ core/java/android/bluetooth/le/BluetoothLeAdvertiser.java
@@ -264,7 +264,9 @@ public final class BluetoothLeAdvertiser {
                     Log.e(TAG, "Failed to start registeration", e);
                 }
                 if (mClientIf > 0 && mIsAdvertising) {
-                    mLeAdvertisers.put(mAdvertiseCallback, this);
+                    if (!mLeAdvertisers.containsKey(mAdvertiseCallback)) {
+                        mLeAdvertisers.put(mAdvertiseCallback, this);
+                    }
                 } else if (mClientIf <= 0) {
                     // Post internal error if registration failed.
                     postStartFailure(mAdvertiseCallback,
@@ -330,6 +332,9 @@ public final class BluetoothLeAdvertiser {
                         // Start success
                         mIsAdvertising = true;
                         postStartSuccess(mAdvertiseCallback, settings);
+                        if (!mLeAdvertisers.containsKey(mAdvertiseCallback)) {
+                            mLeAdvertisers.put(mAdvertiseCallback, this);
+                        }
                     } else {
                         // Start failure.
                         postStartFailure(mAdvertiseCallback, status);
