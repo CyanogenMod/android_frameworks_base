cca7647487d10046ebebce550e45ccb58248f453
Phil Tunstall <ptunstall@gmail.com>
2014-09-16 22:22:19 +0100

    AudioService: Reduce volume to safe level when headset is connected (2/2)
    This is a safety feature for users who opt to raise volume above the designated
    safe level while using a headset.
    
    Currently when a user does this they are provided with no protection against
    dangerously high volume levels until the next time the headset volume warning
    is displayed.
    
    This patch adds an optional feature to ensure volume is no higher than the safe
    level every time a headset is connected.
    
    Change-Id: Idaa9cc9549bc3c6278e1e44490433c5366c29c16

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -2904,6 +2904,15 @@ public class AudioService extends IAudioService.Stub {
 
     public void setWiredDeviceConnectionState(int device, int state, String name) {
         synchronized (mConnectedDevices) {
+            if ((state == 1) && ((device & mSafeMediaVolumeDevices) != 0)) {
+                boolean restoreSafeVolume = Settings.System.getIntForUser(
+                        mContentResolver,
+                        Settings.System.SAFE_HEADSET_VOLUME_RESTORE,
+                        0, UserHandle.USER_CURRENT) != 0;
+                if (restoreSafeVolume) {
+                    enforceSafeMediaVolume();
+                }
+            }
             int delay = checkSendBecomingNoisyIntent(device, state);
             queueMsgUnderWakeLock(mAudioHandler,
                     MSG_SET_WIRED_DEVICE_CONNECTION_STATE,
@@ -4254,12 +4263,11 @@ public class AudioService extends IAudioService.Stub {
                     }
                 }
             } else if (action.equals(Intent.ACTION_HEADSET_PLUG)) {
+                applyCurrentStreamVolume();
+
                 state = intent.getIntExtra("state", 0);
                 if (state == 1) {
                     // Headset plugged in
-                    adjustCurrentStreamVolume();
-                    // TODO: Cap volume at safe levels
-
                     boolean launchPlayer = Settings.System.getIntForUser(
                             context.getContentResolver(),
                             Settings.System.HEADSET_CONNECT_PLAYER,
@@ -4274,9 +4282,6 @@ public class AudioService extends IAudioService.Stub {
                             Log.w(TAG, "No music player found to start after headset connection");
                         }
                     }
-                } else {
-                    // Headset disconnected
-                    adjustCurrentStreamVolume();
                 }
             } else if (action.equals(Intent.ACTION_USB_AUDIO_ACCESSORY_PLUG) ||
                            action.equals(Intent.ACTION_USB_AUDIO_DEVICE_PLUG)) {
@@ -4368,7 +4373,7 @@ public class AudioService extends IAudioService.Stub {
                         0,
                         null,
                         SAFE_VOLUME_CONFIGURE_TIMEOUT_MS);
-                adjustCurrentStreamVolume();
+                applyCurrentStreamVolume();
             } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
                 AudioSystem.setParameters("screen_state=on");
             } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
@@ -4400,7 +4405,10 @@ public class AudioService extends IAudioService.Stub {
             }
         }
 
-        private void adjustCurrentStreamVolume() {
+        // Forces volume to update. This is needed by devices affected by a bug
+        // where the new volume is not applied when the audio device changes.
+        // See: review.cyanogenmod.org/33778
+        private void applyCurrentStreamVolume() {
             VolumeStreamState streamState;
             int device;
 
