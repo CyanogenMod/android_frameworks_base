827b641c777ad5aa1755e4e4eed5f4dc7e221222
Clark Scheff <clark@cyngn.com>
2014-09-15 17:27:34 -0700

    Themes: Only process themes that are applied during boot
    Currently, the PackageManagerService will process all themes and
    ensure their resources are compiled and available to the system.
    This is highly inefficient and can bog the system down when booting.
    
    This patch will only process the themes that are currently applied
    durring boot and lets the ThemeService take care of queuing up the
    others for processing.   This reduces boot time considerably,
    especially when the user has many themes installed.
    
    Change-Id: I52168a1751ad3bfc458048dd1558ad71cc16317a

--- test-runner/src/android/test/mock/MockPackageManager.java
+++ test-runner/src/android/test/mock/MockPackageManager.java
@@ -680,6 +680,14 @@ public class MockPackageManager extends PackageManager {
      * @hide
      */
     @Override
+    public void processThemeResources(String themePkgName) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @hide
+     */
+    @Override
     public void setComponentProtectedSetting(ComponentName componentName, boolean newState) {
         throw new UnsupportedOperationException();
     }
