33a0efa4aa0f182e2a81607c5c948928773c1c92
Rakesh Pallerla <rakesh@codeaurora.org>
2014-09-26 16:13:46 +0530

    Store Network Mode selected in subId Table.
    Modify code to store network selected by user in
    subId table.
    DELETED:telephony/java/android/telephony/SubInfoRecord.java
    
    Change-Id: I361c5cbc47ea6079deb3e807276a3f3504a9535e

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -260,11 +260,21 @@ public class SubscriptionManager {
     public static final String SIM_PROVISIONING_STATUS = "sim_provisioning_status";
 
     /**
+     * The Network mode of SIM/sub.
+     * <P>Type: INTEGER (int)</P>
+     */
+    /** @hide */
+    public static final String NETWORK_MODE = "network_mode";
+
+    /**
      * TelephonyProvider column name for the user configured network mode associated with a SIM.
      * <P>Type: INTEGER (int)</P>
      * @hide
      */
     public static final String USER_NETWORK_MODE = "user_network_mode";
+
+    /** @hide */
+    public static final int DEFAULT_NW_MODE = -1;
 
     /**
      *  TelephonyProvider column name for extreme threat in CB settings
