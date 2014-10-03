c130cba1f11b5a4c0bd57e6266c5d9dbdcef9dd6
Matthias Yzusqui <ymatxt@gmail.com>
2014-10-04 00:01:04 +0200

    wifi: show devices behind a firewall that are connected to the  WiFi hotspot
    * Clients that are behind a firewall, in particular Windows-PCs
    tend to not respond to the request invoked by the isReached(val)-method.
    Executing a ping instead and evaluating the outcome enables us to bypass the firewall.
    
    Change-Id: If857545c7a4d5fbc9d2dc919096a9c84e64d6877

--- core/java/com/android/internal/util/wifi/ClientsList.java
+++ core/java/com/android/internal/util/wifi/ClientsList.java
@@ -26,7 +26,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.lang.Process;
+import java.lang.Runtime;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -41,6 +42,7 @@ public class ClientsList {
      *                       (probably disconnected) clients, {@code true} otherwise
      * @return ArrayList of {@link ClientScanResult}
      */
+
     public static ArrayList<ClientScanResult> get(boolean onlyReachables, Context context) {
         BufferedReader br = null;
         ArrayList<ClientScanResult> result = new ArrayList<ClientScanResult>();
@@ -57,8 +59,7 @@ public class ClientsList {
                     String mac = splitted[3];
 
                     if (mac.matches("..:..:..:..:..:..")) {
-                        InetAddress address = InetAddress.getByName(splitted[0]);
-                        boolean isReachable = address.isReachable(3000);
+                        boolean isReachable = isReachableByPing(splitted[0]);
 
                         if (!onlyReachables || isReachable) {
                             ClientScanResult client = new ClientScanResult();
@@ -134,6 +135,26 @@ public class ClientsList {
             eventType = parser.next();
         }
         return "";
+    }
+
+    private static boolean isReachableByPing(String host) {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process mIpAddrProcess = runtime.exec("/system/bin/ping -c 1 -w 3 " + host);
+            int mExitValue = mIpAddrProcess.waitFor();
+            mIpAddrProcess.destroy();
+
+            if (mExitValue == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (InterruptedException e) {
+            Log.d(TAG, "catch InterruptedException hit in run", e);
+        } catch (IOException e) {
+            Log.d(TAG, "catch IOException hit in run", e);
+        }
+        return false;
     }
 
     public static class ClientScanResult {
