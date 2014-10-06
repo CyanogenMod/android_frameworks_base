c5b7a6820f1c916dea15c2c0e71d477a7901e95b
kaiyiz <kaiyiz@codeaurora.org>
2014-10-06 11:40:09 +0800

    Regional: Customize auto-popup USB menu
    If the feature is enabled, phone will automatically pop up USB
    menu when connect with USB cable into PC.
    
    The feature can be enable/disable by overlay key:
    always_popup_usb_computer_connection_option
    
    Default is disable(false).
    
    Change-Id: Iacd8fa169942d3b77885e8f8d8b53dce94a690ac

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -660,6 +660,10 @@ public class UsbDeviceManager {
                     if (mDebuggingManager != null) {
                         mDebuggingManager.setAdbEnabled(mAdbEnabled);
                     }
+                    if (mContext.getResources().getBoolean(com.android.internal.
+                            R.bool.always_popup_usb_computer_connection_option)) {
+                        updateState(bootState(mConnected, mConfigured));
+                    }
                     break;
                 case MSG_USER_SWITCHED: {
                     UserManager userManager =
@@ -684,6 +688,18 @@ public class UsbDeviceManager {
                     break;
                 }
             }
+        }
+
+        private String bootState(boolean isConnected, boolean isConfigured) {
+            String bootState = "CONNECTED";
+            if (!isConnected && !isConfigured) {
+                bootState = "DISCONNECTED";
+            } else if (isConnected && !isConfigured) {
+                bootState = "CONNECTED";
+            } else if (isConnected && isConfigured) {
+                bootState = "CONFIGURED";
+            }
+            return bootState;
         }
 
         public UsbAccessory getCurrentAccessory() {
@@ -713,7 +729,7 @@ public class UsbDeviceManager {
                     //}
                 }
             }
-            if (id != mUsbNotificationId) {
+            if (id != mUsbNotificationId && mBootCompleted) {
                 // clear notification if title needs changing
                 if (mUsbNotificationId != 0) {
                     mNotificationManager.cancelAsUser(null, mUsbNotificationId,
@@ -747,6 +763,9 @@ public class UsbDeviceManager {
                     mNotificationManager.notifyAsUser(null, id, notification,
                             UserHandle.ALL);
                     mUsbNotificationId = id;
+                    if (r.getBoolean(com.android.internal.R.bool.
+                            always_popup_usb_computer_connection_option))
+                        mContext.startActivity(intent);
                 }
             }
         }
