c1e1ac25b2d887dbf59923ef02b2b31dbaa287c0
Natiq Ahmed <mnatiq@codeaurora.org>
2014-10-17 18:28:11 +0530

    Compiler Fix for new Aosp.
    Compiler Fix for new Aosp
    
    Change-Id: I9fc53b52afc890b76a328702882262608a02c16a

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -3842,7 +3842,7 @@ public class ConnectivityService extends IConnectivityManager.Stub {
             for (NetworkAgentInfo nai : mNetworkAgentInfos.values()) {
                 if(mDefaultRequest.networkCapabilities.satisfiedByNetworkCapabilities(
                             nai.networkCapabilities)) {
-                    if(nai.currentScore > networkSwitchTo.currentScore){
+                    if(nai.getCurrentScore() > networkSwitchTo.getCurrentScore()){
                         networkSwitchTo = nai;
                     }
                 }
