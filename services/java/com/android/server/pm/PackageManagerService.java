6b675187aa589990641c83c411909ee6237acbf4
Andy Mast <andy@cyngn.com>
2014-10-31 10:42:00 -0700

    Fix crash while installing themes rapidly
    A theme can be installed on a PM or theme service thread.
    If multiple themes are being installed at once and one of
    those themes happens to be part of the boot config, then
    two themes will try to be installed at the same time. This
    causes problems with files like the temp AndroidManfiest.xml
    which are not designed to be multi-thread safe.
    
    This patch fixes the issue by making sure a theme is
    never installed in a PM thread except during boot.
    
    Change-Id: Ic9a0ffe43d0199a2a893579f3d2dbb6d9340e7b4

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -5530,7 +5530,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 Exception failedException = null;
 
                 insertIntoOverlayMap(target, pkg);
-                if (mBootThemeConfig != null &&
+                if (isBootScan && mBootThemeConfig != null &&
                         (pkg.packageName.equals(mBootThemeConfig.getOverlayPkgName()) ||
                         pkg.packageName.equals(
                                 mBootThemeConfig.getOverlayPkgNameForApp(target)))) {
@@ -5565,7 +5565,7 @@ public class PackageManagerService extends IPackageManager.Stub {
             }
 
             //Icon Packs need aapt too
-            if ((mBootThemeConfig != null &&
+            if (isBootScan && (mBootThemeConfig != null &&
                     pkg.packageName.equals(mBootThemeConfig.getIconPackPkgName()))) {
                 if (isIconCompileNeeded(pkg)) {
                     try {
