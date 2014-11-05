1439b0c930753046fd2d55427d58f839cc5b049f
Bhavya Sokke Mallikarjunappa <bsokke@codeaurora.org>
2014-11-05 11:20:25 -0800

    Override updateBlockedUids method
    Override updateBlockedUids method in BridgePowermanager
    
    Change-Id: I8533e55a41bf7a1b69a46277798735bf314d2ef3

--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
@@ -161,4 +161,8 @@ public class BridgePowerManager implements IPowerManager {
         // pass for now
     }
 
+    @Override
+    public void updateBlockedUids(int uid, boolean isBlocked) {
+        // pass for now.
+    }
 }
