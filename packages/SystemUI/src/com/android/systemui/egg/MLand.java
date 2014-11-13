aed50cbe3c3dbe175dac695764df586d8dda14c9
Michael Bestas <mikeioannina@gmail.com>
2014-11-13 21:22:46 +0200

    Add CM PlatLogo (1/2)
    * Icons by: Björn Lundén & Asher Simonds
    
    Change-Id: Ie2b092bafd3588270177648003dafed8226cf298

--- packages/SystemUI/src/com/android/systemui/egg/MLand.java
+++ packages/SystemUI/src/com/android/systemui/egg/MLand.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2014-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,6 +210,8 @@ public class MLand extends FrameLayout {
 
         // we assume everything will be laid out left|top
         setLayoutDirection(LAYOUT_DIRECTION_LTR);
+
+        Player.eggPlayer = getEggPlayer();
 
         setupPlayers(DEFAULT_PLAYERS);
 
@@ -1007,7 +1010,7 @@ public class MLand extends FrameLayout {
         public void step(long t_ms, long dt_ms, float t, float dt);
     }
 
-    private static class Player extends ImageView implements GameView {
+    protected static class Player extends ImageView implements GameView {
         public float dv;
         public int color;
         private MLand mLand;
@@ -1016,6 +1019,8 @@ public class MLand extends FrameLayout {
         private boolean mAlive;
         private int mScore;
         private TextView mScoreField;
+
+        protected static int eggPlayer;
 
         private final int[] sColors = new int[] {
                 //0xFF78C557,
@@ -1088,7 +1093,7 @@ public class MLand extends FrameLayout {
         public Player(Context context) {
             super(context);
 
-            setBackgroundResource(R.drawable.android);
+            setBackgroundResource(eggPlayer);
             getBackground().setTintMode(PorterDuff.Mode.SRC_ATOP);
             color = sColors[(sNextColor++%sColors.length)];
             getBackground().setTint(color);
@@ -1437,5 +1442,9 @@ public class MLand extends FrameLayout {
             w = h = irand(PARAMS.STAR_SIZE_MIN, PARAMS.STAR_SIZE_MAX);
             v = z = 0;
         }
+    }
+
+    protected int getEggPlayer() {
+        return R.drawable.android;
     }
 }
