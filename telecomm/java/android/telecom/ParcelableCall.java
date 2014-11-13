f042c5296262f5c83a4f4d9621e9e4d7241c09d3
Danny Baumann <dannybaumann@web.de>
2014-11-13 14:28:53 +0100

    Proper supplementary service notification handling (1/5).
    Remove CAF's passing of implementation details from the base classes,
    and replace it by well-defined call properties.
    
    Change-Id: I4fa94d4ba68a1570d3f822be569ae124882c0e66

--- telecomm/java/android/telecom/ParcelableCall.java
+++ telecomm/java/android/telecom/ParcelableCall.java
@@ -54,8 +54,6 @@ public final class ParcelableCall implements Parcelable {
     private final int mVideoState;
     private final List<String> mConferenceableCallIds;
     private final Bundle mExtras;
-    private int mNotificationType;
-    private int mCode;
     boolean mIsActiveSub;
 
     public ParcelableCall(
@@ -79,8 +77,6 @@ public final class ParcelableCall implements Parcelable {
             int videoState,
             List<String> conferenceableCallIds,
             Bundle extras,
-            int notificationType,
-            int code,
             boolean isActiveSub) {
         mId = id;
         mState = state;
@@ -102,8 +98,6 @@ public final class ParcelableCall implements Parcelable {
         mVideoState = videoState;
         mConferenceableCallIds = Collections.unmodifiableList(conferenceableCallIds);
         mExtras = extras;
-        mNotificationType = notificationType;
-        mCode = code;
         mIsActiveSub = isActiveSub;
     }
 
@@ -241,14 +235,6 @@ public final class ParcelableCall implements Parcelable {
         return mExtras;
     }
 
-    public int getNotificationType() {
-        return mNotificationType;
-    }
-
-    public int getNotificationCode() {
-        return mCode;
-    }
-
     /** Responsible for creating ParcelableCall objects for deserialized Parcels. */
     public static final Parcelable.Creator<ParcelableCall> CREATOR =
             new Parcelable.Creator<ParcelableCall> () {
@@ -279,9 +265,7 @@ public final class ParcelableCall implements Parcelable {
             List<String> conferenceableCallIds = new ArrayList<>();
             source.readList(conferenceableCallIds, classLoader);
             Bundle extras = source.readParcelable(classLoader);
-            int notificationType = source.readInt();
-            int code = source.readInt();
-            boolean isActiveSub = (source.readInt() == 1) ? true : false;
+            boolean isActiveSub = source.readInt() == 1;
             return new ParcelableCall(
                     id,
                     state,
@@ -303,8 +287,6 @@ public final class ParcelableCall implements Parcelable {
                     videoState,
                     conferenceableCallIds,
                     extras,
-                    notificationType,
-                    code,
                     isActiveSub);
         }
 
@@ -344,8 +326,6 @@ public final class ParcelableCall implements Parcelable {
         destination.writeInt(mVideoState);
         destination.writeList(mConferenceableCallIds);
         destination.writeParcelable(mExtras, 0);
-        destination.writeInt(mNotificationType);
-        destination.writeInt(mCode);
         destination.writeInt(mIsActiveSub ? 1 : 0);
     }
 
