4cdfad4100af9be5b0562dbe67e9e3ffcc57fbc4
Danesh Mondegarian <daneshm90@gmail.com>
2014-11-14 09:44:50 -0800

    SystemUI : Make battery percentage configurable
    Change-Id: Ibfc0cf9c5a4efc13ecd3c3e99723bb2b717c50a7

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -22,16 +22,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
+import android.content.ContentResolver;
 
 import com.android.systemui.statusbar.policy.BatteryController;
 
@@ -151,6 +155,14 @@ public class BatteryMeterView extends View implements DemoMode,
 
     BatteryTracker mTracker = new BatteryTracker();
 
+    private ContentObserver mObserver = new ContentObserver(new Handler()) {
+        public void onChange(boolean selfChange, Uri uri) {
+            mShowPercent = ENABLE_PERCENT && 0 != Settings.System.getInt(
+                getContext().getContentResolver(), "status_bar_show_battery_percent", 0);
+            postInvalidate();
+        }
+    };
+
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
@@ -164,6 +176,8 @@ public class BatteryMeterView extends View implements DemoMode,
             mTracker.onReceive(getContext(), sticky);
         }
         mBatteryController.addStateChangedCallback(this);
+       getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+           "status_bar_show_battery_percent"), false, mObserver);
     }
 
     @Override
@@ -397,8 +411,8 @@ public class BatteryMeterView extends View implements DemoMode,
         boolean pctOpaque = false;
         float pctX = 0, pctY = 0;
         String pctText = null;
-        if (!tracker.plugged && level > mCriticalLevel && mShowPercent
-                && !(tracker.level == 100 && !SHOW_100_PERCENT)) {
+        if (!tracker.plugged && level > mCriticalLevel && (mShowPercent
+                || !(tracker.level == 100 && !SHOW_100_PERCENT))) {
             mTextPaint.setColor(getColorForLevel(level));
             mTextPaint.setTextSize(height *
                     (SINGLE_DIGIT_PERCENT ? 0.75f
