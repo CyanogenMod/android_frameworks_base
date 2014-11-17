c5f9d64c3e54afb165e68fc2cfcaa5f0618dfef3
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-11-17 11:22:10 -0500

    TaskStackView: Catch possible NPE when clearing tasks
    * 'Attempting to invoke virtual method ...  on a null object reference
    
    Change-Id: I00b631e2426d2a39539027288a1f471a6aea79ec
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -488,7 +488,9 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                 for (int i = 0; i < count; i++) {
                     Task t = tasks.get(i);
                     TaskView tv = getChildViewForTask(t);
-                    tv.dismissTask();
+                    try {
+                        tv.dismissTask();
+                    } catch (NullPointerException ignored{}
                 }
             }
         });
