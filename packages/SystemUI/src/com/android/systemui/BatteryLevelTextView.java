9eff2a04b541558bcef1910e63ce0b1ef06269df
Matt Garnes <matt@cyngn.com>
2014-11-17 16:56:16 -0800

    Add battery level text to status bar (1/2)
    1. Add TextView to display battery level percentage next to the battery
    icon.
    2. Remove support for displaying battery percentage on the actual
    battery icon itself.
    
    Change-Id: Iab6228f5316908961c8647773bd3c8655df29679

---
+++ packages/SystemUI/src/com/android/systemui/BatteryLevelTextView.java
@@ -0,0 +1,66 @@
+package com.android.systemui;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+import com.android.systemui.statusbar.policy.BatteryController;
+
+public class BatteryLevelTextView extends TextView implements
+        BatteryController.BatteryStateChangeCallback{
+    public static final String PERCENT = "%";
+    private BatteryController mBatteryController;
+    private boolean mShow;
+
+    private ContentObserver mObserver = new ContentObserver(new Handler()) {
+        public void onChange(boolean selfChange, Uri uri) {
+            loadShowBatteryTextSetting();
+            setVisibility(mShow ? View.VISIBLE : View.GONE);
+        }
+    };
+
+    public BatteryLevelTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        loadShowBatteryTextSetting();
+    }
+
+    private void loadShowBatteryTextSetting() {
+        mShow = 0 != Settings.System.getInt(
+            getContext().getContentResolver(), "status_bar_show_battery_percent", 0);
+    }
+
+    @Override
+    public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
+        setText(level + PERCENT);
+    }
+
+    public void setBatteryController(BatteryController batteryController) {
+        mBatteryController = batteryController;
+        mBatteryController.addStateChangedCallback(this);
+    }
+
+    @Override
+    public void onPowerSaveChanged() {
+        // Not used
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+           "status_bar_show_battery_percent"), false, mObserver);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        if (mBatteryController != null) {
+            mBatteryController.removeStateChangedCallback(this);
+        }
+    }
+}
