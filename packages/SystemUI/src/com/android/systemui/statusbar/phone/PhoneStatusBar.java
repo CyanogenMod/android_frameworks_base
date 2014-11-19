c1cf81fd942aff954903511d28e42a6409881825
Roman Birg <roman@cyngn.com>
2014-11-19 07:37:12 -0800

    SystemUI: fix status bar brightness slider eating events
    In some cases it would eat the event and not lose focus to the rest of
    the system.
    
    Change-Id: I94d532d09ea762edf7ac037043ed023caf42c401
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2736,19 +2736,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         if (mBrightnessChanged && upOrCancel) {
             mBrightnessChanged = false;
-            if (mJustPeeked) {
-                /**
-                 * if we were just peeking, eat the event and collapse the status bar, otherwise
-                 * the event gets passed down and a full expand might happen.
-                 */
+            if (mJustPeeked && mExpandedVisible) {
                 mNotificationPanel.fling(10, false);
-                if (mExpandedVisible) {
-                    mExpandedVisible = false;
-                    visibilityChanged(false);
-                    setInteracting(StatusBarManager.WINDOW_STATUS_BAR, false);
-                }
-                disable(mDisabledUnmodified, true /* animate */);
-                return true;
             }
         }
         return false;
