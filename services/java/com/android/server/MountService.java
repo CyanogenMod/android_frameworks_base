b0dc588d6def87ad878b9c6c8d61dfb3fd7e3d99
Scott Mertz <scott@cyngn.com>
2014-11-19 11:50:16 -0800

    Revert "UsbSettings: Add UMS mode support"
    This reverts commit 650a8551b1bc2e8381195808eb0877b1975c42ae.
    
    Change-Id: I98115eb08741a7e3a7e7c055bea224fbabf963e2

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -1570,6 +1570,9 @@ class MountService extends IMountService.Stub
         waitForReady();
         validatePermission(android.Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS);
 
+        final StorageVolume primary = getPrimaryPhysicalVolume();
+        if (primary == null) return;
+
         // TODO: Add support for multiple share methods
 
         for (String path : getShareableVolumes()) {
