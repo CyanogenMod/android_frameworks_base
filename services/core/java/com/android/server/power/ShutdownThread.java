3b7037541d562d4a4bce958126a3f646bbc69c01
Michael Bestas <mikeioannina@gmail.com>
2014-11-21 23:31:53 +0200

    Enhance power menu reboot option
    * Use our old reboot icon
    * Set correct dialog title & message when rebooting
    * Move strings to cm_strings
    * Add back reboot_download string used by samsung devices
    * Remove Chinese translations, will be handled through crowdin
    
    Change-Id: I3ba0e20dcdba0a295a071304048eebab6cfcf49d

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -349,8 +349,13 @@ public final class ShutdownThread extends Thread {
             // throw up an indeterminate system dialog to indicate radio is
             // shutting down.
             ProgressDialog pd = new ProgressDialog(context);
-            pd.setTitle(context.getText(com.android.internal.R.string.power_off));
-            pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
+            if (mReboot) {
+                pd.setTitle(context.getText(com.android.internal.R.string.reboot_title));
+                pd.setMessage(context.getText(com.android.internal.R.string.reboot_progress));
+            } else {
+                pd.setTitle(context.getText(com.android.internal.R.string.power_off));
+                pd.setMessage(context.getText(com.android.internal.R.string.shutdown_progress));
+            }
             pd.setIndeterminate(true);
             pd.setCancelable(false);
             pd.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
