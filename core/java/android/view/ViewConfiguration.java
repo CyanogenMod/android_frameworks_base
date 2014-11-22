b1393caa89a6959ba4238fa8c6173ded1c793ddb
fitsnugly <rickfitsnugly@gmail.com>
2014-11-22 17:27:56 -0500

    Always report physical menu key is not present
    Change-Id: I56693db0e25b458c7d2a7f05462f8f51be30726e

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -744,7 +744,7 @@ public class ViewConfiguration {
      * @return true if a permanent menu key is present, false otherwise.
      */
     public boolean hasPermanentMenuKey() {
-        return sHasPermanentMenuKey;
+        return false;
     }
 
     /**
