baa109f61fb5c44f8cf5f7d86e7bcaafd3c180f5
Marco Magliona <marco.magliona@gmail.com>
2014-11-27 01:19:00 +0100

    frameworks_base: fix SAMSUNG RIL compilation
    Bring back setMobileDataEnabled in ConnectivityManager which was deprecated in
    API version 21 (lollipop). This function is needed for Samsung RIL in order to compile
    and work since Google hasn't provided an alternative to it.
    
    Change-Id: Ie00ae896958da6ce9eec43c5e3e30fa212242c1f

--- core/java/android/net/IConnectivityManager.aidl
+++ core/java/android/net/IConnectivityManager.aidl
@@ -74,6 +74,8 @@ interface IConnectivityManager
     /** Policy control over specific {@link NetworkStateTracker}. */
     void setPolicyDataEnable(int networkType, boolean enabled);
 
+    void setMobileDataEnabled(boolean enabled);
+
     int tether(String iface);
 
     int untether(String iface);
--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -290,6 +290,12 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     private static final int EVENT_CHANGE_MOBILE_DATA_ENABLED = 2;
 
     /**
+     * used internally to set enable/disable cellular data
+     * arg1 = ENBALED or DISABLED
+     */
+    private static final int EVENT_SET_MOBILE_DATA = 7;
+
+    /**
      * used internally to clear a wakelock when transitioning
      * from one net to another.  Clear happens when we get a new
      * network - EVENT_EXPIRE_NET_TRANSITION_WAKELOCK happens
@@ -1336,6 +1342,32 @@ public class ConnectivityService extends IConnectivityManager.Stub {
         }
     };
 
+    /**
+     * @see ConnectivityManager#setMobileDataEnabled(boolean)
+     */
+    public void setMobileDataEnabled(boolean enabled) {
+        enforceChangePermission();
+        if (DBG) log("setMobileDataEnabled(" + enabled + ")");
+
+        mHandler.sendMessage(mHandler.obtainMessage(EVENT_SET_MOBILE_DATA,
+                (enabled ? ENABLED : DISABLED), 0));
+    }
+
+    private void handleSetMobileData(boolean enabled) {
+        if (mNetTrackers[ConnectivityManager.TYPE_MOBILE] != null) {
+            if (VDBG) {
+                log(mNetTrackers[ConnectivityManager.TYPE_MOBILE].toString() + enabled);
+            }
+            mNetTrackers[ConnectivityManager.TYPE_MOBILE].setUserDataEnable(enabled);
+        }
+        if (mNetTrackers[ConnectivityManager.TYPE_WIMAX] != null) {
+            if (VDBG) {
+                log(mNetTrackers[ConnectivityManager.TYPE_WIMAX].toString() + enabled);
+            }
+            mNetTrackers[ConnectivityManager.TYPE_WIMAX].setUserDataEnable(enabled);
+        }
+    }
+
     @Override
     public void setPolicyDataEnable(int networkType, boolean enabled) {
         // only someone like NPMS should only be calling us
@@ -2329,6 +2361,11 @@ public class ConnectivityService extends IConnectivityManager.Stub {
                 case EVENT_SEND_STICKY_BROADCAST_INTENT: {
                     Intent intent = (Intent)msg.obj;
                     sendStickyBroadcast(intent);
+                    break;
+                }
+                case EVENT_SET_MOBILE_DATA: {
+                    boolean enabled = (msg.arg1 == ENABLED);
+                    handleSetMobileData(enabled);
                     break;
                 }
                 case EVENT_SET_POLICY_DATA_ENABLE: {
