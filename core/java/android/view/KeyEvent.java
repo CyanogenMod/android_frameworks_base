0fc12abfefd558a8ff059b481f663dc9002c7310
yuri salpietro <yuri.salpietro@gmail.com>
2014-11-30 22:08:12 +0100

    enable wake up with home button
    plus
    
    frameworks/base/core/res/res/values/config.xml
    <!-- enable support screen off gesture -->
    <bool name="g_offscreenGestureSupport">false</bool>

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -1809,6 +1809,7 @@ public class KeyEvent extends InputEvent implements Parcelable {
             case KeyEvent.KEYCODE_SLEEP:
             case KeyEvent.KEYCODE_WAKEUP:
             case KeyEvent.KEYCODE_PAIRING:
+            case KeyEvent.KEYCODE_HOME:
                 return true;
         }
         return false;
