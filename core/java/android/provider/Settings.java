473ace1f82d27a39c42e0e0c35c95974849b30c5
Diogo Ferreira <defer@cyngn.com>
2014-12-01 17:17:21 +0000

    Recents: Port "Development shortcut" (2/2)
    This patch includes the equivalent of our Development Shortcuts in
    CM11 recents. Recents is very different now and this is mostly a rewrite.
    
    The semantics changed a bit as well. The current code already has special
    developer behavior when long-pressing the app icon and that is also where
    the developer shortcuts are hooked. If the app icon is long-pressed and
    developer shortcuts are active, a menu will be shown with the several available
    options.
    
    Fixes CML-61
    
    Change-Id: Ifdeaf83cf8db0f41ea38a9e8e926109c569b091a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6607,6 +6607,14 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         * Whether to display a menu containing 'Wipe data', 'Force close' and other options
+         * in the notification area and in the recent app list
+         * @hide
+         */
+        public static final String DEVELOPMENT_SHORTCUT = "development_shortcut";
+
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -6664,6 +6672,8 @@ public final class Settings {
             DOUBLE_TAP_TO_WAKE,
             ADVANCED_REBOOT,
             CAMERA_GESTURE_DISABLED,
+            ADVANCED_REBOOT,
+            DEVELOPMENT_SHORTCUT
         };
 
         /**
