4fe4c9392ff4ade7e046a2fd3e2da8e469c01f67
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-12-01 22:08:24 +0200

    SystemUI: Add option to disable search bar in recents (2/2)
    Coded from scratch by yashade2001.
    Firstly written for Project D.I.S.C.O. ROM.
    Thanks to TheMrcool212 for his help.
    
    Change-Id: I3f985aab873d05350c8301bdedae5eeee49f70c7

--- packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
@@ -65,7 +65,7 @@ public class RecentsConfiguration {
 
     /** Search bar */
     int searchBarAppWidgetId = -1;
-    public int searchBarSpaceHeightPx;
+    public static int searchBarSpaceHeightPx;
 
     /** Task stack */
     public int taskStackScrollDuration;
@@ -198,7 +198,6 @@ public class RecentsConfiguration {
         maxNumTasksToLoad = ActivityManager.getMaxRecentTasksStatic();
 
         // Search Bar
-        searchBarSpaceHeightPx = res.getDimensionPixelSize(R.dimen.recents_search_bar_space_height);
         searchBarAppWidgetId = settings.getInt(Constants.Values.App.Key_SearchAppWidgetId, -1);
 
         // Task stack
