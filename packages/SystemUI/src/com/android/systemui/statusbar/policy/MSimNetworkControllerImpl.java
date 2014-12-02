6bd91640ee82a8481b54e4a5c48150d84239fbd2
Adnan <adnan@cyngn.com>
2014-12-02 15:25:25 -0800

    SystemUI: Allow showing data type with roaming indicator on MSIM.
      If a device sets a system property "ro.config.always_show_roaming"
      the roaming indicator will not replace the data type icon, but create
      a new indicator alongside signaling that the sim is roaming.
    
    Change-Id: I60ac97572b9b50e4809dbd809a8e5a2d0763542e

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
@@ -86,6 +86,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
     int[] mMSimDataDirectionIconId; // data + data direction on phones
     int[] mMSimDataSignalIconId;
     int[] mMSimDataTypeIconId;
+    int[] mMSimDataRoamIconId;
     int[] mNoMSimIconId;
     int[] mMSimMobileActivityIconId; // overlay arrows for data direction
 
@@ -113,7 +114,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
         void setWifiIndicators(boolean visible, int strengthIcon, int activityIcon,
                 String contentDescription);
         void setMobileDataIndicators(boolean visible, int strengthIcon, int activityIcon,
-                int typeIcon, String contentDescription, String typeContentDescription,
+                int typeIcon, int roamingIcon, String contentDescription, String typeContentDescription,
                 int phoneId, int noSimIcon);
         void setIsAirplaneMode(boolean is, int airplaneIcon);
     }
@@ -132,6 +133,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
         mMSimIconId = new int[numPhones];
         mMSimPhoneSignalIconId = new int[numPhones];
         mMSimDataTypeIconId = new int[numPhones];
+        mMSimDataRoamIconId = new int[numPhones];
         mNoMSimIconId = new int[numPhones];
         mMSimMobileActivityIconId = new int[numPhones];
         mMSimContentDescriptionPhoneSignal = new String[numPhones];
@@ -299,6 +301,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                 mMSimPhoneSignalIconId[phoneId],
                 mMSimMobileActivityIconId[phoneId],
                 mMSimDataTypeIconId[phoneId],
+                mMSimDataRoamIconId[phoneId],
                 mMSimContentDescriptionPhoneSignal[phoneId],
                 mMSimContentDescriptionDataType[phoneId],
                 phoneId,
@@ -310,6 +313,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                     mAlwaysShowCdmaRssi ? mPhoneSignalIconId : mWimaxIconId,
                     mMSimMobileActivityIconId[phoneId],
                     mMSimDataTypeIconId[phoneId],
+                    mMSimDataRoamIconId[phoneId],
                     mContentDescriptionWimax,
                     mMSimContentDescriptionDataType[phoneId],
                     phoneId,
@@ -322,6 +326,7 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                         : mMSimDataSignalIconId[phoneId],
                     mMSimMobileActivityIconId[phoneId],
                     mMSimDataTypeIconId[phoneId],
+                    mMSimDataRoamIconId[phoneId],
                     mMSimContentDescriptionPhoneSignal[phoneId],
                     mMSimContentDescriptionDataType[phoneId],
                     phoneId,
@@ -816,11 +821,17 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                 }
             }
         } else if (isRoaming(phoneId)) {
-            mMSimDataTypeIconId[phoneId] = R.drawable.stat_sys_data_fully_connected_roam;
+            if (SystemProperties.getBoolean("ro.config.always_show_roaming", false)) {
+                mMSimDataRoamIconId[phoneId] = R.drawable.stat_sys_data_msim_roam;
+            } else {
+                mMSimDataTypeIconId[phoneId] = R.drawable.stat_sys_data_fully_connected_roam;
+            }
             setQSDataTypeIcon = true;
             if (phoneId == dataSub) {
                 mQSDataTypeIconId = R.drawable.stat_sys_data_fully_connected_roam;
             }
+        } else if (!isRoaming(phoneId)) {
+            mMSimDataRoamIconId[phoneId] = 0;
         }
 
         if (setQSDataTypeIcon && phoneId == dataSub) {
@@ -1200,10 +1211,16 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                     }
                 }
             } else if (isRoaming(phoneId)) {
-                mMSimDataTypeIconId[phoneId] = R.drawable.stat_sys_data_fully_connected_roam;
+                if (SystemProperties.getBoolean("ro.config.always_show_roaming", false)) {
+                    mMSimDataRoamIconId[phoneId] = R.drawable.stat_sys_data_msim_roam;
+                } else {
+                    mMSimDataTypeIconId[phoneId] = R.drawable.stat_sys_data_fully_connected_roam;
+                }
                 if (phoneId == dataSub) {
                     mQSDataTypeIconId = R.drawable.stat_sys_data_fully_connected_roam;
                 }
+            } else if (!isRoaming(phoneId)) {
+                mMSimDataRoamIconId[phoneId] = 0;
             }
         }
 
