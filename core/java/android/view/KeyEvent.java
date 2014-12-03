d58db0d7401d6e4996a72678b07c2685c2762425
Martin Brabham <optedoblivion@cyngn.com>
2014-12-03 11:48:28 -0800

    Android Policy: handle volume key event as wake key when preference is set
    Change-Id: If9a61cd65553bf00f0efda1a75b1ab75b9129090

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -1831,6 +1831,9 @@ public class KeyEvent extends InputEvent implements Parcelable {
             case KeyEvent.KEYCODE_HOME:
             case KeyEvent.KEYCODE_WAKEUP:
             case KeyEvent.KEYCODE_PAIRING:
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case KeyEvent.KEYCODE_VOLUME_MUTE:
                 return true;
         }
         return false;
