539bc6197e1406338ef9254dad0da5efa48c1f82
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    systemui: Forward-port notification counters
     * Port from CM11
    
    Change-Id: Id9d0954acffd00bdb470b9eccbdb3b45fbd995c1

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2301,6 +2301,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.DEV_FORCE_SHOW_NAVBAR,
                     R.integer.def_force_disable_navkeys);
+
+            loadIntegerSetting(stmt, Settings.System.STATUS_BAR_NOTIF_COUNT,
+                    R.integer.def_notif_count);
+
         } finally {
             if (stmt != null) stmt.close();
         }
