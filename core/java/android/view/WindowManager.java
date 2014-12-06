f1a5167d0c5ef7a7db8be15d5c43937418aa2b1f
Byunghun Jeon <bjeon@codeaurora.org>
2014-12-05 18:44:11 -0800

    WindowManager: Upper level changes to expose blur effect
    Upper level changes to expose blur-behind and blur-mask effect
    
    Change-Id: I6d37b43888c8c5e028974bd714596d8178cb5114
    
    WindowManager: Adding template for BlurLayer file
    
    Cyanogen changes:
      * Use config.xml instead of settings
      * Don't break stuff if not supported in SF
      * Incremental fade for insecure lockscreen
      * Disabled extra effects for now
    
    Change-Id: Icefe452f7a015656661e8543849c9b88889dbb40
    
    SystemUI: hide blur when showing keyguard media
    
    It's still showing up behind eating up cycles
    
    Change-Id: I8b1a0f2cfafcc1dcf2a9a472fc15b987e4a3992b
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1134,6 +1134,19 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
 
         /**
+         * Window flag: adding additional blur layer and set this as masking layer
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING = 0x40000000;
+
+        /**
+         * Window flag: adding additional blur layer and set this as masking layer.
+         * This is faster and ugglier than non-scaled version.
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING_SCALED = 0x80000000;
+
+        /**
          * Control flags that are private to the platform.
          * @hide
          */
@@ -1587,6 +1600,14 @@ public interface WindowManager extends ViewManager {
          */
         public long userActivityTimeout = -1;
 
+        /**
+         * Threshold value that blur masking layer uses to determine whether
+         * to use or discard the blurred color.
+         * Value should be between 0.0 and 1.0
+         * @hide
+         */
+        public float blurMaskAlphaThreshold = 0.0f;
+
         public LayoutParams() {
             super(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
             type = TYPE_APPLICATION;
@@ -1693,6 +1714,7 @@ public interface WindowManager extends ViewManager {
             out.writeInt(surfaceInsets.bottom);
             out.writeInt(hasManualSurfaceInsets ? 1 : 0);
             out.writeInt(needsMenuKey);
+            out.writeFloat(blurMaskAlphaThreshold);
         }
 
         public static final Parcelable.Creator<LayoutParams> CREATOR
@@ -1743,6 +1765,7 @@ public interface WindowManager extends ViewManager {
             surfaceInsets.bottom = in.readInt();
             hasManualSurfaceInsets = in.readInt() != 0;
             needsMenuKey = in.readInt();
+            blurMaskAlphaThreshold = in.readFloat();
         }
 
         @SuppressWarnings({"PointlessBitwiseExpression"})
@@ -1781,6 +1804,8 @@ public interface WindowManager extends ViewManager {
         public static final int NEEDS_MENU_KEY_CHANGED = 1 << 22;
         /** {@hide} */
         public static final int PREFERRED_DISPLAY_MODE_ID = 1 << 23;
+        /** {@hide} */
+        public static final int BLUR_MASK_ALPHA_THRESHOLD_CHANGED = 1 << 30;
         /** {@hide} */
         public static final int EVERYTHING_CHANGED = 0xffffffff;
 
@@ -1939,6 +1964,11 @@ public interface WindowManager extends ViewManager {
             if (needsMenuKey != o.needsMenuKey) {
                 needsMenuKey = o.needsMenuKey;
                 changes |= NEEDS_MENU_KEY_CHANGED;
+            }
+
+            if (blurMaskAlphaThreshold != o.blurMaskAlphaThreshold) {
+                blurMaskAlphaThreshold = o.blurMaskAlphaThreshold;
+                changes |= BLUR_MASK_ALPHA_THRESHOLD_CHANGED;
             }
 
             return changes;
