5c182ed0defe9511f0be83cecc3459dca4cdcaec
Steve Kondik <steve@cyngn.com>
2014-12-05 18:44:11 -0800

    windowmanager: Add support for blur effects
      * Upper level changes to expose blur-behind and blur-mask effect
      * Support for blur layer on the lockscreen
    
      * Original work by CodeAurora / Qualcomm
      * Contributions from:
        - Steve Kondik
        - Danesh Mondegarian
        - Roman Birg
        - Clark Scheff
        - Michael Bestas
    
    Change-Id: I6285905a4e3c00dfb5471876d0cfb16b4b4e9893

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -239,6 +239,7 @@ public interface WindowManager extends ViewManager {
          * @see #TYPE_SYSTEM_ERROR
          * @see #TYPE_INPUT_METHOD
          * @see #TYPE_INPUT_METHOD_DIALOG
+         * @see #TYPE_KEYGUARD_PANEL
          */
         @ViewDebug.ExportedProperty(mapping = {
             @ViewDebug.IntToString(from = TYPE_BASE_APPLICATION, to = "TYPE_BASE_APPLICATION"),
@@ -281,7 +282,8 @@ public interface WindowManager extends ViewManager {
             @ViewDebug.IntToString(from = TYPE_VOICE_INTERACTION_STARTING, to = "TYPE_VOICE_INTERACTION_STARTING"),
             @ViewDebug.IntToString(from = TYPE_DOCK_DIVIDER, to = "TYPE_DOCK_DIVIDER"),
             @ViewDebug.IntToString(from = TYPE_QS_DIALOG, to = "TYPE_QS_DIALOG"),
-            @ViewDebug.IntToString(from = TYPE_SCREENSHOT, to = "TYPE_SCREENSHOT")
+            @ViewDebug.IntToString(from = TYPE_SCREENSHOT, to = "TYPE_SCREENSHOT"),
+            @ViewDebug.IntToString(from = TYPE_KEYGUARD_PANEL, to = "TYPE_KEYGUARD_PANEL"),
         })
         public int type;
 
@@ -640,6 +642,13 @@ public interface WindowManager extends ViewManager {
          * @hide
          */
         public static final int TYPE_SCREENSHOT = FIRST_SYSTEM_WINDOW + 36;
+
+        /**
+         * Window type: Windows that are layered within the keyguard
+         * This type is LAST_SYSTEM_WINDOW-1 to avoid future conflicts with AOSP
+         * @hide
+         */
+        public static final int TYPE_KEYGUARD_PANEL = FIRST_SYSTEM_WINDOW+998;
 
         /**
          * End of types of system windows.
@@ -1260,6 +1269,19 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
 
         /**
+         * Window flag: adding additional blur layer and set this as masking layer
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING = 0x40000000;
+
+        /**
+         * Window flag: adding additional blur layer and set this as masking layer.
+         * This is faster and ugglier than non-scaled version.
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING_SCALED = 0x80000000;
+
+        /**
          * Control flags that are private to the platform.
          * @hide
          */
@@ -1531,6 +1553,15 @@ public interface WindowManager extends ViewManager {
         public float dimAmount = 1.0f;
 
         /**
+         * When {@link #FLAG_BLUR_BEHIND} is set, this is the amount of blur
+         * to apply.  Range is from 1.0 for maximum to 0.0 for no
+         * blur.
+         * @hide
+         */
+        public float blurAmount = 1.0f;
+
+
+        /**
          * Default value for {@link #screenBrightness} and {@link #buttonBrightness}
          * indicating that the brightness value is not overridden for this window
          * and normal brightness policy should be used.
@@ -1749,6 +1780,14 @@ public interface WindowManager extends ViewManager {
          */
         public long removeTimeoutMilliseconds = -1;
 
+        /**
+         * Threshold value that blur masking layer uses to determine whether
+         * to use or discard the blurred color.
+         * Value should be between 0.0 and 1.0
+         * @hide
+         */
+        public float blurMaskAlphaThreshold = 0.0f;
+
         public LayoutParams() {
             super(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
             type = TYPE_APPLICATION;
@@ -1860,6 +1899,7 @@ public interface WindowManager extends ViewManager {
             out.writeInt(windowAnimations);
             out.writeFloat(alpha);
             out.writeFloat(dimAmount);
+            out.writeFloat(blurAmount);
             out.writeFloat(screenBrightness);
             out.writeFloat(buttonBrightness);
             out.writeInt(rotationAnimation);
@@ -1884,6 +1924,7 @@ public interface WindowManager extends ViewManager {
             out.writeInt(accessibilityIdOfAnchor);
             TextUtils.writeToParcel(accessibilityTitle, out, parcelableFlags);
             out.writeLong(removeTimeoutMilliseconds);
+            out.writeFloat(blurMaskAlphaThreshold);
         }
 
         public static final Parcelable.Creator<LayoutParams> CREATOR
@@ -1914,6 +1955,7 @@ public interface WindowManager extends ViewManager {
             windowAnimations = in.readInt();
             alpha = in.readFloat();
             dimAmount = in.readFloat();
+            blurAmount = in.readFloat();
             screenBrightness = in.readFloat();
             buttonBrightness = in.readFloat();
             rotationAnimation = in.readInt();
@@ -1938,6 +1980,7 @@ public interface WindowManager extends ViewManager {
             accessibilityIdOfAnchor = in.readInt();
             accessibilityTitle = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(in);
             removeTimeoutMilliseconds = in.readLong();
+            blurMaskAlphaThreshold = in.readFloat();
         }
 
         @SuppressWarnings({"PointlessBitwiseExpression"})
@@ -1980,6 +2023,10 @@ public interface WindowManager extends ViewManager {
         public static final int ACCESSIBILITY_ANCHOR_CHANGED = 1 << 24;
         /** {@hide} */
         public static final int ACCESSIBILITY_TITLE_CHANGED = 1 << 25;
+        /** {@hide} */
+        public static final int BLUR_AMOUNT_CHANGED = 1 << 29;
+        /** {@hide} */
+        public static final int BLUR_MASK_ALPHA_THRESHOLD_CHANGED = 1 << 30;
         /** {@hide} */
         public static final int EVERYTHING_CHANGED = 0xffffffff;
 
@@ -2076,6 +2123,10 @@ public interface WindowManager extends ViewManager {
                 dimAmount = o.dimAmount;
                 changes |= DIM_AMOUNT_CHANGED;
             }
+            if (blurAmount != o.blurAmount) {
+                blurAmount = o.blurAmount;
+                changes |= BLUR_AMOUNT_CHANGED;
+            }
             if (screenBrightness != o.screenBrightness) {
                 screenBrightness = o.screenBrightness;
                 changes |= SCREEN_BRIGHTNESS_CHANGED;
@@ -2160,6 +2211,11 @@ public interface WindowManager extends ViewManager {
 
             // This can't change, it's only set at window creation time.
             removeTimeoutMilliseconds = o.removeTimeoutMilliseconds;
+
+            if (blurMaskAlphaThreshold != o.blurMaskAlphaThreshold) {
+                blurMaskAlphaThreshold = o.blurMaskAlphaThreshold;
+                changes |= BLUR_MASK_ALPHA_THRESHOLD_CHANGED;
+            }
 
             return changes;
         }
