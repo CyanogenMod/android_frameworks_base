30bea51c3bc41fd90d7cd4d5020c913c43900fde
Alex Cruz <mazdarider23@gmail.com>
2014-12-06 17:19:23 +0100

    FWB: Network traffic supercharged (1/2)
    Bring back AICP kk traffic
    
    Change-Id: Ide3ac899da7632dd1fb3a0615ab9560a4ae6cfd0
    
    temasek: Cherry-picked colors only.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -179,6 +179,9 @@ public class NetworkTraffic extends TextView {
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.NETWORK_TRAFFIC_COLOR), false,
+                    this, UserHandle.USER_ALL);
         }
 
         /*
@@ -268,6 +271,20 @@ public class NetworkTraffic extends TextView {
                 UserHandle.USER_CURRENT);
 
         mState = Settings.System.getInt(resolver, Settings.System.NETWORK_TRAFFIC_STATE, 0);
+
+	    int defaultColor = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_COLOR, 0xFFFFFFFF);
+
+	    int mNetworkTrafficColor = Settings.System.getInt(resolver,
+                Settings.System.NETWORK_TRAFFIC_COLOR, -2);
+
+	    if (mNetworkTrafficColor == Integer.MIN_VALUE
+                || mNetworkTrafficColor == -2) {
+            mNetworkTrafficColor = defaultColor;
+        }
+
+	    setTextColor(mNetworkTrafficColor);
+
         if (isSet(mState, MASK_UNIT)) {
             KB = KILOBYTE;
         } else {
