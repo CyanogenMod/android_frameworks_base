947dea1eeaa66f5e41006bba95776a7b8975a87a
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-07 13:51:00 +0200

    frameworks/base: Don't report existing menu key if it was remapped.
    Change-Id: Iabad06cd42cebecdff6f0440899432513681b063

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -11096,6 +11096,11 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.hasNavigationBar();
     }
 
+    @Override
+    public boolean hasPermanentMenuKey() {
+        return mPolicy.hasPermanentMenuKey();
+    }
+
     @Override 
     public boolean needsNavigationBar() {
         return mPolicy.needsNavigationBar();
