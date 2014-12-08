c29e4b193ff2003cca562aede624a747e38a87b1
willl03 <wgangers@gmail.com>
2014-12-08 11:13:28 -0500

    Only go HOME if screen is fully awake
    Avoid going home when hardware home button is used to wake the device on an insecure keyguard
    
    Change-Id: I5d5d8c4fff76967c29e70251f7b165205005ba11

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3835,8 +3835,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
             sendCloseSystemWindows(SYSTEM_DIALOG_REASON_HOME_KEY);
             hideRecentApps(false, true);
-        } else {
-            // Otherwise, just launch Home
+        } else if (mScreenOnFully) {
+            // check if screen is fully on before going home
+            // to avoid hardware home button wake going home
             sendCloseSystemWindows(SYSTEM_DIALOG_REASON_HOME_KEY);
             startDockOrHome(true /*fromHomeKey*/, awakenFromDreams);
         }
