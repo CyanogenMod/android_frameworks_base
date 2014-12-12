639cb66db35cd8fcbb9aacb6482fd012e1f3d9a0
Danny Baumann <dannybaumann@web.de>
2014-12-12 10:11:34 +0100

    Update color dependent items when reusing notifications.
    The notification might have changed its color setting.
    
    Change-Id: Icf494db47b30af5505107eec2f728fb8bcba421a
    (cherry picked from commit 97a001be48231153b352437d2f7076beb374e7ad)

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2025,6 +2025,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         } else {
             entry.row.setOnClickListener(null);
         }
+        applyColorsAndBackgrounds(notification, entry);
         entry.row.setStatusBarNotification(notification);
         entry.row.notifyContentUpdated();
         entry.row.resetHeight();
