6c03bf37ba1728fe862eae09122aa3df1c091bab
Diogo Ferreira <defer@cyngn.com>
2014-12-15 12:07:42 +0000

    appops: Add support for SU
    This patch adds support for superuser app-ops control. The end-goal
    is to better prepare superuser for the multi-user experience while
    also replacing the superuser app with existing components.
    
    Change-Id: I52acf98a39f18f12a23f4f902a39ae3f7c5b4895

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -245,7 +245,9 @@ public class AppOpsManager {
     /** @hide */
     public static final int OP_ALARM_WAKEUP = 60;
     /** @hide */
-    public static final int _NUM_OP = 61;
+    public static final int OP_SU = 61;
+    /** @hide */
+    public static final int _NUM_OP = 62;
 
     /** Access to coarse location information. */
     public static final String OPSTR_COARSE_LOCATION =
@@ -376,6 +378,8 @@ public class AppOpsManager {
             "android:data_connect_change";
     private static final String OPSTR_ALARM_WAKEUP =
             "android:alarm_wakeup";
+    private static final String OPSTR_SU =
+            "android:su";
 
     /**
      * This maps each operation to the operation that serves as the
@@ -447,6 +451,7 @@ public class AppOpsManager {
             OP_DELETE_CALL_LOG,
             OP_DATA_CONNECT_CHANGE,
             OP_ALARM_WAKEUP,
+            OP_SU
     };
 
     /**
@@ -515,6 +520,7 @@ public class AppOpsManager {
             null,
             null,
             null,
+            OPSTR_SU,
     };
 
     /**
@@ -583,6 +589,7 @@ public class AppOpsManager {
         OPSTR_DELETE_CALL_LOG,
         OPSTR_DATA_CONNECT_CHANGE,
         OPSTR_ALARM_WAKEUP,
+        OPSTR_SU,
     };
 
     /**
@@ -651,6 +658,7 @@ public class AppOpsManager {
             "DELETE_CALL_LOG",
             "DATA_CONNECT_CHANGE",
             "ALARM_WAKEUP",
+            "SU",
     };
 
     /**
@@ -719,6 +727,7 @@ public class AppOpsManager {
             android.Manifest.permission.WRITE_CALL_LOG,
             android.Manifest.permission.MODIFY_PHONE_STATE,
             null, // OP_ALARM_WAKEUP
+            null,
     };
 
     /**
@@ -788,6 +797,7 @@ public class AppOpsManager {
             null, //DELETE_CALL_LOG
             null, //DATA_CONNECT_CHANGE
             null, //ALARM_WAKEUP
+            UserManager.DISALLOW_SU, //SU TODO: this should really be investigated.
     };
 
     /**
@@ -856,6 +866,7 @@ public class AppOpsManager {
             false, //DELETE_CALL_LOG
             false, //DATA_CONNECT_CHANGE
             true, //ALARM_WAKEUP
+            false, //SU
     };
 
     /**
@@ -923,6 +934,7 @@ public class AppOpsManager {
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_ALLOWED,
             AppOpsManager.MODE_ALLOWED, // OP_ALARM_WAKEUP
+            AppOpsManager.MODE_ASK, // OP_SU
     };
 
     /**
@@ -991,6 +1003,7 @@ public class AppOpsManager {
             AppOpsManager.MODE_ASK,     // OP_DELETE_CALL_LOG
             AppOpsManager.MODE_ASK,     // OP_DATA_CONNECT_CHANGE
             AppOpsManager.MODE_ALLOWED, // OP_ALARM_WAKEUP
+            AppOpsManager.MODE_ASK,     // OP_SU
     };
 
     /**
@@ -1058,6 +1071,7 @@ public class AppOpsManager {
         true,     // OP_DELETE_CALL_LOG
         true,     // OP_DATA_CONNECT_CHANGE
         false,    // OP_ALARM_WAKEUP
+        true,     // OP_SU
     };
 
     /**
@@ -1129,6 +1143,7 @@ public class AppOpsManager {
             false,     // OP_DELETE_CALL_LOG
             false,     // OP_DATA_CONNECT_CHANGE
             false,     // OP_ALARM_WAKEUP
+            false,     // OP_SU
     };
 
     private static HashMap<String, Integer> sOpStrToOp = new HashMap<String, Integer>();
