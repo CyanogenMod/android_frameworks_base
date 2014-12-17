6ba7b2c4c8bcdc6320f5d258cbd3f0cefaff66a8
Diogo Ferreira <defer@cyngn.com>
2014-12-17 17:16:16 +0000

    su: Add an indicator when a 'su' session is active
    This adds an indicator to the statusbar that is visible whenever a root
    session is active, akin to the fine location icon.
    
    Change-Id: I669fcc87e2b7a3201308170d7727ec5fb0e00e2b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -180,6 +180,7 @@ import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
 import com.android.systemui.statusbar.policy.RotationLockControllerImpl;
 import com.android.systemui.statusbar.policy.SecurityControllerImpl;
+import com.android.systemui.statusbar.policy.SuControllerImpl;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.WeatherControllerImpl;
@@ -279,6 +280,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     AccessibilityController mAccessibilityController;
     MSimNetworkControllerImpl mMSimNetworkController;
     WeatherControllerImpl mWeatherController;
+    SuControllerImpl mSuController;
 
     int mNaturalBarHeight = -1;
     int mIconSize = -1;
@@ -771,7 +773,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         // Lastly, call to the icon policy to install/update all the icons.
-        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController);
+        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController, mSuController);
         mSettingsObserver.onChange(false); // set up
 
         mHeadsUpObserver.onChange(true); // set up
@@ -1012,6 +1014,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mVolumeComponent = getComponent(VolumeComponent.class);
         mZenModeController = mVolumeComponent.getZenController();
         mCastController = new CastControllerImpl(mContext);
+        mSuController = new SuControllerImpl(mContext);
 
         if (isMSim()) {
             mMSimNetworkController = new MSimNetworkControllerImpl(mContext);
--- services/core/java/com/android/server/AppOpsService.java
+++ services/core/java/com/android/server/AppOpsService.java
@@ -36,6 +36,7 @@ import android.app.ActivityThread;
 import android.app.AppOpsManager;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
@@ -223,6 +224,13 @@ public class AppOpsService extends IAppOpsService.Stub {
                     finishOperationLocked(mStartedOps.get(i));
                 }
                 mClients.remove(mAppToken);
+            }
+
+            // We cannot broadcast on the synchronized block above because the broadcast might
+            // trigger another appop call that eventually arrives here from a different thread,
+            // causing a deadlock.
+            for (int i=mStartedOps.size()-1; i>=0; i--) {
+                broadcastOpIfNeeded(mStartedOps.get(i).op);
             }
         }
     }
@@ -715,6 +723,7 @@ public class AppOpsService extends IAppOpsService.Stub {
                 op.time = System.currentTimeMillis();
                 op.rejectTime = 0;
                 op.allowedCount++;
+                broadcastOpIfNeeded(code);
                 return AppOpsManager.MODE_ALLOWED;
             } else {
                 if (Looper.myLooper() == mLooper) {
@@ -731,7 +740,10 @@ public class AppOpsService extends IAppOpsService.Stub {
                 req = askOperationLocked(code, uid, packageName, switchOp);
             }
         }
-        return req.get();
+
+        int result = req.get();
+        broadcastOpIfNeeded(code);
+        return result;
     }
 
     @Override
@@ -779,6 +791,7 @@ public class AppOpsService extends IAppOpsService.Stub {
                 if (client.mStartedOps != null) {
                     client.mStartedOps.add(op);
                 }
+                broadcastOpIfNeeded(code);
                 return AppOpsManager.MODE_ALLOWED;
             } else {
                 if (Looper.myLooper() == mLooper) {
@@ -797,7 +810,9 @@ public class AppOpsService extends IAppOpsService.Stub {
                 req = askOperationLocked(code, uid, packageName, switchOp);
             }
         }
-        return req.get();
+        int result = req.get();
+        broadcastOpIfNeeded(code);
+        return result;
     }
 
     @Override
@@ -818,6 +833,7 @@ public class AppOpsService extends IAppOpsService.Stub {
             }
             finishOperationLocked(op);
         }
+        broadcastOpIfNeeded(code);
     }
 
     void finishOperationLocked(Op op) {
@@ -1584,6 +1600,16 @@ public class AppOpsService extends IAppOpsService.Stub {
                 } catch (RemoteException e) {
                 }
             }
+        }
+    }
+
+    private void broadcastOpIfNeeded(int op) {
+        switch (op) {
+            case AppOpsManager.OP_SU:
+                mContext.sendBroadcast(new Intent(AppOpsManager.ACTION_SU_SESSION_CHANGED));
+                break;
+            default:
+                break;
         }
     }
 
