b21dcd64b12533515142c5d65288bae51526aa00
Austin T. Conn <atc3030dev@gmail.com>
2014-12-17 13:28:00 -0500

    Settings: Forward Port Volume Long Press Skip Tracks (1/2)
    Bring Forward Volume Rocker Track Change
    
    Requires Settings Change
    http://review.cyanogenmod.org/#/c/82222/
    
    Change-Id: I70b00e08fe48707110725dd1daa56b1e48d15a03

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2197,6 +2197,12 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
+
+        /**
          * Microphone mute (int 1 = mute, 0 = not muted).
          *
          * @hide
