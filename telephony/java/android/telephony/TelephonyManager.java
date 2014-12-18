235840df4a5d631669a5b1296f1eb79c6a1437c1
Libin.Tang@motorola.com <Libin.Tang@motorola.com>
2014-12-17 22:47:45 -0600

    IMS: add the api to get IMS registration information
    Conflicts:
    	telephony/java/android/telephony/TelephonyManager.java
    
    Bug: 18668325
    Change-Id: Ie694c7f1cc12a573cbef2815199ae6c91cf8088e

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3952,6 +3952,20 @@ public class TelephonyManager {
     }
 
    /**
+    * Returns the IMS Registration Status
+    *@hide
+    */
+   public boolean isImsRegistered() {
+       try {
+           return getITelephony().isImsRegistered();
+       } catch (RemoteException ex) {
+           return false;
+       } catch (NullPointerException ex) {
+           return false;
+       }
+   }
+
+   /**
     * Set TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC for the default phone.
     *
     * @hide
