74a32bc1b1c2b61bf7f3ca25159484f539741a8c
Adnan Begovic <adnan@cyngn.com>
2014-12-18 15:30:33 -0800

    Add global scramble pin layout setting.
    Change-Id: I87e6baf041644cdde55fef34f4032c6950ac428f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1962,6 +1962,12 @@ public final class Settings {
         public static final String LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED =
             "lock_pattern_tactile_feedback_enabled";
 
+        /**
+         * Whether to scramble a pin unlock layout
+         * @hide
+         */
+        public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT =
+                "lockscreen_scramble_pin_layout";
 
         /**
          * A formatted string of the next alarm that is set, or the empty string
