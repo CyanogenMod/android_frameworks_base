2229a78d5e62a10f26a88e16afd9cbca7c698dbd
Dave Kessler <activethrasher00@gmail.com>
2014-12-19 18:21:01 -0500

    Power menu customizations [1/2]
    power
    reboot
    airplane
    sound
    profile switcher
    user switcher
    device lockdown
    settings shortcut
    bug reports
    
    [mikeioannina]: Modify for CyanogenMod
    
    Change-Id: I6f1ef036a5495ef49b9520c63df5ce322991cd08

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -165,10 +165,19 @@ public final class ShutdownThread extends Thread {
         }
 
         boolean showRebootOption = false;
-        String[] defaultActions = context.getResources().getStringArray(
-                com.android.internal.R.array.config_globalActionsList);
-        for (int i = 0; i < defaultActions.length; i++) {
-            if (defaultActions[i].equals("reboot")) {
+
+        String[] actionsArray;
+        String actions = Settings.Global.getStringForUser(context.getContentResolver(),
+                Settings.Global.POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+        if (actions == null) {
+            actionsArray = context.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList);
+        } else {
+            actionsArray = actions.split("\\|");
+        }
+
+        for (int i = 0; i < actionsArray.length; i++) {
+            if (actionsArray[i].equals("reboot")) {
                 showRebootOption = true;
                 break;
             }
