2497aa9e16d9aefd937a9b5a13ea1ed28d4a28cb
jrior001 <jriordan001@gmail.com>
2014-12-23 12:36:22 -0500

    Wake Keys: enforce the wake keys overlay
    * Keys disabled as wake keys by the overlay were
      still being allowed to wake the device. This change
      enforces the hardwareWakeKeys settings.
    
    Change-Id: Ifde0491b2de64a7f61a101cf22f5589cb5e841e2

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -396,12 +396,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mBackKillTimeout;
 
     int mDeviceHardwareKeys;
+    int mDeviceHardwareWakeKeys;
 
     // Button wake control flags
     boolean mHomeWakeScreen;
     boolean mBackWakeScreen;
     boolean mMenuWakeScreen;
     boolean mVolumeWakeScreen;
+    boolean useHomeWake;
+    boolean useBackWake;
+    boolean useMenuWake;
+    boolean useVolumeWake;
 
     // During wakeup by volume keys, we still need to capture subsequent events
     // until the key is released. This is required since the beep sound is produced
@@ -1296,6 +1301,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.bool.config_enableTranslucentDecor);
         mDeviceHardwareKeys = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_deviceHardwareKeys);
+        mDeviceHardwareWakeKeys = mContext.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareWakeKeys);
         mBackKillTimeout = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_backKillTimeout);
 
@@ -1420,6 +1427,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void updateKeyAssignments() {
         int activeHardwareKeys = mDeviceHardwareKeys;
+        int activeHardwareWakeKeys = mDeviceHardwareWakeKeys;
 
         if (mDevForceNavbar) {
             activeHardwareKeys = 0;
@@ -1428,6 +1436,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean hasHome = (activeHardwareKeys & KEY_MASK_HOME) != 0;
         final boolean hasAssist = (activeHardwareKeys & KEY_MASK_ASSIST) != 0;
         final boolean hasAppSwitch = (activeHardwareKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        useMenuWake = (activeHardwareWakeKeys & KEY_MASK_MENU) != 0;
+        useHomeWake = (activeHardwareWakeKeys & KEY_MASK_HOME) != 0;
+        useBackWake = (activeHardwareWakeKeys & KEY_MASK_BACK) != 0;
+        useVolumeWake = (activeHardwareWakeKeys & KEY_MASK_VOLUME) != 0;
+
         final ContentResolver resolver = mContext.getContentResolver();
 
         // Initialize all assignments to sane defaults.
@@ -5084,7 +5098,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_HOME:
-                if (down && !interactive && mHomeWakeScreen) {
+                if (down && !interactive && mHomeWakeScreen && useHomeWake) {
                     isWakeKey = true;
                 }
                 break;
@@ -5274,7 +5288,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
             case KeyEvent.KEYCODE_VOLUME_MUTE:
-                return mVolumeWakeScreen || mDockMode != Intent.EXTRA_DOCK_STATE_UNDOCKED;
+                return (mVolumeWakeScreen && useVolumeWake) ||
+                         mDockMode != Intent.EXTRA_DOCK_STATE_UNDOCKED;
 
             // ignore media and camera keys
             case KeyEvent.KEYCODE_MUTE:
@@ -5294,9 +5309,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 return false;
 
             case KeyEvent.KEYCODE_BACK:
-                return mBackWakeScreen;
+                return (mBackWakeScreen && useBackWake);
             case KeyEvent.KEYCODE_MENU:
-                return mMenuWakeScreen;
+                return (mMenuWakeScreen && useMenuWake);
         }
         return true;
     }
