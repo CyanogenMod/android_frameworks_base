43c468ea54a9676be308923bd8a7e46c944e9d16
Scott Warner <Tortel1210@gmail.com>
2014-12-28 21:38:04 -0500

    QuickSettings: Add long click support for all tiles
    Add actions for the default tiles
    
    Change-Id: I327eca67a29cee2d1c839b83cc7067133b86dc63

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -20,6 +20,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +41,7 @@ import com.android.systemui.statusbar.policy.SignalCallbackAdapter;
 public class CellularTile extends QSTile<QSTile.SignalState> {
     private static final Intent CELLULAR_SETTINGS = new Intent().setComponent(new ComponentName(
             "com.android.settings", "com.android.settings.Settings$DataUsageSummaryActivity"));
+    private static final Intent WIRELESS_SETTINGS = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
 
     private final NetworkController mController;
     private final MobileDataController mDataController;
@@ -86,6 +88,11 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
         } else {
             mHost.startActivityDismissingKeyguard(CELLULAR_SETTINGS);
         }
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.startActivityDismissingKeyguard(WIRELESS_SETTINGS);
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -116,6 +116,11 @@ public class WifiTile extends QSTile<QSTile.SignalState> {
     }
 
     @Override
+    protected void handleLongClick() {
+        mHost.startActivityDismissingKeyguard(WIFI_SETTINGS);
+    }
+
+    @Override
     protected void handleUpdateState(SignalState state, Object arg) {
         state.visible = true;
         if (DEBUG) Log.d(TAG, "handleUpdateState arg=" + arg);
