321ed988ad043d34c616c31f71f03077bbe337b2
Matt Mower <mowerm@gmail.com>
2014-12-31 15:02:58 -0600

    Keyguard: Forward port lockscreen quick unlock (1/2)
    * PasswordTextView QuickUnlock listener functions and method thanks to
      ParanoidAndroid project:
        Keyguard: Implement quick unlock feature [1/2]
        BigBrother1984 <carlosavignano@aospa.co>
        I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
    Change-Id: I3b92cd7d45462f4b9e4d745e891a33a90bce58bc

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
@@ -213,6 +213,8 @@ public class KeyguardSimPukView extends KeyguardPinBasedInputView {
         if (mEcaView instanceof EmergencyCarrierArea) {
             ((EmergencyCarrierArea) mEcaView).setCarrierTextVisible(true);
         }
+
+        mPasswordEntry.setQuickUnlockListener(null);
     }
 
     @Override
