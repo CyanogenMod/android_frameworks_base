515534b8f11f0bc90a3927afaf5aba1a6b98d918
Praveen Chavan <pchavan@codeaurora.org>
2015-01-05 12:47:40 -0800

    Revert "Remove obsolete OMA-DRM support".
    This reverts commit 853ad6fbe34fa26e81e4b7325309a034d7a1b038.
    
    Change-Id: I8ff1ef3498432e31e9bbe9e7a2960392c485ada7

--- media/java/android/media/Ringtone.java
+++ media/java/android/media/Ringtone.java
@@ -24,6 +24,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.RemoteException;
+import android.provider.DrmStore;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
@@ -47,6 +48,12 @@ public class Ringtone {
         MediaStore.Audio.Media._ID,
         MediaStore.Audio.Media.DATA,
         MediaStore.Audio.Media.TITLE
+    };
+
+    private static final String[] DRM_COLUMNS = new String[] {
+        DrmStore.Audio._ID,
+        DrmStore.Audio.DATA,
+        DrmStore.Audio.TITLE
     };
 
     private final Context mContext;
@@ -130,8 +137,8 @@ public class Ringtone {
     }
 
     /**
-     * Returns a human-presentable title for ringtone. Looks in media
-     * content provider. If not in either, uses the filename
+     * Returns a human-presentable title for ringtone. Looks in media and DRM
+     * content providers. If not in either, uses the filename
      * 
      * @param context A context used for querying. 
      */
@@ -171,7 +178,9 @@ public class Ringtone {
                 }
             } else {
                 try {
-                    if (MediaStore.AUTHORITY.equals(authority)) {
+                    if (DrmStore.AUTHORITY.equals(authority)) {
+                        cursor = res.query(uri, DRM_COLUMNS, null, null, null);
+                    } else if (MediaStore.AUTHORITY.equals(authority)) {
                         cursor = res.query(uri, MEDIA_COLUMNS, null, null, null);
                     }
                 } catch (SecurityException e) {
