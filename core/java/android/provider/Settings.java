ece08dedf62b90113e6a6188484fd9245add1a06
cristianomatos <cristianobmatos@gmail.com>
2015-01-05 22:05:02 -0200

    Add the ability to hide superuser status bar icon [1/2]
    - Add a simple option to hide the su icon placed in status bar when a session is active
    
    - Remove Superuser icon from QS tiles
    
    Forward ported from crDroid sources: https://github.com/crdroidandroid/android_frameworks_base/commit/d8a9b6dedc2bc009a152d942a71b2dfb62a24b53
    
    Change-Id: I458b7132e0bb8ffff28a101199abf4e49ded2190

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2407,6 +2407,11 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+        /**
          * Scaling factor for fonts, float.
          */
         public static final String FONT_SCALE = "font_scale";
