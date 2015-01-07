e5ab1bb8ebd9abd3c00230011dd00205af938863
New One Android <kameron19921016@qq.com>
2015-01-08 02:18:56 +0800

    Add Shortcut 1/3

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1222,6 +1222,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KEY_ACTION_LAST_APP:
                 ActionUtils.switchToLastApp(mContext, mCurrentUserId);
                 break;
+                case KEY_ACTION_SCREENSHOT:
+                mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
+                break;
+            case KEY_ACTION_REBOOT:
+                showGlobalActions();
+                break;
             default:
                 break;
         }
