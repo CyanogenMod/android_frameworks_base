a0ed98cd2496ac86c9517591e10d9660421c60ff
New One Android <kameron19921016@qq.com>
2015-01-08 13:08:08 +0800

    Update BarTransitions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
@@ -69,6 +69,12 @@ public class BarTransitions {
         return mMode;
     }
 
+    public void setWarningColor(int color) {
+         if (mBarBackground != null) {
+             mBarBackground.setWarningColor(color);
+         }
+    }
+
     public void transitionTo(int mode, boolean animate) {
         // low-end devices do not support translucent modes, fallback to opaque
         if (!HIGH_END && (mode == MODE_SEMI_TRANSPARENT || mode == MODE_TRANSLUCENT
@@ -113,7 +119,7 @@ public class BarTransitions {
         private final int mOpaque;
         private final int mSemiTransparent;
         private final int mTransparent;
-        private final int mWarning;
+        private int mWarning;
         private final Drawable mGradient;
         private final TimeInterpolator mInterpolator;
 
@@ -159,6 +165,12 @@ public class BarTransitions {
         protected void onBoundsChange(Rect bounds) {
             super.onBoundsChange(bounds);
             mGradient.setBounds(bounds);
+        }
+
+        public void setWarningColor(int color) {
+            if (!DEBUG_COLORS) {
+                mWarning = color;
+            }
         }
 
         public void applyModeBackground(int oldMode, int newMode, boolean animate) {
