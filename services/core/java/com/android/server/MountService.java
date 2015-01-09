36cf0520bfb5ead6740a84cf939f8b02790aedfa
padarshr <padarshr@codeaurora.org>
2015-01-09 17:22:38 +0530

    Do not mount Volume if formatting is in progress
    This change will avoid mounting the volume if the
    state of volume at that point is 'formatting'
    
    CRs-Fixed: 689602
    
    Change-Id: Iab3f8936017ea3a8a8da64f8e01b357dc045cbcf

--- services/core/java/com/android/server/MountService.java
+++ services/core/java/com/android/server/MountService.java
@@ -754,6 +754,9 @@ class MountService extends IMountService.Stub
         synchronized (mVolumesLock) {
             oldState = mVolumeStates.put(path, state);
             volume.setState(state);
+            if (!Environment.MEDIA_FORMATTING.equals(state)) {
+                volume.setIsFormatting(false);
+            }
         }
 
         if (state.equals(oldState)) {
@@ -1099,6 +1102,9 @@ class MountService extends IMountService.Stub
         } else if (newState == VolumeState.Unmounting) {
             action = Intent.ACTION_MEDIA_EJECT;
         } else if (newState == VolumeState.Formatting) {
+            synchronized (mVolumesLock) {
+                volume.setIsFormatting(true);
+            }
         } else if (newState == VolumeState.Shared) {
             if (DEBUG_EVENTS) Slog.i(TAG, "Updating volume state media mounted");
             /* Send the media unmounted event first */
@@ -1127,6 +1133,9 @@ class MountService extends IMountService.Stub
         final StorageVolume volume;
         synchronized (mVolumesLock) {
             volume = mVolumesByPath.get(path);
+            if (volume.getIsFormatting()) {
+                return StorageResultCode.OperationFailedStorageBusy;
+            }
         }
 
         if (!volume.isEmulated() && hasUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA)) {
