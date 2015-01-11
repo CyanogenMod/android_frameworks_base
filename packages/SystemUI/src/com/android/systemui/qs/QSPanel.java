48231fddbd34d8f3a1061d7e4ceb500e0b229bdc
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-11 20:07:15 +0000

    Quick settings: Option to disable brightness slider (1/2)
    also fixed ripple placement for dual tiles
    
    before:
    https://www.dropbox.com/s/r4m1ytiwbdbj1xw/Screenshot_2015-01-12-21-29-55.png?dl=0
    
    after:
    https://www.dropbox.com/s/lvswhsubux548xh/Screenshot_2015-01-12-21-27-31.png?dl=0
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Change-Id: I98a28e99e78ee4d8bb4f03bc53e867723d62b052

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -118,6 +118,25 @@ public class QSPanel extends ViewGroup {
         });
     }
 
+    /**
+     * Enable/disable brightness slider.
+     */
+    private boolean showBrightnessSlider() {
+        boolean brightnessSliderEnabled = Settings.System.getIntForUser(
+            mContext.getContentResolver(), Settings.System.QS_SHOW_BRIGHTNESS_SLIDER,
+                1, UserHandle.USER_CURRENT) == 1;
+        ToggleSlider brightnessSlider = (ToggleSlider) findViewById(R.id.brightness_slider);
+        if (brightnessSliderEnabled) {
+            mBrightnessView.setVisibility(VISIBLE);
+            brightnessSlider.setVisibility(VISIBLE);
+        } else {
+            mBrightnessView.setVisibility(GONE);
+            brightnessSlider.setVisibility(GONE);
+        }
+        updateResources();
+        return brightnessSliderEnabled;
+    }
+
     private void updateDetailText() {
         mDetailDoneButton.setText(R.string.quick_settings_done);
         mDetailSettingsButton.setText(R.string.quick_settings_more_settings);
@@ -206,7 +225,7 @@ public class QSPanel extends ViewGroup {
         if (mListening) {
             refreshAllTiles();
         }
-        if (listening) {
+        if (listening && showBrightnessSlider()) {
             mBrightnessController.registerCallbacks();
         } else {
             mBrightnessController.unregisterCallbacks();
@@ -433,7 +452,7 @@ public class QSPanel extends ViewGroup {
                 tileRecord.tileView.setVisibility(newVis);
             }
         }
-        mBrightnessView.setVisibility(newVis);
+        mBrightnessView.setVisibility(showBrightnessSlider() ? newVis : GONE);
         if (mGridContentVisible != visible) {
             MetricsLogger.visibility(mContext, MetricsLogger.QS_PANEL, newVis);
         }
