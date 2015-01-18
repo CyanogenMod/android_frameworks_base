780fe9d873f57d0adf9cf9296c8401599071f5a1
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    SystemUI: Lockscreen shortcut customization
    Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardAffordanceView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardAffordanceView.java
@@ -25,9 +25,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.CanvasProperty;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v7.graphics.Palette;
 import android.util.AttributeSet;
 import android.view.DisplayListCanvas;
 import android.view.RenderNodeAnimator;
@@ -45,7 +49,7 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
  * An ImageView which does not have overlapping renderings commands and therefore does not need a
  * layer when alpha is changed.
  */
-public class KeyguardAffordanceView extends ImageView {
+public class KeyguardAffordanceView extends ImageView implements Palette.PaletteAsyncListener {
 
     private static final long CIRCLE_APPEAR_DURATION = 80;
     private static final long CIRCLE_DISAPPEAR_MAX_DURATION = 200;
@@ -81,6 +85,7 @@ public class KeyguardAffordanceView extends ImageView {
     private boolean mSupportHardware;
     private boolean mFinishing;
     private boolean mLaunchingAffordance;
+    private ColorFilter mDefaultFilter;
 
     private CanvasProperty<Float> mHwCircleRadius;
     private CanvasProperty<Float> mHwCenterX;
@@ -162,21 +167,66 @@ public class KeyguardAffordanceView extends ImageView {
         canvas.restore();
     }
 
+
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        super.setImageDrawable(drawable);
+        doPaletteIfNecessary();
+    }
+
+    private void doPaletteIfNecessary() {
+        if (mDefaultFilter != null && getDrawable() instanceof BitmapDrawable) {
+            Palette.generateAsync(((BitmapDrawable) getDrawable()).getBitmap(), this);
+        }
+    }
+
+
     public void setPreviewView(View v) {
         View oldPreviewView = mPreviewView;
         mPreviewView = v;
         if (mPreviewView != null) {
             mPreviewView.setVisibility(mLaunchingAffordance
                     ? oldPreviewView.getVisibility() : INVISIBLE);
+            mPreviewView.setVisibility(INVISIBLE);
+            addOverlay();
         }
     }
 
+    private void addOverlay() {
+        if (mPreviewView != null) {
+            mPreviewView.getOverlay().clear();
+            if (mDefaultFilter != null) {
+                ColorDrawable d = new ColorDrawable(mCircleColor);
+                d.setBounds(0, 0, mPreviewView.getWidth(), mPreviewView.getHeight());
+                mPreviewView.getOverlay().add(d);
+            }
+        }
+    }
+
+    public void setDefaultFilter(ColorFilter filter) {
+        mDefaultFilter = filter;
+        mCircleColor = Color.WHITE;
+        addOverlay();
+        updateIconColor();
+    }
+
     private void updateIconColor() {
+        if (getDrawable() == null) {
+            return;
+        }
         Drawable drawable = getDrawable().mutate();
         float alpha = mCircleRadius / mMinBackgroundRadius;
         alpha = Math.min(1.0f, alpha);
         int color = (int) mColorInterpolator.evaluate(alpha, mNormalColor, mInverseColor);
-        drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        if (mDefaultFilter != null) {
+            if (alpha == 0) {
+                drawable.setColorFilter(mDefaultFilter);
+            } else {
+                drawable.setColorFilter(color, PorterDuff.Mode.DST_IN);
+            }
+        } else {
+            drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        }
     }
 
     private void drawBackgroundCircle(Canvas canvas) {
@@ -550,5 +600,11 @@ public class KeyguardAffordanceView extends ImageView {
 
     public void setLaunchingAffordance(boolean launchingAffordance) {
         mLaunchingAffordance = launchingAffordance;
+    }
+
+    @Override
+    public void onGenerated(Palette palette) {
+        mCircleColor = palette.getDarkVibrantColor(Color.WHITE);
+        addOverlay();
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2075,13 +2075,9 @@ public class NotificationPanelView extends PanelView implements
         });
         rightIcon = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? !rightIcon : rightIcon;
         if (rightIcon) {
-            mStatusBar.onCameraHintStarted();
+            mStatusBar.onCameraHintStarted(mKeyguardBottomArea.getRightHint());
         } else {
-            if (mKeyguardBottomArea.isLeftVoiceAssist()) {
-                mStatusBar.onVoiceAssistHintStarted();
-            } else {
-                mStatusBar.onPhoneHintStarted();
-            }
+            mStatusBar.onLeftHintStarted(mKeyguardBottomArea.getLeftHint());
         }
     }
 
