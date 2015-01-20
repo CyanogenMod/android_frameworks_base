b2682d109735ac21d1db58284393115102efaa34
Adnan Begovic <adnan@cyngn.com>
2015-01-20 23:51:06 +0000

    Revert "Keyguard: Create lockscreen weather, move weatherimpl to utils."
    This reverts commit d855e463bb934f41cee72a893609626de0a03f25.
    
    Change-Id: Iff2982cdb2afc0c227be75f0437ac4cf8630d8b8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -49,8 +49,6 @@ import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
-import com.android.internal.util.cm.WeatherController;
-import com.android.internal.util.cm.WeatherControllerImpl;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryLevelTextView;
@@ -61,6 +59,8 @@ import com.android.systemui.qs.QSTile;
 import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.UserInfoController;
+import com.android.systemui.statusbar.policy.WeatherController;
+import com.android.systemui.statusbar.policy.WeatherControllerImpl;
 
 /**
  * The view to manage the header area in the expanded status bar.
