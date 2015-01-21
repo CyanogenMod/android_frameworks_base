cc72ad218ec5bc022da1088673955301d58abf44
Roman Birg <roman@cyngn.com>
2015-01-20 16:14:08 -0800

    SystemUI: port lock screen toggle tile
    Change-Id: Ibec9993f3bd339a2d48e00eea6442618a91b15c9
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
@@ -32,6 +32,10 @@ public class KeyguardServiceDelegate {
     public static final String KEYGUARD_PACKAGE = "com.android.systemui";
     public static final String KEYGUARD_CLASS = "com.android.systemui.keyguard.KeyguardService";
 
+    private static final String ACTION_STATE_CHANGE =
+            "com.android.internal.action.KEYGUARD_SERVICE_STATE_CHANGED";
+    private static final String EXTRA_ACTIVE = "active";
+
     private static final String TAG = "KeyguardServiceDelegate";
     private static final boolean DEBUG = true;
     protected KeyguardServiceWrapper mKeyguardService;
@@ -119,6 +123,12 @@ public class KeyguardServiceDelegate {
         }
     }
 
+    private void sendStateChangeBroadcast(boolean bound) {
+        Intent i = new Intent(ACTION_STATE_CHANGE);
+        i.putExtra(EXTRA_ACTIVE, bound);
+        mScrim.getContext().sendStickyBroadcast(i);
+    }
+
     private final ServiceConnection mKeyguardConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -130,6 +140,7 @@ public class KeyguardServiceDelegate {
                 mKeyguardService.onSystemReady();
                 // This is used to hide the scrim once keyguard displays.
                 mKeyguardService.onScreenTurnedOn(new KeyguardShowDelegate(null));
+                sendStateChangeBroadcast(true);
             }
             if (mKeyguardState.bootCompleted) {
                 mKeyguardService.onBootCompleted();
@@ -140,6 +151,7 @@ public class KeyguardServiceDelegate {
         public void onServiceDisconnected(ComponentName name) {
             if (DEBUG) Log.v(TAG, "*** Keyguard disconnected (boo!)");
             mKeyguardService = null;
+            sendStateChangeBroadcast(false);
         }
 
     };
@@ -255,6 +267,7 @@ public class KeyguardServiceDelegate {
         } else {
             mKeyguardState.systemIsReady = true;
         }
+        sendStateChangeBroadcast(true);
     }
 
     public void doKeyguardTimeout(Bundle options) {
