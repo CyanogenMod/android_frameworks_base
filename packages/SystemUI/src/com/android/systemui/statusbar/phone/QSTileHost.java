d1933f924a1b1b37d079d6b18f2cdc08b20421bb
Roman Birg <roman@cyngn.com>
2015-01-21 14:13:00 -0800

    SystemUI: port visualizer tile
    Change-Id: I78c5b95008183e664453529e5b2d3ba601f0d266
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -266,6 +267,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
         else if (tileSpec.equals("notifications")) return new NotificationsTile(this);
+        else if (tileSpec.equals("visualizer")) return new VisualizerTile(this);
         else if (tileSpec.equals("data")
                 && mConnectivityManager.isNetworkSupported(ConnectivityManager.TYPE_MOBILE))
             return new DataTile(this);
