4f81f7db3eb179816ac5235dad15fb9fa08e34d5
Roman Birg <roman@cyngn.com>
2015-01-21 14:13:00 -0800

    SystemUI: port visualizer tile
    Change-Id: I78c5b95008183e664453529e5b2d3ba601f0d266
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -54,6 +54,7 @@ import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.PerfProfileTile;
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.VisualizerTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -257,6 +258,7 @@ public class QSTileHost implements QSTile.Host {
                 newTiles.put(tileSpec, t);
             }
         }
+        newTiles.put("visualizer", new VisualizerTile(this));
 
         mTiles.clear();
         mTiles.putAll(newTiles);
@@ -318,6 +320,8 @@ public class QSTileHost implements QSTile.Host {
                 return new NfcTile(this);
             case QSConstants.TILE_LOCKSCREEN:
                 return new LockscreenToggleTile(this);
+//            case QSConstants.TILE_VISUALIZER:
+//                return new VisualizerTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
