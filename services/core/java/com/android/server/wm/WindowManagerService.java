d9bec36412deb0f5c13f4c0e11f86f08f2cfda77
JooJooBee666 <desire4homicide@gmail.com>
2015-01-22 15:35:27 -0500

    WindowManagerService: Fix for On-Screen keyboard popups on QWERTY devices
    For the 3 QWERTY keyboard users, this fixes the issue with the On-Screen
     keyboard popping up when users select a text field, regardless of
     HardwareKeyboard status by forcing a call to the update method, which
     never occurs if the setting is not in the settings db.
    
    Change-Id: Ibf3f05762e72077bbe7946f83b0e320bb94a06c5

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7288,7 +7288,7 @@ public class WindowManagerService extends IWindowManager.Stub
             }
             if (hardKeyboardAvailable != mHardKeyboardAvailable) {
                 mHardKeyboardAvailable = hardKeyboardAvailable;
-                mShowImeWithHardKeyboard = hardKeyboardAvailable;
+                updateShowImeWithHardKeyboard();
                 mH.removeMessages(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
                 mH.sendEmptyMessage(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
             }
