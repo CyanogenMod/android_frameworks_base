3729d1fc1f00cf7e23e606f3c727ee6746bb68e9
Adnan Begovic <adnan@cyngn.com>
2015-01-21 18:54:39 -0800

    SystemUI: Create LteTile
    Change-Id: Ic193279f930af54d5754358c7e4c97b6b07d2010

--- core/java/com/android/internal/util/cm/QSUtils.java
+++ core/java/com/android/internal/util/cm/QSUtils.java
@@ -24,6 +24,7 @@ import android.hardware.camera2.CameraManager;
 import android.net.ConnectivityManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import com.android.internal.telephony.PhoneConstants;
 
 import java.util.Iterator;
 import java.util.List;
@@ -74,6 +75,9 @@ public class QSUtils {
                     case QSConstants.TILE_BLUETOOTH:
                         removeTile = !deviceSupportsBluetooth();
                         break;
+                    case QSConstants.TILE_LTE:
+                        removeTile = !deviceSupportsLte(context);
+                        break;
                 }
                 if (removeTile) {
                     iterator.remove();
@@ -83,6 +87,13 @@ public class QSUtils {
 
             sAvailableTilesFiltered = true;
         }
+    }
+
+    public static boolean deviceSupportsLte(Context ctx) {
+        final TelephonyManager tm = (TelephonyManager)
+                ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
+                || tm.getLteOnGsmMode() != 0;
     }
 
     private static boolean deviceSupportsDdsSupported(Context context) {
