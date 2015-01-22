30f59fea47c121892436d2dff4f488bf44d66785
JooJooBee666 <desire4homicide@gmail.com>
2015-01-22 15:35:27 -0500

    WindowManagerService: Fix On-Screen keyboard popups when hw-keyboard is present
    This fixes the issue with the On-Screen keyboard popping up when users
    with a hardware keyboard select a text input field.  The original line of
    "mShowImeWithHardKeyboard = hardKeyboardAvailable" is unwarranted and the
    call to updateShowImeWithHardKeyboard() was redundant.
    
    Change-Id: Ibf3f05762e72077bbe7946f83b0e320bb94a06c5

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7419,7 +7419,6 @@ public class WindowManagerService extends IWindowManager.Stub
             }
             if (hardKeyboardAvailable != mHardKeyboardAvailable) {
                 mHardKeyboardAvailable = hardKeyboardAvailable;
-                mShowImeWithHardKeyboard = hardKeyboardAvailable;
                 mH.removeMessages(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
                 mH.sendEmptyMessage(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
             }
