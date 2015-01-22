67b378b83e28d68c2d27a1fb94465fca8ba6ab65
Roman Birg <roman@cyngn.com>
2015-01-21 18:29:59 -0800

    SystemUI: port performance profile toggle
    Change-Id: Idb4b022f80d199eb6f3a2e0002ad960307c3a89c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -37,6 +37,7 @@ public class QSConstants {
     public static final String TILE_DDS = "dds";
     public static final String TILE_APN = "apn";
     public static final String TILE_PROFILES = "profiles";
+    public static final String TILE_PERFORMANCE = "performance";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -64,5 +65,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_DDS);
         TILES_AVAILABLE.add(TILE_APN);
         TILES_AVAILABLE.add(TILE_PROFILES);
+        TILES_AVAILABLE.add(TILE_PERFORMANCE);
     }
 }
