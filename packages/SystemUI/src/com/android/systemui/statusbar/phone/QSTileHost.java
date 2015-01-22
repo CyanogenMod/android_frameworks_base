ea2ae0dbbc9f7791f06934a66b1de19d5d894175
Roman Birg <roman@cyngn.com>
2015-01-21 18:29:59 -0800

    SystemUI: port performance profile toggle
    Change-Id: Idb4b022f80d199eb6f3a2e0002ad960307c3a89c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -46,7 +46,11 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
+<<<<<<< HEAD
 import com.android.systemui.qs.tiles.ProfilesTile;
+=======
+import com.android.systemui.qs.tiles.PerfProfileTile;
+>>>>>>> 4bd7b3f... SystemUI: port performance profile toggle
 import com.android.systemui.qs.tiles.RoamingTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -303,6 +307,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ApnTile(this);
             case QSConstants.TILE_PROFILES:
                 return new ProfilesTile(this);
+            case QSConstants.TILE_PERFORMANCE:
+                return new PerfProfileTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
