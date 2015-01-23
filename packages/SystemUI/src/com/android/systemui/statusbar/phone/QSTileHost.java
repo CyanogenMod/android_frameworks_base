40d1e3e1648641f3abf8b1e5461d7153a0706747
Danesh M <daneshm90@gmail.com>
2015-01-23 11:57:52 -0800

    SystemUI : Add adb over network tile
    Change-Id: Ib1fa0c87562ff3a077bf9c7283ce81fc4a8271db

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -33,6 +33,7 @@ import com.android.internal.util.cm.QSConstants;
 import com.android.internal.util.cm.QSUtils;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
+import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.ApnTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -306,6 +307,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ProfilesTile(this);
             case QSConstants.TILE_PERFORMANCE:
                 return new PerfProfileTile(this);
+            case QSConstants.TILE_ADB_NETWORK:
+                return new AdbOverNetworkTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
