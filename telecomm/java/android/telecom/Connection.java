295e0b5cb3f8049303ed5cbb93414e75e74213c4
Tyler Gunn <tgunn@google.com>
2015-01-29 11:47:24 -0800

    Fixing conference merge where only one party is added to conference.
    - Adding "onConferenceStarted" listener for Connections.
    - This is necessary so that an ImsCall can report the fact that it has
    went from being a single party call to a multiparty call.  This was not
    previously necessary since the multiparty bit change would be detected
    when one of the connections being merged changed state.  Since we now must
    defer the establishment of the conference until all connections have been
    merged, we need a means of detecting when the call becomes multiparty.
    
    Bug: 18960042
    Change-Id: If007e4d230071f198db726a67e4e640cd0d11fd0

--- telecomm/java/android/telecom/Connection.java
+++ telecomm/java/android/telecom/Connection.java
@@ -342,6 +342,7 @@ public abstract class Connection implements IConferenceable {
         /** @hide */
         public void onConferenceParticipantsChanged(Connection c,
                 List<ConferenceParticipant> participants) {}
+        public void onConferenceStarted() {}
     }
 
     /** @hide */
@@ -1655,6 +1656,15 @@ public abstract class Connection implements IConferenceable {
             List<ConferenceParticipant> conferenceParticipants) {
         for (Listener l : mListeners) {
             l.onConferenceParticipantsChanged(this, conferenceParticipants);
+        }
+    }
+
+    /**
+     * Notifies listeners that a conference call has been started.
+     */
+    protected void notifyConferenceStarted() {
+        for (Listener l : mListeners) {
+            l.onConferenceStarted();
         }
     }
 }
