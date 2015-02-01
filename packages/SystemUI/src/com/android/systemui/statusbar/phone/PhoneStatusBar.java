fb724357a54bdd33813d49253041c8f679343c79
kaiyiz <kaiyiz@codeaurora.org>
2015-02-01 10:39:20 +0800

    SystemUI: Fix no LED indication for missed call when screen off
    When device is going to sleep, showKeyguard() will be invoked to show
    lock screen. It shouldn't clear the LED notification at this moment.
    
    Do not clear the LED notification in showKeyguard(). This should only
    happen when user is actively viewing notification.
    
    Change-Id: Id81b9e854df9510d436f6eb8185ee4411c914dd9
    CRs-Fixed: 783613

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2724,7 +2724,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarWindowManager.setStatusBarExpanded(true);
         mStatusBarView.setFocusable(false);
 
-        visibilityChanged(true);
+        if (!force) {
+            visibilityChanged(true);
+        }
         mWaitingForKeyguardExit = false;
         disable(mDisabledUnmodified, !force /* animate */);
         setInteracting(StatusBarManager.WINDOW_STATUS_BAR, true);
