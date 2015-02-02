6906494b82adc999a58a2a169379f04576c2b3d8
Clark Scheff <clark@cyngn.com>
2015-02-02 10:20:18 -0800

    Themes: Handle case when keyguard is showing during theme change
    After we recreate the status bar we need to check if the keyguard
    was showing and if so make sure it is showing after the theme
    change.  We also need to make sure the phone status bar is not
    visible if the keyguard was showing, otherwise you see two status
    bars.
    
    Change-Id: Iba09e3e5c8cf52d49f292423a50034a3cf84d9c1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3419,6 +3419,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         });
         // restart the keyguard so it picks up the newly created ScrimController
         startKeyguard();
+
+        // if the keyguard was showing while this change occurred we'll need to do some extra work
+        if (mState == StatusBarState.KEYGUARD) {
+            // this will make sure the keyguard is showing
+            showKeyguard();
+        }
     }
 
     private void removeAllViews(ViewGroup parent) {
