edac65a3f89be5843cf4c1bc0fa2794f57f7c890
Steve Kondik <shade@chemlab.org>
2015-02-02 16:20:35 -0500

    input: Add option to toggle pointer icon when using stylus
    * The visible pointer icon when hovering or drawing with the stylus is
      quite annoying when trying to actually draw with it. Turn it off by
      default and add an option to turn it on.
    
    Change-Id: I26ba2ca8b92511799033759ca85c36f4ba2b7833

--- services/core/jni/com_android_server_input_InputManagerService.cpp
+++ services/core/jni/com_android_server_input_InputManagerService.cpp
@@ -202,6 +202,7 @@ public:
     void setSystemUiVisibility(int32_t visibility);
     void setPointerSpeed(int32_t speed);
     void setShowTouches(bool enabled);
+    void setStylusIconEnabled(bool enabled);
     void setVolumeKeysRotation(int mode);
     void setInteractive(bool interactive);
     void reloadCalibration();
@@ -277,6 +278,9 @@ private:
         // Show touches feature enable/disable.
         bool showTouches;
 
+        // Show icon when stylus is used
+        bool stylusIconEnabled;
+
         // Volume keys rotation mode (0 - off, 1 - phone, 2 - tablet)
         int32_t volumeKeysRotationMode;
 
@@ -316,6 +320,7 @@ NativeInputManager::NativeInputManager(jobject contextObj,
         mLocked.pointerSpeed = 0;
         mLocked.pointerGesturesEnabled = true;
         mLocked.showTouches = false;
+        mLocked.stylusIconEnabled = false;
         mLocked.volumeKeysRotationMode = 0;
     }
     mInteractive = true;
@@ -464,6 +469,7 @@ void NativeInputManager::getReaderConfiguration(InputReaderConfiguration* outCon
         outConfig->pointerGesturesEnabled = mLocked.pointerGesturesEnabled;
 
         outConfig->showTouches = mLocked.showTouches;
+        outConfig->stylusIconEnabled = mLocked.stylusIconEnabled;
         outConfig->volumeKeysRotationMode = mLocked.volumeKeysRotationMode;
 
         outConfig->setDisplayInfo(false /*external*/, mLocked.internalViewport);
@@ -771,6 +777,22 @@ void NativeInputManager::setShowTouches(bool enabled) {
 
     mInputManager->getReader()->requestRefreshConfiguration(
             InputReaderConfiguration::CHANGE_SHOW_TOUCHES);
+}
+
+void NativeInputManager::setStylusIconEnabled(bool enabled) {
+    { // acquire lock
+        AutoMutex _l(mLock);
+
+        if (mLocked.stylusIconEnabled == enabled) {
+            return;
+        }
+
+        ALOGI("Setting stylus icon enabled to %s.", enabled ? "enabled" : "disabled");
+        mLocked.stylusIconEnabled = enabled;
+    } // release lock
+
+    mInputManager->getReader()->requestRefreshConfiguration(
+            InputReaderConfiguration::CHANGE_STYLUS_ICON_ENABLED);
 }
 
 void NativeInputManager::setVolumeKeysRotation(int mode) {
@@ -1394,6 +1416,13 @@ static void nativeSetShowTouches(JNIEnv* /* env */,
     im->setShowTouches(enabled);
 }
 
+static void nativeSetStylusIconEnabled(JNIEnv* env,
+        jclass clazz, jlong ptr, jboolean enabled) {
+    NativeInputManager* im = reinterpret_cast<NativeInputManager*>(ptr);
+
+    im->setStylusIconEnabled(enabled);
+}
+
 static void nativeSetVolumeKeysRotation(JNIEnv* env,
         jclass clazz, jlong ptr, int mode) {
     NativeInputManager* im = reinterpret_cast<NativeInputManager*>(ptr);
@@ -1544,6 +1573,8 @@ static const JNINativeMethod gInputManagerMethods[] = {
             (void*) nativeSetPointerSpeed },
     { "nativeSetShowTouches", "(JZ)V",
             (void*) nativeSetShowTouches },
+    { "nativeSetStylusIconEnabled", "(JZ)V",
+            (void*) nativeSetStylusIconEnabled },
     { "nativeSetVolumeKeysRotation", "(JI)V",
             (void*) nativeSetVolumeKeysRotation },
     { "nativeSetInteractive", "(JZ)V",
