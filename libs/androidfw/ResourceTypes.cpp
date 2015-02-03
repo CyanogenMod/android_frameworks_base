036e6ab8ad65248135bfae331419091cb933576a
d34d <clark@cyngn.com>
2015-02-03 09:58:52 -0800

    Themes: Clear bag cache when removing overlay assets
    The bag cache needs to be cleared from the group that was being
    overlaid, otherwise it will be referencing styles and attributes
    that may no longer exist.
    
    Change-Id: Ieef3ff2c55a643e12e9c4b8b4f6d5fd54da663d7

--- libs/androidfw/ResourceTypes.cpp
+++ libs/androidfw/ResourceTypes.cpp
@@ -6751,6 +6751,7 @@ void ResTable::removeAssetsByCookie(const String8& /* packageName */, int32_t co
                 PackageGroup* grp = mPackageGroups.itemAt(i);
                 if (grp->overlayPackage == pkg) {
                     removeIdmappedTypesFromPackageGroup(grp);
+                    grp->clearBagCache();
                     grp->overlayPackage = NULL;
                 }
             }
