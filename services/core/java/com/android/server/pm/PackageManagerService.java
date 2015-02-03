96716fd07de6422d62499b3a484eb45bf7f23e72
d34d <clark@cyngn.com>
2015-02-03 09:04:48 -0800

    Themes: Use mRealPackage, when available, for idmapping
    Some packages replace existing apps and take on their package name,
    i.e. com.cyanogenmod.trebuchet -> com.android.launcher3.  When this
    happens pkg.packageName becomes the name of the package it is
    overriding and mRealPackage will be the original package name.
    
    Because it is possible for the resources in the new package to not
    match up with the package it replaced, we need to perform the idmap
    using the original package name.
    
    Change-Id: Ic22c7005274ae5c4ea29d9a4b29f9b9f40719e68

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -5695,6 +5695,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                     opkg.baseCodePath + ": overlay not trusted");
             return false;
         }
+
+        // Some apps like to take on the package name of an existing app so we'll use the
+        // "real" package name, if it is non-null, when performing the idmap
+        final String pkgName = pkg.mRealPackage != null ? pkg.mRealPackage : pkg.packageName;
         ArrayMap<String, PackageParser.Package> overlaySet = mOverlays.get(pkg.packageName);
         if (overlaySet == null) {
             Slog.e(TAG, "was about to create idmap for " + pkg.baseCodePath + " and " +
@@ -5703,7 +5707,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
         final int sharedGid = UserHandle.getSharedAppGid(pkg.applicationInfo.uid);
         final String cachePath =
-                ThemeUtils.getTargetCacheDir(pkg.packageName, opkg.packageName);
+                ThemeUtils.getTargetCacheDir(pkgName, opkg.packageName);
         if (mInstaller.idmap(pkg.baseCodePath, opkg.baseCodePath, cachePath, sharedGid,
                 getPackageHashCode(pkg), getPackageHashCode(opkg)) != 0) {
             Slog.e(TAG, "Failed to generate idmap for " + pkg.baseCodePath +
