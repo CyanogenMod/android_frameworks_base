710c539eedb29042635b450df06c397d921c0e38
Adnan Begovic <adnan@cyngn.com>
2015-02-04 01:00:06 -0800

    Create configurable default expanded desktop style.
    Change-Id: I90c7f8544cda9b91fb9b700fcf146f4e58b15a26

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7577,6 +7577,15 @@ public final class Settings {
         public static final String POLICY_CONTROL = "policy_control";
 
         /**
+         * Defines global runtime overrides to window policy style.
+         *
+         * See {@link android.view.WindowManagerPolicyControl} for value definitions.
+         *
+         * @hide
+         */
+        public static final String POLICY_CONTROL_STYLE = "policy_control_style";
+
+        /**
          * Defines global zen mode.  ZEN_MODE_OFF, ZEN_MODE_IMPORTANT_INTERRUPTIONS,
          * or ZEN_MODE_NO_INTERRUPTIONS.
          *
