d5bcfd9aa216a7d88289ddb8e50bbb41c1fcc3fb
linus_lee <llee@cyngn.com>
2015-02-05 15:47:37 -0800

    Enable Buttons and Status Bar settings Multi-user enabled
    The main issue is SystemUI process is shared across users, so calling getString(...)
    will call UserHandle.myUserId() which will return the user id of the process.
    To fix this, all relevant calls should be changed to get*ForUser as well as listen
    to UserChanged event so that when the user switches, the settings are updated
    
    Change-Id: I9ac3fbfec1646993028c34789a4299d85079f8f1

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -39,6 +39,7 @@ import android.widget.ImageView;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.R;
+import com.android.systemui.cm.UserContentObserver;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -337,7 +338,7 @@ public class StatusBarIconView extends AnimatedImageView {
             + " notification=" + mNotification + ")";
     }
 
-    static class GlobalSettingsObserver extends ContentObserver {
+    static class GlobalSettingsObserver extends UserContentObserver {
         private static GlobalSettingsObserver sInstance;
         private ArrayList<StatusBarIconView> mIconViews = new ArrayList<StatusBarIconView>();
         private Context mContext;
@@ -368,18 +369,24 @@ public class StatusBarIconView extends AnimatedImageView {
             }
         }
 
-        void observe() {
+        @Override
+        protected void observe() {
+            super.observe();
+
             mContext.getContentResolver().registerContentObserver(
                     Settings.System.getUriFor(Settings.System.STATUS_BAR_NOTIF_COUNT),
                     false, this);
         }
 
-        void unobserve() {
+        @Override
+        protected void unobserve() {
+            super.unobserve();
+
             mContext.getContentResolver().unregisterContentObserver(this);
         }
 
         @Override
-        public void onChange(boolean selfChange) {
+        public void update() {
             boolean showIconCount = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.STATUS_BAR_NOTIF_COUNT, 0, UserHandle.USER_CURRENT) == 1;
             for (StatusBarIconView sbiv : mIconViews) {
