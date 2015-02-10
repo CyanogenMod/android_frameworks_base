f5164a5b113093d891d6b1928dd66d5d354b015f
Divya Sharma <c_shard@codeaurora.org>
2015-02-09 17:00:12 -0800

    Disable: SystemUI
    Change-Id: I4023bbeee929d11a0691b5be6c84370cac834fba

--- packages/SystemUI/tests/Android.mk
+++ packages/SystemUI/tests/Android.mk
@@ -36,4 +36,4 @@ LOCAL_STATIC_JAVA_LIBRARIES := mockito-target Keyguard
 # UI it doesn't own. This is necessary to allow screenshots to be taken
 LOCAL_CERTIFICATE := platform
 
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
