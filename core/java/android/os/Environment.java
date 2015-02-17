33c2679a3c5291c7c84f3e4d6981ffcb4bd9bf36
Maunik Shah <mshah@codeaurora.org>
2015-02-17 15:47:45 +0530

    Hiding secondary storage APIs which are only used by frameworks
    Below APIs are marked as hidden APIs:
    1. getSecondaryStorageDirectory()
    2. getSecondaryStorageState()
    3. isNoEmulatedStorageExist()
    
    Change-Id: I81245f8635695a4206afa87560e6faa7540a9593
    CRs-Fixed: 794399

--- core/java/android/os/Environment.java
+++ core/java/android/os/Environment.java
@@ -175,6 +175,7 @@ public class Environment {
             return mExternalDirsForApp[0];
         }
 
+        /** {@hide} */
         public File getSecondaryStorageDirectory() {
             return mExternalDirsForApp[1];
         }
@@ -425,6 +426,7 @@ public class Environment {
         return sCurrentUser.getExternalDirsForApp()[0];
     }
 
+    /** {@hide} */
     public static File getSecondaryStorageDirectory() {
         throwIfUserRequired();
         return sCurrentUser.getExternalDirsForApp()[1];
@@ -743,6 +745,7 @@ public class Environment {
         return getExternalStorageState(path);
     }
 
+    /** {@hide} */
     public static String getSecondaryStorageState() {
         final File externalDir = sCurrentUser.getExternalDirsForApp()[1];
         return getStorageState(externalDir);
@@ -785,6 +788,7 @@ public class Environment {
         return isExternalStorageRemovable(externalDir);
     }
 
+    /** {@hide} */
     public static boolean isNoEmulatedStorageExist() {
         final StorageVolume volume = getNoEmulatedVolume();
         return (volume != null);
