1dc2144f898253e95b61df005faf52c9fe0cb644
Steve Kondik <steve@cyngn.com>
2015-02-18 03:52:24 -0800

    livedisplay: Implement active display adjustment
     * LiveDisplay is our new name for the various display technologies
       which adjust the screen based on environmental conditions other
       than the standard automatic backlight controls.
     * This patch implements automatic color temperature adjustment based
       on time of day. This is similar to f.lux or Redshift. My eyes are
       so happy now!
     * Automatic outdoor/SRE feature is now handled here.
     * Handling of CABC/CABL and color enhancements also handled here.
     * Manual RGB tweaking is handled here.
     * Can delegate to DisplayColor HAL if available, otherwise uses
       SurfaceFlinger's 1015 operation to apply changes.
     * Happily coexists with the new accessibility features for color
       blindness correction and color inversion.
    
    Change-Id: Iac1b74f410957f8e2d8290465c4ce9cc1fd97a88

--- services/core/java/com/android/server/display/AutomaticBrightnessController.java
+++ services/core/java/com/android/server/display/AutomaticBrightnessController.java
@@ -21,6 +21,7 @@ import com.android.server.twilight.TwilightListener;
 import com.android.server.twilight.TwilightManager;
 import com.android.server.twilight.TwilightState;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -171,9 +172,15 @@ class AutomaticBrightnessController {
     // The last screen auto-brightness gamma.  (For printing in dump() only.)
     private float mLastScreenAutoBrightnessGamma = 1.0f;
 
-    public AutomaticBrightnessController(Callbacks callbacks, Looper looper,
+    // Night mode color temperature adjustments
+    private final LiveDisplayController mLiveDisplay;
+
+    private final Context mContext;
+
+    public AutomaticBrightnessController(Context context, Callbacks callbacks, Looper looper,
             SensorManager sensorManager, Spline autoBrightnessSpline,
             int lightSensorWarmUpTime, int brightnessMin, int brightnessMax) {
+        mContext = context;
         mCallbacks = callbacks;
         mTwilight = LocalServices.getService(TwilightManager.class);
         mSensorManager = sensorManager;
@@ -189,9 +196,10 @@ class AutomaticBrightnessController {
             mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         }
 
-        if (USE_TWILIGHT_ADJUSTMENT) {
-            mTwilight.registerListener(mTwilightListener, mHandler);
-        }
+        mLiveDisplay = new LiveDisplayController(mContext, looper);
+        mTwilight.registerListener(mTwilightListener, mHandler);
+
+
     }
 
     public int getAutomaticScreenBrightness() {
@@ -431,6 +439,8 @@ class AutomaticBrightnessController {
                 Slog.d(TAG, "updateAutoBrightness: adjGamma=" + adjGamma);
             }
         }
+
+        mLiveDisplay.updateLiveDisplay(mAmbientLux);
 
         if (USE_TWILIGHT_ADJUSTMENT) {
             TwilightState state = mTwilight.getCurrentState();
