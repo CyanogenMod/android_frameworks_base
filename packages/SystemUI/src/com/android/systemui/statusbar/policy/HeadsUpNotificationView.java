0ba8e35e9bbd6d045aaafb28b01a3dc211331226
Sebastiano Poggi <poggos@gmail.com>
2015-02-21 13:43:01 +0100

    Avoid dismissing headsup notifications on swipe up
    From https://groups.google.com/forum/m/?fromgroups#!topic/android-contrib/CU4eYkApZx0
    
    Change-Id: I8861005838ba125f476814b375613c4f25a9c4d6
    Signed-off-by: linuxx <joey@cyanogenmoditalia.it>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -41,6 +41,8 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.Callback, ExpandHelper.Callback,
         ViewTreeObserver.OnComputeInternalInsetsListener {
+    public static final int DIRECTION_X = 0;
+    public static final int DIRECTION_Y = 1;
     private static final String TAG = "HeadsUpNotificationView";
     private static final boolean DEBUG = false;
     private static final boolean SPEW = DEBUG;
@@ -323,7 +325,7 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
     @Override
     public void onChildDismissed(View v) {
         Log.v(TAG, "User swiped heads up to dismiss");
-        mBar.onHeadsUpDismissed();
+        mBar.onHeadsUpDismissed(DIRECTION_X);
     }
 
     @Override
@@ -406,7 +408,7 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
                         }
                         if (dY < 0) {
                             if (DEBUG_EDGE_SWIPE) Log.d(TAG, "found a close");
-                            mBar.onHeadsUpDismissed();
+                            mBar.onHeadsUpDismissed(DIRECTION_Y);
                         }
                         mConsuming = true;
                     }
