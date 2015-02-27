15be382d73904283935f37d371f675edc3839485
Jorge Ruesga <jorge@ruesga.com>
2015-02-27 20:27:14 +0100

    systemui: animate clear recents exit
    Currently, the animation just clear all the tasks at the same time. Just add
    a delay between every task remove call, and hide clear recents before start
    any animation.
    
    Change-Id: Idbdeacc68c6519d28dc875d09cd75c71e2e9f701
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -488,7 +488,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
 
         Task t = mStack.getTasks().get(mFocusedTaskIndex);
         TaskView tv = getChildViewForTask(t);
-        tv.dismissTask();
+        tv.dismissTask(0L);
     }
 
     public void dismissAllTasks() {
@@ -499,11 +499,14 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                 tasks.addAll(mStack.getTasks());
 
                 // Remove visible TaskViews
+                long dismissDelay = 0;
                 int childCount = getChildCount();
+                int delay = mConfig.taskViewRemoveAnimDuration / childCount;
                 for (int i = 0; i < childCount; i++) {
                     TaskView tv = (TaskView) getChildAt(i);
                     tasks.remove(tv.getTask());
-                    tv.dismissTask();
+                    tv.dismissTask(dismissDelay);
+                    dismissDelay += delay;
                 }
 
                 int size = tasks.size();
@@ -1193,7 +1196,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         public void run() {
                             mStack.removeTask(t);
                         }
-                    });
+                    }, 0L);
                 } else {
                     // Otherwise, remove the task from the stack immediately
                     mStack.removeTask(t);
