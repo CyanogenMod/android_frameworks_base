cd69d1bf59974b90b94fcb217105dbbe8a59cda1
Roman Birg <roman@cyngn.com>
2015-03-03 13:33:30 -0800

    SystemUI: fix crashes with custom lockscreen shortcuts
    Change-Id: If80769dca229cfbd63e82e541c15e86e2bf5656f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
 
 LOCAL_STATIC_JAVA_LIBRARIES := Keyguard \
     android-support-v7-palette \
+    android-support-v4 \
     android-visualizer
 
 LOCAL_JAVA_LIBRARIES := telephony-common
