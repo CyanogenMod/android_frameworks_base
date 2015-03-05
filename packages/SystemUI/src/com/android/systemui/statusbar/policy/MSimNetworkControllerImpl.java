676a0c5dfd19874a3b1e4f7f789673683e9d3cec
Clark Scheff <clark@cyngn.com>
2015-03-05 15:19:29 -0800

    Themes: Don't recreate network controllers
    When re-creating the network controllers we lose their current
    state, which results in a bug on multi-sim devices where the first
    sim slot will show the no connectivity icon rather than the no sim
    icon when no sim is present.
    
    Change-Id: I169734b1f5c7b17e11cc15a3434a22ed9f7bce1f
    REF: TOMATOL-160

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
@@ -275,6 +275,11 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
         refreshSignalCluster(cluster, phoneId);
     }
 
+    @Override
+    public void removeAllSignalClusters() {
+        mSimSignalClusters.clear();
+    }
+
     public void refreshSignalCluster(MSimSignalCluster cluster, int phoneId) {
         cluster.setWifiIndicators(
                 // only show wifi in the cluster if connected or if wifi-only
