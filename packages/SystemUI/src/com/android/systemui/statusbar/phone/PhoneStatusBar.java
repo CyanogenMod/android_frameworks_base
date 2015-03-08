73a01ef5692e4222c20abd7035ac8114243369ad
Steve Kondik <steve@cyngn.com>
2015-03-08 17:41:29 +0100

    systemui: Fix KeyguardMonitor callbacks during configuration changes
     * Some of these objects are destroyed onConfigurationChange, we need to make
       sure that the SBWM gets a fresh KeyguardMonitor or certain features
       will end up wildly broken.
    
    Change-Id: I430fb31916cc4e3bc262ab7f8ec1bde3b4ac0df8
    (cherry picked from commit 0beb0fcad4678241b8f5bfe13e81a8ced62cd05c)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1187,7 +1187,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 (ViewStub) mStatusBarWindowContent.findViewById(R.id.keyguard_user_switcher),
                 mKeyguardStatusBar, mNotificationPanel, mUserSwitcherController);
 
-
         // Set up the quick settings tile panel
         mQSPanel = (QSPanel) mStatusBarWindowContent.findViewById(R.id.quick_settings_panel);
         if (mQSPanel != null) {
@@ -1216,6 +1215,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mQSPanel.setTiles(qsh.getTiles());
                 }
             });
+        }
+
+        if (mStatusBarWindowManager != null) {
+            mStatusBarWindowManager.setKeyguardMonitor(mKeyguardMonitor);
         }
 
         // User info. Trigger first load.
@@ -3502,7 +3505,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private void addStatusBarWindow() {
         makeStatusBarView();
         mStatusBarWindow.addContent(mStatusBarWindowContent);
-        mStatusBarWindowManager = new StatusBarWindowManager(mContext, mKeyguardMonitor);
+        mStatusBarWindowManager = new StatusBarWindowManager(mContext);
+        mStatusBarWindowManager.setKeyguardMonitor(mKeyguardMonitor);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
     }
 
@@ -3773,6 +3777,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // Halts the old ticker. A new ticker is created in makeStatusBarView() so
         // this MUST happen before makeStatusBarView();
         haltTicker();
+
+        mKeyguardMonitor.removeCallbacks();
 
         makeStatusBarView();
         repositionNavigationBar();
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
@@ -55,26 +55,28 @@ public class StatusBarWindowManager implements KeyguardMonitor.Callback {
     private final SurfaceSession mFxSession;
     private final WindowManagerPolicy mPolicy = PolicyManager.makeNewWindowManager();
 
-    private final KeyguardMonitor mKeyguardMonitor;
+    private KeyguardMonitor mKeyguardMonitor;
 
     private static final int TYPE_LAYER_MULTIPLIER = 10000; // refer to WindowManagerService.TYPE_LAYER_MULTIPLIER
     private static final int TYPE_LAYER_OFFSET = 1000;      // refer to WindowManagerService.TYPE_LAYER_OFFSET
 
     private final State mCurrentState = new State();
 
-    public StatusBarWindowManager(Context context, KeyguardMonitor kgm) {
+    public StatusBarWindowManager(Context context) {
         mContext = context;
         mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         mKeyguardScreenRotation = shouldEnableKeyguardScreenRotation();
-
-        mKeyguardMonitor = kgm;
-        mKeyguardMonitor.addCallback(this);
 
         mKeyguardBlurEnabled = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_ui_blur_enabled);
         mFxSession = new SurfaceSession();
         mStatusBarLayer = mPolicy.windowTypeToLayerLw(WindowManager.LayoutParams.TYPE_STATUS_BAR)
                           * TYPE_LAYER_MULTIPLIER + TYPE_LAYER_OFFSET;
+    }
+
+    public void setKeyguardMonitor(KeyguardMonitor kgm) {
+        mKeyguardMonitor = kgm;
+        kgm.addCallback(this);
     }
 
     private boolean shouldEnableKeyguardScreenRotation() {
