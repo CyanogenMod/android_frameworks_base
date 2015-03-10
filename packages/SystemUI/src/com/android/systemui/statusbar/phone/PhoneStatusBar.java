1e3893b811320a0a88877ddd735eeb48747d66ae
Roman Birg <roman@cyngn.com>
2015-03-10 16:37:31 -0700

    SystemUI: fix hidden clock appearing
    We need to take into account whether the clock is hidden when we animate
    it back in to make sure we don't touch it if it is.
    
    Change-Id: I8b01a5a6b04e5773a79524a4443ec8aba9ed1e33
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2393,10 +2393,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mSystemIconArea.animate().cancel();
             mClockView.animate().cancel();
             if ((state & StatusBarManager.DISABLE_SYSTEM_INFO) != 0) {
-                animateStatusBarHide(mClockView, animate);
+                if (mClockLocation != Clock.STYLE_HIDE_CLOCK) {
+                    animateStatusBarHide(mClockView, animate);
+                }
                 animateStatusBarHide(mSystemIconArea, animate);
             } else {
-                animateStatusBarShow(mClockView, animate);
+                if (mClockLocation != Clock.STYLE_HIDE_CLOCK) {
+                    animateStatusBarShow(mClockView, animate);
+                }
                 animateStatusBarShow(mSystemIconArea, animate);
             }
         }
