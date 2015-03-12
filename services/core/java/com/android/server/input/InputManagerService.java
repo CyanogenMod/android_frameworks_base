d5aeb074e1c45772642f6d27602726f2da22781a
Matt Garnes <matt@cyngn.com>
2015-03-12 16:11:14 -0700

    Properly clean up when setting new InputFilter.
    In f1b93471565c43e57e9e199596d51f7caaec8ebd, the single InputFilter that
    is set in InputManagerService was replaced with a chain of filters that
    are all listening for InputEvents.
    
    The original field mInputFilter was replaced in this patch but not
    removed. When a new InputFilter is added, we check that this unused
    field mInputFilter != null before doing necessary teardown of the previous
    filter. Since this is always null, this causes the previous filter to
    not be disconnected when a new one is set with setInputFilter().
    
    If the user toggles "Magnification Gestures" on and off twice in Accessibility
    Settings, this will send the old and new InputFilters into a loop
    sending and receiving touch events, locking up the device completely
    until reboot.
    
    Remove all references to the unused mInputFilter field.
    
    Change-Id: I6ef67a664f1e783f3f0402b7c2a0984499a4b614

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -150,7 +150,6 @@ public class InputManagerService extends IInputManager.Stub
 
     // State for the currently installed input filter.
     final Object mInputFilterLock = new Object();
-    IInputFilter mInputFilter; // guarded by mInputFilterLock
     ChainedInputFilterHost mInputFilterHost; // guarded by mInputFilterLock
     ArrayList<ChainedInputFilterHost> mInputFilterChain =
             new ArrayList<ChainedInputFilterHost>(); // guarded by mInputFilterLock
@@ -504,20 +503,10 @@ public class InputManagerService extends IInputManager.Stub
      */
     public void setInputFilter(IInputFilter filter) {
         synchronized (mInputFilterLock) {
-            final IInputFilter oldFilter = mInputFilter;
-            if (oldFilter == filter) {
-                return; // nothing to do
-            }
-
-            if (oldFilter != null) {
+            if (mInputFilterHost != null) {
                 mInputFilterHost.disconnectLocked();
                 mInputFilterChain.remove(mInputFilterHost);
                 mInputFilterHost = null;
-                try {
-                    oldFilter.uninstall();
-                } catch (RemoteException re) {
-                    /* ignore */
-                }
             }
 
             if (filter != null) {
