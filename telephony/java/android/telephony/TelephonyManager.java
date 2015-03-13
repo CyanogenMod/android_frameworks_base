fcd21906c680a54e4f2488ec20c3328702fb2106
Preeti Ahuja <preetia@codeaurora.org>
2015-03-13 16:00:58 -0700

    Handle NPE in TelephonyManager
    ITelphony service instance could be null at boot up
    when device activation is taking place.
    
    Change-Id: If72622a2d22a20b1ce909134ce4c6446b132dffe

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3796,6 +3796,8 @@ public class TelephonyManager {
             return getITelephony().isDataPossibleForSubscription(subId, apnType);
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", npe);
         }
         return false;
     }
@@ -3807,6 +3809,8 @@ public class TelephonyManager {
             return getITelephony().needsOtaServiceProvisioning();
         } catch (RemoteException e) {
             Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", npe);
         }
         return false;
     }
