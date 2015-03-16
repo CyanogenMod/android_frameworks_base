701bc3edf35bad52305d6f7bccd153db5161b55e
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-03-16 17:21:39 +0530

    Lockscreen visualizer: Disable out animation
    Change-Id: Ic9766f1b06093caebec575aea689a018498a3aa8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -98,7 +98,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private static final Intent PHONE_INTENT = new Intent(Intent.ACTION_DIAL);
 
     // the length to animate the visualizer in and out
-    private static final int VISUALIZER_ANIMATION_DURATION = 300;
+    private static final int VISUALIZER_ANIMATION_DURATION_IN = 300;
+    private static final int VISUALIZER_ANIMATION_DURATION_OUT = 0;
 
     private KeyguardAffordanceView mCameraImageView;
     private KeyguardAffordanceView mPhoneImageView;
@@ -808,7 +809,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
             mVisualizer.animate()
                     .alpha(1f)
-                    .setDuration(VISUALIZER_ANIMATION_DURATION);
+                    .setDuration(VISUALIZER_ANIMATION_DURATION_IN);
             AsyncTask.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -828,7 +829,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
 
             mVisualizer.animate()
                     .alpha(0f)
-                    .setDuration(VISUALIZER_ANIMATION_DURATION);
+                    .setDuration(VISUALIZER_ANIMATION_DURATION_OUT);
             AsyncTask.execute(new Runnable() {
                 @Override
                 public void run() {
