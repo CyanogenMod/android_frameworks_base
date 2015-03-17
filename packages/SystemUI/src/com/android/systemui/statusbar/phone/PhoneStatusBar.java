efaa3c35e207e357fe0c053378e1cc29bca3c08b
Roman Birg <roman@cyngn.com>
2015-03-17 13:22:32 -0700

    improve lock screen wallpaper behavior
     - Fix lockscreen wallpaper not being reloaded on user switch.
       There was an issue where the wrong user id was being queried for the
       keyguard wallpaper. Always use the current user id.
    
     - Also clean up a left-over call for some regular wallpaper
       migration logic that the lock screen wallpaper
    
     - Add method for removing references to the current lock screen
       wallpaper, which we call from systemui when switching users.
    
     - Only apply the lock screen wallpaper if there is no media metadata
       to show. There were some cases when it would still appear even when
       music was playing.
    
    Change-Id: I610a38ac11a19638298ca9490b3c87b7ab6106f2
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1827,7 +1827,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         // apply user lockscreen image
-        if (backdropBitmap == null) {
+        if (mMediaMetadata == null && backdropBitmap == null) {
             WallpaperManager wm = (WallpaperManager)
                     mContext.getSystemService(Context.WALLPAPER_SERVICE);
             if (wm != null) {
@@ -3249,7 +3249,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateNotifications();
         resetUserSetupObserver();
         setControllerUsers();
+<<<<<<< HEAD
         mAssistManager.onUserSwitched(newUserId);
+=======
+
+        WallpaperManager wm = (WallpaperManager)
+                mContext.getSystemService(Context.WALLPAPER_SERVICE);
+        wm.forgetLoadedKeyguardWallpaper();
+        updateMediaMetaData(true);
+
+        if (mNavigationBarView != null) {
+            mNavigationBarView.updateSettings();
+        }
+>>>>>>> 8c7b965... improve lock screen wallpaper behavior
     }
 
     private void setControllerUsers() {
