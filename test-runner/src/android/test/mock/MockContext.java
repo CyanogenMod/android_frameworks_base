3bca252211010f6386332b92cbe929a9f879885d
d34d <clark@cyngn.com>
2015-03-20 22:33:23 -0700

    Themes: Properly hide recreateTheme
    Change-Id: Ifa23515cfbf8eaf16d2bbe26374cbdcf65929dff

--- test-runner/src/android/test/mock/MockContext.java
+++ test-runner/src/android/test/mock/MockContext.java
@@ -97,6 +97,7 @@ public class MockContext extends Context {
         throw new UnsupportedOperationException();
     }
 
+    /** @hide */
     @Override
     public void recreateTheme() {
         throw new UnsupportedOperationException();
