20e77160b49b52df80102abf52bd1714c9012a48
Scott Mertz <scott@cyngn.com>
2015-03-21 12:16:25 -0700

    PackageManager: don't attempt to get theme on core boot
    Change-Id: Id4b12279a52e53c236c61074c5d92bcb44f59a64

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1648,7 +1648,9 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
             }
 
-            mBootThemeConfig = ThemeUtils.getBootThemeDirty();
+            if (!mOnlyCore) {
+                mBootThemeConfig = ThemeUtils.getBootThemeDirty();
+            }
 
             // Collect vendor overlay packages.
             // (Do this before scanning any apps.)
