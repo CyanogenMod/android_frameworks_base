93e03711c504c75fb2447bc837dd0cd174d335e2
Nalla Kartheek <karthe@codeaurora.org>
2015-03-22 14:47:36 +0530

    Wifi: Maintain configKey generation logic same in all APIs
    AutoJoinController uses configKey to distinguish networks. It compares
    configKeys of saved network with recent ScanResult. WifiConfiguration
    has two different APIs to generate configKey based on input variables.
    Framework should use same logic to generate configkey even though input
    differs. This fix ensures the parity between configKey(boolean) and
    configKey(ScanResult) APIs.
    
    Change-Id: I4d19b3ba37d784d1a73c53df33b52c3a427b60b9
    CRs-Fixed: 806222

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -1452,14 +1452,14 @@ public class WifiConfiguration implements Parcelable {
             key = mCachedConfigKey;
         } else {
             if (allowedKeyManagement.get(KeyMgmt.WPA_PSK)) {
-                key = SSID + KeyMgmt.strings[KeyMgmt.WPA_PSK];
+                key = SSID + "-" + KeyMgmt.strings[KeyMgmt.WPA_PSK];
             } else if (allowedKeyManagement.get(KeyMgmt.WPA_EAP) ||
                     allowedKeyManagement.get(KeyMgmt.IEEE8021X)) {
-                key = SSID + KeyMgmt.strings[KeyMgmt.WPA_EAP];
+                key = SSID + "-" + KeyMgmt.strings[KeyMgmt.WPA_EAP];
             } else if (wepKeys[0] != null) {
-                key = SSID + "WEP";
+                key = SSID + "-WEP";
             } else {
-                key = SSID + KeyMgmt.strings[KeyMgmt.NONE];
+                key = SSID + "-" + KeyMgmt.strings[KeyMgmt.NONE];
             }
             mCachedConfigKey = key;
         }
@@ -1481,14 +1481,13 @@ public class WifiConfiguration implements Parcelable {
 
         if (result.capabilities.contains("WEP")) {
             key = key + "-WEP";
-        }
-
-        if (result.capabilities.contains("PSK")) {
+        } else if (result.capabilities.contains("PSK")) {
             key = key + "-" + KeyMgmt.strings[KeyMgmt.WPA_PSK];
-        }
-
-        if (result.capabilities.contains("EAP")) {
+        } else if (result.capabilities.contains("EAP") ||
+                   result.capabilities.contains("IEEE8021X")) {
             key = key + "-" + KeyMgmt.strings[KeyMgmt.WPA_EAP];
+        } else {
+            key = key + "-" + KeyMgmt.strings[KeyMgmt.NONE];
         }
 
         return key;
