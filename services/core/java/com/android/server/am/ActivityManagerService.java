6053fa08a19f9246d7f10ce45e855c19d90baff8
Steve Kondik <steve@cyngn.com>
2015-03-22 12:24:16 -0700

    activitymanager: Fix duplicate case
     * (merge with privacy guard)
    
    Change-Id: Ie886337db330f59e36ca0742a7dce5e156c347e5

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1273,8 +1273,8 @@ public final class ActivityManagerService extends ActivityManagerNative
     static final int DISMISS_DIALOG_MSG = 48;
     static final int NOTIFY_TASK_STACK_CHANGE_LISTENERS_MSG = 49;
 
-    static final int POST_PRIVACY_NOTIFICATION_MSG = 48;
-    static final int CANCEL_PRIVACY_NOTIFICATION_MSG = 49;
+    static final int POST_PRIVACY_NOTIFICATION_MSG = 60;
+    static final int CANCEL_PRIVACY_NOTIFICATION_MSG = 61;
 
     static final int FIRST_ACTIVITY_STACK_MSG = 100;
     static final int FIRST_BROADCAST_QUEUE_MSG = 200;
