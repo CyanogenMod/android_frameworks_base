5a118a017a49932db7f4089a942fab75e536ea08
Roman Birg <roman@cyngn.com>
2015-03-25 16:01:25 -0700

    SystemUI: allow WiFi & Bluetooth tile details to scroll
    We can now show all WiFi/Bluetooth devices in the lists
    
    Change-Id: Ie71971842902cb6ba9f0757f3893a7a7214ad789
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -84,7 +84,7 @@ public interface NetworkController {
 
         public interface AccessPointCallback {
             void onAccessPointsChanged(List<AccessPoint> accessPoints);
-            void onSettingsActivityTriggered(Intent settingsIntent);
+            void onSettingsActivityTriggered(Intent intent);
         }
     }
 
