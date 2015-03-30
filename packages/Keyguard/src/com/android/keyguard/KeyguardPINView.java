a19aaeb764692bed75fc76733a1346f78a3e78dc
Adnan Begovic <adnan@cyngn.com>
2015-03-29 17:49:18 -0700

    Revert "keyguard: Fix scramble pin"
    This reverts commit c3c6fdea74fb3baf0ce594e9f93b9076a52b57c9.
    
    Change-Id: I170f27cd0e42043e98caee1adc0e28a1bbd99ce3

--- packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
@@ -137,10 +137,24 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
 
         if (scramblePin) {
             Collections.shuffle(sNumbers);
+            // get all children who are NumPadKey's
+            LinearLayout bouncer = (LinearLayout) findViewById(R.id.keyguard_bouncer_frame);
+            List<NumPadKey> views = new ArrayList<NumPadKey>();
+            for (int i = 0; i < bouncer.getChildCount(); i++) {
+                if (bouncer.getChildAt(i) instanceof LinearLayout) {
+                    LinearLayout nestedLayout = ((LinearLayout) bouncer.getChildAt(i));
+                    for (int j = 0; j < nestedLayout.getChildCount(); j++){
+                        View view = nestedLayout.getChildAt(j);
+                        if (view.getClass() == NumPadKey.class) {
+                            views.add((NumPadKey) view);
+                        }
+                    }
+                }
+            }
 
             // reset the digits in the views
             for (int i = 0; i < sNumbers.size(); i++) {
-                ((NumPadKey)mViews[(i / 3) + 1][i % 3]).setDigit(sNumbers.get(i));
+                mViews[(i / 3) + 1][i % 3].setDigit(sNumbers.get(i));
             }
         }
 
