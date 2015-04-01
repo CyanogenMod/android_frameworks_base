3a0751a35dcc871042a0ae9c77d5744c7a1276dd
André Rivotti Casimiro <acasimiro@cyngn.com>
2015-04-01 13:53:04 +0100

    Fix set pattern screen after rotating the device 1/2
    Allows the usage of a temporary pattern size while setting a new one.
    
    Change-Id: I3e62c2da4f290986f6295da8dee6900454b49141

--- core/java/com/android/internal/widget/LockPatternView.java
+++ core/java/com/android/internal/widget/LockPatternView.java
@@ -1076,7 +1076,8 @@ public class LockPatternView extends View {
     protected Parcelable onSaveInstanceState() {
         Parcelable superState = super.onSaveInstanceState();
         return new SavedState(superState,
-                mLockPatternUtils == null ? "" : mLockPatternUtils.patternToString(mPattern),
+                mLockPatternUtils == null ? "" : mLockPatternUtils.patternToString(mPattern,
+                        mPatternSize),
                 mPatternDisplayMode.ordinal(), mPatternSize,
                 mInputEnabled, mInStealthMode, mEnableHapticFeedback, mVisibleDots, mShowErrorPath);
     }
@@ -1085,13 +1086,13 @@ public class LockPatternView extends View {
     protected void onRestoreInstanceState(Parcelable state) {
         final SavedState ss = (SavedState) state;
         super.onRestoreInstanceState(ss.getSuperState());
+        mPatternSize = ss.getPatternSize();
         if (mLockPatternUtils != null) {
             setPattern(
                     DisplayMode.Correct,
-                    mLockPatternUtils.stringToPattern(ss.getSerializedPattern()));
+                    mLockPatternUtils.stringToPattern(ss.getSerializedPattern(), mPatternSize));
         }
         mPatternDisplayMode = DisplayMode.values()[ss.getDisplayMode()];
-        mPatternSize = ss.getPatternSize();
         mInputEnabled = ss.isInputEnabled();
         mInStealthMode = ss.isInStealthMode();
         mEnableHapticFeedback = ss.isTactileFeedbackEnabled();
