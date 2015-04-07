00887675dc903121281b0ed69abd66cf4aae4dd7
Steve Kondik <steve@cyngn.com>
2015-04-07 12:56:26 -0400

    systemui: Boost when expanding the notification shade
    Change-Id: I3b6edaf850d7453920fe553203e74afc30dc47da

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -22,6 +22,7 @@ import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.PowerManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -202,6 +203,8 @@ public class NotificationStackScrollLayout extends ViewGroup
     private boolean mDisallowScrollingInThisMotion;
     private long mGoToFullShadeDelay;
 
+    private final PowerManager mPm;
+
     private ViewTreeObserver.OnPreDrawListener mChildrenUpdater
             = new ViewTreeObserver.OnPreDrawListener() {
         @Override
@@ -236,6 +239,8 @@ public class NotificationStackScrollLayout extends ViewGroup
                 minHeight, maxHeight);
         mExpandHelper.setEventSource(this);
         mExpandHelper.setScrollAdapter(this);
+
+        mPm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
 
         mSwipeHelper = new SwipeHelper(SwipeHelper.X, this, getContext());
         mSwipeHelper.setLongPressListener(mLongPressListener);
@@ -762,6 +767,11 @@ public class NotificationStackScrollLayout extends ViewGroup
                 && !mOnlyScrollingInThisMotion) {
             horizontalSwipeWantsIt = mSwipeHelper.onTouchEvent(ev);
         }
+
+        if (expandWantsIt && mIsBeingDragged) {
+            mPm.cpuBoost(200 * 1000);
+        }
+
         return horizontalSwipeWantsIt || scrollerWantsIt || expandWantsIt || super.onTouchEvent(ev);
     }
 
