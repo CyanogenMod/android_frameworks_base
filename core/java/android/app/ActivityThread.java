2ed573c078a27fe1411e83621f8573dfdba22fd3
Steve Kondik <steve@cyngn.com>
2015-04-09 22:39:54 -0400

    Revert "Remove unnecessary sleep in ActivityThread."
     * Causing mismatched refcounts during bootup.
    
    This reverts commit b96f7c2a5cb9db95bc16cfe38e911cec0b6fda46.
    
    Change-Id: I5fa29dc504b83909ccc210a97a9abd75644cc39c

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -5158,15 +5158,11 @@ public final class ActivityThread {
                                                     UserHandle.myUserId());
             RuntimeInit.setApplicationObject(mAppThread.asBinder());
             final IActivityManager mgr = ActivityManagerNative.getDefault();
-            new Thread() {
-                public void run() {
-                    try {
-                        mgr.attachApplication(mAppThread);
-                    } catch (RemoteException ex) {
-                        // Ignore
-                    }
-                }
-            }.start();
+            try {
+                mgr.attachApplication(mAppThread);
+            } catch (RemoteException ex) {
+                // Ignore
+            }
             // Watch for getting close to heap limit.
             BinderInternal.addGcWatcher(new Runnable() {
                 @Override public void run() {
