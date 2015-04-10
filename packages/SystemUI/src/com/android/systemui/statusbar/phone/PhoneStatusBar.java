673fc4969bc5f211254dec767db31f4499960dad
Roman Birg <roman@cyngn.com>
2015-04-10 12:02:44 -0700

    SystemUI: hide navigation bar faster in SetupWizard
    We use immersive mode in SetupWizard, and after bringing up the IME,
    and in an external activity (i.e. Google account setup), the navigation
    bar lingers around for 3000ms. This provides for a poor experience, so
    let's hide it immediately if the device is not provisioned.
    
    Change-Id: Id610ecddce75f1e21136db0d9a4160fe3d5e41bd
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -247,6 +247,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private static final int STATUS_OR_NAV_TRANSIENT =
             View.STATUS_BAR_TRANSIENT | View.NAVIGATION_BAR_TRANSIENT;
     private static final long AUTOHIDE_TIMEOUT_MS = 3000;
+    private static final long AUTOHIDE_TIMEOUT_MS_PROVISIONING = 0;
 
     /** The minimum delay in ms between reports of notification visibility. */
     private static final int VISIBILITY_REPORT_MIN_DELAY_MS = 500;
@@ -3265,7 +3266,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private void scheduleAutohide() {
         cancelAutohide();
-        mHandler.postDelayed(mAutohide, AUTOHIDE_TIMEOUT_MS);
+        mHandler.postDelayed(mAutohide, isDeviceProvisioned()
+                ? AUTOHIDE_TIMEOUT_MS : AUTOHIDE_TIMEOUT_MS_PROVISIONING);
     }
 
     private void checkUserAutohide(View v, MotionEvent event) {
