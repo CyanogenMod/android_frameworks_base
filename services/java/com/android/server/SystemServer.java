98453ae687d683f5c9a2c90478b653ac57f1ee0a
Gianmarco Reverberi <gianmarco.reverberi@gmail.com>
2015-04-11 16:21:10 +0200

    SystemServer: remove unused first boot flag
    This was used before enabling boot message on first
    boot, but now it's useless
    
    Change-Id: I1e69052e32185ca25c0037f3c54e0ce78bbc440c

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -171,7 +171,6 @@ public final class SystemServer {
     private ContentResolver mContentResolver;
 
     private boolean mOnlyCore;
-    private boolean mFirstBoot;
 
     /**
      * Called to initialize native system services.
@@ -375,7 +374,6 @@ public final class SystemServer {
         Slog.i(TAG, "Package Manager");
         mPackageManagerService = PackageManagerService.main(mSystemContext, mInstaller,
                 mFactoryTestMode != FactoryTest.FACTORY_TEST_OFF, mOnlyCore);
-        mFirstBoot = mPackageManagerService.isFirstBoot();
         mPackageManager = mSystemContext.getPackageManager();
 
         Slog.i(TAG, "User Service");
