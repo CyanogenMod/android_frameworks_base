eca44b9e0ffefdb6896009e3e234448470b1c343
Roman Birg <roman@cyngn.com>
2015-04-15 09:19:09 -0700

    Torch: remind user flashlight is still on
    Post a notification which informs the user the flashlight is still
    enabled. But only do this when the user turns the screen off *while* the
    flashlight is still on, so they see it next time they use the device.
    Tapping on the notification will disable the flashlight.
    
    Change-Id: I3689ff6498b97b813ccc10dc7dca3527fc8455aa
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
@@ -17,7 +17,13 @@
 package com.android.systemui.qs.tiles;
 
 import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.TorchManager;
 import android.os.SystemClock;
 
@@ -49,8 +55,8 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
     @Override
     protected void handleDestroy() {
-        super.handleDestroy();
         mTorchManager.removeListener(this);
+        super.handleDestroy();
     }
 
     @Override
