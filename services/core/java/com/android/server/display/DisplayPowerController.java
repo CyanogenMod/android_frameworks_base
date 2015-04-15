a3befaaf1081e8f1a26f63f61bc16182d0a2c6d0
Erica Chang <echang@cyngn.com>
2015-04-14 17:43:10 -0700

    frameworks: display: Added automatic brightness debounce configs
    Change-Id: I047936c098c2f0ddb0d5e258d28f1b7cc7169c79

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -323,7 +323,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 if (bottom < screenBrightnessRangeMinimum) {
                     screenBrightnessRangeMinimum = bottom;
                 }
-                mAutomaticBrightnessController = new AutomaticBrightnessController(this,
+                mAutomaticBrightnessController = new AutomaticBrightnessController(mContext, this,
                         handler.getLooper(), sensorManager, screenAutoBrightnessSpline,
                         lightSensorWarmUpTimeConfig, screenBrightnessRangeMinimum,
                         mScreenBrightnessRangeMaximum);
