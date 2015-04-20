0633f9e23f39028cbbcf069c96dd3dff9d3621e3
Christopher R. Palmer <crpalmer@gmail.com>
2015-04-20 05:27:18 -0400

    doze: Do not bother checking proximity for the DOZE_ACTION intent
    The intent is a desire to show the intent and trust the sender to
    determine whether or not it should really be shown.
    
    Change-Id: I1c613071b136de61f12bc0556283a80e1248a4a8

--- packages/SystemUI/src/com/android/systemui/doze/DozeService.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -217,7 +217,8 @@ public class DozeService extends DreamService {
             // Here we need a wakelock to stay awake until the pulse is finished.
             mWakeLock.acquire();
             mPulsing = true;
-            if (!mDozeParameters.getProxCheckBeforePulse()) {
+            if (!mDozeParameters.getProxCheckBeforePulse() ||
+                    reason == DozeLog.PULSE_REASON_INTENT) {
                 // skip proximity check
                 continuePulsing(reason);
                 return;
