9dfdc9216432ecfeef52cafa818dfeeb4576235d
Roman Birg <roman@cyngn.com>
2015-04-21 13:13:06 -0700

    SystemUI: add a disabled state for Quick Tiles
    Some tiles may want to be visible, but not allow any user interaction
    due to security reasons. Add a visual disabled state for tiles which are
    not enabled.
    
    With this patch, the Profiles tile and the Lockscreen toggle tile
    disable themselves on a secure lock screen.
    
    Ref: CYNGNOS-1597
    
    Change-Id: I65ec7837661483d238d8dc3fa18419f76c8029dd
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 6fb2c183a3a769382f63109fa5a15937692253d1)

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LockscreenToggleTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LockscreenToggleTile.java
@@ -75,6 +75,7 @@ public class LockscreenToggleTile extends QSTile<QSTile.BooleanState>
         if (listening) {
             mSettingsObserver.observe();
             mKeyguard.addCallback(this);
+            refreshState();
         } else {
             mSettingsObserver.unobserve();
             mKeyguard.removeCallback(this);
@@ -89,6 +90,7 @@ public class LockscreenToggleTile extends QSTile<QSTile.BooleanState>
     @Override
     protected void handleClick() {
         setPersistedState(!mPersistedState);
+        refreshState();
     }
 
     @Override
@@ -104,7 +106,8 @@ public class LockscreenToggleTile extends QSTile<QSTile.BooleanState>
                 || mKeyguardViewMediator.getKeyguardEnabledInternal();
 
         state.value = lockscreenEnabled;
-        state.visible = !mKeyguard.isShowing() || !mKeyguard.isSecure();
+        state.visible = mKeyguardViewMediator.isKeyguardBound();
+        state.enabled = !mKeyguard.isShowing() || !mKeyguard.isSecure();
         state.label = mContext.getString(lockscreenEnforced
                 ? R.string.quick_settings_lockscreen_label_enforced
                 : R.string.quick_settings_lockscreen_label);
