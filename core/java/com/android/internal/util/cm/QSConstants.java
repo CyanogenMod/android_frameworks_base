472792b0024991fb22672ddb44f8bcdb4136a61e
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    QS: Add heads up tile (1/2)
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -47,6 +47,7 @@ public class QSConstants {
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_LIVE_DISPLAY = "live_display";
     public static final String TILE_USB_TETHER = "usb_tether";
+    public static final String TILE_HEADS_UP = "heads_up";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -84,5 +85,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
         TILES_AVAILABLE.add(TILE_USB_TETHER);
+        TILES_AVAILABLE.add(TILE_HEADS_UP);
     }
 }
