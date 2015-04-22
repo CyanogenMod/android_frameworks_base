80d89b300ec9ba9255323c7be4e70e41bfaf3210
Dan Pasanen <dan.pasanen@gmail.com>
2015-04-21 23:19:44 -0500

    TelephonyManager: don't dump a stack trace on a null tether apn
    * This being null seems to be a perfectly reasonable situation
    * Please stop clogging up my radio logs
    
    Change-Id: Id119b5f6988999e026e4e98f6a53194303cbdc6f

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3670,7 +3670,6 @@ public class TelephonyManager {
         } catch (RemoteException ex) {
             Rlog.e(TAG, "hasMatchedTetherApnSetting RemoteException", ex);
         } catch (NullPointerException ex) {
-            Rlog.e(TAG, "hasMatchedTetherApnSetting NPE", ex);
         }
         return 2;
     }
