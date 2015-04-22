9ad73a960efce403e164a098aeb8b564e072e42a
Alexander Martinz <eviscerationls@gmail.com>
2015-04-22 23:49:12 +0200

    Keyguard: only tint the sim card drawable when multisim is enabled
    Change-Id: Ia0634a9ace63496f806ac8345bcc47bfc0b415cb
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
@@ -437,9 +437,11 @@ public class KeyguardSimPukView extends KeyguardPinBasedInputView {
 
         mSubNameView.setText(displayName);
 
-        final int color = info != null && info.getIconTint() != 0
-                ? info.getIconTint() : Color.WHITE;
-        mSimImageView.setImageTintList(ColorStateList.valueOf(color));
+        if (mKgUpdateMonitor.getNumPhones() > 1) {
+            final int color = info != null && info.getIconTint() != 0
+                    ? info.getIconTint() : Color.WHITE;
+            mSimImageView.setImageTintList(ColorStateList.valueOf(color));
+        }
     }
 
     private void showDefaultMessage() {
