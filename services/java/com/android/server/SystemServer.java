09093645573fd85d46ba9c1caebbf500738f2e9e
Adnan Begovic <adnan@cyngn.com>
2015-04-26 02:29:03 -0700

    SystemServer: Allow starting of overlaid external services.
      This allows external services overlaid in vendor/ whose implementation is
      in a different framework jar to be explicitly started via reflection
      similar to WIFI_SERVICE
    
    Change-Id: I119eda4ab0f447b5bee88e7634a20bdd569ef017

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -454,6 +454,8 @@ public final class SystemServer {
         boolean digitalPenCapable =
             Resources.getSystem().getBoolean(com.android.internal.R.bool.config_digitalPenCapable);
         boolean disableAtlas = SystemProperties.getBoolean("config.disable_atlas", false);
+        String[] externalServices = Resources.getSystem()
+                .getStringArray(com.android.internal.R.array.config_externalCMServices);
 
         try {
             Slog.i(TAG, "Reading configuration...");
@@ -1195,6 +1197,15 @@ public final class SystemServer {
                 edgeGestureService.systemReady();
             } catch (Throwable e) {
                 reportWtf("making EdgeGesture service ready", e);
+            }
+        }
+
+        for (String service : externalServices) {
+            try {
+                Slog.i(TAG, service);
+                mSystemServiceManager.startService(service);
+            } catch (Throwable e) {
+                Slog.e(TAG, "Failure starting " + service , e);
             }
         }
 
