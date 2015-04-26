047c3953e1bce805b21b694081ef89ef7e0f2bb1
Noeljunior <liamgliam@gmail.com>
2015-04-26 11:46:43 +0100

    QS: Add Ambient Display Tile (1/2)
    Change-Id: I31ffd7d83a65db294759374900da0fca1c760549

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -48,6 +48,7 @@ public class QSConstants {
     public static final String TILE_LIVE_DISPLAY = "live_display";
     public static final String TILE_USB_TETHER = "usb_tether";
     public static final String TILE_HEADS_UP = "heads_up";
+    public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -86,5 +87,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
         TILES_AVAILABLE.add(TILE_USB_TETHER);
         TILES_AVAILABLE.add(TILE_HEADS_UP);
+        TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
     }
 }
