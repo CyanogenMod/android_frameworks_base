c4de5997db96051a6d08245f6941a066820f054a
bmc08gt <brandon.mcansh@gmail.com>
2015-04-27 17:08:57 +0200

    SystemBars: Allow the StatusBarCompoent by externally defined
    * The default UI for fugu is a TV with no statusbar, however we allow for a full android
        experience with a mouse and keyboard as well.
    * Allow ro.cm.StatusBarComponent=<component> be set in the build prop to enable the desired statubar.
    
    * A HomeSwitcher is to be created for TvSettings and will also set the STATUSBAR_COMPONENT Settings.dB value,
         depending on if LeanbackLauncher or Trebuchet is the default.
    
    * Order of preference of components:
          -> Prop
          -> Database (System.STATUSBAR_COMPONENT)
          -> config value
    
    Change-Id: I8c0c9bccf9283960abd40b55a7c9ff47e3a97317
    Signed-off-by: bmc08gt <brandon.mcansh@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3509,6 +3509,13 @@ public final class Settings {
         public static final String NONE_IS_SILENT = "none_is_silent";
 
         /**
+         * The statusbar configuration to be used for its creation in SystemUI
+         * Fallback is the config_statusBarComponent value
+         * @hide
+         */
+        public static final String STATUSBAR_COMPONENT = "statusbar_component";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
