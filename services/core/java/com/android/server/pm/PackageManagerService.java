ba2bae231616f3a608e4eaf8d6e0c3168fdeea75
Ethan Chen <intervigil@gmail.com>
2015-04-30 06:56:18 +0000

    Revert "Show Optimizing app dialog on first boot"
    This reverts commit 2038e683e4acd72d39f517a189ede10b69d4587b.
    
    Change-Id: Iaa9ec1bc5692607fb5b0b01c04c3fdbca903da3a

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4942,11 +4942,13 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (DEBUG_DEXOPT) {
             Log.i(TAG, "Optimizing app " + curr + " of " + total + ": " + pkg.packageName);
         }
-        try {
-            ActivityManagerNative.getDefault().showBootMessage(
-                    mContext.getResources().getString(R.string.android_upgrading_apk,
-                            curr, total), true);
-        } catch (RemoteException e) {
+        if (!isFirstBoot()) {
+            try {
+                ActivityManagerNative.getDefault().showBootMessage(
+                        mContext.getResources().getString(R.string.android_upgrading_apk,
+                                curr, total), true);
+            } catch (RemoteException e) {
+            }
         }
         PackageParser.Package p = pkg;
         synchronized (mInstallLock) {
