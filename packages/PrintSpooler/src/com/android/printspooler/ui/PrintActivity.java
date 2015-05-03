47b0575433134ecc352adabc58139ce4d644461a
Naman Dwivedi <namandwivedi14@gmail.com>
2015-05-03 13:23:58 +0000

    Revert "printspooler: Print view crash."
    This reverts commit e053d115d524d1e70b83a6d0fe2654326a89c60d.
    
    Open webpage in chrome(or any thing to print) and click print and then press back, throws a IllegalStateException.
    This commit added the doFinsh() call in onDestroy, doFinish() is called which further calls unbindService() in PrintSpoolerProvider, but since service has already been unbinded, it throws a Service not registered error.
    
    Change-Id: I118dd1abbdb7c541fad672143404ec9713643c64

--- packages/PrintSpooler/src/com/android/printspooler/ui/PrintActivity.java
+++ packages/PrintSpooler/src/com/android/printspooler/ui/PrintActivity.java
@@ -366,12 +366,6 @@ public class PrintActivity extends Activity implements RemotePrintDocument.Updat
     }
 
     @Override
-    protected void onDestroy() {
-        doFinish();
-        super.onDestroy();
-    }
-
-    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             event.startTracking();
