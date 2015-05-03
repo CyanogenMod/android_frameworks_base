39a23793dbbd4afaee1874bb1beb73393d27a4df
Matt Garnes <matt@cyngn.com>
2015-05-03 11:12:41 -0700

    Profiles: Add custom action support.
    Change-Id: I1ac79c805df1151957e1f2cec3b8bd517b6266bf

--- core/java/android/app/Profile.java
+++ core/java/android/app/Profile.java
@@ -79,6 +79,8 @@ public final class Profile implements Parcelable, Comparable {
 
     private BrightnessSettings mBrightness = new BrightnessSettings();
 
+    private CustomActions mCustomActions = new CustomActions();
+
     private int mScreenLockMode = LockMode.DEFAULT;
 
     private int mExpandedDesktopMode = ExpandedDesktopMode.DEFAULT;
@@ -101,7 +103,7 @@ public final class Profile implements Parcelable, Comparable {
     public static class TriggerType {
         public static final int WIFI = 0;
         public static final int BLUETOOTH = 1;
-        public static final int STRING = 2;
+        public static final int STRING = 3;
     }
 
     /** @hide */
@@ -423,6 +425,7 @@ public final class Profile implements Parcelable, Comparable {
         dest.writeParcelable(mRingMode, flags);
         dest.writeParcelable(mAirplaneMode, flags);
         dest.writeParcelable(mBrightness, flags);
+        dest.writeParcelable(mCustomActions, flags);
         dest.writeInt(mScreenLockMode);
         dest.writeMap(mTriggers);
         dest.writeInt(mExpandedDesktopMode);
@@ -458,6 +461,7 @@ public final class Profile implements Parcelable, Comparable {
         mRingMode = (RingModeSettings) in.readParcelable(null);
         mAirplaneMode = (AirplaneModeSettings) in.readParcelable(null);
         mBrightness = (BrightnessSettings) in.readParcelable(null);
+        mCustomActions = (CustomActions) in.readParcelable(null);
         mScreenLockMode = in.readInt();
         in.readMap(mTriggers, null);
         mExpandedDesktopMode = in.readInt();
@@ -580,6 +584,15 @@ public final class Profile implements Parcelable, Comparable {
         mDirty = true;
     }
 
+    public CustomActions getCustomActions() {
+        return mCustomActions;
+    }
+
+    public void setCustomActions(CustomActions actions) {
+        mCustomActions = actions;
+        mDirty = true;
+    }
+
     /** @hide */
     public boolean isDirty() {
         if (mDirty) {
@@ -607,6 +620,9 @@ public final class Profile implements Parcelable, Comparable {
             return true;
         }
         if (mBrightness.isDirty()) {
+            return true;
+        }
+        if (mCustomActions.isDirty()) {
             return true;
         }
         return false;
@@ -653,6 +669,8 @@ public final class Profile implements Parcelable, Comparable {
         mAirplaneMode.getXmlString(builder, context);
 
         mBrightness.getXmlString(builder, context);
+
+        mCustomActions.getXmlString(builder, context);
 
         mRingMode.getXmlString(builder, context);
 
@@ -784,6 +802,10 @@ public final class Profile implements Parcelable, Comparable {
                     BrightnessSettings bd = BrightnessSettings.fromXml(xpp, context);
                     profile.setBrightness(bd);
                 }
+                if (name.equals("customActions")) {
+                    CustomActions customActions = CustomActions.fromXml(xpp, context);
+                    profile.setCustomActions(customActions);
+                }
                 if (name.equals("screen-lock-mode")) {
                     profile.setScreenLockMode(Integer.valueOf(xpp.nextText()));
                 }
@@ -837,6 +859,9 @@ public final class Profile implements Parcelable, Comparable {
 
         // Set brightness
         mBrightness.processOverride(context);
+
+        // Send the custom actions
+        mCustomActions.processActions(context);
 
         // Set expanded desktop
         // if (mExpandedDesktopMode != ExpandedDesktopMode.DEFAULT) {
