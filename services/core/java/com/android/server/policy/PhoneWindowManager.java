173a4d8ab994d5d0c1015887a077bd13f605f97f
Roman Birg <roman@cyngn.com>
2015-05-06 12:08:44 -0700

    DreamManager: be aware of device lid
    If the device lid controls whether the screen goes to sleep, the
    DreamManager (and so also DozeMode) should be aware and not try to
    activate itself if the cover is closed.
    
    Change-Id: Ie855a8a403548bcabbf1e0824e5cb128110066c2
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6989,27 +6989,36 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void applyLidSwitchState() {
         mPowerManager.setKeyboardVisibility(isBuiltInKeyboardVisible());
-
-        if (mLidState == LID_CLOSED && mLidControlsSleep) {
-            if (mFocusedWindow != null && (mFocusedWindow.getAttrs().flags
-                    & WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON) != 0) {
-                // if an application requests that the screen be turned on
-                // and there's a closed device cover, don't turn the screen off!
-                return;
+        if (mLidControlsSleep) {
+            IDreamManager dreamManager = getDreamManager();
+            if (dreamManager != null) {
+                try {
+                    dreamManager.setLidState(mLidState);
+                } catch (RemoteException e) {
+                }
             }
 
-            TelecomManager telephonyService = getTelecommService();
-            if (!(telephonyService == null
-                    || telephonyService.isRinging())) {
-                mPowerManager.goToSleep(SystemClock.uptimeMillis(),
-                        PowerManager.GO_TO_SLEEP_REASON_LID_SWITCH,
-                        PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE);
+            if (mLidState == LID_CLOSED) {
+                if (mFocusedWindow != null && (mFocusedWindow.getAttrs().flags
+                        & WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON) != 0) {
+                    // if an application requests that the screen be turned on
+                    // and there's a closed device cover, don't turn the screen off!
+                    return;
+                }
+
+                TelecomManager telephonyService = getTelecommService();
+                if (!(telephonyService == null
+                        || telephonyService.isRinging())) {
+                    mPowerManager.goToSleep(SystemClock.uptimeMillis(),
+                            PowerManager.GO_TO_SLEEP_REASON_LID_SWITCH,
+                            PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE);
+                }
+
             }
 
-        }
-
-        synchronized (mLock) {
-            updateWakeGestureListenerLp();
+            synchronized (mLock) {
+                updateWakeGestureListenerLp();
+            }
         }
     }
 
