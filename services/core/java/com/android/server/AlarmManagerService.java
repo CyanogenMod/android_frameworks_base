2c29d53490fb7de209bec5c2b63ec923055a248e
Charles Tsai <charles_tsai@htc.com>
2015-05-07 20:13:07 +0800

    Reducing computation time while doing alarm re-batching
    Enhancement of issue 75287:
    http://code.google.com/p/android/issues/detail?id=75287
    
    Not necessary to remove alarm for rebatching,
    because each alarm in rebatching procedure is mutually unique.
    Skipping this to reduce computation time of rebatching;
    it will improve performance while AlarmManager.cancel() invoked.
    Otherwise, application may suffer in ANR.
    
    Change-Id: Idaba4b9b22c0553a20ed80932f058d8b363687d7
    Signed-off-by: Charles Tsai <charles_tsai@htc.com>

--- services/core/java/com/android/server/AlarmManagerService.java
+++ services/core/java/com/android/server/AlarmManagerService.java
@@ -561,7 +561,7 @@ class AlarmManagerService extends SystemService {
                 }
                 setImplLocked(a.type, a.when, whenElapsed, a.windowLength, maxElapsed,
                         a.repeatInterval, a.operation, batch.standalone, doValidate, a.workSource,
-                        a.alarmClock, a.userId, false);
+                        a.alarmClock, a.userId, false, true);
             }
         }
     }
@@ -808,19 +808,24 @@ class AlarmManagerService extends SystemService {
             }
             setImplLocked(type, triggerAtTime, triggerElapsed, windowLength, maxElapsed,
                     interval, operation, isStandalone, true, workSource, alarmClock, userId,
-                    wakeupFiltered);
+                    wakeupFiltered, false);
         }
     }
 
     private void setImplLocked(int type, long when, long whenElapsed, long windowLength,
             long maxWhen, long interval, PendingIntent operation, boolean isStandalone,
             boolean doValidate, WorkSource workSource, AlarmManager.AlarmClockInfo alarmClock,
-            int userId, boolean wakeupFiltered) {
+            int userId, boolean wakeupFiltered, boolean isRebatching) {
         Alarm a = new Alarm(type, when, whenElapsed, windowLength, maxWhen, interval,
                 operation, workSource, alarmClock, userId);
 
         // Remove this alarm if already scheduled.
-        final boolean foundExistingWakeup = removeWithStatusLocked(operation);
+        final boolean foundExistingWakeup;
+        if (!isRebatching) {
+            foundExistingWakeup = removeWithStatusLocked(operation);
+        } else {
+            foundExistingWakeup = false;
+        }
 
         // note AppOp for accounting purposes
         // skip if the alarm already existed
@@ -1605,7 +1610,7 @@ class AlarmManagerService extends SystemService {
                     setImplLocked(alarm.type, alarm.when + delta, nextElapsed, alarm.windowLength,
                             maxTriggerTime(nowELAPSED, nextElapsed, alarm.repeatInterval),
                             alarm.repeatInterval, alarm.operation, batch.standalone, true,
-                            alarm.workSource, alarm.alarmClock, alarm.userId, false);
+                            alarm.workSource, alarm.alarmClock, alarm.userId, false, false);
                 }
 
                 if (alarm.wakeup) {
