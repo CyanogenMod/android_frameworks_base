59c97bd81d9ee11ef623ded7ecd9149245d8082a
d34d <clark@cyngn.com>
2015-05-11 11:43:41 -0700

    Remove resources.apk from ZipSet when removing overlay
    When a theme that is currently applied is updated, the resources.apk
    prior to the update remains in the AssetManager ZipSet and is still
    used when loading themed assets.  This patch removes the resources.apk
    from the ZipSet so that the new one can be correctly loaded in.
    
    Change-Id: Ia0b415a3598779825090f365581d374bf51c812d
    REF: THEMES-639

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -1054,6 +1054,9 @@ bool AssetManager::removeOverlayPath(const String8& packageName, int32_t cookie)
         return false;
     }
 
+    const asset_path& oap = mAssetPaths.itemAt(which);
+    mZipSet.closeZip(oap.resApkPath);
+
     mAssetPaths.removeAt(which);
 
     ResTable* rt = mResources;
@@ -2323,6 +2326,10 @@ ZipFileRO* AssetManager::ZipSet::getZip(const String8& path)
         mZipFile.editItemAt(idx) = zip;
     }
     return zip->getZip();
+}
+
+void AssetManager::ZipSet::closeZip(const String8& zip) {
+    closeZip(getIndex(zip));
 }
 
 Asset* AssetManager::ZipSet::getZipResourceTableAsset(const String8& path)
