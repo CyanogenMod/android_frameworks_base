fd6ea1862be5582a61c30f70751704a8a2f57f78
Christopher R. Palmer <crpalmer@gmail.com>
2015-05-10 21:25:43 -0400

    dozeui: Broadcast an intent when beginning a doze pulse
    This will allow other components that need to update their
    state to properly render the lock screen to do so rather
    than display stale information.
    
    Change-Id: I383c657c1b760fe8610ef431d494c0fe05d63fdb

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -2858,6 +2858,10 @@ public class Intent implements Parcelable, Cloneable {
     /** {@hide} */
     public static final String ACTION_MASTER_CLEAR = "android.intent.action.MASTER_CLEAR";
 
+    /** {@hide} */
+    public static final String ACTION_DOZE_PULSE_STARTING =
+            "android.intent.action.DOZE_PULSE_STARTING";
+
     // ---------------------------------------------------------------------
     // ---------------------------------------------------------------------
     // Standard intent categories (see addCategory()).
