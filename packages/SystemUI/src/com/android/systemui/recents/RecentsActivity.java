988e7b8b4ec4732a0bbdc695513f2a5a97793ba0
Veeti Paananen <veeti.paananen@rojekti.fi>
2015-05-16 18:53:42 +0300

    Make sure the search bar space height is initialized
    Fixes broken enter animation with the search bar enabled that would
    persist until a configuration change inside the recents activity.
    
    Also reverted pointless static variable introduced in
    c5017462f54b6b665238173da4ae1e1ffa3358ff.
    
    Change-Id: Ib316949b1f94b998835ed044d837aeb6df5f19dc

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -275,10 +275,10 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
             // Update search bar space height
             if (showSearchBar) {
-                RecentsConfiguration.searchBarSpaceHeightPx = getResources().getDimensionPixelSize(
+                mConfig.searchBarSpaceHeightPx = getResources().getDimensionPixelSize(
                     R.dimen.recents_search_bar_space_height);
             } else {
-                RecentsConfiguration.searchBarSpaceHeightPx = 0;
+                mConfig.searchBarSpaceHeightPx = 0;
             }
         }
 
--- packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsConfiguration.java
@@ -77,7 +77,7 @@ public class RecentsConfiguration {
 
     /** Search bar */
     int searchBarAppWidgetId = -1;
-    public static int searchBarSpaceHeightPx;
+    public int searchBarSpaceHeightPx;
 
     /** Task stack */
     public int taskStackScrollDuration;
@@ -211,6 +211,7 @@ public class RecentsConfiguration {
         maxNumTasksToLoad = ActivityManager.getMaxRecentTasksStatic();
 
         // Search Bar
+        searchBarSpaceHeightPx = res.getDimensionPixelSize(R.dimen.recents_search_bar_space_height);
         searchBarAppWidgetId = settings.getInt(Constants.Values.App.Key_SearchAppWidgetId, -1);
 
         // Task stack
