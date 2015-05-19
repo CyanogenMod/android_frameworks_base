7724a1f8b27b355b9048394c14fc838bfab73738
Veeti Paananen <veeti.paananen@rojekti.fi>
2015-05-19 23:04:07 +0300

    Align recents clear button to screen edge instead of tasks if close
    Change-Id: I903088a34762d13c489b2f81eeb8ff3db13e0981

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -358,6 +358,10 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             } else {
                 // Adjust to task views
                 params.rightMargin = (width / 2) - (taskViewWidth / 2);
+
+                // If very close to the screen edge, align to it
+                if (params.rightMargin < mClearRecents.getWidth())
+                    params.rightMargin = width - taskStackBounds.right;
             }
             mClearRecents.setLayoutParams(params);
         }
