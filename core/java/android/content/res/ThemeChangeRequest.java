6a43d207fc45e2c25adfed375fb54c0dadf6bb28
d34d <clark@cyngn.com>
2015-05-20 14:28:40 -0700

    Live lock screen support [1/2]
    Change-Id: I15bc712b2a143f72adbfa61fad94fcfe18b98379

--- core/java/android/content/res/ThemeChangeRequest.java
+++ core/java/android/content/res/ThemeChangeRequest.java
@@ -79,6 +79,10 @@ public final class ThemeChangeRequest implements Parcelable {
         return getThemePackageNameForComponent(MODIFIES_RINGTONES);
     }
 
+    public String getLiveLockScreenThemePackageName() {
+        return getThemePackageNameForComponent(MODIFIES_LIVE_LOCK_SCREEN);
+    }
+
     public final Map<String, String> getThemeComponentsMap() {
         return Collections.unmodifiableMap(mThemeComponents);
     }
@@ -237,6 +241,10 @@ public final class ThemeChangeRequest implements Parcelable {
 
         public Builder setRingtone(String pkgName) {
             return setComponent(MODIFIES_RINGTONES, pkgName);
+        }
+
+        public Builder setLiveLockScreen(String pkgName) {
+            return setComponent(MODIFIES_LIVE_LOCK_SCREEN, pkgName);
         }
 
         public Builder setComponent(String component, String pkgName) {
