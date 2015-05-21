c403c9bb284436c1c4daab2717aa73936d732593
Roman Birg <roman@cyngn.com>
2015-05-21 06:22:24 -0700

    unbreak density scaling
    Only apply density scaling to built in screens so we don't affect
    overlays.
    
    Change-Id: I0b4849141cf081694aae5668ae60764e70be7db1
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/view/DisplayInfo.java
+++ core/java/android/view/DisplayInfo.java
@@ -441,6 +441,9 @@ public final class DisplayInfo implements Parcelable {
         outMetrics.scaledDensity = outMetrics.noncompatScaledDensity = outMetrics.density;
         outMetrics.xdpi = outMetrics.noncompatXdpi = physicalXDpi;
         outMetrics.ydpi = outMetrics.noncompatYdpi = physicalYDpi;
+        if (type == Display.TYPE_BUILT_IN) {
+            outMetrics.updateDensity();
+        }
 
         if (!compatInfo.equals(CompatibilityInfo.DEFAULT_COMPATIBILITY_INFO)) {
             compatInfo.applyToDisplayMetrics(outMetrics);
