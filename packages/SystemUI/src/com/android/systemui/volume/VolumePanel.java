5267772e902913818f5cabfce31313ddb8e8ebbf
William C. Garrison III <bill@cs.pitt.edu>
2015-05-21 00:08:49 -0400

    VolumePanel: Properly update expanded volume panel sliders
    Fixed a series of bugs where volume sliders are not updated when
    toggling ringer modes or changing Zen mode with the expanded volume
    panel
    
    updateStates() on line 720 is needed to enable (resp. disable) the
    sliders when volume is unmuted (resp. muted) by clicking the icon.
    
    On lines 1593--1597, we need to check whether the volume panel is
    expanded: if so, changing Zen modes may require updating more than
    just the active stream's slider.
    
    updateStates() on line 1682 is needed to enable the sliders when
    volume is unmuted by dragging the slider from muted state.
    
    Change-Id: I3f6c01320a53b6ea37abae9145d7d520d3ea46e0

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -717,6 +717,7 @@ public class VolumePanel extends Handler implements DemoMode {
                         public void onClick(View v) {
                             resetTimeout();
                             toggleRinger(sc);
+                            updateStates();
                         }
                     });
                 }
@@ -1589,7 +1590,11 @@ public class VolumePanel extends Handler implements DemoMode {
             case MSG_INTERNAL_RINGER_MODE_CHANGED:
             case MSG_NOTIFICATION_EFFECTS_SUPPRESSOR_CHANGED: {
                 if (isShowing()) {
-                    updateActiveSlider();
+                    if (mExtendedPanelExpanded) {
+                        updateStates();
+                    } else {
+                        updateActiveSlider();
+                    }
                 }
                 break;
             }
@@ -1674,6 +1679,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 StreamControl sc = (StreamControl) tag;
                 setStreamVolume(sc, progress,
                         AudioManager.FLAG_SHOW_UI | AudioManager.FLAG_VIBRATE);
+                updateStates();
             }
             resetTimeout();
         }
