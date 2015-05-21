96a2c3410f3c71d3ab20857036422f1d64c3a6d3
Steve Kondik <steve@cyngn.com>
2015-05-21 13:10:54 -0700

    power: Fix locking for wakeup proximity check
     * Often I'm noticing high power consumption due to the proximity sensor
       never turning off. Add synchronization around this code to ensure
       ordering.
    
    Change-Id: I8ccf3152166cd896e0ce2551c01b8ac8f501d782

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -3004,6 +3004,12 @@ public final class PowerManagerService extends SystemService
     }
 
     private void cleanupProximity() {
+        synchronized (mProximityWakeLock) {
+            cleanupProximityLocked();
+        }
+    }
+
+    private void cleanupProximityLocked() {
         if (mProximityWakeLock.isHeld()) {
             mProximityWakeLock.release();
         }
@@ -3277,28 +3283,30 @@ public final class PowerManagerService extends SystemService
                 r.run();
                 return;
             }
-            mProximityWakeLock.acquire();
-            mProximityListener = new SensorEventListener() {
-                @Override
-                public void onSensorChanged(SensorEvent event) {
-                    cleanupProximity();
-                    if (!mHandler.hasMessages(MSG_WAKE_UP)) {
-                        Slog.w(TAG, "The proximity sensor took too long, wake event already triggered!");
-                        return;
+            synchronized (mProximityWakeLock) {
+                mProximityWakeLock.acquire();
+                mProximityListener = new SensorEventListener() {
+                    @Override
+                    public void onSensorChanged(SensorEvent event) {
+                        cleanupProximityLocked();
+                        if (!mHandler.hasMessages(MSG_WAKE_UP)) {
+                            Slog.w(TAG, "The proximity sensor took too long, wake event already triggered!");
+                            return;
+                        }
+                        mHandler.removeMessages(MSG_WAKE_UP);
+                        float distance = event.values[0];
+                        if (distance >= PROXIMITY_NEAR_THRESHOLD ||
+                                distance >= mProximitySensor.getMaximumRange()) {
+                            r.run();
+                        }
                     }
-                    mHandler.removeMessages(MSG_WAKE_UP);
-                    float distance = event.values[0];
-                    if (distance >= PROXIMITY_NEAR_THRESHOLD ||
-                            distance >= mProximitySensor.getMaximumRange()) {
-                        r.run();
-                    }
-                }
 
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-            };
-            mSensorManager.registerListener(mProximityListener,
-                   mProximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+                    @Override
+                    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+                };
+                mSensorManager.registerListener(mProximityListener,
+                       mProximitySensor, SensorManager.SENSOR_DELAY_FASTEST);
+            }
         }
 
         @Override // Binder call
