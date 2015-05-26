5dc6390dfd24aa93c121786a1b5a41d3f64b769c
Andy Mast <andy@cyngn.com>
2015-05-26 10:59:20 -0700

    Theme Versioning/Capabilities
    Change-Id: Id37031f353e059ef1e57f3d5d19a6c0f8d3c5ad1

---
+++ core/java/android/app/ThemeVersion.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.app;
+
+/**
+ * Warning: Careful moving/refactoring this class as our SDK references it.
+ * ThemeVersion 1 = CM11
+ * ThemeVersion 2 = CM12/CM12.1 First Release
+ * ThemeVersion 3 = CM12.1 W/ Wallpaper Packs
+ * @hide
+ */
+public class ThemeVersion {
+    /**
+     *  Increment this anytime changes are made to:
+     *  1) Changes to ThemesContract
+     *  2) Changes to ThemeService API
+     *  3) Changes to ThemeManager API
+     */
+    public static int THEME_VERSION = 3;
+
+    /**
+     * Change this if a change to the contract or service would break compatibility.
+     * Example: A client app like chooser might be outdated from the framework.
+     * It could then query the FW for this value and determine whether its safe to proceed.
+     */
+    public static int MIN_SUPPORTED_THEME_VERSION = 1;
+
+    /**
+     * Do not change the order of this. See SDK.
+     * Increment the minSupportedVersion when the fw can no longer support a theme's apk structure
+     * Increment currentVersion when a change to the theme's apk structure is changed
+     * For example, CM11 to CM12 introduces new resources to overlay, so the overlays
+     * version should change. Because the changes are not compatible with CM11, the minVersion
+     * must change as well.
+     *
+     * If a new feature is added to a component (ex rotations in icon packs), the current version
+     * for the ICON component would be incremented. If a new component is created, then add it
+     * to the enum list.
+     *
+     */
+    public static enum ComponentVersion {
+        OVERLAY(2, 2),
+        BOOT_ANIM(1, 1),
+        WALLPAPER(1, 1),
+        LOCKSCREEN(1, 1),
+        FONT(1, 2),
+        ICON(1, 1),
+        SOUNDS(1, 1);
+
+        public int minSupportedVersion;
+        public int currentVersion;
+
+        private ComponentVersion(int minSupportedVersion, int currentVersion) {
+            this.minSupportedVersion = minSupportedVersion;
+            this.currentVersion = currentVersion;
+        }
+    }
+}
