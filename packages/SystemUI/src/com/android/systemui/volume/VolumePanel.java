dc0ec88a1b8df7ed6ce0354fccebd3bb7e27cfed
William C. Garrison III <bill@cs.pitt.edu>
2015-05-26 11:58:35 -0400

    VolumePanel: show hint for unlinked notification slider too
    Change-Id: Ia79e5ed5d6516e61a8833dca07f0a8de2afae654

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -927,6 +927,8 @@ public class VolumePanel extends Handler implements DemoMode {
     private void updateSliderEnabled(final StreamControl sc, boolean muted, boolean fixedVolume) {
         final boolean wasEnabled = sc.seekbarView.isEnabled();
         final boolean isRinger = isNotificationOrRing(sc.streamType);
+        final boolean isUnlinkedNotification =
+                !mVolumeLinkNotification && sc.streamType == AudioManager.STREAM_NOTIFICATION;
         if (sc.streamType == STREAM_REMOTE_MUSIC) {
             // never disable touch interactions for remote playback, the muting is not tied to
             // the state of the phone.
@@ -945,7 +947,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 (sc.streamType != mAudioManager.getMasterStreamType() && !isRinger && muted) ||
                 (sSafetyWarning != null)) {
             sc.seekbarView.setEnabled(false);
-            if (!mVolumeLinkNotification && sc.streamType == AudioManager.STREAM_NOTIFICATION) {
+            if (isUnlinkedNotification) {
                 sc.icon.setEnabled(false);
                 sc.icon.setAlpha(mDisabledAlpha);
             }
@@ -954,17 +956,22 @@ public class VolumePanel extends Handler implements DemoMode {
             sc.icon.setEnabled(true);
             sc.icon.setAlpha(1f);
         }
-        // show the silent hint when the disabled slider is touched in silent mode
-        if (isRinger && wasEnabled != sc.seekbarView.isEnabled()) {
+        // show the appropriate hint when a disabled slider is touched
+        if ((isRinger || isUnlinkedNotification) && wasEnabled != sc.seekbarView.isEnabled()) {
             if (sc.seekbarView.isEnabled()) {
                 sc.group.setOnTouchListener(null);
-                sc.icon.setClickable(mHasVibrator);
+                sc.icon.setClickable(isRinger && mHasVibrator);
             } else {
                 final View.OnTouchListener showHintOnTouch = new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
                         resetTimeout();
-                        showSilentHint();
+                        if (mAudioManager.getRingerModeInternal() ==
+                                AudioManager.RINGER_MODE_VIBRATE) {
+                            showRingerHint();
+                        } else {
+                            showSilentHint();
+                        }
                         return false;
                     }
                 };
@@ -976,6 +983,13 @@ public class VolumePanel extends Handler implements DemoMode {
     private void showSilentHint() {
         if (mZenPanel != null) {
             mZenPanel.showSilentHint();
+        }
+    }
+
+    private void showRingerHint() {
+        final StreamControl ringer = mStreamControls.get(AudioManager.STREAM_RING);
+        if (ringer != null) {
+            mIconPulser.start(ringer.icon);
         }
     }
 
