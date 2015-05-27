b5f13f0e5c5e4f8c41c0f6e39cf026c020542174
fire855 <thefire855@gmail.com>
2015-05-27 23:21:02 +0200

    Revert "opengl: Add GLES31 and GLES31Ext classes"
    This reverts commit 7ab63acdd0a257272512d0bcf5e06036fa0b9fdf.

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -35,8 +35,6 @@ LOCAL_SRC_FILES:= \
 	android_opengl_GLES11Ext.cpp \
 	android_opengl_GLES20.cpp \
 	android_opengl_GLES30.cpp \
-	android_opengl_GLES31.cpp \
-	android_opengl_GLES31Ext.cpp \
 	android_database_CursorWindow.cpp \
 	android_database_SQLiteCommon.cpp \
 	android_database_SQLiteConnection.cpp \
