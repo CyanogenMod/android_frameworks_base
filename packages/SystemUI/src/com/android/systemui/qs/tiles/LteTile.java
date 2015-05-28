d3db2aa7535ebc4b262d0387fae7af7977bd983d
emancebo <emancebo@cyngn.com>
2015-05-28 11:16:41 -0700

    quick settings tiles: add missing LTE modes to LteTile
    Change-Id: I07584d0d22f4e535c3ccf75d614347f62157a859

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
@@ -73,6 +73,9 @@ public class LteTile extends QSTile<QSTile.BooleanState> {
             case Phone.NT_MODE_LTE_ONLY:
             case Phone.NT_MODE_LTE_WCDMA:
             case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_TD_SCDMA_LTE:
+            case Phone.NT_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_TD_SCDMA_GSM_LTE:
             case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
             case Phone.NT_MODE_TD_SCDMA_WCDMA_LTE:
                 state.visible = true;
