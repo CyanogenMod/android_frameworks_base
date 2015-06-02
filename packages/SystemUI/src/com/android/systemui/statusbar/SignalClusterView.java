bd8e85d62f9984b8c42513482d1523a1d6f46508
LuK1337 <priv.luk@gmail.com>
2015-06-02 16:13:39 +0200

    SystemUI: use smaller padding for battery
    before:
    - single sim (thea): http://imgur.com/cDDXbQI,VIF3Fpo,BnUrpM8,x72ULxt,bxWvo47
    - multi sim (titan): http://imgur.com/nzYyfcq,BqLCYAK,04ms3ko,nGzuZpl,EyHyzNU
    
    after:
    - single sim (thea): http://imgur.com/3Rn7mQS,2NPta3o,fkiCXZF,xd4nbgt,mxTrks5
    - multi sim (titan): http://imgur.com/hqZjExZ,C16MrmZ,jaZGRRk,Lo8BP5k,QNbjTF3
    
    Change-Id: I02be624cdeea9ac0d9a4b79e851449facbec1901

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -88,8 +88,6 @@ public class SignalClusterView
 
     private int mWideTypeIconStartPadding;
     private int mSecondaryTelephonyPadding;
-    private int mEndPadding;
-    private int mEndPaddingNothingVisible;
 
     private boolean mBlockAirplane;
     private boolean mBlockMobile;
@@ -150,10 +148,6 @@ public class SignalClusterView
                 R.dimen.wide_type_icon_start_padding);
         mSecondaryTelephonyPadding = getContext().getResources().getDimensionPixelSize(
                 R.dimen.secondary_telephony_padding);
-        mEndPadding = getContext().getResources().getDimensionPixelSize(
-                R.dimen.signal_cluster_battery_padding);
-        mEndPaddingNothingVisible = getContext().getResources().getDimensionPixelSize(
-                R.dimen.no_signal_cluster_battery_padding);
     }
 
     @Override
@@ -447,10 +441,6 @@ public class SignalClusterView
         }
 
         mNoSimsCombo.setVisibility(mNoSimsVisible ? View.VISIBLE : View.GONE);
-
-        boolean anythingVisible = mNoSimsVisible || mWifiVisible || mIsAirplaneMode
-                || anyMobileVisible || mVpnVisible || mEthernetVisible;
-        setPaddingRelative(0, 0, anythingVisible ? mEndPadding : mEndPaddingNothingVisible, 0);
     }
 
     public void setIconTint(int tint, float darkIntensity) {
