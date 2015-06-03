e4902f77cd54b50ed9b34f55e9351f5d32fc78f8
d34d <clark@cyngn.com>
2015-06-03 13:08:20 -0700

    Keyguard: Third party keyguard support
    Enables support for third party keyguards.  Currently for use with
    the themable animated lock screen.
    
    This is currently only for system apps or apps signed with the same
    key as the system.
    
    Change-Id: Ifcbf9368d951da3ea7ab8e6205d08acb078d8e5d

--- packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSecurityModel.java
@@ -34,6 +34,7 @@ public class KeyguardSecurityModel {
     public enum SecurityMode {
         Invalid, // NULL state
         None, // No security enabled
+        ThirdParty, // Insecure 3rd party
         Pattern, // Unlock by drawing a pattern.
         Password, // Unlock by entering an alphanumeric password
         PIN, // Strictly numeric password
@@ -109,6 +110,10 @@ public class KeyguardSecurityModel {
                         mode = mLockPatternUtils.isPermanentlyLocked() ?
                             SecurityMode.Account : SecurityMode.Pattern;
                     }
+                    break;
+                case DevicePolicyManager.PASSWORD_THIRD_PARTY_UNSECURED:
+                        // currently set this to none and let systemui handle the rest
+                        mode = SecurityMode.ThirdParty;
                     break;
 
                 default:
