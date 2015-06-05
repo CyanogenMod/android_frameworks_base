122d76e99ee0f7858eab2536042fb0e75f9f9f5a
Stephen Bird <sbird@cyngn.com>
2015-06-05 16:08:25 -0700

    MediaScanner: Detect folders
    If folders are ignored, MTP will show folders as files
    
    Change-Id: I960b92ed2f17c7da635d369e3c37560e167485a1
    (cherry picked from commit 63b5c3dc0d75d17e4d35b3397faa5409a53b8e99)
    (cherry picked from commit 0e543c26b691925a67dbe60210aee1561850cee9)

--- media/java/android/media/MediaScanner.java
+++ media/java/android/media/MediaScanner.java
@@ -1364,7 +1364,7 @@ public class MediaScanner
 
             // always scan the file, so we can return the content://media Uri for existing files
             return mClient.doScanFile(path, mimeType, lastModifiedSeconds, file.length(),
-                    false, true, MediaScanner.isNoMediaPath(path));
+                    file.isDirectory(), true, MediaScanner.isNoMediaPath(path));
         } catch (RemoteException e) {
             Log.e(TAG, "RemoteException in MediaScanner.scanFile()", e);
             return null;
