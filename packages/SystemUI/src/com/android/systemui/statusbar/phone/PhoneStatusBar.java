ebe3b5c67d141ae78816b6e92fbaddf660149815
Roman Birg <roman@cyngn.com>
2015-06-09 23:28:39 +0000

    Revert "SystemUI: Fix no LED indication for missed call when screen off"
    This reverts commit fb724357a54bdd33813d49253041c8f679343c79.
    
    Change-Id: I724f2a80459cb7d617b271934cd28d2e3c2a046e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2694,9 +2694,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarWindowManager.setStatusBarExpanded(true);
         mStatusBarView.setFocusable(false);
 
-        if (!force) {
-            visibilityChanged(true);
-        }
+        visibilityChanged(true);
         mWaitingForKeyguardExit = false;
         disable(mDisabledUnmodified, !force /* animate */);
         setInteracting(StatusBarManager.WINDOW_STATUS_BAR, true);
