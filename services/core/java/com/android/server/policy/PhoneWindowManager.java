6826db0ba4a322b7108ae738ef747ad609ea33c7
d34d <clark@cyngn.com>
2015-06-09 09:47:41 -0700

    Don't set mHomeConsumed to true for KEY_ACTION_SLEEP
    When the device goes to sleep it does not receive the key up event
    which would normally set mHomeConsumed to false.  This behavior
    causes the next double tap action to be missed.  Not setting
    mHomeConsumed to true for KEY_ACTION_SLEEP fixes this issue.
    
    Change-Id: If78b637c8abf5eac5894194be2dd8645012a259a

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3178,7 +3178,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
                     performKeyAction(mDoubleTapOnHomeBehavior, event);
-                    mHomeConsumed = true;
+                    mHomeConsumed = mDoubleTapOnHomeBehavior != KEY_ACTION_SLEEP;
                 } else if (mLongPressOnHomeBehavior == KEY_ACTION_APP_SWITCH
                         || mDoubleTapOnHomeBehavior == KEY_ACTION_APP_SWITCH) {
                     preloadRecentApps();
