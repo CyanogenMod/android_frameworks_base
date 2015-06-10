f0be7d0cef1dbc143669d73c121503d435ea79dc
blong <blong@codeaurora.org>
2015-06-10 10:38:26 +0800

    Filter internal account when set device owner
    Change-Id: I1bf3fe42e7e3d27e8cf443c9386edb178384a402

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -22,6 +22,7 @@ import static android.app.admin.DevicePolicyManager.WIPE_RESET_PROTECTION_DATA;
 import static android.content.pm.PackageManager.GET_UNINSTALLED_PACKAGES;
 
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.ActivityManagerNative;
@@ -4094,7 +4095,20 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
     private boolean allowedToSetDeviceOwnerOnDevice() {
         int callingId = Binder.getCallingUid();
         if (callingId == Process.SHELL_UID || callingId == Process.ROOT_UID) {
-            return AccountManager.get(mContext).getAccounts().length == 0;
+            Account[] mAccounts = AccountManager.get(mContext).getAccounts();
+            if(mAccounts.length == 0) {
+                return true;
+            } else {
+                int i = mAccounts.length;
+                for (Account account : mAccounts) {
+                    if (account.type
+                            .equals("com.qualcomm.qti.calendarlocalaccount")
+                            || account.type.equals("com.android.localphone")
+                            || account.type.equals("com.android.sim"))
+                        i--;
+                }
+                return i == 0;
+            }
         } else {
             return !hasUserSetupCompleted(UserHandle.USER_OWNER);
         }
