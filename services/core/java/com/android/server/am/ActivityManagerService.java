16208a2d198dff1da9bf23426ab60411615b7027
heydabop <heydabop@gmail.com>
2015-06-12 01:13:24 -0500

    fix build
    Change-Id: Ib87773c24290ccf4d7cfe325268c2f90b7454da4

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18859,7 +18859,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
     }
 
-    private postKillProc(final ProcessRecord app, final String reason, final boolean noisy) {
+    private void postKillProc(final ProcessRecord app, final String reason, final boolean noisy) {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
