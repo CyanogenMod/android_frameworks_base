d0f364b19b6de133d738ef61a6e09edbdaf7be43
Jorge Ruesga <jorge@ruesga.com>
2015-06-12 08:04:19 +0200

    qs: user external resource package for custom tiles
    Load resources from the passed resource package instead of the context of the published
    custom tile
    
    Change-Id: Id53961215764cb7cb1cd72ddc284a0ae0f83b895
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CustomQSTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CustomQSTile.java
@@ -119,7 +119,7 @@ public class CustomQSTile extends QSTile<QSTile.State> {
         state.iconId = 0;
         final int iconId = customTile.icon;
         if (iconId != 0) {
-            final String iconPackage = mTile.getPackage();
+            final String iconPackage = mTile.getResPkg();
             if (!TextUtils.isEmpty(iconPackage)) {
                 state.icon = new ExternalIcon(iconPackage, iconId);
             } else {
