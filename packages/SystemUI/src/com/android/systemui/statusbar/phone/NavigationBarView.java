2bd6a43f93f73f1b287ac0db364868a7a7eaa0d4
Roman Birg <roman@cyngn.com>
2015-06-15 10:09:48 -0700

    SystemUI: don't recreate NavBarEditor on every rotate
    Change-Id: I9882085b2a16e306a96342a537045b2db111a07b
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -30,7 +30,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -62,7 +61,6 @@ import com.android.systemui.statusbar.policy.KeyButtonView;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class NavigationBarView extends LinearLayout {
     final static boolean DEBUG = false;
@@ -74,7 +72,7 @@ public class NavigationBarView extends LinearLayout {
     final static String NAVBAR_EDIT_ACTION = "android.intent.action.NAVBAR_EDIT";
 
     private boolean mInEditMode;
-    private NavbarEditor mEditBar;
+    private NavbarEditor mLandscapeNavEditor, mPortraitNavEditor;
     private NavBarReceiver mNavBarReceiver;
     private OnClickListener mRecentsClickListener;
     private OnTouchListener mRecentsPreloadListener;
@@ -573,7 +571,19 @@ public class NavigationBarView extends LinearLayout {
         } else {
             mVertical = getWidth() > 0 && getHeight() > getWidth();
         }
-        mEditBar = new NavbarEditor(mCurrentView, mVertical, mIsLayoutRtl);
+
+        if (mPortraitNavEditor == null) {
+            mPortraitNavEditor = new NavbarEditor(
+                    mRotatedViews[Configuration.ORIENTATION_PORTRAIT], false);
+        }
+        mPortraitNavEditor.setRtl(mIsLayoutRtl);
+
+        if (mLandscapeNavEditor == null) {
+            mLandscapeNavEditor = new NavbarEditor(
+                    mRotatedViews[Configuration.ORIENTATION_LANDSCAPE], true);
+        }
+        mLandscapeNavEditor.setRtl(mIsLayoutRtl);
+
         updateSettings();
         getImeSwitchButton().setOnClickListener(mImeSwitcherClickListener);
 
@@ -657,6 +667,12 @@ public class NavigationBarView extends LinearLayout {
                 .getLayoutDirection() == LAYOUT_DIRECTION_RTL;
         if (mIsLayoutRtl != isLayoutRtl) {
             mIsLayoutRtl = isLayoutRtl;
+            if (mPortraitNavEditor != null) {
+                mPortraitNavEditor.setRtl(mIsLayoutRtl);
+            }
+            if (mLandscapeNavEditor != null) {
+                mLandscapeNavEditor.setRtl(mIsLayoutRtl);
+            }
             reorient();
         }
     }
@@ -854,12 +870,15 @@ public class NavigationBarView extends LinearLayout {
                 mInEditMode = edit;
                 if (edit) {
                     removeButtonListeners();
-                    mEditBar.setEditMode(true);
+                    mPortraitNavEditor.setEditMode(true);
+                    mLandscapeNavEditor.setEditMode(true);
                 } else {
                     if (save) {
-                        mEditBar.saveKeys();
+                        mPortraitNavEditor.saveKeys();
+                        mLandscapeNavEditor.saveKeys();
                     }
-                    mEditBar.setEditMode(false);
+                    mPortraitNavEditor.setEditMode(false);
+                    mLandscapeNavEditor.setEditMode(false);
                     updateSettings();
                 }
             }
@@ -867,7 +886,8 @@ public class NavigationBarView extends LinearLayout {
     }
 
     public void updateSettings() {
-        mEditBar.updateKeys();
+        mPortraitNavEditor.updateKeys();
+        mLandscapeNavEditor.updateKeys();
         removeButtonListeners();
         updateButtonListeners();
         setDisabledFlags(mDisabledFlags, true /* force */);
