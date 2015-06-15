e11dc5b7acadcf667e63430939014dca11fa039c
LuK1337 <priv.luk@gmail.com>
2015-06-15 18:35:31 +0200

    SystemUI: HeadsUp: Check if view isn't already in windowManager
    Log: http://pastebin.com/cLSKwxwa
    
    Change-Id: Ibfc9325ed83f15360c0957fd50c5826362a3f45c

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1732,7 +1732,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         lp.packageName = mContext.getPackageName();
         lp.windowAnimations = R.style.Animation_StatusBar_HeadsUp;
 
-        mWindowManager.addView(mHeadsUpNotificationView, lp);
+        if (!mHeadsUpNotificationView.isAttachedToWindow()) {
+            mWindowManager.addView(mHeadsUpNotificationView, lp);
+        } else {
+            mWindowManager.updateViewLayout(mHeadsUpNotificationView, lp);
+        }
     }
 
     private void removeHeadsUpView() {
@@ -4058,6 +4062,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         makeStatusBarView();
         repositionNavigationBar();
         addHeadsUpView();
+
+        if (mUseHeadsUp) {
+            addHeadsUpView();
+        }
+
         if (mNavigationBarView != null) {
             mNavigationBarView.updateResources(getNavbarThemedResources());
         }
