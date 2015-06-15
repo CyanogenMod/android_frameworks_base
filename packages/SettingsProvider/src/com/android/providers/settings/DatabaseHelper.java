f166866cd68efa963534c5bc7fc9ca38e4aa2838
Kra1o5 <kra1o5x@gmail.com>
2015-06-15 10:49:58 +0200

    Revert "Determine default 12/24 hour time format setting from locale."
    This reverts commit 164fba7c8312b4931bce1128d46c8e31e778c048.
    
    Change-Id: I543493ce851514dafa94a22c929e17e85e537bb7

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -47,7 +47,6 @@ import android.provider.Settings.Global;
 import android.provider.Settings.Secure;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
-import android.text.format.DateFormat;
 import android.util.Log;
 
 import com.android.internal.content.PackageHelper;
@@ -2604,9 +2603,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
 
-            boolean is24Hour = DateFormat.is24HourFormatLocale(mContext);
-            String defaultTimeFormat = is24Hour ? "24" : "12";
-            loadSetting(stmt, Settings.System.TIME_12_24, defaultTimeFormat);
+            loadStringSetting(stmt, Settings.System.TIME_12_24,
+                    R.string.def_time_format);
 
             loadStringSetting(stmt, Settings.System.DATE_FORMAT,
                     R.string.def_date_format);
