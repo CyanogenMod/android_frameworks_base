1be9bd1a453cbae6aaf7b38371b81a979e00e951
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:12:19 +0300

    lockscreen: Add option for showing unlock screen directly
     * Option appears on PIN, pattern and password methods
     * User can press back button to see swipe-up-to-unlock screen
     * Instantly hide keyguard if Smart Lock has unlocked phone
    
    Change-Id: I2ceeed348d7e61632b8344f35431f30cba62ca4f
    Ticket-Id: CYNGNOS-1873
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    
    SettingsProvider: Add missing platform sdk dependency
    Change-Id: I4844eaa0a989bf634a2251e95dc84a505037c012
    
    The change-id below is so that Gerrit won't get confused.
    Change-Id: I0cfb43bfb631fce532dfacdf56455309a04ddfa2

--- packages/SettingsProvider/Android.mk
+++ packages/SettingsProvider/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files) \
     src/com/android/providers/settings/EventLogTags.logtags
 
 LOCAL_JAVA_LIBRARIES := telephony-common ims-common
+LOCAL_STATIC_JAVA_LIBRARIES := org.cyanogenmod.platform.sdk
 
 LOCAL_PACKAGE_NAME := SettingsProvider
 LOCAL_CERTIFICATE := platform
