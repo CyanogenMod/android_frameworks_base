c9e70bc01910eed3f4dffe6f24554d04263edb7f
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:12:19 +0300

    lockscreen: Add option for showing unlock screen directly (1/3)
    * Option appears on PIN,pattern and password methods
    * User should press back button to see swipe-up-to-unlock screen.
    * Instantly hides keyguard if Smart Lock has unlocked phone.
    
    CYNGNOS-1873
    
    Change-Id: I2ceeed348d7e61632b8344f35431f30cba62ca4f
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    (cherry picked from commit 5711b9e460e76ee6dd6c3bbcf5565dcb650cd111)

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1726,7 +1726,7 @@ public class KeyguardViewMediator extends SystemUI {
     private void handleReset() {
         synchronized (KeyguardViewMediator.this) {
             if (DEBUG) Log.d(TAG, "handleReset");
-            mStatusBarKeyguardViewManager.reset();
+            mStatusBarKeyguardViewManager.reset(false);
         }
     }
 
