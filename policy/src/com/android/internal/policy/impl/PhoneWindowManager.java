2b22017044dfc477ee5ff2c7b9dd92c0055691ca
eray orçunus <erayorcunus@gmail.com>
2015-06-22 22:47:40 +0300

    Rotation related corrections
    - There are some conditions deleted while placing rotation angles code, I added them.
    
    - Rotation lock was screwed up since CM 12. I haven't seen any device 
    has CM 12 and rotation lock at the same time, I don't know is there any. 
    I've looked to changes in Lollipop and I've seen Google added 
    config_allowAllRotations condition for it. Since CM using user configurable
    allowed rotations for years, I've placed is current rotation one of the allowed
    rotations check instead of areAllRotationsAllowed.
    
    - Also, a CAF commit had killed rotation lock ability.
    
    Change-Id: I8f1b468249c68e7b6514d1a96bdb3fc638af84fd
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -6353,25 +6353,29 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 // Rotation setting bitmask
                 // 1=0 2=90 4=180 8=270
                 boolean allowed = true;
-                if (mUserRotationAngles < 0) {
-                    // Not set by user so use these defaults
-                    mUserRotationAngles = mAllowAllRotations == 1 ?
+                if(orientation != ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+                        && orientation != ActivityInfo.SCREEN_ORIENTATION_FULL_USER)
+                {
+                    if (mUserRotationAngles < 0) {
+                        // Not set by user so use these defaults
+                        mUserRotationAngles = mAllowAllRotations == 1 ?
                             (1 | 2 | 4 | 8) : // All angles
-                                (1 | 2 | 8); // All except 180
-                }
-                switch (sensorRotation) {
-                    case Surface.ROTATION_0:
-                        allowed = (mUserRotationAngles & 1) != 0;
-                        break;
-                    case Surface.ROTATION_90:
-                        allowed = (mUserRotationAngles & 2) != 0;
-                        break;
-                    case Surface.ROTATION_180:
-                        allowed = (mUserRotationAngles & 4) != 0;
-                        break;
-                    case Surface.ROTATION_270:
-                        allowed = (mUserRotationAngles & 8) != 0;
-                        break;
+                            (1 | 2 | 8); // All except 180
+                    }
+                    switch (sensorRotation){
+                        case Surface.ROTATION_0:
+                            allowed = (mUserRotationAngles & 1) != 0;
+                            break;
+                        case Surface.ROTATION_90:
+                            allowed = (mUserRotationAngles & 2) != 0;
+                            break;
+                        case Surface.ROTATION_180:
+                            allowed = (mUserRotationAngles & 4) != 0;
+                            break;
+                        case Surface.ROTATION_270:
+                            allowed = (mUserRotationAngles & 8) != 0;
+                            break;
+                    }
                 }
                 if (allowed) {
                     preferredRotation = sensorRotation;
