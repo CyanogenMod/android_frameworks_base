2717b06b5b2632a142046adae2919d79b9e9c212
Roman Birg <roman@cyngn.com>
2015-06-24 17:47:51 -0700

    SystemUI: dynamically generate visualizer color
    Change-Id: I3d317e02433f995ffb8ace5cd7dcd63a5c75883a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2286,6 +2286,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 backdropBitmap = mMediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
                 // might still be null
             }
+            mBackdrop.updateVisualizerColor(backdropBitmap);
         }
 
         // apply user lockscreen image
