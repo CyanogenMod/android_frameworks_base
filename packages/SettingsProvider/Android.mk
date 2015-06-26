f8548c680c1716709845162b9e71060432992228
Adnan Begovic <adnan@cyngn.com>
2015-06-25 17:37:13 -0700

    frameworks: Factor out Profiles API to CMSDK.
    Change-Id: I58ef6ca9abc9d525a48a105793b421a6897f37b8

--- packages/SettingsProvider/Android.mk
+++ packages/SettingsProvider/Android.mk
@@ -11,6 +11,9 @@ LOCAL_PACKAGE_NAME := SettingsProvider
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.sdk
+
 include $(BUILD_PACKAGE)
 
 ########################
