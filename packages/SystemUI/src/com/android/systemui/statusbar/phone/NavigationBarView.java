e8785f2749f78d5d48fc7704fe10448a7ef68c8b
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-07-01 23:56:22 -0400

    NavigationBarView: Set buttonWithTagVisibility for Search
    * a67d350 set this correctly for home, recent, and back, but neglected Search which leaves it
         visible on secure lockscreen during pin/password entry.
    * Set the button with tag visibility so it is hidden with the rest of the buttons
    
    OSS-JIRA : NIGHTLIES-1476
    
    Change-Id: I64b7ca216e04ae2509fa8048544ab3b47aef32ab
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -504,6 +504,7 @@ public class NavigationBarView extends LinearLayout {
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_BACK, !disableBack);
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_HOME, !disableHome);
         setButtonWithTagVisibility(NavbarEditor.NAVBAR_RECENT, !disableRecent);
+        setButtonWithTagVisibility(NavbarEditor.NAVBAR_SEARCH, !disableSearch);
 
         mBarTransitions.applyBackButtonQuiescentAlpha(mBarTransitions.getMode(), true /*animate*/);
     }
