912c1a759a279d0dcad5547f73b35a938aac4c1c
Rob <spirit2457@gmail.com>
2015-07-03 20:44:57 +0100

    Revert "Wait until the Keyguard is drawn to dismiss the boot dialog."
    This reverts commit 1718ee0cd308881f67271e17b6565cc507050655.
    
    Change-Id: I8c347fb23f71b049737ddb3103e1b3b10b40c80d

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -348,6 +348,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int[] mNavigationBarHeightForRotation = new int[4];
     int[] mNavigationBarWidthForRotation = new int[4];
 
+    boolean mBootMessageNeedsHiding;
     KeyguardServiceDelegate mKeyguardDelegate;
     final Runnable mWindowManagerDrawCallback = new Runnable() {
         @Override
@@ -361,7 +362,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         public void onShown(IBinder windowToken) {
             if (DEBUG_WAKEUP) Slog.d(TAG, "mKeyguardDelegate.ShowListener.onShown.");
             mHandler.sendEmptyMessage(MSG_KEYGUARD_DRAWN_COMPLETE);
-            hideBootMessages();
         }
     };
 
@@ -6124,6 +6124,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!mKeyguardDrawnOnce && mAwake) {
                 mKeyguardDrawnOnce = true;
                 enableScreen = true;
+                if (mBootMessageNeedsHiding) {
+                    mBootMessageNeedsHiding = false;
+                    hideBootMessages();
+                }
             } else {
                 enableScreen = false;
             }
@@ -6143,8 +6147,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void handleHideBootMessage() {
         synchronized (mLock) {
-            if (!mKeyguardDrawComplete) {
-                return; // keyguard hasn't completed drawing, not done booting.
+            if (!mKeyguardDrawnOnce) {
+                mBootMessageNeedsHiding = true;
+                return; // keyguard hasn't drawn the first time yet, not done booting
             }
         }
 
