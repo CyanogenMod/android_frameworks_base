66f0471a888d1363cf7815d7b9150f8f7e61223f
Steve Kondik <steve@cyngn.com>
2015-07-04 01:45:28 -0700

    livedisplay: Fix outdoor mode detection
     * Query CmHardware to determine if outdoor mode is available for the
       QS tile.
    
    Change-Id: I0d6e0f160d25feac86133ded6bccad945d546bbd
    (cherry picked from commit 4b1337e5a8e6fc05bc34c036291c3cccfa9d2659)

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -16,10 +16,12 @@
 
 package com.android.systemui.qs.tiles;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.database.ContentObserver;
+import android.hardware.CmHardwareManager;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -69,9 +71,11 @@ public class LiveDisplayTile extends QSTile<LiveDisplayTile.LiveDisplayState> {
         mAnnouncementEntries = res.getStringArray(R.array.live_display_announcement);
         mValues = res.getStringArray(com.android.internal.R.array.live_display_values);
 
-        mOutdoorModeAvailable = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.DISPLAY_AUTO_OUTDOOR_MODE,
-                -1, UserHandle.USER_CURRENT) > -1;
+        final CmHardwareManager hardware =
+                (CmHardwareManager) mContext.getSystemService(Context.CMHW_SERVICE);
+
+        mOutdoorModeAvailable =
+                hardware.isSupported(CmHardwareManager.FEATURE_SUNLIGHT_ENHANCEMENT);
 
         mDefaultDayTemperature = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_dayColorTemperature);
