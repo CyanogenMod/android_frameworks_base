8b2bd154b56f2628f4fb687d55cba6f24d216ff8
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-07-06 01:47:16 -0700

    Revert "Wait until the Keyguard is drawn to dismiss the boot dialog."
    This reverts commit e6074260e10044f969009bb6b8552efe80fe1eb3.
    
    Change-Id: I2a7b948d3f9f2a62b90a4bfa0e85a73108979ab8

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -347,6 +347,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int[] mNavigationBarHeightForRotation = new int[4];
     int[] mNavigationBarWidthForRotation = new int[4];
 
+    boolean mBootMessageNeedsHiding;
     KeyguardServiceDelegate mKeyguardDelegate;
     final Runnable mWindowManagerDrawCallback = new Runnable() {
         @Override
@@ -360,7 +361,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         public void onShown(IBinder windowToken) {
             if (DEBUG_WAKEUP) Slog.d(TAG, "mKeyguardDelegate.ShowListener.onShown.");
             mHandler.sendEmptyMessage(MSG_KEYGUARD_DRAWN_COMPLETE);
-            hideBootMessages();
         }
     };
 
@@ -6123,6 +6123,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (!mKeyguardDrawnOnce && mAwake) {
                 mKeyguardDrawnOnce = true;
                 enableScreen = true;
+                if (mBootMessageNeedsHiding) {
+                    mBootMessageNeedsHiding = false;
+                    hideBootMessages();
+                }
             } else {
                 enableScreen = false;
             }
@@ -6142,8 +6146,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void handleHideBootMessage() {
         synchronized (mLock) {
-            if (!mKeyguardDrawComplete) {
-                return; // keyguard hasn't completed drawing, not done booting.
+            if (!mKeyguardDrawnOnce) {
+                mBootMessageNeedsHiding = true;
+                return; // keyguard hasn't drawn the first time yet, not done booting
             }
         }
 
