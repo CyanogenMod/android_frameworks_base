0866fdeb89537b4bf55fe9e171be7874e1c14462
Andy Mast <andy@cyngn.com>
2015-07-09 16:35:36 -0700

    Keyguard Fingerprint: Send Unlock Stats
    Change-Id: I2feac5db2604cef85ea9eafd69e1bc2272ff2d93

--- packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1268,10 +1268,20 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         return mFailedAttempts;
     }
 
+    public int getFailedFingerprintUnlockAttempts() {
+        return mFailedFingerprintAttempts;
+    }
+
     public void clearFailedUnlockAttempts() {
+        clearFailedUnlockAttempts(false);
+    }
+
+    public void clearFailedUnlockAttempts(boolean clearFingers) {
         mFailedAttempts = 0;
         mFailedBiometricUnlockAttempts = 0;
-        mFailedFingerprintAttempts = 0;
+        if (clearFingers) {
+            mFailedFingerprintAttempts = 0;
+        }
     }
 
     public void startFingerAuthIfUsingFingerprint() {
@@ -1294,6 +1304,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
 
     public void clearFingerprintRecognized() {
         mUserFingerprintRecognized.clear();
+    }
+
+    public boolean isFingerprintRecognized() {
+       return (mUserFingerprintRecognized.size() > 0);
     }
 
     public void reportFailedUnlockAttempt() {
