CyanogenMod/android_frameworks_base

commit 83b589ec9c743174ac473622adb3f759e9d598af

Update indonesia country code in PhoneNumberUtils
Change-Id: Ia8735d21c5a28721b2d06322c44e14b7a0f16e71
(cherry picked from commit 030132cbd4422a40c94a341312d34685c31f48ad)

telephony/java/android/telephony/PhoneNumberUtils.java
@@ -155,7 +155,7 @@ public class PhoneNumberUtils
     private static Pattern sCdmaLocalRewritePattern;
     static {
         sCdmaLocalRewriteWhitelist = new SparseArray<RewriteRule>();
-        addRewriteRule(62, "IN", "0"); // indonesia
+        addRewriteRule(62, "ID", "0"); // indonesia
 
         StringBuffer regex = new StringBuffer();
         regex.append("[+](");

commit a3cb300dc3e71e9437b794c26d9e31de9a23bb8f

Fix build break introduced in 83b589ec9c743174ac473622adb3f759e9d598af
Change-Id: I44380084be69afd30153f1779ad35a6e29e4092a

telephony/java/android/telephony/PhoneNumberUtils.java
@@ -41,6 +41,8 @@ import android.text.style.TtsSpan;
 import android.util.SparseIntArray;
 
 import static com.android.internal.telephony.PhoneConstants.SUBSCRIPTION_KEY;
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ISO_COUNTRY;
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_OPERATOR_ISO_COUNTRY;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_OPERATOR_IDP_STRING;
 
 import java.util.Locale;