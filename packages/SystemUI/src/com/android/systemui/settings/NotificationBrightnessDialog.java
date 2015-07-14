206c0a51394262dce9bea7f79064731563ce2e85
AdrianDC <radian.dc@gmail.com>
2015-07-14 10:58:39 +0200

    Illumination Bar [3/3]: Multiple LEDs and brightness support
    Implement a support for the multiple LEDs setting
    and a overall brightness control for the LEDs.
    
    The settings are deactivated by default
    and will be ignored by the other phones.
    Current LibLights will simply not use the new variables.
    
    SONY Illumination Bar will be activated on phone like the Xperia SP,
    allowing the users to choose between the 3 LEDs or the basic 1,
    and brightness setting allows to set the maximum brightness
    on all notifications, preventing single apps to be at 100%.
    
    Changes includes :
      framework/base
      hardware/libhardware
      packages/Apps/Settings
    
    Device LibLights library rewritten for the SONY Huashan :
      https://github.com/AdrianDC/android_device_sony_huashan/tree/cm-12.1/liblights
    
    Screenshot of the Settings :
      http://forum.xda-developers.com/showpost.php?p=61856458&postcount=17
    
    Status of the changes :
      Confirmed working on the Xperia SP, on a fully clean build.
    
    Change-Id: Ie8712c13e43f823996f10ca83b4a9f95fb750e96
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/settings/NotificationBrightnessDialog.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2012-2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.settings;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.android.systemui.R;
+
+/** A dialog that provides controls for adjusting the notifications brightness. */
+public class NotificationBrightnessDialog extends Activity {
+
+    private NotificationBrightnessController mNotificationBrightnessController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Window window = getWindow();
+
+        window.setGravity(Gravity.TOP);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_notification_brightness_dialog);
+
+        final ToggleSlider slider = (ToggleSlider) findViewById(R.id.notification_brightness_slider);
+        mNotificationBrightnessController = new NotificationBrightnessController(this, slider);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mNotificationBrightnessController.registerCallbacks();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mNotificationBrightnessController.unregisterCallbacks();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
+                || keyCode == KeyEvent.KEYCODE_VOLUME_UP
+                || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE) {
+            finish();
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
