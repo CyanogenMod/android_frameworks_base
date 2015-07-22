c6e3e0708389607fa55d682e628ef7388c5633d8
Chirayu Desai <chirayudesai1@gmail.com>
2015-07-23 01:04:17 +0530

    Add the user set network mode to the siminfo table
    * To track what the user set for the particular sub independent
      of the currently set network mode (i.e. PREFERRED_NETWORK_MODE)
    * Useful on MSIM where the modem may only support 2G on one of the slots,
      so the preferred network mode (and the NETWORK_MODE column) would
      not always reflect the user desired network mode.
    
    Change-Id: I69d730e03a075b2684ca03a7df0086d95e0e329f
    Ref: CYNGNOS-291

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -260,6 +260,13 @@ public class SubscriptionManager {
     public static final String SIM_PROVISIONING_STATUS = "sim_provisioning_status";
 
     /**
+     * TelephonyProvider column name for the user configured network mode associated with a SIM.
+     * <P>Type: INTEGER (int)</P>
+     * @hide
+     */
+    public static final String USER_NETWORK_MODE = "user_network_mode";
+
+    /**
      *  TelephonyProvider column name for extreme threat in CB settings
      * @hide
      */
