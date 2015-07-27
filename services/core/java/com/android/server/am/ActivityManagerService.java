09ed90a4e2ea3d407c09ef2519b96afc8faf74ce
Pat Erley <perley@cyngn.com>
2015-07-27 15:14:04 -0700

    ActivityManagerService: Don't export 'system' process in running app list
    It is now considered a security risk to include the system process in the
    list of active processes by upstream, so exclude it.
    
    LETTUCE-742
    
    Change-Id: Id92b6b4927d2aad4add71f429c508078b3d7105d

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12516,6 +12516,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                 if (!allUsers && app.userId != userId) {
                     continue;
                 }
+                if (app.uid == Process.SYSTEM_UID) {
+                    continue;
+                }
                 if ((app.thread != null) && (!app.crashing && !app.notResponding)) {
                     // Generate process state info for running application
                     ActivityManager.RunningAppProcessInfo currApp = 
