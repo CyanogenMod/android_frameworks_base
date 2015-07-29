e12dbdf91118efaeb79d19b36162b9ede9a2362b
Adnan Begovic <adnan@cyngn.com>
2015-07-28 18:40:46 -0700

    systemui: Support remote expanded style.
    Change-Id: Ia324db41310c61cd0acd83633a9728b7562fd300

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -284,6 +284,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     };
 
+    public RemoteViews.OnClickHandler getOnClickHandler() {
+        return mOnClickHandler;
+    }
+
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
         @Override
         public boolean onClickHandler(
