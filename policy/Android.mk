9d5132c8c1be997399f7bc94e8ab161b4ebbbc75
Adnan Begovic <adnan@cyngn.com>
2015-07-29 17:39:37 -0700

    policy: Remove references of Profiles in android.policy.
      We can't reference the CMSDK within a jar that is exported in the
      BOOTCLASSPATH, otherwise we end up overriding the 3rd party
      applications classloaded jar and causing various issues during
      runtime.
    
    Change-Id: Idcbd8f33db89efe55a3b9e5f4ebe6dd47f812e00

--- policy/Android.mk
+++ policy/Android.mk
@@ -7,8 +7,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
             
 LOCAL_MODULE := android.policy
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform.sdk
 
 include $(BUILD_JAVA_LIBRARY)
 
