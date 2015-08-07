4e1902a0cb5ac1bc541367b7f1cb5eb1fd8afde8
d34d <clark@cyngn.com>
2015-08-07 10:32:20 -0700

    Keyguard: Stop fingerprint auth when not showing
    Change-Id: Ie754ac2c9a4b13f1fd2c146511e3b17baa420793
    REF: CYNGNOS-709
    (cherry picked from commit 3137f81c5879ec5e74bfe14f70eea133e0dcb836)

--- packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1281,6 +1281,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
                 cb.onKeyguardVisibilityChangedRaw(isShowing);
             }
         }
+        // Make sure if we are not showing that we stop fingerprint authentication
+        if (!isShowing && mLockPatternUtils.usingFingerprint()) {
+            stopAuthenticatingFingerprint();
+        }
     }
 
     /**
