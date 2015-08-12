f3de2deef705d020b29b289a1ad1551008d19d56
Ricardo Cerqueira <ricardo@cyngn.com>
2015-08-12 18:41:57 +0100

    PBD: Apply the same emmc discard logic as ext4_utils
    If we have SECURE_DISCARD disabled (currently only affects ext4_utils)
    don't invoke BLKSECDISCARD for FRP, either. Slow flash is slow.
    
    Addresses CYNGNOS-729
    
    Change-Id: I05f1c945aa4d6b441c6b55dd438cf06d12bd0548
    (cherry picked from commit a9521fe4e1bd6ce9040f2aedc84b5ffa20c67360)

--- services/core/jni/com_android_server_PersistentDataBlockService.cpp
+++ services/core/jni/com_android_server_PersistentDataBlockService.cpp
@@ -50,6 +50,7 @@ namespace android {
         int ret;
         uint64_t len = get_block_device_size(fd);
 
+#ifndef NO_SECURE_DISCARD
         range[0] = 0;
         range[1] = len;
 
@@ -59,6 +60,7 @@ namespace android {
         ret = ioctl(fd, BLKSECDISCARD, &range);
         if (ret < 0) {
             ALOGE("Something went wrong secure discarding block: %s\n", strerror(errno));
+#endif
             range[0] = 0;
             range[1] = len;
             ret = ioctl(fd, BLKDISCARD, &range);
@@ -70,7 +72,9 @@ namespace android {
                 return 0;
             }
 
+#ifndef NO_SECURE_DISCARD
         }
+#endif
 
         return ret;
     }
