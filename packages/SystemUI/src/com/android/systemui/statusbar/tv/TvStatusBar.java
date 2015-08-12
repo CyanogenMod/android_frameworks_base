80fa7c814a1dec4205702f75b680b3b599a7f362
Roman Birg <roman@cyngn.com>
2015-08-12 12:56:31 -0700

    SystemUI: fix empty notification space in shade_locked
    In change-id I806d1fd8eac73c4af0820319d127423ae6467f60, we started
    funneling update notification calls through the handler
    to avoid queueing up hundreds of these calls if a
    notification became spammy and to only execute one update if many were
    queued.
    
    However, some animations and state transitions are expecting the action
    to be done immediately after the call. Facilitate these requests by
    allowing them to bypass the handler if they want.
    
    Change-Id: Ice14cdaf93a4d370779b4a3e8996d17cd42fdff8
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -118,7 +118,7 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
-    protected void updateNotifications() {
+    protected void updateNotifications(boolean immediate) {
     }
 
     @Override
