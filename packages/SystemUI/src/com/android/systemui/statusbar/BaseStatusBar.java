00afc70e48d890c8f6cd4eae49717a05df343743
Roman Birg <roman@cyngn.com>
2015-08-12 12:56:31 -0700

    SystemUI: fix empty notification space in shade_locked
    In 8e98622a2c514f8bd11743781d4097e544d2f580, we started
    funneling update notification calls through the handler
    to avoid queueing up hundreds of these calls if a
    notification became spammy and to only execute one update if many were
    queued.
    
    However, some animations and state transitions are expecting the action
    to be done immediately after the call. Facilitate these requests by
    allowing them to bypass the handler if they want.
    
    Ref: CYNGNOS-768
    
    Change-Id: Ice14cdaf93a4d370779b4a3e8996d17cd42fdff8
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit f4659b2b91ee6464769f62c771ea7cef7ef99250)

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1831,7 +1831,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected void setZenMode(int mode) {
         if (!isDeviceProvisioned()) return;
         mZenMode = mode;
-        updateNotifications();
+        updateNotifications(true);
     }
 
     // extended in PhoneStatusBar
@@ -1853,7 +1853,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected abstract void haltTicker();
     protected abstract void setAreThereNotifications();
-    protected abstract void updateNotifications();
+    protected void updateNotifications() {
+        updateNotifications(false);
+    }
+    protected abstract void updateNotifications(boolean immediate);
     protected abstract void tick(StatusBarNotification n, boolean firstTime);
     protected abstract void updateExpandedViewPos(int expandedPosition);
     protected abstract boolean shouldDisableNavbarGestures();
