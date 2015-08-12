9105aa149cf840a9ce207786c01500326228491a
d34d <clark@cyngn.com>
2015-08-12 14:34:22 -0700

    Keyguard: Don't show keyguard if CryptKeeper enabled
    Some devices are exhibiting an issue where the keyguard will be
    shown on top of the decrypt device screen when they have their
    sim locked.  When this occurs, the user is not able to enter their
    pin/password to decrypt the device even after the keyguard is
    dismissed.
    
    Change-Id: I7362b77a6ea2ec41c8835ae763ce427c12c02138
    REF: NIGHTLIES-1254

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -27,10 +27,12 @@ import android.app.StatusBarManager;
 import android.app.admin.DevicePolicyManager;
 import android.app.trust.TrustManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.content.pm.UserInfo;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -145,6 +147,9 @@ public class KeyguardViewMediator extends SystemUI {
     private static final String KEYGUARD_SERVICE_ACTION_STATE_CHANGE =
             "com.android.internal.action.KEYGUARD_SERVICE_STATE_CHANGED";
     private static final String KEYGUARD_SERVICE_EXTRA_ACTIVE = "active";
+
+    private static final String SETTINGS_PACKAGE = "com.android.settings";
+    private static final String CRYPT_KEEPER_ACTIVITY = SETTINGS_PACKAGE + ".CryptKeeper";
 
     // used for handler messages
     private static final int SHOW = 2;
@@ -328,6 +333,8 @@ public class KeyguardViewMediator extends SystemUI {
     private KeyguardDisplayManager mKeyguardDisplayManager;
 
     private final ArrayList<IKeyguardStateCallback> mKeyguardStateCallbacks = new ArrayList<>();
+
+    private int mCyrptKeeperEnabledState = -1;
 
     KeyguardUpdateMonitorCallback mUpdateCallback = new KeyguardUpdateMonitorCallback() {
 
@@ -803,6 +810,16 @@ public class KeyguardViewMediator extends SystemUI {
         return false;
     }
 
+    private boolean isCryptKeeperEnabled() {
+        if (mCyrptKeeperEnabledState == -1) {
+            PackageManager pm = mContext.getPackageManager();
+            mCyrptKeeperEnabledState = pm.getComponentEnabledSetting(
+                    new ComponentName(SETTINGS_PACKAGE, CRYPT_KEEPER_ACTIVITY));
+        }
+
+        return mCyrptKeeperEnabledState != PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+    }
+
     public boolean isKeyguardBound() {
         return mKeyguardBound;
     }
@@ -1048,6 +1065,14 @@ public class KeyguardViewMediator extends SystemUI {
         // if the keyguard is already showing, don't bother
         if (mStatusBarKeyguardViewManager.isShowing()) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because it is already showing");
+            resetStateLocked();
+            return;
+        }
+
+        // Ugly hack to ensure keyguard is not shown on top of the CryptKeeper which prevents
+        // a user from being able to decrypt their device.
+        if (isCryptKeeperEnabled()) {
+            if (DEBUG) Log.d(TAG, "doKeyguard: not showing because CryptKeeper is enabled");
             resetStateLocked();
             return;
         }
