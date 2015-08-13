a525a4cecac1bca39a27ed0ddfca5cacf1000194
Puneet Mishra <puneetm@codeaurora.org>
2015-08-13 15:34:19 +0100

    NFC: Allow extensions to ApduServiceInfo
    Mobile network operators expect additional
    features in ApduServiceInfo to support their
    payment solutions. To achieve this, the final
    keyword is removed from the class definition
    and its instance variables, thereby allowing
    extensions to the class.
    
    Change-Id: I9058d08e729027b04ee94405c66ff9d99c0f963b

--- core/java/android/nfc/cardemulation/ApduServiceInfo.java
+++ core/java/android/nfc/cardemulation/ApduServiceInfo.java
@@ -48,53 +48,53 @@ import java.util.Map;
 /**
  * @hide
  */
-public final class ApduServiceInfo implements Parcelable {
+public class ApduServiceInfo implements Parcelable {
     static final String TAG = "ApduServiceInfo";
 
     /**
      * The service that implements this
      */
-    final ResolveInfo mService;
+    protected ResolveInfo mService;
 
     /**
      * Description of the service
      */
-    final String mDescription;
+    protected String mDescription;
 
     /**
      * Whether this service represents AIDs running on the host CPU
      */
-    final boolean mOnHost;
+    protected boolean mOnHost;
 
     /**
      * Mapping from category to static AID group
      */
-    final HashMap<String, AidGroup> mStaticAidGroups;
+    protected HashMap<String, AidGroup> mStaticAidGroups;
 
     /**
      * Mapping from category to dynamic AID group
      */
-    final HashMap<String, AidGroup> mDynamicAidGroups;
+    protected HashMap<String, AidGroup> mDynamicAidGroups;
 
     /**
      * Whether this service should only be started when the device is unlocked.
      */
-    final boolean mRequiresDeviceUnlock;
+    protected boolean mRequiresDeviceUnlock;
 
     /**
      * The id of the service banner specified in XML.
      */
-    final int mBannerResourceId;
+    protected int mBannerResourceId;
 
     /**
      * The uid of the package the service belongs to
      */
-    final int mUid;
+    protected int mUid;
 
     /**
      * Settings Activity for this service
      */
-    final String mSettingsActivityName;
+    protected String mSettingsActivityName;
 
     /**
      * @hide
