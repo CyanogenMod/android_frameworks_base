4c8571203939f58fceace83c4a565cfe925d2d07
Roman Birg <roman@cyngn.com>
2015-08-14 14:26:20 -0700

    Themes: don't recreateDefaults on typeface when locale changes
    When changing the device locales, we don't need to call recreateDefaults()
    every time the locale config has changed, as it is a very expensive
    operation.
    
    Change-Id: Iada86642145aa48772e70e89786e482640c75474
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4157,7 +4157,9 @@ public final class ActivityThread {
             boolean hasFontConfigChange = ((configDiff & ActivityInfo.CONFIG_THEME_FONT) != 0);
             if (hasLocaleConfigChange || hasFontConfigChange) {
                 Canvas.freeTextLayoutCaches();
-                Typeface.recreateDefaults();
+                if (hasFontConfigChange) {
+                    Typeface.recreateDefaults();
+                }
                 if (DEBUG_CONFIGURATION) Slog.v(TAG, "Cleared TextLayout Caches");
             }
         }
