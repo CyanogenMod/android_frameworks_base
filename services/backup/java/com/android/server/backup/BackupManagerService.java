0027f56e24616d7a5c62573fd2dd194f91af7110
myfluxi <linflux@arcor.de>
2015-08-14 20:06:52 +0200

    backup: Fix the system server crash fix
    We still need the firefighters after commit eff323bf.
    
    --------- beginning of crash
    E/AndroidRuntime(649): *** FATAL EXCEPTION IN SYSTEM PROCESS: backup
    E/AndroidRuntime(649): java.lang.NullPointerException: Attempt to invoke virtual method 'java.io.FileDescriptor android.os.ParcelFileDescriptor.getFileDescriptor()' on a null object reference
    E/AndroidRuntime(649):       at com.android.server.backup.BackupManagerService$PerformBackupTask.operationComplete(BackupManagerService.java:2826)
    E/AndroidRuntime(649):       at com.android.server.backup.BackupManagerService$BackupHandler.handleMessage(BackupManagerService.java:778)
    E/AndroidRuntime(649):       at android.os.Handler.dispatchMessage(Handler.java:102)
    E/AndroidRuntime(649):       at android.os.Looper.loop(Looper.java:135)
    E/AndroidRuntime(649):       at android.os.HandlerThread.run(HandlerThread.java:61)
    
    Change-Id: I28a9e8920bb33e687394fd6da6daede31bd77420

--- services/backup/java/com/android/server/backup/BackupManagerService.java
+++ services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -2931,7 +2931,7 @@ public class BackupManagerService {
 
             final String pkgName = mCurrentPackage.packageName;
             final long filepos = mBackupDataName.length();
-            if (mBackupDataName == null) {
+            if (mBackupData == null) {
                 failAgent(mAgentBinder, "Backup data was null: " + mBackupDataName);
                 addBackupTrace("backup data was null: " + mBackupDataName);
                 agentErrorCleanup();
