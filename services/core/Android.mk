26eb6bd1fc9dc654c4acad91c90c5cdb5d6b66e5
Steve Kondik <steve@cyngn.com>
2015-08-15 21:25:33 -0700

    cmhw: Move CMHW out of the framework and into CMSDK
    Change-Id: Ia21fb9472e40cc524cf86eabe4715bec5f9a186e

--- services/core/Android.mk
+++ services/core/Android.mk
@@ -13,8 +13,6 @@ LOCAL_JAVA_LIBRARIES := android.policy telephony-common
 
 LOCAL_JAVA_LIBRARIES += services.accessibility
 
-LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
-
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.platform.sdk
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
