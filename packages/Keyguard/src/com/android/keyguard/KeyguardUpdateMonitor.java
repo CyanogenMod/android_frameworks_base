dafbede64992e6394694919b949bf7c49ddeea34
d34d <clark@cyngn.com>
2015-08-17 12:19:06 -0700

    Fingerprint: Don't start fingerprint auth in constructor
    Wait until the keyguard is visible and the screen is on before
    starting the fingerprint authentication.
    
    Change-Id: Ic2cf699e57edc8152ee44f322fd1f847ada4bf03

--- packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -696,6 +696,9 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
                 cb.onScreenTurnedOn();
             }
         }
+        if (isKeyguardVisible()) {
+            startFingerAuthIfUsingFingerprint();
+        }
     }
 
     protected void handleScreenTurnedOff(int arg1) {
@@ -809,7 +812,6 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         trustManager.registerTrustListener(this);
 
         mLockPatternUtils = new LockPatternUtils(mContext);
-        startFingerAuthIfUsingFingerprint();
     }
 
     private boolean isDeviceProvisionedInSettingsDb() {
@@ -1096,8 +1098,13 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
             }
         }
         // Make sure if we are not showing that we stop fingerprint authentication
-        if (!isShowing && mLockPatternUtils.usingFingerprint()) {
-            stopAuthenticatingFingerprint();
+        if (mLockPatternUtils.usingFingerprint()) {
+            if (!isShowing) {
+                stopAuthenticatingFingerprint();
+            } else if (isScreenOn()) {
+                // This handles the case when booting and screen is already on
+                startFingerAuthIfUsingFingerprint();
+            }
         }
     }
 
