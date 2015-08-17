a7fccfca31c505d4b8ae5c289d560bc39dc41d7a
stofstik <verstegen.daan@gmail.com>
2015-08-17 17:42:53 +0200

    Added show volume panel tile to QS
    Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
    JIRA: CYAN-6721

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -53,6 +53,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.statusbar.CustomTileData;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -336,6 +337,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("compass")) return new CompassTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("sync")) return new SyncTile(this);
+        else if (tileSpec.equals("volume_panel")) return new VolumeTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -410,6 +412,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("compass")) return R.string.qs_tile_compass;
         else if (spec.equals("nfc")) return R.string.quick_settings_nfc;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
+        else if (spec.equals("volume_panel")) return R.string.quick_settings_volume_panel_label;
         return 0;
     }
 
