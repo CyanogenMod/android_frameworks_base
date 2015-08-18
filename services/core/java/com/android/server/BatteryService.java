f375b8aaf0cdd83dfb822cfd80edcd9162406f90
AdrianDC <radian.dc@gmail.com>
2015-08-18 19:33:18 +0200

    LEDs Brightness [2/2]: Lights notifications brightness support
    Implement the support of an overall brightness control for the LEDs.
    
    The setting is deactivated by default
    and will be ignored by the unimplemented phones.
    Current LibLights will simply not use the new variable.
    
    Changes includes :
      frameworks/base
      packages/Apps/Settings
    
    Change-Id: I1c0de01b1c6a2a2cf1432028a2e69f90b2373b2c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -99,6 +99,9 @@ public final class BatteryService extends SystemService {
 
     private static final int BATTERY_SCALE = 100;    // battery capacity is a percentage
 
+    // notification light maximum brightness value to use
+    private static final int LIGHT_BRIGHTNESS_MAXIMUM = 255;
+
     // Used locally for determining when to make a last ditch effort to log
     // discharge stats before the device dies.
     private int mCriticalBatteryLevel;
@@ -130,6 +133,9 @@ public final class BatteryService extends SystemService {
 
     private int mInvalidCharger;
     private int mLastInvalidCharger;
+
+    private boolean mAdjustableNotificationLedBrightness;
+    private int mNotificationLedBrightnessLevel = LIGHT_BRIGHTNESS_MAXIMUM;
 
     private int mLowBatteryWarningLevel;
     private int mLowBatteryCloseWarningLevel;
@@ -745,6 +751,10 @@ public final class BatteryService extends SystemService {
             mMultiColorLed = context.getResources().getBoolean(
                     com.android.internal.R.bool.config_multiColorBatteryLed);
 
+            // Is the notification LED brightness changeable ?
+            mAdjustableNotificationLedBrightness = context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_adjustableNotificationLedBrightness);
+
             mBatteryLedOn = context.getResources().getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryLedOn);
             mBatteryLedOff = context.getResources().getInteger(
@@ -795,6 +805,7 @@ public final class BatteryService extends SystemService {
                 // No lights if explicitly disabled
                 mBatteryLight.turnOff();
             } else if (level < mLowBatteryWarningLevel) {
+                mBatteryLight.setModes(mNotificationLedBrightnessLevel);
                 if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
                     // Battery is charging and low
                     mBatteryLight.setColor(mBatteryLowARGB);
@@ -808,6 +819,7 @@ public final class BatteryService extends SystemService {
                 }
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
+                mBatteryLight.setModes(mNotificationLedBrightnessLevel);
                 if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
                     // Battery is full or charging and nearly full
                     mBatteryLight.setColor(mBatteryFullARGB);
@@ -909,6 +921,13 @@ public final class BatteryService extends SystemService {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_LIGHT_PULSE), false, this, UserHandle.USER_ALL);
 
+            // Notification LED brightness
+            if (mAdjustableNotificationLedBrightness) {
+                resolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL),
+                        false, this, UserHandle.USER_ALL);
+            }
+
             // Light colors
             if (mMultiColorLed) {
                 // Register observer if we have a multi color led
@@ -952,6 +971,13 @@ public final class BatteryService extends SystemService {
             mBatteryFullARGB = Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_FULL_COLOR, res.getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB));
+
+            // Notification LED brightness
+            if (mAdjustableNotificationLedBrightness) {
+                mNotificationLedBrightnessLevel = Settings.System.getInt(resolver,
+                        Settings.System.NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL,
+                        LIGHT_BRIGHTNESS_MAXIMUM);
+            }
 
             updateLedPulse();
         }
