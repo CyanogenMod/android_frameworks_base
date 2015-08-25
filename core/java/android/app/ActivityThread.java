edc277906b4fc58893df2c3543d6b233caccbb76
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-08-24 17:58:41 -0700

    Select correct 12 or 24 time format according to Locale
    apps that depend on java.text.DateFormat and friends to format
    a date or time string will expect it to correctly handle 12 or 24
    time format according to the current locale and user selections in
    settings. This fixes a case where this was not being done properly
    when setting up a process in a locale that defaults to 24 hour
    time display when the user had not explicitly overriden the
    default format. It also includes a fix to update time formats
    if the user changes their locale without requiring the process
    to restart.
    
    This is a fix for CYNGNOS-495 which noted incorrect date
    formatting in the call log after choosing a locale that uses
    24 hour time formats.
    
    Change-Id: I449e7ef22f09a29de775aaf3fd8d38ca634a0bb4
    (cherry picked from commit 110e53971a6f07588887914093ba95de1bd47f51)

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4411,7 +4411,7 @@ public final class ActivityThread {
         }
 
 
-        final boolean is24Hr = "24".equals(mCoreSettings.getString(Settings.System.TIME_12_24));
+        final boolean is24Hr = android.text.format.DateFormat.is24HourFormat(appContext);
         DateFormat.set24HourTimePref(is24Hr);
 
         View.mDebugViewAttributes =
