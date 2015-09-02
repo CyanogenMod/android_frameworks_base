1518107254ee43187486e366626b20915ce83cc1
Jorge Ruesga <jorge@ruesga.com>
2015-09-03 01:14:17 +0200

    systemui: left hand navigation ring
    Fix navigation ring position when navbar is set to left hand mode
    
    Change-Id: I5a6fb634b8aafe90fa7fd18f35ce521f150556ce
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -82,6 +82,7 @@ public class SearchPanelView extends FrameLayout implements StatusBarPanel,
 
     private int mThreshold;
     private boolean mHorizontal;
+    private boolean mLeftNavbar;
 
     private boolean mLaunching;
     private boolean mDragging;
@@ -345,7 +346,8 @@ public class SearchPanelView extends FrameLayout implements StatusBarPanel,
                     mDragging = true;
                 }
                 if (mDragging) {
-                    float offset = Math.max(mStartDrag - currentTouch, 0.0f);
+                    float offset = Math.max(mHorizontal && mLeftNavbar
+                            ? currentTouch - mStartTouch : mStartTouch - currentTouch, 0.0f);
                     mCircle.setDragDistance(offset);
                     int indexOfIntersect = mCircle.isIntersecting(event);
                     mDraggedFarEnough = indexOfIntersect != -1;
@@ -405,6 +407,11 @@ public class SearchPanelView extends FrameLayout implements StatusBarPanel,
     public void setHorizontal(boolean horizontal) {
         mHorizontal = horizontal;
         mCircle.setHorizontal(horizontal);
+    }
+
+    public void setLeftNavbar(boolean leftNavbar) {
+        mLeftNavbar = leftNavbar;
+        mCircle.setLeftNavbar(mLeftNavbar);
     }
 
     @Override
