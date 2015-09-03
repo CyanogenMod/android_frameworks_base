5e7ef8bec21bff42bf91986119eb4c74df1cb0bd
Normal Ra <normalraw@gmail.com>
2015-09-03 21:28:31 +0200

    SystemUI: fix NFC tile disappearing randomly
    While the system is booting up the NfcAdapter.getNfcAdapter(Context context)
    might throw an UnsupportedOperationException while retrieving the NFC service.
    Because the NfcManager catches this exception and sets its private
    mAdapter variable null, subsequent calls to the manager will yield only
    null and therefore the NFC tile will never be able to obtain an instance
    of an NfcAdapter.
    
    Change-Id: I0cdda111f8e740a6925ffba382d510588af4af5a

--- packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
@@ -38,7 +38,11 @@ public class NfcTile extends QSTile<QSTile.BooleanState> {
 
     public NfcTile(Host host) {
         super(host);
-        mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
+        try {
+            mNfcAdapter = NfcAdapter.getNfcAdapter(mContext);
+        } catch (UnsupportedOperationException e) {
+            mNfcAdapter = null;
+        }
     }
 
     @Override
@@ -90,7 +94,11 @@ public class NfcTile extends QSTile<QSTile.BooleanState> {
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         if (mNfcAdapter == null) {
-            mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
+            try {
+                mNfcAdapter = NfcAdapter.getNfcAdapter(mContext);
+            } catch (UnsupportedOperationException e) {
+                mNfcAdapter = null;
+            }
         }
         state.visible = mNfcAdapter != null;
         state.value = mNfcAdapter != null && isEnabled();
@@ -121,7 +129,11 @@ public class NfcTile extends QSTile<QSTile.BooleanState> {
         mListening = listening;
         if (listening) {
             if (mNfcAdapter == null) {
-                mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
+                try {
+                    mNfcAdapter = NfcAdapter.getNfcAdapter(mContext);
+                } catch (UnsupportedOperationException e) {
+                    mNfcAdapter = null;
+                }
                 refreshState();
             }
             mContext.registerReceiver(mReceiver,
