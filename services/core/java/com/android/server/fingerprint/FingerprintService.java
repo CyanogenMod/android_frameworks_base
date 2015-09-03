5e06069cadcec387f9c6ac204e501e780e221fe4
Roman Birg <roman@cyngn.com>
2015-09-02 17:34:28 -0700

    fingerprint: add setWakeup command
    Change-Id: I680bea26e259a4667641760e3b6dcb831810f6f8
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -62,6 +62,9 @@ import java.util.Map;
 public class FingerprintService extends SystemService {
     private final String TAG = "FingerprintService";
     private static final boolean DEBUG = true;
+
+    private static final String PARAM_WAKEUP = "wakeup";
+
     private ArrayMap<IBinder, ClientData> mClients = new ArrayMap<IBinder, ClientData>();
 
     private static final int MSG_NOTIFY = 10;
@@ -81,6 +84,9 @@ public class FingerprintService extends SystemService {
     private Context mContext;
     private int mState = STATE_IDLE;
 
+    // Whether the sensor should be in wake up mode
+    private boolean mWakeupMode = false;
+    private IBinder mWakeClient;
 
     private static final long MS_PER_SEC = 1000;
 
@@ -166,7 +172,9 @@ public class FingerprintService extends SystemService {
         int newState = mState;
         for (Iterator<Map.Entry<IBinder, ClientData>> it = mClients.entrySet().iterator();
                 it.hasNext(); ) {
-            ClientData clientData = it.next().getValue();
+            final Map.Entry<IBinder, ClientData> entry = it.next();
+            IBinder client = entry.getKey();
+            ClientData clientData = entry.getValue();
             switch (msg) {
                 case FingerprintManager.FINGERPRINT_ERROR: {
                     final int error = arg1;
@@ -183,7 +191,7 @@ public class FingerprintService extends SystemService {
                 break;
                 case FingerprintManager.FINGERPRINT_ACQUIRED: {
                     final int acquireInfo = arg1;
-                    if (mState == STATE_AUTHENTICATING) {
+                    if (mState == STATE_AUTHENTICATING || client == mWakeClient) {
                         try {
                             vibrateDeviceIfSupported();
                             if (clientData != null && clientData.receiver != null) {
@@ -201,7 +209,7 @@ public class FingerprintService extends SystemService {
                 }
                 case FingerprintManager.FINGERPRINT_PROCESSED: {
                     final int fingerId = arg1;
-                    if (mState == STATE_AUTHENTICATING) {
+                    if (mState == STATE_AUTHENTICATING || client == mWakeClient) {
                         try {
                             newState = STATE_IDLE;
                             if (clientData != null && clientData.receiver != null) {
@@ -502,6 +510,23 @@ public class FingerprintService extends SystemService {
         }
     }
 
+    private void setWakeupMode(IBinder token, int userId, boolean wakeupMode) {
+        enforceCrossUserPermission(userId, "User " + UserHandle.getCallingUserId()
+                + " trying to add account for " + userId);
+
+        if (wakeupMode != mWakeupMode) {
+            mWakeupMode = wakeupMode;
+            if (mWakeupMode) {
+                mWakeClient = token;
+            } else {
+                mWakeClient = null;
+            }
+
+            final String params = PARAM_WAKEUP + "=" + (wakeupMode ? 1 : 0);
+            nativeSetParameters(params);
+        }
+    }
+
     private final class FingerprintServiceWrapper extends IFingerprintService.Stub {
         private final static String DUMP_CMD_REMOVE_FINGER = "removeFinger";
         private final static String DUMP_CMD_PRINT_ENROLLMENTS = "printEnrollments";
@@ -580,6 +605,13 @@ public class FingerprintService extends SystemService {
             checkPermission();
             throwIfNoFingerprint();
             return FingerprintService.this.getState();
+        }
+
+        @Override
+        public void setWakeup(IBinder token, int userId, boolean wakeup) throws RemoteException {
+            checkPermission();
+            throwIfNoFingerprint();
+            setWakeupMode(token, userId, wakeup);
         }
 
         /**
