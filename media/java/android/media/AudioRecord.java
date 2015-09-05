717d3890bf748dcead9c14218af60b472d96b6e1
Matt Garnes <matt@cyngn.com>
2015-09-04 18:02:20 -0700

    Add broadcast and query API for AudioSource.HOTWORD.
    - When the AudioSource.HOTWORD input becomes active or is released, send
      a Broadcast with the package name and the new state of the audio input
      to any applications that hold CAPTURE_AUDIO_OUTPUT.
    - Store the package name of the application that controls the hOTWORD
      input or set it to null if the input is not in use.
    - Add a new method to AudioService to retrieve the package name of the
      application that currently controls the HOTWORD input.
    
    Change-Id: I2f11888f3711d23b6287a4de7b81d361734a8f3b

--- media/java/android/media/AudioRecord.java
+++ media/java/android/media/AudioRecord.java
@@ -650,6 +650,11 @@ public class AudioRecord
                 mRecordingState = RECORDSTATE_RECORDING;
             }
         }
+
+        if (getRecordingState() == RECORDSTATE_RECORDING &&
+                getAudioSource() == MediaRecorder.AudioSource.HOTWORD) {
+            handleHotwordInput(true);
+        }
     }
 
     /**
@@ -695,6 +700,10 @@ public class AudioRecord
             native_stop();
             mRecordingState = RECORDSTATE_STOPPED;
         }
+
+        if (getAudioSource() == MediaRecorder.AudioSource.HOTWORD) {
+            handleHotwordInput(false);
+        }
     }
 
     private final IBinder mICallBack = new Binder();
@@ -708,6 +717,16 @@ public class AudioRecord
             ias.forceRemoteSubmixFullVolume(starting, mICallBack);
         } catch (RemoteException e) {
             Log.e(TAG, "Error talking to AudioService when handling full submix volume", e);
+        }
+    }
+
+    private void handleHotwordInput(boolean listening) {
+        final IBinder b = ServiceManager.getService(android.content.Context.AUDIO_SERVICE);
+        final IAudioService ias = IAudioService.Stub.asInterface(b);
+        try {
+            ias.handleHotwordInput(listening);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error talking to AudioService when handling hotword input.", e);
         }
     }
 
