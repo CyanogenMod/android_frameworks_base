4ff7268558c77636db8108f20163c68dda0905df
Christopher R. Palmer <crpalmer@gmail.com>
2015-09-12 12:29:09 -0400

    battery: Add fast charging UI support [3/3]
    Change-Id: I810f93e81829f300c6a3124253678c92ac0746d9

--- core/java/android/os/BatteryProperties.java
+++ core/java/android/os/BatteryProperties.java
@@ -24,6 +24,7 @@ public class BatteryProperties implements Parcelable {
     public boolean chargerUsbOnline;
     public boolean chargerWirelessOnline;
     public int batteryStatus;
+    public int batteryChargeType;
     public int batteryHealth;
     public boolean batteryPresent;
     public int batteryLevel;
@@ -49,6 +50,7 @@ public class BatteryProperties implements Parcelable {
         chargerUsbOnline = other.chargerUsbOnline;
         chargerWirelessOnline = other.chargerWirelessOnline;
         batteryStatus = other.batteryStatus;
+        batteryChargeType = other.batteryChargeType;
         batteryHealth = other.batteryHealth;
         batteryPresent = other.batteryPresent;
         batteryLevel = other.batteryLevel;
@@ -77,6 +79,7 @@ public class BatteryProperties implements Parcelable {
         chargerUsbOnline = p.readInt() == 1 ? true : false;
         chargerWirelessOnline = p.readInt() == 1 ? true : false;
         batteryStatus = p.readInt();
+        batteryChargeType = p.readInt();
         batteryHealth = p.readInt();
         batteryPresent = p.readInt() == 1 ? true : false;
         batteryLevel = p.readInt();
@@ -111,6 +114,7 @@ public class BatteryProperties implements Parcelable {
         p.writeInt(chargerUsbOnline ? 1 : 0);
         p.writeInt(chargerWirelessOnline ? 1 : 0);
         p.writeInt(batteryStatus);
+        p.writeInt(batteryChargeType);
         p.writeInt(batteryHealth);
         p.writeInt(batteryPresent ? 1 : 0);
         p.writeInt(batteryLevel);
