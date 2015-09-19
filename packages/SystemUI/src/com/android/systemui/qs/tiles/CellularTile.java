310fc10ced228c5103bd8e6e301816bc2230e381
Noeljunior <liamgliam@gmail.com>
2015-09-19 17:41:13 +0100

    Make the Cellular tile behave like wifi tile: click to toggle and secondary click to open detailled view
    Change-Id: I9af98b7ecf09b290c61abb4f7fa3c334d5f24b73

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -90,6 +90,15 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     @Override
     protected void handleClick() {
         if (mDataController.isMobileDataSupported()) {
+            mDataController.setMobileDataEnabled(!mDataController.isMobileDataEnabled());
+        } else {
+            mHost.startSettingsActivity(DATA_USAGE_SETTINGS);
+        }
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        if (mDataController.isMobileDataSupported()) {
             showDetail(true);
         } else {
             mHost.startSettingsActivity(DATA_USAGE_SETTINGS);
