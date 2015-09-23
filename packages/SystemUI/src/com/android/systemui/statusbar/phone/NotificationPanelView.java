5dae048d03353d45ff22c05ca8182ba18fc6fb67
Roman Birg <roman@cyngn.com>
2015-09-23 11:46:44 -0700

    Fingerprint Wakeup: add some error handling; fix race conditions
    - If the FingerprintManager reports that the HW is unavailable, it's
      likely the service isn't up yet. Try to reinitialize the fingerprint
      state when this happens.
    
    - Fix a race condition when screen is turned off and fingerprint is
      recognized but keyguard has not yet come back up.
    
    - don't count Fingerprint sensor errors against failed user count
    
    - Always stop authenticating when keyguard goes away.
    
    - Don't start authentication when max fingerprint attempt limit has been
      reached
    
    - Make final vibrate when limit has been reached more obvious
    
    Fingerprint Service changes:
    
    - Don't remove wake client when removing listener. These should be
      handled separately.
    
    - Remove wake client when wake is disabled
    
    - remove finalize() call - the binderDied() interface accomplishes what
      we need to do the proper cleanup.
    
    Ref: CYNGNOS-1125
    
    Change-Id: Id22d8f4f409e3989d0a094eb4c4bab7da996937f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2126,8 +2126,8 @@ public class NotificationPanelView extends PanelView implements
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
         @Override
-        public void onFingerprintAttemptFailed() {
-            if (!mStatusBar.isBouncerShowing() && mStatusBar.isScreenOnFromKeyguard()) {
+        public void onFingerprintAttemptFailed(boolean error, int errorCode) {
+            if (!error && !mStatusBar.isBouncerShowing() && mStatusBar.isScreenOnFromKeyguard()) {
                 NotificationPanelView.super.startHintAnimation(true /* fingerprintHint */);
             }
         }
