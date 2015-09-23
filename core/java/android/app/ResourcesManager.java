0d1caf7e8f129ad5cc18c9f61dbb702f6b82abf1
d34d <clark@cyngn.com>
2015-09-22 19:12:14 -0700

    Fix issue where non-themable app still had themed fonts
    The previous logic made it possible for a non-themable app to retain
    the applied font rather than use the system fonts as expected.
    
    Change-Id: Ia2ff7ec7741681712f201ce85e52df1c0a6a0225
    TICKET: CYNGNOS-939

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -177,8 +177,9 @@ public class ResourcesManager {
             Configuration overrideConfiguration, CompatibilityInfo compatInfo, IBinder token,
             Context context, boolean isThemeable) {
         final float scale = compatInfo.applicationScale;
+        ThemeConfig themeConfig = getThemeConfig();
         ResourcesKey key = new ResourcesKey(resDir, displayId, overrideConfiguration, scale,
-                isThemeable, getThemeConfig(), token);
+                isThemeable, themeConfig, token);
         Resources r;
         synchronized (this) {
             // Resources is app scale dependent.
@@ -256,25 +257,27 @@ public class ResourcesManager {
 
         boolean iconsAttached = false;
         /* Attach theme information to the resulting AssetManager when appropriate. */
-        if (isThemeable && config != null && !context.getPackageManager().isSafeMode()) {
-            if (config.themeConfig == null) {
+        if (config != null && !context.getPackageManager().isSafeMode()) {
+            if (themeConfig == null) {
                 try {
-                    config.themeConfig = ThemeConfig.getBootTheme(context.getContentResolver());
+                    themeConfig = ThemeConfig.getBootTheme(context.getContentResolver());
                 } catch (Exception e) {
                     Slog.d(TAG, "ThemeConfig.getBootTheme failed, falling back to system theme", e);
-                    config.themeConfig = ThemeConfig.getSystemTheme();
+                    themeConfig = ThemeConfig.getSystemTheme();
                 }
             }
 
-            if (config.themeConfig != null) {
-                attachThemeAssets(assets, config.themeConfig);
-                attachCommonAssets(assets, config.themeConfig);
-                iconsAttached = attachIconAssets(assets, config.themeConfig);
+            if (isThemeable) {
+                if (themeConfig != null) {
+                    attachThemeAssets(assets, themeConfig);
+                    attachCommonAssets(assets, themeConfig);
+                    iconsAttached = attachIconAssets(assets, themeConfig);
+                }
+            } else if (themeConfig != null &&
+                    !ThemeConfig.SYSTEM_DEFAULT.equals(themeConfig.getFontPkgName())) {
+                // use system fonts if not themeable and a theme font is currently in use
+                Typeface.recreateDefaults(true);
             }
-        } else if (!isThemeable && config.themeConfig != null &&
-                !ThemeConfig.SYSTEM_DEFAULT.equals(config.themeConfig.getFontPkgName())) {
-            // use system fonts if not themeable and a theme font is currently in use
-            Typeface.recreateDefaults(true);
         }
 
         r = new Resources(assets, dm, config, compatInfo, token);
@@ -775,10 +778,7 @@ public class ResourcesManager {
     }
 
     private ThemeConfig getThemeConfig() {
-        Configuration config = getConfiguration();
-        if (config != null) {
-            return config.themeConfig;
-        }
-        return null;
+        final Configuration config = getConfiguration();
+        return config != null ? config.themeConfig : null;
     }
 }
