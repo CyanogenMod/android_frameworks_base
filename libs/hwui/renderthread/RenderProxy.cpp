3973c59b77243793c9f5d9aed7a44483decf8c17
Thomas Buhot <thomas.buhot@intel.com>
2015-09-24 10:38:07 +0200

    libhwui: make setSurface asynchronous
    On the critical path of the cold launch of applications
    the main thread of the started application tells the RenderThread
    to create a surface. This process is synchronous and blocks
    the main thread of the application until the creation
    of the EGLContext is complete.
    As a consequence the launch time of the application is delayed
    by time spent allocating the EGL Context in the RenderThread.
    
    With this optimization the launch time of the settings
    application is reduced by 20 ms to 40 ms.
    
    This optimization applies to the launch of all
    applications.
    
    Change-Id: Ibf47aaa0abb8dedf7aa00693073db3785d9d6b08
    Signed-off-by: Thomas Buhot <thomas.buhot@intel.com>
    Signed-off-by: Zhiquan Liu <zhiquan.liu@intel.com>

--- libs/hwui/renderthread/RenderProxy.cpp
+++ libs/hwui/renderthread/RenderProxy.cpp
@@ -147,7 +147,9 @@ bool RenderProxy::initialize(const sp<ANativeWindow>& window) {
     SETUP_TASK(initialize);
     args->context = mContext;
     args->window = window.get();
-    return (bool) postAndWait(task);
+    bool ret = mContext->mCanvas == NULL;
+    post(task);
+    return ret;
 }
 
 CREATE_BRIDGE2(updateSurface, CanvasContext* context, ANativeWindow* window) {
