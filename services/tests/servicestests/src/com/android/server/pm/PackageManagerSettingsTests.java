747c88e1a9af1f67971d7726121039c6670d57d7
Adnan Begovic <adnan@cyngn.com>
2015-09-29 15:16:29 -0700

    pm: Create per-mcc prebundled install filter.
      If packages are region locked, avoid installing them
      for the SKU, otherwise if a package is specified for
      a mobile country code, allow it to be installed.
    
    TICKET: CYNGNOS-912
    Change-Id: I69c5be62dff771fc7d3117e9edf65d8ea4150f28

--- services/tests/servicestests/src/com/android/server/pm/PackageManagerSettingsTests.java
+++ services/tests/servicestests/src/com/android/server/pm/PackageManagerSettingsTests.java
@@ -21,8 +21,14 @@ import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
 import static android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
 
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.SystemProperties;
 import android.test.AndroidTestCase;
+import android.text.TextUtils;
 import android.util.ArraySet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 import com.android.internal.os.AtomicFile;
@@ -194,5 +200,35 @@ public class PackageManagerSettingsTests extends AndroidTestCase {
         assertEquals(1, ps.getEnabledComponents(1).size());
         hasEnabled = ps.getEnabledComponents(0) != null && ps.getEnabledComponents(0).size() > 0;
         assertEquals(false, hasEnabled);
+    }
+
+    public void testPrebundledRegionLockedAccessible() {
+        Configuration tempConfiguration = new Configuration();
+        String mcc = ("310");
+        if (!TextUtils.isEmpty(mcc)) {
+            tempConfiguration.mcc = Integer.parseInt(mcc);
+        }
+        Resources customResources = new Resources(new AssetManager(), new DisplayMetrics(),
+                tempConfiguration);
+        Settings settings = new Settings(getContext(), getContext().getFilesDir());
+        String expectedPackageNeededForRegion = "com.fat.bloat.spam";
+        String expectedMccCorrect = "310";
+        assertTrue(settings.isPrebundledPackagedNeededForRegion(expectedPackageNeededForRegion,
+                expectedMccCorrect, customResources));
+    }
+
+    public void testPrebundledRegionLocked() {
+        Configuration tempConfiguration = new Configuration();
+        String mcc = ("311");
+        if (!TextUtils.isEmpty(mcc)) {
+            tempConfiguration.mcc = Integer.parseInt(mcc);
+        }
+        Resources customResources = new Resources(new AssetManager(), new DisplayMetrics(),
+                tempConfiguration);
+        Settings settings = new Settings(getContext(), getContext().getFilesDir());
+        String expectedPackageNeededForRegion = "com.fat.bloat.spam";
+        String expectedMccWrong = "311";
+        assertFalse(settings.isPrebundledPackagedNeededForRegion(expectedPackageNeededForRegion,
+                expectedMccWrong, customResources));
     }
 }
