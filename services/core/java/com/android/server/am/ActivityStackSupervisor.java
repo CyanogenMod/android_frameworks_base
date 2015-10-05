958ab3431a1d1f1695326e3bd469690d26436b69
Roman Birg <roman@cyngn.com>
2015-10-05 15:47:04 -0700

    ActivityStackSupervisor: remove launch boosts
    This is causing some unexpected behaviors in some edge cases.
    
    Ref: CYNGNOS-1163
    
    Change-Id: Ie57f8f119283d00b8bb4bf4ceeddbd3ae8590191
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1350,10 +1350,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                             Display.DEFAULT_DISPLAY : mFocusedStack.mDisplayId) :
                             (container.mActivityDisplay == null ? Display.DEFAULT_DISPLAY :
                                     container.mActivityDisplay.mDisplayId)));
-            Trace.traceBegin(Trace.TRACE_TAG_ACTIVITY_MANAGER , "startActivityLocked");
-            /* Acquire perf lock during new app launch */
-            mPm.launchBoost();
-            Trace.traceEnd(Trace.TRACE_TAG_ACTIVITY_MANAGER);
         }
 
         ActivityRecord sourceRecord = null;
@@ -2545,12 +2541,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
     }
 
     void findTaskToMoveToFrontLocked(TaskRecord task, int flags, Bundle options, String reason) {
-        ActivityRecord top = task.stack.topRunningActivityLocked(null);
-        /* App is launching from recent apps and it's a new process */
-        if(top != null && top.state == ActivityState.DESTROYED) {
-            mPm.launchBoost();
-        }
-
         if ((flags & ActivityManager.MOVE_TASK_NO_USER_ACTION) == 0) {
             mUserLeaving = true;
         }
@@ -2744,15 +2734,10 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
                 final ActivityRecord ar = stack.findTaskLocked(r);
                 if (ar != null) {
-                    if (ar.state == ActivityState.DESTROYED) {
-                        mPm.launchBoost();
-                    }
                     return ar;
                 }
             }
         }
-        mPm.launchBoost();
-
         /* Delay Binder Explicit GC during application launch */
         BinderInternal.modifyDelayedGcParams();
 
