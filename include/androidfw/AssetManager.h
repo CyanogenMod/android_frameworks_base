d8eacf9aaf5fb07b89f2ec11c3300506a5be8b83
d34d <clark@cyngn.com>
2015-10-26 21:26:58 -0700

    Themes: Remove entryIdx param from appendPathToResTable
    Change-Id: Ia7e3d0462e7d9cf67024f14a3a36b0ad54dad527

--- include/androidfw/AssetManager.h
+++ include/androidfw/AssetManager.h
@@ -300,7 +300,7 @@ private:
     const ResTable* getResTable(bool required = true) const;
     void setLocaleLocked(const char* locale);
     void updateResourceParamsLocked() const;
-    bool appendPathToResTable(const asset_path& ap, size_t* entryIdx) const;
+    bool appendPathToResTable(const asset_path& ap) const;
 
     Asset* openIdmapLocked(const struct asset_path& ap) const;
 
