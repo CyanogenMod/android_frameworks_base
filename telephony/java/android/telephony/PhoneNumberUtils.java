CyanogenMod/android_frameworks_base

commit 83b589ec9c743174ac473622adb3f759e9d598af

Update indonesia country code in PhoneNumberUtils
Change-Id: Ia8735d21c5a28721b2d06322c44e14b7a0f16e71
(cherry picked from commit 030132cbd4422a40c94a341312d34685c31f48ad)

telephony/java/android/telephony/PhoneNumberUtils.java
@@ -155,7 +155,7 @@ public class PhoneNumberUtils
     private static Pattern sCdmaLocalRewritePattern;
     static {
         sCdmaLocalRewriteWhitelist = new SparseArray<RewriteRule>();
-        addRewriteRule(62, "IN", "0"); // indonesia
+        addRewriteRule(62, "ID", "0"); // indonesia
 
         StringBuffer regex = new StringBuffer();
         regex.append("[+](");

commit e567677bae7dae384ac992408efe90420fa5b4ea

When calling indonesia using +62 prefix while already in indonesia, replace +62 prefix with 0 (CDMA only)
Change-Id: Ia7e960794b611126a692a458857d8fe0bccb8c10
(cherry picked from commit e538a839ea48f46cb0533960bc0c5294b83cf802)
(cherry picked from commit b36bbedb2616b6fe01761d0ca347ad9ea4e06e35)

Update indonesia country code in PhoneNumberUtils

Change-Id: Ia8735d21c5a28721b2d06322c44e14b7a0f16e71
(cherry picked from commit 030132cbd4422a40c94a341312d34685c31f48ad)

Fix build break introduced in 83b589ec9c743174ac473622adb3f759e9d598af

Change-Id: I44380084be69afd30153f1779ad35a6e29e4092a

telephony/java/android/telephony/PhoneNumberUtils.java
@@ -16,6 +16,7 @@
 
 package android.telephony;
 
+import android.util.SparseArray;
 import com.android.i18n.phonenumbers.NumberParseException;
 import com.android.i18n.phonenumbers.PhoneNumberUtil;
 import com.android.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
@@ -38,6 +39,8 @@ import android.text.TextUtils;
 import android.text.style.TtsSpan;
 import android.util.SparseIntArray;
 
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ISO_COUNTRY;
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_OPERATOR_ISO_COUNTRY;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_OPERATOR_IDP_STRING;
 
 import java.util.Locale;
@@ -139,6 +142,54 @@ public class PhoneNumberUtils
     /** Returns true if ch is not dialable or alpha char */
     private static boolean isSeparator(char ch) {
         return !isDialable(ch) && !(('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z'));
+    }
+
+    /**
+     * On some CDMA networks +COUNTRYCODE must be rewritten to 0 when making a local
+     * call from within the user's home network.  We maintain a white list of
+     * (country code prefix) -> (rewrite rule) to perform this substitution.
+     *
+     * Since country codes are variable length it is easiest to compile a regex
+     */
+    private static SparseArray<RewriteRule> sCdmaLocalRewriteWhitelist;
+    private static Pattern sCdmaLocalRewritePattern;
+    static {
+        sCdmaLocalRewriteWhitelist = new SparseArray<RewriteRule>();
+        addRewriteRule(62, "ID", "0"); // indonesia
+
+        StringBuffer regex = new StringBuffer();
+        regex.append("[+](");
+        for (int i=0; i < sCdmaLocalRewriteWhitelist.size(); ++i) {
+            int countryCode = sCdmaLocalRewriteWhitelist.keyAt(i);
+            if (i > 0) {
+                regex.append("|");
+            }
+            regex.append(countryCode);
+        }
+        regex.append(")");
+        sCdmaLocalRewritePattern = Pattern.compile(regex.toString());
+    }
+
+    private static class RewriteRule {
+        public int countryCodePrefix;
+        public String isoCountryCode;
+        public String replacement;
+
+        public RewriteRule(int countryCodePrefix, String isoCountryCode, String replacement) {
+            this.countryCodePrefix = countryCodePrefix;
+            this.isoCountryCode = isoCountryCode;
+            this.replacement = replacement;
+        }
+
+        public String apply(String dialStr) {
+            return dialStr.replaceFirst("[+]" + countryCodePrefix, replacement);
+        }
+    }
+
+    private static void addRewriteRule(int countryCodePrefix,
+                                       String isoCountryCode, String replacement) {
+        sCdmaLocalRewriteWhitelist.put(countryCodePrefix,
+                new RewriteRule(countryCodePrefix, isoCountryCode, replacement));
     }
 
     /** Extracts the phone number from an Intent.
@@ -2568,6 +2619,29 @@ public class PhoneNumberUtils
     }
 
     /**
+     * Returns a rewrite rule for the country code prefix if the dial string matches the
+     * whitelist and the user is in their home network
+     *
+     * @param dialStr number being dialed
+     * @param currIso ISO code of currently attached network
+     * @param defaultIso ISO code of user's sim
+     * @return RewriteRule or null if conditions fail
+     */
+    private static RewriteRule getCdmaLocalRewriteRule(String dialStr,
+                                                       String currIso, String defaultIso) {
+        Matcher m = sCdmaLocalRewritePattern.matcher(dialStr);
+        if (m.find()) {
+            String dialPrefix = m.group(1);
+            RewriteRule rule = sCdmaLocalRewriteWhitelist.get(Integer.valueOf(dialPrefix));
+            if (currIso.equalsIgnoreCase(defaultIso) &&
+                    currIso.equalsIgnoreCase(rule.isoCountryCode)) {
+                return rule;
+            }
+        }
+        return null;
+    }
+
+    /**
      * Determines if the specified number is actually a URI
      * (i.e. a SIP address) rather than a regular PSTN phone number,
      * based on whether or not the number contains an "@" character.
@@ -2630,8 +2704,16 @@ public class PhoneNumberUtils
                 // Remove the leading plus sign
                 retStr = newStr;
             } else {
-                // Replaces the plus sign with the default IDP
-                retStr = networkDialStr.replaceFirst("[+]", getCurrentIdp(useNanp));
+                RewriteRule rewriteRule =
+                        getCdmaLocalRewriteRule(networkDialStr,
+                                SystemProperties.get(PROPERTY_OPERATOR_ISO_COUNTRY, ""),
+                                SystemProperties.get(PROPERTY_ICC_OPERATOR_ISO_COUNTRY, ""));
+                if (rewriteRule != null) {
+                    retStr = rewriteRule.apply(networkDialStr);
+                } else {
+                    // Replaces the plus sign with the default IDP
+                    retStr = networkDialStr.replaceFirst("[+]", getCurrentIdp(useNanp));
+                }
             }
         }
         if (DBG) log("processPlusCode, retStr=" + retStr);