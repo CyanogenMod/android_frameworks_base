3937aff9acd7f99bc79a3372534695166cdb44c9
Scott Mertz <scott@cyngn.com>
2015-11-02 16:07:48 -0800

    power: safely convert int to void *
    Change-Id: Ib916cac573af5293e3731e8f036a112380e22b0d

--- services/core/jni/com_android_server_power_PowerManagerService.cpp
+++ services/core/jni/com_android_server_power_PowerManagerService.cpp
@@ -167,7 +167,7 @@ static void nativeSetFeature(JNIEnv *env, jclass clazz, jint featureId, jint dat
 static void nativeCpuBoost(JNIEnv *env, jobject clazz, jint duration) {
     // Tell the Power HAL to boost the CPU
     if (gPowerModule && gPowerModule->powerHint) {
-        gPowerModule->powerHint(gPowerModule, POWER_HINT_CPU_BOOST, (void *) duration);
+        gPowerModule->powerHint(gPowerModule, POWER_HINT_CPU_BOOST, (void *)(intptr_t) duration);
     }
 }
 
@@ -181,7 +181,7 @@ static void nativeLaunchBoost(JNIEnv *env, jobject clazz) {
 static void nativeSetPowerProfile(JNIEnv *env, jobject clazz, jint profile) {
     // Tell the Power HAL to select a power profile
     if (gPowerModule && gPowerModule->powerHint) {
-        gPowerModule->powerHint(gPowerModule, POWER_HINT_SET_PROFILE, (void *) profile);
+        gPowerModule->powerHint(gPowerModule, POWER_HINT_SET_PROFILE, (void *)(intptr_t) profile);
     }
 }
 
