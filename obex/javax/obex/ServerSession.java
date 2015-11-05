3db159dbd2b42bfd2896106b9ab7d97c472742e0
Hemant Gupta <hemantg@codeaurora.org>
2015-11-05 14:12:01 +0530

    Bluetooth: OBEX: Don't close transport when obex is disconnected
    A case when remote device has disconnected obex connection, but
    has still kept the transport (L2CAP/RFCOMM) open. In such cases
    obex server was unnecessary assuming that transport would be
    disconnected so was informing registered users that transport has
    been disconnected, which was leading to issues on new obex connect
    on same transport channel from remote obex client. This patch removes
    check to break out of loop after obex disconnection, as that is
    already handled in case when read would return -1 in case transport
    is closed.
    
    Change-Id: Ic1682ce77c07cf2e43da09dc95fff2214338f758
    CRs-Fixed: 935761

--- obex/javax/obex/ServerSession.java
+++ obex/javax/obex/ServerSession.java
@@ -104,7 +104,6 @@ public final class ServerSession extends ObexSession implements Runnable {
 
                     case ObexHelper.OBEX_OPCODE_DISCONNECT:
                         handleDisconnectRequest();
-                        done = true;
                         break;
 
                     case ObexHelper.OBEX_OPCODE_GET:
@@ -125,6 +124,7 @@ public final class ServerSession extends ObexSession implements Runnable {
                         break;
 
                     case -1:
+                        Log.v(TAG, "Read request returned -1, exiting from loop");
                         done = true;
                         break;
 
