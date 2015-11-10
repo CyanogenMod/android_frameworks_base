122679c93cc16bc24582cafb19bbe43634f29b3e
Scott Mertz <scott@cyngn.com>
2015-11-10 12:00:22 -0800

    telecomm: fix checkapi
    Partial application of de776397ad8e7137cc1bc5beeef7b3c53282f7bf
    
    Change-Id: I82eac6ece5f05b378645e98ccdf595a843359662

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -497,6 +497,7 @@ public final class Call {
 
         /**
          * @return the time the Call object was created
+         * {@hide}
          */
         public long getCreateTimeMillis() {
             return mCreateTimeMillis;
