0a0629f159eda4f023f6ddea61ddc3c4d535aee3
Adnan Begovic <adnan@cyngn.com>
2015-11-11 12:05:59 -0800

    fw: Move DOUBLE_TAP_SLEEP_GESTURE to CMSettings.
    Change-Id: I8274b7c241cef6835a1114a702e68c95b6d2e036

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -52,6 +52,7 @@ import com.android.systemui.statusbar.BaseStatusBar;
 import com.android.systemui.statusbar.DragDownHelper;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout;
+import cyanogenmod.providers.CMSettings;
 
 
 public class StatusBarWindowView extends FrameLayout {
@@ -375,8 +376,8 @@ public class StatusBarWindowView extends FrameLayout {
 
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(Settings.System.getUriFor(Settings.System.DOUBLE_TAP_SLEEP_GESTURE), false,
-                    this);
+            resolver.registerContentObserver(CMSettings.System.getUriFor(
+                            CMSettings.System.DOUBLE_TAP_SLEEP_GESTURE), false, this);
             update();
         }
 
@@ -397,8 +398,8 @@ public class StatusBarWindowView extends FrameLayout {
 
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
-            mDoubleTapToSleepEnabled = Settings.System
-                    .getInt(resolver, Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 1) == 1;
+            mDoubleTapToSleepEnabled = CMSettings.System
+                    .getInt(resolver, CMSettings.System.DOUBLE_TAP_SLEEP_GESTURE, 1) == 1;
         }
     }
 }
