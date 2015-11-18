1e5c3b5a5dbf900e5b48ecfe62d00a7ea445a2d1
Sudheer Papothi <spapothi@codeaurora.org>
2015-11-19 01:25:04 +0530

    Reduce the delay during headset removal
    AudioService currently waits for one second delay for device switch
    after headset removal. This is to avoid any fake interrupts during
    removal. But, codec already waits for fake interrupts before actually
    triggering the removal interrupt. So, the delay is more during
    device switch from headset to speaker. Change reduces the delay
    by 300msec in AudioService as codec is handling the fake removal
    interrupts.
    
    Change-Id: I9d5dc1a82b85c0be8aaef3b65286450dfed13d35

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -5210,7 +5210,7 @@ public class AudioService extends IAudioService.Stub {
                         0,
                         null,
                         0);
-                delay = 1000;
+                delay = 700;
             }
         }
 
