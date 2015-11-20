49054d0d079ae36012a0c1814e4805ee9c60e270
myzb <myzb.dev@gmail.com>
2015-11-20 11:28:03 +0100

    Keyguard: Undo UI changes after merge of branch 'LA.BF64.1.2.2_rb4.7'
    Revert "Hide emergency button when OOS"
    
    This reverts commit 5ccaaff4818af8d48e072cbd2b8b784e5cd0c593.
    
    Revert "EmergencyCall Button on Swipe Lock Screen"
    
    This reverts commit 5f712cc90ebb11a04e72ab6b04a1229eeddef9fc.
    
    Revert "Emergency call button on Swipe lock Screen."
    
    This reverts commit f17cb289ae5f760a51b8463fe270b336d1e3b408.
    
    * The pin/pattern/etc entry screen already provides the emergency
      button. If no lockscreen security is chosen any person could do
      calls by just navigating into the dialer. Why overbloat the swipe
      lockscreen?
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
    
    Change-Id: Ie95f0b075e25b625298eac3c06d278e6cb1e7b17

--- packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
+++ packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
@@ -22,10 +22,8 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.PowerManager;
 import android.os.SystemClock;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.telecom.TelecomManager;
-import android.telephony.ServiceState;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
@@ -57,11 +55,6 @@ public class EmergencyButton extends Button {
 
         @Override
         public void onPhoneStateChanged(int phoneState) {
-            updateEmergencyCallButton();
-        }
-
-        @Override
-        public void onServiceStateChanged(int sub, ServiceState state) {
             updateEmergencyCallButton();
         }
     };
@@ -142,7 +135,7 @@ public class EmergencyButton extends Button {
         }
     }
 
-    public void updateEmergencyCallButton() {
+    private void updateEmergencyCallButton() {
         boolean visible = false;
         if (mIsVoiceCapable) {
             // Emergency calling requires voice capability.
@@ -156,13 +149,7 @@ public class EmergencyButton extends Button {
                     visible = mEnableEmergencyCallWhileSimLocked;
                 } else {
                     // Only show if there is a secure screen (pin/pattern/SIM pin/SIM puk);
-                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser()) ||
-                            SystemProperties.getBoolean("persist.radio.emgcy_btn_onswipe", false);
-                }
-
-                if (mContext.getResources().getBoolean(R.bool.kg_hide_emgcy_btn_when_oos)) {
-                    KeyguardUpdateMonitor monitor = KeyguardUpdateMonitor.getInstance(mContext);
-                    visible = visible && !monitor.isOOS();
+                    visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser());
                 }
             }
         }
