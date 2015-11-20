ecbb53e9c336a40a9755db4141238928e19a1155
Nalla Kartheek <karthe@codeaurora.org>
2015-11-20 13:19:54 +0530

    Introduce a flag to signify the presence of saved profile in scan results
    This flag shall signify that the saved profile is seen in the scan results
    and based on this such entries are displayed as found, without which, the
    scan results ended up showing these saved profiles forever, though the
    profile is not scanned.
    
    Change-Id: I0c953b5e86a547bd108cf0a8b553e62b2c31fa4e
    CRs-Fixed: 940917

--- packages/SettingsLib/src/com/android/settingslib/wifi/WifiTracker.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/WifiTracker.java
@@ -319,7 +319,8 @@ public class WifiTracker {
                 if (config.selfAdded && config.numAssociation == 0) {
                     continue;
                 }
-                AccessPoint accessPoint = new AccessPoint(mContext, config);
+                AccessPoint accessPoint = getCachedOrCreate(config, cachedAccessPoints);
+                accessPoint.foundInScanResult = false;
                 if (mLastInfo != null && mLastNetworkInfo != null) {
                     if (config.isPasspoint() == false) {
                         accessPoint.update(connectionConfig, mLastInfo, mLastNetworkInfo);
@@ -352,6 +353,7 @@ public class WifiTracker {
                 boolean found = false;
                 for (AccessPoint accessPoint : apMap.getAll(result.SSID)) {
                     if (accessPoint.update(result)) {
+                        accessPoint.foundInScanResult = true;
                         found = true;
                         break;
                     }
