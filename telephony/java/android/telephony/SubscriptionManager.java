d64e39111d7b62b1123b84592caae8a2351eb006
Deepak Kundra <deepakkundra@gmail.com>
2015-11-19 16:30:57 -0800

    quailstar: @hide public API added via CAF
    Change-Id: I0c56c164edc3028de6da73e272982018ca932149

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -124,18 +124,26 @@ public class SubscriptionManager {
      * The Network mode of SIM/sub.
      * <P>Type: INTEGER (int)</P>
      */
+    /** @hide */
     public static final String NETWORK_MODE = "network_mode";
 
+    /** @hide */
     public static final int DEFAULT_NW_MODE = -1;
 
     /**
      * The activation state of SIM/sub.
      * <P>Type: INTEGER (int)</P>
      */
+    /** @hide */
     public static final String SUB_STATE = "sub_state";
 
+    /** @hide */
     public static final int INACTIVE = 0;
+
+    /** @hide */
     public static final int ACTIVE = 1;
+
+    /** @hide */
     public static final int SUB_CONFIGURATION_IN_PROGRESS = 2;
 
     /**
@@ -1135,6 +1143,7 @@ public class SubscriptionManager {
         return phoneId >= 0 && phoneId < TelephonyManager.getDefault().getPhoneCount();
     }
 
+    /** @hide */
     public static void activateSubId(int subId) {
         logd("activateSubId sub id = " + subId);
         try {
@@ -1144,6 +1153,7 @@ public class SubscriptionManager {
         }
     }
 
+    /** @hide */
     public static void deactivateSubId(int subId) {
         logd("deactivateSubId sub id = " + subId);
         try {
@@ -1153,6 +1163,7 @@ public class SubscriptionManager {
         }
     }
 
+    /** @hide */
     public static int getSubState(int subId) {
         logd("getSubState sub id = " + subId);
         try {
@@ -1162,6 +1173,7 @@ public class SubscriptionManager {
         }
     }
 
+    /** @hide */
     public static int setSubState(int subId, int subState) {
         logd("setSubState sub id = " + subId + " state = " + subState);
         try {
