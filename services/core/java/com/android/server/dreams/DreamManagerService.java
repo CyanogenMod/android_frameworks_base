5e7beef63f132c003fa5db4def16bdcfe65109cf
Morningstar <admirethedistance@gmail.com>
2015-11-28 16:44:17 -0700

    add doze enabled by default settings
    Change-Id: I0a0f23970c8a38dfe071b90501955900ff9613c2

--- services/core/java/com/android/server/dreams/DreamManagerService.java
+++ services/core/java/com/android/server/dreams/DreamManagerService.java
@@ -372,7 +372,8 @@ public final class DreamManagerService extends SystemService {
                     com.android.internal.R.string.config_dozeComponent);
         }
         boolean enabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
-                Settings.Secure.DOZE_ENABLED, 1, userId) != 0;
+                Settings.Secure.DOZE_ENABLED, mContext.getResources().getBoolean(
+                    com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0, userId) != 0;
         return TextUtils.isEmpty(name) || !enabled ? null : ComponentName.unflattenFromString(name);
     }
 
