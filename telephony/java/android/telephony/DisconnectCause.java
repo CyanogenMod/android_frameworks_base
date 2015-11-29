02d5014e4181e7ac549e214cdb9e40f835019736
Arne Coucheron <arco68@gmail.com>
2015-11-30 00:10:44 +0100

    telephony: Update MAXIMUM_VALID_VALUE for disconnect codes
     * CALL_BLACKLISTED is the last disconnect code
    
    Change-Id: I5ea563a65f6da8184e0ca78a968bc0dac5e0520b

--- telephony/java/android/telephony/DisconnectCause.java
+++ telephony/java/android/telephony/DisconnectCause.java
@@ -262,7 +262,7 @@ public class DisconnectCause {
     public static final int MINIMUM_VALID_VALUE = NOT_DISCONNECTED;
 
     /** Largest valid value for call disconnect codes. */
-    public static final int MAXIMUM_VALID_VALUE = EMERGENCY_PERM_FAILURE;
+    public static final int MAXIMUM_VALID_VALUE = CALL_BLACKLISTED;
 
     /** Private constructor to avoid class instantiation. */
     private DisconnectCause() {
