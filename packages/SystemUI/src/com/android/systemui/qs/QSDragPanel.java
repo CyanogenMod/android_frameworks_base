d10e52b5754ed3b89d1ae737d512383b37cf55ed
Adnan Begovic <adnan@cyngn.com>
2015-12-02 15:43:28 -0800

    SystemUI: Move remove button impl to QSDragPanel.
      Expose a few methods to make attaching listeners to
      objects in the detail adapter possible.
    
    Change-Id: Ic72f27f7db4323de4a7b45643230bd20f8c8b4b5

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -24,6 +24,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
@@ -42,6 +43,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.FontSizeUtils;
 import com.android.systemui.R;
 import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.qs.tiles.EditTile;
@@ -52,7 +54,11 @@ import com.android.systemui.statusbar.phone.QSTileHost;
 import com.android.systemui.statusbar.phone.SystemUIDialog;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
 import com.android.systemui.tuner.QsTuner;
+
 import com.viewpagerindicator.CirclePageIndicator;
+import cyanogenmod.providers.CMSettings;
+
+import cyanogenmod.app.StatusBarPanelCustomTile;
 import cyanogenmod.providers.CMSettings;
 
 import java.util.ArrayList;
@@ -76,6 +82,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
     QSPanelTopView mQsPanelTop;
     CirclePageIndicator mPageIndicator;
 
+    private TextView mDetailRemoveButton;
     private DragTileRecord mDraggingRecord;
     private boolean mEditing;
     private boolean mDragging;
@@ -109,6 +116,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
 
         mDetail = LayoutInflater.from(mContext).inflate(R.layout.qs_detail, this, false);
         mDetailContent = (ViewGroup) mDetail.findViewById(android.R.id.content);
+        mDetailRemoveButton = (TextView) mDetail.findViewById(android.R.id.button3);
         mDetailSettingsButton = (TextView) mDetail.findViewById(android.R.id.button2);
         mDetailDoneButton = (TextView) mDetail.findViewById(android.R.id.button1);
         updateDetailText();
@@ -418,6 +426,12 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             return getPagesForCount(initialSize + mTempTiles.size());
         }
         return getPagesForCount(initialSize);
+    }
+
+    @Override
+    protected void updateDetailText() {
+        super.updateDetailText();
+        mDetailRemoveButton.setText(R.string.quick_settings_remove);
     }
 
     /**
@@ -1454,12 +1468,34 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         }
     }
 
+    @Override
+    protected void handleShowDetailImpl(Record r, boolean show, int x, int y) {
+        super.handleShowDetailImpl(r, show, x, y);
+        if (show) {
+            final StatusBarPanelCustomTile customTile = r.detailAdapter.getCustomTile();
+            mDetailRemoveButton.setVisibility(customTile != null ? VISIBLE : GONE);
+            mDetailRemoveButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mHost.collapsePanels();
+                    mHost.removeCustomTile(customTile);
+                }
+            });
+        }
+    }
+
     public int getDesiredColumnCount(int page, int row) {
         if (page == 0 && row == 0) {
             return 2; // TODO change if large tiles are disabled
         } else {
             return mColumns;
         }
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        FontSizeUtils.updateFontSize(mDetailRemoveButton, R.dimen.qs_detail_button_text_size);
     }
 
     @Override
