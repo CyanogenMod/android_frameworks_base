be6efb019240c0d99055db427cd29fe03a624c85
Roman Birg <roman@cyngn.com>
2015-12-01 19:58:58 -0800

    Quick settings: add statusbar setting page while editing
    Add a new page to the quick settings pager while in edit mode. Settings
    not currently hooked up.
    
    Change-Id: Ie8416ac5446794a03e33ae260538ffcd9a253d57
    Signed-off-by: Roman Birg <roman@cyngn.com>

---
+++ packages/SystemUI/src/com/android/systemui/qs/QSSettings.java
@@ -0,0 +1,16 @@
+package com.android.systemui.qs;
+
+import android.annotation.Nullable;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class QSSettings extends LinearLayout {
+    public QSSettings(Context context) {
+        super(context);
+    }
+
+    public QSSettings(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
