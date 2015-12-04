7ea79cf51c98dead5163ea3c35fe823cbc85d4b6
Mike xdnax <mikecoconut5832@gmail.com>
2015-12-04 01:34:49 -0500

    SystemUI: Enable three icon switching within QS DND tile
    This expands the QS DND tile to use three states instead of two.
    
    Java commits by @nicholaschum
    
    What this does:
    - Removes the two states (true/false) clause {? XXX:YYY;} and uses an
    switch(mZen) instead for the two cases and the default. If in the
    future, we want to add another tile, just add another case.
    - Adds two vector drawables originally by Cataclysm dev @atl4ntis,
    modified and reformatted - this allows for themers to theme this
    resource as well by adding the two new drawables.
    
    PS1: Uses if/elsif/else clause
    PS2: Update to use switch(mZen) clause
    
    Change-Id: If34e225bdc81e944a12ffd764a521de934aca3b3

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -141,7 +141,7 @@ public class DndTile extends QSTile<QSTile.BooleanState> {
         state.visible = isVisible(mContext);
         switch (zen) {
             case Global.ZEN_MODE_IMPORTANT_INTERRUPTIONS:
-                state.icon = ResourceIcon.get(R.drawable.ic_qs_dnd_on);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_dnd_on_priority);
                 state.label = mContext.getString(R.string.quick_settings_dnd_priority_label);
                 state.contentDescription = mContext.getString(
                         R.string.accessibility_quick_settings_dnd_priority_on);
