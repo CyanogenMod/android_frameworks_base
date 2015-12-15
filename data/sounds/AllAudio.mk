915f37448694c5ccb7638edf3d80bb15784186a1
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-12-15 15:39:32 -0800

    Add custom camera shutter
    SAMBAR-1185
    Change-Id: I71ca056813005ea7b17213fa8a47061de6192aa7

--- data/sounds/AllAudio.mk
+++ data/sounds/AllAudio.mk
@@ -327,7 +327,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/effects/ogg/Trusted_48k.ogg:system/media/audio/ui/Trusted.ogg \
     $(LOCAL_PATH)/effects/ogg/VideoRecord_48k.ogg:system/media/audio/ui/VideoRecord.ogg \
     $(LOCAL_PATH)/effects/ogg/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
-    $(LOCAL_PATH)/effects/ogg/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg \
     $(LOCAL_PATH)/effects/ogg/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
     $(LOCAL_PATH)/notifications/material/ogg/Ariel.ogg:system/media/audio/notifications/Ariel.ogg \
     $(LOCAL_PATH)/notifications/material/ogg/Carme.ogg:system/media/audio/notifications/Carme.ogg \
@@ -340,3 +339,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/notifications/material/ogg/Salacia.ogg:system/media/audio/notifications/Salacia.ogg \
     $(LOCAL_PATH)/notifications/material/ogg/Tethys.ogg:system/media/audio/notifications/Tethys.ogg \
     $(LOCAL_PATH)/notifications/material/ogg/Titan.ogg:system/media/audio/notifications/Titan.ogg
+
+ifeq ($(TARGET_NEEDS_BOOSTED_SOUNDS),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/effects/boosted/Roachpowder_camera-shutter.aiff:system/media/audio/ui/camera_click.ogg
+else
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/effects/ogg/camera_click_48k.ogg:system/media/audio/ui/camera_click.ogg
+endif
