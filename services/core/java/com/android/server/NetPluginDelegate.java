22b4f52441cc1d5ed83562067db2934c0d120bc7
Bryse Flowers <bflowers@codeaurora.org>
2015-12-21 12:36:34 -0800

    server: modify network stat plugin framework
    Add setUpstream function from Tethering to handle IPv6-only
    Tethering.
    
    TICKET: PAELLA-86
    Change-Id: I38392d0678df027c54469564746ee5d9897f5829
    (cherry picked from commit a77b5bc9da5d2516c601fae49772801af6c51a33)

--- services/core/java/com/android/server/NetPluginDelegate.java
+++ services/core/java/com/android/server/NetPluginDelegate.java
@@ -27,17 +27,16 @@
  *IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.android.server.net;
+package com.android.server;
 
 import dalvik.system.PathClassLoader;
 
-import java.lang.reflect.Constructor;
-
 import android.util.Slog;
+import android.net.Network;
 import android.net.NetworkStats;
 import android.util.Log;
 
-class NetPluginDelegate {
+public class NetPluginDelegate {
 
     private static final String TAG = "ConnectivityExtension";
     private static final boolean LOGV = false;
@@ -47,7 +46,7 @@ class NetPluginDelegate {
 
     private static boolean extensionFailed;
 
-    static void getTetherStats(NetworkStats uidStats, NetworkStats devStats,
+    public static void getTetherStats(NetworkStats uidStats, NetworkStats devStats,
             NetworkStats xtStats) {
         if (!loadTetherExtJar()) {
             return;
@@ -60,9 +59,10 @@ class NetPluginDelegate {
             e.printStackTrace();
             Log.w(TAG, "error in invoke method");
         }
+        if (LOGV) Slog.v(TAG, "getTetherStats() X");
     }
 
-    static void setQuota(String iface, long quota) {
+    public static void setQuota(String iface, long quota) {
         if (!loadTetherExtJar()) {
             return;
         }
@@ -72,8 +72,20 @@ class NetPluginDelegate {
         } catch (Exception ex) {
             Log.w(TAG, "Error calling setQuota Method on extension jar");
         }
+        if (LOGV) Slog.v(TAG, "setQuota(" + iface + ", " + quota + ") X");
     }
 
+    public static void setUpstream(Network net) {
+        if (LOGV) Slog.v(TAG, "setUpstream(" + net + ") E");
+        loadTetherExtJar();
+        try {
+            tetherExtensionClass.getMethod("setUpstream", Network.class).invoke(
+                    tetherExtensionObj, net);
+        } catch (Exception ex) {
+            Log.w(TAG, "Error calling setUpstream Method on extension jar");
+        }
+        if (LOGV) Slog.v(TAG, "setUpstream(" + net + ") E");
+    }
 
 
     private static boolean loadTetherExtJar() {
