9174653f13d8ad8d4a1eed4d389b1f8549902730
Roman Birg <roman@cyngn.com>
2015-12-23 12:49:47 -0800

    SystemUI: fix flicker when removing tiles
    Instead of clearing all tiles and re-adding them all one by one when
    adding or removing tiles, we'll cache the ones already present and
    re-use them to avoid having to lay them out from scratch.
    
    Change-Id: I6d083337c08aca65f5ba8073f53c486bff6045a0
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPage.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPage.java
@@ -52,13 +52,6 @@ public class QSPage extends ViewGroup {
     }
 
     @Override
-    public void requestLayout() {
-        if (mPanel == null || !mPanel.mSettingTiles) {
-            super.requestLayout();
-        }
-    }
-
-    @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         final int width = MeasureSpec.getSize(widthMeasureSpec);
         if (mPanel.mCurrentlyAnimating.isEmpty() && !mPanel.isDragging()) {
