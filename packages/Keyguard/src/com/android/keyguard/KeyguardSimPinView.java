CyanogenMod/android_frameworks_base

commit 18281968a799556a68abbb992adfd04c4cc754ff

SIM PIN Retry attempts display on Bootup.
When SIM state is changed to ABSENT/NOT READY, Closing Keygaurd
is not required Explicitely in M as it is handled by default.
Hence re designed the logic to avoid exceptions.

Change-Id: I8024b6ddd75c2630734e8d750d83f88c994e165a

packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -59,12 +59,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
         @Override
         public void onSimStateChanged(int subId, int slotId, State simState) {
            if (DEBUG) Log.v(TAG, "onSimStateChanged(subId=" + subId + ",state=" + simState + ")");
-           switch (simState) {
-               case NOT_READY:
-               case ABSENT:
-                   closeKeyGuard();
-                   break;
-           }
+           resetState();
        };
     };
 
@@ -255,7 +250,12 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                             }
                             resetPasswordText(true /* animate */);
                             if (result == PhoneConstants.PIN_RESULT_SUCCESS) {
-                                closeKeyGuard();
+                                KeyguardUpdateMonitor.getInstance(getContext())
+                                        .reportSimUnlocked(mSubId);
+                                mRemainingAttempts = -1;
+                                if (mCallback != null) {
+                                    mCallback.dismiss(true);
+                                }
                             } else {
                                 mShowDefaultMessage = false;
                                 if (result == PhoneConstants.PIN_PASSWORD_INCORRECT) {
@@ -296,16 +296,6 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
     @Override
     public boolean startDisappearAnimation(Runnable finishRunnable) {
         return false;
-    }
-
-    private void closeKeyGuard() {
-        if (DEBUG) Log.d(TAG, "closeKeyGuard: Verification Completed, closing Keyguard.");
-        mRemainingAttempts = -1;
-        KeyguardUpdateMonitor.getInstance(getContext())
-                         .reportSimUnlocked(mSubId);
-        mCallback.dismiss(true);
-        mShowDefaultMessage = true;
-        reset();
     }
 
     private void showDefaultMessage() {

commit cff2037318804970e8545cefc50362fa84a04b64

Avoid wrongly reported SIM PIN mismatch.
(At least) Qualcomm RILs report SIM unlock success as result=0,
attemptsRemaining=0. As reportSimUnlocked() causes showDefaultMessage()
to be called (via reportSimUnlocked -> handleSimStateChange ->
onSimStateChanged -> resetState -> showDefaultMessage), which accesses
mRemainingAttempts to determine the message to show, set
mRemainingAttempts before calling reportSimUnlocked().

This resets that code sequence back to AOSP; it got broken in commit
18281968a799556a68abbb992adfd04c4cc754ff.

Change-Id: Icf7b15f1bf55bb32c8b58bd6c77109971cbc463b

packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -257,9 +257,9 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                             }
                             resetPasswordText(true /* animate */);
                             if (result == PhoneConstants.PIN_RESULT_SUCCESS) {
+                                mRemainingAttempts = -1;
                                 KeyguardUpdateMonitor.getInstance(getContext())
                                         .reportSimUnlocked(mSubId);
-                                mRemainingAttempts = -1;
                                 if (mCallback != null) {
                                     mCallback.dismiss(true);
                                 }