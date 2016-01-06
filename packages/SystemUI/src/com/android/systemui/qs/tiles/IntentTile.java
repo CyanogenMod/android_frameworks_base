7228565cebce596041bcbaa85e2c6d8fe3bf4929
Suresh Koleti <skolet@codeaurora.org>
2016-01-06 16:23:34 +0530

    Send intent onLaunch of StatusBar in IntentTile
    Enabled option in IntentTile of Quick Settings
    to send a broadcast when the StatusBar is
    dragged down to see the Quick Settings.
    
    Change-Id: Ic8e8ffc70d9255275ad9654a4de13861e4457774
    CRs-Fixed: 1061684

--- packages/SystemUI/src/com/android/systemui/qs/tiles/IntentTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/IntentTile.java
@@ -43,6 +43,9 @@ public class IntentTile extends QSTile<QSTile.State> {
     private String mOnClickUri;
     private PendingIntent mOnLongClick;
     private String mOnLongClickUri;
+    private PendingIntent mOnLaunch;
+    private String mOnLaunchUri;
+
     private int mCurrentUserId;
     private String mIntentPackage;
 
@@ -72,6 +75,9 @@ public class IntentTile extends QSTile<QSTile.State> {
 
     @Override
     public void setListening(boolean listening) {
+        if (listening){
+            sendIntent("onLaunch", mOnLaunch, mOnLaunchUri);
+        }
     }
 
     @Override
@@ -160,6 +166,8 @@ public class IntentTile extends QSTile<QSTile.State> {
         mOnClickUri = intent.getStringExtra("onClickUri");
         mOnLongClick = intent.getParcelableExtra("onLongClick");
         mOnLongClickUri = intent.getStringExtra("onLongClickUri");
+        mOnLaunch = intent.getParcelableExtra("onLaunch");
+        mOnLaunchUri = intent.getStringExtra("onLaunchUri");
         mIntentPackage = intent.getStringExtra("package");
         mIntentPackage = mIntentPackage == null ? "" : mIntentPackage;
     }
