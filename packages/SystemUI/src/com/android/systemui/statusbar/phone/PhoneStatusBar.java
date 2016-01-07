93b7ee0a6365f719af97ac390208e5c6ec830fe5
lion0738 <lion0738@naver.com>
2016-01-07 13:11:28 +0900

    SystemUI: Show lockscreen wallpaper if no album art is available
    If album art is disabled for some reason (ex. turning off in eleven)
    we should display lockscreen wallpaper instead of default wallpaper.
    
    Change-Id: Ia3ccc5cbf62cbbc446fbdd0975701a1e5277824f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2158,8 +2158,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         // apply user lockscreen image
-        if (backdropBitmap == null && mMediaMetadata == null &&
-                !mNotificationPanel.hasExternalKeyguardView()) {
+        if (backdropBitmap == null && !mNotificationPanel.hasExternalKeyguardView()) {
             backdropBitmap = mKeyguardWallpaper;
         }
 
