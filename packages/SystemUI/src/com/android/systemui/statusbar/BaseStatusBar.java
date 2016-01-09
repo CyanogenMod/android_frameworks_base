7961f6390455d9685688d1c3bbee0bc6755f0b9c
Jack Boswell <boswelja@outlook.com>
2016-01-09 20:38:26 +1300

    Another duplicate fix

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -233,8 +233,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected int mZenMode;
 
 
-    protected AppSidebar mAppSidebar;
-
     protected int mSidebarPosition;
 
     protected AppCircleSidebar mAppCircleSidebar;
@@ -2299,67 +2297,5 @@ public abstract class BaseStatusBar extends SystemUI implements
         if (mAssistManager != null) {
             mAssistManager.startAssist(args);
         }
-    }
-    protected void addAppCircleSidebar() {
-
-        if (mAppCircleSidebar == null) {
-
-            mAppCircleSidebar = (AppCircleSidebar) View.inflate(mContext, R.layout.app_circle_sidebar, null);
-
-            mWindowManager.addView(mAppCircleSidebar, getAppCircleSidebarLayoutParams());
-
-        }
-
-    }
-
-
-    protected void removeAppCircleSidebar() {
-
-        if (mAppCircleSidebar != null) {
-
-            mWindowManager.removeView(mAppCircleSidebar);
-
-        }
-
-    }
-
-    protected WindowManager.LayoutParams getAppCircleSidebarLayoutParams() {
-
-        int maxWidth =
-
-                mContext.getResources().getDimensionPixelSize(R.dimen.app_sidebar_trigger_width);
-
-
-
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
-
-                maxWidth,
-
-                ViewGroup.LayoutParams.MATCH_PARENT,
-
-                WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
-
-                0
-
-                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
-
-                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-
-                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-
-                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
-
-                PixelFormat.TRANSLUCENT);
-
-        lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
-
-        lp.gravity = Gravity.TOP | Gravity.RIGHT;
-
-        lp.setTitle("AppCircleSidebar");
-
-
-        return lp;
-
     }
 }
