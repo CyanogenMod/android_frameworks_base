376c28225c7a24dc344f362a7da75827e3523be7
d34d <clark@cyngn.com>
2016-01-14 10:44:38 -0800

    SysUI: Clean up objects when recreating statusbar
    Recreating the status bar for theme changes causes several views
    and objects to not get GCed.  By analyzing hprof dumps of
    com.android.systemui we can identify these issues and get things
    properly cleaned up, and that's what this patch is doing.
    
    Change-Id: Ieaec759e4378a9a7701e6fba46ed33464caaee0a
    TICKET: CYNGNOS-1041
    (cherry picked from commit 18b5c9014f88c6af2e689ed1890d41fbc20c0139)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3633,6 +3633,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         networkController.removeSignalCallback(signalCluster);
         networkController.removeSignalCallback(signalClusterKeyguard);
         networkController.removeSignalCallback(signalClusterQs);
+
+        if (signalCluster != null) signalCluster.setSecurityController(null);
+        if (signalClusterKeyguard != null) signalClusterKeyguard.setSecurityController(null);
+        if (signalClusterQs != null) signalClusterQs.setSecurityController(null);
     }
 
     private void recreateStatusBar() {
@@ -3640,9 +3644,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (mNetworkController != null) {
             removeSignalCallbacks(mNetworkController);
+            if (mNetworkController.hasVoiceCallingFeature()) {
+                mNetworkController.removeEmergencyListener(mHeader);
+            }
         }
         if (mHeadsUpManager != null) {
             mHeadsUpManager.removeListener(mNotificationPanel);
+            mHeadsUpManager.removeListener(mScrimController);
+        }
+        if (mIconController != null) {
+            mIconController.cleanup();
+        }
+        if (mKeyguardIndicationController != null) {
+            mKeyguardIndicationController.cleanup();
         }
 
         mStatusBarWindow.removeContent(mStatusBarWindowContent);
@@ -3664,10 +3678,16 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // extract notifications.
         RankingMap rankingMap = mNotificationData.getRankingMap();
         int nNotifs = mNotificationData.size();
-        ArrayList<Pair<String, StatusBarNotification>> notifications =
-                new ArrayList<Pair<String, StatusBarNotification>>(nNotifs);
+        ArrayList<Pair<String, StatusBarNotification>> notifications = new ArrayList<>(nNotifs);
         copyNotifications(notifications, mNotificationData);
-        mNotificationData.clear();
+        // now remove all the notification views since we'll be re-inflating these with the copied
+        // data
+        for (int i = 0; i < nNotifs; i++) {
+            final NotificationData.Entry entry = mNotificationData.get(i);
+            if (entry != null) {
+                removeNotificationViews(entry.key, rankingMap);
+            }
+        }
 
         makeStatusBarView();
         repositionNavigationBar();
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -486,6 +486,14 @@ public class StatusBarIconController implements Tunable {
         return mStatusIcons;
     }
 
+    public void cleanup() {
+        TunerService.get(mContext).removeTunable(this);
+        mClockController.cleanup();
+        if (mSignalCluster != null) {
+            mSignalCluster.setSecurityController(null);
+        }
+    }
+
     private void refreshAllIconsForLayout(LinearLayout ll) {
         final int count = ll.getChildCount();
         for (int n = 0; n < count; n++) {
