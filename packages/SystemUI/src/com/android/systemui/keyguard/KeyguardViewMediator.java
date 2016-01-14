610a2bd57e1d9cf48c34924ddd973071c5df8938
Roman Birg <roman@cyngn.com>
2016-01-14 13:20:35 -0800

    Fingerprint: fix restarting timeout when screen is on
    Sometimes an event might have removed the KEYGUARD_FINGERPRINT_AUTH
    message even if it was queued up by the restart counter event at the
    same time as the restart event.
    
    With this change we don't immediately queue up two messages, but instead
    start fingerprint auth immediately after the fingerprint timeout message
    executes so it is always run and cannot be cancelled by other events.
    
    Ref: SAMBAR-1243
    
    Change-Id: If0a7e6f975fbe0c9a68ca89ecd211149fe2b646a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -611,9 +611,6 @@ public class KeyguardViewMediator extends SystemUI {
                         if (mUpdateMonitor.isOnLastFingerprintAttempt()) {
                             mHandler.sendEmptyMessageDelayed(KEYGUARD_FINGERPRINT_TIMEOUT,
                                     FINGERPRINT_TIMEOUT_DELAY);
-                            mHandler.sendMessageDelayed(
-                                    mHandler.obtainMessage(KEYGUARD_FINGERPRINT_AUTH, 1, 0),
-                                    FINGERPRINT_TIMEOUT_DELAY);
                         }
                     }
                 } else if (mUpdateMonitor.isMaxFingerprintAttemptsReached()) {
@@ -629,9 +626,6 @@ public class KeyguardViewMediator extends SystemUI {
                         }
 
                         mHandler.sendEmptyMessageDelayed(KEYGUARD_FINGERPRINT_TIMEOUT,
-                                FINGERPRINT_TIMEOUT_DELAY);
-                        mHandler.sendMessageDelayed(
-                                mHandler.obtainMessage(KEYGUARD_FINGERPRINT_AUTH, 1, 0),
                                 FINGERPRINT_TIMEOUT_DELAY);
 
                     }
@@ -1662,6 +1656,7 @@ public class KeyguardViewMediator extends SystemUI {
                     break;
                 case KEYGUARD_FINGERPRINT_TIMEOUT:
                     mUpdateMonitor.clearFailedFingerprintAttempts();
+                    mHandler.obtainMessage(KEYGUARD_FINGERPRINT_AUTH, 1, 0).sendToTarget();
                     break;
             }
         }
