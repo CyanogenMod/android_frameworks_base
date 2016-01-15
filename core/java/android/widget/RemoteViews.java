12245b83014eda3359c3194ada8f5a00e24c4bb6
d34d <clark@cyngn.com>
2016-01-15 12:07:17 -0800

    Themes: Fix applying theme for notifications
    Notifications should be inflated using the current theme applied
    to SystemUI.  This was not happening because the Context created
    when applying a RemoteView was returning a cahced context rather
    than one with the supplied theme applied.
    
    This patch also moves mCurrentTheme into BaseStatusBar so it can
    be used when inflating the remote views for notifications.
    
    Change-Id: I577ae30fc02028c7e497cd774f79029f8a045d1f
    TICKET: CYNGNOS-1608

--- core/java/android/widget/RemoteViews.java
+++ core/java/android/widget/RemoteViews.java
@@ -2815,7 +2815,8 @@ public class RemoteViews implements Parcelable, Filter {
     private Context getContextForResources(Context context, String themePackageName) {
         if (mApplication != null) {
             if (context.getUserId() == UserHandle.getUserId(mApplication.uid)
-                    && context.getPackageName().equals(mApplication.packageName)) {
+                    && context.getPackageName().equals(mApplication.packageName)
+                    && themePackageName == null) {
                 return context;
             }
             try {
