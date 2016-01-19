44140d046c8f253c7ce26e07864bce1e505518c0
d34d <clark@cyngn.com>
2016-01-18 19:52:10 -0800

    Themes: Reference app resource when compiling theme [2/2]
    This allows us to pass in the path to the target apk as an additional
    included resource apk, allowing theme designers to reference styles
    and attributes from the original package without needing to duplicate
    them.  Duplicating attributes never worked quite well, and causes
    all sorts of issues, including crashing apps.
    
    Instead of a theme declaring attributes, that are app specific, it
    can simply reference the originals.  This way things like colors that
    are style specific get included correctly.
    
    Here's an example of stat_sys_wifi_signal_4_fully.xml using this feature.
    By using ?com.android.systemui:attr/ we are able to reference attributes
    defined in SystemUI directly in our themed resource.
    
    <vector xmlns:android="http://schemas.android.com/apk/res/android"
        android:width="17.25dp"
        android:height="18dp"
        android:viewportWidth="46.0"
        android:viewportHeight="48.0">
    
        <path
            android:fillColor="?com.android.systemui:attr/backgroundColor"
            android:pathData="M20.4,18.0"/>
        <path
            android:fillColor="?com.android.systemui:attr/fillColor"
            android:pathData="M42.0,32.0l0.0,-4.0L26.0,18.0L26.0,7.0c0.0,-1.7 -1.3,-3.0 -3.0,
            -3.0c-1.7,0.0 -3.0,1.3 -3.0,3.0l0.0,11.0L4.0,28.0l0.0,4.0l16.0,-5.0l0.0,11.0l-4.0,
            3.0l0.0,3.0l7.0,-2.0l7.0,2.0l0.0,-3.0l-4.0,-3.0L26.0,27.0L42.0,32.0z"/>
    </vector>
    
    Change-Id: Icc794da9af2cd48099d42f9d7fbadaad8612b5db

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8267,8 +8267,12 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
 
         boolean hasCommonResources = (hasCommonResources(pkg) && !COMMON_OVERLAY.equals(target));
+        PackageParser.Package targetPkg = mPackages.get(target);
+        String appPath = targetPkg != null ? targetPkg.baseCodePath : "";
+
         if (mInstaller.aapt(pkg.baseCodePath, internalPath, resPath, sharedGid, pkgId,
                 pkg.applicationInfo.targetSdkVersion,
+                appPath,
                 hasCommonResources ? ThemeUtils.getTargetCacheDir(COMMON_OVERLAY, pkg)
                         + File.separator + "resources.apk" : "") != 0) {
             throw new AaptException("Failed to run aapt");
@@ -8282,7 +8286,7 @@ public class PackageManagerService extends IPackageManager.Stub {
         if (mInstaller.aapt(pkg.baseCodePath, APK_PATH_TO_ICONS, resPath, sharedGid,
                 Resources.THEME_ICON_PKG_ID,
                 pkg.applicationInfo.targetSdkVersion,
-                "") != 0) {
+                "", "") != 0) {
             throw new AaptException("Failed to run aapt");
         }
     }
