ed44fa2bdae8877b82d16312ce7f330dec9fb045
Roman Birg <roman@cyngn.com>
2016-01-19 16:38:02 -0800

    SystemUI: handle when brightness slider is hidden
    With this patch, the quick settings area should automatically expand and
    make room for the tile trashcan/instruction area when the user has the
    brightness slider hidden.
    
    Also adds a scrollview to the QS Settings page to allow the list to grow
    without impacting the height of the qs panel.
    
    Ref: CYNGNOS-1644
    
    Change-Id: I03f552ec28a978c4cad1e7e7a52648ec3d311a07
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit 1a99a4a44b776b033643197ac1bfa953875e517d)

--- packages/SystemUI/src/com/android/systemui/qs/QSViewPager.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSViewPager.java
@@ -14,7 +14,7 @@ public class QSViewPager extends ViewPager {
     private static final String TAG = "QSViewPager";
 
     protected static final float SCROLL_PERCENT = .10f;
-    private boolean mPagingEnabled;
+
     QSDragPanel mDragPanel;
 
     public QSViewPager(Context context) {
@@ -34,7 +34,6 @@ public class QSViewPager extends ViewPager {
     public boolean canScrollHorizontally(int direction) {
         if (direction < 0
                 && mDragPanel.isDragging()
-                && mPagingEnabled
                 && getCurrentItem() == 1) {
             // can't scroll left while not editing, OR dragging on the first page
             return false;
@@ -49,7 +48,7 @@ public class QSViewPager extends ViewPager {
             View child = getChildAt(i);
             child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
             int h = child.getMeasuredHeight();
-            if (h > height) height = h;
+            if (h > height && !(child instanceof QSSettings)) height = h;
         }
         heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -101,32 +100,6 @@ public class QSViewPager extends ViewPager {
             animator.start();
         } else {
             Log.e(TAG, "can't start fake drag?");
-        }
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-        if (mPagingEnabled) {
-            return super.onInterceptTouchEvent(event);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (mPagingEnabled) {
-            return super.onTouchEvent(event);
-        }
-        return false;
-    }
-
-    public void setPagingEnabled(boolean enabled) {
-        if (mPagingEnabled == enabled) return;
-        mPagingEnabled = enabled;
-        //Log.i(TAG, "setPagingEnabled() called with " + "enabled = [" + enabled + "]");
-        if (getCurrentItem() > 0 && !mPagingEnabled) {
-            //Log.w(TAG, "resetting to item 0 because paging is disabled.");
-            setCurrentItem(0, true);
         }
     }
 }
