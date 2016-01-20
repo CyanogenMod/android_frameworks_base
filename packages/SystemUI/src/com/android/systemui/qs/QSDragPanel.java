5bf8aa42c503732b49ad8b589bbcd0853698fa24
Roman Birg <roman@cyngn.com>
2016-01-19 16:38:02 -0800

    SystemUI: handle when brightness slider is hidden
    With this patch, the quick settings area should automatically expand and
    make room for the tile trashcan/instruction area when the user has the
    brightness slider hidden.
    
    Ref: CYNGNOS-1644
    
    Change-Id: I03f552ec28a978c4cad1e7e7a52648ec3d311a07
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -376,6 +376,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             r.tile.setListening(mListening);
         }
         mFooter.setListening(mListening);
+        mQsPanelTop.setListening(mListening);
         if (mListening) {
             refreshAllTiles();
         }
@@ -414,6 +415,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
             setupRecord(record);
             drawTile(record, record.tile.getState());
         }
+        mQsPanelTop.mHasBrightnessSliderToDisplay = showBrightnessSlider();
         mQsPanelTop.setEditing(editing);
         mPageIndicator.setEditing(editing);
         mPagerAdapter.notifyDataSetChanged();
@@ -729,13 +731,6 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         mHost.remove(spec);
     }
 
-    public void ensurePagerState() {
-        if (!isShowingDetail()) {
-            final boolean pagingEnabled = getVisibleTilePageCount() > 1 || mDragging || mEditing;
-            mViewPager.setPagingEnabled(pagingEnabled);
-        }
-    }
-
     public int getTilesPerPage(boolean firstPage) {
         if ((!mFirstRowLarge && firstPage) || !firstPage) {
             return QSTileHost.TILES_PER_PAGE + 1;
@@ -854,12 +849,10 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         if (!isShowingDetail() && !isClosingDetail()) {
             mQsPanelTop.bringToFront();
         }
-
-        ensurePagerState();
     }
 
     protected int getRowTop(int row) {
-        int baseHeight = mBrightnessView.getMeasuredHeight();
+        int baseHeight = mQsPanelTop.getMeasuredHeight();
         if (row <= 0) return baseHeight;
         return baseHeight + mLargeCellHeight - mDualTileUnderlap + (row - 1) * mCellHeight;
     }
@@ -1728,22 +1721,6 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         }
     }
 
-    @Override
-    protected void setGridContentVisibility(boolean visible) {
-        int newVis = visible ? VISIBLE : INVISIBLE;
-        for (int i = 0; i < mRecords.size(); i++) {
-            TileRecord tileRecord = mRecords.get(i);
-            if (tileRecord.tileView.getVisibility() != GONE) {
-                tileRecord.tileView.setVisibility(newVis);
-            }
-        }
-        mQsPanelTop.setVisibility(showBrightnessSlider() ? newVis : GONE);
-        if (mGridContentVisible != visible) {
-            MetricsLogger.visibility(mContext, MetricsLogger.QS_PANEL, newVis);
-        }
-        mGridContentVisible = visible;
-    }
-
     public void updateResources() {
         final Resources res = mContext.getResources();
         final int columns = Math.max(1, res.getInteger(R.integer.quick_settings_num_columns));
@@ -1772,6 +1749,12 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
 
     public boolean isAnimating(TileRecord t) {
         return mCurrentlyAnimating.contains(t);
+    }
+
+    public void cleanup() {
+        if (mSettingsObserver != null) {
+            mSettingsObserver.unobserve();
+        }
     }
 
     public static class TilesListAdapter extends BaseExpandableListAdapter
