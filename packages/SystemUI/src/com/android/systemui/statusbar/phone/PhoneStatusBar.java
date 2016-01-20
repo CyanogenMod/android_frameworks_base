2506506afc4ffe660c51396139162efae118b4a3
Roman Birg <roman@cyngn.com>
2016-01-19 16:38:02 -0800

    SystemUI: handle when brightness slider is hidden
    With this patch, the quick settings area should automatically expand and
    make room for the tile trashcan/instruction area when the user has the
    brightness slider hidden.
    
    Also adds a scrollview to the QS Settings page to allow the list to grow
    without impacting the height of the qs panel.
    
    Ref: CYNGNOS-1644
    
    Change-Id: I03f552ec28a978c4cad1e7e7a52648ec3d311a07
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3736,6 +3736,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mQSPanel.getHost().setCustomTileListenerService(null);
         mQSPanel.setTiles(new ArrayList<QSTile<?>>());
+        mQSPanel.setListening(false);
 
         makeStatusBarView();
         repositionNavigationBar();
