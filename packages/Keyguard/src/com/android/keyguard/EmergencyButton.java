2ce7d20b3ab0b044be3bedc3e19f615386ca5843
mqi <mqi@codeaurora.org>
2016-01-22 15:37:25 +0800

    SystemUI: Fix no emergency call button on lock screen
    Someone added system property check before showing emergency call
    button which is always false.
    
    Replace the conditon check with config value check.
    
    Change-Id: I823ca081bfa887e7c0fa9d3ef66a500023cf1d32
    CRs-Fixed: 963192

--- packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
+++ packages/Keyguard/src/com/android/keyguard/EmergencyButton.java
@@ -166,7 +166,7 @@ public class EmergencyButton extends Button {
                 } else {
                     // Only show if there is a secure screen (pin/pattern/SIM pin/SIM puk);
                     visible = mLockPatternUtils.isSecure(KeyguardUpdateMonitor.getCurrentUser()) ||
-                            SystemProperties.getBoolean("persist.radio.emgcy_btn_onswipe", false);
+                              mContext.getResources().getBoolean(R.bool.config_showEmergencyButton);
                 }
             }
         }
