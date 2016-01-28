26a3b57c812cea4c85b102bfe2b7a4f6521bc012
niks255 <niks255@mail.ru>
2016-01-28 22:03:59 +0300

    Add wakelock option for flashlight
    On some legacy devices flashlight can't stay on when screen is off. With this change, flashlight can use wakelock. For obvious reasons, this is turned off by default and needs to be enabled through overlay.
    
    Change-Id: Ia69309cf023430c9e90634a722be97ebbf6677ca

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
@@ -29,6 +29,8 @@ import android.hardware.camera2.CameraManager;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Process;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -49,6 +51,8 @@ public class FlashlightController {
     private static final int DISPATCH_CHANGED = 1;
     private static final int DISPATCH_AVAILABILITY_CHANGED = 2;
 
+    private static boolean mUseWakeLock;
+
     private static final String ACTION_TURN_FLASHLIGHT_OFF =
             "com.android.systemui.action.TURN_FLASHLIGHT_OFF";
 
@@ -65,6 +69,8 @@ public class FlashlightController {
 
     private final String mCameraId;
     private boolean mTorchAvailable;
+
+    private WakeLock mWakeLock;
 
     private Notification mNotification = null;
     private boolean mReceiverRegistered;
@@ -98,6 +104,11 @@ public class FlashlightController {
             mCameraId = cameraId;
         }
 
+        mUseWakeLock = mContext.getResources().getBoolean(R.bool.flashlight_use_wakelock);
+
+        PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+
         if (mCameraId != null) {
             ensureHandler();
             mCameraManager.registerTorchCallback(mTorchCallback, mHandler);
@@ -115,6 +126,11 @@ public class FlashlightController {
                     Log.e(TAG, "Couldn't set torch mode", e);
                     mFlashlightEnabled = false;
                     pendingError = true;
+
+                    if (mUseWakeLock) {
+                        if (mWakeLock.isHeld())
+                            mWakeLock.release();
+                    }
                 }
             }
         }
@@ -299,6 +315,15 @@ public class FlashlightController {
             synchronized (FlashlightController.this) {
                 changed = mFlashlightEnabled != enabled;
                 mFlashlightEnabled = enabled;
+
+                if (mUseWakeLock) {
+                    if (mFlashlightEnabled) {
+                        mWakeLock.acquire();
+                    } else {
+                        if (mWakeLock.isHeld())
+                            mWakeLock.release();
+                    }
+                }
             }
             if (changed) {
                 if (DEBUG) Log.d(TAG, "dispatchModeChanged(" + enabled + ")");
