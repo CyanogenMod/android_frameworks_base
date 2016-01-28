ba3656f9759e73c4570d614aa778ba9d46ed02b5
Danesh M <daneshm90@gmail.com>
2016-01-28 10:40:45 -0800

    SeekBarVolumizer : Disable notification volume if RINGER_MODE_VIBRATE
    CYNGNOS-1638
    
    Change-Id: Ic48cecfd5290efa7ac20fea8b8b58e6c10f29348

--- core/java/android/preference/SeekBarVolumizer.java
+++ core/java/android/preference/SeekBarVolumizer.java
@@ -147,7 +147,7 @@ public class SeekBarVolumizer implements OnSeekBarChangeListener, Handler.Callba
             mSeekBar.setProgress(mLastAudibleStreamVolume);
         } else if (mNotificationOrRing && mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
             mSeekBar.setProgress(0);
-            mSeekBar.setEnabled(enableSeekBar());
+            mSeekBar.setEnabled(mStreamType == AudioManager.MODE_RINGTONE);
         } else if (mMuted) {
             mSeekBar.setProgress(0);
         } else {
