f0897c623f912e6ae53e10bde249e26dd05043ae
Roman Birg <roman@cyngn.com>
2016-02-05 15:03:19 -0800

    SystemUI: handle hiding recents search bar dynamically
    If the "Show recents search bar" is toggled inside Settings, we need to
    catch that and reload the header bar as needed. Otherwise, having the
    setting disabled would still include search bar bounds while the view
    was hidden, causing some jarring animation jumps.
    
    Ref: CYNGNOS-342
    
    Change-Id: I74597d6d225af0395da6cb7a611bd230fc9e0675
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -261,10 +261,14 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                 mEmptyView.setVisibility(View.GONE);
                 mEmptyView.setOnClickListener(null);
             }
-            if (mRecentsView.hasValidSearchBar()) {
-                mRecentsView.setSearchBarVisibility(View.VISIBLE);
+            if (!mConfig.searchBarEnabled) {
+                mRecentsView.setSearchBarVisibility(View.GONE);
             } else {
-                refreshSearchWidgetView();
+                if (mRecentsView.hasValidSearchBar()) {
+                    mRecentsView.setSearchBarVisibility(View.VISIBLE);
+                } else {
+                    refreshSearchWidgetView();
+                }
             }
         }
 
