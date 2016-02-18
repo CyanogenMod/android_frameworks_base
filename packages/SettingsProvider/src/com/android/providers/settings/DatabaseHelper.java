c014d6c5814fe766bc8ba586919e0986297b23d1
Ankit Gupta <agupta@cyngn.com>
2016-02-18 11:39:51 -0800

    SettingsProvider: allow ambient display/doze mode to be overlayed
    Issue-id: YAM-140
    Change-Id: I3c296c645281509b7860e54d4e32790881d30ed6
    (cherry picked from commit 3c1279528e3a6017ff42f2d7aa436be88c9022fa)

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2710,6 +2710,8 @@ class DatabaseHelper extends SQLiteOpenHelper {
                     com.android.internal.R.string.config_dreamsDefaultComponent);
             loadStringSetting(stmt, Settings.Secure.SCREENSAVER_DEFAULT_COMPONENT,
                     com.android.internal.R.string.config_dreamsDefaultComponent);
+            loadBooleanSetting(stmt, Settings.Secure.DOZE_ENABLED,
+                                     R.bool.def_dozeEnabledByDefault);
 
             loadBooleanSetting(stmt, Settings.Secure.ACCESSIBILITY_DISPLAY_MAGNIFICATION_ENABLED,
                     R.bool.def_accessibility_display_magnification_enabled);
