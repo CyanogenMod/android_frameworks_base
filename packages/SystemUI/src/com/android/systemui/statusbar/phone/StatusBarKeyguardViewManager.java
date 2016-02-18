6050db8bc284712086ccc01588bea5c83491faf5
Adnan Begovic <adnan@cyngn.com>
2016-02-17 16:11:54 -0800

    SystemUI: Dismiss external keyguard views on FP auth.
    Change-Id: I26be3bf614f95e3c8bc278610ab64fc86d02fda2
    TICKET: CYNGNOS-2084

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -564,6 +564,7 @@ public class StatusBarKeyguardViewManager {
     public void animateCollapsePanels(float speedUpFactor) {
         mPhoneStatusBar.animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE, true /* force */,
                 false /* delayed */, speedUpFactor);
+        dismiss(false);
     }
 
     /**
