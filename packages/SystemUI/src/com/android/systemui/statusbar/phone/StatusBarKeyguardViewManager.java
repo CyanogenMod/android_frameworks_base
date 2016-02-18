f37a0b261cc9fabf28c0ba1c95c929a3c8d5378d
Adnan Begovic <adnan@cyngn.com>
2016-02-18 11:56:37 -0800

    SystemUI: Make sure to dismiss after disabling ext kg focus.
      Also verify that a third party keyguard component is even
      set.
    
    Change-Id: I9b8193c013067b76d16c035e11eb40f3cf8f46a7
    TICKET: CYNGNOS-2084

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -35,6 +35,7 @@ import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.R;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.statusbar.CommandQueue;
+import org.cyanogenmod.internal.util.CmLockPatternUtils;
 
 import static com.android.keyguard.KeyguardHostView.OnDismissAction;
 
@@ -564,7 +565,10 @@ public class StatusBarKeyguardViewManager {
     public void animateCollapsePanels(float speedUpFactor) {
         mPhoneStatusBar.animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_NONE, true /* force */,
                 false /* delayed */, speedUpFactor);
-        dismiss(false);
+        if (mStatusBarWindowManager.keyguardExternalViewHasFocus()) {
+            mStatusBarWindowManager.setKeyguardExternalViewFocus(false);
+            dismiss(false);
+        }
     }
 
     /**
