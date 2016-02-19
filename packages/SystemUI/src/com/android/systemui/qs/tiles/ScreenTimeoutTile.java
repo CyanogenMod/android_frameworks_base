d2ba0edebeff82f91623ae68e447a5fcf51b0bb5
Roman Birg <roman@cyngn.com>
2016-02-19 08:50:05 -0800

    SystemUI: fix screentimeout tile icon disappearing
    While going away from the detail view, sometimes it wasn't able to
    refresh it's state, resulting in the icon not showing and then appearing
    suddenly.
    
    Ref: CYNGNOS-2056
    
    Change-Id: I9a634cc4c774c78dc7316619db446f4ea6c2c79f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenTimeoutTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenTimeoutTile.java
@@ -61,9 +61,6 @@ public class ScreenTimeoutTile extends QSTile<ScreenTimeoutTile.TimeoutState> {
             new AnimationIcon(R.drawable.ic_qs_screen_timeout_long_reverse_avd);
 
     private String[] mEntries, mValues;
-    private boolean mShowingDetail;
-    ArrayList<Integer> mAnimationList
-            = new ArrayList<Integer>();
 
     public ScreenTimeoutTile(Host host) {
         super(host);
@@ -127,8 +124,6 @@ public class ScreenTimeoutTile extends QSTile<ScreenTimeoutTile.TimeoutState> {
     @Override
     protected void handleClick() {
         if (mEntries.length > 0) {
-            mShowingDetail = true;
-            mAnimationList.clear();
             showDetail(true);
         }
     }
@@ -190,10 +185,6 @@ public class ScreenTimeoutTile extends QSTile<ScreenTimeoutTile.TimeoutState> {
     }
     @Override
     protected void handleUpdateState(final TimeoutState state, Object arg) {
-        if (mAnimationList.isEmpty() && mShowingDetail && arg == null) {
-            return;
-        }
-
         int newTimeout = getScreenTimeout();
 
         AnimationIcon d = null;
@@ -334,7 +325,6 @@ public class ScreenTimeoutTile extends QSTile<ScreenTimeoutTile.TimeoutState> {
                     mUiHandler.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            mShowingDetail = false;
                             refreshState(true);
                         }
                     }, 100);
