3ee775e3357d48dfe7873ff303af80fe2bc2c4c4
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-22 18:01:07 +0200

    SystemUI: Fix volume panel inconsistencies
    * Use xml drawables everywhere
    * Add muted notification icon
    * Use AOSP naming scheme for drawables
    
    Change-Id: I14a3af9a7aecf4828f81a8e39855608e452848d6

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -194,7 +194,7 @@ public class VolumeDialog {
                 });
 
         addRow(AudioManager.STREAM_RING,
-                R.drawable.ic_ringer_audible, R.drawable.ic_volume_ringer_mute, true);
+                R.drawable.ic_volume_ringer, R.drawable.ic_volume_ringer_mute, true);
         addRow(AudioManager.STREAM_MUSIC,
                 R.drawable.ic_volume_media, R.drawable.ic_volume_media_mute, true);
         addRow(AudioManager.STREAM_ALARM,
@@ -652,9 +652,9 @@ public class VolumeDialog {
                 removeRow(notificationRow);
             }
         } else if (!mState.linkedNotification) {
-            // TODO get icon for mute state
             addRow(AudioManager.STREAM_NOTIFICATION,
-                    R.drawable.ic_notification_audible, R.drawable.ic_notification_audible, true);
+                    R.drawable.ic_volume_notification, R.drawable.ic_volume_notification_mute,
+                    true);
         }
     }
 
