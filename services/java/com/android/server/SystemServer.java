d0b204d2f11e433aa353c78336874388e8c8b68c
d34d <clark@cyngn.com>
2016-02-23 14:44:08 -0800

    Themes: Refactor themes to CMSDK [1/6]
    Change-Id: I3688b37342eddcfceeabaae982085884e9bc63ee
    TICKET: CYNGNOS-2126

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -25,15 +25,9 @@ import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
-import android.content.pm.ThemeUtils;
 import android.content.res.Configuration;
-import android.content.res.Resources.Theme;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.content.res.ThemeConfig;
 import android.database.ContentObserver;
 import android.os.Build;
 import android.os.Environment;
@@ -47,7 +41,6 @@ import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.storage.IMountService;
-import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.EventLog;
 import android.util.Slog;
@@ -570,7 +563,6 @@ public final class SystemServer {
         MediaRouterService mediaRouter = null;
         GestureService gestureService = null;
         EdgeGestureService edgeGestureService = null;
-        ThemeService themeService = null;
 
         // Bring up services needed for UI.
         if (mFactoryTestMode != FactoryTest.FACTORY_TEST_LOW_LEVEL) {
@@ -1003,14 +995,6 @@ public final class SystemServer {
                 mSystemServiceManager.startService(TvInputManagerService.class);
             }
 
-            try {
-                Slog.i(TAG, "Theme Service");
-                themeService = new ThemeService(context);
-                ServiceManager.addService(Context.THEME_SERVICE, themeService);
-            } catch (Throwable e) {
-                reportWtf("starting Theme Service", e);
-            }
-
             if (!disableNonCoreServices) {
                 try {
                     Slog.i(TAG, "Media Router Service");
@@ -1157,16 +1141,6 @@ public final class SystemServer {
             }
         }
 
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_APP_FAILURE);
-        filter.addAction(Intent.ACTION_APP_FAILURE_RESET);
-        filter.addAction(Intent.ACTION_PACKAGE_ADDED);
-        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        filter.addAction(ThemeUtils.ACTION_THEME_CHANGED);
-        filter.addCategory(Intent.CATEGORY_THEME_PACKAGE_INSTALLED_STATE_CHANGE);
-        filter.addDataScheme("package");
-        context.registerReceiver(new AppsFailureReceiver(), filter);
-
         // These are needed to propagate to the runnable below.
         final NetworkManagementService networkManagementF = networkManagement;
         final NetworkStatsService networkStatsF = networkStats;
@@ -1187,7 +1161,6 @@ public final class SystemServer {
         final MediaRouterService mediaRouterF = mediaRouter;
         final AudioService audioServiceF = audioService;
         final MmsServiceBroker mmsServiceF = mmsService;
-        final ThemeService themeServiceF = themeService;
 
         // We now tell the activity manager it is okay to run third party
         // code.  It will call back into us once it has gotten to the state
@@ -1325,17 +1298,6 @@ public final class SystemServer {
                     if (mmsServiceF != null) mmsServiceF.systemRunning();
                 } catch (Throwable e) {
                     reportWtf("Notifying MmsService running", e);
-                }
-
-                try {
-                    // now that the system is up, apply default theme if applicable
-                    if (themeServiceF != null) themeServiceF.systemRunning();
-                    ThemeConfig themeConfig =
-                            ThemeConfig.getBootTheme(context.getContentResolver());
-                    String iconPkg = themeConfig.getIconPackPkgName();
-                    mPackageManagerService.updateIconMapping(iconPkg);
-                } catch (Throwable e) {
-                    reportWtf("Icon Mapping failed", e);
                 }
             }
         });
