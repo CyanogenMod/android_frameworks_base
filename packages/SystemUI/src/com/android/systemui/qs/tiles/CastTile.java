7f4697c2889bb8fef2c0951513a5952a74329e58
Roman Birg <roman@cyngn.com>
2016-03-14 09:44:36 -0700

    Cast Tile: only scan while in detail view
    Small optimization, otherwise we'd scan until the user closed the status
    bar.
    
    Change-Id: I8d9d933287e0225a8557a7aec06eb60392464fa7
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
@@ -208,12 +208,14 @@ public class CastTile extends QSTile<QSTile.BooleanState> {
                     @Override
                     public void onViewAttachedToWindow(View v) {
                         if (DEBUG) Log.d(TAG, "onViewAttachedToWindow");
+                        mController.setDiscovering(true);
                     }
 
                     @Override
                     public void onViewDetachedFromWindow(View v) {
                         if (DEBUG) Log.d(TAG, "onViewDetachedFromWindow");
                         mVisibleOrder.clear();
+                        mController.setDiscovering(false);
                     }
                 });
             }
@@ -221,7 +223,6 @@ public class CastTile extends QSTile<QSTile.BooleanState> {
                     R.string.quick_settings_cast_detail_empty_text);
             mItems.setCallback(this);
             updateItems(mController.getCastDevices());
-            mController.setDiscovering(true);
             return mItems;
         }
 
