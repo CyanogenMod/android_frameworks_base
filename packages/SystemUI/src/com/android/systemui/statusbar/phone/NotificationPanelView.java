779a3ebd6ee5826ba1f4bb5c303369ba49d512a6
Roman Birg <roman@cyngn.com>
2016-04-04 15:29:34 -0700

    SystemUI: show visualizer in shade-locked view
    This brings back the behavior we had in 12.1 - visualizer would still be
    visible in the shade locked state.
    
    Also with the following improvements:
        * use less alpha
        * always attach/detach in a background thread (Async task)
        * use the statusbar state instead keyguard view state listener
    
    Change-Id: I6b8a57f9f0bba6ba0591de0cb1f94a0eb904cc1c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2044,7 +2044,6 @@ public class NotificationPanelView extends PanelView implements
                 * mKeyguardStatusBarAnimateAlpha);
         mKeyguardStatusBar.setVisibility(mKeyguardStatusBar.getAlpha() != 0f
                 && !mDozing ? VISIBLE : INVISIBLE);
-        mStatusBar.getVisualizer().setAlpha(mKeyguardStatusBar.getAlpha());
     }
 
     private void updateHeaderKeyguard() {
