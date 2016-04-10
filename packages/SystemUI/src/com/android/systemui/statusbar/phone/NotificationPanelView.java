17a68e1a6a920e6fa63da1f3158b43e0c7bed482
d34d <clark@cyngn.com>
2016-04-10 12:03:47 -0700

    SysUI: reset mCanDismissKeyguard in setBarState
    This patch ensures mCanDismissKeyguard is in the correct state when
    setBarState is called, which fixes issues with bouncer showing when
    calls come in or an alarm goes off.  This also seems to fix an issue
    where the bouncer is not shown when fingerprint authenticiation is
    used for the first time on boot.
    
    Change-Id: Ie19fd501b334d4faee14f1ddeb101cbec2515440
    TICKET: CYNGNOS-2400 NIGHTLIES-2767

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1300,6 +1300,7 @@ public class NotificationPanelView extends PanelView implements
 
         mStatusBarState = statusBarState;
         mKeyguardShowing = keyguardShowing;
+        mCanDismissKeyguard = keyguardShowing;
 
         if (goingToFullShade || (oldState == StatusBarState.KEYGUARD
                 && statusBarState == StatusBarState.SHADE_LOCKED)) {
