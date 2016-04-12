a9be982198b37a1da9afdd76f4645c08bbffb2ef
Danesh M <daneshm90@gmail.com>
2016-04-12 11:30:47 -0700

    Lockscreen : Ensure we hide bottom area in bouncer
    Since the bottom area lives in its own window, ensure we
    hide it when the bouncer is showing to avoid overlaps.
    
    CYNGNOS-2422
    Change-Id: I465d24c9d64e67213ce2dff83f17625c490684f6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -101,7 +101,7 @@ public class StatusBarKeyguardViewManager {
         if (mBouncer != null) mBouncer.removeView();
         mFingerprintUnlockController = fingerprintUnlockController;
         mBouncer = new KeyguardBouncer(mContext, mViewMediatorCallback, mLockPatternUtils,
-                mStatusBarWindowManager, container);
+                mStatusBarWindowManager, container, mPhoneStatusBar);
     }
 
     /**
@@ -394,7 +394,6 @@ public class StatusBarKeyguardViewManager {
     public void dismiss() {
         if ((mDeviceInteractive || mDeviceWillWakeUp)) {
             showBouncer();
-            mPhoneStatusBar.mKeyguardBottomArea.setVisibility(View.GONE);
         }
     }
 
