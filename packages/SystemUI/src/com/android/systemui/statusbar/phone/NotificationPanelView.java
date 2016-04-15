c5522a541aa410c025e8ac93da44366ed2897f21
Danesh M <danesh@cyngn.com>
2016-04-15 16:29:40 -0700

    Lockscreen : Ensure window bounds change after view animations
    Changing window bounds when a view inside the window is performing
    animations, causes flickers. Ensure we change window bounds
    only after all the animations have completed.
    
    Change-Id: I2143d3d15cdfe660d864e1af902267b0903b2afa

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -464,6 +464,10 @@ public class NotificationPanelView extends PanelView implements
                     } else {
                         intercept = NotificationPanelView.this.onTouchEvent(e);
                     }
+                } else {
+                    // Ensure we expand as early as possible
+                    // to avoid any simultaneous animations on the views
+                    mKeyguardBottomArea.expand(true);
                 }
                 return intercept;
             }
@@ -2316,13 +2320,17 @@ public class NotificationPanelView extends PanelView implements
         requestDisallowInterceptTouchEvent(true);
         mOnlyAffordanceInThisMotion = true;
         mQsTracking = false;
-        mKeyguardBottomArea.expand(true);
     }
 
     @Override
     public void onSwipingAborted() {
         mKeyguardBottomArea.unbindCameraPrewarmService(false /* launched */);
-        mKeyguardBottomArea.expand(false);
+        mKeyguardBottomArea.animate().setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mKeyguardBottomArea.expand(false);
+            }
+        });
     }
 
     @Override
