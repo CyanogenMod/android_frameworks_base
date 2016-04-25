a1dd1b926549e037dd19f41892ffcd59ea32692e
d34d <clark@cyngn.com>
2016-04-25 13:37:58 -0700

    SysUI: Animate notificaiton panel back into view
    Add an animation for when the user has swiped the left edge to bring
    the notificaiton panel back into view from the left side.
    
    Change-Id: Ie2ab3482a39d2d9c7b93a853f6ebae43a174e2c0
    TICKET: CYNGNOS-2549
    (cherry picked from commit a86c8a9551afa0ee0a215013aa4707ff6e111663)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5365,6 +5365,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mLiveLockScreenController.isShowingLiveLockScreenView();
     }
 
+    public void slideNotificationPanelIn() {
+        mNotificationPanel.slideLockScreenIn();
+    }
+
     private final class ShadeUpdates {
         private final ArraySet<String> mVisibleNotifications = new ArraySet<String>();
         private final ArraySet<String> mNewVisibleNotifications = new ArraySet<String>();
