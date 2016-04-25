18e92714b46e397749d49659eb87a0b48747b7bf
d34d <clark@cyngn.com>
2016-04-25 13:37:58 -0700

    SysUI: Animate notificaiton panel back into view
    Add an animation for when the user has swiped the left edge to bring
    the notificaiton panel back into view from the left side.
    
    Change-Id: Ie2ab3482a39d2d9c7b93a853f6ebae43a174e2c0
    TICKET: CYNGNOS-2549

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1297,7 +1297,10 @@ public class KeyguardViewMediator extends SystemUI {
         mHandler.post(new Runnable() {
             @Override
             public void run() {
+                // Get the keyguard into the correct state by calling mStatusBar.showKeyguard()
                 mStatusBar.showKeyguard();
+                // Now have the notification panel slid back into view
+                mStatusBar.slideNotificationPanelIn();
             }
         });
     }
