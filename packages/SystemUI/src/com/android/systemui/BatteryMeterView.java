ea16fe822befc126b70c52232b0a61932c0fb930
d34d <clark@cyngn.com>
2016-04-25 08:16:03 -0700

    SystemUI: Add VectorDrawable battery styles
    The current implementation of BatteryMeterView uses arrays of points
    that are drawn programmatically.  This limits the ability to overlay
    or theme the battery. By utilizing VectorDrawables and
    AnimatedVectorDrawables we can achieve the same look and feel of
    stock while allowing for more unique battery meters.
    
    Implement portrait, landscape and circle AVD battery styles.
    The various dimensions adjustments are to accommodate the
    slightly different sizes of the AVD battery styles.
    
    Forward port the AVD battery commits from CM 13.0:
    db5cf5dc36c629b9afba8f883ef5e33a33608409 SysUI: Use VectorDrawable's for rendering battery
    d37a5aa57bb1c5d958abe036b5558ba4dac270c5 SysUI: Fix coloring of frame and bolt
    2b21b18e109ddab30d7a0ff47dd900e55167ec37 SysUI: Use linear interpolator for battery level
    5953be013f1f3fe14b5f4176b743c4032e114bfc SysUI: Allow styling of battery text/charge blend mode
    e3ddac3a775d2e8808da40d302794e9b50016d9b SysUI: Adjust battery dimensions for better look
    1530ec9e734cae8bfd966f3c43b3be9041386c29 SystemUI: Set circle battery path as untranslatable
    
    Change-Id: I91b4f5229774c979a7aa9ab771ffcb95ceb71b84

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -66,6 +66,10 @@ public class BatteryMeterView extends ImageView implements
                 com.android.internal.R.string.status_bar_battery);
         setImageDrawable(mDrawable);
 
+        // The BatteryMeterDrawable wants to use the clear xfermode,
+        // so use a separate layer to not make it clear the background with it.
+        setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
         mContext = context;
         mFrameColor = frameColor;
     }
@@ -134,7 +138,7 @@ public class BatteryMeterView extends ImageView implements
                 setImageDrawable(null);
                 break;
             default:
-                mDrawable = new BatteryMeterDrawable(mContext, new Handler(), mFrameColor);
+                mDrawable = new BatteryMeterDrawable(mContext, new Handler(), mFrameColor, style);
                 setImageDrawable(mDrawable);
                 setVisibility(View.VISIBLE);
                 break;
