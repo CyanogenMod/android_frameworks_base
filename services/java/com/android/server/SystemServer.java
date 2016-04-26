cccd3b52943acd1bfa3a232d676733b02f796033
Adnan Begovic <adnan@cyngn.com>
2016-04-26 16:56:49 -0700

    services: Kick off to CMSystemServer for external service init.
    Change-Id: I2dfe51a4cadfe02f0295be36fb30a304219047ff

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -108,6 +108,9 @@ import java.lang.reflect.Method;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -564,6 +567,9 @@ public final class SystemServer {
                 false);
         boolean isEmulator = SystemProperties.get("ro.kernel.qemu").equals("1");
         boolean enableWigig = SystemProperties.getBoolean("persist.wigig.enable", false);
+
+        String externalServer = context.getResources().getString(
+                org.cyanogenmod.platform.internal.R.string.config_externalSystemServer);
 
         try {
             Slog.i(TAG, "Reading configuration...");
@@ -1222,6 +1228,24 @@ public final class SystemServer {
 
         // MMS service broker
         mmsService = mSystemServiceManager.startService(MmsServiceBroker.class);
+
+        final Class<?> serverClazz;
+        try {
+            serverClazz = Class.forName(externalServer);
+            final Constructor<?> constructor = serverClazz.getDeclaredConstructor(Context.class);
+            constructor.setAccessible(true);
+            final Object baseObject = constructor.newInstance(mSystemContext);
+            final Method method = baseObject.getClass().getDeclaredMethod("run");
+            method.setAccessible(true);
+            method.invoke(baseObject);
+        } catch (ClassNotFoundException
+                | IllegalAccessException
+                | InvocationTargetException
+                | InstantiationException
+                | NoSuchMethodException e) {
+            Slog.wtf(TAG, "Unable to start  " + externalServer);
+            Slog.wtf(TAG, e);
+        }
 
         // It is now time to start up the app processes...
 
