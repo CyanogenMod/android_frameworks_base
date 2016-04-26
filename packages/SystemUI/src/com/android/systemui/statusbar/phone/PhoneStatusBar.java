0d29718ffa32c58f23ed766ed344579eeb92e599
d34d <clark@cyngn.com>
2016-04-26 14:56:14 -0700

    SysUI: Don't call removeAllViews on AdapterView
    Change-Id: I957c87e499fb719d1d45671c968c625dac75ecc7
    TICKET: OPO-649

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -106,6 +106,7 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.PathInterpolator;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -3882,7 +3883,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 removeAllViews((ViewGroup) child);
             }
         }
-        parent.removeAllViews();
+
+        if (!(parent instanceof AdapterView)) parent.removeAllViews();
     }
 
     /**
