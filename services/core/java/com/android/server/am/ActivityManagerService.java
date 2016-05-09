75b55fde36030b37f6246aef26d399eeea7bb2bb
Jessica Wagantall <jwagantall@cyngn.com>
2016-05-09 14:45:56 -0700

    Kill the real/isolated uid group, not the ApplicationInfo uid
    This is a direct reimplementation in L of fixes applied to N in
    these two commits:
    
    8dc8d37c1d7d694016f1ec2b3cea5fb723567be8
    e8741d23d2dd05c4cb3fed5ee6a4040ee96a60e3
    
    Bug 19285814
    Ticket: CYNGNOS-2707
    
    Change-Id: I59bcc8f1d41c426e9da635bea9ad1d7c6756d5aa

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2583,7 +2583,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             if (!app.killed) {
                 Slog.wtfStack(TAG, "Removing process that hasn't been killed: " + app);
                 Process.killProcessQuiet(app.pid);
-                Process.killProcessGroup(app.info.uid, app.pid);
+                Process.killProcessGroup(app.uid, app.pid);
             }
             if (lrui <= mLruProcessActivityStart) {
                 mLruProcessActivityStart--;
@@ -2950,7 +2950,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             // clean it up now.
             if (DEBUG_PROCESSES || DEBUG_CLEANUP) Slog.v(TAG, "App died: " + app);
             checkTime(startTime, "startProcess: bad proc running, killing");
-            Process.killProcessGroup(app.info.uid, app.pid);
+            Process.killProcessGroup(app.uid, app.pid);
             handleAppDiedLocked(app, true, true);
             checkTime(startTime, "startProcess: done killing old proc");
         }
@@ -4832,13 +4832,9 @@ public final class ActivityManagerService extends ActivityManagerNative
             stats.noteProcessDiedLocked(app.info.uid, pid);
         }
 
-        if (!app.killed) {
-            if (!fromBinderDied) {
-                Process.killProcessQuiet(pid);
-            }
-            Process.killProcessGroup(app.info.uid, pid);
-            app.killed = true;
-        }
+        Process.killProcessQuiet(pid);
+        Process.killProcessGroup(app.uid, pid);
+        app.killed = true;
 
         // Clean up already done if the process has been re-started.
         if (app.pid == pid && app.thread != null &&
