bc109ada46c51aa1048183b79e2f7db66ff1f526
Roman Birg <roman@cyngn.com>
2016-05-11 10:54:51 -0700

    SystemUI: fix blank qs tile issues
    The blank tiles were an artifact of precaching custom tiles. We need to
    not show them or count them if they don't have an actual tile attached
    to them yet.
    
    Also, allow disabled tiles to be edited.
    
    Ticket: CYNGNOS-2530
    
    Change-Id: Ie6e0e6e18435eb53e9b419a05d780bdd1ee808b7
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -372,10 +372,13 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
 
     protected void drawTile(TileRecord r, QSTile.State state) {
         if (mEditing) {
-            if ((r.tile instanceof CustomQSTile) && ((CustomQSTile) r.tile).isUserRemoved()) {
-                // don't modify visibility state.
+            if ((r.tile instanceof CustomQSTile)
+                    && (((CustomQSTile) r.tile).isUserRemoved()
+                    || ((CustomQSTile) r.tile).getTile() == null)) {
+                // don't modify visibility state if removed, or not yet published
             } else {
                 state.visible = true;
+                state.enabled = true;
             }
         }
         final int visibility = state.visible ? VISIBLE : GONE;
@@ -623,6 +626,13 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
         final Iterator<QSTile<?>> newTileIterator = tilesCollection.iterator();
         while (newTileIterator.hasNext()) {
             QSTile<?> tile = newTileIterator.next();
+            if (tile instanceof CustomQSTile) {
+                if (((CustomQSTile) tile).isUserRemoved()
+                        || ((CustomQSTile) tile).getTile() == null) {
+                    // tile not published yet
+                    continue;
+                }
+            }
             final int tileDestPage = getPagesForCount(runningCount + 1) - 1;
 
             if (DEBUG_TILES) {
