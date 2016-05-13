4ccaf0a84605f10c9d0a7ee90d44da5fb590b59e
Danesh M <danesh@cyngn.com>
2016-05-13 10:52:34 -0700

    SystemUI : Fix multi-user for keyguard
    OPO-696
    Change-Id: I4383eb49abc1f61bf41e7279ff8e0ed2c1dbb6eb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3787,6 +3787,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mLiveLockScreenController.cleanup();
         }
 
+        mKeyguardBottomArea.cleanup();
         mStatusBarWindow.removeContent(mStatusBarWindowContent);
         mStatusBarWindow.clearDisappearingChildren();
 
