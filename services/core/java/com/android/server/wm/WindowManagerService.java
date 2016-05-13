e6d7c4c744313fa0483d4f46dded9df03ed2972b
Juancarlospr5 <tumayordeseo5@gmail.com>
2016-05-13 01:23:01 -0400

    Ajustar velocidad animaciones y transcisiones

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -649,9 +649,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    float mWindowAnimationScaleSetting = 1.0f;
-    float mTransitionAnimationScaleSetting = 1.0f;
-    float mAnimatorDurationScaleSetting = 1.0f;
+    float mWindowAnimationScaleSetting = 0.7f;
+    float mTransitionAnimationScaleSetting = 0.7f;
+    float mAnimatorDurationScaleSetting = 0.7f;
     boolean mAnimationsDisabled = false;
 
     final InputManagerService mInputManager;
