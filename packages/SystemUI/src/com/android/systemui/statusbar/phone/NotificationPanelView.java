e7635fcb4054b58b912f4b9197963a8b05c059c4
Danesh M <danesh@cyngn.com>
2016-05-13 10:52:34 -0700

    SystemUI : Fix multi-user for keyguard
    Change-Id: I4383eb49abc1f61bf41e7279ff8e0ed2c1dbb6eb

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1872,10 +1872,6 @@ public class NotificationPanelView extends PanelView implements
         updateNotificationTranslucency();
         updatePanelExpanded();
         mNotificationStackScroller.setShadeExpanded(!isFullyCollapsed());
-        if (mShowingExternalKeyguard && expandedHeight >= getMaxPanelHeight()) {
-            mStatusBar.unfocusKeyguardExternalView();
-            mShowingExternalKeyguard = false;
-        }
         if (DEBUG) {
             invalidate();
         }
