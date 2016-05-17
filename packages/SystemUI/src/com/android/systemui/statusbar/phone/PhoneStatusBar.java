6a1e01938f50adb7013e0c315b1b7626514e3df1
Danesh M <danesh@cyngn.com>
2016-05-17 16:49:02 -0700

    SystemUI : Fix left swipe when dismissing bouncer
    - Only show bouncer when requesting dismiss, which will set the states
    for us.
    - Also remove duplicate states.
    - Ensure we disable edge gesture when showKeyguard is called
    - Bail out on showKeyguard if already in keyguard.
    
    CYNGNOS-2872
    
    Change-Id: I6be10923de1590b386c270925654669a8f832cfe
    (cherry picked from commit 2cfbb7a359f8fe549d734693727afa4670876eb7)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -928,6 +928,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mLiveLockScreenController = new LiveLockScreenController(mContext, this,
                 mNotificationPanel);
         mNotificationPanel.setLiveController(mLiveLockScreenController);
+        if (mStatusBarWindowManager != null) {
+            mStatusBarWindowManager.setLiveLockscreenController(mLiveLockScreenController);
+        }
 
         if (mHeadsUpManager == null) {
             mHeadsUpManager = new HeadsUpManager(context, mStatusBarWindow);
@@ -3462,6 +3465,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarWindowManager = new StatusBarWindowManager(mContext, mKeyguardMonitor);
         mStatusBarWindowManager.setShowingMedia(mKeyguardShowingMedia);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
+        if (mLiveLockScreenController != null) {
+            mStatusBarWindowManager.setLiveLockscreenController(mLiveLockScreenController);
+        }
     }
 
     // called by makeStatusbar and also by PhoneStatusBarView
@@ -4349,6 +4355,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mAssistManager.onLockscreenShown();
         mKeyguardBottomArea.requestFocus();
+        try {
+            WindowManagerGlobal.getWindowManagerService()
+                    .setLiveLockscreenEdgeDetector(false);
+        } catch (RemoteException e){
+            e.printStackTrace();
+        }
         if (mLiveLockScreenController.isShowingLiveLockScreenView()) {
             mLiveLockScreenController.getLiveLockScreenView().onKeyguardShowing(
                     mStatusBarKeyguardViewManager.isScreenTurnedOn());
@@ -4686,8 +4698,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void showBouncer() {
         if (!mRecreating && mNotificationPanel.mCanDismissKeyguard
                 && (mState != StatusBarState.SHADE || mLiveLockScreenController.getLiveLockScreenHasFocus())) {
-            // ensure external keyguard view does not have focus
-            unfocusKeyguardExternalView();
             mWaitingForKeyguardExit = mStatusBarKeyguardViewManager.isShowing();
             mStatusBarKeyguardViewManager.dismiss();
         }
