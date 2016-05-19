c1542b49b1d38ebc97d8496d77da09e1ee4aae60
d34d <clark@cyngn.com>
2016-05-19 12:48:53 -0700

    SysUI: Fix coloring of battery drawables
    The frame color is now correctly tinted based on background color
    set by setDarkIntensity()
    
    Battery bolt and percentage text have their alpha set to full opacity
    to increase visibility when drawn on a light colored status bar.
    
    Change-Id: I907101ed1465f99b92370332d4839b7f0aad13b2

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -537,7 +537,9 @@ public class BatteryMeterView extends View implements DemoMode,
         @Override
         public void setDarkIntensity(int backgroundColor, int fillColor) {
             mIconTint = fillColor;
-            mBoltDrawable.setTint(fillColor);
+            // make bolt drawable tint fully opaque so it stands out
+            mBoltDrawable.setTint(0xff000000 | fillColor);
+            mFrameDrawable.setTint(backgroundColor);
             updateBoltDrawableLayer(mBatteryDrawable, mBoltDrawable);
             invalidate();
         }
@@ -660,7 +662,8 @@ public class BatteryMeterView extends View implements DemoMode,
                     && (mShowPercent && !(level == 100 && !SHOW_100_PERCENT))) {
                 // draw the percentage text
                 String pctText = String.valueOf(SINGLE_DIGIT_PERCENT ? (level/10) : level);
-                mTextAndBoltPaint.setColor(getColorForLevel(level));
+                // make paint color fully opaque so text stands out better
+                mTextAndBoltPaint.setColor(0xff000000 | getColorForLevel(level));
                 canvas.drawText(pctText, mTextX, mTextY, mTextAndBoltPaint);
             } else if (level <= mCriticalLevel) {
                 // draw the warning text
