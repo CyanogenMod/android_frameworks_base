fc2b615a15f638fe019a9cefba2afebb400ff52d
Juancarlospr5 <tumayordeseo5@gmail.com>
2016-05-31 00:59:51 -0400

    Revert parcial "base: add auto brightness toggle to qs"

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1195,15 +1195,18 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // Set up the quick settings tile panel
         mQSPanel = (QSDragPanel) mStatusBarWindowContent.findViewById(R.id.quick_settings_panel);
         if (mQSPanel != null) {
-            final QSTileHost qsh = new QSTileHost(mContext, this,
-                    mBluetoothController, mLocationController, mRotationLockController,
-                    mNetworkController, mZenModeController, mHotspotController,
-                    mCastController, mFlashlightController,
-                    mUserSwitcherController, mKeyguardMonitor,
-                    mSecurityController);
-            mQSPanel.setHost(qsh);
-            mQSPanel.setTiles(qsh.getTiles());
-            mBrightnessMirrorController = new BrightnessMirrorController(mContext, mStatusBarWindowContent);
+            if (mQSTileHost == null) {
+                 mQSTileHost = new QSTileHost(mContext, this,
+                         mBluetoothController, mLocationController, mRotationLockController,
+                         mNetworkController, mZenModeController, mHotspotController,
+                         mCastController, mFlashlightController,
+                         mUserSwitcherController, mKeyguardMonitor,
+                         mSecurityController, mBatteryController);
+             }
+             mQSPanel.setHost(mQSTileHost);
+             if (mBrightnessMirrorController == null) {
+                 mBrightnessMirrorController = new BrightnessMirrorController(mContext, mStatusBarWindowContent);
+             }
             mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
             mQSPanel.setTiles(mQSTileHost.getTiles());
             mHeader.setQSPanel(mQSPanel);
