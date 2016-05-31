a1cb6d55b9db2208a03ea73c954ec4c8a1679a6f
Michael W <baddaemon87@gmail.com>
2016-05-31 10:40:39 +0200

    isRoaming: fix possible NPE
    Looks like isRoaming() is called with
    mServiceState being null sometimes
    Check for non null as well
    
    (Reference:
    BugReport 13-20160520-26 L #36)
    
    Change-Id: I90a7feb2f5b72a4b94bbb4e7a5c4f486b416143b

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -271,7 +271,7 @@ public class MobileSignalController extends SignalController<
     }
 
     private boolean isCdma() {
-        return (mSignalStrength != null) && !mSignalStrength.isGsm();
+        return (mServiceState != null) && (mSignalStrength != null) && !mSignalStrength.isGsm();
     }
 
     public boolean isEmergencyOnly() {
