35f7282ca1135214f85e964e0bd2f95c4489d5e3
Sultanxda <sultanxda@gmail.com>
2016-06-02 10:09:38 -0700

    fw: Fix power key interception for PRIVATE_FLAG_PREVENT_POWER_KEY
    TICKET: CYNGNOS-2958
    Change-Id: I67a7469257a1617b1c999f5533c5863014595dcc
    (cherry picked from commit ce709c9f22d500606d20c682298e937bd32e14d9)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3265,8 +3265,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             if (mTopFullscreenOpaqueWindowState != null &&
                     (mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
-                            & (WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS |
-                                WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY)) != 0
+                            & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS) != 0
                     && mScreenOnFully) {
                 return 0;
             }
@@ -5855,7 +5854,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_POWER: {
                 if (mTopFullscreenOpaqueWindowState != null &&
                         (mTopFullscreenOpaqueWindowState.getAttrs().privateFlags
-                        & WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS) != 0
+                        & (WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_SYSTEM_KEYS |
+                            WindowManager.LayoutParams.PRIVATE_FLAG_PREVENT_POWER_KEY)) != 0
                         && mScreenOnFully) {
                     return result;
                 }
