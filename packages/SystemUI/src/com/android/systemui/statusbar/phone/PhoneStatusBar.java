f92577f8f4f24679f560f392437436e684789462
Danesh M <danesh@cyngn.com>
2016-06-03 11:55:04 -0700

    Only show keyguard panel if on lockscreen + no activity on top
    There is a bug in window manager, where if the keyguard is collapsed,
    the reference to the window is lost when destroying its surface. This in
    turn prevents the ime window from being reordered later on. For normal apps
    this is not an issue, but since we're above normal apps (KEYGUARD_PANEL),
    this rises an issue for us in this scenario :
    
    1) Set password lockscreen
    2) Swipe up to unlock
    3) Collapse keyboard
    4) Launch emergency dialer
    5) Press home button
    
    Notice that all touch events are going to the external keyguard and the
    reason is, window manager placed it above TYPE_INPUT since thats what its
    layer definition is and it never had a chance to re-order the input window
    since it lost reference in mInputMethodWindow
    
    Change-Id: I59dd41547902ae5e23bbeb0a01ee27f11fc29dfc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4536,6 +4536,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isSecure();
     }
 
+    public boolean isKeyguardInputRestricted() {
+        return mStatusBarKeyguardViewManager != null && mStatusBarKeyguardViewManager.isInputRestricted();
+    }
+
     public long calculateGoingToFullShadeDelay() {
         return mKeyguardFadingAwayDelay + mKeyguardFadingAwayDuration;
     }
