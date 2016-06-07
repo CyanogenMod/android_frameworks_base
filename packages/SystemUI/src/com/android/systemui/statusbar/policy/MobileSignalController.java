423a82e0870ff4fdf6ea60f5a56b37be062557b5
Diogo Ferreira <defer@cyngn.com>
2016-06-07 13:55:49 +0100

    systemui: Fix compilation failure
    An invisible character was breaking the build.
    
    Change-Id: I5946edce7b9a9eed1fad95033c3988bfeef67812

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -279,7 +279,7 @@ public class MobileSignalController extends SignalController<
     }
 
     private boolean isRoaming() {
-        if (mServiceState == null) {
+        if (mServiceState == null) {
             return false;
         } else if (isCdma()) {
             final int iconMode = mServiceState.getCdmaEriIconMode();
