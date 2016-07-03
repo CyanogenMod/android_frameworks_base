aca55d090cc249da2bdb7dd78389e9e441d156af
LuK1337 <priv.luk@gmail.com>
2016-07-04 01:11:54 +0200

    SystemUI: Hide unprovisioned SIMs
    * There's no point of displaying null signal icon.
    
    Change-Id: Idf077c89c5e0894901cf1a553e18930c0c61bacd

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -22,7 +22,10 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.util.ArraySet;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -33,6 +36,7 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.android.internal.telephony.IExtTelephony;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.policy.NetworkController.IconState;
@@ -225,11 +229,24 @@ public class SignalClusterView
     public void setMobileDataIndicators(IconState statusIcon, IconState qsIcon, int statusType,
             int qsType, boolean activityIn, boolean activityOut, String typeContentDescription,
             String description, boolean isWide, boolean showRoamingIndicator, int subId) {
+        boolean hideSim = false;
+        int slotId = SubscriptionManager.getSlotId(subId);
         PhoneState state = getState(subId);
+        IExtTelephony mExtTelephony = IExtTelephony.Stub.
+                asInterface(ServiceManager.getService("extphone"));
         if (state == null) {
             return;
         }
-        state.mMobileVisible = statusIcon.visible && !mBlockMobile;
+
+        try {
+            if (mExtTelephony != null && mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId) == 0) {
+                hideSim = true;
+            }
+        } catch (RemoteException e) {
+            // Do nothing, we don't want log spam or anything.
+        }
+
+        state.mMobileVisible = !hideSim && statusIcon.visible && !mBlockMobile;
         state.mMobileStrengthId = statusIcon.icon;
         state.mMobileTypeId = statusType;
         state.mMobileDescription = statusIcon.contentDescription;
