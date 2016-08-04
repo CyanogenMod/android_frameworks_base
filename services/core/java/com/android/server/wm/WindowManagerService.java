a92b91d7966350ad37f5c60586e5ccb9abc696be
Scott Mertz <scott@cyngn.com>
2016-08-04 15:17:01 -0700

    fw: enforce android.permission.PREVENT_SYSTEM_KEYS in system server
    Since privateFlags can be accessed and modified via reflection
    due to change 5d927c2d8e, the prevent system keys or prevent
    power keys flag can be set and deployed without ever calling
    setPrivateFlags directly.
    
    Move enforcing to system server to prevent app tampering.
    
    TICKET: CYAN-7921 (open source JIRA)
    Found by Maciej Krysztofiak
    
    Change-Id: I53c2804d0283692036f5cc95e6ad57821bc0655a

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3164,7 +3164,9 @@ public class WindowManagerService extends IWindowManager.Stub
             }
 
             if (attrs != null) {
+                Binder.restoreCallingIdentity(origId);
                 mPolicy.adjustWindowParamsLw(attrs);
+                origId = Binder.clearCallingIdentity();
             }
 
             // if they don't have the permission, mask out the status bar bits
