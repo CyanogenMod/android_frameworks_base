aa3c7499b9c56171c71e698064d00eaf4cfcba06
Steve Kondik <steve@cyngn.com>
2016-08-30 18:11:35 -0700

    systemui: Fix compilation
    Change-Id: I03fad10b746e2bba81c30ba396a5af92b1ea75c9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BaseStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BaseStatusBarHeader.java
@@ -25,6 +25,7 @@ import com.android.systemui.statusbar.policy.BatteryController;
 import com.android.systemui.statusbar.policy.NetworkControllerImpl;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.UserInfoController;
+import com.android.systemui.statusbar.policy.WeatherController;
 
 public abstract class BaseStatusBarHeader extends RelativeLayout implements
         NetworkControllerImpl.EmergencyListener {
@@ -46,4 +47,5 @@ public abstract class BaseStatusBarHeader extends RelativeLayout implements
     public abstract void setNextAlarmController(NextAlarmController nextAlarmController);
     public abstract void setUserInfoController(UserInfoController userInfoController);
     public abstract void setCallback(Callback qsPanelCallback);
+    public abstract void setWeatherController(WeatherController weatherController);
 }
