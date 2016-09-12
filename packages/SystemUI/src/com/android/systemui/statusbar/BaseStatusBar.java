541e989f8bab9e33615829f744284a84ab9e8a1a
Alexander Martinz <eviscerationls@gmail.com>
2016-09-12 17:47:25 +0200

    systemui: fix NPE when opening now on tap
    Change-Id: Id99396c826f7fe3e10c29964df0699014811db07
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -942,6 +942,10 @@ public abstract class BaseStatusBar extends SystemUI implements
             mLayoutDirection = ld;
             refreshLayout(ld);
         }
+
+        if (mAssistManager != null) {
+            mAssistManager.onConfigurationChanged();
+        }
     }
 
     protected void onDensityOrFontScaleChanged() {
