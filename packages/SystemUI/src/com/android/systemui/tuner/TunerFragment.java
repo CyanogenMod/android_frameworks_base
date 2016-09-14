311db3cbfbf89bff9bea1133183c6d485b0656b0
Steve Kondik <steve@cyngn.com>
2016-09-14 05:08:02 -0700

    systemui: Fix a few issues with Tuner
     * Allow launching PreferenceScreens directly by setting the
       "tuner" extra to a valid key.
     * Remove the silly warning. There are no dragons here.
     * Move CM stuff to CM manifest.
     * Fix the ActionBar title when navigating.
    
    Change-Id: Ideac31dbfd71d7c0aa7fc3c20395c24128c598fc

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -66,12 +66,16 @@ public class TunerFragment extends PreferenceFragment {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.tuner_prefs);
 
+        /**
+         * OHAI
         if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
                 0) == 0) {
             if (getFragmentManager().findFragmentByTag(WARNING_TAG) == null) {
                 new TunerWarningFragment().show(getFragmentManager(), WARNING_TAG);
             }
         }
+         */
+
     }
 
     @Override
