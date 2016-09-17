51b1a575c79d9de5eca5550977edb818d5620e10
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2016-09-17 13:11:23 -0700

    Fix build on MacOS due to use of deprecated APIs using Xcode 8
    Change-Id: I99f6e5c2d5e323ae3e7afef6c86d62e34256ad7b

--- tools/aapt2/Android.mk
+++ tools/aapt2/Android.mk
@@ -139,7 +139,7 @@ hostLdLibs_linux := -lz
 hostLdLibs_darwin := -lz
 
 cFlags := -Wall -Werror -Wno-unused-parameter -UNDEBUG
-cFlags_darwin := -D_DARWIN_UNLIMITED_STREAMS
+cFlags_darwin := -D_DARWIN_UNLIMITED_STREAMS -Wno-deprecated-declarations
 cFlags_windows := -Wno-maybe-uninitialized # Incorrectly marking use of Maybe.value() as error.
 cppFlags := -std=c++11 -Wno-missing-field-initializers -fno-exceptions -fno-rtti
 protoIncludes := $(call generated-sources-dir-for,STATIC_LIBRARIES,libaapt2,HOST)
