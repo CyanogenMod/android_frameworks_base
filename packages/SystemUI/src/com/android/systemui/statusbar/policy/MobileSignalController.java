f6df92c7f1ec0b486f539115f462ca78049aff92
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-24 15:45:10 +0800

    Revert "Support to show long or short name in SystemUI."
    This config option introduced by CAF seems pretty pointless.
    Revert the original CAF commit to reduce deviation from AOSP.
    
    This essentially reverts commit 9bb392c0fcdb41870cd7b1c004250e92bbf0672f.
    
    Change-Id: I8439467f26ae1d612146f1213dfa90e8c0fecc7e

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -625,12 +625,6 @@ public class MobileSignalController extends SignalController<
             mCurrentState.networkName = mServiceState.getOperatorAlphaShort();
         }
 
-        if (!showLongOperatorName() && mServiceState != null) {
-            mCurrentState.networkNameData = TextUtils.isEmpty(mServiceState.
-                    getOperatorAlphaShort()) ? mCurrentState.networkNameData : mServiceState.
-                    getOperatorAlphaShort() + " " + getNetworkClassString(mServiceState);
-        }
-
         if (mConfig.readIconsFromXml) {
             mCurrentState.voiceLevel = getVoiceSignalLevel();
         }
@@ -644,15 +638,6 @@ public class MobileSignalController extends SignalController<
 
     private boolean isDataDisabled() {
         return !mPhone.getDataEnabled(mSubscriptionInfo.getSubscriptionId());
-    }
-
-    private boolean showLongOperatorName() {
-        if (mContext.getResources().getBoolean(R.bool.config_show_long_operator_name) || (mContext.
-                getResources().getBoolean(R.bool.config_show_long_operator_name_when_roaming) &&
-                isRoaming())) {
-            return true;
-        }
-        return false;
     }
 
     private void generateIconGroup() {
