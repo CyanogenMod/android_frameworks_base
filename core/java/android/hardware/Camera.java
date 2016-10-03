5c17aa2901a5c395f6a80afd7bb96b9dd8237ab4
Steve Kondik <steve@cyngn.com>
2016-10-02 23:21:41 -0700

    camera: Tolerate errors in getHorizontal/VerticalViewAngle
     * Don't try to parse "nan"
    
    Change-Id: I5fdfc3055b0c8514c3848ae3c26788478bfda9ae

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -3795,7 +3795,12 @@ public class Camera {
          *         valid value.
          */
         public float getHorizontalViewAngle() {
-            return Float.parseFloat(get(KEY_HORIZONTAL_VIEW_ANGLE));
+            try {
+                return Float.parseFloat(get(KEY_HORIZONTAL_VIEW_ANGLE));
+            } catch (NumberFormatException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+            return 0;
         }
 
         /**
@@ -3805,7 +3810,12 @@ public class Camera {
          *         valid value.
          */
         public float getVerticalViewAngle() {
-            return Float.parseFloat(get(KEY_VERTICAL_VIEW_ANGLE));
+            try {
+                return Float.parseFloat(get(KEY_VERTICAL_VIEW_ANGLE));
+            } catch (NumberFormatException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+            return 0;
         }
 
         /**
