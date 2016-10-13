317cefec0190b3b894f54aa9370d3d0269fac850
Steve Kondik <steve@cyngn.com>
2016-10-13 04:32:21 -0700

    windowmanager: Add support for blur effects
      * Support for blur layer on the lockscreen
      * Additional code for blur-behind and surface-blur has
        been removed from this commit as it is not in use.
    
      * Original work by CodeAurora / Qualcomm
      * Contributions from:
        - Steve Kondik
        - Danesh Mondegarian
        - Roman Birg
        - Clark Scheff
        - Michael Bestas
    
    Change-Id: I6285905a4e3c00dfb5471876d0cfb16b4b4e9893

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1286,6 +1286,19 @@ public interface WindowManager extends ViewManager {
         public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
 
         /**
+         * Window flag: adding additional blur layer and set this as masking layer
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING = 0x40000000;
+
+        /**
+         * Window flag: adding additional blur layer and set this as masking layer.
+         * This is faster and ugglier than non-scaled version.
+         * {@hide}
+         */
+        public static final int PRIVATE_FLAG_BLUR_WITH_MASKING_SCALED = 0x80000000;
+
+        /**
          * Control flags that are private to the platform.
          * @hide
          */
@@ -1557,6 +1570,15 @@ public interface WindowManager extends ViewManager {
         public float dimAmount = 1.0f;
 
         /**
+         * When {@link #FLAG_BLUR_BEHIND} is set, this is the amount of blur
+         * to apply.  Range is from 1.0 for maximum to 0.0 for no
+         * blur.
+         * @hide
+         */
+        public float blurAmount = 1.0f;
+
+
+        /**
          * Default value for {@link #screenBrightness} and {@link #buttonBrightness}
          * indicating that the brightness value is not overridden for this window
          * and normal brightness policy should be used.
@@ -1775,6 +1797,14 @@ public interface WindowManager extends ViewManager {
          */
         public long removeTimeoutMilliseconds = -1;
 
+        /**
+         * Threshold value that blur masking layer uses to determine whether
+         * to use or discard the blurred color.
+         * Value should be between 0.0 and 1.0
+         * @hide
+         */
+        public float blurMaskAlphaThreshold = 0.0f;
+
         public LayoutParams() {
             super(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
             type = TYPE_APPLICATION;
@@ -1886,6 +1916,7 @@ public interface WindowManager extends ViewManager {
             out.writeInt(windowAnimations);
             out.writeFloat(alpha);
             out.writeFloat(dimAmount);
+            out.writeFloat(blurAmount);
             out.writeFloat(screenBrightness);
             out.writeFloat(buttonBrightness);
             out.writeInt(rotationAnimation);
@@ -1910,6 +1941,7 @@ public interface WindowManager extends ViewManager {
             out.writeInt(accessibilityIdOfAnchor);
             TextUtils.writeToParcel(accessibilityTitle, out, parcelableFlags);
             out.writeLong(removeTimeoutMilliseconds);
+            out.writeFloat(blurMaskAlphaThreshold);
         }
 
         public static final Parcelable.Creator<LayoutParams> CREATOR
@@ -1940,6 +1972,7 @@ public interface WindowManager extends ViewManager {
             windowAnimations = in.readInt();
             alpha = in.readFloat();
             dimAmount = in.readFloat();
+            blurAmount = in.readFloat();
             screenBrightness = in.readFloat();
             buttonBrightness = in.readFloat();
             rotationAnimation = in.readInt();
@@ -1964,6 +1997,7 @@ public interface WindowManager extends ViewManager {
             accessibilityIdOfAnchor = in.readInt();
             accessibilityTitle = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(in);
             removeTimeoutMilliseconds = in.readLong();
+            blurMaskAlphaThreshold = in.readFloat();
         }
 
         @SuppressWarnings({"PointlessBitwiseExpression"})
@@ -2006,6 +2040,10 @@ public interface WindowManager extends ViewManager {
         public static final int ACCESSIBILITY_ANCHOR_CHANGED = 1 << 24;
         /** {@hide} */
         public static final int ACCESSIBILITY_TITLE_CHANGED = 1 << 25;
+        /** {@hide} */
+        public static final int BLUR_AMOUNT_CHANGED = 1 << 29;
+        /** {@hide} */
+        public static final int BLUR_MASK_ALPHA_THRESHOLD_CHANGED = 1 << 30;
         /** {@hide} */
         public static final int EVERYTHING_CHANGED = 0xffffffff;
 
@@ -2102,6 +2140,10 @@ public interface WindowManager extends ViewManager {
                 dimAmount = o.dimAmount;
                 changes |= DIM_AMOUNT_CHANGED;
             }
+            if (blurAmount != o.blurAmount) {
+                blurAmount = o.blurAmount;
+                changes |= BLUR_AMOUNT_CHANGED;
+            }
             if (screenBrightness != o.screenBrightness) {
                 screenBrightness = o.screenBrightness;
                 changes |= SCREEN_BRIGHTNESS_CHANGED;
@@ -2186,6 +2228,11 @@ public interface WindowManager extends ViewManager {
 
             // This can't change, it's only set at window creation time.
             removeTimeoutMilliseconds = o.removeTimeoutMilliseconds;
+
+            if (blurMaskAlphaThreshold != o.blurMaskAlphaThreshold) {
+                blurMaskAlphaThreshold = o.blurMaskAlphaThreshold;
+                changes |= BLUR_MASK_ALPHA_THRESHOLD_CHANGED;
+            }
 
             return changes;
         }
