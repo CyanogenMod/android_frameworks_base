c09ef79c5d7e7b65e43757048547338625996ce0
Steve Kondik <steve@cyngn.com>
2016-10-13 04:32:21 -0700

    windowmanager: Add support for blur effects
      * Support for blur layer on the lockscreen
      * Additional code for blur-behind and surface-blur has
        been removed from this commit as it is not in use.
    
      * Original work by CodeAurora / Qualcomm
      * Contributions from:
        - Steve Kondik
        - Danesh Mondegarian
        - Roman Birg
        - Clark Scheff
        - Michael Bestas
    
    Change-Id: I6285905a4e3c00dfb5471876d0cfb16b4b4e9893

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -181,6 +181,7 @@ public class PhoneStatusBarView extends PanelBar {
         super.panelExpansionChanged(frac, expanded);
         mPanelFraction = frac;
         updateScrimFraction();
+        mBar.setBlur(frac);
     }
 
     private void updateScrimFraction() {
