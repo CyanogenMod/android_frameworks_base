9204422284a8752f0657f3c033759672244a7696
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-17 22:00:05 +0800

    SystemUI: Implement battery level text customisations
    This squashes the battery text-related changes in the commits:
    2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    738f5a87a361c9c1027e3764072a37a50467fe6d SystemUI: Expose values in status bar expanded header
    9500061a111fed25bed3ad30173d8e35063f32d0 Themes: Expose hard code for themes in SystemUI
    
    Change-Id: Idec48a3536a3dd529f7dd6076bf33a20cd8020c1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -116,6 +116,7 @@ import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
 import com.android.systemui.AutoReinflateContainer;
 import com.android.systemui.AutoReinflateContainer.InflateListener;
+import com.android.systemui.BatteryLevelTextView;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogConstants;
@@ -1100,6 +1101,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
                 mBatteryController);
+        ((BatteryLevelTextView) mStatusBarView.findViewById(R.id.battery_level))
+                .setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setWeatherController(mWeatherController);
 
