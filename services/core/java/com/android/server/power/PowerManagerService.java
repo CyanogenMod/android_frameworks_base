1a1607790c1bb4a09ca2344161d82c8c46b9a2f5
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2016-10-23 12:42:25 +0300

    Let InCallUI handle proximity sensor for incoming calls [1/2]
    Change-Id: Ibf5c4a0c9ea38f45d70e0ab544a48bab3a79e5f3

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -220,6 +220,8 @@ public final class PowerManagerService extends SystemService
     private int mKeyboardBrightness;
     private int mKeyboardBrightnessSettingDefault;
 
+    private TelephonyManager mTelephonyManager;
+
     private final Object mLock = new Object();
 
     // A bitfield that indicates what parts of the power state have
@@ -4041,6 +4043,14 @@ public final class PowerManagerService extends SystemService
         } else {
             r.run();
         }
+    }
+
+    TelephonyManager getTelephonyManager() {
+        if (mTelephonyManager == null) {
+            mTelephonyManager = (TelephonyManager)mContext.getSystemService(
+                    Context.TELEPHONY_SERVICE);
+        }
+        return mTelephonyManager;
     }
 
     private void runPostProximityCheck(final Runnable r) {
