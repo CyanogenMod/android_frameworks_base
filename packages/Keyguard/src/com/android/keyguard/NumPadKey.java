2b1f9e7b42af4fb7cf32d8ad271f0cf0412e7532
Sam Mortimer <sam@mortimer.me.uk>
2016-10-26 14:44:48 -0700

    Remove duplicate onDetachedFromWindow() function definition
    Change-Id: I02c2168a8fd9317cdd8c9bc46222410dc2cb6f7e

--- packages/Keyguard/src/com/android/keyguard/NumPadKey.java
+++ packages/Keyguard/src/com/android/keyguard/NumPadKey.java
@@ -108,14 +108,6 @@ public class NumPadKey extends ViewGroup {
         setContentDescription(mDigitText.getText().toString());
     }
 
-    @Override
-    public void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-
-        // Reset the "announced headset" flag when detached.
-        ObscureSpeechDelegate.sAnnouncedHeadset = false;
-    }
-
     public void setDigit(int digit) {
         mDigit = digit;
         updateText();
