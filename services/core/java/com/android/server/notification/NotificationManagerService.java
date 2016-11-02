a5219a2adbdce6e10e5ff26ac6245c7106832b7a
Danny Baumann <dannybaumann@web.de>
2016-11-02 10:16:01 +0100

    Revert "Mms: Fix no ringtone for MT SMS during the call."
    This reverts commit 2e0f419190512e7d548e92c1c4ed25b8b9f8cc18.
    
    Change-Id: Iaad6a2e3ccb6f9edcd4e7014fd55bef5cc14ae1f

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -98,7 +98,6 @@ import android.os.IInterface;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Parcelable;
-import android.os.PersistableBundle;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
@@ -117,7 +116,6 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.NotificationRankingUpdate;
 import android.service.notification.StatusBarNotification;
 import android.service.notification.ZenModeConfig;
-import android.telephony.CarrierConfigManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -338,8 +336,6 @@ public class NotificationManagerService extends SystemService {
     private RankingHandler mRankingHandler;
     private long mLastOverRateLogTime;
     private float mMaxPackageEnqueueRate = DEFAULT_MAX_NOTIFICATION_ENQUEUE_RATE;
-    private PersistableBundle mCarrierConfig;
-    private CarrierConfigManager mConfigManager;
     private String mSystemNotificationSound;
 
     private static class Archive {
@@ -1225,8 +1221,6 @@ public class NotificationManagerService extends SystemService {
 
         publishBinderService(Context.NOTIFICATION_SERVICE, mService);
         publishLocalService(NotificationManagerInternal.class, mInternalService);
-        mConfigManager = (CarrierConfigManager)
-                getContext().getSystemService(Context.CARRIER_CONFIG_SERVICE);
     }
 
     private void sendRegisteredOnlyBroadcast(String action) {
@@ -2982,14 +2976,7 @@ public class NotificationManagerService extends SystemService {
         boolean hasValidVibrate = false;
         boolean hasValidSound = false;
 
-        boolean smsRingtone =  false;
-        if (mCarrierConfig == null) {
-            mCarrierConfig = mConfigManager.getConfig();
-        } else {
-            smsRingtone = mCarrierConfig.getBoolean(
-                CarrierConfigManager.KEY_CONFIG_SMS_RINGTONE_INCALL);
-        }
-        if ((disableEffects == null || (smsRingtone && mInCall))
+        if (disableEffects == null
                 && (record.getUserId() == UserHandle.USER_ALL ||
                     record.getUserId() == currentUser ||
                     mUserProfiles.isCurrentProfile(record.getUserId()))
